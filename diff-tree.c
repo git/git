c5b423860d4a0f2c1144c578207f327e0985bba7
Linus Torvalds <torvalds@ppc970.osdl.org>
2005-04-23 22:08:00 -0700

    Support a fine-grained diff-tree
    This is based on a patch by David Woodhouse, but with the selection
    tests much simplified and streamlined.
    
    It makes diff-tree take extra arguments, specifying the files or         
    directories which should be considered "interesting". Changes in
    uninteresting directories are not reported.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- diff-tree.c
+++ diff-tree.c
@@ -3,6 +3,11 @@
 static int recursive = 0;
 static int line_termination = '\n';
 
+// What paths are we interested in?
+static int nr_paths = 0;
+static char **paths = NULL;
+static int *pathlens = NULL;
+
 static int diff_tree_sha1(const unsigned char *old, const unsigned char *new, const char *base);
 
 static void update_tree_entry(void **bufp, unsigned long *sizep)
@@ -131,9 +136,63 @@ static int compare_tree_entry(void *tree1, unsigned long size1, void *tree2, uns
 	return 0;
 }
 
+static int interesting(void *tree, unsigned long size, const char *base)
+{
+	const char *path;
+	unsigned mode;
+	int i;
+	int baselen, pathlen;
+
+	if (!nr_paths)
+		return 1;
+
+	(void)extract(tree, size, &path, &mode);
+
+	pathlen = strlen(path);
+	baselen = strlen(base);
+
+	for (i=0; i < nr_paths; i++) {
+		const char *match = paths[i];
+		int matchlen = pathlens[i];
+
+		if (baselen >= matchlen) {
+			/* If it doesn't match, move along... */
+			if (strncmp(base, match, matchlen))
+				continue;
+
+			/* The base is a subdirectory of a path which was specified. */
+			return 1;
+		}
+
+		/* Does the base match? */
+		if (strncmp(base, match, baselen))
+			continue;
+
+		match += baselen;
+		matchlen -= baselen;
+
+		if (pathlen > matchlen)
+			continue;
+
+		if (strncmp(path, match, pathlen))
+			continue;
+
+		return 1;
+	}
+	return 0; /* No matches */
+}
+
 static int diff_tree(void *tree1, unsigned long size1, void *tree2, unsigned long size2, const char *base)
 {
 	while (size1 | size2) {
+		if (nr_paths && size1 && !interesting(tree1, size1, base)) {
+			update_tree_entry(&tree1, &size1);
+			continue;
+		}
+		if (nr_paths && size2 && !interesting(tree2, size2, base)) {
+			update_tree_entry(&tree2, &size2);
+			continue;
+		}
 		if (!size1) {
 			show_file("+", tree2, size2, base);
 			update_tree_entry(&tree2, &size2);
@@ -184,8 +243,12 @@ int main(int argc, char **argv)
 {
 	unsigned char old[20], new[20];
 
-	while (argc > 3) {
+	for (;;) {
 		char *arg = argv[1];
+
+		if (!arg || *arg != '-')
+			break;
+
 		argv++;
 		argc--;
 		if (!strcmp(arg, "-r")) {
@@ -199,7 +262,18 @@ int main(int argc, char **argv)
 		usage(diff_tree_usage);
 	}
 
-	if (argc != 3 || get_sha1_hex(argv[1], old) || get_sha1_hex(argv[2], new))
+	if (argc < 3 || get_sha1_hex(argv[1], old) || get_sha1_hex(argv[2], new))
 		usage(diff_tree_usage);
+
+	if (argc > 3) {
+		int i;
+
+		paths = &argv[3];
+		nr_paths = argc - 3;
+		pathlens = malloc(nr_paths * sizeof(int));
+		for (i=0; i<nr_paths; i++)
+			pathlens[i] = strlen(paths[i]);
+	}
+
 	return diff_tree_sha1(old, new, "");
 }
