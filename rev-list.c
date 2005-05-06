fcfda02bc7e3a9dacf63fa43df8eee530a471f96
Kay Sievers <kay.sievers@vrfy.org>
2005-05-06 10:00:11 +0200

    [PATCH] control/limit output of git-rev-list
    gitweb.cgi's default view is the log of the last day and git-rev-list
    can stop crawling the whole repo if we have all our data to display in the
    browser. Also the rss-feed query needs only the last 20 items. This
    will speeds up these queries dramatically.
    
      usage: rev-list [OPTION] commit-id
        --max-count=nr
        --max-age=epoch
        --min-age=epoch
    
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- rev-list.c
+++ rev-list.c
@@ -6,9 +6,31 @@ int main(int argc, char **argv)
 	unsigned char sha1[20];
 	struct commit_list *list = NULL;
 	struct commit *commit;
+	char *commit_arg = NULL;
+	int i;
+	unsigned long max_age = -1;
+	unsigned long min_age = -1;
+	int max_count = -1;
 
-	if (argc != 2 || get_sha1(argv[1], sha1))
-		usage("rev-list <commit-id>");
+	for (i = 1 ; i < argc; i++) {
+		char *arg = argv[i];
+
+		if (!strncmp(arg, "--max-count=", 12)) {
+			max_count = atoi(arg + 12);
+		} else if (!strncmp(arg, "--max-age=", 10)) {
+			max_age = atoi(arg + 10);
+		} else if (!strncmp(arg, "--min-age=", 10)) {
+			min_age = atoi(arg + 10);
+		} else {
+			commit_arg = arg;
+		}
+	}
+
+	if (!commit_arg || get_sha1(commit_arg, sha1))
+		usage("usage: rev-list [OPTION] commit-id\n"
+		      "  --max-count=nr\n"
+		      "  --max-age=epoch\n"
+		      "  --min-age=epoch\n");
 
 	commit = lookup_commit(sha1);
 	if (!commit || parse_commit(commit) < 0)
@@ -17,6 +39,13 @@ int main(int argc, char **argv)
 	commit_list_insert(commit, &list);
 	do {
 		struct commit *commit = pop_most_recent_commit(&list, 0x1);
+
+		if (min_age != -1 && (commit->date > min_age))
+			continue;
+		if (max_age != -1 && (commit->date < max_age))
+			break;
+		if (max_count != -1 && !max_count--)
+			break;
 		printf("%s\n", sha1_to_hex(commit->object.sha1));
 	} while (list);
 	return 0;
