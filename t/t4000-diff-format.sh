13ab4462d2aefb252d7c916bd537151856b7c967
Junio C Hamano <junkio@cox.net>
2005-05-13 22:52:42 -0700

    The test of the basic diff functionality
    This test comes from "[PATCH 2/2] The core GIT tests: recent additions and
    fixes" but couldn't be included before since it depended on the modechange
    diff output changes.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Petr Baudis <pasky@ucw.cz>

---
+++ t/t4000-diff-format.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Copyright (c) 2005 Junio C Hamano
+#
+
+test_description='Test built-in diff output engine.
+
+'
+. ./test-lib.sh
+
+echo >path0 'Line 1
+Line 2
+line 3'
+cat path0 >path1
+chmod +x path1
+
+test_expect_success \
+    'update-cache --add two files with and without +x.' \
+    'git-update-cache --add path0 path1'
+
+mv path0 path0-
+sed -e 's/line/Line/' <path0- >path0
+chmod +x path0
+rm -f path1
+test_expect_success \
+    'git-diff-files -p after editing work tree.' \
+    'git-diff-files -p >current'
+cat >expected <<\EOF
+# mode: 100644 100755 path0
+--- a/path0
++++ b/path0
+@@ -1,3 +1,3 @@
+ Line 1
+ Line 2
+-line 3
++Line 3
+# mode: 100755 . path1
+--- a/path1
++++ /dev/null
+@@ -1,3 +0,0 @@
+-Line 1
+-Line 2
+-line 3
+EOF
+
+test_expect_success \
+    'validate git-diff-files -p output.' \
+    'cmp -s current expected'
+
+test_done
