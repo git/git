2f34ba32dde0131ab280f8ca902f04e39af7dcc0
Linus Torvalds <torvalds@g5.osdl.org>
2005-07-05 15:45:00 -0700

    Fix silly thinko in "head_ref()"
    It did a "for_each_ref()" in addition to the HEAD case, which was a
    left-over from an early broken test.

--- refs.c
+++ refs.c
@@ -73,8 +73,8 @@ int head_ref(int (*fn)(const char *path, const unsigned char *sha1))
 	unsigned char sha1[20];
 	const char *headpath = git_path("HEAD");
 	if (!read_ref(headpath, sha1))
-		fn(headpath, sha1);
-	return do_for_each_ref(get_refs_directory(), fn);
+		return fn(headpath, sha1);
+	return 0;
 }
 
 int for_each_ref(int (*fn)(const char *path, const unsigned char *sha1))
