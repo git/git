584c6cc91a61ec615419452ca540041be6ba9f38
Linus Torvalds <torvalds@g5.osdl.org>
2005-07-08 13:58:40 -0700

    Teach 'git-send-pack' to send new branches and tags.
    The protocol always supported it, but send-pack didn't actually know how
    to tell the other side about a new branch/tag.
    
    NOTE! You'll have to name it explicitly on the command line: if you
    don't name any branches, git-send-pack will default to the branches that
    already exist.

--- send-pack.c
+++ send-pack.c
@@ -1,4 +1,5 @@
 #include "cache.h"
+#include "refs.h"
 #include "pkt-line.h"
 
 static const char send_pack_usage[] = "git-send-pack [--exec=other] destination [heads]*";
@@ -10,6 +11,17 @@ struct ref {
 	unsigned char new_sha1[20];
 	char name[0];
 };
+
+static int is_zero_sha1(const unsigned char *sha1)
+{
+	int i;
+
+	for (i = 0; i < 20; i++) {
+		if (*sha1++)
+			return 0;
+	}
+	return 1;
+}
 
 static void exec_pack_objects(void)
 {
@@ -33,11 +45,15 @@ static void exec_rev_list(struct ref *refs)
 		char *buf = malloc(100);
 		if (i > 900)
 			die("git-rev-list environment overflow");
-		args[i++] = buf;
-		snprintf(buf, 50, "^%s", sha1_to_hex(refs->old_sha1));
-		buf += 50;
-		args[i++] = buf;
-		snprintf(buf, 50, "%s", sha1_to_hex(refs->new_sha1));
+		if (!is_zero_sha1(refs->old_sha1)) {
+			args[i++] = buf;
+			snprintf(buf, 50, "^%s", sha1_to_hex(refs->old_sha1));
+			buf += 50;
+		}
+		if (!is_zero_sha1(refs->new_sha1)) {
+			args[i++] = buf;
+			snprintf(buf, 50, "%s", sha1_to_hex(refs->new_sha1));
+		}
 		refs = refs->next;
 	}
 	args[i] = NULL;
@@ -104,14 +120,54 @@ static int read_ref(const char *ref, unsigned char *sha1)
 	return ret;
 }
 
+static int ref_newer(const unsigned char *new_sha1, const unsigned char *old_sha1)
+{
+	if (!has_sha1_file(old_sha1))
+		return 0;
+	/*
+	 * FIXME! It is not correct to say that the new one is newer
+	 * just because we don't have the old one!
+	 *
+	 * We should really see if we can reach the old_sha1 commit
+	 * from the new_sha1 one.
+	 */
+	return 1;
+}
+
+static int local_ref_nr_match;
+static char **local_ref_match;
+static struct ref **local_ref_list;
+
+static int try_to_match(const char *refname, const unsigned char *sha1)
+{
+	struct ref *ref;
+	int len;
+
+	if (!path_match(refname, local_ref_nr_match, local_ref_match))
+		return 0;
+
+	len = strlen(refname)+1;
+	ref = xmalloc(sizeof(*ref) + len);
+	memset(ref->old_sha1, 0, 20);
+	memcpy(ref->new_sha1, sha1, 20);
+	memcpy(ref->name, refname, len);
+	ref->next = NULL;
+	*local_ref_list = ref;
+	local_ref_list = &ref->next;
+	return 0;
+}
+
 static int send_pack(int in, int out, int nr_match, char **match)
 {
 	struct ref *ref_list = NULL, **last_ref = &ref_list;
 	struct ref *ref;
+	int new_refs;
 
+	/*
+	 * Read all the refs from the other end
+	 */
 	for (;;) {
 		unsigned char old_sha1[20];
-		unsigned char new_sha1[20];
 		static char buffer[1000];
 		char *name;
 		int len;
@@ -125,27 +181,65 @@ static int send_pack(int in, int out, int nr_match, char **match)
 		if (len < 42 || get_sha1_hex(buffer, old_sha1) || buffer[40] != ' ')
 			die("protocol error: expected sha/ref, got '%s'", buffer);
 		name = buffer + 41;
-		if (nr_match && !path_match(name, nr_match, match))
-			continue;
-		if (read_ref(name, new_sha1) < 0)
-			return error("no such local reference '%s'", name);
-		if (!has_sha1_file(old_sha1))
-			return error("remote '%s' points to object I don't have", name);
-		if (!memcmp(old_sha1, new_sha1, 20)) {
-			fprintf(stderr, "'%s' unchanged\n", name);
-			continue;
-		}
 		ref = xmalloc(sizeof(*ref) + len - 40);
 		memcpy(ref->old_sha1, old_sha1, 20);
-		memcpy(ref->new_sha1, new_sha1, 20);
+		memset(ref->new_sha1, 0, 20);
 		memcpy(ref->name, buffer + 41, len - 40);
 		ref->next = NULL;
 		*last_ref = ref;
 		last_ref = &ref->next;
 	}
 
+	/*
+	 * Go through the refs, see if we want to update
+	 * any of them..
+	 */
+	for (ref = ref_list; ref; ref = ref->next) {
+		unsigned char new_sha1[20];
+		char *name = ref->name;
+
+		if (nr_match && !path_match(name, nr_match, match))
+			continue;
+
+		if (read_ref(name, new_sha1) < 0)
+			continue;
+
+		if (nr_match && !path_match(name, nr_match, match))
+			continue;
+
+		if (!memcmp(ref->old_sha1, new_sha1, 20)) {
+			fprintf(stderr, "'%s' unchanged\n", name);
+			continue;
+		}
+
+		if (!ref_newer(new_sha1, ref->old_sha1)) {
+			error("remote '%s' points to object I don't have", name);
+			continue;
+		}
+
+		/* Ok, mark it for update */
+		memcpy(ref->new_sha1, new_sha1, 20);
+	}
+
+	/*
+	 * See if we have any refs that the other end didn't have
+	 */
+	if (nr_match) {
+		local_ref_nr_match = nr_match;
+		local_ref_match = match;
+		local_ref_list = last_ref;
+		for_each_ref(try_to_match);
+	}
+
+	/*
+	 * Finally, tell the other end!
+	 */
+	new_refs = 0;
 	for (ref = ref_list; ref; ref = ref->next) {
 		char old_hex[60], *new_hex;
+		if (is_zero_sha1(ref->new_sha1))
+			continue;
+		new_refs++;
 		strcpy(old_hex, sha1_to_hex(ref->old_sha1));
 		new_hex = sha1_to_hex(ref->new_sha1);
 		packet_write(out, "%s %s %s", old_hex, new_hex, ref->name);
@@ -153,7 +247,7 @@ static int send_pack(int in, int out, int nr_match, char **match)
 	}
 	
 	packet_flush(out);
-	if (ref_list)
+	if (new_refs)
 		pack_objects(out, ref_list);
 	close(out);
 	return 0;
