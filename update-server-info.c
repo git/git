d119e3de13ea1493107bd57381d0ce9c9dd90976
Junio C Hamano <junkio@cox.net>
2005-10-08 15:54:35 -0700

    Also use 'track_object_refs = 0' in update-server-info.
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- update-server-info.c
+++ update-server-info.c
@@ -1,4 +1,5 @@
 #include "cache.h"
+#include "object.h"
 
 static const char update_server_info_usage[] =
 "git-update-server-info [--force]";
@@ -7,6 +8,8 @@ int main(int ac, char **av)
 {
 	int i;
 	int force = 0;
+	track_object_refs = 0;
+
 	for (i = 1; i < ac; i++) {
 		if (av[i][0] == '-') {
 			if (!strcmp("--force", av[i]) ||
