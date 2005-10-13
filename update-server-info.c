35613ba4ba967ff408f1b32275f4ebcd51c98097
Sergey Vlasov <vsu@altlinux.ru>
2005-10-13 10:01:57 -0700

    Revert "Also use 'track_object_refs = 0' in update-server-info."
    This reverts d119e3de13ea1493107bd57381d0ce9c9dd90976 commit.
    
    Object references are used in server-info.c:find_pack_info_one() to
    find out which objects in the pack are heads, therefore tracking of
    references cannot be disabled.
    
    Signed-off-by: Sergey Vlasov <vsu@altlinux.ru>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- update-server-info.c
+++ update-server-info.c
@@ -1,5 +1,4 @@
 #include "cache.h"
-#include "object.h"
 
 static const char update_server_info_usage[] =
 "git-update-server-info [--force]";
@@ -8,8 +7,6 @@ int main(int ac, char **av)
 {
 	int i;
 	int force = 0;
-	track_object_refs = 0;
-
 	for (i = 1; i < ac; i++) {
 		if (av[i][0] == '-') {
 			if (!strcmp("--force", av[i]) ||
