562051809589574576971c53c23aad93f8c395d9
Junio C Hamano <junkio@cox.net>
2005-11-29 21:59:04 -0800

    git-diff: do not turn off -p/-M with any diff options.
    When the user gives a diff option (e.g. --cached) to "git diff",
    we turned off the built-in default option -p, which is usually
    not what user wants to see.
    
    This commit makes lack of --name-status, --name-only nor -r to
    add -p, and lack of -B*, -C* nor -M* to add -M to the flags
    given to the underlying diff.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- git-diff.sh
+++ git-diff.sh
@@ -7,8 +7,6 @@ rev=$(git-rev-parse --revs-only --no-flags --sq "$@") || exit
 flags=$(git-rev-parse --no-revs --flags --sq "$@")
 files=$(git-rev-parse --no-revs --no-flags --sq "$@")
 
-: ${flags:="'-M' '-p'"}
-
 # I often say 'git diff --cached -p' and get scolded by git-diff-files, but
 # obviously I mean 'git diff --cached -p HEAD' in that case.
 case "$rev" in
@@ -18,6 +16,21 @@ case "$rev" in
 		rev='HEAD '
 		;;
 	esac
+esac
+
+# If we do not have --name-status, --name-only nor -r, default to -p.
+# If we do not have -B nor -C, default to -M.
+case " $flags " in
+*" '--name-status' "* | *" '--name-only' "* | *" '-r' "* )
+	;;
+*)
+	flags="$flags'-p' " ;;
+esac
+case " $flags " in
+*" '-"[BCM]* | *" '--find-copies-harder' "*)
+	;; # something like -M50.
+*)
+	flags="$flags'-M' " ;;
 esac
 
 case "$rev" in
