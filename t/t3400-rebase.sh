294c695d8cfbcf95a5c33fc6ba386f496964defb
Amos Waterland <apw@us.ibm.com>
2005-12-14 17:48:19 -0500

    git rebase loses author name/email if given bad email address
    If GIT_AUTHOR_EMAIL is of a certain form, `git rebase master' will blow
    away the author name and email when fast-forward merging commits.  I
    have not tracked it down, but here is a testcase that demonstrates the
    behavior.
    
    Signed-off-by: Amos Waterland <apw@us.ibm.com>
    Acked-by: Michal Ostrowski <mostrows@watson.ibm.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

---
+++ t/t3400-rebase.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Copyright (c) 2005 Amos Waterland
+#
+
+test_description='git rebase should not destroy author information
+
+This test runs git rebase and checks that the author information is not lost.
+'
+. ./test-lib.sh
+
+export GIT_AUTHOR_EMAIL=bogus_email_address
+
+test_expect_success \
+    'prepare repository with topic branch, then rebase against master' \
+    'echo First > A &&
+     git-update-index --add A &&
+     git-commit -m "Add A." &&
+     git checkout -b my-topic-branch &&
+     echo Second > B &&
+     git-update-index --add B &&
+     git-commit -m "Add B." &&
+     git checkout -f master &&
+     echo Third >> A &&
+     git-update-index A &&
+     git-commit -m "Modify A." &&
+     git checkout -f my-topic-branch &&
+     git rebase master'
+
+test_expect_failure \
+    'the rebase operation should not have destroyed author information' \
+    'git log | grep "Author:" | grep "<>"'
+
+test_done
