ee34518d629331dadd58b1a75294369d679eda8b
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2005-12-16 02:40:25 +0100

    We do not like "HEAD" as a new branch name
    This makes git-check-ref-format fail for "HEAD". Since the check is only
    executed when creating refs, the existing symbolic ref is safe.
    
    Otherwise these commands, most likely are pilot errors, would do
    pretty funky stuff:
    
    	git checkout -b HEAD
    	git pull . other:HEAD
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- refs.c
+++ refs.c
@@ -345,6 +345,11 @@ int check_ref_format(const char *ref)
 		if (!ch) {
 			if (level < 2)
 				return -1; /* at least of form "heads/blah" */
+
+			/* do not allow ref name to end in "HEAD" */
+			if (cp - ref > 4 && !strcmp(cp - 4, "HEAD"))
+				return -1;
+
 			return 0;
 		}
 	}
