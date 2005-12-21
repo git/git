21b1aced83195af50fd8bc9a88e7734c8ee77c0e
Junio C Hamano <junkio@cox.net>
2005-12-21 12:09:17 -0800

    objects/info/packs: work around bug in http-fetch.c::fetch_indices()
    The code to fetch pack index files in deployed clients have a
    bug that causes it to ignore the pack file on the last line of
    objects/info/packs file, so append an empty line to work it
    around.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- server-info.c
+++ server-info.c
@@ -200,6 +200,7 @@ static void write_pack_info_file(FILE *fp)
 	int i;
 	for (i = 0; i < num_pack; i++)
 		fprintf(fp, "P %s\n", info[i]->p->pack_name + objdirlen + 6);
+	fputc('\n', fp);
 }
 
 static int update_info_packs(int force)
