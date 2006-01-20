5b2bcc7b2d546c636f79490655b3347acc91d17f
Junio C Hamano <junkio@cox.net>
2006-01-20 15:00:12 -0800

    git-grep: clarification on parameters.
    We forgot to make sure that there is no more than one pattern
    parameter.  Also when looking for files in a directory called
    '--others', it passed that path limiter without preceding the
    end-of-options marker '--' to underlying git-ls-files, which
    misunderstood it as one of its options instead.
    
    	$ git grep --others -e Meta/Make Meta
    	$ git grep -o -e Meta/Make Meta
    	$ git grep -o Meta/Make Meta
    
    look for a string "Meta/Make" from untracked files in Meta/
    directory.
    
    	$ git grep Meta/Make --others
    
    looks for the same string from tracked files in ./--others
    directory.
    
    On the other hand,
    
    	$ git grep -e Meta/Make --others
    
    does not have a freestanding pattern, so everybody is parameter
    and there is no path specifier.  It looks for the string in all
    the untracked files without any path limiter.
    
    [jc: updated with usability enhancements and documentation
    cleanups from Sean.]
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- git-grep.sh
+++ git-grep.sh
@@ -3,22 +3,32 @@
 # Copyright (c) Linus Torvalds, 2005
 #
 
-USAGE='<option>... <pattern> <path>...'
+USAGE='[<option>...] [-e] <pattern> [<path>...]'
 SUBDIRECTORY_OK='Yes'
 . git-sh-setup
 
+got_pattern () {
+	if [ -z "$no_more_patterns" ]
+	then
+		pattern="$1" no_more_patterns=yes
+	else
+		die "git-grep: do not specify more than one pattern"
+	fi
+}
+
+no_more_patterns=
 pattern=
 flags=()
 git_flags=()
 while : ; do
 	case "$1" in
-	--cached|--deleted|--others|--killed|\
-	--ignored|--exclude=*|\
+	-o|--cached|--deleted|--others|--killed|\
+	--ignored|--modified|--exclude=*|\
 	--exclude-from=*|\--exclude-per-directory=*)
 		git_flags=("${git_flags[@]}" "$1")
 		;;
 	-e)
-		pattern="$2"
+		got_pattern "$2"
 		shift
 		;;
 	-A|-B|-C|-D|-d|-f|-m)
@@ -26,7 +36,7 @@ while : ; do
 		shift
 		;;
 	--)
-		# The rest are git-ls-files paths (or flags)
+		# The rest are git-ls-files paths
 		shift
 		break
 		;;
@@ -34,10 +44,12 @@ while : ; do
 		flags=("${flags[@]}" "$1")
 		;;
 	*)
-		if [ -z "$pattern" ]; then
-			pattern="$1"
+		if [ -z "$no_more_patterns" ]
+		then
+			got_pattern "$1"
 			shift
 		fi
+		[ "$1" = -- ] && shift
 		break
 		;;
 	esac
@@ -46,5 +58,5 @@ done
 [ "$pattern" ] || {
 	usage
 }
-git-ls-files -z "${git_flags[@]}" "$@" |
-	xargs -0 grep "${flags[@]}" -e "$pattern"
+git-ls-files -z "${git_flags[@]}" -- "$@" |
+	xargs -0 grep "${flags[@]}" -e "$pattern" --
