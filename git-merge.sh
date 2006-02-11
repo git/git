180b0d7483711120e28289ff7d9fa346eddd5cb7
Olaf Hering <olh@suse.de>
2006-02-11 12:26:30 +0100

    allow double click on current HEAD id after git-pull
    Double click on to current HEAD commit id is not possible,
    the dot has to go.
    
    [jc: by popular requests.]
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- git-merge.sh
+++ git-merge.sh
@@ -131,7 +131,7 @@ case "$#,$common,$no_commit" in
 	;;
 1,"$head",*)
 	# Again the most common case of merging one remote.
-	echo "Updating from $head to $1."
+	echo "Updating from $head to $1"
 	git-update-index --refresh 2>/dev/null
 	new_head=$(git-rev-parse --verify "$1^0") &&
 	git-read-tree -u -v -m $head "$new_head" &&
