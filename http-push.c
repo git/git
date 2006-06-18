cfd432e63d77478d913497a62827ed95c56dda73
Florian Forster <octo@verplant.org>
2006-06-18 17:18:03 +0200

    Remove ranges from switch statements.
    Though very nice and readable, the "case 'a'...'z':" construct is not ANSI C99
    compliant. This patch unfolds the range in `quote.c' and substitutes the
    switch-statement with an if-statement in `http-fetch.c' and `http-push.c'.
    
    Signed-off-by: Florian Forster <octo@verplant.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- http-push.c
+++ http-push.c
@@ -1077,13 +1077,14 @@ static int fetch_indices(void)
 
 static inline int needs_quote(int ch)
 {
-	switch (ch) {
-	case '/': case '-': case '.':
-	case 'A'...'Z':	case 'a'...'z':	case '0'...'9':
+	if (((ch >= 'A') && (ch <= 'Z'))
+			|| ((ch >= 'a') && (ch <= 'z'))
+			|| ((ch >= '0') && (ch <= '9'))
+			|| (ch == '/')
+			|| (ch == '-')
+			|| (ch == '.'))
 		return 0;
-	default:
-		return 1;
-	}
+	return 1;
 }
 
 static inline int hex(int v)
