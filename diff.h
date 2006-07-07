ca49920f6f8e7de3463757662841c608ca978888
Stephan Feder <sf@b-i-t.de>
2006-07-07 15:57:08 +0200

    Add -a and --text to common diff options help
    Signed-off-by: Stephan Feder <sf@b-i-t.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- diff.h
+++ diff.h
@@ -162,7 +162,8 @@ extern void diffcore_std_no_resolve(struct diff_options *);
 "  -O<file>      reorder diffs according to the <file>.\n" \
 "  -S<string>    find filepair whose only one side contains the string.\n" \
 "  --pickaxe-all\n" \
-"                show all files diff when -S is used and hit is found.\n"
+"                show all files diff when -S is used and hit is found.\n" \
+"  -a  --text    treat all files as text.\n"
 
 extern int diff_queue_is_empty(void);
 extern void diff_flush(struct diff_options*);
