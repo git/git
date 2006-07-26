afd222967c6abedcf75e28fe0e57cd5644f6c865
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2006-07-26 15:44:08 +0200

    Extend testing git-mv for renaming of subdirectories
    Signed-off-by: Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- t/t7001-mv.sh
+++ t/t7001-mv.sh
@@ -38,4 +38,25 @@ test_expect_success \
     'git-diff-tree -r -M --name-status  HEAD^ HEAD | \
     grep -E "^R100.+path1/COPYING.+path0/COPYING"'
 
+test_expect_success \
+    'adding another file' \
+    'cp ../../README path0/README &&
+     git-add path0/README &&
+     git-commit -m add2 -a'
+
+test_expect_success \
+    'moving whole subdirectory' \
+    'git-mv path0 path2'
+
+test_expect_success \
+    'commiting the change' \
+    'git-commit -m dir-move -a'
+
+test_expect_success \
+    'checking the commit' \
+    'git-diff-tree -r -M --name-status  HEAD^ HEAD | \
+     grep -E "^R100.+path0/COPYING.+path2/COPYING" &&
+     git-diff-tree -r -M --name-status  HEAD^ HEAD | \
+     grep -E "^R100.+path0/README.+path2/README"'
+
 test_done
