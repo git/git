4adffc7b54e8c5d8fc7510007dd9b8235a67430f
Junio C Hamano <junkio@cox.net>
2006-09-16 11:06:02 -0700

    Add ftp:// protocol support for git-http-fetch
    Based on Sasha Khapyorsky's patch but adjusted to the refactored
    "missing target" detection code.
    
    It might have been better if the program were called
    git-url-fetch but it is too late now ;-).
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- http-fetch.c
+++ http-fetch.c
@@ -149,7 +149,9 @@ static int missing__target(int code, int result)
 	return	/* file:// URL -- do we ever use one??? */
 		(result == CURLE_FILE_COULDNT_READ_FILE) ||
 		/* http:// and https:// URL */
-		(code == 404 && result == CURLE_HTTP_RETURNED_ERROR)
+		(code == 404 && result == CURLE_HTTP_RETURNED_ERROR) ||
+		/* ftp:// URL */
+		(code == 550 && result == CURLE_FTP_COULDNT_RETR_FILE)
 		;
 }
 
