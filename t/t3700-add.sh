fd28b34afd9bbd58297a25edced3f504c9a5487a
Shawn Pearce <spearce@spearce.org>
2006-09-27 01:21:19 -0400

    Ignore executable bit when adding files if filemode=0.
    If the user has configured core.filemode=0 then we shouldn't set
    the execute bit in the index when adding a new file as the user
    has indicated that the local filesystem can't be trusted.
    
    This means that when adding files that should be marked executable
    in a repository with core.filemode=0 the user must perform a
    'git update-index --chmod=+x' on the file before committing the
    addition.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- t/t3700-add.sh
+++ t/t3700-add.sh
@@ -19,4 +19,26 @@ test_expect_success \
     'Test that "git-add -- -q" works' \
     'touch -- -q && git-add -- -q'
 
+test_expect_success \
+	'git-add: Test that executable bit is not used if core.filemode=0' \
+	'git repo-config core.filemode 0 &&
+	 echo foo >xfoo1 &&
+	 chmod 755 xfoo1 &&
+	 git-add xfoo1 &&
+	 case "`git-ls-files --stage xfoo1`" in
+	 100644" "*xfoo1) echo ok;;
+	 *) echo fail; git-ls-files --stage xfoo1; exit 1;;
+	 esac'
+
+test_expect_success \
+	'git-update-index --add: Test that executable bit is not used...' \
+	'git repo-config core.filemode 0 &&
+	 echo foo >xfoo2 &&
+	 chmod 755 xfoo2 &&
+	 git-add xfoo2 &&
+	 case "`git-ls-files --stage xfoo2`" in
+	 100644" "*xfoo2) echo ok;;
+	 *) echo fail; git-ls-files --stage xfoo2; exit 1;;
+	 esac'
+
 test_done
