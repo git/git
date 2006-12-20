5caf92322303a0942e0cee5a93cb24f2d8d44022
Junio C Hamano <junkio@cox.net>
2006-12-19 18:26:48 -0800

    fix populate-filespec
    I hand munged the original patch when committing 1510fea78, and
    screwed up the conversion.
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- diff.c
+++ diff.c
@@ -1223,7 +1223,7 @@ static int reuse_worktree_file(const char *name, const unsigned char *sha1, int 
 	 * objects however would tend to be slower as they need
 	 * to be individually opened and inflated.
 	 */
-	if (FAST_WORKING_DIRECTORY && !want_file && has_sha1_pack(sha1, NULL))
+	if (!FAST_WORKING_DIRECTORY && !want_file && has_sha1_pack(sha1, NULL))
 		return 0;
 
 	len = strlen(name);
