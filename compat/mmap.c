8e554429e8a9dbb5dba6b8aeca66fc27c0bb5538
Shawn O. Pearce <spearce@spearce.org>
2006-12-24 00:45:47 -0500

    Switch git_mmap to use pread.
    Now that Git depends on pread in index-pack its safe to say we can
    also depend on it within the git_mmap emulation we activate when
    NO_MMAP is set.  On most systems pread should be slightly faster
    than an lseek/read/lseek sequence as its one system call vs. three
    system calls.
    
    We also now honor EAGAIN and EINTR error codes from pread and
    restart the prior read.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- compat/mmap.c
+++ compat/mmap.c
@@ -2,16 +2,10 @@
 
 void *git_mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	int n = 0;
-	off_t current_offset = lseek(fd, 0, SEEK_CUR);
+	size_t n = 0;
 
 	if (start != NULL || !(flags & MAP_PRIVATE))
 		die("Invalid usage of mmap when built with NO_MMAP");
-
-	if (lseek(fd, offset, SEEK_SET) < 0) {
-		errno = EINVAL;
-		return MAP_FAILED;
-	}
 
 	start = xmalloc(length);
 	if (start == NULL) {
@@ -20,25 +14,22 @@ void *git_mmap(void *start, size_t length, int prot, int flags, int fd, off_t of
 	}
 
 	while (n < length) {
-		int count = read(fd, start+n, length-n);
+		ssize_t count = pread(fd, (char *)start + n, length - n, offset + n);
 
 		if (count == 0) {
-			memset(start+n, 0, length-n);
+			memset((char *)start+n, 0, length-n);
 			break;
 		}
 
 		if (count < 0) {
+			if (errno == EAGAIN || errno == EINTR)
+				continue;
 			free(start);
 			errno = EACCES;
 			return MAP_FAILED;
 		}
 
 		n += count;
-	}
-
-	if (current_offset != lseek(fd, current_offset, SEEK_SET)) {
-		errno = EINVAL;
-		return MAP_FAILED;
 	}
 
 	return start;
