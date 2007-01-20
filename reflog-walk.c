40ab7c33cd6dfdf887ed15baee1325ee445eced7
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-01-20 10:49:15 +0100

    --walk-reflogs: actually find the right commit by date.
    Embarassing thinko.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>

--- reflog-walk.c
+++ reflog-walk.c
@@ -71,7 +71,7 @@ static int get_reflog_recno_by_time(struct complete_reflogs *array,
 	unsigned long timestamp)
 {
 	int i;
-	for (i = array->nr - 1; i >= 0; i++)
+	for (i = array->nr - 1; i >= 0; i--)
 		if (timestamp >= array->items[i].timestamp)
 			return i;
 	return -1;
