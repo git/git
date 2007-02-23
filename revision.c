3d1efd8f1ddc0de423426b5a2a8c6c0a30f1d46e
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-02-23 02:56:31 +0100

    git-bundle: fix 'create --all'

--- revision.c
+++ revision.c
@@ -480,7 +480,7 @@ static int handle_one_ref(const char *path, const unsigned char *sha1, int flag,
 	struct all_refs_cb *cb = cb_data;
 	struct object *object = get_reference(cb->all_revs, path, sha1,
 					      cb->all_flags);
-	add_pending_object(cb->all_revs, object, "");
+	add_pending_object(cb->all_revs, object, path);
 	return 0;
 }
 
