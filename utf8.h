094e03b039aeaad759227540491c235b3a83b83a
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-02-27 16:20:31 +0100

    Actually make print_wrapped_text() useful
    Now, it returns the current column, does not add a newline, and you can
    pass a negative indent, to indicate that the indent was already printed.
    
    With this, you can actually continue in the middle of a paragraph, not
    having to print everything into a buffer first.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- utf8.h
+++ utf8.h
@@ -5,7 +5,7 @@ int utf8_width(const char **start);
 int is_utf8(const char *text);
 int is_encoding_utf8(const char *name);
 
-void print_wrapped_text(const char *text, int indent, int indent2, int len);
+int print_wrapped_text(const char *text, int indent, int indent2, int len);
 
 #ifndef NO_ICONV
 char *reencode_string(const char *in, const char *out_encoding, const char *in_encoding);
