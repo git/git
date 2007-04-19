744747ef1d75c85fb3a1785cb08d36497128d3d3
Brian Gernhardt <benji@silverinsanity.com>
2007-04-19 00:51:06 -0400

    Remove case-sensitive file in t3030-merge-recursive.
    Rename "A" to the unused "c"
    
    Signed-off-by: Brian Gernhardt <benji@silverinsanity.com>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- t/t3030-merge-recursive.sh
+++ t/t3030-merge-recursive.sh
@@ -9,12 +9,12 @@ test_expect_success 'setup 1' '
 	echo hello >a &&
 	o0=$(git hash-object a) &&
 	cp a b &&
-	cp a A &&
+	cp a c &&
 	mkdir d &&
 	cp a d/e &&
 
 	test_tick &&
-	git add a b A d/e &&
+	git add a b c d/e &&
 	git commit -m initial &&
 	c0=$(git rev-parse --verify HEAD) &&
 	git branch side &&
@@ -34,13 +34,13 @@ test_expect_success 'setup 1' '
 	c1=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o1	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o1	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o1 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -48,17 +48,17 @@ test_expect_success 'setup 1' '
 
 test_expect_success 'setup 2' '
 
-	rm -rf [Aabd] &&
+	rm -rf [abcd] &&
 	git checkout side &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -73,13 +73,13 @@ test_expect_success 'setup 2' '
 	c2=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o2	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o2 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -87,17 +87,17 @@ test_expect_success 'setup 2' '
 
 test_expect_success 'setup 3' '
 
-	rm -rf [Aabd] &&
+	rm -rf [abcd] &&
 	git checkout df-1 &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -110,13 +110,13 @@ test_expect_success 'setup 3' '
 	c3=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o3	b/c"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o3 0	b/c"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -124,17 +124,17 @@ test_expect_success 'setup 3' '
 
 test_expect_success 'setup 4' '
 
-	rm -rf [Aabd] &&
+	rm -rf [abcd] &&
 	git checkout df-2 &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -147,13 +147,13 @@ test_expect_success 'setup 4' '
 	c4=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o4	a/c"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o4 0	a/c"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -161,17 +161,17 @@ test_expect_success 'setup 4' '
 
 test_expect_success 'setup 5' '
 
-	rm -rf [Aabd] &&
+	rm -rf [abcd] &&
 	git checkout remove &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -188,11 +188,11 @@ test_expect_success 'setup 5' '
 	c5=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o5	a"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o5 0	a"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -201,17 +201,17 @@ test_expect_success 'setup 5' '
 
 test_expect_success 'setup 6' '
 
-	rm -rf [Aabd] &&
+	rm -rf [abcd] &&
 	git checkout df-3 &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o0	d/e"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o0 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -224,13 +224,13 @@ test_expect_success 'setup 6' '
 	c6=$(git rev-parse --verify HEAD) &&
 	( git ls-tree -r HEAD ; git ls-files -s ) >actual &&
 	(
-		echo "100644 blob $o0	A"
 		echo "100644 blob $o0	a"
 		echo "100644 blob $o0	b"
+		echo "100644 blob $o0	c"
 		echo "100644 blob $o6	d"
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o6 0	d"
 	) >expected &&
 	git diff -u expected actual
@@ -238,7 +238,7 @@ test_expect_success 'setup 6' '
 
 test_expect_success 'merge-recursive simple' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git checkout -f "$c2" &&
 
 	git-merge-recursive "$c0" -- "$c2" "$c1"
@@ -258,11 +258,11 @@ test_expect_success 'merge-recursive result' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 1	a"
 		echo "100644 $o2 2	a"
 		echo "100644 $o1 3	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -271,7 +271,7 @@ test_expect_success 'merge-recursive result' '
 
 test_expect_success 'merge-recursive remove conflict' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git checkout -f "$c1" &&
 
 	git-merge-recursive "$c0" -- "$c1" "$c5"
@@ -291,10 +291,10 @@ test_expect_success 'merge-recursive remove conflict' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 1	a"
 		echo "100644 $o1 2	a"
 		echo "100644 $o5 3	a"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -302,7 +302,7 @@ test_expect_success 'merge-recursive remove conflict' '
 '
 
 test_expect_success 'merge-recursive d/f simple' '
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git reset --hard &&
 	git checkout -f "$c1" &&
 
@@ -313,9 +313,9 @@ test_expect_success 'merge-recursive result' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o1 0	a"
 		echo "100644 $o3 0	b/c"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -324,7 +324,7 @@ test_expect_success 'merge-recursive result' '
 
 test_expect_success 'merge-recursive d/f conflict' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git reset --hard &&
 	git checkout -f "$c1" &&
 
@@ -345,11 +345,11 @@ test_expect_success 'merge-recursive d/f conflict result' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 1	a"
 		echo "100644 $o1 2	a"
 		echo "100644 $o4 0	a/c"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -358,7 +358,7 @@ test_expect_success 'merge-recursive d/f conflict result' '
 
 test_expect_success 'merge-recursive d/f conflict the other way' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git reset --hard &&
 	git checkout -f "$c4" &&
 
@@ -379,11 +379,11 @@ test_expect_success 'merge-recursive d/f conflict result the other way' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o0 1	a"
 		echo "100644 $o1 3	a"
 		echo "100644 $o4 0	a/c"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -392,7 +392,7 @@ test_expect_success 'merge-recursive d/f conflict result the other way' '
 
 test_expect_success 'merge-recursive d/f conflict' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git reset --hard &&
 	git checkout -f "$c1" &&
 
@@ -413,9 +413,9 @@ test_expect_success 'merge-recursive d/f conflict result' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o1 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o6 3	d"
 		echo "100644 $o0 1	d/e"
 		echo "100644 $o1 2	d/e"
@@ -426,7 +426,7 @@ test_expect_success 'merge-recursive d/f conflict result' '
 
 test_expect_success 'merge-recursive d/f conflict' '
 
-	rm -fr [Aabd] &&
+	rm -fr [abcd] &&
 	git reset --hard &&
 	git checkout -f "$c6" &&
 
@@ -447,9 +447,9 @@ test_expect_success 'merge-recursive d/f conflict result' '
 
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
 		echo "100644 $o1 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o6 2	d"
 		echo "100644 $o0 1	d/e"
 		echo "100644 $o1 3	d/e"
@@ -471,13 +471,13 @@ test_expect_success 'reset and bind merge' '
 	git read-tree --prefix=M/ master &&
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
-		echo "100644 $o0 0	M/A"
 		echo "100644 $o1 0	M/a"
 		echo "100644 $o0 0	M/b"
+		echo "100644 $o0 0	M/c"
 		echo "100644 $o1 0	M/d/e"
 		echo "100644 $o1 0	a"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual &&
@@ -485,17 +485,17 @@ test_expect_success 'reset and bind merge' '
 	git read-tree --prefix=a1/ master &&
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
-		echo "100644 $o0 0	M/A"
 		echo "100644 $o1 0	M/a"
 		echo "100644 $o0 0	M/b"
+		echo "100644 $o0 0	M/c"
 		echo "100644 $o1 0	M/d/e"
 		echo "100644 $o1 0	a"
-		echo "100644 $o0 0	a1/A"
 		echo "100644 $o1 0	a1/a"
 		echo "100644 $o0 0	a1/b"
+		echo "100644 $o0 0	a1/c"
 		echo "100644 $o1 0	a1/d/e"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
 	) >expected &&
 	git diff -u expected actual
@@ -503,21 +503,21 @@ test_expect_success 'reset and bind merge' '
 	git read-tree --prefix=z/ master &&
 	git ls-files -s >actual &&
 	(
-		echo "100644 $o0 0	A"
-		echo "100644 $o0 0	M/A"
 		echo "100644 $o1 0	M/a"
 		echo "100644 $o0 0	M/b"
+		echo "100644 $o0 0	M/c"
 		echo "100644 $o1 0	M/d/e"
 		echo "100644 $o1 0	a"
-		echo "100644 $o0 0	a1/A"
 		echo "100644 $o1 0	a1/a"
 		echo "100644 $o0 0	a1/b"
+		echo "100644 $o0 0	a1/c"
 		echo "100644 $o1 0	a1/d/e"
 		echo "100644 $o0 0	b"
+		echo "100644 $o0 0	c"
 		echo "100644 $o1 0	d/e"
-		echo "100644 $o0 0	z/A"
 		echo "100644 $o1 0	z/a"
 		echo "100644 $o0 0	z/b"
+		echo "100644 $o0 0	z/c"
 		echo "100644 $o1 0	z/d/e"
 	) >expected &&
 	git diff -u expected actual
