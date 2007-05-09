0d7566a5baa9cc67fd3361b64bb48757e637fdb0
Theodore Tso <tytso@mit.edu>
2007-05-09 15:48:39 -0400

    Add --aggressive option to 'git gc'
    This option causes 'git gc' to more aggressively optimize the
    repository at the cost of taking much more wall clock and CPU time.
    
    Today this option causes git-pack-objects to use --no-use-delta
    option, and it allows the --window parameter to be set via the
    gc.aggressiveWindow configuration parameter.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-gc.c
+++ builtin-gc.c
@@ -15,13 +15,15 @@
 
 #define FAILED_RUN "failed to run %s"
 
-static const char builtin_gc_usage[] = "git-gc [--prune]";
+static const char builtin_gc_usage[] = "git-gc [--prune] [--aggressive]";
 
 static int pack_refs = -1;
+static int aggressive_window = -1;
 
+#define MAX_ADD 10
 static const char *argv_pack_refs[] = {"pack-refs", "--prune", NULL};
 static const char *argv_reflog[] = {"reflog", "expire", "--all", NULL};
-static const char *argv_repack[] = {"repack", "-a", "-d", "-l", NULL};
+static const char *argv_repack[MAX_ADD] = {"repack", "-a", "-d", "-l", NULL};
 static const char *argv_prune[] = {"prune", NULL};
 static const char *argv_rerere[] = {"rerere", "gc", NULL};
 
@@ -34,13 +36,31 @@ static int gc_config(const char *var, const char *value)
 			pack_refs = git_config_bool(var, value);
 		return 0;
 	}
+	if (!strcmp(var, "gc.aggressivewindow")) {
+		aggressive_window = git_config_int(var, value);
+		return 0;
+	}
 	return git_default_config(var, value);
+}
+
+static void append_option(const char **cmd, const char *opt, int max_length)
+{
+	int i;
+
+	for (i = 0; cmd[i]; i++)
+		;
+
+	if (i + 2 >= max_length)
+		die("Too many options specified");
+	cmd[i++] = opt;
+	cmd[i] = NULL;
 }
 
 int cmd_gc(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	int prune = 0;
+	char buf[80];
 
 	git_config(gc_config);
 
@@ -51,6 +71,14 @@ int cmd_gc(int argc, const char **argv, const char *prefix)
 		const char *arg = argv[i];
 		if (!strcmp(arg, "--prune")) {
 			prune = 1;
+			continue;
+		}
+		if (!strcmp(arg, "--aggressive")) {
+			append_option(argv_repack, "-f", MAX_ADD);
+			if (aggressive_window > 0) {
+				sprintf(buf, "--window=%d", aggressive_window);
+				append_option(argv_repack, buf, MAX_ADD);
+			}
 			continue;
 		}
 		/* perhaps other parameters later... */
