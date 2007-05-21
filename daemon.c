bc4e7d035840539eb5aa13ab3d51ab43cc2c1b50
Jim Meyering <jim@meyering.net>
2007-05-21 09:58:01 +0200

    git-daemon: don't ignore pid-file write failure
    Note: since the consequence of failure is to call die,
    I don't bother to close "f".
    
    Signed-off-by: Jim Meyering <jim@meyering.net>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- daemon.c
+++ daemon.c
@@ -970,8 +970,8 @@ static void store_pid(const char *path)
 	FILE *f = fopen(path, "w");
 	if (!f)
 		die("cannot open pid file %s: %s", path, strerror(errno));
-	fprintf(f, "%d\n", getpid());
-	fclose(f);
+	if (fprintf(f, "%d\n", getpid()) < 0 || fclose(f) != 0)
+		die("failed to write pid file %s: %s", path, strerror(errno));
 }
 
 static int serve(char *listen_addr, int listen_port, struct passwd *pass, gid_t gid)
