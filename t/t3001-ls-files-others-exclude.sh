0ba956d331e4c14fb1600a62044492c6c27af71a
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-05-22 01:12:17 +0100

    git-status: respect core.excludesFile
    git-add reads this variable, and honours the contents of that file if that
    exists. Match this behaviour in git-status, too.
    
    Noticed by Evan Carroll on IRC.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- t/t3001-ls-files-others-exclude.sh
+++ t/t3001-ls-files-others-exclude.sh
@@ -79,4 +79,24 @@ test_expect_success \
        >output &&
      git diff expect output'
 
+cat > excludes-file << EOF
+*.[1-8]
+e*
+EOF
+
+git-config core.excludesFile excludes-file
+
+git-runstatus | grep "^#	" > output
+
+cat > expect << EOF
+#	.gitignore
+#	a.6
+#	one/
+#	output
+#	three/
+EOF
+
+test_expect_success 'git-status honours core.excludesfile' \
+	'diff -u expect output'
+
 test_done
