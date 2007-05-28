a588d88aaff312f3afd5713ffcb4e4b1829fb5a6
Martin Koegler <mkoegler@auto.tuwien.ac.at>
2007-05-28 23:20:57 +0200

    builtin-pack-objects: don't fail, if delta is not possible
    If builtin-pack-objects runs out of memory while finding
    the best deltas, it bails out with an error.
    
    If the delta index creation fails (because there is not enough memory),
    we can downgrade the error message to a warning and continue with the
    next object.
    
    Signed-off-by: Martin Koegler <mkoegler@auto.tuwien.ac.at>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-pack-objects.c
+++ builtin-pack-objects.c
@@ -1454,8 +1454,12 @@ static int try_delta(struct unpacked *trg, struct unpacked *src,
 	}
 	if (!src->index) {
 		src->index = create_delta_index(src->data, src_size);
-		if (!src->index)
-			die("out of memory");
+		if (!src->index) {
+			static int warned = 0;
+			if (!warned++)
+				warning("suboptimal pack - out of memory");
+			return 0;
+		}
 	}
 
 	delta_buf = create_delta(src->index, trg->data, trg_size, &delta_size, max_size);
