9489d0f197185d584294aa99a09a1b3c5ebb25e0
Matthias Lederhofer <matled@gmx.net>
2007-06-06 09:16:56 +0200

    filter-branch: always export GIT_DIR if it is set
    Currently filter-branch exports GIT_DIR only if it is an
    relative path but git-sh-setup might also set GIT_DIR to an
    absolute path that is not exported yet.  Additionally export
    GIT_WORK_TREE with GIT_DIR to ensure that cwd is used as
    working tree even for bare repositories.
    
    Signed-off-by: Matthias Lederhofer <matled@gmx.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-filter-branch.sh
+++ git-filter-branch.sh
@@ -301,9 +301,10 @@ case "$GIT_DIR" in
 /*)
 	;;
 *)
-	export GIT_DIR="$(pwd)/../../$GIT_DIR"
+	GIT_DIR="$(pwd)/../../$GIT_DIR"
 	;;
 esac
+export GIT_DIR GIT_WORK_TREE=.
 
 export GIT_INDEX_FILE="$(pwd)/../index"
 git-read-tree # seed the index file
