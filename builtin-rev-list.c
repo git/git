1ed84157a21a3e868228b15588e4aadfbe5a030b
Junio C Hamano <gitster@pobox.com>
2007-07-08 19:05:31 -0700

    Revert 88494423 (removal of duplicate parents in the output codepath)
    Now this is not needed, as we rewrite the parent list in the commit
    object itself.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-rev-list.c
+++ builtin-rev-list.c
@@ -70,21 +70,9 @@ static void show_commit(struct commit *commit)
 	if (revs.parents) {
 		struct commit_list *parents = commit->parents;
 		while (parents) {
-			struct object *o = &(parents->item->object);
+			printf(" %s", sha1_to_hex(parents->item->object.sha1));
 			parents = parents->next;
-			if (o->flags & TMP_MARK)
-				continue;
-			printf(" %s", sha1_to_hex(o->sha1));
-			o->flags |= TMP_MARK;
 		}
-		/* TMP_MARK is a general purpose flag that can
-		 * be used locally, but the user should clean
-		 * things up after it is done with them.
-		 */
-		for (parents = commit->parents;
-		     parents;
-		     parents = parents->next)
-			parents->item->object.flags &= ~TMP_MARK;
 	}
 	if (revs.commit_format == CMIT_FMT_ONELINE)
 		putchar(' ');
