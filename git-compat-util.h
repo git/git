cef2e2635135ba148e853cacc9076e3595808b73
Johannes Sixt <johannes.sixt@telecom.at>
2007-07-21 23:16:20 +0200

    Add a fake S_ISSOCK() macro

--- git-compat-util.h
+++ git-compat-util.h
@@ -375,6 +375,7 @@ static inline int strtoul_ui(char const *s, int base, unsigned int *result)
 #ifndef S_ISLNK
 #define S_IFLNK    0120000 /* Symbolic link */
 #define S_ISLNK(x) (((x) & S_IFMT) == S_IFLNK)
+#define S_ISSOCK(x) 0
 #endif
 
 #ifndef S_ISGRP
