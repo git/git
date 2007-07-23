7b69b873faf3cfd592bb64a496c6982a540aa7ea
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-07-24 00:39:50 +0100

    git log -g: Complain, but do not fail, when no reflogs are there
    When asking "git log -g --all", clearly you want to see only those refs
    that do have reflogs, but you do not want it to fail, either.
    
    So instead of die()ing, complain about it, but move on to the other refs.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -118,10 +118,11 @@ static void add_pending_object_with_mode(struct rev_info *revs, struct object *o
 {
 	if (revs->no_walk && (obj->flags & UNINTERESTING))
 		die("object ranges do not make sense when not walking revisions");
+	if (revs->reflog_info && obj->type == OBJ_COMMIT &&
+			add_reflog_for_walk(revs->reflog_info,
+				(struct commit *)obj, name))
+		return;
 	add_object_array_with_mode(obj, name, &revs->pending, mode);
-	if (revs->reflog_info && obj->type == OBJ_COMMIT)
-		add_reflog_for_walk(revs->reflog_info,
-				(struct commit *)obj, name);
 }
 
 void add_pending_object(struct rev_info *revs, struct object *obj, const char *name)
