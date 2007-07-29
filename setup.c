299726d53841984450524f4ade428112e51c853e
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-07-30 00:24:38 +0100

    white space fixes in setup.c
    Some lines were not indented by tabs but by spaces.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -382,11 +382,11 @@ int git_config_perm(const char *var, const char *value)
 
 int check_repository_format_version(const char *var, const char *value)
 {
-       if (strcmp(var, "core.repositoryformatversion") == 0)
-               repository_format_version = git_config_int(var, value);
+	if (strcmp(var, "core.repositoryformatversion") == 0)
+		repository_format_version = git_config_int(var, value);
 	else if (strcmp(var, "core.sharedrepository") == 0)
 		shared_repository = git_config_perm(var, value);
-       return 0;
+	return 0;
 }
 
 int check_repository_format(void)
