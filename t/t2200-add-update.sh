a4882c27f8b3793d94b03fd503a0c67ad9772cf6
Junio C Hamano <gitster@pobox.com>
2007-08-15 14:12:14 -0700

    Fix "git add -u" data corruption.
    This applies to 'maint' to fix a rather serious data corruption
    issue.  When "git add -u" affects a subdirectory in such a way
    that the only changes to its contents are path removals, the
    next tree object written out of that index was bogus, as the
    remove codepath forgot to invalidate the cache-tree entry.
    
    Reported by Salikh Zakirov.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2200-add-update.sh
+++ t/t2200-add-update.sh
@@ -13,26 +13,53 @@ only the updates to dir/sub.'
 
 . ./test-lib.sh
 
-test_expect_success 'setup' '
-echo initial >top &&
-mkdir dir &&
-echo initial >dir/sub &&
-git-add dir/sub top &&
-git-commit -m initial &&
-echo changed >top &&
-echo changed >dir/sub &&
-echo other >dir/other
+test_expect_success setup '
+	echo initial >check &&
+	echo initial >top &&
+	mkdir dir1 dir2 &&
+	echo initial >dir1/sub1 &&
+	echo initial >dir1/sub2 &&
+	echo initial >dir2/sub3 &&
+	git add check dir1 dir2 top &&
+	test_tick
+	git-commit -m initial &&
+
+	echo changed >check &&
+	echo changed >top &&
+	echo changed >dir2/sub3 &&
+	rm -f dir1/sub1 &&
+	echo other >dir2/other
 '
 
-test_expect_success 'update' 'git-add -u dir'
+test_expect_success update '
+	git add -u dir1 dir2
+'
 
-test_expect_success 'update touched correct path' \
-  'test "`git-diff-files --name-status dir/sub`" = ""'
+test_expect_success 'update noticed a removal' '
+	test "$(git-ls-files dir1/sub1)" = ""
+'
 
-test_expect_success 'update did not touch other tracked files' \
-  'test "`git-diff-files --name-status top`" = "M	top"'
+test_expect_success 'update touched correct path' '
+	test "$(git-diff-files --name-status dir2/sub3)" = ""
+'
 
-test_expect_success 'update did not touch untracked files' \
-  'test "`git-diff-files --name-status dir/other`" = ""'
+test_expect_success 'update did not touch other tracked files' '
+	test "$(git-diff-files --name-status check)" = "M	check" &&
+	test "$(git-diff-files --name-status top)" = "M	top"
+'
+
+test_expect_success 'update did not touch untracked files' '
+	test "$(git-ls-files dir2/other)" = ""
+'
+
+test_expect_success 'cache tree has not been corrupted' '
+
+	git ls-files -s |
+	sed -e "s/ 0	/	/" >expect &&
+	git ls-tree -r $(git write-tree) |
+	sed -e "s/ blob / /" >current &&
+	diff -u expect current
+
+'
 
 test_done
