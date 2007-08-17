a9ab2009dbbf769aadd52957950c1bad60a0c8fd
Junio C Hamano <gitster@pobox.com>
2007-08-16 19:24:08 -0700

    Clean-up read-tree error condition.
    This is a follow-up to f34f2b0b; list_tree() function is where it
    first notices that the command line fed too many trees for us to
    handle, so move the error exit message to there, and raise the
    MAX_TREES to 8 (not that it matters very much in practice).
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-read-tree.c
+++ builtin-read-tree.c
@@ -13,7 +13,7 @@
 #include "dir.h"
 #include "builtin.h"
 
-#define MAX_TREES 4
+#define MAX_TREES 8
 static int nr_trees;
 static struct tree *trees[MAX_TREES];
 
@@ -21,8 +21,8 @@ static int list_tree(unsigned char *sha1)
 {
 	struct tree *tree;
 
-	if (nr_trees >= 4)
-		return -1;
+	if (nr_trees >= MAX_TREES)
+		die("I cannot read more than %d trees", MAX_TREES);
 	tree = parse_tree_indirect(sha1);
 	if (!tree)
 		return -1;
@@ -263,9 +263,6 @@ int cmd_read_tree(int argc, const char **argv, const char *unused_prefix)
 		else
 			opts.head_idx = 1;
 	}
-
-	if (MAX_TREES < nr_trees)
-		die("I cannot read more than %d trees", MAX_TREES);
 
 	for (i = 0; i < nr_trees; i++) {
 		struct tree *tree = trees[i];
