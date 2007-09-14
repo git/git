43b98acc23306fd7fff888477937063361a09593
Benoit Sigoure <tsuna@lrde.epita.fr>
2007-09-14 10:29:04 +0200

    Add test to check recent fix to "git add -u"
    An earlier commit fixed type-change case in "git add -u".
    This adds a test to make sure we do not introduce regression.
    
    At the same time, it fixes a stupid typo in the error message.
    
    Signed-off-by: Benoit Sigoure <tsuna@lrde.epita.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-add.c
+++ builtin-add.c
@@ -95,7 +95,7 @@ static void update_callback(struct diff_queue_struct *q,
 		const char *path = p->one->path;
 		switch (p->status) {
 		default:
-			die("unexpacted diff status %c", p->status);
+			die("unexpected diff status %c", p->status);
 		case DIFF_STATUS_UNMERGED:
 		case DIFF_STATUS_MODIFIED:
 		case DIFF_STATUS_TYPE_CHANGED:
