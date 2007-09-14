b78281f7215c0236da6bd5c04ec5e500c83fd101
Junio C Hamano <gitster@pobox.com>
2007-09-14 12:12:32 -0700

    diff --no-index: do not forget to run diff_setup_done()
    Code inspection by Linus found this.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff-lib.c
+++ diff-lib.c
@@ -298,6 +298,8 @@ int setup_diff_no_index(struct rev_info *revs,
 	revs->diffopt.nr_paths = 2;
 	revs->diffopt.no_index = 1;
 	revs->max_count = -2;
+	if (diff_setup_done(&revs->diffopt) < 0)
+		die("diff_setup_done failed");
 	return 0;
 }
 
