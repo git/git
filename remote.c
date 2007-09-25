7dfee372b08f653c2709c90bea986e4ed608f410
Shawn O. Pearce <spearce@spearce.org>
2007-09-25 00:13:19 -0400

    Cleanup unnecessary break in remote.c
    This simple change makes the body of "case 0" easier to read; no
    matter what the value of matched_src is we want to break out of
    the switch and not fall through.  We only want to display an error
    if matched_src is NULL, as this indicates there is no local branch
    matching the input.
    
    Also modified the default case's error message so it uses one less
    line of text.  Even at 8 column per tab indentation we still don't
    break 80 columns with this new formatting.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -598,15 +598,12 @@ static int match_explicit(struct ref *src, struct ref *dst,
 		 * way to delete 'other' ref at the remote end.
 		 */
 		matched_src = try_explicit_object_name(rs->src);
-		if (matched_src)
-			break;
-		error("src refspec %s does not match any.",
-		      rs->src);
+		if (!matched_src)
+			error("src refspec %s does not match any.", rs->src);
 		break;
 	default:
 		matched_src = NULL;
-		error("src refspec %s matches more than one.",
-		      rs->src);
+		error("src refspec %s matches more than one.", rs->src);
 		break;
 	}
 
