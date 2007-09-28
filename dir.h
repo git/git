7155b727c9baae9ef6d7829370aefc09c4ab64e2
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-09-28 16:28:54 +0100

    Introduce remove_dir_recursively()
    There was a function called remove_empty_dir_recursive() buried
    in refs.c.  Expose a slightly enhanced version in dir.h: it can now
    optionally remove a non-empty directory.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- dir.h
+++ dir.h
@@ -64,4 +64,6 @@ extern struct dir_entry *dir_add_name(struct dir_struct *dir, const char *pathna
 extern char *get_relative_cwd(char *buffer, int size, const char *dir);
 extern int is_inside_dir(const char *dir);
 
+extern int remove_dir_recursively(struct strbuf *path, int only_empty);
+
 #endif
