d6db1ad51a5ac8154fcc2413f1eb6142c1a0639a
Shawn O. Pearce <spearce@spearce.org>
2007-10-12 12:18:02 -0400

    git-gui: Ensure copyright message is correctly read as UTF-8
    On Windows using the native Tcl/Tk the copyright header is
    being read from the script using the system encoding, which
    may not be utf-8.  This causes the multi-byte copyright symbol
    (which is actually encoded as utf-8) to read as two characters
    and not as a proper copyright symbol.  Explicitly asking Tcl
    to read this sequence of bytes as utf-8 corrects the issue.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- git-gui.sh
+++ git-gui.sh
@@ -10,7 +10,7 @@
  exec wish "$argv0" -- "$@"
 
 set appvers {@@GITGUI_VERSION@@}
-set copyright {
+set copyright [encoding convertfrom utf-8 {
 Copyright © 2006, 2007 Shawn Pearce, et. al.
 
 This program is free software; you can redistribute it and/or modify
@@ -25,7 +25,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA}
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA}]
 
 ######################################################################
 ##
