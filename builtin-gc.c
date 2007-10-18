130faabe48fd988c1eb078ef467a44e124a00a40
koreth@midwinter.com <koreth@midwinter.com>
2007-10-17 21:41:43 -0700

    Add a message explaining that automatic GC is about to start
    Signed-off-by: Steven Grimm <koreth@midwinter.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- builtin-gc.c
+++ builtin-gc.c
@@ -205,6 +205,10 @@ int cmd_gc(int argc, const char **argv, const char *prefix)
 		prune = 0;
 		if (!need_to_gc())
 			return 0;
+		fprintf(stderr, "Packing your repository for optimum "
+			"performance. If you would rather run\n"
+			"\"git gc\" by hand, run \"git config gc.auto 0\" "
+			"to disable automatic cleanup.\n");
 	} else {
 		/*
 		 * Use safer (for shared repos) "-A" option to
