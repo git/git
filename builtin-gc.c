dd8b883ab9d9a18c5ec8157765244316b2490618
Jeff King <peff@peff.net>
2007-10-18 21:12:11 -0400

    git-gc: improve wording of --auto notification
    The previous message had too much of a "boy, you should
    really turn off this annoying gc" flair to it. Instead,
    let's make sure the user understands what is happening, that
    they can run it themselves, and where to find more info.
    
    Suggested by Brian Gernhardt.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- builtin-gc.c
+++ builtin-gc.c
@@ -206,9 +206,9 @@ int cmd_gc(int argc, const char **argv, const char *prefix)
 		if (!need_to_gc())
 			return 0;
 		fprintf(stderr, "Packing your repository for optimum "
-			"performance. If you would rather run\n"
-			"\"git gc\" by hand, run \"git config gc.auto 0\" "
-			"to disable automatic cleanup.\n");
+			"performance. You may also\n"
+			"run \"git gc\" manually. See "
+			"\"git help gc\" for more information.\n");
 	} else {
 		/*
 		 * Use safer (for shared repos) "-A" option to
