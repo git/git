a238917ba48c44de8a00e3aee7898d9beb5afcd8
Junio C Hamano <gitster@pobox.com>
2007-10-26 23:26:41 -0700

    help: remove extra blank line after "See 'git --help'" message
    The double LF were there only because we gave a list of common
    commands.  WIth the list gone, there is no reason to have the
    extra blank line.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -185,7 +185,7 @@ static void show_man_page(const char *git_cmd)
 
 void help_unknown_cmd(const char *cmd)
 {
-	fprintf(stderr, "git: '%s' is not a git-command. See --help\n\n", cmd);
+	fprintf(stderr, "git: '%s' is not a git-command. See 'git --help'.\n", cmd);
 	exit(1);
 }
 
