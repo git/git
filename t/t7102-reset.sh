620a6cd42ed5ea94684b22714181bf03871733dd
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2007-11-03 15:21:21 +0000

    builtin-reset: avoid forking "update-index --refresh"
    Instead of forking update-index, call refresh_cache() directly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7102-reset.sh
+++ t/t7102-reset.sh
@@ -418,4 +418,14 @@ test_expect_success 'resetting an unmodified path is a no-op' '
 	git diff-index --cached --exit-code HEAD
 '
 
+cat > expect << EOF
+file2: needs update
+EOF
+
+test_expect_success '--mixed refreshes the index' '
+	echo 123 >> file2 &&
+	git reset --mixed HEAD > output &&
+	git diff --exit-code expect output
+'
+
 test_done
