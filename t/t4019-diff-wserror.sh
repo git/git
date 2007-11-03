49e703afda0e4e67050fcc8e05b175987c83391c
Junio C Hamano <gitster@pobox.com>
2007-11-02 17:46:55 -0700

    core.whitespace: add test for diff whitespace error highlighting
    This tests seletive enabling/disabling of whitespace error
    highlighting done by colored diff output.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t4019-diff-wserror.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+test_description='diff whitespace error detection'
+
+. ./test-lib.sh
+
+test_expect_success setup '
+
+	git config diff.color.whitespace "blue reverse" &&
+	>F &&
+	git add F &&
+	echo "         Eight SP indent" >>F &&
+	echo " 	HT and SP indent" >>F &&
+	echo "With trailing SP " >>F &&
+	echo "No problem" >>F
+
+'
+
+blue_grep='7;34m' ;# ESC [ 7 ; 3 4 m
+
+test_expect_success default '
+
+	git diff --color >output
+	grep "$blue_grep" output >error
+	grep -v "$blue_grep" output >normal
+
+	grep Eight normal >/dev/null &&
+	grep HT error >/dev/null &&
+	grep With error >/dev/null &&
+	grep No normal >/dev/null
+
+'
+
+test_expect_success 'without -trail' '
+
+	git config core.whitespace -trail
+	git diff --color >output
+	grep "$blue_grep" output >error
+	grep -v "$blue_grep" output >normal
+
+	grep Eight normal >/dev/null &&
+	grep HT error >/dev/null &&
+	grep With normal >/dev/null &&
+	grep No normal >/dev/null
+
+'
+
+test_expect_success 'without -space' '
+
+	git config core.whitespace -space
+	git diff --color >output
+	grep "$blue_grep" output >error
+	grep -v "$blue_grep" output >normal
+
+	grep Eight normal >/dev/null &&
+	grep HT normal >/dev/null &&
+	grep With error >/dev/null &&
+	grep No normal >/dev/null
+
+'
+
+test_expect_success 'with indent-non-tab only' '
+
+	git config core.whitespace indent,-trailing,-space
+	git diff --color >output
+	grep "$blue_grep" output >error
+	grep -v "$blue_grep" output >normal
+
+	grep Eight error >/dev/null &&
+	grep HT normal >/dev/null &&
+	grep With normal >/dev/null &&
+	grep No normal >/dev/null
+
+'
+
+test_done
