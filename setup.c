1e8a1954519a070e92cb73f756b271664e1cc4d1
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2007-11-03 20:18:06 +0700

    Add missing inside_work_tree setting in setup_git_directory_gently
    When both GIT_DIR and GIT_WORK_TREE are set, and
    setup_git_directory_gently() changes the current working
    directory accordingly, it should also set inside_work_tree = 1.
    
    Without this, work_tree handling code in setup_git_directory()
    will be activated. If you stay in root work tree (no prefix),
    it does not harm. It does if you work from a subdirectory though.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -240,6 +240,7 @@ const char *setup_git_directory_gently(int *nongit_ok)
 			if (chdir(work_tree_env) < 0)
 				die ("Could not chdir to %s", work_tree_env);
 			strcat(buffer, "/");
+			inside_work_tree = 1;
 			return retval;
 		}
 		if (nongit_ok) {
