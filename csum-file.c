ec640ed1cf1d62730555705ec18b785c43e81f62
Nicolas Pitre <nico@cam.org>
2007-11-04 22:54:50 -0500

    remove dead code from the csum-file interface
    The provided name argument is always constant and valid in every
    caller's context, so no need to have an array of PATH_MAX chars to copy
    it into when a simple pointer will do.  Unfortunately that means getting
    rid of wascally wabbits too.
    
    The 'error' field is also unused.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- csum-file.c
+++ csum-file.c
@@ -88,22 +88,12 @@ struct sha1file *sha1fd(int fd, const char *name)
 
 struct sha1file *sha1fd_throughput(int fd, const char *name, struct progress *tp)
 {
-	struct sha1file *f;
-	unsigned len;
-
-	f = xmalloc(sizeof(*f));
-
-	len = strlen(name);
-	if (len >= PATH_MAX)
-		die("you wascally wabbit, you");
-	f->namelen = len;
-	memcpy(f->name, name, len+1);
-
+	struct sha1file *f = xmalloc(sizeof(*f));
 	f->fd = fd;
-	f->error = 0;
 	f->offset = 0;
 	f->total = 0;
 	f->tp = tp;
+	f->name = name;
 	f->do_crc = 0;
 	SHA1_Init(&f->ctx);
 	return f;
