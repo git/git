a3b0079c6a2e6336b061465623b8f2db308a6978
Shawn O. Pearce <spearce@spearce.org>
2007-11-11 02:29:30 -0500

    git-fetch: Always fetch tags if the object they reference exists
    Previously git-fetch.sh used `git cat-file -t` to determine if an
    object referenced by a tag exists, and if so fetch that tag locally.
    This was subtly broken during the port to C based builtin-fetch as
    lookup_object() only works to locate an object if it was previously
    accessed by the transport.  Not all transports will access all
    objects in this way, so tags were not always being fetched.
    
    The rsync transport never loads objects into the internal object
    table so automated tag following didn't work if rsync was used.
    Automated tag following also didn't work on the native transport
    if the new tag was behind the common point(s) negotiated between
    the two ends of the connection as the tag's referrant would not
    be loaded into the internal object table.  Further the automated
    tag following was broken with the HTTP commit walker if the new
    tag's referrant was behind an existing ref, as the walker would
    stop before loading the tag's referrant into the object table.
    
    Switching to has_sha1_file() restores the original behavior from
    the shell script by checking if the object exists in the ODB,
    without relying on the state left behind by a transport.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- builtin-fetch.c
+++ builtin-fetch.c
@@ -384,7 +384,7 @@ static struct ref *find_non_local_tags(struct transport *transport,
 
 		if (!path_list_has_path(&existing_refs, ref_name) &&
 		    !path_list_has_path(&new_refs, ref_name) &&
-		    lookup_object(ref->old_sha1)) {
+		    has_sha1_file(ref->old_sha1)) {
 			path_list_insert(ref_name, &new_refs);
 
 			rm = alloc_ref(strlen(ref_name) + 1);
