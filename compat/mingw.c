8ddaf895a80c1d42bd0e923cb0f1f041d00a27ea
Steffen Prohaska <prohaska@zib.de>
2007-11-17 20:27:40 +0100

    compat/mingw.c: Add cast of handle to fix warning
    [js: whitespace removed]
    
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Johannes Sixt <johannes.sixt@telecom.at>

--- compat/mingw.c
+++ compat/mingw.c
@@ -235,7 +235,7 @@ int pipe(int filedes[2])
 		CloseHandle(h[0]);
 		return -1;
 	}
-	fd = _open_osfhandle(h[0], O_NOINHERIT);
+	fd = _open_osfhandle((int)h[0], O_NOINHERIT);
 	if (fd < 0) {
 		close(filedes[0]);
 		close(filedes[1]);
@@ -245,7 +245,7 @@ int pipe(int filedes[2])
 	}
 	close(filedes[0]);
 	filedes[0] = fd;
-	fd = _open_osfhandle(h[1], O_NOINHERIT);
+	fd = _open_osfhandle((int)h[1], O_NOINHERIT);
 	if (fd < 0) {
 		close(filedes[0]);
 		close(filedes[1]);
