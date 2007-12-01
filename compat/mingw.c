f7597acac069bbaad8887cb315a4e5420222971a
Johannes Sixt <johannes.sixt@telecom.at>
2007-12-01 22:09:17 +0100

    Windows: A minimal implemention of getpwuid().
    getpwuid() is implemented just enough that GIT does not issue errors.
    Since the information that it returns is not very useful, users are
    required to set up user.name and user.email configuration.
    
    All uses of getpwuid() are like getpwuid(getuid()), hence, the return value
    of getuid() is irrelevant and the uid parameter is not even looked at.
    
    Side note: getpwnam() is only used to resolve '~' and '~username' paths,
    which is an idiom not known on Windows, hence, we don't implement it.
    
    Signed-off-by: Johannes Sixt <johannes.sixt@telecom.at>

--- compat/mingw.c
+++ compat/mingw.c
@@ -75,7 +75,15 @@ char *mingw_getcwd(char *pointer, int len)
 
 struct passwd *getpwuid(int uid)
 {
+	static char user_name[100];
 	static struct passwd p;
+
+	DWORD len = sizeof(user_name);
+	if (!GetUserName(user_name, &len))
+		return NULL;
+	p.pw_name = user_name;
+	p.pw_gecos = "unknown";
+	p.pw_dir = NULL;
 	return &p;
 }
 
