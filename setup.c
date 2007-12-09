bb4e3527b5d59a2db406dfa6519927a97d61770d
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2007-12-09 17:14:39 +0700

    Remove repo version check from setup_git_directory
    setup_git_directory_gently has done the check already.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -388,7 +388,6 @@ int check_repository_format(void)
 const char *setup_git_directory(void)
 {
 	const char *retval = setup_git_directory_gently(NULL);
-	check_repository_format();
 
 	/* If the work tree is not the default one, recompute prefix */
 	if (inside_work_tree < 0) {
