cf7e147cce9248e34b2e926d813c975c539aeaa6
Wincent Colaiuta <win@wincent.com>
2007-12-10 08:42:45 +0100

    Style fixes for pre-commit hook tests
    As pointed out by Junio on the mailing list, surrounding tests in
    double quotes can lead to bugs wherein variables get substituted away,
    so this isn't just style churn but important to prevent others from
    looking at these tests in the future and thinking that this is "the
    way" that Git tests should be written.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7503-pre-commit-hook.sh
+++ t/t7503-pre-commit-hook.sh
@@ -4,15 +4,21 @@ test_description='pre-commit hook'
 
 . ./test-lib.sh
 
-test_expect_success "with no hook" \
-	"echo 'foo' > file &&
-	 git add file &&
-	 git commit -m 'first'"
+test_expect_success 'with no hook' '
 
-test_expect_success "--no-verify with no hook" \
-	"echo 'bar' > file &&
-	 git add file &&
-	 git commit --no-verify -m 'bar'"
+	echo "foo" > file &&
+	git add file &&
+	git commit -m "first"
+
+'
+
+test_expect_success '--no-verify with no hook' '
+
+	echo "bar" > file &&
+	git add file &&
+	git commit --no-verify -m "bar"
+
+'
 
 # now install hook that always succeeds
 HOOKDIR="$(git rev-parse --git-dir)/hooks"
@@ -24,15 +30,21 @@ exit 0
 EOF
 chmod +x "$HOOK"
 
-test_expect_success "with succeeding hook" \
-	"echo 'more' >> file &&
-	 git add file &&
-	 git commit -m 'more'"
+test_expect_success 'with succeeding hook' '
 
-test_expect_success "--no-verify with succeeding hook" \
-	"echo 'even more' >> file &&
-	 git add file &&
-	 git commit --no-verify -m 'even more'"
+	echo "more" >> file &&
+	git add file &&
+	git commit -m "more"
+
+'
+
+test_expect_success '--no-verify with succeeding hook' '
+
+	echo "even more" >> file &&
+	git add file &&
+	git commit --no-verify -m "even more"
+
+'
 
 # now a hook that fails
 cat > "$HOOK" <<EOF
@@ -40,25 +52,37 @@ cat > "$HOOK" <<EOF
 exit 1
 EOF
 
-test_expect_failure "with failing hook" \
-	"echo 'another' >> file &&
-	 git add file &&
-	 git commit -m 'another'"
+test_expect_failure 'with failing hook' '
 
-test_expect_success "--no-verify with failing hook" \
-	"echo 'stuff' >> file &&
-	 git add file &&
-	 git commit --no-verify -m 'stuff'"
+	echo "another" >> file &&
+	git add file &&
+	git commit -m "another"
+
+'
+
+test_expect_success '--no-verify with failing hook' '
+
+	echo "stuff" >> file &&
+	git add file &&
+	git commit --no-verify -m "stuff"
+
+'
 
 chmod -x "$HOOK"
-test_expect_success "with non-executable hook" \
-	"echo 'content' >> file &&
-	 git add file &&
-	 git commit -m 'content'"
+test_expect_success 'with non-executable hook' '
 
-test_expect_success "--no-verify with non-executable hook" \
-	"echo 'more content' >> file &&
-	 git add file &&
-	 git commit --no-verify -m 'more content'"
+	echo "content" >> file &&
+	git add file &&
+	git commit -m "content"
+
+'
+
+test_expect_success '--no-verify with non-executable hook' '
+
+	echo "more content" >> file &&
+	git add file &&
+	git commit --no-verify -m "more content"
+
+'
 
 test_done
