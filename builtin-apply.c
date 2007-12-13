45e2a4b2b0a07cda05dda08b47906100c4711e4e
Wincent Colaiuta <win@wincent.com>
2007-12-13 14:32:30 +0100

    Make "diff --check" output match "git apply"
    For consistency, make the two tools report whitespace errors in the
    same way (the output of "diff --check" has been tweaked to match
    that of "git apply").
    
    Note that although the textual content is basically the same only
    "git diff --check" provides a colorized version of the problematic
    lines; making "git apply" do colorization will require more extensive
    changes (figuring out the diff colorization preferences of the user)
    and so that will be a subject for another commit.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-apply.c
+++ builtin-apply.c
@@ -912,8 +912,8 @@ static void check_whitespace(const char *line, int len, unsigned ws_rule)
 		;
 	else {
 		err = whitespace_error_string(result);
-		fprintf(stderr, "%s.\n%s:%d:%.*s\n",
-		     err, patch_input_file, linenr, len - 2, line + 1);
+		fprintf(stderr, "%s:%d: %s.\n%.*s\n",
+		     patch_input_file, linenr, err, len - 2, line + 1);
 		free(err);
 	}
 }
--- diff.c
+++ diff.c
@@ -973,8 +973,7 @@ static void checkdiff_consume(void *priv, char *line, unsigned long len)
 		if (!data->status)
 			return;
 		err = whitespace_error_string(data->status);
-		printf("%s:%d: %s%s:%s ", data->filename, data->lineno,
-		    ws, err, reset);
+		printf("%s:%d: %s.\n", data->filename, data->lineno, err);
 		free(err);
 		emit_line(set, reset, line, 1);
 		(void)check_and_emit_line(line + 1, len - 1, data->ws_rule,
