a2cf9f445e9074df1a45f5179cef0d4bb8647dd8
Pierre Habouzit <madcoder@debian.org>
2007-12-24 12:18:22 +0100

    git-name-rev: add a --(no-)undefined option.
    Rework get_rev_name to return NULL rather than "undefined" when a
    reference is undefined. If --undefined is passed (default), git-name-rev
    prints "undefined" for the name, else it die()s.
    
    Make git-describe use --no-undefined when calling git-name-rev so
    that --contains behavior matches the standard git-describe one.
    
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-describe.c
+++ builtin-describe.c
@@ -271,10 +271,11 @@ int cmd_describe(int argc, const char **argv, const char *prefix)
 	save_commit_buffer = 0;
 
 	if (contains) {
-		const char **args = xmalloc((5 + argc) * sizeof(char*));
+		const char **args = xmalloc((6 + argc) * sizeof(char*));
 		int i = 0;
 		args[i++] = "name-rev";
 		args[i++] = "--name-only";
+		args[i++] = "--no-undefined";
 		if (!all) {
 			args[i++] = "--tags";
 			if (pattern) {
