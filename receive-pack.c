061d6b9a7bfcf88ff3895aa1be8424ae1647cbca
Martin Koegler <mkoegler@auto.tuwien.ac.at>
2008-01-04 20:37:17 +0100

    receive-pack: reject invalid refnames
    Signed-off-by: Martin Koegler <mkoegler@auto.tuwien.ac.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- receive-pack.c
+++ receive-pack.c
@@ -165,7 +165,8 @@ static const char *update(struct command *cmd)
 	unsigned char *new_sha1 = cmd->new_sha1;
 	struct ref_lock *lock;
 
-	if (!prefixcmp(name, "refs/") && check_ref_format(name + 5)) {
+	/* only refs/... are allowed */
+	if (prefixcmp(name, "refs/") || check_ref_format(name + 5)) {
 		error("refusing to create funny ref '%s' remotely", name);
 		return "funny refname";
 	}
