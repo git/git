7ec43959f779fa75278c204a71acd4572acc9a1e
Nicolas Pitre <nico@cam.org>
2008-01-07 16:05:22 -0500

    slightly better auto gc message
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-gc.c
+++ builtin-gc.c
@@ -205,7 +205,7 @@ int cmd_gc(int argc, const char **argv, const char *prefix)
 		prune = 0;
 		if (!need_to_gc())
 			return 0;
-		fprintf(stderr, "Packing your repository for optimum "
+		fprintf(stderr, "Auto packing your repository for optimum "
 			"performance. You may also\n"
 			"run \"git gc\" manually. See "
 			"\"git help gc\" for more information.\n");
