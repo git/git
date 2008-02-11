180483c5c94a37c9fe304dded99d3db56d2d2b38
Junio C Hamano <gitster@pobox.com>
2008-02-11 11:00:32 -0800

    setup.c: guard config parser from value=NULL
    core.worktree expects a string value
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -372,6 +372,8 @@ int check_repository_format_version(const char *var, const char *value)
 		if (is_bare_repository_cfg == 1)
 			inside_work_tree = -1;
 	} else if (strcmp(var, "core.worktree") == 0) {
+		if (!value)
+			return config_error_nonbool(var);
 		if (git_work_tree_cfg)
 			free(git_work_tree_cfg);
 		git_work_tree_cfg = xstrdup(value);
