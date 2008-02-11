d865eb2a0d5f121a46dfcc18c78e7f27e0af0fbc
Junio C Hamano <gitster@pobox.com>
2008-02-11 10:46:39 -0800

    builtin-commit.c: guard config parser from value=NULL
    commit.template configuration expects a string value.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-commit.c
+++ builtin-commit.c
@@ -743,6 +743,8 @@ static void print_summary(const char *prefix, const unsigned char *sha1)
 int git_commit_config(const char *k, const char *v)
 {
 	if (!strcmp(k, "commit.template")) {
+		if (!v)
+			return config_error_nonbool(v);
 		template_file = xstrdup(v);
 		return 0;
 	}
