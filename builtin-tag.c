fd74cb0874126876227a958f6250323a4a4478a5
Gerrit Pape <pape@smarden.org>
2008-02-22 09:53:09 +0000

    builtin-tag.c: remove cruft
    After changing builtin-tag.c to use strbuf in fd17f5b (Replace all
    read_fd use with strbuf_read, and get rid of it.), the last condition
    in do_sign() will always be false, as it's checked already right
    above.  So let's remove the cruft.
    
    Signed-off-by: Gerrit Pape <pape@smarden.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-tag.c
+++ builtin-tag.c
@@ -236,9 +236,6 @@ static int do_sign(struct strbuf *buffer)
 	if (finish_command(&gpg) || !len || len < 0)
 		return error("gpg failed to sign the tag");
 
-	if (len < 0)
-		return error("could not read the entire signature from gpg.");
-
 	return 0;
 }
 
