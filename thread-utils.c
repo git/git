833e3df17188e48b2d31db0966a4051dad5e03b9
Andreas Ericsson <ae@op5.se>
2008-02-22 20:11:56 -0600

    pack-objects: Add runtime detection of online CPU's
    Packing objects can be done in parallell nowadays, but it's
    only done if the config option pack.threads is set to a value
    above 1. Because of that, the code-path used is often not the
    most optimal one.
    
    This patch adds a routine to detect the number of online CPU's
    at runtime (online_cpus()). When pack.threads (or --threads=) is
    given a value of 0, the number of threads is set to the number of
    online CPU's. This feature is also documented.
    
    As per Nicolas Pitre's recommendations, the default is still to
    run pack-objects single-threaded unless explicitly activated,
    either by configuration or by command line parameter.
    
    The routine online_cpus() is a rework of "numcpus.c", written by
    one Philip Willoughby <pgw99@doc.ic.ac.uk>. numcpus.c is in the
    public domain and can presently be downloaded from
    http://csgsoft.doc.ic.ac.uk/numcpus/
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    Signed-off-by: Brandon Casey <casey@nrlssc.navy.mil>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ thread-utils.c
@@ -0,0 +1,48 @@
+#include "cache.h"
+
+#ifdef _WIN32
+#  define WIN32_LEAN_AND_MEAN
+#  include <windows.h>
+#elif defined(hpux) || defined(__hpux) || defined(_hpux)
+#  include <sys/pstat.h>
+#endif
+
+/*
+ * By doing this in two steps we can at least get
+ * the function to be somewhat coherent, even
+ * with this disgusting nest of #ifdefs.
+ */
+#ifndef _SC_NPROCESSORS_ONLN
+#  ifdef _SC_NPROC_ONLN
+#    define _SC_NPROCESSORS_ONLN _SC_NPROC_ONLN
+#  elif defined _SC_CRAY_NCPU
+#    define _SC_NPROCESSORS_ONLN _SC_CRAY_NCPU
+#  endif
+#endif
+
+int online_cpus(void)
+{
+#ifdef _SC_NPROCESSORS_ONLN
+	long ncpus;
+#endif
+
+#ifdef _WIN32
+	SYSTEM_INFO info;
+	GetSystemInfo(&info);
+
+	if ((int)info.dwNumberOfProcessors > 0)
+		return (int)info.dwNumberOfProcessors;
+#elif defined(hpux) || defined(__hpux) || defined(_hpux)
+	struct pst_dynamic psd;
+
+	if (!pstat_getdynamic(&psd, sizeof(psd), (size_t)1, 0))
+		return (int)psd.psd_proc_cnt;
+#endif
+
+#ifdef _SC_NPROCESSORS_ONLN
+	if ((ncpus = (long)sysconf(_SC_NPROCESSORS_ONLN)) > 0)
+		return (int)ncpus;
+#endif
+
+	return 1;
+}
