ce4a7bff41c6bb4dc2d578264a429b5e13e89bdc
Shawn O. Pearce <spearce@spearce.org>
2008-02-24 22:57:29 -0500

    Correct fast-export file mode strings to match fast-import standard
    The fast-import file format does not expect leading '0' in front
    of a file mode; that is we want '100644' and '0100644'.
    
    Thanks to Ian Clatworthy of the Bazaar project for noticing the
    difference in output/input.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fast-export.c
+++ builtin-fast-export.c
@@ -123,7 +123,7 @@ static void show_filemodify(struct diff_queue_struct *q,
 			printf("D %s\n", spec->path);
 		else {
 			struct object *object = lookup_object(spec->sha1);
-			printf("M 0%06o :%d %s\n", spec->mode,
+			printf("M %06o :%d %s\n", spec->mode,
 			       get_object_mark(object), spec->path);
 		}
 	}
