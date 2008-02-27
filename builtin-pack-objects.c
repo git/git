392b78ca42b6b1a68a77966acab38554c6edab3e
Junio C Hamano <gitster@pobox.com>
2008-02-26 23:27:31 -0800

    Revert "pack-objects: Print a message describing the number of threads for packing"
    This reverts commit 6c723f5e6bc579e06a904874f1ceeb8ff2b5a17c.
    The additional message may be interesting for git developers,
    but not useful for the end users, and clutters the output.

--- builtin-pack-objects.c
+++ builtin-pack-objects.c
@@ -2239,9 +2239,6 @@ int cmd_pack_objects(int argc, const char **argv, const char *prefix)
 #ifdef THREADED_DELTA_SEARCH
 	if (!delta_search_threads)	/* --threads=0 means autodetect */
 		delta_search_threads = online_cpus();
-	if (progress)
-		fprintf(stderr, "Using %d pack threads.\n",
-			delta_search_threads);
 #endif
 
 	prepare_packed_git();
