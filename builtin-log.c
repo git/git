42be5cc61202014d4f1df61f9791067191f9393d
Junio C Hamano <gitster@pobox.com>
2008-02-27 22:08:57 -0800

    format-patch: remove a leftover debugging message
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-log.c
+++ builtin-log.c
@@ -960,7 +960,6 @@ int cmd_format_patch(int argc, const char **argv, const char *prefix)
 	rev.boundary = 1;
 	while ((commit = get_revision(&rev)) != NULL) {
 		if (commit->object.flags & BOUNDARY) {
-			fprintf(stderr, "Boundary %s\n", sha1_to_hex(commit->object.sha1));
 			boundary_count++;
 			origin = (boundary_count == 1) ? commit : NULL;
 			continue;
