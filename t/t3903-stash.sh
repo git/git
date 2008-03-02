059f13045aab9e22a3db2d53ee6abe4a08c4582c
Brandon Casey <drafnel@gmail.com>
2008-03-02 14:58:48 -0600

    t3903-stash.sh: Add missing '&&' to body of testcase
    Signed-off-by: Brandon Casey <drafnel@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3903-stash.sh
+++ t/t3903-stash.sh
@@ -40,8 +40,8 @@ test_expect_success 'parents of stash' '
 test_expect_success 'apply needs clean working directory' '
 	echo 4 > other-file &&
 	git add other-file &&
-	echo 5 > other-file
-	! git stash apply
+	echo 5 > other-file &&
+ 	test_must_fail git stash apply
 '
 
 test_expect_success 'apply stashed changes' '
