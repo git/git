d89c1dfac939623a269f60d4e27e3a2929dca29c
Jeff King <peff@peff.net>
2008-03-12 17:41:02 -0400

    filter-branch: don't use xargs -0
    Some versions of xargs don't understand "-0"; fortunately in
    this case we can get the same effect by using "git clean".
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-filter-branch.sh
+++ git-filter-branch.sh
@@ -281,7 +281,7 @@ while read commit parents; do
 			die "Could not checkout the index"
 		# files that $commit removed are now still in the working tree;
 		# remove them, else they would be added again
-		git ls-files -z --others | xargs -0 rm -f
+		git clean -q -f -x
 		eval "$filter_tree" < /dev/null ||
 			die "tree filter failed: $filter_tree"
 
