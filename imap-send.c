5b67b8e2d4ed8a152694f4555549fcde5dae77f5
Gerrit Pape <pape@smarden.org>
2008-03-26 18:05:17 +0000

    imap-send: properly error out if imap.host is not set in config
    If no imap host is specified in the git config, git imap-send used
    to try to lookup a null pointer through gethostbyname(), causing a
    segfault.  Since setting the imap.host variable is mandatory,
    imap-send now properly fails with an explanatory error message.
    
    The problem has been reported by picca through
     http://bugs.debian.org/472632
    
    Signed-off-by: Gerrit Pape <pape@smarden.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- imap-send.c
+++ imap-send.c
@@ -1302,6 +1302,10 @@ main(int argc, char **argv)
 		fprintf( stderr, "no imap store specified\n" );
 		return 1;
 	}
+	if (!server.host) {
+		fprintf( stderr, "no imap host specified\n" );
+		return 1;
+	}
 
 	/* read the messages */
 	if (!read_message( stdin, &all_msgs )) {
