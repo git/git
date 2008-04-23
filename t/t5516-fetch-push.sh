f8aae120345f511e59bb008e8de2a8f6e65cf377
Jeff King <peff@peff.net>
2008-04-23 05:16:06 -0400

    push: allow unqualified dest refspecs to DWIM
    Previously, a push like:
    
      git push remote src:dst
    
    would go through the following steps:
    
      1. check for an unambiguous 'dst' on the remote; if it
         exists, then push to that ref
      2. otherwise, check if 'dst' begins with 'refs/'; if it
         does, create a new ref
      3. otherwise, complain because we don't know where in the
         refs hierarchy to put 'dst'
    
    However, in some cases, we can guess about the ref type of
    'dst' based on the ref type of 'src'. Specifically, before
    complaining we now check:
    
      2.5. if 'src' resolves to a ref starting with refs/heads
           or refs/tags, then prepend that to 'dst'
    
    So now this creates a new branch on the remote, whereas it
    previously failed with an error message:
    
      git push master:newbranch
    
    Note that, by design, we limit this DWIM behavior only to
    source refs which resolve exactly (including symrefs which
    resolve to existing refs). We still complain on a partial
    destination refspec if the source is a raw sha1, or a ref
    expression such as 'master~10'.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5516-fetch-push.sh
+++ t/t5516-fetch-push.sh
@@ -273,6 +273,37 @@ test_expect_success 'push with colon-less refspec (4)' '
 
 '
 
+test_expect_success 'push head with non-existant, incomplete dest' '
+
+	mk_test &&
+	git push testrepo master:branch &&
+	check_push_result $the_commit heads/branch
+
+'
+
+test_expect_success 'push tag with non-existant, incomplete dest' '
+
+	mk_test &&
+	git tag -f v1.0 &&
+	git push testrepo v1.0:tag &&
+	check_push_result $the_commit tags/tag
+
+'
+
+test_expect_success 'push sha1 with non-existant, incomplete dest' '
+
+	mk_test &&
+	test_must_fail git push testrepo `git rev-parse master`:foo
+
+'
+
+test_expect_success 'push ref expression with non-existant, incomplete dest' '
+
+	mk_test &&
+	test_must_fail git push testrepo master^:branch
+
+'
+
 test_expect_success 'push with HEAD' '
 
 	mk_test heads/master &&
@@ -308,6 +339,15 @@ test_expect_success 'push with +HEAD' '
 	# With force rewinding should succeed
 	git push testrepo +HEAD &&
 	check_push_result $the_first_commit heads/local
+
+'
+
+test_expect_success 'push HEAD with non-existant, incomplete dest' '
+
+	mk_test &&
+	git checkout master &&
+	git push testrepo HEAD:branch &&
+	check_push_result $the_commit heads/branch
 
 '
 
