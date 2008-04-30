b8960bbe7bdfc0b232462f916ee8151c83afd16f
Jeff King <peff@peff.net>
2008-04-30 13:25:53 -0400

    diff: make "too many files" rename warning optional
    In many cases, the warning ends up as clutter, because the
    diff is being done "behind the scenes" from the user (e.g.,
    when generating a commit diffstat), and whether we show
    renames or not is not particularly interesting to the user.
    
    However, in the case of a merge (which is what motivated the
    warning in the first place), it is a useful hint as to why a
    merge with renames might have failed.
    
    This patch makes the warning optional based on the code
    calling into diffcore. We default to not showing the
    warning, but turn it on for merges.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-merge-recursive.c
+++ builtin-merge-recursive.c
@@ -365,6 +365,7 @@ static struct path_list *get_renames(struct tree *tree,
 	opts.rename_limit = merge_rename_limit >= 0 ? merge_rename_limit :
 			    diff_rename_limit >= 0 ? diff_rename_limit :
 			    500;
+	opts.warn_on_too_large_rename = 1;
 	opts.output_format = DIFF_FORMAT_NO_OUTPUT;
 	if (diff_setup_done(&opts) < 0)
 		die("diff setup failed");
