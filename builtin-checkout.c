278863180a280304d6b7f4ad6f11c1a810463507
Jeff King <peff@peff.net>
2008-05-02 10:05:36 -0400

    checkout: don't rfc2047-encode oneline on detached HEAD
    When calling pretty_print_commit, there is an implicit
    assumption that passing in a non-NULL "subject" variable
    for oneline or email formats means that the output is part
    of a subject and therefore "subject" to rfc2047 encoding.
    This is not the desired effect when reporting the movement
    of detached HEAD.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -142,7 +142,7 @@ static void describe_detached_head(char *msg, struct commit *commit)
 	struct strbuf sb;
 	strbuf_init(&sb, 0);
 	parse_commit(commit);
-	pretty_print_commit(CMIT_FMT_ONELINE, commit, &sb, 0, "", "", 0, 0);
+	pretty_print_commit(CMIT_FMT_ONELINE, commit, &sb, 0, NULL, NULL, 0, 0);
 	fprintf(stderr, "%s %s... %s\n", msg,
 		find_unique_abbrev(commit->object.sha1, DEFAULT_ABBREV), sb.buf);
 	strbuf_release(&sb);
