d5350fd2b3a4721885bf1b3353478970caef268c
Gerrit Pape <pape@smarden.org>
2008-05-27 08:59:16 +0000

    commit --interactive: properly update the index before commiting
    When adding files through git commit --interactive, and 'quit'
    afterwards, the message in the editor of the commit message indicates
    that many (maybe all) files are deleted from the tree.  Dismissing that
    and running git commit afterwards does the right thing.  This commit
    fixes git commit --interactive to properly update the index before
    commiting.
    
    Reported by Jiří Paleček through
     http://bugs.debian.org/480429
    
    Signed-off-by: Gerrit Pape <pape@smarden.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-commit.c
+++ builtin-commit.c
@@ -219,6 +219,8 @@ static char *prepare_index(int argc, const char **argv, const char *prefix)
 
 	if (interactive) {
 		interactive_add(argc, argv, prefix);
+		if (read_cache() < 0)
+			die("index file corrupt");
 		commit_style = COMMIT_AS_IS;
 		return get_index_file();
 	}
