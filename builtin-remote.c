8b7d4e738ee5489fecb5c4a55d14884b09e0dc6d
Junio C Hamano <gitster@pobox.com>
2008-06-11 23:43:25 -0700

    "remote prune": be quiet when there is nothing to prune
    The previous commit made it always say "Pruning $remote" but reported the
    URL only when there is something to prune.  Make it consistent by not
    saying anything at all when there is nothing to prune.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-remote.c
+++ builtin-remote.c
@@ -560,12 +560,13 @@ static int prune(int argc, const char **argv)
 
 		get_remote_ref_states(*argv, &states, 1);
 
-		printf("Pruning %s\n", *argv);
-		if (states.stale.nr)
+		if (states.stale.nr) {
+			printf("Pruning %s\n", *argv);
 			printf("URL: %s\n",
 			       states.remote->url_nr
 			       ? states.remote->url[0]
 			       : "(no URL)");
+		}
 
 		for (i = 0; i < states.stale.nr; i++) {
 			const char *refname = states.stale.items[i].util;
