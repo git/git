7f8365f894d60f240edd356f32e3c1bda994ed41
Junio C Hamano <gitster@pobox.com>
2008-06-22 02:03:26 -0700

    t4200: fix rerere test
    The test used "diff-files -q" which is not about reporting if there is
    a difference at all.  Instead, make sure that the path remains as
    conflicting in the index after rerere autoresolves it, as we will be
    adding rerere.autoupdate configuration with the next patch.

--- t/t4200-rerere.sh
+++ t/t4200-rerere.sh
@@ -193,9 +193,9 @@ test_expect_success 'resolution was recorded properly' '
 	echo Bello > file3 &&
 	git add file3 &&
 	git commit -m version2 &&
-	! git merge fifth &&
-	git diff-files -q &&
-	test Cello = "$(cat file3)"
+	test_must_fail git merge fifth &&
+	test Cello = "$(cat file3)" &&
+	test 0 != $(git ls-files -u | wc -l)
 '
 
 test_done
