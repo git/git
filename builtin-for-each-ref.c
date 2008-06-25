87412ec1f17bc2705595ebabc010d94cb46478b7
Jeff King <peff@peff.net>
2008-06-25 12:08:15 -0400

    for-each-ref: implement missing tag values
    The "type" and "object" fields for tags were accepted as
    valid atoms, but never implemented. Consequently, they
    simply returned the empty string, even for valid tags.
    
    Noticed by Lea Wiemann.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-for-each-ref.c
+++ builtin-for-each-ref.c
@@ -234,6 +234,13 @@ static void grab_tag_values(struct atom_value *val, int deref, struct object *ob
 			name++;
 		if (!strcmp(name, "tag"))
 			v->s = tag->tag;
+		else if (!strcmp(name, "type") && tag->tagged)
+			v->s = typename(tag->tagged->type);
+		else if (!strcmp(name, "object") && tag->tagged) {
+			char *s = xmalloc(41);
+			strcpy(s, sha1_to_hex(tag->tagged->sha1));
+			v->s = s;
+		}
 	}
 }
 
