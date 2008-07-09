14c8eef4e02f9c2f7955c435467f42fa3aeac072
Peter Harris <git@peter.is-a-geek.org>
2008-07-09 17:34:20 -0400

    Add ANSI control code emulation for the Windows console
    This adds only the minimum necessary to keep git pull/merge's diffstat from
    wrapping. Notably absent is support for the K (erase) operation, and support
    for POSIX write.
    
    Signed-off-by: Peter Harris <git@peter.is-a-geek.org>
    Signed-off-by: Johannes Sixt <johannes.sixt@telecom.at>

--- compat/mingw.h
+++ compat/mingw.h
@@ -194,6 +194,17 @@ sig_handler_t mingw_signal(int sig, sig_handler_t handler);
 #define signal mingw_signal
 
 /*
+ * ANSI emulation wrappers
+ */
+
+int winansi_fputs(const char *str, FILE *stream);
+int winansi_printf(const char *format, ...) __attribute__((format (printf, 1, 2)));
+int winansi_fprintf(FILE *stream, const char *format, ...) __attribute__((format (printf, 2, 3)));
+#define fputs winansi_fputs
+#define printf(...) winansi_printf(__VA_ARGS__)
+#define fprintf(...) winansi_fprintf(__VA_ARGS__)
+
+/*
  * git specific compatibility
  */
 
