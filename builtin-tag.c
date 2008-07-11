c4adea82c5064971ff6d090be8db89c44456186b
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-07-11 18:55:57 +0200

    Convert CR/LF to LF in tag signatures
    On Windows, gpg outputs CR/LF signatures.  But since the tag messages
    are already stripped of the CR by stripspace(), it is arguably nicer
    to do the same for the tag signature.  Actually, this patch does not
    look for CR/LF, but strips all CRs from the signature.  It does so not
    only on Windows but on all platforms to keep the code simpler.
    
    [ spr: ported code to use strbuf ]
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-tag.c
+++ builtin-tag.c
@@ -202,6 +202,7 @@ static int do_sign(struct strbuf *buffer)
 	const char *args[4];
 	char *bracket;
 	int len;
+	int i, j;
 
 	if (!*signingkey) {
 		if (strlcpy(signingkey, git_committer_info(IDENT_ERROR_ON_NO_NAME),
@@ -240,6 +241,15 @@ static int do_sign(struct strbuf *buffer)
 
 	if (finish_command(&gpg) || !len || len < 0)
 		return error("gpg failed to sign the tag");
+
+	/* Strip CR from the line endings, in case we are on Windows. */
+	for (i = j = 0; i < buffer->len; i++)
+		if (buffer->buf[i] != '\r') {
+			if (i != j)
+				buffer->buf[j] = buffer->buf[i];
+			j++;
+		}
+	strbuf_setlen(buffer, j);
 
 	return 0;
 }
