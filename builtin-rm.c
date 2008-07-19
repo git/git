4d2646727997c82c04a82ce38e56d76e7b5d268b
Olivier Marin <dkr@freesurf.fr>
2008-07-19 18:24:46 +0200

    builtin-rm: fix index lock file path
    When hold_locked_index() is called with a relative git_dir and you are
    outside the work tree, the lock file become relative to the current
    directory. So when later setup_work_tree() change the current directory
    it breaks lock file path and commit_locked_index() fails.
    
    This patch move index locking code after setup_work_tree() call to make
    lock file relative to the working tree as it should be and add a test
    case.
    
    Noticed by Nick Andrew.
    
    Signed-off-by: Olivier Marin <dkr@freesurf.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-rm.c
+++ builtin-rm.c
@@ -146,17 +146,17 @@ int cmd_rm(int argc, const char **argv, const char *prefix)
 
 	git_config(git_default_config, NULL);
 
-	newfd = hold_locked_index(&lock_file, 1);
-
-	if (read_cache() < 0)
-		die("index file corrupt");
-
 	argc = parse_options(argc, argv, builtin_rm_options, builtin_rm_usage, 0);
 	if (!argc)
 		usage_with_options(builtin_rm_usage, builtin_rm_options);
 
 	if (!index_only)
 		setup_work_tree();
+
+	newfd = hold_locked_index(&lock_file, 1);
+
+	if (read_cache() < 0)
+		die("index file corrupt");
 
 	pathspec = get_pathspec(prefix, argv);
 	seen = NULL;
