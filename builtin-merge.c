74f5b7fba420eb6b807db48d0ee7a000202f8225
Michele Ballabio <barra_cuda@katamail.com>
2008-07-20 14:34:47 +0200

    builtin-merge.c: Fix option parsing
    Now "git merge -m" needs a message, and errors out with the usage
    text if none is given.
    This way, t7600-merge.sh is fixed.
    
    Signed-off-by: Michele Ballabio <barra_cuda@katamail.com>
    Acked-by: Miklos Vajna <vmiklos@frugalware.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-merge.c
+++ builtin-merge.c
@@ -66,10 +66,11 @@ static int option_parse_message(const struct option *opt,
 
 	if (unset)
 		strbuf_setlen(buf, 0);
-	else {
+	else if (arg) {
 		strbuf_addf(buf, "%s\n\n", arg);
 		have_message = 1;
-	}
+	} else
+		return error("switch `m' requires a value");
 	return 0;
 }
 
