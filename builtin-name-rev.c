b003c00b7b5e352569061fec0b1e1bd0d0fa8b6a
Pieter de Bie <pdebie@ai.rug.nl>
2008-07-31 15:20:34 +0200

    git-name-rev: allow --name-only in combination with --stdin
    Signed-off-by: Pieter de Bie <pdebie@ai.rug.nl>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-name-rev.c
+++ builtin-name-rev.c
@@ -204,8 +204,13 @@ static void name_rev_line(char *p, struct name_ref_data *data)
 			if (!name)
 				continue;
 
-			fwrite(p_start, p - p_start + 1, 1, stdout);
-			printf(" (%s)", name);
+			if (data->name_only) {
+				fwrite(p_start, p - p_start + 1 - 40, 1, stdout);
+				printf(name);
+			} else {
+				fwrite(p_start, p - p_start + 1, 1, stdout);
+				printf(" (%s)", name);
+			}
 			p_start = p + 1;
 		}
 	}
