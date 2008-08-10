90fb46ec83cd837364592ce4d95055a3b6fe89a4
Pieter de Bie <pdebie@ai.rug.nl>
2008-08-10 22:22:21 +0200

    builtin-reflog: Allow reflog expire to name partial ref
    This allows you to specify 'git reflog expire master' without needing
    to give the full refname like 'git reflog expire refs/heads/master'
    
    Signed-off-by: Pieter de Bie <pdebie@ai.rug.nl>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-reflog.c
+++ builtin-reflog.c
@@ -540,11 +540,11 @@ static int cmd_reflog_expire(int argc, const char **argv, const char *prefix)
 		free(collected.e);
 	}
 
-	while (i < argc) {
-		const char *ref = argv[i++];
+	for (; i < argc; i++) {
+		char *ref;
 		unsigned char sha1[20];
-		if (!resolve_ref(ref, sha1, 1, NULL)) {
-			status |= error("%s points nowhere!", ref);
+		if (!dwim_log(argv[i], strlen(argv[i]), sha1, &ref)) {
+			status |= error("%s points nowhere!", argv[i]);
 			continue;
 		}
 		set_reflog_expiry_param(&cb, explicit_expiry, ref);
