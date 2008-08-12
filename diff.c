04c6e9e9ca34226db095bbaa1218030f99f0b7c6
Junio C Hamano <gitster@pobox.com>
2008-08-11 22:15:28 -0700

    diff --check: do not unconditionally complain about trailing empty lines
    Recently "git diff --check" learned to detect new trailing blank lines
    just like "git apply --whitespace" does.  However this check should not
    trigger unconditionally.  This patch makes it honor the whitespace
    settings from core.whitespace and gitattributes.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -1631,7 +1631,8 @@ static void builtin_checkdiff(const char *name_a, const char *name_b,
 		ecb.priv = &data;
 		xdi_diff(&mf1, &mf2, &xpp, &xecfg, &ecb);
 
-		if (data.trailing_blanks_start) {
+		if ((data.ws_rule & WS_TRAILING_SPACE) &&
+		    data.trailing_blanks_start) {
 			fprintf(o->file, "%s:%d: ends with blank lines.\n",
 				data.filename, data.trailing_blanks_start);
 			data.status = 1; /* report errors */
