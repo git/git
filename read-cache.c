16ce2e4c8f9664f8ec5ae2bffed34d093d83a4d3
Junio C Hamano <gitster@pobox.com>
2008-08-16 23:02:08 -0700

    index: future proof for "extended" index entries
    We do not have any more bits in the on-disk index flags word, but we would
    need to have more in the future.  Use the last remaining bits as a signal
    to tell us that the index entry we are looking at is an extended one.
    
    Since we do not understand the extended format yet, we will just error out
    when we see it.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- read-cache.c
+++ read-cache.c
@@ -1118,6 +1118,10 @@ static void convert_from_disk(struct ondisk_cache_entry *ondisk, struct cache_en
 	ce->ce_size  = ntohl(ondisk->size);
 	/* On-disk flags are just 16 bits */
 	ce->ce_flags = ntohs(ondisk->flags);
+
+	/* For future extension: we do not understand this entry yet */
+	if (ce->ce_flags & CE_EXTENDED)
+		die("Unknown index entry format");
 	hashcpy(ce->sha1, ondisk->sha1);
 
 	len = ce->ce_flags & CE_NAMEMASK;
