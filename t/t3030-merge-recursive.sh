446247db78a733f44d2470afb1f1983d28058159
Junio C Hamano <gitster@pobox.com>
2008-08-23 12:56:57 -0700

    merge: fix numerus bugs around "trivial merge" area
    The "trivial merge" codepath wants to optimize itself by making an
    internal call to the read-tree machinery, but it does not read the index
    before doing so, and the codepath is never exercised.  Incidentally, this
    failure to read the index upfront means that the safety to refuse doing
    anything when the index is unmerged does not kick in, either.
    
    These two problem are fixed by using read_cache_unmerged() that does read
    the index before checking if it is unmerged at the beginning of
    cmd_merge().
    
    The primary logic of the merge, however, assumes that the process never
    reads the index in-core, and the call to write_cache_as_tree() it makes
    from write_tree_trivial() will always read from the on-disk index that is
    prepared the strategy back-ends.  This assumption is now broken by the
    above fix.  To fix this issue, we now call discard_cache() before calling
    write_tree_trivial() when it wants to write the on-disk index as a tree.
    
    When multiple strategies are tried, their results are evaluated by reading
    the resulting index and inspecting it.  The codepath needs to make a call
    to read_cache() for each successful strategy, and for that to work, they
    need to discard_cache() the one read by the previous round.
    
    Also the "trivial merge" forgot that the current commit is one of the
    parents of the resulting commit.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3030-merge-recursive.sh
+++ t/t3030-merge-recursive.sh
@@ -269,6 +269,17 @@ test_expect_success 'merge-recursive result' '
 
 '
 
+test_expect_success 'fail if the index has unresolved entries' '
+
+	rm -fr [abcd] &&
+	git checkout -f "$c1" &&
+
+	test_must_fail git merge "$c5" &&
+	test_must_fail git merge "$c5" 2> out &&
+	grep "You are in the middle of a conflicted merge" out
+
+'
+
 test_expect_success 'merge-recursive remove conflict' '
 
 	rm -fr [abcd] &&
