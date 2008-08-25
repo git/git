68daa64df2363f848d818dda9fc414511d9330da
Jeff King <peff@peff.net>
2008-08-24 22:10:29 -0400

    format-patch: use default diff format even with patch options
    Previously, running "git format-patch -U5" would cause the
    low-level diff machinery to change the diff output format
    from "not specified" to "patch". This meant that
    format-patch thought we explicitly specified a diff output
    format, and would not use the default format. The resulting
    message lacked both the diffstat and the summary, as well as
    the separating "---".
    
    Now format-patch explicitly checks for this condition and
    uses the default. That means that "git format-patch -p" will
    now have the "-p" ignored.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-log.c
+++ builtin-log.c
@@ -932,7 +932,8 @@ int cmd_format_patch(int argc, const char **argv, const char *prefix)
 	if (argc > 1)
 		die ("unrecognized argument: %s", argv[1]);
 
-	if (!rev.diffopt.output_format)
+	if (!rev.diffopt.output_format
+		|| rev.diffopt.output_format == DIFF_FORMAT_PATCH)
 		rev.diffopt.output_format = DIFF_FORMAT_DIFFSTAT | DIFF_FORMAT_SUMMARY | DIFF_FORMAT_PATCH;
 
 	if (!DIFF_OPT_TST(&rev.diffopt, TEXT) && !no_binary_diff)
