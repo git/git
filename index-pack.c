d0b92a3f6e4d98a38a86cbd86f0e39eea9005958
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2008-08-26 21:32:42 +0700

    index-pack: setup git repository
    "git index-pack" is an independent command and does not setup git
    repository while still need pack.indexversion. It may miss the
    info if it is in a subdirectory of the repository.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- index-pack.c
+++ index-pack.c
@@ -876,7 +876,9 @@ int main(int argc, char **argv)
 	char *index_name_buf = NULL, *keep_name_buf = NULL;
 	struct pack_idx_entry **idx_objects;
 	unsigned char sha1[20];
+	int nongit = 0;
 
+	setup_git_directory_gently(&nongit);
 	git_config(git_index_pack_config, NULL);
 
 	for (i = 1; i < argc; i++) {
