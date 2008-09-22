8b4eb6b6cd65042c6ecb4f06f19c1f2441899ed6
Petr Baudis <pasky@suse.cz>
2008-09-22 19:20:21 +0200

    Do not perform cross-directory renames when creating packs
    A comment on top of create_tmpfile() describes caveats ('can have
    problems on various systems (FAT, NFS, Coda)') that should apply
    in this situation as well.  This in the end did not end up solving
    any of my personal problems, but it might be a useful cleanup patch
    nevertheless.
    
    Signed-off-by: Petr Baudis <pasky@suse.cz>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fast-import.c
+++ fast-import.c
@@ -816,7 +816,7 @@ static void start_packfile(void)
 	int pack_fd;
 
 	snprintf(tmpfile, sizeof(tmpfile),
-		"%s/tmp_pack_XXXXXX", get_object_directory());
+		"%s/pack/tmp_pack_XXXXXX", get_object_directory());
 	pack_fd = xmkstemp(tmpfile);
 	p = xcalloc(1, sizeof(*p) + strlen(tmpfile) + 2);
 	strcpy(p->pack_name, tmpfile);
@@ -878,7 +878,7 @@ static char *create_index(void)
 	}
 
 	snprintf(tmpfile, sizeof(tmpfile),
-		"%s/tmp_idx_XXXXXX", get_object_directory());
+		"%s/pack/tmp_idx_XXXXXX", get_object_directory());
 	idx_fd = xmkstemp(tmpfile);
 	f = sha1fd(idx_fd, tmpfile);
 	sha1write(f, array, 256 * sizeof(int));
