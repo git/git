048f2762007d022defceb6850a44bc1bd5ccebf7
Dmitry Potapov <dpotapov@gmail.com>
2008-10-05 06:14:40 +0400

    do not segfault if make_cache_entry failed
    Signed-off-by: Dmitry Potapov <dpotapov@gmail.com>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -206,6 +206,8 @@ static int checkout_merged(int pos, struct checkout *state)
 	ce = make_cache_entry(create_ce_mode(active_cache[pos+1]->ce_mode),
 			      sha1,
 			      path, 2, 0);
+	if (!ce)
+		die("make_cache_entry failed for path '%s'", path);
 	status = checkout_entry(ce, state, NULL);
 	return status;
 }
--- builtin-reset.c
+++ builtin-reset.c
@@ -121,6 +121,9 @@ static void update_index_from_diff(struct diff_queue_struct *q,
 			struct cache_entry *ce;
 			ce = make_cache_entry(one->mode, one->sha1, one->path,
 				0, 0);
+			if (!ce)
+				die("make_cache_entry failed for path '%s'",
+				    one->path);
 			add_cache_entry(ce, ADD_CACHE_OK_TO_ADD |
 				ADD_CACHE_OK_TO_REPLACE);
 		} else
