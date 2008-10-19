828e32b82e3e2bb10d6d730d3abe505063b481f6
Junio C Hamano <gitster@pobox.com>
2008-10-19 15:32:35 -0700

    Fix mismerge at cdb22c4 in builtin-checkout.c
    The code to complain when -b is not given but an explicit --track/--no-track
    override was given from the command line was unchanged on one branch and
    reworked on the other branch.  The merge result incorrectly kept it.
    
    Spotted by Matt McCutchen.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -635,9 +635,6 @@ int cmd_checkout(int argc, const char **argv, const char *prefix)
 		git_xmerge_config("merge.conflictstyle", conflict_style, NULL);
 	}
 
-	if (!opts.new_branch && (opts.track != git_branch_track))
-		die("git checkout: --track and --no-track require -b");
-
 	if (opts.force && opts.merge)
 		die("git checkout: -f and -m are incompatible");
 
