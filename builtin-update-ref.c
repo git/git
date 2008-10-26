eca35a25a92a1ad725af2a549fc9158488c4cc43
Miklos Vajna <vmiklos@frugalware.org>
2008-10-26 03:33:56 +0100

    Fix git branch -m for symrefs.
    This had two problems with symrefs. First, it copied the actual sha1
    instead of the "pointer", second it failed to remove the old ref after a
    successful rename.
    
    Given that till now delete_ref() always dereferenced symrefs, a new
    parameters has been introduced to delete_ref() to allow deleting refs
    without a dereference.
    
    Signed-off-by: Miklos Vajna <vmiklos@frugalware.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-update-ref.c
+++ builtin-update-ref.c
@@ -48,7 +48,7 @@ int cmd_update_ref(int argc, const char **argv, const char *prefix)
 		die("%s: not a valid old SHA1", oldval);
 
 	if (delete)
-		return delete_ref(refname, oldval ? oldsha1 : NULL);
+		return delete_ref(refname, oldval ? oldsha1 : NULL, 0);
 	else
 		return update_ref(msg, refname, sha1, oldval ? oldsha1 : NULL,
 				  no_deref ? REF_NODEREF : 0, DIE_ON_ERR);
