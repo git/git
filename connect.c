a80732897931c737c01d04289dd1512bd12a4b04
Tom Preston-Werner <tom@github.com>
2008-11-01 11:44:45 -0700

    connect.c: add a way for git-daemon to pass an error back to client
    The current behavior of git-daemon is to simply close the connection on
    any error condition. This leaves the client without any information as
    to the cause of the failed fetch/push/etc.
    
    This patch allows get_remote_heads to accept a line prefixed with "ERR"
    that it can display to the user in an informative fashion. Once clients
    can understand this ERR line, git-daemon can be made to properly report
    "repository not found", "permission denied", or other errors.
    
    Example
    
    S: ERR No matching repository.
    C: fatal: remote error: No matching repository.
    
    Signed-off-by: Tom Preston-Werner <tom@github.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- connect.c
+++ connect.c
@@ -70,6 +70,9 @@ struct ref **get_remote_heads(int in, struct ref **list,
 		if (buffer[len-1] == '\n')
 			buffer[--len] = 0;
 
+		if (len > 4 && !prefixcmp(buffer, "ERR "))
+			die("remote error: %s", buffer + 4);
+
 		if (len < 42 || get_sha1_hex(buffer, old_sha1) || buffer[40] != ' ')
 			die("protocol error: expected sha/ref, got '%s'", buffer);
 		name = buffer + 41;
