8c1f6f6c5797d1a3fa9ec9d38ae30b4f19e70a3c
Junio C Hamano <gitster@pobox.com>
2008-11-09 13:08:38 -0800

    t5303: work around printf breakage in dash
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5303-pack-corruption-resilience.sh
+++ t/t5303-pack-corruption-resilience.sh
@@ -234,7 +234,7 @@ test_expect_success \
     'corruption #1 in delta base reference of first delta (OBJ_OFS_DELTA)' \
     'create_new_pack --delta-base-offset &&
      git prune-packed &&
-     printf "\x01" | do_corrupt_object $blob_2 2 &&
+     tr "\000" "\001" </dev/zero | do_corrupt_object $blob_2 2 &&
      git cat-file blob $blob_1 > /dev/null &&
      test_must_fail git cat-file blob $blob_2 > /dev/null &&
      test_must_fail git cat-file blob $blob_3 > /dev/null'
