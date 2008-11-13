d0f19d0471792ace6d440c2789ce42227f07e69e
Brandon Casey <casey@nrlssc.navy.mil>
2008-11-13 14:20:37 -0600

    revision.c: use proper data type in call to sizeof() within xrealloc
    A type char** was being used instead of char*.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -968,7 +968,7 @@ static void add_ignore_packed(struct rev_info *revs, const char *name)
 	int num = ++revs->num_ignore_packed;
 
 	revs->ignore_packed = xrealloc(revs->ignore_packed,
-				       sizeof(const char **) * (num + 1));
+				       sizeof(const char *) * (num + 1));
 	revs->ignore_packed[num-1] = name;
 	revs->ignore_packed[num] = NULL;
 }
