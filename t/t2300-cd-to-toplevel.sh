08fc0608657ee91bc85276667804c36a93138c7d
Marcel M. Cary <marcel@oak.homeunix.org>
2008-12-15 09:34:37 -0800

    git-sh-setup: Fix scripts whose PWD is a symlink into a git work-dir
    I want directories of my working tree to be linked to from various
    paths on my filesystem where third-party components expect them, both
    in development and production environments.  A build system's install
    step could solve this, but I develop scripts and web pages that don't
    need to be built.  Git's submodule system could solve this, but we
    tend to develop, branch, and test those directories all in unison, so
    one big repository feels more natural.  We prefer to edit and commit
    on the symlinked paths, not the canonical ones, and in that setting,
    "git pull" fails to find the top-level directory of the repository
    while other commands work fine.
    
    "git pull" fails because POSIX shells have a notion of current working
    directory that is different from getcwd().  The shell stores this path
    in PWD.  As a result, "cd ../" can be interpreted differently in a
    shell script than chdir("../") in a C program.  The shell interprets
    "../" by essentially stripping the last textual path component from
    PWD, whereas C chdir() follows the ".." link in the current directory
    on the filesystem.  When PWD is a symlink, these are different
    destinations.  As a result, Git's C commands find the correct
    top-level working tree, and shell scripts do not.
    
    Changes:
    
    * When interpreting a relative upward (../) path in cd_to_toplevel,
      prepend the cwd without symlinks, given by /bin/pwd
    * Add tests for cd_to_toplevel and "git pull" in a symlinked
      directory that failed before this fix, plus contrasting scenarios
      that already worked
    
    Signed-off-by: Marcel M. Cary <marcel@oak.homeunix.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t2300-cd-to-toplevel.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+test_description='cd_to_toplevel'
+
+. ./test-lib.sh
+
+test_cd_to_toplevel () {
+	test_expect_success "$2" '
+		(
+			cd '"'$1'"' &&
+			. git-sh-setup &&
+			cd_to_toplevel &&
+			[ "$(/bin/pwd)" = "$TOPLEVEL" ]
+		)
+	'
+}
+
+TOPLEVEL="$(/bin/pwd)/repo"
+mkdir -p repo/sub/dir
+mv .git repo/
+SUBDIRECTORY_OK=1
+
+test_cd_to_toplevel repo 'at physical root'
+
+test_cd_to_toplevel repo/sub/dir 'at physical subdir'
+
+ln -s repo symrepo
+test_cd_to_toplevel symrepo 'at symbolic root'
+
+ln -s repo/sub/dir subdir-link
+test_cd_to_toplevel subdir-link 'at symbolic subdir'
+
+cd repo
+ln -s sub/dir internal-link
+test_cd_to_toplevel internal-link 'at internal symbolic subdir'
+
+test_done
