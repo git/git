c569b1fee151e467854ca268997f4f3baaf73564
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2008-12-26 10:46:25 +0100

    daemon: handle freopen() failure
    Die if stderr couldn't be sent to /dev/null when operating in inetd
    mode and report the error message from the OS.
    
    This fixes a compiler warning about the return value of freopen()
    being ignored on Ubuntu 8.10.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- daemon.c
+++ daemon.c
@@ -1118,7 +1118,9 @@ int main(int argc, char **argv)
 		struct sockaddr *peer = (struct sockaddr *)&ss;
 		socklen_t slen = sizeof(ss);
 
-		freopen("/dev/null", "w", stderr);
+		if (!freopen("/dev/null", "w", stderr))
+			die("failed to redirect stderr to /dev/null: %s",
+			    strerror(errno));
 
 		if (getpeername(0, peer, &slen))
 			peer = NULL;
