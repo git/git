806d5e90440046a7807f757d70acb29573f89524
Kirill Smelkov <kirr@mns.spb.ru>
2009-01-13 12:33:48 +0300

    mailinfo: add explicit test for mails like '<a.u.thor@example.com> (A U Thor)'
    Signed-off-by: Kirill Smelkov <kirr@mns.spb.ru>

--- t/t5100-mailinfo.sh
+++ t/t5100-mailinfo.sh
@@ -11,7 +11,7 @@ test_expect_success 'split sample box' \
 	'git mailsplit -o. ../t5100/sample.mbox >last &&
 	last=`cat last` &&
 	echo total is $last &&
-	test `cat last` = 12'
+	test `cat last` = 13'
 
 for mail in `echo 00*`
 do
