a094080e8eec0bffcd17da6804ee09bc5309da6c
Johannes Sixt <j6t@kdbg.org>
2009-01-22 11:30:16 +0100

    Do not issue the warning about the fallback of the PREFIX.
    It confuses gitk when it is browsing this repository on Windows.

--- exec_cmd.c
+++ exec_cmd.c
@@ -28,9 +28,10 @@ const char *system_path(const char *path)
 	    !(prefix = strip_path_suffix(argv0_path, BINDIR)) &&
 	    !(prefix = strip_path_suffix(argv0_path, "git"))) {
 		prefix = PREFIX;
-		fprintf(stderr, "RUNTIME_PREFIX requested, "
-				"but prefix computation failed.  "
-				"Using static fallback '%s'.\n", prefix);
+		/*
+		 * RUNTIME_PREFIX requested, but prefix computation failed.
+		 * Using static fallback.
+		 */
 	}
 #endif
 
