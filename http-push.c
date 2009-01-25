d456c9fd1e96c21ca32a224ed56ee34252605e34
Junio C Hamano <gitster@pobox.com>
2009-01-25 00:04:15 -0800

    http-push.c: style fixes
    b1c7d4a (http-push: refactor lock-related headers creation for curl
    requests, 2009-01-24) had many style violations that slipped through.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http-push.c
+++ http-push.c
@@ -184,21 +184,22 @@ enum dav_header_flag {
 	DAV_HEADER_TIMEOUT = (1u << 2)
 };
 
-static struct curl_slist *get_dav_token_headers(struct remote_lock *lock, enum dav_header_flag options) {
+static struct curl_slist *get_dav_token_headers(struct remote_lock *lock, enum dav_header_flag options)
+{
 	struct strbuf buf = STRBUF_INIT;
 	struct curl_slist *dav_headers = NULL;
 
-	if(options & DAV_HEADER_IF) {
+	if (options & DAV_HEADER_IF) {
 		strbuf_addf(&buf, "If: (<%s>)", lock->token);
 		dav_headers = curl_slist_append(dav_headers, buf.buf);
 		strbuf_reset(&buf);
 	}
-	if(options & DAV_HEADER_LOCK) {
+	if (options & DAV_HEADER_LOCK) {
 		strbuf_addf(&buf, "Lock-Token: <%s>", lock->token);
 		dav_headers = curl_slist_append(dav_headers, buf.buf);
 		strbuf_reset(&buf);
 	}
-	if(options & DAV_HEADER_TIMEOUT) {
+	if (options & DAV_HEADER_TIMEOUT) {
 		strbuf_addf(&buf, "Timeout: Second-%ld", lock->timeout);
 		dav_headers = curl_slist_append(dav_headers, buf.buf);
 		strbuf_reset(&buf);
