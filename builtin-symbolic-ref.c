e9cc02f0e41fd5d2f51e3c3f2b4f8cfa9e434432
Jeff King <peff@peff.net>
2009-02-13 13:26:09 -0500

    symbolic-ref: allow refs/<whatever> in HEAD
    Commit afe5d3d5 introduced a safety valve to symbolic-ref to
    disallow installing an invalid HEAD. It was accompanied by
    b229d18a, which changed validate_headref to require that
    HEAD contain a pointer to refs/heads/ instead of just refs/.
    Therefore, the safety valve also checked for refs/heads/.
    
    As it turns out, topgit is using refs/top-bases/ in HEAD,
    leading us to re-loosen (at least temporarily) the
    validate_headref check made in b229d18a. This patch does the
    corresponding loosening for the symbolic-ref safety valve,
    so that the two are in agreement once more.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-symbolic-ref.c
+++ builtin-symbolic-ref.c
@@ -45,8 +45,8 @@ int cmd_symbolic_ref(int argc, const char **argv, const char *prefix)
 		break;
 	case 2:
 		if (!strcmp(argv[0], "HEAD") &&
-		    prefixcmp(argv[1], "refs/heads/"))
-			die("Refusing to point HEAD outside of refs/heads/");
+		    prefixcmp(argv[1], "refs/"))
+			die("Refusing to point HEAD outside of refs/");
 		create_symref(argv[0], argv[1], msg);
 		break;
 	default:
