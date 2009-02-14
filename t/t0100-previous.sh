c9717ee97075bb333684fa2f63e9136c5f7d89bf
Junio C Hamano <gitster@pobox.com>
2009-02-13 23:26:12 -0800

    Teach @{-1} to git merge
    1.6.2 will have @{-1} syntax advertised as "usable anywhere you can use
    a branch name".  However, "git merge @{-1}" did not work.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0100-previous.sh
+++ t/t0100-previous.sh
@@ -22,5 +22,28 @@ test_expect_success 'branch -d @{-12} when there is not enough switches yet' '
 	git rev-parse --verify refs/heads/master
 '
 
+test_expect_success 'merge @{-1}' '
+	git checkout A &&
+	test_commit B &&
+	git checkout A &&
+	test_commit C &&
+	git branch -f master B &&
+	git branch -f other &&
+	git checkout other &&
+	git checkout master &&
+	git merge @{-1} &&
+	git cat-file commit HEAD | grep "Merge branch '\''other'\''"
+'
+
+test_expect_success 'merge @{-1} when there is not enough switches yet' '
+	git reflog expire --expire=now &&
+	git checkout -f master &&
+	git reset --hard B &&
+	git branch -f other C &&
+	git checkout other &&
+	git checkout master &&
+	test_must_fail git merge @{-12}
+'
+
 test_done
 
