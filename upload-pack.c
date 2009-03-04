05ac6b34e2c2adda5cb85dd0bdacb47fe5db953e
Jeff King <peff@peff.net>
2009-03-04 03:32:29 -0500

    improve missing repository error message
    Certain remote commands, when asked to do something in a
    particular directory that was not actually a git repository,
    would say "unable to chdir or not a git archive". The
    "chdir" bit is an unnecessary detail, and the term "git
    archive" is much less common these days than "git repository".
    
    So let's switch them all to:
    
      fatal: '%s' does not appear to be a git repository
    
    Signed-off-by: Jeff King <peff@peff.net>
    Acked-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- upload-pack.c
+++ upload-pack.c
@@ -645,7 +645,7 @@ int main(int argc, char **argv)
 	dir = argv[i];
 
 	if (!enter_repo(dir, strict))
-		die("'%s': unable to chdir or not a git archive", dir);
+		die("'%s' does not appear to be a git repository", dir);
 	if (is_repository_shallow())
 		die("attempt to fetch/clone from a shallow repository");
 	if (getenv("GIT_DEBUG_SEND_PACK"))
