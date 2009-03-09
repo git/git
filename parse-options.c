0d260f9a09a2febeb86fdada7224d271a76d2e3c
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2009-03-09 21:57:38 +0100

    parseopt: prevent KEEP_UNKNOWN and STOP_AT_NON_OPTION from being used together
    As suggested by Junio, disallow the flags PARSE_OPT_KEEP_UNKNOWN and
    PARSE_OPT_STOP_AT_NON_OPTION to be turned on at the same time, as a
    value of an unknown option could be mistakenly classified as a
    non-option, stopping the parser early.  E.g.:
    
    	git cmd --known --unknown value arg0 arg1
    
    The parser should have stopped at "arg0", but it already stops at
    "value".
    
    This patch makes parse_options() die if the two flags are used in
    combination.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.c
+++ parse-options.c
@@ -244,6 +244,9 @@ void parse_options_start(struct parse_opt_ctx_t *ctx,
 	ctx->out  = argv;
 	ctx->cpidx = ((flags & PARSE_OPT_KEEP_ARGV0) != 0);
 	ctx->flags = flags;
+	if ((flags & PARSE_OPT_KEEP_UNKNOWN) &&
+	    (flags & PARSE_OPT_STOP_AT_NON_OPTION))
+		die("STOP_AT_NON_OPTION and KEEP_UNKNOWN don't go together");
 }
 
 static int usage_with_options_internal(const char * const *,
