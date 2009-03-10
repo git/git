34df8abaf358c83cc1447d0a81bda7848685a1c9
Johannes Sixt <j6t@kdbg.org>
2009-03-10 22:54:17 +0100

    recv_sideband: Bands #2 and #3 always go to stderr
    This removes the last parameter of recv_sideband, by which the callers
    told which channel bands #2 and #3 should be written to.
    
    Sayeth Shawn Pearce:
    
       The definition of the streams in the current sideband protocol
       are rather well defined for the one protocol that uses it,
       fetch-pack/receive-pack:
    
         stream #1:  pack data
         stream #2:  stderr messages, progress, meant for tty
         stream #3:  abort message, remote is dead, goodbye!
    
    Since both callers of the function passed 2 for the parameter, we hereby
    remove it and send bands #2 and #3 to stderr explicitly using fprintf.
    
    This has the nice side-effect that these two streams pass through our
    ANSI emulation layer on Windows.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Acked-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-archive.c
+++ builtin-archive.c
@@ -52,7 +52,7 @@ static int run_remote_archiver(int argc, const char **argv,
 		die("git archive: expected a flush");
 
 	/* Now, start reading from fd[0] and spit it out to stdout */
-	rv = recv_sideband("archive", fd[0], 1, 2);
+	rv = recv_sideband("archive", fd[0], 1);
 	close(fd[0]);
 	close(fd[1]);
 	rv |= finish_connect(conn);
