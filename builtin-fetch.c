fa685bdf45cbaa997255cc78a23494b995e9769a
Daniel Barkalow <barkalow@iabervon.org>
2009-03-11 01:47:20 -0400

    Give error when no remote is configured
    When there's no explicitly-named remote, we use the remote specified
    for the current branch, which in turn defaults to "origin". But it
    this case should require the remote to actually be configured, and not
    fall back to the path "origin".
    
    Possibly, the config file's "remote = something" should require the
    something to be a configured remote instead of a bare repository URL,
    but we actually test with a bare repository URL.
    
    In fetch, we were giving the sensible error message when coming up
    with a URL failed, but this wasn't actually reachable, so move that
    error up and use it when appropriate.
    
    In push, we need a new error message, because the old one (formerly
    unreachable without a lot of help) used the repo name, which was NULL.
    
    Signed-off-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fetch.c
+++ builtin-fetch.c
@@ -636,6 +636,9 @@ int cmd_fetch(int argc, const char **argv, const char *prefix)
 	else
 		remote = remote_get(argv[0]);
 
+	if (!remote)
+		die("Where do you want to fetch from today?");
+
 	transport = transport_get(remote, remote->url[0]);
 	if (verbosity >= 2)
 		transport->verbose = 1;
@@ -647,9 +650,6 @@ int cmd_fetch(int argc, const char **argv, const char *prefix)
 		set_option(TRANS_OPT_KEEP, "yes");
 	if (depth)
 		set_option(TRANS_OPT_DEPTH, depth);
-
-	if (!transport->url)
-		die("Where do you want to fetch from today?");
 
 	if (argc > 1) {
 		int j = 0;
--- remote.c
+++ remote.c
@@ -38,6 +38,7 @@ static int branches_nr;
 
 static struct branch *current_branch;
 static const char *default_remote_name;
+static int explicit_default_remote_name;
 
 static struct rewrite **rewrite;
 static int rewrite_alloc;
@@ -102,6 +103,16 @@ static void add_url(struct remote *remote, const char *url)
 static void add_url_alias(struct remote *remote, const char *url)
 {
 	add_url(remote, alias_url(url));
+}
+
+static struct remote *get_remote_by_name(const char *name)
+{
+	int i;
+	for (i = 0; i < remotes_nr; i++) {
+		if (!strcmp(name, remotes[i]->name))
+			return remotes[i];
+	}
+	return NULL;
 }
 
 static struct remote *make_remote(const char *name, int len)
@@ -330,8 +341,10 @@ static int handle_config(const char *key, const char *value, void *cb)
 			if (!value)
 				return config_error_nonbool(key);
 			branch->remote_name = xstrdup(value);
-			if (branch == current_branch)
+			if (branch == current_branch) {
 				default_remote_name = branch->remote_name;
+				explicit_default_remote_name = 1;
+			}
 		} else if (!strcmp(subkey, ".merge")) {
 			if (!value)
 				return config_error_nonbool(key);
@@ -643,11 +656,22 @@ static int valid_remote_nick(const char *name)
 struct remote *remote_get(const char *name)
 {
 	struct remote *ret;
+	int name_given = 0;
 
 	read_config();
-	if (!name)
+	if (name)
+		name_given = 1;
+	else {
 		name = default_remote_name;
-	ret = make_remote(name, 0);
+		name_given = explicit_default_remote_name;
+	}
+	if (name_given)
+		ret = make_remote(name, 0);
+	else {
+		ret = get_remote_by_name(name);
+		if (!ret)
+			return NULL;
+	}
 	if (valid_remote_nick(name)) {
 		if (!ret->url)
 			read_remotes_file(ret);
