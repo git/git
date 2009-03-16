6e89ec0f1e716c11bf7fa214b4c7b975ea77c736
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2009-03-16 13:20:04 +1100

    grep: prefer builtin over external one when coloring results
    As far as I know, not all grep programs support coloring, so we should
    rely on builtin grep. If you want external grep, set
    color.grep.external to empty string.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-grep.c
+++ builtin-grep.c
@@ -825,6 +825,8 @@ int cmd_grep(int argc, const char **argv, const char *prefix)
 		}
 	}
 
+	if (opt.color && !opt.color_external)
+		builtin_grep = 1;
 	if (!opt.pattern_list)
 		die("no pattern given.");
 	if ((opt.regflags != REG_NEWLINE) && opt.fixed)
