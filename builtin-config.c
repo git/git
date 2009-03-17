bf71b4b3ee07291e97c4dabfb97e7397eec904e0
Carlos Rica <jasampler@gmail.com>
2009-03-17 10:46:37 +0100

    config: test for --replace-all with one argument and fix documentation.
    Option --replace-all only allows at least two arguments, so
    documentation was needing to be updated accordingly. A test showing
    that the command fails with only one parameter is also provided.
    
    Signed-off-by: Carlos Rica <jasampler@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-config.c
+++ builtin-config.c
@@ -55,7 +55,7 @@ static struct option builtin_config_options[] = {
 	OPT_BIT(0, "get", &actions, "get value: name [value-regex]", ACTION_GET),
 	OPT_BIT(0, "get-all", &actions, "get all values: key [value-regex]", ACTION_GET_ALL),
 	OPT_BIT(0, "get-regexp", &actions, "get values for regexp: name-regex [value-regex]", ACTION_GET_REGEXP),
-	OPT_BIT(0, "replace-all", &actions, "replace all matching variables: name [value [value_regex]", ACTION_REPLACE_ALL),
+	OPT_BIT(0, "replace-all", &actions, "replace all matching variables: name value [value_regex]", ACTION_REPLACE_ALL),
 	OPT_BIT(0, "add", &actions, "adds a new variable: name value", ACTION_ADD),
 	OPT_BIT(0, "unset", &actions, "removes a variable: name [value-regex]", ACTION_UNSET),
 	OPT_BIT(0, "unset-all", &actions, "removes all matches: name [value-regex]", ACTION_UNSET_ALL),
