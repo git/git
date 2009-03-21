03d3aada5a2a68a7acdb6286fd72155f01626e41
Junio C Hamano <gitster@pobox.com>
2009-03-21 13:23:27 -0700

    Fix branch -m @{-1} newname
    The command is supposed to rename the branch we were on before switched
    from to a new name, but was not aware of the short-hand notation we added
    recently.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-branch.c
+++ builtin-branch.c
@@ -468,18 +468,18 @@ static void rename_branch(const char *oldname, const char *newname, int force)
 	if (!oldname)
 		die("cannot rename the current branch while not on any.");
 
-	strbuf_addf(&oldref, "refs/heads/%s", oldname);
-
+	strbuf_branchname(&oldref, oldname);
+	strbuf_splice(&oldref, 0, 0, "refs/heads/", 11);
 	if (check_ref_format(oldref.buf))
-		die("Invalid branch name: %s", oldref.buf);
+		die("Invalid branch name: '%s'", oldname);
 
-	strbuf_addf(&newref, "refs/heads/%s", newname);
-
+	strbuf_branchname(&newref, newname);
+	strbuf_splice(&newref, 0, 0, "refs/heads/", 11);
 	if (check_ref_format(newref.buf))
-		die("Invalid branch name: %s", newref.buf);
+		die("Invalid branch name: '%s'", newname);
 
 	if (resolve_ref(newref.buf, sha1, 1, NULL) && !force)
-		die("A branch named '%s' already exists.", newname);
+		die("A branch named '%s' already exists.", newref.buf + 11);
 
 	strbuf_addf(&logmsg, "Branch: renamed %s to %s",
 		 oldref.buf, newref.buf);
