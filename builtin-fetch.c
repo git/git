26284f9356f4c4d8865db8bb024121311a1f90ff
Alex Riesen <raa.lkml@gmail.com>
2009-03-22 23:07:33 +0100

    Improve error message about fetch into current branch
    Otherwise, it is hard to guess why the fetch failed.
    Make sure we at least mention that the repository must be bare.
    Also the current branch is printed.
    
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fetch.c
+++ builtin-fetch.c
@@ -544,7 +544,8 @@ static void check_not_current_branch(struct ref *ref_map)
 	for (; ref_map; ref_map = ref_map->next)
 		if (ref_map->peer_ref && !strcmp(current_branch->refname,
 					ref_map->peer_ref->name))
-			die("Refusing to fetch into current branch");
+			die("Refusing to fetch into current branch %s "
+			    "of non-bare repository", current_branch->refname);
 }
 
 static int do_fetch(struct transport *transport,
