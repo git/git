aadd44404baab7b1f7234c8815bfd1e6c980cd99
Erik Faye-Lund <kusmabite@gmail.com>
2009-03-23 12:53:09 +0000

    builtin-fast-export.c: handle nested tags
    When tags that points to tags are passed to fast-export, an error is given,
    saying "Tag [TAGNAME] points nowhere?". This fix calls parse_object() on the
    object before referencing it's tag, to ensure the tag-info is fully initialized.
    In addition, it inserts a comment to point out where nested tags are handled.
    This is consistent with the comment for signed tags.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t9301-fast-export.sh
+++ t/t9301-fast-export.sh
@@ -271,7 +271,7 @@ test_expect_success 'set-up a few more tags for tag export tests' '
 # NEEDSWORK: not just check return status, but validate the output
 test_expect_success 'tree_tag'        'git fast-export tree_tag'
 test_expect_success 'tree_tag-obj'    'git fast-export tree_tag-obj'
-test_expect_failure 'tag-obj_tag'     'git fast-export tag-obj_tag'
-test_expect_failure 'tag-obj_tag-obj' 'git fast-export tag-obj_tag-obj'
+test_expect_success 'tag-obj_tag'     'git fast-export tag-obj_tag'
+test_expect_success 'tag-obj_tag-obj' 'git fast-export tag-obj_tag-obj'
 
 test_done
