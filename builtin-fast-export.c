1982467d9229e3c92157f2a41363365dcb866e86
Erik Faye-Lund <kusmabite@gmail.com>
2009-03-23 12:53:09 +0000

    builtin-fast-export.c: handle nested tags
    When tags that points to tags are passed to fast-export, an error is given,
    saying "Tag [TAGNAME] points nowhere?". This fix calls parse_object() on the
    object before referencing it's tag, to ensure the tag-info is fully initialized.
    In addition, it inserts a comment to point out where nested tags are handled.
    This is consistent with the comment for signed tags.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fast-export.c
+++ builtin-fast-export.c
@@ -362,7 +362,10 @@ static void get_tags_and_duplicates(struct object_array *pending,
 			break;
 		case OBJ_TAG:
 			tag = (struct tag *)e->item;
+
+			/* handle nested tags */
 			while (tag && tag->object.type == OBJ_TAG) {
+				parse_object(tag->object.sha1);
 				string_list_append(full_name, extra_refs)->util = tag;
 				tag = (struct tag *)tag->tagged;
 			}
@@ -375,7 +378,7 @@ static void get_tags_and_duplicates(struct object_array *pending,
 			case OBJ_BLOB:
 				handle_object(tag->object.sha1);
 				continue;
-			default:
+			default: /* OBJ_TAG (nested tags) is already handled */
 				warning("Tag points to object of unexpected type %s, skipping.",
 					typename(tag->object.type));
 				continue;
