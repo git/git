426193c025eb76125a99b3fe143eea50e849f9ca
Erik Faye-Lund <kusmabite@gmail.com>
2009-03-23 12:53:08 +0000

    builtin-fast-export.c: fix crash on tagged trees
    If a tag object points to a tree (or another unhandled type), the commit-
    pointer is left uninitialized and later dereferenced. This patch adds a
    default case to the switch that issues a warning and skips the object.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t9301-fast-export.sh
+++ t/t9301-fast-export.sh
@@ -269,9 +269,8 @@ test_expect_success 'set-up a few more tags for tag export tests' '
 '
 
 # NEEDSWORK: not just check return status, but validate the output
-# two tests commented out due to crash and thus unreliable return code
-test_expect_failure 'tree_tag'        'git fast-export tree_tag'
-test_expect_failure 'tree_tag-obj'    'git fast-export tree_tag-obj'
+test_expect_success 'tree_tag'        'git fast-export tree_tag'
+test_expect_success 'tree_tag-obj'    'git fast-export tree_tag-obj'
 test_expect_failure 'tag-obj_tag'     'git fast-export tag-obj_tag'
 test_expect_failure 'tag-obj_tag-obj' 'git fast-export tag-obj_tag-obj'
 
