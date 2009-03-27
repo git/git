6828f72ffee0d603b96bcde080ec13504f617669
Brandon Casey <casey@nrlssc.navy.mil>
2009-03-26 21:37:53 -0500

    builtin-send-pack.c: avoid empty structure initialization
    The IRIX6.5 MIPSpro Compiler doesn't like it.
    
    Signed-off-by: Brandon Casey <casey@nrlssc.navy.mil>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-send-pack.c
+++ builtin-send-pack.c
@@ -10,8 +10,7 @@ static const char send_pack_usage[] =
 "git send-pack [--all | --mirror] [--dry-run] [--force] [--receive-pack=<git-receive-pack>] [--verbose] [--thin] [<host>:]<directory> [<ref>...]\n"
 "  --all and explicit <ref> specification are mutually exclusive.";
 
-static struct send_pack_args args = {
-};
+static struct send_pack_args args;
 
 static int feed_object(const unsigned char *sha1, int fd, int negative)
 {
