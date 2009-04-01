50b5f420fe94399e3da867a0ff0585bc5d3fe86f
Ali Gholami Rudi <ali@rudi.ir>
2009-04-01 15:52:25 +0430

    builtin-clone.c: no need to strdup for setenv
    The setenv function makes a copy, itself.
    
    Signed-off-by: Ali Gholami Rudi <ali@rudi.ir>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-clone.c
+++ builtin-clone.c
@@ -406,7 +406,7 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 	atexit(remove_junk);
 	sigchain_push_common(remove_junk_on_signal);
 
-	setenv(CONFIG_ENVIRONMENT, xstrdup(mkpath("%s/config", git_dir)), 1);
+	setenv(CONFIG_ENVIRONMENT, mkpath("%s/config", git_dir), 1);
 
 	if (safe_create_leading_directories_const(git_dir) < 0)
 		die("could not create leading directories of '%s'", git_dir);
