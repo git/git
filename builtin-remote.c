b344e1614b15dfde0ab4dfc175bed1aac39bc264
Finn Arne Gangstad <finnag@pvv.org>
2009-04-06 15:41:02 +0200

    git remote update: Fallback to remote if group does not exist
    Previously, git remote update <remote> would fail unless there was
    a remote group configured with the same name as the remote.
    git remote update will now fall back to using the remote if no matching
    group can be found.
    
    This enables "git remote update -p <remote>..." to fetch and prune one
    or more remotes, for example.
    
    Signed-off-by: Finn Arne Gangstad <finnag@pvv.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-remote.c
+++ builtin-remote.c
@@ -1232,8 +1232,14 @@ static int update(int argc, const char **argv)
 		int groups_found = 0;
 		remote_group.name = argv[i];
 		result = git_config(get_remote_group, &groups_found);
-		if (!groups_found && (i != 1 || strcmp(argv[1], "default")))
-			die("No such remote group: '%s'", argv[i]);
+		if (!groups_found && (i != 1 || strcmp(argv[1], "default"))) {
+			struct remote *remote;
+			if (!remote_is_configured(argv[i]))
+				die("No such remote or remote group: %s",
+				    argv[i]);
+			remote = remote_get(argv[i]);
+			string_list_append(remote->name, remote_group.list);
+		}
 	}
 
 	if (!result && !list.nr  && argc == 2 && !strcmp(argv[1], "default"))
