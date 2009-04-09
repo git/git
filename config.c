8f8c6fafd92fd547547bd7735e2d121a20997703
Linus Torvalds <torvalds@linux-foundation.org>
2009-04-09 12:40:39 -0700

    Allow users to un-configure rename detection
    On Thu, 9 Apr 2009, Linus Torvalds wrote:
    >
    > 	[diff]
    > 		renames = no
    
    Btw, while doing this, I also though that "renames = on/off" made more
    sense, but while we allow yes/no and true/false for booleans, we don't
    allow on/off.
    
    Should we? Maybe. Here's a stupid patch.
    
    		Linus
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -331,9 +331,9 @@ int git_config_bool_or_int(const char *name, const char *value, int *is_bool)
 		return 1;
 	if (!*value)
 		return 0;
-	if (!strcasecmp(value, "true") || !strcasecmp(value, "yes"))
+	if (!strcasecmp(value, "true") || !strcasecmp(value, "yes") || !strcasecmp(value, "on"))
 		return 1;
-	if (!strcasecmp(value, "false") || !strcasecmp(value, "no"))
+	if (!strcasecmp(value, "false") || !strcasecmp(value, "no") || !strcasecmp(value, "off"))
 		return 0;
 	*is_bool = 0;
 	return git_config_int(name, value);
