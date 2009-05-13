95405ba6cf7adeaa4a066e8a3a1b76b73f7b9341
Alex Riesen <raa.lkml@gmail.com>
2009-05-13 20:08:53 +0200

    Quote LF in urls git fetch saves in FETCH_HEAD
    The fmt-merge-msg does a strong syntax checking of its input and fails
    with if it is incorrect. The LF character is the only character
    important for fmt-merge-msg. As the url in FETCH_HEAD plays only
    informational role, a quoted representation of the url should be good
    and true enough.
    The url often comes from either user-editable config or command line,
    so it is reasonable to expect all kinds of characters in it, including
    the characters which the format of FETCH_HEAD considers special (line
    separator in this case).
    
    Noticed and reported by Hugo Mildenberger.
    
    Signed-off-by: Alex Riesen <raa.lkml@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fetch.c
+++ builtin-fetch.c
@@ -353,12 +353,18 @@ static int store_updated_refs(const char *url, const char *remote_name,
 						    kind);
 			note_len += sprintf(note + note_len, "'%s' of ", what);
 		}
-		note_len += sprintf(note + note_len, "%.*s", url_len, url);
-		fprintf(fp, "%s\t%s\t%s\n",
+		note[note_len] = '\0';
+		fprintf(fp, "%s\t%s\t%s",
 			sha1_to_hex(commit ? commit->object.sha1 :
 				    rm->old_sha1),
 			rm->merge ? "" : "not-for-merge",
 			note);
+		for (i = 0; i < url_len; ++i)
+			if ('\n' == url[i])
+				fputs("\\n", fp);
+			else
+				fputc(url[i], fp);
+		fputc('\n', fp);
 
 		if (ref)
 			rc |= update_local_ref(ref, what, note);
