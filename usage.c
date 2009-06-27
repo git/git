b875036e5a2ab569a2123abe9ebfe25258227951
Thomas Rast <trast@student.ethz.ch>
2009-06-27 17:58:44 +0200

    Introduce die_errno() that appends strerror(errno) to die()
    There are many calls to die() that do, or should, report
    strerror(errno) to indicate how the syscall they guard failed.
    Introduce a small helper function for this case.
    
    Note:
    
    - POSIX says vsnprintf can modify errno in some unlikely cases, so we
      have to use errno early.
    
    - We take some care to pass the original format to die_routine(), in
      case someone wants to call die_errno() with custom format
      characters.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- usage.c
+++ usage.c
@@ -60,6 +60,18 @@ void die(const char *err, ...)
 	va_end(params);
 }
 
+void die_errno(const char *fmt, ...)
+{
+	va_list params;
+	char fmt_with_err[1024];
+
+	snprintf(fmt_with_err, sizeof(fmt_with_err), "%s: %s", fmt, strerror(errno));
+
+	va_start(params, fmt);
+	die_routine(fmt_with_err, params);
+	va_end(params);
+}
+
 int error(const char *err, ...)
 {
 	va_list params;
