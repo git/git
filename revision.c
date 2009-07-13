f222abdeec7838891e79abd152c6cb67e532b68d
Linus Torvalds <torvalds@linux-foundation.org>
2009-07-13 14:41:12 -0700

    Make 'git show' more useful
    For some reason, I ended up doing
    
    	git show HEAD~5..
    
    as an odd way of asking for a log. I realize I should just have used "git
    log", but at the same time it does make perfect conceptual sense. After
    all, you _could_ have done
    
    	git show HEAD HEAD~1 HEAD~2 HEAD~3 HEAD~4
    
    and saying "git show HEAD~5.." is pretty natural. It's not like "git show"
    only ever showed a single commit (or other object) before either! So
    conceptually, giving a commit range is a very sensible operation, even
    though you'd traditionally have used "git log" for that.
    
    However, doing that currently results in an error
    
    	fatal: object ranges do not make sense when not walking revisions
    
    which admittedly _also_ makes perfect sense - from an internal git
    implementation standpoint in 'revision.c'.
    
    However, I think that asking to show a range makes sense to a user, while
    saying "object ranges no not make sense when not walking revisions" only
    makes sense to a git developer.
    
    So on the whole, of the two different "makes perfect sense" behaviors, I
    think I originally picked the wrong one. And quite frankly, I don't really
    see anybody actually _depending_ on that error case. So why not change it?
    
    So rather than error out, just turn that non-walking error case into a
    "silently turn on walking" instead.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -133,7 +133,7 @@ void mark_parents_uninteresting(struct commit *commit)
 static void add_pending_object_with_mode(struct rev_info *revs, struct object *obj, const char *name, unsigned mode)
 {
 	if (revs->no_walk && (obj->flags & UNINTERESTING))
-		die("object ranges do not make sense when not walking revisions");
+		revs->no_walk = 0;
 	if (revs->reflog_info && obj->type == OBJ_COMMIT &&
 			add_reflog_for_walk(revs->reflog_info,
 				(struct commit *)obj, name))
