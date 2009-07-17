ca4ca9ed069c8ce13e1a38e79fbaea849754de94
Jeff King <peff@peff.net>
2009-07-17 19:18:34 -0400

    show: suppress extra newline when showing annotated tag
    When showing a tag, our header parsing finishes with the
    offset pointing to the newline separating the tag header
    from the tag body. This means that the printed body will
    always start with a newline.
    
    However, we also add an extra newline when printing the
    tagger information. This leads to an ugly double-newline:
    
        $ git show v1.6.3
        tag v1.6.3
        Tagger: Junio C Hamano <gitster@pobox.com>
        Date:   Wed May 6 18:16:47 2009 -0700
    
        GIT 1.6.3
        -----BEGIN PGP SIGNATURE-----
        ...
    
    This patch removes the extra newline from the end of the
    tagger headers. This is a better solution than suppressing
    the separator newline, because it retains the behavior for
    tags which have no tagger. E.g., "git show v0.99" will
    continue to look like:
    
          $ git show v0.99
          tag v0.99
    
          Test-release for wider distribution.
          ...
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-log.c
+++ builtin-log.c
@@ -257,7 +257,7 @@ static void show_tagger(char *buf, int len, struct rev_info *rev)
 	pp_user_info("Tagger", rev->commit_format, &out, buf, rev->date_mode,
 		git_log_output_encoding ?
 		git_log_output_encoding: git_commit_encoding);
-	printf("%s\n", out.buf);
+	printf("%s", out.buf);
 	strbuf_release(&out);
 }
 
