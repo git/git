57f6ec029090f64377ec5c0926b6e2e39b0caa4f
Ori Avtalion <ori@avtalion.name>
2009-08-07 17:24:21 +0300

    Change mentions of "git programs" to "git commands"
    Most of the docs and printouts refer to "commands" when discussing what
    the end users call via the "git" top-level program. We should refer them
    as "git programs" when we discuss the fact that the commands are
    implemented as separate programs, but in other contexts, it is better to
    use the term "git commands" consistently.
    
    Signed-off-by: Ori Avtalion <ori@avtalion.name>
    Signed-off-by: Nanako Shiraishi <nanako3@lavabit.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -334,7 +334,7 @@ const char *help_unknown_cmd(const char *cmd)
 		const char *assumed = main_cmds.names[0]->name;
 		main_cmds.names[0] = NULL;
 		clean_cmdnames(&main_cmds);
-		fprintf(stderr, "WARNING: You called a Git program named '%s', "
+		fprintf(stderr, "WARNING: You called a Git command named '%s', "
 			"which does not exist.\n"
 			"Continuing under the assumption that you meant '%s'\n",
 			cmd, assumed);
