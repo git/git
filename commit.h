46b5139cae7306194a39fdaf5c6abc12ab531c84
Thomas Rast <trast@student.ethz.ch>
2009-08-13 14:29:41 +0200

    builtin-add: refactor the meat of interactive_add()
    This moves the call setup for 'git add--interactive' to a separate
    function, as other users will call it without running
    validate_pathspec() first.
    
    Signed-off-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- commit.h
+++ commit.h
@@ -137,6 +137,8 @@ int is_descendant_of(struct commit *, struct commit_list *);
 int in_merge_bases(struct commit *, struct commit **, int);
 
 extern int interactive_add(int argc, const char **argv, const char *prefix);
+extern int run_add_interactive(const char *revision, const char *patch_mode,
+			       const char **pathspec);
 
 static inline int single_parent(struct commit *commit)
 {
