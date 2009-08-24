86386829d425a3d3ae6ce713c58328607e50e523
Nicolas Pitre <nico@cam.org>
2009-08-24 00:04:09 -0400

    fix simple deepening of a repo
    If all refs sent by the remote repo during a fetch are reachable
    locally, then no further conversation is performed with the remote. This
    check is skipped when the --depth argument is provided to allow the
    deepening of a shallow clone which corresponding remote repo has no
    changed.
    
    However, some additional filtering was added in commit c29727d5 to
    remove those refs which are equal on both sides.  If the remote repo has
    not changed, then the list of refs to give the remote process becomes
    empty and simply attempting to deepen a shallow repo always fails.
    
    Let's stop being smart in that case and simply send the whole list over
    when that condition is met.  The remote will do the right thing anyways.
    
    Test cases for this issue are also provided.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport.c
+++ transport.c
@@ -1049,16 +1049,30 @@ const struct ref *transport_get_remote_refs(struct transport *transport)
 int transport_fetch_refs(struct transport *transport, const struct ref *refs)
 {
 	int rc;
-	int nr_heads = 0, nr_alloc = 0;
+	int nr_heads = 0, nr_alloc = 0, nr_refs = 0;
 	const struct ref **heads = NULL;
 	const struct ref *rm;
 
 	for (rm = refs; rm; rm = rm->next) {
+		nr_refs++;
 		if (rm->peer_ref &&
 		    !hashcmp(rm->peer_ref->old_sha1, rm->old_sha1))
 			continue;
 		ALLOC_GROW(heads, nr_heads + 1, nr_alloc);
 		heads[nr_heads++] = rm;
+	}
+
+	if (!nr_heads) {
+		/*
+		 * When deepening of a shallow repository is requested,
+		 * then local and remote refs are likely to still be equal.
+		 * Just feed them all to the fetch method in that case.
+		 * This condition shouldn't be met in a non-deepening fetch
+		 * (see builtin-fetch.c:quickfetch()).
+		 */
+		heads = xmalloc(nr_refs * sizeof(*heads));
+		for (rm = refs; rm; rm = rm->next)
+			heads[nr_heads++] = rm;
 	}
 
 	rc = transport->fetch(transport, nr_heads, heads);
