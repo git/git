1be224ba6e99f0ab34c998d7fa8023b76a15c8b6
Junio C Hamano <gitster@pobox.com>
2009-09-28 23:40:08 -0700

    builtin-mailinfo.c: check error status from rewind and ftruncate
    A recent "cut at scissors" implementation rewinds and truncates
    the output file to store the message when it sees a scissors mark,
    but it did not check if these library calls succeeded.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    [sp: Use fseek as rewind returns void]
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- builtin-mailinfo.c
+++ builtin-mailinfo.c
@@ -785,8 +785,10 @@ static int handle_commit_msg(struct strbuf *line)
 
 	if (use_scissors && is_scissors_line(line)) {
 		int i;
-		rewind(cmitmsg);
-		ftruncate(fileno(cmitmsg), 0);
+		if (fseek(cmitmsg, 0L, SEEK_SET))
+			die_errno("Could not rewind output message file");
+		if (ftruncate(fileno(cmitmsg), 0))
+			die_errno("Could not truncate output message file at scissors");
 		still_looking = 1;
 
 		/*
