c6dfb399448f6de17ce417052f1bb345c9e022c9
Johannes Sixt <j6t@kdbg.org>
2009-10-13 12:53:28 +0200

    remote-curl: add missing initialization of argv0_path
    All programs, in particular also the stand-alone programs (non-builtins)
    must call git_extract_argv0_path(argv[0]) in order to help builds that
    derive the installation prefix at runtime, such as the MinGW build.
    Without this call, the program segfaults (or raises an assertion
    failure).
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Tested-by: Michael Wookey <michaelwookey@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote-curl.c
+++ remote-curl.c
@@ -82,6 +82,7 @@ int main(int argc, const char **argv)
 	const char *url;
 	struct walker *walker = NULL;
 
+	git_extract_argv0_path(argv[0]);
 	setup_git_directory();
 	if (argc < 2) {
 		fprintf(stderr, "Remote needed\n");
