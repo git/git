752c0c24926aacbceca0d27de6ad22cbb7dd0709
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-10-19 14:38:32 +0200

    Add the --submodule option to the diff option family
    When you use the option --submodule=log you can see the submodule
    summaries inlined in the diff, instead of not-quite-helpful SHA-1 pairs.
    
    The format imitates what "git submodule summary" shows.
    
    To do that, <path>/.git/objects/ is added to the alternate object
    databases (if that directory exists).
    
    This option was requested by Jens Lehmann at the GitTogether in Berlin.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ submodule.h
@@ -0,0 +1,8 @@
+#ifndef SUBMODULE_H
+#define SUBMODULE_H
+
+void show_submodule_summary(FILE *f, const char *path,
+		unsigned char one[20], unsigned char two[20],
+		const char *del, const char *add, const char *reset);
+
+#endif
