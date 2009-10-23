024ab976fffdc1d78b6d0b15e261b52f3a5d594b
Junio C Hamano <gitster@pobox.com>
2009-10-23 11:42:39 -0700

    Do not fail "describe --always" in a tag-less repository
    This fixes a regression introduce by d68dc34 (git-describe: Die early if
    there are no possible descriptions, 2009-08-06).
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-describe.c
+++ builtin-describe.c
@@ -197,7 +197,7 @@ static void describe(const char *arg, int last_one)
 		for_each_ref(get_name, NULL);
 	}
 
-	if (!found_names)
+	if (!found_names && !always)
 		die("cannot describe '%s'", sha1_to_hex(sha1));
 
 	n = cmit->util;
