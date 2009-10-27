a29aa47da79cfd0ef7ee0ef423e7e5a9a3cf07bd
Gerrit Pape <pape@smarden.org>
2009-10-27 13:31:33 +0000

    help -i: properly error out if no info viewer can be found
    With this commit, git help -i <cmd> prints an error message and exits
    non-zero instead of being silent and exit code 0.
    
    Reported by Trent W. Buck through
     http://bugs.debian.org/537664
    
    Signed-off-by: Gerrit Pape <pape@smarden.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-help.c
+++ builtin-help.c
@@ -372,6 +372,7 @@ static void show_info_page(const char *git_cmd)
 	const char *page = cmd_to_page(git_cmd);
 	setenv("INFOPATH", system_path(GIT_INFO_PATH), 1);
 	execlp("info", "info", "gitman", page, NULL);
+	die("no info viewer handled the request");
 }
 
 static void get_html_page_path(struct strbuf *page_path, const char *page)
