1e380ddcd29f1e25bfb58b7068fdafb038c8dd9a
Vietor Liu <vietor@vxwo.org>
2009-10-31 14:36:03 +0800

    imap-send.c: fix compiler warnings for OpenSSL 1.0
    The openssl/CHANGES file says:
    
        Let the TLSv1_method() etc. functions return a 'const' SSL_METHOD
        pointer and make the SSL_METHOD parameter in SSL_CTX_new,
        SSL_CTX_set_ssl_version and SSL_set_ssl_method 'const'.
    
    In older versions, unqualified pointers were used, so we unfortunately
    cannot unconditionally update the type of the variable we use.
    
    Signed-off-by: Vietor Liu <vietor@vxwo.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- imap-send.c
+++ imap-send.c
@@ -273,7 +273,11 @@ static int ssl_socket_connect(struct imap_socket *sock, int use_tls_only, int ve
 	fprintf(stderr, "SSL requested but SSL support not compiled in\n");
 	return -1;
 #else
+#if (OPENSSL_VERSION_NUMBER >= 0x10000000L)
+	const SSL_METHOD *meth;
+#else
 	SSL_METHOD *meth;
+#endif
 	SSL_CTX *ctx;
 	int ret;
 
