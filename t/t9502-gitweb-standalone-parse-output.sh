b629275fd02aa07c2630d1a8c8a14011ff164043
Mark Rada <marada@uwaterloo.ca>
2009-11-07 16:13:29 +0100

    gitweb: Smarter snapshot names
    Teach gitweb how to produce nicer snapshot names by only using the
    short hash id.  If clients make requests using a tree-ish that is not
    a partial or full SHA-1 hash, then the short hash will also be appended
    to whatever they asked for.  If clients request snapshot of a tag
    (which means that $hash ('h') parameter has 'refs/tags/' prefix),
    use only tag name.
    
    Update tests cases in t9502-gitweb-standalone-parse-output.
    
    Gitweb uses the following format for snapshot filenames:
      <sanitized project name>-<version info>.<snapshot suffix>
    where <sanitized project name> is project name with '.git' or '/.git'
    suffix stripped, unless '.git' is the whole project name.  For
    snapshot prefix it uses:
      <sanitized project name>-<version info>/
    as compared to <sanitized project name>/ before (without version info).
    
    Current rules for <version info>:
    * if 'h' / $hash parameter is SHA-1 or shortened SHA-1, use SHA-1
      shortened to to 7 characters
    * otherwise if 'h' / $hash parameter is tag name (it begins with
      'refs/tags/' prefix, use tag name (with 'refs/tags/' stripped
    * otherwise if 'h' / $hash parameter starts with 'refs/heads/' prefix,
      strip this prefix, convert '/' into '.', and append shortened SHA-1
      after '-', i.e. use <sanitized hash>-<shortened sha1>
    
    Signed-off-by: Mark Rada <marada@uwaterloo.ca>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Jakub Narebski <jnareb@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t9502-gitweb-standalone-parse-output.sh
+++ t/t9502-gitweb-standalone-parse-output.sh
@@ -56,29 +56,57 @@ test_debug '
 
 test_expect_success 'snapshot: full sha1' '
 	gitweb_run "p=.git;a=snapshot;h=$FULL_ID;sf=tar" &&
-	check_snapshot ".git-$FULL_ID" ".git"
+	check_snapshot ".git-$SHORT_ID"
 '
 test_debug 'cat gitweb.headers && cat file_list'
 
 test_expect_success 'snapshot: shortened sha1' '
 	gitweb_run "p=.git;a=snapshot;h=$SHORT_ID;sf=tar" &&
-	check_snapshot ".git-$SHORT_ID" ".git"
+	check_snapshot ".git-$SHORT_ID"
+'
+test_debug 'cat gitweb.headers && cat file_list'
+
+test_expect_success 'snapshot: almost full sha1' '
+	ID=$(git rev-parse --short=30 HEAD) &&
+	gitweb_run "p=.git;a=snapshot;h=$ID;sf=tar" &&
+	check_snapshot ".git-$SHORT_ID"
 '
 test_debug 'cat gitweb.headers && cat file_list'
 
 test_expect_success 'snapshot: HEAD' '
 	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tar" &&
-	check_snapshot ".git-HEAD" ".git"
+	check_snapshot ".git-HEAD-$SHORT_ID"
 '
 test_debug 'cat gitweb.headers && cat file_list'
 
 test_expect_success 'snapshot: short branch name (master)' '
 	gitweb_run "p=.git;a=snapshot;h=master;sf=tar" &&
-	check_snapshot ".git-master" ".git"
+	ID=$(git rev-parse --verify --short=7 master) &&
+	check_snapshot ".git-master-$ID"
 '
 test_debug 'cat gitweb.headers && cat file_list'
 
-test_expect_failure 'snapshot: hierarchical branch name (xx/test)' '
+test_expect_success 'snapshot: short tag name (first)' '
+	gitweb_run "p=.git;a=snapshot;h=first;sf=tar" &&
+	ID=$(git rev-parse --verify --short=7 first) &&
+	check_snapshot ".git-first-$ID"
+'
+test_debug 'cat gitweb.headers && cat file_list'
+
+test_expect_success 'snapshot: full branch name (refs/heads/master)' '
+	gitweb_run "p=.git;a=snapshot;h=refs/heads/master;sf=tar" &&
+	ID=$(git rev-parse --verify --short=7 master) &&
+	check_snapshot ".git-master-$ID"
+'
+test_debug 'cat gitweb.headers && cat file_list'
+
+test_expect_success 'snapshot: full tag name (refs/tags/first)' '
+	gitweb_run "p=.git;a=snapshot;h=refs/tags/first;sf=tar" &&
+	check_snapshot ".git-first"
+'
+test_debug 'cat gitweb.headers && cat file_list'
+
+test_expect_success 'snapshot: hierarchical branch name (xx/test)' '
 	gitweb_run "p=.git;a=snapshot;h=xx/test;sf=tar" &&
 	! grep "filename=.*/" gitweb.headers
 '
