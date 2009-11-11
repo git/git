c27b39252ff713db414b8dbbfb263d0468031fab
Jonathan Nieder <jrnieder@gmail.com>
2009-11-11 17:57:36 -0600

    Suppress warnings from "git var -l"
    For scripts using "git var -l" to read all logical variables at
    once, not all per-variable warnings will be relevant.  So suppress
    them.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- var.c
+++ var.c
@@ -22,7 +22,7 @@ static void list_vars(void)
 {
 	struct git_var *ptr;
 	for (ptr = git_vars; ptr->read; ptr++)
-		printf("%s=%s\n", ptr->name, ptr->read(IDENT_WARN_ON_NO_NAME));
+		printf("%s=%s\n", ptr->name, ptr->read(0));
 }
 
 static const char *read_var(const char *var)
