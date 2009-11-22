625a860cb73b259683a4285705ac17e92298d5d5
Björn Gustavsson <bgustavsson@gmail.com>
2009-11-22 22:19:53 +0100

    Fix truncated usage messages
    The usage messages for some commands (such as 'git diff-tree')
    are truncated because they don't fit in a fixed buffer of
    1024 bytes.
    
    It would be tempting to eliminate the buffer and the problem once
    and for all by doing the output in three steps, but doing so could
    (according to commit d048a96e) increase the likelyhood of messing
    up the display.
    
    So we just increase the size of the buffer.
    
    Signed-off-by: Björn Gustavsson <bgustavsson@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- usage.c
+++ usage.c
@@ -7,7 +7,7 @@
 
 static void report(const char *prefix, const char *err, va_list params)
 {
-	char msg[1024];
+	char msg[4096];
 	vsnprintf(msg, sizeof(msg), err, params);
 	fprintf(stderr, "%s%s\n", prefix, msg);
 }
