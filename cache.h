182852417371b8e7cbdabf53c164354e84fcecc0
Erik Faye-Lund <kusmabite@googlemail.com>
2009-12-16 22:20:55 +0100

    core.hidedotfiles: hide '.git' dir by default
    At least for cross-platform projects, it makes sense to hide the
    files starting with a dot, as this is the behavior on Unix/MacOSX.
    
    However, at least Eclipse has problems interpreting the hidden flag
    correctly, so the default is to hide only the .git/ directory.
    
    The config setting core.hideDotFiles therefore supports not only
    'true' and 'false', but also 'dotGitOnly'.
    
    [jes: clarified the commit message, made git init respect the setting
    by marking the .git/ directory only after reading the config, and added
    documentation, and rebased on top of current junio/next]
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- cache.h
+++ cache.h
@@ -556,6 +556,13 @@ extern int core_preload_index;
 extern int core_apply_sparse_checkout;
 extern int precomposed_unicode;
 
+enum hide_dotfiles_type {
+	HIDE_DOTFILES_FALSE = 0,
+	HIDE_DOTFILES_TRUE,
+	HIDE_DOTFILES_DOTGITONLY,
+};
+extern enum hide_dotfiles_type hide_dotfiles;
+
 enum branch_track {
 	BRANCH_TRACK_UNSPECIFIED = -1,
 	BRANCH_TRACK_NEVER = 0,
