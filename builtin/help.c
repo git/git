22a5289ef16aaefe66ed62d25b2b33f465ce9d78
Frank Li <lznuaa@gmail.com>
2009-12-21 13:13:44 +0800

    Build Git Okay after rebase
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- builtin/help.c
+++ builtin/help.c
@@ -403,26 +403,6 @@ static void open_html(const char *path)
 }
 #endif
 
-static void show_chm_page(const char *git_cmd)
-{
-	//const char *page = cmd_to_page(git_cmd);
-	struct strbuf page_path; /* it leaks but we exec bellow */
-
-	struct stat st;
-	const char *git_path = system_path(GIT_EXEC_PATH);
-	int i;
-
-	/* Check that we have a git documentation directory. */
-	if (stat(mkpath("%s/TortoiseGit_en.chm", git_path), &st)
-	    || !S_ISREG(st.st_mode))
-		die("'%s': not a documentation directory.", git_path);
-
-	strbuf_init(&page_path, 0);
-	strbuf_addf(&page_path, "%s/TortoiseGit_en.chm::/git-%s(1).html", git_path, git_cmd);
-	
-	ShellExecute(NULL, "open","hh.exe", page_path.buf,NULL, SW_SHOW);
-}
-
 static void show_html_page(const char *git_cmd)
 {
 	const char *page = cmd_to_page(git_cmd);
--- compat/msvc.h
+++ compat/msvc.h
@@ -39,4 +39,65 @@ int mingw_fstat(int fd, struct stat *buf);
 
 #undef ALREADY_DECLARED_STAT_FUNCS
 
+/*
+   Even though _stati64 is normally just defined at _stat64
+   on Windows, we specify it here as a proper struct to avoid
+   compiler warnings about macro redefinition due to magic in
+   mingw.h. Struct taken from ReactOS (GNU GPL license).
+*/
+struct _stati64 {
+	_dev_t  st_dev;
+	_ino_t  st_ino;
+	unsigned short st_mode;
+	short   st_nlink;
+	short   st_uid;
+	short   st_gid;
+	_dev_t  st_rdev;
+	__int64 st_size;
+	time_t  st_atime;
+	time_t  st_mtime;
+	time_t  st_ctime;
+};
+
+#define NO_PREAD
+#define NO_OPENSSL
+#define	NO_LIBGEN_H
+#define	NO_SYMLINK_HEAD
+#define	NO_IPV6
+#define	NO_SETENV
+#define	NO_UNSETENV
+#define	NO_STRCASESTR
+#define	NO_STRLCPY
+#define	NO_MEMMEM
+#define	NO_ICONV
+#define	NO_C99_FORMAT
+#define	NO_STRTOUMAX
+#define	NO_STRTOULL
+#define	NO_MKDTEMP
+#define	NO_MKSTEMPS
+#define	SNPRINTF_RETURNS_BOGUS
+#define	NO_SVN_TESTS
+#define	NO_PERL_MAKEMAKER 
+#define	RUNTIME_PREFIX
+#define	NO_POSIX_ONLY_PROGRAMS
+#define	NO_ST_BLOCKS_IN_STRUCT_STAT
+#define	NO_NSEC 
+#define	USE_WIN32_MMAP
+#define	UNRELIABLE_FSTAT 
+#define	NO_REGEX
+#define	NO_CURL
+#define	NO_PTHREADS 
+
+/*Git runtime infomation*/
+#define ETC_GITCONFIG "%HOME%"
+#define SHA1_HEADER "block-sha1\\sha1.h"
+#define GIT_EXEC_PATH "bin"
+#define GIT_VERSION "1.7.2.3"
+#define BINDIR "bin"
+#define PREFIX "."
+#define GIT_MAN_PATH "man"
+#define GIT_INFO_PATH "info"
+#define GIT_HTML_PATH "html"
+#define DEFAULT_GIT_TEMPLATE_DIR "templates"
+#define GIT_USER_AGENT "git/1.7.2.3"
 #endif
