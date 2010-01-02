fd0a8c2e6428acb883bf4707de54b3e026c57455
Tarmigan Casebolt <tarmigan+git@gmail.com>
2010-01-02 13:43:59 -0800

    Smart-http tests: Test http-backend without curl or a webserver
    This reuses many of the tests from the old t5560 but runs those tests
    without curl or a webserver.  This will hopefully increase the testing
    coverage for http-backend because it does not require users to set
    GIT_TEST_HTTPD.
    
    Signed-off-by: Tarmigan Casebolt <tarmigan+git@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5560-http-backend-noserver.sh
+++ t/t5560-http-backend-noserver.sh
@@ -6,18 +6,34 @@ test_description='test git-http-backend-noserver'
 HTTPD_DOCUMENT_ROOT_PATH="$TRASH_DIRECTORY"
 
 run_backend() {
-	REQUEST_METHOD=GET \
+	echo "$2" |
+	QUERY_STRING="${1#*\?}" \
 	GIT_PROJECT_ROOT="$HTTPD_DOCUMENT_ROOT_PATH" \
-	PATH_INFO="$1" \
+	PATH_INFO="${1%%\?*}" \
 	git http-backend >act.out 2>act.err
 }
 
 GET() {
-	return 0
+	REQUEST_METHOD="GET" \
+	run_backend "/repo.git/$1" &&
+	if ! grep "Status" act.out >act
+	then
+		printf "Status: 200 OK\r\n" >act
+	fi
+	printf "Status: $2\r\n" >exp &&
+	test_cmp exp act
 }
 
 POST() {
-	return 0
+	REQUEST_METHOD="POST" \
+	CONTENT_TYPE="application/x-$1-request" \
+	run_backend "/repo.git/$1" "$2" &&
+	if ! grep "Status" act.out >act
+	then
+		printf "Status: 200 OK\r\n" >act
+	fi
+	printf "Status: $3\r\n" >exp &&
+	test_cmp exp act
 }
 
 log_div() {
@@ -28,9 +44,9 @@ log_div() {
 
 expect_aliased() {
 	if test $1 = 0; then
-		run_backend "$2"
+		REQUEST_METHOD=GET run_backend "$2"
 	else
-		run_backend "$2" &&
+		REQUEST_METHOD=GET run_backend "$2" &&
 		echo "fatal: '$2': aliased" >exp.err &&
 		test_cmp exp.err act.err
 	fi
