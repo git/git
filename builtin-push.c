c0eb604330e1288300d915f25868d1eed88d3038
Matthieu Moy <Matthieu.Moy@imag.fr>
2010-01-11 22:09:44 +0100

    push: spell 'Note about fast-forwards' section name correctly in error message.
    The error message in case of non-fast forward points to 'git push
    --help', but used to talk about a section 'non-fast-forward', while the
    actual section name is 'Note about fast-forwards'.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-push.c
+++ builtin-push.c
@@ -125,8 +125,8 @@ static int push_with_options(struct transport *transport, int flags)
 
 	if (nonfastforward && advice_push_nonfastforward) {
 		printf("To prevent you from losing history, non-fast-forward updates were rejected\n"
-		       "Merge the remote changes before pushing again.  See the 'non-fast-forward'\n"
-		       "section of 'git push --help' for details.\n");
+		       "Merge the remote changes before pushing again.  See the 'Note about\n"
+		       "fast-forwards' section of 'git push --help' for details.\n");
 	}
 
 	return 1;
