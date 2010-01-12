d38a30df7dd54c5c6883af1de1a03ec7d523cee5
Matthieu Moy <Matthieu.Moy@imag.fr>
2010-01-12 10:54:44 +0100

    Be more user-friendly when refusing to do something because of conflict.
    Various commands refuse to run in the presence of conflicts (commit,
    merge, pull, cherry-pick/revert). They all used to provide rough, and
    inconsistant error messages.
    
    A new variable advice.resolveconflict is introduced, and allows more
    verbose messages, pointing the user to the appropriate solution.
    
    For commit, the error message used to look like this:
    
    $ git commit
    foo.txt: needs merge
    foo.txt: unmerged (c34a92682e0394bc0d6f4d4a67a8e2d32395c169)
    foo.txt: unmerged (3afcd75de8de0bb5076942fcb17446be50451030)
    foo.txt: unmerged (c9785d77b76dfe4fb038bf927ee518f6ae45ede4)
    error: Error building trees
    
    The "need merge" line is given by refresh_cache. We add the IN_PORCELAIN
    option to make the output more consistant with the other porcelain
    commands, and catch the error in return, to stop with a clean error
    message. The next lines were displayed by a call to cache_tree_update(),
    which is not reached anymore if we noticed the conflict.
    
    The new output looks like:
    
    U       foo.txt
    fatal: 'commit' is not possible because you have unmerged files.
    Please, fix them up in the work tree, and then use 'git add/rm <file>' as
    appropriate to mark resolution and make a commit, or use 'git commit -a'.
    
    Pull is slightly modified to abort immediately if $GIT_DIR/MERGE_HEAD
    exists instead of waiting for merge to complain.
    
    The behavior of merge and the test-case are slightly modified to reflect
    the usual flow: start with conflicts, fix them, and afterwards get rid of
    MERGE_HEAD, with different error messages at each stage.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- advice.h
+++ advice.h
@@ -1,10 +1,15 @@
 #ifndef ADVICE_H
 #define ADVICE_H
 
+#include "git-compat-util.h"
+
 extern int advice_push_nonfastforward;
 extern int advice_status_hints;
 extern int advice_commit_before_merge;
+extern int advice_resolve_conflict;
 
 int git_default_advice_config(const char *var, const char *value);
+
+extern void NORETURN die_resolve_conflict(const char *me);
 
 #endif /* ADVICE_H */
