81d2caefedefa40557baf7af846580f35de3f995
Junio C Hamano <gitster@pobox.com>
2010-01-12 12:09:54 -0800

    strbuf_addbuf(): allow passing the same buf to dst and src
    If sb and sb2 are the same (i.e. doubling the string), the underlying
    strbuf_add() can make sb2->buf invalid by calling strbuf_grow(sb) at
    the beginning; if realloc(3) done by strbuf_grow() needs to move the
    string, strbuf_add() will read from an already freed buffer.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- strbuf.h
+++ strbuf.h
@@ -105,6 +105,7 @@ static inline void strbuf_addstr(struct strbuf *sb, const char *s) {
 	strbuf_add(sb, s, strlen(s));
 }
 static inline void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2) {
+	strbuf_grow(sb, sb2->len);
 	strbuf_add(sb, sb2->buf, sb2->len);
 }
 extern void strbuf_adddup(struct strbuf *sb, size_t pos, size_t len);
