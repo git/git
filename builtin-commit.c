ab6854515ee2280f187fbc2ab08f9e9115438f3e
Junio C Hamano <gitster@pobox.com>
2010-01-19 17:10:35 -0800

    status: don't require the repository to be writable
    We need to update the index before hooks run when actually making a
    commit, but we shouldn't have to write the index when running "status".
    If we can, then we have already spent cycles to refresh the index and
    it is a waste not to write it out, but it is not a disaster if we cannot
    write it out.  The main reason the user is running "git status" is to get
    the "status", and refreshing the index is a mere side effect that we can
    do without.
    
    Discovery and initial attempted fix by Dscho.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-commit.c
+++ builtin-commit.c
@@ -278,11 +278,13 @@ static char *prepare_index(int argc, const char **argv, const char *prefix, int 
 	 * We still need to refresh the index here.
 	 */
 	if (!pathspec || !*pathspec) {
-		fd = hold_locked_index(&index_lock, 1);
+		fd = hold_locked_index(&index_lock, !is_status);
 		refresh_cache(refresh_flags);
-		if (write_cache(fd, active_cache, active_nr) ||
-		    commit_locked_index(&index_lock))
-			die("unable to write new_index file");
+		if (0 <= fd) {
+			if (write_cache(fd, active_cache, active_nr) ||
+			    commit_locked_index(&index_lock))
+				die("unable to write new_index file");
+		}
 		commit_style = COMMIT_AS_IS;
 		return get_index_file();
 	}
