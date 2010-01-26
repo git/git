93cfa7c7a85efbdb00daade4ad0afc11bd2fdf37
Junio C Hamano <gitster@pobox.com>
2010-01-26 11:58:00 -0800

    approxidate_careful() reports errorneous date string
    For a long time, the time based reflog syntax (e.g. master@{yesterday})
    didn't complain when the "human readable" timestamp was misspelled, as
    the underlying mechanism tried to be as lenient as possible.  The funny
    thing was that parsing of "@{now}" even relied on the fact that anything
    not recognized by the machinery returned the current timestamp.
    
    Introduce approxidate_careful() that takes an optional pointer to an
    integer, that gets assigned 1 when the input does not make sense as a
    timestamp.
    
    As I am too lazy to fix all the callers that use approxidate(), most of
    the callers do not take advantage of the error checking, but convert the
    code to parse reflog to use it as a demonstration.
    
    Tests are mostly from Jeff King.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_name.c
+++ sha1_name.c
@@ -395,8 +395,11 @@ static int get_sha1_basic(const char *str, int len, unsigned char *sha1)
 		} else if (0 <= nth)
 			at_time = 0;
 		else {
+			int errors = 0;
 			char *tmp = xstrndup(str + at + 2, reflog_len);
-			at_time = approxidate(tmp);
+			at_time = approxidate_careful(tmp, &errors);
+			if (errors)
+				die("Bogus timestamp '%s'", tmp);
 			free(tmp);
 		}
 		if (read_ref_at(real_ref, at_time, nth, sha1, NULL,
