87635ed04500887db49e4ce65f0b8df2c48c8210
Heiko Voigt <hvoigt@hvoigt.net>
2010-01-26 21:17:36 +0100

    mingw: move unlink wrapper to mingw.c
    The next patch implements a workaround in case unlink fails on Windows.
    
    Signed-off-by: Heiko Voigt <hvoigt@hvoigt.net>

--- compat/mingw.h
+++ compat/mingw.h
@@ -119,14 +119,6 @@ static inline int fcntl(int fd, int cmd, ...)
 int mingw_mkdir(const char *path, int mode);
 #define mkdir mingw_mkdir
 
-static inline int mingw_unlink(const char *pathname)
-{
-	/* read-only files cannot be removed */
-	chmod(pathname, 0666);
-	return unlink(pathname);
-}
-#define unlink mingw_unlink
-
 static inline pid_t waitpid(pid_t pid, int *status, unsigned options)
 {
 	if (options == 0)
@@ -176,6 +168,9 @@ int link(const char *oldpath, const char *newpath);
 /*
  * replacements of existing functions
  */
+
+int mingw_unlink(const char *pathname);
+#define unlink mingw_unlink
 
 int mingw_open (const char *filename, int oflags, ...);
 #define open mingw_open
