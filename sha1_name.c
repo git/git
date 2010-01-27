a5e10acbb946305ca42f510cdb196d171020238e
Junio C Hamano <gitster@pobox.com>
2010-01-27 10:53:09 -0800

    Update @{bogus.timestamp} fix not to die()
    The caller will say "It is not a valid object name" if it wants to, and
    some callers may even try to see if it names an object and otherwise try to
    see if it is a path.
    
    Pointed out by Jeff King.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_name.c
+++ sha1_name.c
@@ -398,9 +398,9 @@ static int get_sha1_basic(const char *str, int len, unsigned char *sha1)
 			int errors = 0;
 			char *tmp = xstrndup(str + at + 2, reflog_len);
 			at_time = approxidate_careful(tmp, &errors);
-			if (errors)
-				die("Bogus timestamp '%s'", tmp);
 			free(tmp);
+			if (errors)
+				return -1;
 		}
 		if (read_ref_at(real_ref, at_time, nth, sha1, NULL,
 				&co_time, &co_tz, &co_cnt)) {
