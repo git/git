7283bbc70a55d7364fbeaefc1009c03fcfc8d929
Pete Harlan <pgit@pcharlan.com>
2010-02-15 15:33:18 -0800

    Remove hyphen from "git-command" in two error messages
    Signed-off-by: Pete Harlan <pgit@pcharlan.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -350,7 +350,7 @@ const char *help_unknown_cmd(const char *cmd)
 		return assumed;
 	}
 
-	fprintf(stderr, "git: '%s' is not a git-command. See 'git --help'.\n", cmd);
+	fprintf(stderr, "git: '%s' is not a git command. See 'git --help'.\n", cmd);
 
 	if (SIMILAR_ENOUGH(best_similarity)) {
 		fprintf(stderr, "\nDid you mean %s?\n",
