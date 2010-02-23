c31440e711dd6d2910ad70c9ba0ed6f0e2292862
Johannes Sixt <j6t@kdbg.org>
2010-02-23 12:42:56 +0100

    Print RUNTIME_PREFIX warning only when GIT_TRACE is set
    When RUNTIME_PREFIX is enabled, the installation prefix is derived by
    trying a limited set of known locations where the git executable can
    reside. If none of these is found, a warning is emitted.
    
    When git is built in a directory that matches neither of these known names,
    the warning would always be emitted when the uninstalled executable is run.
    This is a problem on Windows, where gitk picks the uninstalled git when
    invoked from the build directory and gets confused by the warning.
    
    Print the warning only when GIT_TRACE is set.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- exec_cmd.c
+++ exec_cmd.c
@@ -28,7 +28,7 @@ const char *system_path(const char *path)
 	    !(prefix = strip_path_suffix(argv0_path, BINDIR)) &&
 	    !(prefix = strip_path_suffix(argv0_path, "git"))) {
 		prefix = PREFIX;
-		fprintf(stderr, "RUNTIME_PREFIX requested, "
+		trace_printf("RUNTIME_PREFIX requested, "
 				"but prefix computation failed.  "
 				"Using static fallback '%s'.\n", prefix);
 	}
