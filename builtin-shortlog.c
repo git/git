3731449591130b2f5ca7257c46721785a6340a18
Michele Ballabio <barra_cuda@katamail.com>
2010-02-24 21:49:03 +0100

    shortlog: warn the user when there is no input
    A simple "git shortlog" outside of a git repository stalls
    waiting for an input. Check if that's the case by testing with
    isatty() before read_from_stdin(), and warn the user like
    "git commit" does in a similar case.
    
    Signed-off-by: Michele Ballabio <barra_cuda@katamail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-shortlog.c
+++ builtin-shortlog.c
@@ -295,6 +295,8 @@ parse_done:
 	if (!nongit && !rev.pending.nr && isatty(0))
 		add_head_to_pending(&rev);
 	if (rev.pending.nr == 0) {
+		if (isatty(0))
+			fprintf(stderr, "(reading log message from standard input)\n");
 		read_from_stdin(&log);
 	}
 	else
