abeb09b646921b725561870ef4dd62294f9863ad
Stefan-W. Hahn <stefan.hahn@s-hahn.de>
2010-02-27 15:20:25 +0100

    documentation: 'git-mailsplit --keep-cr' is not hidden anymore
    So far this was an internal mechanism for rebase, but we will be exposing
    it to the end users.
    
    Signed-off-by: Stefan-W. Hahn <stefan.hahn@s-hahn.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-mailsplit.c
+++ builtin-mailsplit.c
@@ -10,7 +10,7 @@
 #include "strbuf.h"
 
 static const char git_mailsplit_usage[] =
-"git mailsplit [-d<prec>] [-f<n>] [-b] -o<directory> [<mbox>|<Maildir>...]";
+"git mailsplit [-d<prec>] [-f<n>] [-b] [--keep-cr] -o<directory> [<mbox>|<Maildir>...]";
 
 static int is_from_line(const char *line, int len)
 {
