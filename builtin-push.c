011fe9814f7be7df7212229fa4f8393e03829cb7
Larry D'Anna <larry@elder-gods.org>
2010-02-26 23:52:13 -0500

    git-push: fix an advice message so it goes to stderr
    These sort of messages typically go to the standard error.
    
    Signed-off-by: Larry D'Anna <larry@elder-gods.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-push.c
+++ builtin-push.c
@@ -124,9 +124,9 @@ static int push_with_options(struct transport *transport, int flags)
 		return 0;
 
 	if (nonfastforward && advice_push_nonfastforward) {
-		printf("To prevent you from losing history, non-fast-forward updates were rejected\n"
-		       "Merge the remote changes before pushing again.  See the 'Note about\n"
-		       "fast-forwards' section of 'git push --help' for details.\n");
+		fprintf(stderr, "To prevent you from losing history, non-fast-forward updates were rejected\n"
+				"Merge the remote changes before pushing again.  See the 'Note about\n"
+				"fast-forwards' section of 'git push --help' for details.\n");
 	}
 
 	return 1;
