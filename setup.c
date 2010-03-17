502ffe3491bb6a3aff3f93e7eea19c01cf30c337
Lars R. Damerow <lars@pixar.com>
2010-03-17 12:55:52 -0700

    truncate cwd string before printing error message
    Without this truncation the error message printed only shows the cwd
    from the start of the search, not where it failed.
    
    Signed-off-by: Lars R. Damerow <lars@pixar.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -422,8 +422,10 @@ const char *setup_git_directory_gently(int *nongit_ok)
 			}
 			die("Not a git repository (or any of the parent directories): %s", DEFAULT_GIT_DIR_ENVIRONMENT);
 		}
-		if (chdir(".."))
+		if (chdir("..")) {
+			cwd[offset] = '\0';
 			die_errno("Cannot change to '%s/..'", cwd);
+		}
 	}
 
 	inside_git_dir = 0;
