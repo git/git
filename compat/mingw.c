442dada22d3b6e511d30b05a80bab153619e52a7
Pat Thoyts <patthoyts@users.sourceforge.net>
2010-03-18 21:45:19 +0000

    MinGW: Report errors when failing to launch the html browser.
    The mingw function to launch the system html browser is silent if the
    target file does not exist leaving the user confused. Make it display
    something.
    
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>
    Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -1417,6 +1417,7 @@ void mingw_open_html(const char *unixpath)
 			const char *, const char *, const char *, INT);
 	T ShellExecute;
 	HMODULE shell32;
+	int r;
 
 	shell32 = LoadLibrary("shell32.dll");
 	if (!shell32)
@@ -1426,9 +1427,12 @@ void mingw_open_html(const char *unixpath)
 		die("cannot run browser");
 
 	printf("Launching default browser to display HTML ...\n");
-	ShellExecute(NULL, "open", htmlpath, NULL, "\\", 0);
-
+	r = (int)ShellExecute(NULL, "open", htmlpath, NULL, "\\", SW_SHOWNORMAL);
 	FreeLibrary(shell32);
+	/* see the MSDN documentation referring to the result codes here */
+	if (r <= 32) {
+		die("failed to launch browser for %.*s", MAX_PATH, unixpath);
+	}
 }
 
 int link(const char *oldpath, const char *newpath)
