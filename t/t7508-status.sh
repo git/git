4c926b37c21a088163871bb8e6ccdfc4b4952e34
Markus Heidelberg <markus.heidelberg@web.de>
2010-04-02 14:27:20 +0200

    t7508: add test for "git status" refreshing the index
    Signed-off-by: Markus Heidelberg <markus.heidelberg@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7508-status.sh
+++ t/t7508-status.sh
@@ -496,6 +496,16 @@ test_expect_success 'dry-run of partial commit excluding new file in index' '
 	test_cmp expect output
 '
 
+cat >expect <<EOF
+:100644 100644 e69de29bb2d1d6434b8b29ae775ad8c2e48c5391 0000000000000000000000000000000000000000 M	dir1/modified
+EOF
+test_expect_failure 'status refreshes the index' '
+	touch dir2/added &&
+	git status &&
+	git diff-files >output &&
+	test_cmp expect output
+'
+
 test_expect_success 'setup status submodule summary' '
 	test_create_repo sm && (
 		cd sm &&
