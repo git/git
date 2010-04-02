4bb6644d03f6932b94c24c3825e28865f493b692
Markus Heidelberg <markus.heidelberg@web.de>
2010-04-02 23:44:21 +0200

    git status: refresh the index if possible
    This was already the case before commit 9e4b7ab6 (git status: not
    "commit --dry-run" anymore, 2009-08-15) with the difference that it died
    at failure.
    It got lost during the new implementation of "git status", which was
    meant to only change behaviour when invoked with arguments.
    
    Signed-off-by: Markus Heidelberg <markus.heidelberg@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7508-status.sh
+++ t/t7508-status.sh
@@ -499,7 +499,7 @@ test_expect_success 'dry-run of partial commit excluding new file in index' '
 cat >expect <<EOF
 :100644 100644 e69de29bb2d1d6434b8b29ae775ad8c2e48c5391 0000000000000000000000000000000000000000 M	dir1/modified
 EOF
-test_expect_failure 'status refreshes the index' '
+test_expect_success 'status refreshes the index' '
 	touch dir2/added &&
 	git status &&
 	git diff-files >output &&
