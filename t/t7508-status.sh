c5fc0e52b417af5499621801c4880c060d763d6a
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-04-08 00:18:07 +0200

    Work around funny CR issue
    This is really a problem with shell scripts being called on msysGit,
    but there are more important bugs to fix for the moment.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t7508-status.sh
+++ t/t7508-status.sh
@@ -607,6 +607,10 @@ EOF
 test_expect_success 'status submodule summary' '
 	git config status.submodulesummary 10 &&
 	git status >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output
 '
 
@@ -623,6 +627,10 @@ A  sm
 EOF
 test_expect_success 'status -s submodule summary' '
 	git status -s >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output
 '
 
@@ -649,8 +657,16 @@ test_expect_success 'status submodule summary (clean submodule)' '
 	git commit -m "commit submodule" &&
 	git config status.submodulesummary 10 &&
 	test_must_fail git commit --dry-run >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output &&
 	git status >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output
 '
 
@@ -665,6 +681,10 @@ cat >expect <<EOF
 EOF
 test_expect_success 'status -s submodule summary (clean submodule)' '
 	git status -s >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output
 '
 
@@ -700,6 +720,10 @@ EOF
 test_expect_success 'commit --dry-run submodule summary (--amend)' '
 	git config status.submodulesummary 10 &&
 	git commit --dry-run --amend >output &&
+	if test_have_prereq MINGW
+	then
+		dos2unix output
+	fi &&
 	test_cmp expect output
 '
 
