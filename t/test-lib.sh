64b90323f65d2a3ac8d1ff4ed73be881f76aac8a
Michal Sojka <sojkam1@fel.cvut.cz>
2010-04-16 15:53:59 +0200

    test-lib.sh: Add explicit license detail, with change from GPLv2 to GPLv2+.
    Dear Junio,
    
    this is a resend of relicensing patch for test suite library, which
    was initially sent by Carl Worth. Since the time you sent me acks for
    this patch collected by you, I collected 8 additional acks as is
    documented at
    https://git.wiki.kernel.org/index.php/Test-lib_reclicensing. There are
    still three contributors missing: Bert Wesarg, Stephan Beyer and Bryan
    Donlan. The contributions of first two are clearly not copyrightable.
    I'm not sure about the copyrightability of Bryan Donlan's
    contributions (git log -p --author='Bryan Donlan' t/test-lib.sh).
    
    Carl told me that in your ack collection process you missed only three
    acks. So I wonder whether you already did some analysis of which
    contributions are copyrightable. If so, are the missing acks in the
    list bellow?
    
    Thanks
    Michal
    
    8<--------8<--------8<--------
    This file has had no explicit license information noted in it, but
    has clearly been created and modified according to the terms of GPLv2
    as with the rest of the git code base.
    
    The purpose of relicensing is to allow other GPLv3+ projects (in
    particular, the notmuch project: http://notmuchmail.org) to use this
    same test-suite structure and to contribute changes back as well.
    
    Signed-off-by: Carl Worth <cworth@cworth.org>
    Signed-off-by: Michal Sojka <sojkam1@fel.cvut.cz>
    Acked-by: Alex Riesen <raa.lkml@gmail.com>
    Acked-by: Brandon Casey <drafnel@gmail.com>
    Acked-by: Clemens Buchacher <drizzd@aon.at>
    Acked-by: David Reiss <dreiss@facebook.com>
    Acked-by: Emil Sit <sit@emilsit.net>
    Acked-by: Eric Wong <normalperson@yhbt.net>
    Acked-by: Fredrik Kuivinen <frekui@gmail.com>
    Acked-by: Gerrit Pape <pape@smarden.org>
    Acked-by: Christian Couder <chriscool@tuxfamily.org>
    Acked-by: Jakub Narebski <jnareb@gmail.com>
    Acked-by: Jeff King <peff@peff.net>
    Acked-by: Johan Herland <johan@herland.net>
    Acked-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Acked-by: Johannes Sixt <j6t@kdbg.org>
    Acked-by: Jonathan Nieder <jrnieder@gmail.com>
    Acked-by: Josh Triplett <josh@joshtriplett.org>
    Acked-by: Junio C Hamano <gitster@pobox.com>
    Acked-by: Lea Wiemann <lewiemann@gmail.com>
    Acked-by: Markus Heidelberg <markus.heidelberg@web.de>
    Acked-by: Martin Waitz <tali@admingilde.org>
    Acked-by: Matthew Ogilvie <mmogilvi_git@miniinfo.net>
    Acked-by: Matthias Lederhofer <matled@gmx.net>
    Acked-by: Michael J Gruber <git@drmicha.warpmail.net>
    Acked-by: Michele Ballabio <barra_cuda@katamail.com>
    Acked-by: Miklos Vajna <vmiklos@frugalware.org>
    Acked-by: Nicolas Pitre <nico@fluxnic.net>
    Acked-by: Pavel Roskin <proski@gnu.org>
    Acked-by: Petr Baudis <pasky@ucw.cz>
    Acked-by: Pierre Habouzit <madcoder@debian.org>
    Acked-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Acked-by: Shawn O. Pearce <spearce@spearce.org>
    Acked-by: Stephen Boyd <bebarino@gmail.com>
    Acked-by: Sverre Rabbelier <srabbelier@gmail.com>
    Acked-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -2,6 +2,18 @@
 #
 # Copyright (c) 2005 Junio C Hamano
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/ .
 
 # if --tee was passed, write the output not only to the terminal, but
 # additionally to the file test-results/$BASENAME.out, too.
