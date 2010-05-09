921eabde9d0b9b43b293b37ac701bdfd10a64f54
Pete Harlan <pgit@pcharlan.com>
2010-05-09 13:10:17 -0700

    clone: reword messages to match the end-user perception
    When cloning into a non-bare repository, e.g. "git clone $URL mine",
    we used to report that we are cloning into "mine/.git".  Reword the
    report to say "Cloning into mine" instead, as that matches what the
    end-user asked for closer.
    
    Make the message for "git clone --bare $URL mine" to say "Cloning
    into bare repository mine" do make the distinction between this case and
    the above stand out a bit more prominently.
    
    Suggested-by: Jeff King <peff@peff.net>
    Signed-off-by: Pete Harlan <pgit@pcharlan.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -464,7 +464,8 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 	set_git_dir(make_absolute_path(git_dir));
 
 	if (0 <= option_verbosity)
-		printf("Cloning into %s...\n", get_git_dir());
+		printf("Cloning into %s%s...\n",
+		       option_bare ? "bare repository " : "", dir);
 	init_db(option_template, INIT_DB_QUIET);
 
 	/*
