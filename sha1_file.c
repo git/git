560fb6a183e1cdbc2948b06cc60bba07f79804a6
Jeff King <peff@peff.net>
2010-05-22 02:59:42 -0400

    remove over-eager caching in sha1_file_name
    This function takes a sha1 and produces a loose object
    filename. It caches the location of the object directory so
    that it can fill the sha1 information directly without
    allocating a new buffer (and in its original incarnation,
    without calling getenv(), though these days we cache that
    with the code in environment.c).
    
    This cached base directory can become stale, however, if in
    a single process git changes the location of the object
    directory (e.g., by running setup_work_tree, which will
    chdir to the new worktree).
    
    In most cases this isn't a problem, because we tend to set
    up the git repository location and do any chdir()s before
    actually looking up any objects, so the first lookup will
    cache the correct location. In the case of reset --hard,
    however, we do something like:
    
      1. look up the commit object
    
      2. notice we are doing --hard, run setup_work_tree
    
      3. look up the tree object to reset
    
    Step (3) fails because our cache object directory value is
    bogus.
    
    This patch simply removes the caching. We use a static
    buffer instead of allocating one each time (the original
    version treated the malloc'd buffer as a static, so there is
    no change in calling semantics).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -102,20 +102,22 @@ static void fill_sha1_path(char *pathbuf, const unsigned char *sha1)
  */
 char *sha1_file_name(const unsigned char *sha1)
 {
-	static char *name, *base;
+	static char buf[PATH_MAX];
+	const char *objdir;
+	int len;
 
-	if (!base) {
-		const char *sha1_file_directory = get_object_directory();
-		int len = strlen(sha1_file_directory);
-		base = xmalloc(len + 60);
-		memcpy(base, sha1_file_directory, len);
-		memset(base+len, 0, 60);
-		base[len] = '/';
-		base[len+3] = '/';
-		name = base + len + 1;
-	}
-	fill_sha1_path(name, sha1);
-	return base;
+	objdir = get_object_directory();
+	len = strlen(objdir);
+
+	/* '/' + sha1(2) + '/' + sha1(38) + '\0' */
+	if (len + 43 > PATH_MAX)
+		die("insanely long object directory %s", objdir);
+	memcpy(buf, objdir, len);
+	buf[len] = '/';
+	buf[len+3] = '/';
+	buf[len+42] = '\0';
+	fill_sha1_path(buf + len + 1, sha1);
+	return buf;
 }
 
 static char *sha1_get_pack_name(const unsigned char *sha1,
