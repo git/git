761a889a97a33d277f8a6607a4696164391f9d5d
Gary V. Vaughan <git@mlists.thewrittenword.com>
2010-06-01 20:55:36 -0500

    git-compat-util.h: use apparently more common __sgi macro to detect SGI IRIX
    IRIX 6.5.26m does not define the 'sgi' macro, but it does define an '__sgi'
    macro.  Since later IRIX versions (6.5.29m) define both macros, and since
    an underscore prefixed macro is preferred anyway, use '__sgi' to detect
    compilation on SGI IRIX.
    
    Signed-off-by: Brandon Casey <casey@nrlssc.navy.mil>
    Signed-off-by: Gary V. Vaughan <gary@thewrittenword.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-compat-util.h
+++ git-compat-util.h
@@ -56,7 +56,7 @@
 # define _XOPEN_SOURCE 500
 # endif
 #elif !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__USLC__) && \
-      !defined(_M_UNIX) && !defined(sgi) && !defined(__DragonFly__)
+      !defined(_M_UNIX) && !defined(__sgi) && !defined(__DragonFly__)
 #define _XOPEN_SOURCE 600 /* glibc2 and AIX 5.3L need 500, OpenBSD needs 600 for S_ISLNK() */
 #define _XOPEN_SOURCE_EXTENDED 1 /* AIX 5.3L needs this */
 #endif
