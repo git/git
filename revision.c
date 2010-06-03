97b03c353856602a3c30b01baae1efb8dfe4243e
Johan Herland <johan@herland.net>
2010-06-04 01:17:36 +0200

    revision: Fix typo in --ancestry-path error message
    Signed-off-by: Johan Herland <johan@herland.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -745,7 +745,7 @@ static int limit_list(struct rev_info *revs)
 	if (revs->ancestry_path) {
 		bottom = collect_bottom_commits(list);
 		if (!bottom)
-			die("--ancestry-path given but there is no bottom commits");
+			die("--ancestry-path given but there are no bottom commits");
 	}
 
 	while (list) {
