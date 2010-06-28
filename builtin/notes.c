bbb1b8a35a06203020f7ab6d1ad58bcb4afe8e93
Jeff King <peff@peff.net>
2010-06-28 04:59:07 -0400

    notes: check number of parameters to "git notes copy"
    Otherwise we may segfault with too few parameters.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Tested-by: Bert Wesarg <Bert.Wesarg@googlemail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/notes.c
+++ builtin/notes.c
@@ -614,6 +614,10 @@ static int copy(int argc, const char **argv, const char *prefix)
 		}
 	}
 
+	if (argc < 2) {
+		error("too few parameters");
+		usage_with_options(git_notes_copy_usage, options);
+	}
 	if (2 < argc) {
 		error("too many parameters");
 		usage_with_options(git_notes_copy_usage, options);
