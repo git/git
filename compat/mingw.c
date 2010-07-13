6c536a98b3b579c682ad8b6e4b016f87889948b1
Cezary Zawadka <czawadka@gmail.com>
2010-07-13 16:17:43 +0200

    Windows: Allow using UNC path for git repository
    [efl: moved MinGW-specific part to compat/]
    [jes: fixed compilation on non-Windows]
    
    Eric Sunshine fixed mingw_offset_1st_component() to return consistently "foo"
    for UNC "//machine/share/foo", cf
    http://groups.google.com/group/msysgit/browse_thread/thread/c0af578549b5dda0
    
    Author: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Cezary Zawadka <czawadka@gmail.com>
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Stepan Kasal <kasal@ucw.cz>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -1823,3 +1823,27 @@ pid_t waitpid(pid_t pid, int *status, int options)
 	errno = EINVAL;
 	return -1;
 }
+
+int mingw_offset_1st_component(const char *path)
+{
+	int offset = 0;
+	if (has_dos_drive_prefix(path))
+		offset = 2;
+
+	/* unc paths */
+	else if (is_dir_sep(path[0]) && is_dir_sep(path[1])) {
+
+		/* skip server name */
+		char *pos = strpbrk(path + 2, "\\/");
+		if (!pos)
+			return 0; /* Error: malformed unc path */
+
+		do {
+			pos++;
+		} while (*pos && !is_dir_sep(*pos));
+
+		offset = pos - path;
+	}
+
+	return offset + is_dir_sep(path[offset]);
+}
