9439967d264668bc9c0cd27ec3e5d25bdaeae146
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-07-23 17:40:44 +0200

    Make sure that git_getpass() never returns NULL
    The result of git_getpass() is used without checking for NULL, so let's
    just die() instead of returning NULL.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- connect.c
+++ connect.c
@@ -628,8 +628,12 @@ char *git_getpass(const char *prompt)
 
 	askpass = getenv("GIT_ASKPASS");
 
-	if (!askpass || !(*askpass))
-		return getpass(prompt);
+	if (!askpass || !(*askpass)) {
+		char *result = getpass(prompt);
+		if (!result)
+			die_errno("Could not read password");
+		return result;
+	}
 
 	args[0] = askpass;
 	args[1]	= prompt;
