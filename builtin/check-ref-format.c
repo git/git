49cc460d88f5bae79b45ce2db16674f36b6cf38a
Jonathan Nieder <jrnieder@gmail.com>
2010-08-05 22:39:22 -0500

    Allow "check-ref-format --branch" from subdirectory
    check-ref-format --branch requires access to the repository
    to resolve refs like @{-1}.
    
    Noticed by Nguyễn Thái Ngọc Duy.
    
    Cc: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/check-ref-format.c
+++ builtin/check-ref-format.c
@@ -36,7 +36,9 @@ static void collapse_slashes(char *dst, const char *src)
 static int check_ref_format_branch(const char *arg)
 {
 	struct strbuf sb = STRBUF_INIT;
+	int nongit;
 
+	setup_git_directory_gently(&nongit);
 	if (strbuf_check_branch_ref(&sb, arg))
 		die("'%s' is not a valid branch name", arg);
 	printf("%s\n", sb.buf + 11);
