f0ef6a6eff15a3df8dc9ec61d93e3f7e540ed0ac
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2010-08-05 22:20:08 -0500

    ls-remote: run setup_git_directory_gently() sooner
    ls-remote already runs a repository search unconditionally to learn
    about remote nicknames and "[url] insteadof" shortcuts.  Run that
    search a little sooner, and now one can try
    
    	[pager]
    		ls-remote
    
    to automatically paginate ls-remote output, or use repository-local
    
    	[core]
    		pager = whatever
    
    with "git --paginate ls-remote <url>".
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/ls-remote.c
+++ builtin/ls-remote.c
@@ -32,7 +32,6 @@ int cmd_ls_remote(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	const char *dest = NULL;
-	int nongit;
 	unsigned flags = 0;
 	int quiet = 0;
 	const char *uploadpack = NULL;
@@ -41,8 +40,6 @@ int cmd_ls_remote(int argc, const char **argv, const char *prefix)
 	struct remote *remote;
 	struct transport *transport;
 	const struct ref *ref;
-
-	setup_git_directory_gently(&nongit);
 
 	for (i = 1; i < argc; i++) {
 		const char *arg = argv[i];
