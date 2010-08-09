d8faea9d182bd0ca2624be74d9dc32b187618840
Jakub Narebski <jnareb@gmail.com>
2010-08-09 10:50:53 -0400

    diff: strip extra "/" when stripping prefix
    There are two ways a user might want to use "diff --relative":
    
      1. For a file in a directory, like "subdir/file", the user
         can use "--relative=subdir/" to strip the directory.
    
      2. To strip part of a filename, like "foo-10", they can
         use "--relative=foo-".
    
    We currently handle both of those situations. However, if the user passes
    "--relative=subdir" (without the trailing slash), we produce inconsistent
    results. For the unified diff format, we collapse the double-slash of
    "a//file" correctly into "a/file". But for other formats (raw, stat,
    name-status), we end up with "/file".
    
    We can do what the user means here and strip the extra "/" (and only a
    slash).  We are not hurting any existing users of (2) above with this
    behavior change because the existing output for this case was nonsensical.
    
    Patch by Jakub, tests and commit message by Jeff King.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -2704,10 +2704,16 @@ static void diff_fill_sha1_info(struct diff_filespec *one)
 static void strip_prefix(int prefix_length, const char **namep, const char **otherp)
 {
 	/* Strip the prefix but do not molest /dev/null and absolute paths */
-	if (*namep && **namep != '/')
+	if (*namep && **namep != '/') {
 		*namep += prefix_length;
-	if (*otherp && **otherp != '/')
+		if (**namep == '/')
+			++*namep;
+	}
+	if (*otherp && **otherp != '/') {
 		*otherp += prefix_length;
+		if (**otherp == '/')
+			++*otherp;
+	}
 }
 
 static void run_diff(struct diff_filepair *p, struct diff_options *o)
