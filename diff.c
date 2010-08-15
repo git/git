34597c1f5a77c710dae33092cb8a7cb01c6b21c1
Clemens Buchacher <drizzd@aon.at>
2010-08-15 09:20:43 +0200

    hash binary sha1 into patch id
    Since commit 2f82f760 (Take binary diffs into
    account for "git rebase"), binary files are
    included in patch ID computation. Binary files are
    diffed using the text diff algorithm, however,
    which has a huge impact on performance. The
    following tests performance for a 50000 line file
    marked as binary in .gitattributes.
    
    $ git format-patch --stdout --ignore-if-in-upstream master
    
    real    0m0.367s
    user    0m0.354s
    sys     0m0.010s
    
    Instead of diffing the binary files, hash the pre-
    and post-image sha1, which is just as unique. As a
    result, performance is much improved.
    
    $ git format-patch --stdout --ignore-if-in-upstream master
    
    real    0m0.016s
    user    0m0.015s
    sys     0m0.001s
    
    Signed-off-by: Clemens Buchacher <drizzd@aon.at>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -3758,6 +3758,13 @@ static int diff_get_patch_id(struct diff_options *options, unsigned char *sha1)
 					len2, p->two->path);
 		git_SHA1_Update(&ctx, buffer, len1);
 
+		if (diff_filespec_is_binary(p->one) ||
+		    diff_filespec_is_binary(p->two)) {
+			git_SHA1_Update(&ctx, sha1_to_hex(p->one->sha1), 40);
+			git_SHA1_Update(&ctx, sha1_to_hex(p->two->sha1), 40);
+			continue;
+		}
+
 		xpp.flags = 0;
 		xecfg.ctxlen = 3;
 		xecfg.flags = XDL_EMIT_FUNCNAMES;
