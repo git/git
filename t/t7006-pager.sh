41bf3bc232cf5f99fdc6d3d729536a4c4821d3bd
Jonathan Nieder <jrnieder@gmail.com>
2010-08-16 03:08:34 -0500

    t7006 (pager): add missing TTY prerequisites
    The "git bundle unbundle" and "git config" pagination tests are not
    supposed to run when stdout is not a terminal and IO::Pty not available
    to make one on the fly.
    
    Reported-by: Thomas Rast <trast@student.ethz.ch>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7006-pager.sh
+++ t/t7006-pager.sh
@@ -134,7 +134,7 @@ test_expect_success TTY 'configuration can disable pager' '
 	! test -e paginated.out
 '
 
-test_expect_success 'git config uses a pager if configured to' '
+test_expect_success TTY 'git config uses a pager if configured to' '
 	rm -f paginated.out &&
 	git config pager.config true &&
 	test_when_finished "git config --unset pager.config" &&
@@ -142,7 +142,7 @@ test_expect_success 'git config uses a pager if configured to' '
 	test -e paginated.out
 '
 
-test_expect_success 'configuration can enable pager (from subdir)' '
+test_expect_success TTY 'configuration can enable pager (from subdir)' '
 	rm -f paginated.out &&
 	mkdir -p subdir &&
 	git config pager.bundle true &&
