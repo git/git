70256a3a6780a9fd181edf0bf0e762eb56637c9c
Ramkumar Ramachandra <artagnon@gmail.com>
2010-08-24 11:06:51 +0530

    shell: Rewrite documentation and improve error message
    Update the documentation of 'git shell' to mention the interactive
    mode and COMMAND_DIR. Also provide a hint when interactive mode is not
    available in the shell.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Reviewed-by: Greg Brockman <gdb@MIT.EDU>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- shell.c
+++ shell.c
@@ -149,8 +149,11 @@ int main(int argc, char **argv)
 	} else if (argc == 1) {
 		/* Allow the user to run an interactive shell */
 		cd_to_homedir();
-		if (access(COMMAND_DIR, R_OK | X_OK) == -1)
-			die("Sorry, the interactive git-shell is not enabled");
+		if (access(COMMAND_DIR, R_OK | X_OK) == -1) {
+			die("Interactive git shell is not enabled.\n"
+			    "hint: ~/" COMMAND_DIR " should exist "
+			    "and have read and execute access.");
+		}
 		run_shell();
 		exit(0);
 	} else if (argc != 3 || strcmp(argv[1], "-c")) {
