e6eedc31d5c3d448d000eecb678ce891a4800b54
Jon Seymour <jon.seymour@gmail.com>
2010-08-24 14:52:42 +1000

    rev-parse: exit with non-zero status if ref@{n} is not valid.
    "The current behaviour of ref@{...} syntax parser is suboptimal:
    
        $ git rev-parse --verify jch@{99999} && echo true
        warning: Log for 'jch' only has 1368 entries.
        cfb88e9a8d4926b0011ae2dd67e1f57a98f4b768
        true
    
    It even knows that it is running off the cut-off point; it should just
    cause the caller to notice that fact.  I don't think changing it to error
    out should cause any harm to existing callers."
    
    With this change:
    
       $ git rev-parse --verify jch@{99999} || echo false
       fatal: Log for 'jch' only has 1368 entries.
       false
    
       $ git rev-parse jch@{99999} || echo false
       fatal: Log for 'jch' only has 1368 entries.
       false
    
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_name.c
+++ sha1_name.c
@@ -430,10 +430,11 @@ static int get_sha1_basic(const char *str, int len, unsigned char *sha1)
 					"warning: Log for '%.*s' only goes "
 					"back to %s.\n", len, str,
 					show_date(co_time, co_tz, DATE_RFC2822));
-			else
-				fprintf(stderr,
-					"warning: Log for '%.*s' only has "
-					"%d entries.\n", len, str, co_cnt);
+			else {
+				free(real_ref);
+				die("Log for '%.*s' only has %d entries.",
+				    len, str, co_cnt);
+			}
 		}
 	}
 
