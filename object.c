55b4e9e43227632dd372f916c08dca6e26118af5
Jared Hance <jaredhance@gmail.com>
2010-09-05 15:36:33 -0400

    Fix whitespace issue in object.c
    Change some expanded tabs (spaces) to tabs in object.c.
    
    Signed-off-by: Jared Hance <jaredhance@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- object.c
+++ object.c
@@ -211,10 +211,10 @@ struct object_list *object_list_insert(struct object *item,
 				       struct object_list **list_p)
 {
 	struct object_list *new_list = xmalloc(sizeof(struct object_list));
-        new_list->item = item;
-        new_list->next = *list_p;
-        *list_p = new_list;
-        return new_list;
+	new_list->item = item;
+	new_list->next = *list_p;
+	*list_p = new_list;
+	return new_list;
 }
 
 void object_list_append(struct object *item,
