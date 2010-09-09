5418d96ddca8134b5abeb99430f61c062d91f722
Ramsay Jones <ramsay@ramsay1.demon.co.uk>
2010-09-09 18:24:06 +0100

    vcs-svn: Fix some printf format compiler warnings
    In particular, on systems that define uint32_t as an unsigned long,
    gcc complains as follows:
    
          CC vcs-svn/fast_export.o
      vcs-svn/fast_export.c: In function `fast_export_modify':
      vcs-svn/fast_export.c:28: warning: unsigned int format, uint32_t arg (arg 2)
      vcs-svn/fast_export.c:28: warning: int format, uint32_t arg (arg 3)
      vcs-svn/fast_export.c: In function `fast_export_commit':
      vcs-svn/fast_export.c:42: warning: int format, uint32_t arg (arg 5)
      vcs-svn/fast_export.c:62: warning: int format, uint32_t arg (arg 2)
      vcs-svn/fast_export.c: In function `fast_export_blob':
      vcs-svn/fast_export.c:72: warning: int format, uint32_t arg (arg 2)
      vcs-svn/fast_export.c:72: warning: int format, uint32_t arg (arg 3)
          CC vcs-svn/svndump.o
      vcs-svn/svndump.c: In function `svndump_read':
      vcs-svn/svndump.c:260: warning: int format, uint32_t arg (arg 3)
    
    In order to suppress the warnings we use the C99 format specifier
    macros PRIo32 and PRIu32 from <inttypes.h>.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsay1.demon.co.uk>
    Acked-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- vcs-svn/svndump.c
+++ vcs-svn/svndump.c
@@ -257,7 +257,7 @@ void svndump_read(const char *url)
 				handle_node();
 				active_ctx = REV_CTX;
 			} else {
-				fprintf(stderr, "Unexpected content length header: %d\n", len);
+				fprintf(stderr, "Unexpected content length header: %"PRIu32"\n", len);
 				buffer_skip_bytes(len);
 			}
 		}
