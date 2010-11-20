ab389d67592da8b540168bd3e2e4f30f05907646
Frank Li <lznuaa@gmail.com>
2010-11-20 15:07:45 +0800

    Export write_entry to checkout one file at gitdll
    Signed-off-by: Frank Li <lznuaa@gmail.com>

--- entry.c
+++ entry.c
@@ -91,7 +91,7 @@ static void *read_blob_entry(struct cache_entry *ce, unsigned long *size)
 	return NULL;
 }
 
-static int write_entry(struct cache_entry *ce, char *path, const struct checkout *state, int to_tempfile)
+int write_entry(struct cache_entry *ce, char *path, const struct checkout *state, int to_tempfile)
 {
 	unsigned int ce_mode_s_ifmt = ce->ce_mode & S_IFMT;
 	int fd, ret, fstat_done = 0;
