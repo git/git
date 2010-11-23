599b0bf438e387dbaf00dbadcbe41b2a0de90db1
Erik Faye-Lund <kusmabite@gmail.com>
2010-11-23 19:38:24 +0100

    msvc: opendir: fix malloc-failure
    Previsouly, the code checked for malloc-failure after it had accessed
    the returned pointer. Move the check a bit earlier to avoid segfault.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/msvc.c
+++ compat/msvc.c
@@ -7,15 +7,15 @@ DIR *opendir(const char *name)
 {
 	int len;
 	DIR *p;
-	p = (DIR*)malloc(sizeof(DIR));
+	p = malloc(sizeof(DIR));
+	if (!p)
+		return NULL;
+
 	memset(p, 0, sizeof(DIR));
 	strncpy(p->dd_name, name, PATH_MAX);
 	len = strlen(p->dd_name);
 	p->dd_name[len] = '/';
 	p->dd_name[len+1] = '*';
-
-	if (p == NULL)
-		return NULL;
 
 	p->dd_handle = _findfirst(p->dd_name, &p->dd_dta);
 
