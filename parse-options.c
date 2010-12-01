c1f4ec9ef45232d6dbdea4c417a9d41eb8ad7f4f
Stephen Boyd <bebarino@gmail.com>
2010-12-01 17:30:40 -0600

    parse-options: do not infer PARSE_OPT_NOARG from option type
    Simplify the "takes no value" error path by relying on PARSE_OPT_NOARG
    being set correctly.  That is:
    
     - if the PARSE_OPT_NOARG flag is set, reject --opt=value
       regardless of the option type;
     - if the PARSE_OPT_NOARG flag is unset, accept --opt=value
       regardless of the option type.
    
    This way, the accepted usage more closely matches the usage advertised
    with --help-all.
    
    No functional change intended, since the NOARG flag is only used
    with "boolean-only" option types in existing parse_options callers.
    
    Signed-off-by: Stephen Boyd <bebarino@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.c
+++ parse-options.c
@@ -62,23 +62,8 @@ static int get_value(struct parse_opt_ctx_t *p,
 		return opterror(opt, "takes no value", flags);
 	if (unset && (opt->flags & PARSE_OPT_NONEG))
 		return opterror(opt, "isn't available", flags);
-
-	if (!(flags & OPT_SHORT) && p->opt) {
-		switch (opt->type) {
-		case OPTION_CALLBACK:
-			if (!(opt->flags & PARSE_OPT_NOARG))
-				break;
-			/* FALLTHROUGH */
-		case OPTION_BOOLEAN:
-		case OPTION_BIT:
-		case OPTION_NEGBIT:
-		case OPTION_SET_INT:
-		case OPTION_SET_PTR:
-			return opterror(opt, "takes no value", flags);
-		default:
-			break;
-		}
-	}
+	if (!(flags & OPT_SHORT) && p->opt && (opt->flags & PARSE_OPT_NOARG))
+		return opterror(opt, "takes no value", flags);
 
 	switch (opt->type) {
 	case OPTION_BIT:
