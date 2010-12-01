bd40d252ec1ed2716ac9e6bbeab48b3b40bd0d58
knittl <knittl89@googlemail.com>
2010-12-01 14:17:00 +0100

    bash: Match lightweight tags in prompt
    The bash prompt would display a commit's object name when having checked
    out a lightweight tag.  Provide `--tags` to `git describe` in the completion
    script, so it will display lightweight tag names, as it already does for
    annotated tags.
    
    Signed-off-by: Daniel Knittl-Frank <knittl89+git@googlemail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -255,7 +255,7 @@ __git_ps1 ()
 				(describe)
 					git describe HEAD ;;
 				(* | default)
-					git describe --exact-match HEAD ;;
+					git describe --tags --exact-match HEAD ;;
 				esac 2>/dev/null)" ||
 
 				b="$(cut -c1-7 "$g/HEAD" 2>/dev/null)..." ||
