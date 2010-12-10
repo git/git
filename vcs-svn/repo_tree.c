d38f84484f21e7e509ff009d3a17167c9c09f893
Jonathan Nieder <jrnieder@gmail.com>
2010-12-10 04:21:35 -0600

    vcs-svn: use higher mark numbers for blobs
    Prepare to use mark :5 for the commit corresponding to r5 (and so on).
    
    1 billion seems sufficiently high for blob marks to avoid conflicting
    with rev marks, while still leaving room for 3 billion blobs.  Such
    high mark numbers cause trouble with ancient fast-import versions, but
    this topic cannot support git fast-import versions before 1.7.4 (which
    introduces the cat-blob command) anyway.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/repo_tree.c
+++ vcs-svn/repo_tree.c
@@ -292,7 +292,7 @@ void repo_commit(uint32_t revision, uint32_t author, char *log, uint32_t uuid,
 static void mark_init(void)
 {
 	uint32_t i;
-	mark = 0;
+	mark = 1024 * 1024 * 1024;
 	for (i = 0; i < dent_pool.size; i++)
 		if (!repo_dirent_is_dir(dent_pointer(i)) &&
 		    dent_pointer(i)->content_offset > mark)
