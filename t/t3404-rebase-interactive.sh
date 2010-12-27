5cd3e10737135163f47e97e0338dbf33f4772511
Robin H. Johnson <robbat2@gentoo.org>
2010-12-27 08:03:43 +0000

    Fix false positives in t3404 due to SHELL=/bin/false
    If the user's shell in NSS passwd is /bin/false (eg as found during Gentoo's
    package building), the git-rebase exec tests will fail, because they call
    $SHELL around the command, and in the existing testcase, $SHELL was not being
    cleared sufficently.
    
    This lead to false positive failures of t3404 on systems where the package
    build user was locked down as noted above.
    
    Signed-off-by: "Robin H. Johnson" <robbat2@gentoo.org>
    X-Gentoo-Bug: 349083
    X-Gentoo-Bug-URL: http://bugs.gentoo.org/show_bug.cgi?id=349083
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -67,8 +67,9 @@ test_expect_success 'setup' '
 # "exec" commands are ran with the user shell by default, but this may
 # be non-POSIX. For example, if SHELL=zsh then ">file" doesn't work
 # to create a file. Unseting SHELL avoids such non-portable behavior
-# in tests.
+# in tests. It must be exported for it to take effect where needed.
 SHELL=
+export SHELL
 
 test_expect_success 'rebase -i with the exec command' '
 	git checkout master &&
