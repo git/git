9add71ee0e33f3e2f07314cebf7b87c823bafec7
Karsten Blees <blees@dcon.de>
2011-01-07 18:04:16 +0100

    Win32: fix potential multi-threading issue
    ...by removing a static buffer in do_stat_internal.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -475,7 +475,7 @@ static int do_lstat(int follow, const char *file_name, struct stat *buf)
 static int do_stat_internal(int follow, const char *file_name, struct stat *buf)
 {
 	int namelen;
-	static char alt_name[PATH_MAX];
+	char alt_name[PATH_MAX];
 
 	if (!do_lstat(follow, file_name, buf))
 		return 0;
