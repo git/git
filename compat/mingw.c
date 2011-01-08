b2c0b2756d1ad68500b9db0145327be623133ec3
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-01-08 01:46:41 +0100

    Fix old-style function declaration
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -1834,7 +1834,7 @@ pid_t waitpid(pid_t pid, int *status, int options)
 	return -1;
 }
 
-const char *get_windows_home_directory()
+const char *get_windows_home_directory(void)
 {
 	static const char *home_directory = NULL;
 	struct strbuf buf = STRBUF_INIT;
