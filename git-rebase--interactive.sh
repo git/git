960ac5ff99304404d287b8174d7c8a7743c6d97a
Jonathan Nieder <jrnieder@gmail.com>
2011-01-20 18:36:24 -0600

    rebase -i: clarify in-editor documentation of "exec"
    The hints in the current "instruction sheet" template look like so:
    
     # Rebase 3f14246..a1d7e01 onto 3f14246
     #
     # Commands:
     #  p, pick = use commit
     #  r, reword = use commit, but edit the commit message
     #  e, edit = use commit, but stop for amending
     #  s, squash = use commit, but meld into previous commit
     #  f, fixup = like "squash", but discard this commit's log message
     #  x <cmd>, exec <cmd> = Run a shell command <cmd>, and stop if it fails
     #
     # If you remove a line here THAT COMMIT WILL BE LOST.
     # However, if you remove everything, the rebase will be aborted.
     #
    
    This does not make it clear that the format of each line is
    
    	<insn> <commit id> <explanatory text that will be printed>
    
    but the reader will probably infer that from the automatically
    generated pick examples above it.
    
    What about the "exec" instruction?  By analogy, I might imagine that
    the format of that line is "exec <command> <explanatory text>", and
    the "x <cmd>" hint does not address that question (at first I read it
    as taking an argument <cmd> that is the name of a shell).  Meanwhile,
    the mention of <cmd> makes the hints harder to scan as a table.
    
    So remove the <cmd> and add some words to remind the reader that
    "exec" runs a command named by the rest of the line.  To make room, it
    is left to the manpage to explain that that command is run using
    $SHELL and that nonzero status from that command will pause the
    rebase.
    
    Wording from Junio.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-rebase--interactive.sh
+++ git-rebase--interactive.sh
@@ -997,7 +997,7 @@ first and then run 'git rebase --continue' again."
 #  e, edit = use commit, but stop for amending
 #  s, squash = use commit, but meld into previous commit
 #  f, fixup = like "squash", but discard this commit's log message
-#  x <cmd>, exec <cmd> = Run a shell command <cmd>, and stop if it fails
+#  x, exec = run command (the rest of the line) using shell
 #
 # If you remove a line here THAT COMMIT WILL BE LOST.
 # However, if you remove everything, the rebase will be aborted.
