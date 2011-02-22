08eaa4be0332e6f9f8732fbb99272b4bbf3ea3de
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:43 +0000

    i18n: git-checkout "Switched to a .. branch" message
    Split up the "Switched to and reset branch" and "Switched to a new
    branch" messages to make them easier to translate.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -546,16 +546,18 @@ static void update_refs_for_switch(struct checkout_opts *opts,
 	if (new->path) {
 		create_symref("HEAD", new->path, msg.buf);
 		if (!opts->quiet) {
-			if (old->path && !strcmp(new->path, old->path))
+			if (old->path && !strcmp(new->path, old->path)) {
 				fprintf(stderr, _("Already on '%s'\n"),
 					new->name);
-			else if (opts->new_branch)
-				fprintf(stderr, "Switched to%s branch '%s'\n",
-					opts->branch_exists ? " and reset" : " a new",
-					new->name);
-			else
+			} else if (opts->new_branch) {
+				if (opts->branch_exists)
+					fprintf(stderr, _("Switched to and reset branch '%s'\n"), new->name);
+				else
+					fprintf(stderr, _("Switched to a new branch '%s'\n"), new->name);
+			} else {
 				fprintf(stderr, _("Switched to branch '%s'\n"),
 					new->name);
+			}
 		}
 		if (old->path && old->name) {
 			char log_file[PATH_MAX], ref_file[PATH_MAX];
