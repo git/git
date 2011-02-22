b3b298afcbcb5cf5b497928fc9bcd17624112233
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:17 +0000

    i18n: git-status "Initial commit" message
    Gettextize the "# Initial commit" message. A test in t7501-commit.sh
    explicitly checked for this message. Change it to skip under
    GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.c
+++ wt-status.c
@@ -725,7 +725,7 @@ void wt_status_print(struct wt_status *s)
 
 	if (s->is_initial) {
 		status_printf_ln(s, color(WT_STATUS_HEADER, s), "");
-		status_printf_ln(s, color(WT_STATUS_HEADER, s), "Initial commit");
+		status_printf_ln(s, color(WT_STATUS_HEADER, s), _("Initial commit"));
 		status_printf_ln(s, color(WT_STATUS_HEADER, s), "");
 	}
 
