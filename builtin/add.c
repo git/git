475c73eb8c65fe7b4cb55f05aeb9eee547e4a359
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:32 +0000

    i18n: git-add "remove '%s'" message
    Make the "remove '%s'" message translatable. It's displayed under `git
    add -u --verbose`. Also skip the corresponding test when output is not
    in the C locale.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/add.c
+++ builtin/add.c
@@ -74,7 +74,7 @@ static void update_callback(struct diff_queue_struct *q,
 			if (!(data->flags & ADD_CACHE_PRETEND))
 				remove_file_from_index(&the_index, path);
 			if (data->flags & (ADD_CACHE_PRETEND|ADD_CACHE_VERBOSE))
-				printf("remove '%s'\n", path);
+				printf(_("remove '%s'\n"), path);
 			break;
 		}
 	}
