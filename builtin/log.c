f338cb83a0328b9fe4ec37dff0d4dcc3b8f3cde9
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:58 +0000

    i18n: git-log "--OPT does not make sense" messages
    Gettextize the "--name-only/--name-status/--check does not make sense"
    messages. A test in t4014-format-patch.sh explicitly checked for these
    messages. Change them to skip under GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/log.c
+++ builtin/log.c
@@ -1136,11 +1136,11 @@ int cmd_format_patch(int argc, const char **argv, const char *prefix)
 		die (_("unrecognized argument: %s"), argv[1]);
 
 	if (rev.diffopt.output_format & DIFF_FORMAT_NAME)
-		die("--name-only does not make sense");
+		die(_("--name-only does not make sense"));
 	if (rev.diffopt.output_format & DIFF_FORMAT_NAME_STATUS)
-		die("--name-status does not make sense");
+		die(_("--name-status does not make sense"));
 	if (rev.diffopt.output_format & DIFF_FORMAT_CHECKDIFF)
-		die("--check does not make sense");
+		die(_("--check does not make sense"));
 
 	if (!use_patch_format &&
 		(!rev.diffopt.output_format ||
