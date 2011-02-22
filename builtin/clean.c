a66f9b2afde8ae0d6bd8991e6a8f848904c8bdde
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:22 +0000

    i18n: git-clean clean.requireForce messages
    Split up the "clean.requireForce set/defaults to true..." die()
    message to make it easier to translate.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clean.c
+++ builtin/clean.c
@@ -77,9 +77,14 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 	if (ignored && ignored_only)
 		die(_("-x and -X cannot be used together"));
 
-	if (!show_only && !force)
-		die("clean.requireForce %s to true and neither -n nor -f given; "
-		    "refusing to clean", config_set ? "set" : "defaults");
+	if (!show_only && !force) {
+		if (config_set)
+			die(_("clean.requireForce set to true and neither -n nor -f given; "
+				  "refusing to clean"));
+		else
+			die(_("clean.requireForce defaults to true and neither -n nor -f given; "
+				  "refusing to clean"));
+	}
 
 	if (force > 1)
 		rm_flags = 0;
