157efde10f34344f0970ca71f33dd5ae1c29b6f5
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:02 +0000

    i18n: git-merge "Wonderful" message
    Gettextize the "Wonderful" message. A test in t7600-merge.sh
    explicitly checked for this message. Change it to skip under
    GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -803,7 +803,7 @@ static int merge_trivial(void)
 	struct commit_list *parent = xmalloc(sizeof(*parent));
 
 	write_tree_trivial(result_tree);
-	printf("Wonderful.\n");
+	printf(_("Wonderful.\n"));
 	parent->item = lookup_commit(head);
 	parent->next = xmalloc(sizeof(*parent->next));
 	parent->next->item = remoteheads->item;
