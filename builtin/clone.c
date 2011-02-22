3781fcce05c16dcb0714c817ecfabbe8e1ffe62c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:27 +0000

    i18n: git-clone "Cloning into" message
    Separate the "Cloning into %s" and "Cloning into bare repository %s"
    messages to make them easier to translate. No noticeable change
    intended.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -468,9 +468,12 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 		die(_("could not create leading directories of '%s'"), git_dir);
 	set_git_dir(make_absolute_path(git_dir));
 
-	if (0 <= option_verbosity)
-		printf("Cloning into %s%s...\n",
-		       option_bare ? "bare repository " : "", dir);
+	if (0 <= option_verbosity) {
+		if (option_bare)
+			printf("Cloning into bare repository %s...\n", dir);
+		else
+			printf("Cloning into %s...\n", dir);
+	}
 	init_db(option_template, INIT_DB_QUIET);
 
 	/*
