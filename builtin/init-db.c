3e5dd7e960953ad03790309c48406a8aa43cb026
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:25 +0000

    i18n: git-init "Initialized [...] repository" message
    These messages could benefit from splitting up.  An earlier version of
    this patch began like this:
    
        const char *reinit_shared   = _("Reinitialized existing shared Git repository in %s\n");
        const char *init_shared     = _("Initialized empty shared Git repository in %s\n");
        const char *reinit_noshared = _("Reinitialized existing Git repository in %s\n");
        const char *init_noshared   = _("Initialized empty Git repository in %s\n");
    
    But in the first round of gettextization I'm aiming to keep code
    changes to a minimum for ease of review.  So just add a comment
    explaining to translators how the sprintf format gets used so they
    can cope for now if the language's grammar allows.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/init-db.c
+++ builtin/init-db.c
@@ -354,9 +354,15 @@ int init_db(const char *template_dir, unsigned int flags)
 	if (!(flags & INIT_DB_QUIET)) {
 		const char *git_dir = get_git_dir();
 		int len = strlen(git_dir);
-		printf("%s%s Git repository in %s%s\n",
-		       reinit ? "Reinitialized existing" : "Initialized empty",
-		       shared_repository ? " shared" : "",
+
+		/*
+		 * TRANSLATORS: The first '%s' is either "Reinitialized
+		 * existing" or "Initialized empty", the second " shared" or
+		 * "", and the last '%s%s' is the verbatim directory name.
+		 */
+		printf(_("%s%s Git repository in %s%s\n"),
+		       reinit ? _("Reinitialized existing") : _("Initialized empty"),
+		       shared_repository ? _(" shared") : "",
 		       git_dir, len && git_dir[len-1] != '/' ? "/" : "");
 	}
 
