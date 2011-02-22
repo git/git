9c227655b02c8f84e319a57bb8dd7cff63e3fbf8
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:45 +0000

    i18n: git-commit "middle of a merge" message
    Gettextize the "You are in the middle of a merge -- cannot amend."
    message. Several tests in t7110-reset-merge.sh explicitly checked for
    this message. Change them to skip under GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -928,9 +928,9 @@ static int parse_and_validate_options(int argc, const char *argv[],
 	if (amend && initial_commit)
 		die(_("You have nothing to amend."));
 	if (amend && in_merge)
-		die("You are in the middle of a merge -- cannot amend.");
+		die(_("You are in the middle of a merge -- cannot amend."));
 	if (fixup_message && squash_message)
-		die("Options --squash and --fixup cannot be used together");
+		die(_("Options --squash and --fixup cannot be used together"));
 	if (use_message)
 		f++;
 	if (edit_message)
