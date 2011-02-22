48168851cffcd14e01d3a10473a02ca4b4479402
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:41:31 +0000

    i18n: git-add "did not match any files" message
    Make the "did not match any files" message translatable, and skip the
    test that checks for it when the C_LOCALE_OUTPUT prereq is not
    present.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/add.c
+++ builtin/add.c
@@ -451,7 +451,7 @@ int cmd_add(int argc, const char **argv, const char *prefix)
 					if (excluded(&dir, pathspec[i], &dtype))
 						dir_add_ignored(&dir, pathspec[i], strlen(pathspec[i]));
 				} else
-					die("pathspec '%s' did not match any files",
+					die(_("pathspec '%s' did not match any files"),
 					    pathspec[i]);
 			}
 		}
