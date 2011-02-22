2ceb61e0a030eddd46cc62e4d9a0fbf2d16faad1
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:00 +0000

    i18n: git-merge "Updating %s..%s" message
    Gettextize the "Updating %s..%s\n" message. A test in
    t1200-tutorial.sh explicitly checked for this message. Split it into
    two tests to skip the test_cmp test under GETTEXT_POISON=YesPlease.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -1124,7 +1124,7 @@ int cmd_merge(int argc, const char **argv, const char *prefix)
 		strcpy(hex, find_unique_abbrev(head, DEFAULT_ABBREV));
 
 		if (verbosity >= 0)
-			printf("Updating %s..%s\n",
+			printf(_("Updating %s..%s\n"),
 				hex,
 				find_unique_abbrev(remoteheads->item->object.sha1,
 				DEFAULT_ABBREV));
