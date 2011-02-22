e3bd758335ccb2d46ddde628bf391d5155f87730
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-22 23:42:27 +0000

    i18n: git-notes GIT_NOTES_REWRITE_MODE error message
    Use sprintf format for the error message that's displayed if
    GIT_NOTES_REWRITE_MODE is invalid, and leave a note in a TRANSLATORS
    comment indicating what the message means.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/notes.c
+++ builtin/notes.c
@@ -382,8 +382,10 @@ struct notes_rewrite_cfg *init_copy_notes_for_rewrite(const char *cmd)
 		c->mode_from_env = 1;
 		c->combine = parse_combine_notes_fn(rewrite_mode_env);
 		if (!c->combine)
-			error("Bad " GIT_NOTES_REWRITE_MODE_ENVIRONMENT
-			      " value: '%s'", rewrite_mode_env);
+			/* TRANSLATORS: The first %s is the name of the
+			   environment variable, the second %s is its value */
+			error(_("Bad %s value: '%s'"), GIT_NOTES_REWRITE_MODE_ENVIRONMENT,
+					rewrite_mode_env);
 	}
 	if (rewrite_refs_env) {
 		c->refs_from_env = 1;
