62270f6b0a4e67e5044fb0d70d0e82ac6e553eb2
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-02-25 19:29:42 -0800

    branch_merged: fix grammar in warning
    Signed-off-by: Eric Hanchrow <eric.hanchrow@gmail.com>
    Helped-by: Jay Soffian <jaysoffian@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/branch.c
+++ builtin/branch.c
@@ -134,7 +134,7 @@ static int branch_merged(int kind, const char *name,
 	    in_merge_bases(rev, &head_rev, 1) != merged) {
 		if (merged)
 			warning("deleting branch '%s' that has been merged to\n"
-				"         '%s', but it is not yet merged to HEAD.",
+				"         '%s', but not yet been merged to HEAD.",
 				name, reference_name);
 		else
 			warning("not deleting branch '%s' that is not yet merged to\n"
