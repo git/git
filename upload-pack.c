49bee717f7080f99d3405682cc43ad525dde0973
Shawn O. Pearce <spearce@spearce.org>
2011-03-14 16:48:39 -0700

    upload-pack: More aggressively send 'ACK %s ready'
    If a client is merely following the remote (and has not made any
    new commits itself), all "have %s" lines sent by the client will be
    common to the server.  As all lines are common upload-pack never
    calls ok_to_give_up() and does not compute if it has a good cut
    point in the commit graph.
    
    Without this computation the following client is going to send all
    tagged commits, as these were determined to be COMMON_REF during the
    initial advertisement, but the client does not parse their history
    to transitively pass the COMMON flag and empty its queue of commits.
    
    For git.git with 339 commit tags, it takes clients 11 rounds of
    negotation to fully send all tagged commits and exhaust its queue
    of things to send as common.  This is pretty slow for a client that
    has not done any local development activity.
    
    Force computing ok_to_give_up() and send "ACK %s ready" at the end
    of the current round if this round only contained common objects
    and ok_to_give_up() was therefore not called.  This may allow the
    client to break early, avoiding transmission of the COMMON_REFs.
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- upload-pack.c
+++ upload-pack.c
@@ -429,6 +429,8 @@ static int get_common_commits(void)
 	static char line[1000];
 	unsigned char sha1[20];
 	char last_hex[41];
+	int got_common = 0;
+	int got_other = 0;
 
 	save_commit_buffer = 0;
 
@@ -437,16 +439,22 @@ static int get_common_commits(void)
 		reset_timeout();
 
 		if (!len) {
+			if (multi_ack == 2 && got_common
+			    && !got_other && ok_to_give_up())
+				packet_write(1, "ACK %s ready\n", last_hex);
 			if (have_obj.nr == 0 || multi_ack)
 				packet_write(1, "NAK\n");
 			if (stateless_rpc)
 				exit(0);
+			got_common = 0;
+			got_other = 0;
 			continue;
 		}
 		strip(line, len);
 		if (!prefixcmp(line, "have ")) {
 			switch (got_sha1(line+5, sha1)) {
 			case -1: /* they have what we do not */
+				got_other = 1;
 				if (multi_ack && ok_to_give_up()) {
 					const char *hex = sha1_to_hex(sha1);
 					if (multi_ack == 2)
@@ -456,6 +464,7 @@ static int get_common_commits(void)
 				}
 				break;
 			default:
+				got_common = 1;
 				memcpy(last_hex, sha1_to_hex(sha1), 41);
 				if (multi_ack == 2)
 					packet_write(1, "ACK %s common\n", last_hex);
