41e6b91f01bc9bb7e1679542a8cce9bd4252fd2e
Jonathan Nieder <jrnieder@gmail.com>
2011-03-27 12:19:14 -0500

    vcs-svn: add missing cast to printf argument
    gcc -m32 correctly warns:
    
     vcs-svn/fast_export.c: In function 'fast_export_commit':
     vcs-svn/fast_export.c:54:2: warning: format '%llu' expects
       argument of type 'long long unsigned int', but argument 2
       has type 'unsigned int' [-Wformat]
    
    Fix it.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- vcs-svn/fast_export.c
+++ vcs-svn/fast_export.c
@@ -51,7 +51,8 @@ void fast_export_commit(uint32_t revision, const char *author,
 		   *author ? author : "nobody",
 		   *author ? author : "nobody",
 		   *uuid ? uuid : "local", timestamp);
-	printf("data %"PRIuMAX"\n", log->len + strlen(gitsvnline));
+	printf("data %"PRIuMAX"\n",
+		(uintmax_t) (log->len + strlen(gitsvnline)));
 	fwrite(log->buf, log->len, 1, stdout);
 	printf("%s\n", gitsvnline);
 	if (!first_commit_done) {
