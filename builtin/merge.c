650ed9db74188654656afcd38b1d7672161cb90b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-04-10 19:34:03 +0000

    i18n: mark merge "Could not read from" message for translation
    Mark the "Could not read from '%s'" message that was added to
    builtin/merge.c in v1.7.4.2~25^2 (merge: honor prepare-commit-msg
    hook) by Jay Soffian for translation.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -822,7 +822,7 @@ static void read_merge_msg(void)
 {
 	strbuf_reset(&merge_msg);
 	if (strbuf_read_file(&merge_msg, git_path("MERGE_MSG"), 0) < 0)
-		die_errno("Could not read from '%s'", git_path("MERGE_MSG"));
+		die_errno(_("Could not read from '%s'"), git_path("MERGE_MSG"));
 }
 
 static void run_prepare_commit_msg(void)
