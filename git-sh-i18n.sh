64a42951471b03c0e25f9e632973e17e2cc2a758
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-05-14 13:47:44 +0000

    git-sh-i18n.sh: add GIT_GETTEXT_POISON support
    Change git-sh-i18n.sh to support the GIT_GETTEXT_POISON environment
    variable like gettext.c does, this ensures that tests that use
    git-sh-i18n.sh will fail under GETTEXT_POISON=YesPlease if they rely
    on Git's C locale messages without declaring that they do.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-sh-i18n.sh
+++ git-sh-i18n.sh
@@ -5,13 +5,25 @@
 # This is a skeleton no-op implementation of gettext for Git. It'll be
 # replaced by something that uses gettext.sh in a future patch series.
 
-gettext () {
-	printf "%s" "$1"
-}
+if test -z "$GIT_GETTEXT_POISON"
+then
+	gettext () {
+		printf "%s" "$1"
+	}
 
-eval_gettext () {
-	printf "%s" "$1" | (
-		export PATH $(git sh-i18n--envsubst --variables "$1");
-		git sh-i18n--envsubst "$1"
-	)
-}
+	eval_gettext () {
+		printf "%s" "$1" | (
+			export PATH $(git sh-i18n--envsubst --variables "$1");
+			git sh-i18n--envsubst "$1"
+		)
+	}
+else
+	gettext () {
+		printf "%s" "# GETTEXT POISON #"
+	}
+
+	eval_gettext () {
+		printf "%s" "# GETTEXT POISON #"
+	}
+fi
+
