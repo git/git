61432146ad707e969664c0151364e2d8da9fb157
Junio C Hamano <gitster@pobox.com>
2011-05-16 16:19:49 -0700

    sideband_demux(): fix decl-after-stmt
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-send-pack.c
+++ builtin-send-pack.c
@@ -374,11 +374,11 @@ static void print_helper_status(struct ref *ref)
 
 static int sideband_demux(int in, int out, void *data)
 {
-	int *fd = data;
+	int *fd = data, ret;
 #ifndef WIN32
 	close(fd[1]);
 #endif
-	int ret = recv_sideband("send-pack", fd[0], out);
+	ret = recv_sideband("send-pack", fd[0], out);
 	close(out);
 	return ret;
 }
