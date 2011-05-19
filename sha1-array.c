cff38a5e11217dc997c1ffae8d0856023e05554a
Jeff King <peff@peff.net>
2011-05-19 17:34:46 -0400

    receive-pack: eliminate duplicate .have refs
    When receiving a push, we advertise ref tips from any
    alternate repositories, in case that helps the client send a
    smaller pack. Since these refs don't actually exist in the
    destination repository, we don't transmit the real ref
    names, but instead use the pseudo-ref ".have".
    
    If your alternate has a large number of duplicate refs (for
    example, because it is aggregating objects from many related
    repositories, some of which will have the same tags and
    branch tips), then we will send each ".have $sha1" line
    multiple times. This is a pointless waste of bandwidth, as
    we are simply repeating the same fact to the client over and
    over.
    
    This patch eliminates duplicate .have refs early on. It does
    so efficiently by sorting the complete list and skipping
    duplicates. This has the side effect of re-ordering the
    .have lines by ascending sha1; this isn't a problem, though,
    as the original order was meaningless.
    
    There is a similar .have system in fetch-pack, but it
    does not suffer from the same problem. For each alternate
    ref we consider in fetch-pack, we actually open the object
    and mark it with the SEEN flag, so duplicates are
    automatically culled.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1-array.c
+++ sha1-array.c
@@ -41,3 +41,19 @@ void sha1_array_clear(struct sha1_array *array)
 	array->alloc = 0;
 	array->sorted = 0;
 }
+
+void sha1_array_for_each_unique(struct sha1_array *array,
+				for_each_sha1_fn fn,
+				void *data)
+{
+	int i;
+
+	if (!array->sorted)
+		sha1_array_sort(array);
+
+	for (i = 0; i < array->nr; i++) {
+		if (i > 0 && !hashcmp(array->sha1[i], array->sha1[i-1]))
+			continue;
+		fn(array->sha1[i], data);
+	}
+}
--- sha1-array.h
+++ sha1-array.h
@@ -15,4 +15,10 @@ void sha1_array_sort(struct sha1_array *array);
 int sha1_array_lookup(struct sha1_array *array, const unsigned char *sha1);
 void sha1_array_clear(struct sha1_array *array);
 
+typedef void (*for_each_sha1_fn)(const unsigned char sha1[20],
+				 void *data);
+void sha1_array_for_each_unique(struct sha1_array *array,
+				for_each_sha1_fn fn,
+				void *data);
+
 #endif /* SHA1_ARRAY_H */
