0c811a7a6f93691604d48b1c13a6868c89971528
Jeff King <peff@peff.net>
2011-06-11 19:04:09 +0000

    limit "contains" traversals based on commit timestamp
    When looking for commits that contain other commits (e.g.,
    via "git tag --contains"), we can end up traversing useless
    portions of the graph. For example, if I am looking for a
    tag that contains a commit made last week, there is not much
    point in traversing portions of the history graph made five
    years ago.
    
    This optimization can provide massive speedups. For example,
    doing "git tag --contains HEAD~200" in the linux-2.6
    repository goes from:
    
      real    0m5.302s
      user    0m5.116s
      sys     0m0.184s
    
    to:
    
      real    0m0.030s
      user    0m0.020s
      sys     0m0.008s
    
    The downside is that we will no longer find some answers in
    the face of extreme clock skew, as we will stop the
    traversal early when seeing commits skewed too far into the
    past.
    
    Name-rev already implements a similar optimization, using a
    "slop" of one day to allow for a certain amount of clock
    skew in commit timestamps. This patch introduces a
    "core.clockskew" variable, which allows specifying the
    allowable amount of clock skew in seconds.  For safety, it
    defaults to "none", causing a full traversal (i.e., no
    change in behavior from previous versions).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/tag.c
+++ builtin/tag.c
@@ -25,6 +25,8 @@ static const char * const git_tag_usage[] = {
 
 static char signingkey[1000];
 
+static int core_clock_skew = -1;
+
 struct tag_filter {
 	const char *pattern;
 	int lines;
@@ -42,7 +44,8 @@ static int in_commit_list(const struct commit_list *want, struct commit *c)
 }
 
 static int contains_recurse(struct commit *candidate,
-			    const struct commit_list *want)
+			    const struct commit_list *want,
+			    unsigned long cutoff)
 {
 	struct commit_list *p;
 
@@ -59,9 +62,13 @@ static int contains_recurse(struct commit *candidate,
 	if (parse_commit(candidate) < 0)
 		return 0;
 
+	/* stop searching if we go too far back in time */
+	if (candidate->date < cutoff)
+		return 0;
+
 	/* Otherwise recurse and mark ourselves for future traversals. */
 	for (p = candidate->parents; p; p = p->next) {
-		if (contains_recurse(p->item, want)) {
+		if (contains_recurse(p->item, want, cutoff)) {
 			candidate->object.flags |= TMP_MARK;
 			return 1;
 		}
@@ -72,7 +79,22 @@ static int contains_recurse(struct commit *candidate,
 
 static int contains(struct commit *candidate, const struct commit_list *want)
 {
-	return contains_recurse(candidate, want);
+	unsigned long cutoff = 0;
+
+	if (core_clock_skew >= 0) {
+		const struct commit_list *c;
+		unsigned long min_date = ULONG_MAX;
+		for (c = want; c; c = c->next) {
+			if (parse_commit(c->item) < 0)
+				continue;
+			if (c->item->date < min_date)
+				min_date = c->item->date;
+		}
+		if (min_date > core_clock_skew)
+			cutoff = min_date - core_clock_skew;
+	}
+
+	return contains_recurse(candidate, want, cutoff);
 }
 
 static int show_reference(const char *refname, const unsigned char *sha1,
@@ -276,6 +298,14 @@ static int git_tag_config(const char *var, const char *value, void *cb)
 		if (!value)
 			return config_error_nonbool(var);
 		set_signingkey(value);
+		return 0;
+	}
+
+	if (!strcmp(var, "core.clockskew")) {
+		if (!value || !strcmp(value, "none"))
+			core_clock_skew = -1;
+		else
+			core_clock_skew = git_config_int(var, value);
 		return 0;
 	}
 
