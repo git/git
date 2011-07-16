8b5900751a5f19257dc9570dd195aaac46528599
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2011-07-17 03:25:52 +0530

    git-config: Remove extra whitespaces
    Remove extra whitespaces introduced by commits
    01ebb9dc and fc1905bb
    
    Signed-off-by: Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -1479,10 +1479,10 @@ int git_config_rename_section(const char *old_name, const char *new_name)
 		}
 	}
 	fclose(config_file);
- unlock_and_out:
+unlock_and_out:
 	if (commit_lock_file(lock) < 0)
 		ret = error("could not commit config file %s", config_filename);
- out:
+out:
 	free(config_filename);
 	return ret;
 }
