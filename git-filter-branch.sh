9dcca58db42e85e056c6523429204f367a671a0e
Junio C Hamano <gitster@pobox.com>
2011-08-05 15:06:21 -0700

    filter-branch.sh: de-dent usage string
    "Usage: git filter-branch " that is prefixed to the first line is 25
    columns long, so the "[--index-filter ..." on the second line would not
    align with "[--env-filter ..." on the first line to begin with. If the
    second and subsequent lines do not aim to align with anything on the
    first line, it is just fine to indent them with a single HT.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-filter-branch.sh
+++ git-filter-branch.sh
@@ -98,11 +98,11 @@ set_ident () {
 }
 
 USAGE="[--env-filter <command>] [--tree-filter <command>]
-	    [--index-filter <command>] [--parent-filter <command>]
-	    [--msg-filter <command>] [--commit-filter <command>]
-	    [--tag-name-filter <command>] [--subdirectory-filter <directory>]
-	    [--original <namespace>] [-d <directory>] [-f | --force]
-	    [<rev-list options>...]"
+	[--index-filter <command>] [--parent-filter <command>]
+	[--msg-filter <command>] [--commit-filter <command>]
+	[--tag-name-filter <command>] [--subdirectory-filter <directory>]
+	[--original <namespace>] [-d <directory>] [-f | --force]
+	[<rev-list options>...]"
 
 OPTIONS_SPEC=
 . git-sh-setup
