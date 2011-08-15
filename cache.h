abc06822af57347d35c0cd64b67e1faeb5b65c71
Fredrik Gustafsson <iveqy@iveqy.com>
2011-08-15 23:17:46 +0200

    rev-parse: add option --resolve-git-dir <path>
    Check if <path> is a valid git-dir or a valid git-file that points
    to a valid git-dir.
    
    We want tests to be independent from the fact that a git-dir may
    be a git-file. Thus we changed tests to use this feature.
    
    Signed-off-by: Fredrik Gustafsson <iveqy@iveqy.com>
    Mentored-by: Jens Lehmann <Jens.Lehmann@web.de>
    Mentored-by: Heiko Voigt <hvoigt@hvoigt.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -436,6 +436,7 @@ extern char *get_graft_file(void);
 extern int set_git_dir(const char *path);
 extern const char *get_git_work_tree(void);
 extern const char *read_gitfile_gently(const char *path);
+extern const char *resolve_gitdir(const char *suspect);
 extern void set_git_work_tree(const char *tree);
 
 #define ALTERNATE_DB_ENVIRONMENT "GIT_ALTERNATE_OBJECT_DIRECTORIES"
