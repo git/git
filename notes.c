885b797a4abe1fdfe7c4273caa34207d6e5e4760
Michael Haggerty <mhagger@alum.mit.edu>
2011-08-22 12:33:52 +0200

    Do not use C++-style comments
    Detected by "gcc -std=iso9899:1990 ...".  This patch applies against
    "maint".
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes.c
+++ notes.c
@@ -1105,7 +1105,7 @@ int remove_note(struct notes_tree *t, const unsigned char *object_sha1)
 	hashcpy(l.key_sha1, object_sha1);
 	hashclr(l.val_sha1);
 	note_tree_remove(t, t->root, 0, &l);
-	if (is_null_sha1(l.val_sha1)) // no note was removed
+	if (is_null_sha1(l.val_sha1)) /* no note was removed */
 		return 1;
 	t->dirty = 1;
 	return 0;
