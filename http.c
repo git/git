8abc5082222b7f65e07453a0aca46ce6d25ce3a5
Jonathan Nieder <jrnieder@gmail.com>
2011-09-05 17:29:34 -0500

    http: remove extra newline in error message
    There is no need for a blank line between the detailed error message
    and the later "fatal: HTTP request failed" notice.  Keep the newline
    written by error() itself and eliminate the extra one.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http.c
+++ http.c
@@ -903,7 +903,7 @@ int http_error(const char *url, int ret)
 {
 	/* http_request has already handled HTTP_START_FAILED. */
 	if (ret != HTTP_START_FAILED)
-		error("%s while accessing %s\n", curl_errorstr, url);
+		error("%s while accessing %s", curl_errorstr, url);
 
 	return ret;
 }
