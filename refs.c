b54cb795970628714a3a9fa0f4a73cd117b0207f
Michael Haggerty <mhagger@alum.mit.edu>
2011-09-15 23:10:32 +0200

    resolve_ref(): use prefixcmp()
    Terminate the link content string one step earlier, allowing
    prefixcmp() to be used instead of the less clear memcmp().
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -520,8 +520,8 @@ const char *resolve_ref(const char *ref, unsigned char *sha1, int reading, int *
 			len = readlink(path, buffer, sizeof(buffer)-1);
 			if (len < 0)
 				return NULL;
-			if (len >= 5 && !memcmp("refs/", buffer, 5)) {
-				buffer[len] = 0;
+			buffer[len] = 0;
+			if (!prefixcmp(buffer, "refs/")) {
 				strcpy(ref_buffer, buffer);
 				ref = ref_buffer;
 				if (flag)
