7b316a49b55a2cbe0321e72dce28a92126801d49
Sebastian Schuberth <sschuberth@gmail.com>
2011-10-18 18:20:35 +0200

    inet_ntop.c: Work around GCC 4.6's detection of uninitialized variables
    GCC 4.6 claims that
    
        error: 'best.len' may be used uninitialized in this function
    
    so silence that warning which is treated as an error by also initializing
    the "len" members of the struct.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- compat/inet_ntop.c
+++ compat/inet_ntop.c
@@ -98,7 +98,8 @@ inet_ntop6(const u_char *src, char *dst, size_t size)
 	for (i = 0; i < NS_IN6ADDRSZ; i++)
 		words[i / 2] |= (src[i] << ((1 - (i % 2)) << 3));
 	best.base = -1;
-	cur.base = -1;
+	best.len = 0;
+	cur = best;
 	for (i = 0; i < (NS_IN6ADDRSZ / NS_INT16SZ); i++) {
 		if (words[i] == 0) {
 			if (cur.base == -1)
