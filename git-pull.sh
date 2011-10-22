f29a12056b33d04a85f223e031f16d0f75786d57
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-10-21 23:27:09 -0500

    Teach 'git pull' to handle --rebase=interactive
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-pull.sh
+++ git-pull.sh
@@ -43,6 +43,7 @@ log_arg= verbosity= progress= recurse_submodules=
 merge_args= edit=
 curr_branch=$(git symbolic-ref -q HEAD)
 curr_branch_short="${curr_branch#refs/heads/}"
+rebase_options=
 rebase=$(git config --bool branch.$curr_branch_short.rebase)
 if test -z "$rebase"
 then
@@ -113,7 +114,12 @@ do
 	-r|--r|--re|--reb|--reba|--rebas|--rebase)
 		rebase=true
 		;;
+	--rebase=i|--rebase=interactive)
+		rebase_options=-i
+		rebase=true
+		;;
 	--no-r|--no-re|--no-reb|--no-reba|--no-rebas|--no-rebase)
+		rebase_options=
 		rebase=false
 		;;
 	--recurse-submodules)
@@ -278,7 +284,7 @@ fi
 merge_name=$(git fmt-merge-msg $log_arg <"$GIT_DIR/FETCH_HEAD") || exit
 case "$rebase" in
 true)
-	eval="git-rebase $diffstat $strategy_args $merge_args"
+	eval="git-rebase $rebase_options $diffstat $strategy_args $merge_args"
 	eval="$eval --onto $merge_head ${oldremoteref:-$merge_head}"
 	;;
 *)
