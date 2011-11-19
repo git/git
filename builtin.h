1bc01efed17c751e98fd6c5f9889759bfa9a8e9b
Jeff King <peff@peff.net>
2011-11-19 02:40:04 -0500

    upload-archive: use start_command instead of fork
    The POSIX-function fork is not supported on Windows. Use our
    start_command API instead, respawning ourselves in a special
    "writer" mode to follow the alternate code path.
    
    Remove the NOT_MINGW-prereq for t5000, as git-archive --remote
    now works.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin.h
+++ builtin.h
@@ -133,6 +133,7 @@ extern int cmd_update_index(int argc, const char **argv, const char *prefix);
 extern int cmd_update_ref(int argc, const char **argv, const char *prefix);
 extern int cmd_update_server_info(int argc, const char **argv, const char *prefix);
 extern int cmd_upload_archive(int argc, const char **argv, const char *prefix);
+extern int cmd_upload_archive_writer(int argc, const char **argv, const char *prefix);
 extern int cmd_upload_tar(int argc, const char **argv, const char *prefix);
 extern int cmd_var(int argc, const char **argv, const char *prefix);
 extern int cmd_verify_tag(int argc, const char **argv, const char *prefix);
