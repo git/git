534376ca04d524b99d69a30bdcf5e70ac8062aee
Jeff King <peff@peff.net>
2011-12-12 16:54:42 -0500

    mv: be quiet about overwriting
    When a user asks us to force a mv and overwrite the
    destination, we print a warning. However, since a typical
    use would be:
    
      $ git mv one two
      fatal: destination exists, source=one, destination=two
      $ git mv -f one two
      warning: overwriting 'two'
    
    this warning is just noise. We already know we're
    overwriting; that's why we gave -f!
    
    This patch silences the warning unless "--verbose" is given.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/mv.c
+++ builtin/mv.c
@@ -173,7 +173,8 @@ int cmd_mv(int argc, const char **argv, const char *prefix)
 				 * check both source and destination
 				 */
 				if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode)) {
-					warning(_("overwriting '%s'"), dst);
+					if (verbose)
+						warning(_("overwriting '%s'"), dst);
 					bad = NULL;
 				} else
 					bad = _("Cannot overwrite");
