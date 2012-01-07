98c2924cfa84a7f30b17636bd5632e53a0fa002e
Clemens Buchacher <drizzd@aon.at>
2012-01-07 12:54:36 +0100

    credentials: unable to connect to cache daemon
    Error out if we just spawned the daemon and yet we cannot connect.
    
    And always release the string buffer.
    
    Signed-off-by: Clemens Buchacher <drizzd@aon.at>
    Acked-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- credential-cache.c
+++ credential-cache.c
@@ -71,11 +71,10 @@ static void do_cache(const char *socket, const char *action, int timeout,
 			die_errno("unable to relay credential");
 	}
 
-	if (!send_request(socket, &buf))
-		return;
-	if (flags & FLAG_SPAWN) {
+	if (send_request(socket, &buf) < 0 && (flags & FLAG_SPAWN)) {
 		spawn_daemon(socket);
-		send_request(socket, &buf);
+		if (send_request(socket, &buf) < 0)
+			die_errno("unable to connect to cache daemon");
 	}
 	strbuf_release(&buf);
 }
