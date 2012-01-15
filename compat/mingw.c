e92cda2234d42e19970eb03f97064d8c1a7f0070
Karsten Blees <blees@dcon.de>
2012-01-15 02:35:26 +0100

    Win32: patch Windows environment on startup
    Fix Windows specific environment settings on startup rather than checking
    for special values on every getenv call.
    
    As a side effect, this makes the patched environment (i.e. with properly
    initialized TMPDIR and TERM) available to child processes.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -809,7 +809,7 @@ static int environ_size = 0;
 /* allocated size of environ array, in bytes */
 static int environ_alloc = 0;
 
-static char *do_getenv(const char *name)
+char *mingw_getenv(const char *name)
 {
 	char *value;
 	int pos = bsearchenv(environ, name, environ_size - 1);
@@ -817,22 +817,6 @@ static char *do_getenv(const char *name)
 		return NULL;
 	value = strchr(environ[pos], '=');
 	return value ? &value[1] : NULL;
-}
-
-char *mingw_getenv(const char *name)
-{
-	char *result = do_getenv(name);
-	if (!result && !strcmp(name, "TMPDIR")) {
-		/* on Windows it is TMP and TEMP */
-		result = do_getenv("TMP");
-		if (!result)
-			result = do_getenv("TEMP");
-	}
-	else if (!result && !strcmp(name, "TERM")) {
-		/* simulate TERM to enable auto-color (see color.c) */
-		result = "winansi";
-	}
-	return result;
 }
 
 int mingw_putenv(const char *namevalue)
@@ -2104,6 +2088,21 @@ void mingw_startup()
 
 	/* sort environment for O(log n) getenv / putenv */
 	qsort(environ, i, sizeof(char*), compareenv);
+
+	/* fix Windows specific environment settings */
+
+	/* on Windows it is TMP and TEMP */
+	if (!getenv("TMPDIR")) {
+		const char *tmp = getenv("TMP");
+		if (!tmp)
+			tmp = getenv("TEMP");
+		if (tmp)
+			setenv("TMPDIR", tmp, 1);
+	}
+
+	/* simulate TERM to enable auto-color (see color.c) */
+	if (!getenv("TERM"))
+		setenv("TERM", "winansi", 1);
 
 	/* initialize critical section for waitpid pinfo_t list */
 	InitializeCriticalSection(&pinfo_cs);
