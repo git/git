fcc0628adba795b1a6520cd3b81b8b1bc872e304
Karsten Blees <blees@dcon.de>
2012-03-01 21:53:54 +0100

    Win32: fix broken pipe detection
    As of "Win32: Thread-safe windows console output", git-log no longer
    terminates when the pager process dies. This is due to disabling buffering
    for the replaced stdout / stderr streams. Git-log will periodically fflush
    stdout (see write_or_die.c/mayble_flush_or_die()), but with no buffering,
    this is a NOP that always succeeds (so we never detect the EPIPE error).
    
    Exchange the original console handles with our console thread pipe handles
    by accessing the internal MSVCRT data structures directly (which are
    exposed via __pioinfo for some reason).
    
    Implement this with minimal assumptions about the actual data structure to
    make it work with different (hopefully even future) MSVCRT versions.
    
    While messing with internal data structures is ugly, this patch solves the
    problem at the source instead of adding more workarounds. We no longer need
    the special winansi_isatty override, and the limitations documented in
    "Win32: Thread-safe windows console output" are gone (i.e. fdopen(1/2)
    returns unbuffered streams now, and isatty() for duped console file
    descriptors works as expected).
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- compat/mingw.h
+++ compat/mingw.h
@@ -331,9 +331,7 @@ int mingw_raise(int sig);
  */
 
 void winansi_init(void);
-int winansi_isatty(int fd);
 HANDLE winansi_get_osfhandle(int fd);
-#define isatty winansi_isatty
 
 /*
  * git specific compatibility
