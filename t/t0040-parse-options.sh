38916c5b4740f6db09dc140a84bb470dfb582366
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2012-03-03 12:00:29 +0100

    parse-options: typo check for unknown switches
    The user specifies a long option but forgets to type the second
    leading dash, we currently detect and report that fact if its first
    letter is a valid short option. This is done for safety, to avoid
    ambiguity between short options (and their arguments) and a long
    option with a missing dash.
    
    This diagnostic message is also helpful for long options whose first
    letter is not a valid short option, however. Print it in that case,
    too, as a courtesy.
    
    Signed-off-by: Rene Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0040-parse-options.sh
+++ t/t0040-parse-options.sh
@@ -236,6 +236,16 @@ test_expect_success 'detect possible typos' '
 	test_cmp typo.err output.err
 '
 
+cat > typo.err << EOF
+error: did you mean \`--ambiguous\` (with two dashes ?)
+EOF
+
+test_expect_success 'detect possible typos' '
+	test_must_fail test-parse-options -ambiguous > output 2> output.err &&
+	test ! -s output &&
+	test_cmp typo.err output.err
+'
+
 cat > expect <<EOF
 boolean: 0
 integer: 0
