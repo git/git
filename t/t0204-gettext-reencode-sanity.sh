780cab1c6fab45d28110c995b64eb9372edbbad5
Torsten Bögershausen <tboegi@web.de>
2012-03-06 21:39:10 +0100

    t0204: remove a test that checks undefined behaviour
    The test "gettext: Emitting ISO-8859-1 from our UTF-8 *.mo files /
    Runes" asks the i18n machinery to show UTF-8 encoded text in a
    ISO-8859-1 locale with code points that don't fit into ISO-8859-1.
    
    This is asking for an impossible and observing the undefined
    behaviour, which is not a useful test.  Some systems convert these
    code points into question marks while others don't support any i18n
    and show the given template string back.  But there is no reason to
    insist that the fall back behaviour must be one of these two.
    
    Remove it.
    
    Signed-off-by: Torsten Bögershausen <tboegi@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0204-gettext-reencode-sanity.sh
+++ t/t0204-gettext-reencode-sanity.sh
@@ -26,20 +26,6 @@ test_expect_success GETTEXT_ISO_LOCALE 'gettext: Emitting ISO-8859-1 from our UT
     test_cmp expect actual
 '
 
-test_expect_success GETTEXT_ISO_LOCALE 'gettext: Emitting ISO-8859-1 from our UTF-8 *.mo files / Runes' '
-    LANGUAGE=is LC_ALL="$is_IS_iso_locale" gettext "TEST: Old English Runes" >runes &&
-
-	if grep "^TEST: Old English Runes$" runes
-	then
-		say "Your system can not handle this complexity and returns the string as-is"
-	else
-		# Both Solaris and GNU libintl will return this stream of
-		# question marks, so it is s probably portable enough
-		printf "TILRAUN: ?? ???? ??? ?? ???? ?? ??? ????? ??????????? ??? ?? ????" >runes-expect &&
-		test_cmp runes-expect runes
-	fi
-'
-
 test_expect_success GETTEXT_LOCALE 'gettext: Fetching a UTF-8 msgid -> UTF-8' '
     printf "TILRAUN: ‚einfaldar‘ og „tvöfaldar“ gæsalappir" >expect &&
     LANGUAGE=is LC_ALL="$is_IS_locale" gettext "TEST: ‘single’ and “double” quotes" >actual &&
