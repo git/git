090ea12671b2971b1c613f0a3d2657e8cdd35134
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-03-07 17:54:18 +0700

    parse_object: avoid putting whole blob in core
    Traditionally, all the callers of check_sha1_signature() first
    called read_sha1_file() to prepare the whole object data in core,
    and called this function.  The function is used to revalidate what
    we read from the object database actually matches the object name we
    used to ask for the data from the object database.
    
    Update the API to allow callers to pass NULL as the object data, and
    have the function read and hash the object data using streaming API
    to recompute the object name, without having to hold everything in
    core at the same time.  This is most useful in parse_object() that
    parses a blob object, because this caller does not have to keep the
    actual blob data around in memory after a "struct blob" is returned.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- object.c
+++ object.c
@@ -198,6 +198,17 @@ struct object *parse_object(const unsigned char *sha1)
 	if (obj && obj->parsed)
 		return obj;
 
+	if ((obj && obj->type == OBJ_BLOB) ||
+	    (!obj && has_sha1_file(sha1) &&
+	     sha1_object_info(sha1, NULL) == OBJ_BLOB)) {
+		if (check_sha1_signature(repl, NULL, 0, NULL) < 0) {
+			error("sha1 mismatch %s\n", sha1_to_hex(repl));
+			return NULL;
+		}
+		parse_blob_buffer(lookup_blob(sha1), NULL, 0);
+		return lookup_object(sha1);
+	}
+
 	buffer = read_sha1_file(sha1, &type, &size);
 	if (buffer) {
 		if (check_sha1_signature(repl, buffer, size, typename(type)) < 0) {
