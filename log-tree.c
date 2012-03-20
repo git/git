aea69a016f5bda67c359e43a3cbfabe923ad0f5a
Lucian Poston <lucian.poston@gmail.com>
2012-03-20 01:05:34 -0700

    log --graph --stat: three-dash separator should come after graph lines
    Output from "git log --graph --stat -p" emits the three-dash separator
    line before the graph that shows ancestry lines.  The separator should
    come after the ancestry lines just like all the other output.
    
    Signed-off-by: Lucian Poston <lucian.poston@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- log-tree.c
+++ log-tree.c
@@ -570,13 +570,14 @@ int log_tree_diff_flush(struct rev_info *opt)
 		    opt->verbose_header &&
 		    opt->commit_format != CMIT_FMT_ONELINE) {
 			int pch = DIFF_FORMAT_DIFFSTAT | DIFF_FORMAT_PATCH;
-			if ((pch & opt->diffopt.output_format) == pch)
-				printf("---");
 			if (opt->diffopt.output_prefix) {
 				struct strbuf *msg = NULL;
 				msg = opt->diffopt.output_prefix(&opt->diffopt,
 					opt->diffopt.output_prefix_data);
 				fwrite(msg->buf, msg->len, 1, stdout);
+			}
+			if ((pch & opt->diffopt.output_format) == pch) {
+				printf("---");
 			}
 			putchar('\n');
 		}
