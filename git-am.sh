58725efd4a0583e4a3793b054ebaf57bd40e82fa
Johannes Berg <johannes@sipsolutions.net>
2012-03-28 13:11:28 +0200

    am: support --include option
    am supports a number of pass-through options
    to apply, like --exclude and --directory. Add
    --include to this list.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-am.sh
+++ git-am.sh
@@ -23,6 +23,7 @@ ignore-space-change pass it through git-apply
 ignore-whitespace pass it through git-apply
 directory=      pass it through git-apply
 exclude=        pass it through git-apply
+include=        pass it through git-apply
 C=              pass it through git-apply
 p=              pass it through git-apply
 patch-format=   format the patch(es) are in
@@ -363,7 +364,7 @@ do
 		;;
 	--resolvemsg)
 		shift; resolvemsg=$1 ;;
-	--whitespace|--directory|--exclude)
+	--whitespace|--directory|--exclude|--include)
 		git_apply_opt="$git_apply_opt $(sq "$1=$2")"; shift ;;
 	-C|-p)
 		git_apply_opt="$git_apply_opt $(sq "$1$2")"; shift ;;
