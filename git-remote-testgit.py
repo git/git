b6c9867c75117799f817223fac6a9d348d133b67
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-07 02:12:53 -0500

    remote-hg: do not interfer with hg's revs() method
    Matt Mackall introduced a revs() method to the localrepo class on Wed
    Nov 2 13:37:34 2011 in the commit 'localrepo: add revs helper method'.
    It is used when constructing a commit in memory.
    
    If we store the set of revs we want to handle under the same name, it
    overrides that method, resulting in an unpleasant 'TypeError: 'set'
    object is not callable' whenever we want to push (as we are constructing
    commits in memory, then).
    
    So let's work around that by renaming our field to 'revs_' and hope that
    upstream Mercurial does not introduce a field of that name, too.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-remote-testgit.py
+++ git-remote-testgit.py
@@ -56,7 +56,7 @@ class TestgitRemoteHelper(RemoteHelper):
         head is at clone time.
         """
 
-        for ref in repo.revs:
+        for ref in repo.revs_:
             debug("? refs/heads/%s", ref)
             print "? refs/heads/%s" % ref
 
