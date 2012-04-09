07bb9b302231f1b47cd5ef23c168070ad68c36cd
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-09 15:44:14 -0500

    squash! remote-hg: do not interfer with hg's revs() method
    FIXUP: the field is called 'revs_' now, Sverre likes that better.

--- git-remote-testgit.py
+++ git-remote-testgit.py
@@ -56,7 +56,7 @@ class TestgitRemoteHelper(RemoteHelper):
         head is at clone time.
         """
 
-        for ref in repo.revs2:
+        for ref in repo.revs_:
             debug("? refs/heads/%s", ref)
             print "? refs/heads/%s" % ref
 
