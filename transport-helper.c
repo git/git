befcd586d739e40e40938e311caff4ac670ba7dd
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-09 13:04:35 -0500

    Always auto-gc after calling a fast-import transport
    After importing anything with fast-import, we should always let the
    garbage collector do its job, since the objects are written to disk
    inefficiently.
    
    This brings down an initial import of http://selenic.com/hg from about
    230 megabytes to about 14.
    
    In the future, we may want to make this configurable on a per-remote
    basis, or maybe teach fast-import about it in the first place.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- transport-helper.c
+++ transport-helper.c
@@ -16,6 +16,8 @@
 #include "protocol.h"
 
 static int debug;
+/* TODO: put somewhere sensible, e.g. git_transport_options? */
+static int auto_gc = 1;
 
 struct helper_data {
 	const char *name;
@@ -567,6 +569,13 @@ static int fetch_with_import(struct transport *transport,
 		}
 	}
 	strbuf_release(&buf);
+	if (auto_gc) {
+		struct child_process cmd = CHILD_PROCESS_INIT;
+
+		cmd.git_cmd = 1;
+		strvec_pushl(&cmd.args, "gc", "--auto", "--quiet", NULL);
+		run_command(&cmd);
+	}
 	return 0;
 }
 
