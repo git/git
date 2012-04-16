5e71a84a2da8f5dd32c13392854eab4d448c8085
Lucian Poston <lucian.poston@gmail.com>
2012-04-16 03:44:50 -0700

    Add output_prefix_length to diff_options
    Add output_prefix_length to diff_options. Initialize the value to 0 and only
    set it when graph.c:diff_output_prefix_callback() is called.
    
    Signed-off-by: Lucian Poston <lucian.poston@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- graph.c
+++ graph.c
@@ -194,8 +194,10 @@ static struct strbuf *diff_output_prefix_callback(struct diff_options *opt, void
 	struct git_graph *graph = data;
 	static struct strbuf msgbuf = STRBUF_INIT;
 
+	assert(opt);
 	assert(graph);
 
+	opt->output_prefix_length = graph->width;
 	strbuf_reset(&msgbuf);
 	graph_padding_line(graph, &msgbuf);
 	return &msgbuf;
@@ -245,6 +247,7 @@ struct git_graph *graph_init(struct rev_info *opt)
 	 */
 	opt->diffopt.output_prefix = diff_output_prefix_callback;
 	opt->diffopt.output_prefix_data = graph;
+	opt->diffopt.output_prefix_length = 0;
 
 	return graph;
 }
