da79161db6dcb0643ab805e5cf78d7e4e34ce993
Lucian Poston <lucian.poston@gmail.com>
2012-04-18 14:09:33 -0700

    t4052: Test diff-stat output with minimum columns
    When COLUMNS or --stat-width restricts the diff-stat width to near the
    minimum, 26 columns, the graph_width value becomes negative. Consequently, the
    graph part of diff-stat is not resized properly.
    
    Signed-off-by: Lucian Poston <lucian.poston@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4052-stat-output.sh
+++ t/t4052-stat-output.sh
@@ -284,6 +284,34 @@ respects expect200 show --stat
 respects expect200 log -1 --stat
 EOF
 
+cat >expect1 <<'EOF'
+ ...aaaaaaa | 1000 ++++++
+EOF
+cat >expect1-graph <<'EOF'
+|  ...aaaaaaa | 1000 ++++++
+EOF
+while read teststate verb expect cmd args
+do
+	test_expect_$teststate "$cmd $verb prefix greater than COLUMNS (big change)" '
+		COLUMNS=1 git $cmd $args >output
+		grep " | " output >actual &&
+		test_cmp "$expect" actual
+	'
+
+	test "$cmd" != diff || continue
+
+	test_expect_$teststate "$cmd --graph $verb prefix greater than COLUMNS (big change)" '
+		COLUMNS=1 git $cmd $args --graph >output
+		grep " | " output >actual &&
+		test_cmp "$expect-graph" actual
+	'
+done <<\EOF
+success ignores expect80 format-patch -1 --stdout
+failure respects expect1 diff HEAD^ HEAD --stat
+failure respects expect1 show --stat
+failure respects expect1 log -1 --stat
+EOF
+
 cat >expect <<'EOF'
  abcd | 1000 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 EOF
