96dac0ab94a1d18d9ebcd74b21696f7cb2fccf69
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-19 18:21:29 -0500

    Introduce and use test_cmp_text
    On Windows, we suffer from frequently leaked DOS line endings. Let's
    ignore them.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/lib-credential.sh
+++ t/lib-credential.sh
@@ -23,11 +23,7 @@ check() {
 		dos2unix -q stderr
 	fi &&
 	test_cmp expect-stdout stdout &&
-	if test_have_prereq MINGW
-	then
-		dos2unix stderr
-	fi &&
-	test_cmp expect-stderr stderr
+	test_cmp_text expect-stderr stderr
 }
 
 read_chunk() {
