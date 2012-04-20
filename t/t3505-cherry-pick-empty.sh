bedfe86ce6ccee90e2cbecbf8d72e06219a2768a
Neil Horman <nhorman@tuxdriver.com>
2012-04-20 10:36:16 -0400

    git-cherry-pick: Add test to validate new options
    Since we've added the --allow-empty and --keep-redundant-commits
    options to git cherry-pick we should also add a test to ensure that its working
    properly.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3505-cherry-pick-empty.sh
+++ t/t3505-cherry-pick-empty.sh
@@ -18,7 +18,12 @@ test_expect_success setup '
 	echo third >> file1 &&
 	git add file1 &&
 	test_tick &&
-	git commit --allow-empty-message -m ""
+	git commit --allow-empty-message -m "" &&
+
+	git checkout master &&
+	git checkout -b empty-branch2 &&
+	test_tick &&
+	git commit --allow-empty -m "empty"
 
 '
 
@@ -46,6 +51,24 @@ test_expect_success 'index lockfile was removed' '
 
 	test ! -f .git/index.lock
 
+'
+
+test_expect_success 'cherry pick an empty non-ff commit without --allow-empty' '
+	git checkout master &&
+	echo fourth >>file2 &&
+	git add file2 &&
+	git commit -m "fourth" &&
+	test_must_fail git cherry-pick empty-branch2
+'
+
+test_expect_success 'cherry pick an empty non-ff commit with --allow-empty' '
+	git checkout master &&
+	git cherry-pick --allow-empty empty-branch2
+'
+
+test_expect_success 'cherry pick with --keep-redundant-commits' '
+	git checkout master &&
+	git cherry-pick --keep-redundant-commits HEAD^
 '
 
 test_done
