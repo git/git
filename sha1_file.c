5eaeda70de1e7763d070e8ccfbcd8ca10baea800
Pete Wyckoff <pw@padd.com>
2012-04-29 20:28:46 -0400

    remove blank filename in error message
    When write_loose_object() finds that it is unable to
    create a temporary file, it complains, for instance:
    
        unable to create temporary sha1 filename : Too many open files
    
    That extra space was supposed to be the name of the file,
    and will be an empty string if the git_mkstemps_mode() fails.
    
    The name of the temporary file is unimportant; delete it.
    
    Signed-off-by: Pete Wyckoff <pw@padd.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -2511,7 +2511,7 @@ static int write_loose_object(const unsigned char *sha1, char *hdr, int hdrlen,
 		if (errno == EACCES)
 			return error("insufficient permission for adding an object to repository database %s", get_object_directory());
 		else
-			return error("unable to create temporary sha1 filename %s: %s", tmp_file, strerror(errno));
+			return error("unable to create temporary file: %s", strerror(errno));
 	}
 
 	/* Set it up */
