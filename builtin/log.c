59f9b8a9a95cbcf4f6a5123cac04dc5073a8d0cc
Jeff King <peff@peff.net>
2012-05-24 19:32:52 -0400

    format-patch: do not use bogus email addresses in message ids
    We can ask git_committer_info to be strict about coming up
    with an email, which will die automatically on a poorly
    configured machine. This is better than letting invalid
    message-ids into the wild.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/log.c
+++ builtin/log.c
@@ -741,7 +741,7 @@ static void gen_message_id(struct rev_info *info, char *base)
 	struct strbuf buf = STRBUF_INIT;
 	strbuf_addf(&buf, "%s.%lu.git.%s", base,
 		    (unsigned long) time(NULL),
-		    git_committer_info(IDENT_NO_NAME|IDENT_NO_DATE));
+		    git_committer_info(IDENT_NO_NAME|IDENT_NO_DATE|IDENT_STRICT));
 	info->message_id = strbuf_detach(&buf, NULL);
 }
 
