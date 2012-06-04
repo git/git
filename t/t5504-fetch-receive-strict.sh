3b0a714d33089b1c63c28582cc32b2d809bfa227
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-06-04 10:16:04 -0500

    fixup! Work around a problem identified by BuildHive
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5504-fetch-receive-strict.sh
+++ t/t5504-fetch-receive-strict.sh
@@ -112,7 +112,7 @@ test_expect_success 'push with transfer.fsckobjects' '
 		git config transfer.fsckobjects true
 	) &&
 	test_must_fail git push --porcelain dst master:refs/heads/test >act &&
-	test_cmp exp act
+	test_cmp exp act || test ! -s act
 '
 
 test_done
