43bc2302706af6a3bb9266eea1bcb7071c54c8c6
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-06-12 15:45:16 +0200

    fast-export: report SHA-1 instead of gibberish when marks exist already
    Cc: Pieter de Bie <pdebie@ai.rug.nl>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Max Horn <max@quendi.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-fast-export.c
+++ builtin-fast-export.c
@@ -419,7 +419,7 @@ static void import_marks(char * input_file)
 			die ("Could not read blob %s", sha1_to_hex(sha1));
 
 		if (object->flags & SHOWN)
-			error("Object %s already has a mark", sha1);
+			error("Object %s already has a mark", sha1_to_hex(sha1));
 
 		mark_object(object, mark);
 		if (last_idnum < mark)
