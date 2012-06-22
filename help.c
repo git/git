af42ec44170b60f93cfdfdc5912acce5dfe193ac
Heiko Voigt <hvoigt@hvoigt.net>
2012-06-22 19:51:20 +0200

    help: correct behavior for is_executable on Windows
    The previous implementation said that the filesystem information on
    Windows is not reliable to determine whether a file is executable.
    To find gather this information it was peeking into the first two bytes
    of a file to see whether it looks executable.
    Apart from the fact that on Windows executables are usually defined as
    such by their extension it lead to slow opening of help file in some
    situations.
    
    When you have virus scanner running calling open on an executable file
    is a potentially expensive operation. See the following measurements (in
    seconds) for example.
    
    With virus scanner running (coldcache):
    
    $ ./a.exe /libexec/git-core/
    before open (git-add.exe): 0.000000
    after open (git-add.exe): 0.412873
    before open (git-annotate.exe): 0.000175
    after open (git-annotate.exe): 0.397925
    before open (git-apply.exe): 0.000243
    after open (git-apply.exe): 0.399996
    before open (git-archive.exe): 0.000147
    after open (git-archive.exe): 0.397783
    before open (git-bisect--helper.exe): 0.000160
    after open (git-bisect--helper.exe): 0.397700
    before open (git-blame.exe): 0.000160
    after open (git-blame.exe): 0.399136
    ...
    
    With virus scanner running (hotcache):
    
    $ ./a.exe /libexec/git-core/
    before open (git-add.exe): 0.000000
    after open (git-add.exe): 0.000325
    before open (git-annotate.exe): 0.000229
    after open (git-annotate.exe): 0.000177
    before open (git-apply.exe): 0.000167
    after open (git-apply.exe): 0.000150
    before open (git-archive.exe): 0.000154
    after open (git-archive.exe): 0.000156
    before open (git-bisect--helper.exe): 0.000132
    after open (git-bisect--helper.exe): 0.000180
    before open (git-blame.exe): 0.000718
    after open (git-blame.exe): 0.000724
    ...
    
    This test did just list the given directory and open() each file in it.
    
    With this patch I get:
    
    $ time git help git
    Launching default browser to display HTML ...
    
    real    0m8.723s
    user    0m0.000s
    sys     0m0.000s
    
    and without
    
    $ time git help git
    Launching default browser to display HTML ...
    
    real    1m37.734s
    user    0m0.000s
    sys     0m0.031s
    
    both tests with cold cache and giving the machine some time to settle
    down after restart.
    
    Signed-off-by: Heiko Voigt <heiko.voigt@mahr.de>

--- help.c
+++ help.c
@@ -105,7 +105,22 @@ static int is_executable(const char *name)
 		return 0;
 
 #if defined(GIT_WINDOWS_NATIVE)
-{	/* cannot trust the executable bit, peek into the file instead */
+	/*
+	 * On Windows there is no executable bit. The file extension
+	 * indicates whether it can be run as an executable, and Git
+	 * has special-handling to detect scripts and launch them
+	 * through the indicated script interpreter. We test for the
+	 * file extension first because virus scanners may make
+	 * opening an executable for reading expensive.
+	 */
+	if (ends_with(name, ".exe"))
+		return S_IXUSR;
+
+{
+	/*
+	 * Now that we know it does not have an executable extension,
+	 * peek into the file instead.
+	 */
 	char buf[3] = { 0 };
 	int n;
 	int fd = open(name, O_RDONLY);
@@ -113,8 +128,8 @@ static int is_executable(const char *name)
 	if (fd >= 0) {
 		n = read(fd, buf, 2);
 		if (n == 2)
-			/* DOS executables start with "MZ" */
-			if (!strcmp(buf, "#!") || !strcmp(buf, "MZ"))
+			/* look for a she-bang */
+			if (!strcmp(buf, "#!"))
 				st.st_mode |= S_IXUSR;
 		close(fd);
 	}
