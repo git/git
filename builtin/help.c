86272b4ffeb38fb4fb1da3603bba1094fab3731b
Chris Webb <chris@arachsys.com>
2012-06-28 07:58:03 +0100

    Allow help.htmlpath to be a URL prefix
    Setting this to a URL prefix instead of a path to a local directory allows
    git-help --web to work even when HTML docs aren't locally installed, by
    pointing the browser at a copy accessible on the web. For example,
    
        [help]
          format = html
          htmlpath = http://git-scm.com/docs
    
    will use the publicly available documentation on the git homepage.
    
    Signed-off-by: Chris Webb <chris@arachsys.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/help.c
+++ builtin/help.c
@@ -395,9 +395,11 @@ static void get_html_page_path(struct strbuf *page_path, const char *page)
 		html_path = system_path(GIT_HTML_PATH);
 
 	/* Check that we have a git documentation directory. */
-	if (stat(mkpath("%s/git.html", html_path), &st)
-	    || !S_ISREG(st.st_mode))
-		die(_("'%s': not a documentation directory."), html_path);
+	if (!strstr(html_path, "://")) {
+		if (stat(mkpath("%s/git.html", html_path), &st)
+		    || !S_ISREG(st.st_mode))
+			die("'%s': not a documentation directory.", html_path);
+	}
 
 	strbuf_init(page_path, 0);
 	strbuf_addf(page_path, "%s/%s.html", html_path, page);
