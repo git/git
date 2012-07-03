13243c2c7a758bb6510ba26d0372f384b5f422ce
Junio C Hamano <gitster@pobox.com>
2012-07-03 10:04:22 -0700

    reset: the command takes committish
    This is not strictly correct, in that resetting selected index
    entries from corresponding paths out of a given tree without moving
    HEAD is a valid operation, and in such case a tree-ish would suffice.
    
    But the existing code already requires a committish in the codepath,
    so let's be consistent with it for now.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1512-rev-parse-disambiguation.sh
+++ t/t1512-rev-parse-disambiguation.sh
@@ -121,7 +121,7 @@ test_expect_success 'git log takes only commit-ish' '
 	git log 000000000
 '
 
-test_expect_failure 'git reset takes only commit-ish' '
+test_expect_success 'git reset takes only commit-ish' '
 	git reset 000000000
 '
 
