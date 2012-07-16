4220afd7238c94f33bde55cf3fd6c84275eebe9a
Alexander Strasser <eclipse7@gmx.net>
2012-07-16 22:44:30 +0200

    t4012: Break up pipe into serial redirections
    Do not hide possible git errors by masking its process
    exit status.
    
    Signed-off-by: Alexander Strasser <eclipse7@gmx.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4012-diff-binary.sh
+++ t/t4012-diff-binary.sh
@@ -64,7 +64,8 @@ test_expect_success 'apply --numstat understands diff --binary format' '
 # apply needs to be able to skip the binary material correctly
 # in order to report the line number of a corrupt patch.
 test_expect_success 'apply detecting corrupt patch correctly' '
-	 git diff | sed -e "s/-CIT/xCIT/" >broken &&
+	 git diff >output &&
+	 sed -e "s/-CIT/xCIT/" <output >broken &&
 	 if git apply --stat --summary broken 2>detected
 	 then
 		echo unhappy - should have detected an error
