9aa98efca286f7e7795a30614d79f120d0cd786c
Jiang Xin <worldhello.net@gmail.com>
2012-08-27 13:36:53 +0800

    Fix tests under GETTEXT_POISON on pack-object
    Use the i18n-specific test functions in test scripts for pack-object.
    This issue was was introduced in v1.7.10.2-556-g46140:
    
        46140 index-pack: use streaming interface for collision test on large blobs
        cf2ba pack-objects: use streaming interface for reading large loose blobs
    
    and been broken under GETTEXT_POISON=YesPlease since.
    
    Signed-off-by: Jiang Xin <worldhello.net@gmail.com>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5300-pack-object.sh
+++ t/t5300-pack-object.sh
@@ -416,11 +416,11 @@ test_expect_success \
 test_expect_success \
     'make sure index-pack detects the SHA1 collision' \
     'test_must_fail git index-pack -o bad.idx test-3.pack 2>msg &&
-     grep "SHA1 COLLISION FOUND" msg'
+     test_i18ngrep "SHA1 COLLISION FOUND" msg'
 
 test_expect_success \
     'make sure index-pack detects the SHA1 collision (large blobs)' \
     'test_must_fail git -c core.bigfilethreshold=1 index-pack -o bad.idx test-3.pack 2>msg &&
-     grep "SHA1 COLLISION FOUND" msg'
+     test_i18ngrep "SHA1 COLLISION FOUND" msg'
 
 test_done
