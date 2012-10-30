7349afd20e3b0ce3da6f192f593390472acec9d3
Karsten Blees <blees@dcon.de>
2012-10-30 10:50:42 +0100

    update-index/diff-index: use core.preloadindex to improve performance
    'update-index --refresh' and 'diff-index' (without --cached) don't honor
    the core.preloadindex setting yet. Porcelain commands using these (such as
    git [svn] rebase) suffer from this, especially on Windows.
    
    Use read_cache_preload to improve performance.
    
    Additionally, in builtin/diff.c, don't preload index status if we don't
    access the working copy (--cached).
    
    Results with msysgit on WebKit repo (2GB in 200k files):
    
                    | update-index | diff-index | rebase
    ----------------+--------------+------------+---------
    msysgit-v1.8.0  |       9.157s |    10.536s | 42.791s
    + preloadindex  |       9.157s |    10.536s | 28.725s
    + this patch    |       2.329s |     2.752s | 15.152s
    + fscache [1]   |       0.731s |     1.171s |  8.877s
    
    [1] https://github.com/kblees/git/tree/kb/fscache-v3
    
    Thanks-to: Albert Krawczyk <pro-logic@optusnet.com.au>
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Jeff King <peff@peff.net>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -593,6 +593,7 @@ struct refresh_params {
 static int refresh(struct refresh_params *o, unsigned int flag)
 {
 	setup_work_tree();
+	read_cache_preload(NULL);
 	*o->has_errors |= refresh_cache(o->flags | flag);
 	return 0;
 }
