ef49841ddf98ed1eb40c60153072fa1a91fc2f18
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-11-20 08:02:39 +0100

    test-wildmatch: avoid Windows path mangling
    The MSYS bash mangles arguments that begin with a forward slash
    when they are passed to test-wildmatch. This causes tests to fail.
    Avoid mangling by prepending "XXX", which is removed by
    test-wildmatch before further processing.
    
    [J6t: reworded commit message]
    
    Reported-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>

--- test-wildmatch.c
+++ test-wildmatch.c
@@ -3,6 +3,14 @@
 
 int main(int argc, char **argv)
 {
+	int i;
+	for (i = 2; i < argc; i++) {
+		if (argv[i][0] == '/')
+			die("Forward slash is not allowed at the beginning of the\n"
+			    "pattern because Windows does not like it. Use `XXX/' instead.");
+		else if (!strncmp(argv[i], "XXX/", 4))
+			argv[i] += 3;
+	}
 	if (!strcmp(argv[1], "wildmatch"))
 		return !!wildmatch(argv[3], argv[2], 0);
 	else if (!strcmp(argv[1], "iwildmatch"))
