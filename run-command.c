a2767c5c91ebda3c083419c80a7f64248c5ec175
Jeff King <peff@peff.net>
2012-11-30 17:41:38 -0500

    run-command: do not warn about child death from terminal
    SIGINT and SIGQUIT are not generally interesting signals to
    the user, since they are typically caused by them hitting "^C"
    or otherwise telling their terminal to send the signal.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- run-command.c
+++ run-command.c
@@ -242,7 +242,8 @@ static int wait_or_whine(pid_t pid, const char *argv0)
 		error("waitpid is confused (%s)", argv0);
 	} else if (WIFSIGNALED(status)) {
 		code = WTERMSIG(status);
-		error("%s died of signal %d", argv0, code);
+		if (code != SIGINT && code != SIGQUIT)
+			error("%s died of signal %d", argv0, code);
 		/*
 		 * This return value is chosen so that code & 0xff
 		 * mimics the exit code that a POSIX shell would report for
