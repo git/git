176478a8bd028a3877d2be49a1e7a49386c095ce
Erik Faye-Lund <kusmabite@gmail.com>
2012-12-04 09:10:38 +0100

    mingw: make fgetc raise SIGINT if apropriate
    Set a control-handler to prevent the process from terminating, and
    simulate SIGINT so it can be handled by a signal-handler as usual.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mingw.h
+++ compat/mingw.h
@@ -179,6 +179,9 @@ int mingw_open (const char *filename, int oflags, ...);
 ssize_t mingw_write(int fd, const void *buf, size_t count);
 #define write mingw_write
 
+int mingw_fgetc(FILE *stream);
+#define fgetc mingw_fgetc
+
 FILE *mingw_fopen (const char *filename, const char *otype);
 #define fopen mingw_fopen
 
@@ -289,6 +292,9 @@ static inline unsigned int git_ntohl(unsigned int x)
 
 sig_handler_t mingw_signal(int sig, sig_handler_t handler);
 #define signal mingw_signal
+
+int mingw_raise(int sig);
+#define raise mingw_raise
 
 /*
  * ANSI emulation wrappers
