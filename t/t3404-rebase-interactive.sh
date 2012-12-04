694fb7247b0bb3a3f9810f1350f997ebdb6008ae
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-12-04 00:17:45 -0600

    Fix t3404 for Windows
    The usual problem: CR/LF...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -784,7 +784,7 @@ test_expect_success 'running "git rebase -i --exec git show HEAD"' '
 		git rebase -i HEAD~2 >expect
 	) &&
 	sed -e "1,9d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
@@ -797,7 +797,7 @@ test_expect_success 'running "git rebase --exec git show HEAD -i"' '
 		git rebase -i HEAD~2 >expect
 	) &&
 	sed -e "1,9d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
@@ -810,7 +810,7 @@ test_expect_success 'running "git rebase -ix git show HEAD"' '
 		git rebase -i HEAD~2 >expect
 	) &&
 	sed -e "1,9d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
@@ -823,7 +823,7 @@ test_expect_success 'rebase -ix with several <CMD>' '
 		git rebase -i HEAD~2 >expect
 	) &&
 	sed -e "1,9d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
@@ -837,7 +837,7 @@ test_expect_success 'rebase -ix with several instances of --exec' '
 		git rebase -i HEAD~2 >expect
 	) &&
 	sed -e "1,11d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
@@ -862,7 +862,7 @@ test_expect_success 'rebase -ix with --autosquash' '
 		git rebase -i HEAD~4 >expect
 	) &&
 	sed -e "1,13d" expect >expected &&
-	test_cmp expected actual
+	test_cmp_text expected actual
 '
 
 
