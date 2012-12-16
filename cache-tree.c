dbc3904ebc604bf5b818d9840b79228aacdd1343
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-12-16 11:15:25 +0700

    cache-tree: remove dead i-t-a code in verify_cache()
    This code is added in 331fcb5 (git add --intent-to-add: do not let an
    empty blob be committed by accident - 2008-11-28) to forbid committing
    when i-t-a entries are present. When we allow that, we forgot to
    remove this.
    
    Noticed-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache-tree.c
+++ cache-tree.c
@@ -166,12 +166,8 @@ static int verify_cache(struct cache_entry **cache,
 				fprintf(stderr, "...\n");
 				break;
 			}
-			if (ce_stage(ce))
-				fprintf(stderr, "%s: unmerged (%s)\n",
-					ce->name, sha1_to_hex(ce->sha1));
-			else
-				fprintf(stderr, "%s: not added yet\n",
-					ce->name);
+			fprintf(stderr, "%s: unmerged (%s)\n",
+				ce->name, sha1_to_hex(ce->sha1));
 		}
 	}
 	if (funny)
