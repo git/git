d0f9dbb9e29b8dc289ae3b8028586a99fdb0d194
Ramsay Jones <ramsay@ramsay1.demon.co.uk>
2013-01-31 18:32:55 +0000

    msvc: test-svn-fe: Fix linker "unresolved external" error
    In particular, while linking test-svn-fe.exe, the linker complains
    that the external symbol _strtoull is unresolved. A call to this
    function was added in commit ddcc8c5b ("vcs-svn: skeleton of an svn
    delta parser", 25-12-2010).
    
    The NO_STRTOULL build variable attempts to provide support to old
    systems which can't even declare 'unsigned long long' variables,
    let alone provide the strtoll() or strtoull() functions. Setting
    this build variable does not provide an implementation of these
    functions. Rather, it simply allows the compat implementations
    of strto{i,u}max() to use strtol() and strtoul() instead.
    
    In order to fix the linker error on systems with NO_STRTOULL set,
    currently MSVC and OSF1, we can substitute a call to strtoumax().
    
    However, we can easily provide support for the strtoull() and
    strtoll() functions on MSVC, since they are essentially already
    available as _strtoui64() and _strtoi64(). This allows us to
    remove NO_STRTOULL for MSVC.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsay1.demon.co.uk>
    Tested-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/msvc.h
+++ compat/msvc.h
@@ -12,6 +12,8 @@
 #define __attribute__(x)
 #define strncasecmp  _strnicmp
 #define ftruncate    _chsize
+#define strtoull     _strtoui64
+#define strtoll      _strtoi64
 
 static __inline int strcasecmp (const char *s1, const char *s2)
 {
--- test-svn-fe.c
+++ test-svn-fe.c
@@ -24,7 +24,7 @@ static int apply_delta(int argc, char *argv[])
 		die_errno("cannot open preimage");
 	if (buffer_init(&delta, argv[3]))
 		die_errno("cannot open delta");
-	if (svndiff0_apply(&delta, (off_t) strtoull(argv[4], NULL, 0),
+	if (svndiff0_apply(&delta, (off_t) strtoumax(argv[4], NULL, 0),
 					&preimage_view, stdout))
 		return 1;
 	if (buffer_deinit(&preimage))
