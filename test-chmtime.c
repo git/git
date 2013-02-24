b978403aed7f2df46bc4c8a63314959d8982e0ac
David Aguilar <davvid@gmail.com>
2013-02-24 14:48:39 -0800

    tests: use a lowercase "usage:" string
    Adjust test commands and test suites so that their
    usage strings are consistent with Git.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- test-chmtime.c
+++ test-chmtime.c
@@ -114,6 +114,6 @@ int main(int argc, const char *argv[])
 	return 0;
 
 usage:
-	fprintf(stderr, "Usage: %s %s\n", argv[0], usage_str);
+	fprintf(stderr, "usage: %s %s\n", argv[0], usage_str);
 	return -1;
 }
