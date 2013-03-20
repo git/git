8be412a723459feb9f521062e2919f8f6e8389d3
Johannes Sixt <j6t@kdbg.org>
2013-03-20 09:47:57 +0100

    t2003: work around path mangling issue on Windows
    MSYS bash considers the part "/g" in the sed expression "s/./=/g" as an
    absolute path after an assignment, and mangles it to a C:/something
    string. Do not attract bash's attention by avoiding the equals sign.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2003-checkout-cache-mkdir.sh
+++ t/t2003-checkout-cache-mkdir.sh
@@ -94,14 +94,14 @@ test_expect_success 'apply filter from working tree .gitattributes with --prefix
 	rm -fr path0 path1 path2 tmp* &&
 	mkdir path1 &&
 	mkdir tmp &&
-	git config filter.replace-all.smudge "sed -e s/./=/g" &&
+	git config filter.replace-all.smudge "sed -e s/./,/g" &&
 	git config filter.replace-all.clean cat &&
 	git config filter.replace-all.required true &&
 	echo "file1 filter=replace-all" >path1/.gitattributes &&
 	git checkout-index --prefix=tmp/ -f -a &&
 	echo frotz >expected &&
 	test_cmp expected tmp/path0 &&
-	echo ====== >expected &&
+	echo ,,,,,, >expected &&
 	test_cmp expected tmp/path1/file1
 '
 
