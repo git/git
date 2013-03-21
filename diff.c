c9fc4415e2c7d7673cdad34d41114ede3435a395
Max Nanasy <max.nanasy@gmail.com>
2013-03-21 12:53:38 -0700

    diff.c: diff.renamelimit => diff.renameLimit in message
    In the warning message printed when rename or unmodified copy
    detection was skipped due to too many files, change "diff.renamelimit"
    to "diff.renameLimit", in order to make it consistent with git
    documentation, which consistently uses "diff.renameLimit".
    
    Signed-off-by: Max Nanasy <max.nanasy@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -4662,7 +4662,7 @@ int diff_result_code(struct diff_options *opt, int status)
 {
 	int result = 0;
 
-	diff_warn_rename_limit("diff.renamelimit",
+	diff_warn_rename_limit("diff.renameLimit",
 			       opt->needed_rename_limit,
 			       opt->degraded_cc_to_c);
 	if (!DIFF_OPT_TST(opt, EXIT_WITH_STATUS) &&
