110bcdc3d0ea9f0042f24a4c0319dede36dfe7f9
Jeff King <peff@peff.net>
2013-04-05 18:17:40 -0400

    remote-curl: let servers override http 404 advice
    When we get an http 404 trying to get the initial list of
    refs from the server, we try to be helpful and remind the
    user that update-server-info may need to be run. This looks
    like:
    
      $ git clone https://github.com/non/existent
      Cloning into 'existent'...
      fatal: https://github.com/non/existent/info/refs?service=git-upload-pack not found: did you run git update-server-info on the server?
    
    Suggesting update-server-info may be a good suggestion for
    users who are in control of the server repo and who are
    planning to set up dumb http. But for users of smart http,
    and especially users who are not in control of the server
    repo, the advice is useless and confusing.
    
    The previous patch taught remote-curl to show custom advice
    from the server when it is available. When we have shown
    messages from the server, we can also drop our custom
    advice; what the server has to say is likely to be more
    accurate and helpful.
    
    We not only drop the mention of update-server-info, but also
    show only the main repo URL, not the full "info/refs" and
    service parameter. These elements may be useful for
    debugging a broken server configuration, but again, anything
    the server has provided is likely to be more useful (and one
    can still use GIT_CURL_VERBOSE to get much more complete
    debugging information).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote-curl.c
+++ remote-curl.c
@@ -209,7 +209,8 @@ static struct discovery* discover_refs(const char *service, int for_push)
 	case HTTP_OK:
 		break;
 	case HTTP_MISSING_TARGET:
-		show_http_message(&type, &buffer);
+		if (!show_http_message(&type, &buffer))
+			die("repository '%s' not found", url);
 		die("%s not found: did you run git update-server-info on the"
 		    " server?", refs_url);
 	case HTTP_NOAUTH:
