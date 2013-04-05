39a570f26c953e8277a6a26c00da657b240dcdd4
Jeff King <peff@peff.net>
2013-04-05 18:22:01 -0400

    http: re-word http error message
    When we report an http error code, we say something like:
    
      error: The requested URL reported failure: 403 Forbidden while accessing http://example.com/repo.git
    
    Everything between "error:" and "while" is written by curl,
    and the resulting sentence is hard to read (especially
    because there is no punctuation between curl's sentence and
    the remainder of ours). Instead, let's re-order this to give
    better flow:
    
      error: unable to access 'http://example.com/repo.git: The requested URL reported failure: 403 Forbidden
    
    This is still annoyingly long, but at least reads more
    clearly left to right.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http.c
+++ http.c
@@ -943,7 +943,7 @@ cleanup:
 
 void http_error(const char *url)
 {
-	error("%s while accessing %s", curl_errorstr, url);
+	error("unable to access '%s': %s", url, curl_errorstr);
 }
 
 int http_fetch_ref(const char *base, struct ref *ref)
