7323513d287d00fe2b40d659fb49042fb027e749
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-04-12 00:36:10 +0200

    obstack: fix spelling of similar
    Noticed using Lucas De Marchi's codespell tool.
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Acked-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/obstack.h
+++ compat/obstack.h
@@ -128,7 +128,7 @@ extern "C" {
 
 #define __BPTR_ALIGN(B, P, A) ((B) + (((P) - (B) + (A)) & ~(A)))
 
-/* Similiar to _BPTR_ALIGN (B, P, A), except optimize the common case
+/* Similar to _BPTR_ALIGN (B, P, A), except optimize the common case
    where pointers can be converted to integers, aligned as integers,
    and converted back again.  If PTR_INT_TYPE is narrower than a
    pointer (e.g., the AS/400), play it safe and compute the alignment
