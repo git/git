21246dbb9e0af278bc671e32e45a6cae4ad38b6f
Miklos Vajna <vmiklos@suse.cz>
2013-04-11 15:06:52 +0200

    cherry-pick: make sure all input objects are commits
    When a single argument was a non-commit, the error message used to be:
    
    	fatal: BUG: expected exactly one commit from walk
    
    For multiple arguments, when none of the arguments was a commit, the error was:
    
    	fatal: empty commit set passed
    
    Finally, when some of the arguments were non-commits, we ignored those
    arguments.  Fix this bug and make sure all arguments are commits, and
    for the first non-commit, error out with:
    
    	fatal: <name>: Can't cherry-pick a <type>
    
    Signed-off-by: Miklos Vajna <vmiklos@suse.cz>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -1047,6 +1047,7 @@ int sequencer_pick_revisions(struct replay_opts *opts)
 {
 	struct commit_list *todo_list = NULL;
 	unsigned char sha1[20];
+	int i;
 
 	if (opts->subcommand == REPLAY_NONE)
 		assert(opts->revs);
@@ -1066,6 +1067,23 @@ int sequencer_pick_revisions(struct replay_opts *opts)
 		return sequencer_rollback(opts);
 	if (opts->subcommand == REPLAY_CONTINUE)
 		return sequencer_continue(opts);
+
+	for (i = 0; i < opts->revs->pending.nr; i++) {
+		unsigned char sha1[20];
+		const char *name = opts->revs->pending.objects[i].name;
+
+		/* This happens when using --stdin. */
+		if (!strlen(name))
+			continue;
+
+		if (!get_sha1(name, sha1)) {
+			enum object_type type = sha1_object_info(sha1, NULL);
+
+			if (type > 0 && type != OBJ_COMMIT)
+				die(_("%s: can't cherry-pick a %s"), name, typename(type));
+		} else
+			die(_("%s: bad revision"), name);
+	}
 
 	/*
 	 * If we were called as "git cherry-pick <commit>", just
