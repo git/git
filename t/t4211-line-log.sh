d51c5274e458e9c2545144b6821978ccda85c117
Thomas Rast <trast@inf.ethz.ch>
2013-04-12 18:05:10 +0200

    log -L: test merge of parallel modify/rename
    This tests a toy example of a history like
    
      * Merge
      | \
      |  * Modify foo
      |  |
      *  | Rename foo->bar
      | /
      * Create foo
    
    Current log -L fails on this; we'll fix it in the next commit.
    
    Signed-off-by: Thomas Rast <trast@inf.ethz.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4211-line-log.sh
+++ t/t4211-line-log.sh
@@ -8,11 +8,18 @@ test_expect_success 'setup (import history)' '
 	git reset --hard
 '
 
-canned_test () {
-	test_expect_success "$1" "
-		git log $1 >actual &&
-		test_cmp \"\$TEST_DIRECTORY\"/t4211/expect.$2 actual
+canned_test_1 () {
+	test_expect_$1 "$2" "
+		git log $2 >actual &&
+		test_cmp \"\$TEST_DIRECTORY\"/t4211/expect.$3 actual
 	"
+}
+
+canned_test () {
+	canned_test_1 success "$@"
+}
+canned_test_failure () {
+	canned_test_1 failure "$@"
 }
 
 test_bad_opts () {
@@ -38,6 +45,7 @@ canned_test "-L '/long f/',/^}/:a.c -L /main/,/^}/:a.c simple" two-ranges
 canned_test "-L 24,+1:a.c simple" vanishes-early
 
 canned_test "-M -L '/long f/,/^}/:b.c' move-support" move-support-f
+canned_test_failure "-M -L ':f:b.c' parallel-change" parallel-change-f-to-main
 
 canned_test "-L 4,12:a.c -L :main:a.c simple" multiple
 canned_test "-L 4,18:a.c -L :main:a.c simple" multiple-overlapping
