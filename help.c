f2de0b97935da2ba3ccb627457e5fa3fc4e11287
David Aguilar <davvid@gmail.com>
2013-04-16 13:33:25 -0700

    help.c: add a compatibility comment to cmd_version()
    External projects have been known to parse the output of
    "git version".  Help prevent future authors from changing
    its format by adding a comment to its implementation.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- help.c
+++ help.c
@@ -397,6 +397,10 @@ const char *help_unknown_cmd(const char *cmd)
 
 int cmd_version(int argc, const char **argv, const char *prefix)
 {
+	/*
+	 * The format of this string should be kept stable for compatibility
+	 * with external projects that rely on the output of "git version".
+	 */
 	printf("git version %s\n", git_version_string);
 	return 0;
 }
