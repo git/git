0781aa4766ed88d620ea060c7d775ca806ebfa93
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-05-03 19:35:11 +0700

    clone: let the user know when check_everything_connected is run
    check_everything_connected could take a long time, especially in the
    clone case where the whole DAG is traversed. The user deserves to know
    what's going on.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -546,8 +546,12 @@ static void update_remote_refs(const struct ref *refs,
 {
 	const struct ref *rm = mapped_refs;
 
+	if (0 <= option_verbosity)
+		printf(_("Checking connectivity... "));
 	if (check_everything_connected(iterate_ref_map, 0, &rm))
 		die(_("remote did not send all necessary objects"));
+	if (0 <= option_verbosity)
+		printf(_("done\n"));
 
 	if (refs) {
 		write_remote_refs(mapped_refs);
