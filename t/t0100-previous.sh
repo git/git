84cf246670eab56a23ed5554ed084053a0f19f2d
Junio C Hamano <gitster@pobox.com>
2013-05-15 14:32:30 -0700

    strbuf_branchname(): do not double-expand @{-1}~22
    If you were on 'frotz' branch before you checked out your current
    branch, "git merge @{-1}~22" means the same as "git merge frotz~22".
    
    The strbuf_branchname() function, when interpret_branch_name() gives
    up resolving "@{-1}~22" fully, returns "frotz" and tells the caller
    that it only resolved "@{-1}" part of the input, mistakes this as a
    total failure, and appends the whole thing to the result, yielding
    "frotz@{-1}~22", which does not make any sense.
    
    Inspect the return value from interpret_branch_name() a bit more
    carefully.  When it errored out without consuming anything, we will
    get -1 and we should return the whole thing.  Otherwise, we should
    append the remainder (i.e. "~22" in the earlier example) to the
    partially resolved name (i.e. "frotz").
    
    The test suite adds enough number of checkout to make @{-12} in the
    last test in t0100 that tried to check "we haven't flipped branches
    that many times" error case succeed; raise the number to a hundred.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0100-previous.sh
+++ t/t0100-previous.sh
@@ -27,6 +27,7 @@ test_expect_success 'merge @{-1}' '
 	test_commit B &&
 	git checkout A &&
 	test_commit C &&
+	test_commit D &&
 	git branch -f master B &&
 	git branch -f other &&
 	git checkout other &&
@@ -35,14 +36,24 @@ test_expect_success 'merge @{-1}' '
 	git cat-file commit HEAD | grep "Merge branch '\''other'\''"
 '
 
-test_expect_success 'merge @{-1} when there is not enough switches yet' '
+test_expect_success 'merge @{-1}~1' '
+	git checkout master &&
+	git reset --hard B &&
+	git checkout other &&
+	git checkout master &&
+	git merge @{-1}~1 &&
+	git cat-file commit HEAD >actual &&
+	grep "Merge branch '\''other'\''" actual
+'
+
+test_expect_success 'merge @{-100} before checking out that many branches yet' '
 	git reflog expire --expire=now &&
 	git checkout -f master &&
 	git reset --hard B &&
 	git branch -f other C &&
 	git checkout other &&
 	git checkout master &&
-	test_must_fail git merge @{-12}
+	test_must_fail git merge @{-100}
 '
 
 test_done
