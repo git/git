67c9c782dae17822266783f8b5e15d73e7c91c65
Felipe Contreras <felipe.contreras@gmail.com>
2013-05-20 20:02:45 -0500

    transport-helper: barf when user tries old:new
    Otherwise with certain remote helpers (the ones that support 'export'),
    the users will be pushing to the wrong branch:
    
      git push topic:master
    
    Will push the topic branch, as if the user typed:
    
      git push topic
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -813,9 +813,11 @@ static int push_refs_with_export(struct transport *transport,
 			die("remote-helpers do not support ref deletion");
 		}
 
-		if (ref->peer_ref)
+		if (ref->peer_ref) {
+			if (strcmp(ref->peer_ref->name, ref->name))
+				die("remote-helpers do not support old:new syntax");
 			string_list_append(&revlist_args, ref->peer_ref->name);
-
+		}
 	}
 
 	if (get_exporter(transport, &exporter, &revlist_args))
