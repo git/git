21457f5719b894a5997d66ff098b375bc2404260
Felipe Contreras <felipe.contreras@gmail.com>
2013-05-20 20:32:04 -0500

    transport-helper: check if the dry-run is supported
    Certain remote-helpers (the ones with 'export') would try to push
    regardless.
    
    Obviously this is not what the user wants.
    
    Also, add a check for the 'dry-run' option, so remote-helpers can
    implement it.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -789,6 +789,11 @@ static int push_refs_with_export(struct transport *transport,
 	struct string_list revlist_args = STRING_LIST_INIT_NODUP;
 	struct strbuf buf = STRBUF_INIT;
 
+	if (flags & TRANSPORT_PUSH_DRY_RUN) {
+		if (set_helper_option(transport, "dry-run", "true") != 0)
+			die("helper %s does not support dry-run", data->name);
+	}
+
 	helper = get_helper(transport);
 
 	write_constant(helper->in, "export\n");
