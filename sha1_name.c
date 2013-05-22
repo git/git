17bf4ff3cd3d7fd4b252b81417df8be1b3b2b128
Ramkumar Ramachandra <artagnon@gmail.com>
2013-05-22 16:09:54 +0530

    sha1_name: fix error message for @{u}
    Currently, when no (valid) upstream is configured for a branch, you get
    an error like:
    
      $ git show @{u}
      error: No upstream configured for branch 'upstream-error'
      error: No upstream configured for branch 'upstream-error'
      fatal: ambiguous argument '@{u}': unknown revision or path not in the working tree.
      Use '--' to separate paths from revisions, like this:
      'git <command> [<revision>...] -- [<file>...]'
    
    The "error: " line actually appears twice, and the rest of the error
    message is useless.  In sha1_name.c:interpret_branch_name(), there is
    really no point in processing further if @{u} couldn't be resolved, and
    we might as well die() instead of returning an error().  After making
    this change, you get:
    
      $ git show @{u}
      fatal: No upstream configured for branch 'upstream-error'
    
    Also tweak a few tests in t1507 to expect this output.
    
    This only turns error() that may be called after we know we are
    dealing with an @{upstream} marker into die(), without touching
    silent error returns "return -1" from the function.  Any caller that
    wants to handle an error condition itself will not be hurt by this
    change, unless they want to see the message from error() and then
    exit silently without giving its own message, which needs to be
    fixed anyway.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_name.c
+++ sha1_name.c
@@ -1033,14 +1033,15 @@ int interpret_branch_name(const char *name, struct strbuf *buf)
 	 * points to something different than a branch.
 	 */
 	if (!upstream)
-		return error(_("HEAD does not point to a branch"));
+		die(_("HEAD does not point to a branch"));
 	if (!upstream->merge || !upstream->merge[0]->dst) {
 		if (!ref_exists(upstream->refname))
-			return error(_("No such branch: '%s'"), cp);
-		if (!upstream->merge)
-			return error(_("No upstream configured for branch '%s'"),
-				     upstream->name);
-		return error(
+			die(_("No such branch: '%s'"), cp);
+		if (!upstream->merge) {
+			die(_("No upstream configured for branch '%s'"),
+				upstream->name);
+		}
+		die(
 			_("Upstream branch '%s' not stored as a remote-tracking branch"),
 			upstream->merge[0]->src);
 	}
