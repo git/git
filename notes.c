d235e994f893fddd7808a4f5af879fd718563649
Michael Haggerty <mhagger@alum.mit.edu>
2013-05-25 11:08:20 +0200

    string_list_add_one_ref(): rename first parameter to "refname"
    This is the usual convention.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes.c
+++ notes.c
@@ -918,12 +918,12 @@ out:
 	return ret;
 }
 
-static int string_list_add_one_ref(const char *path, const unsigned char *sha1,
+static int string_list_add_one_ref(const char *refname, const unsigned char *sha1,
 				   int flag, void *cb)
 {
 	struct string_list *refs = cb;
-	if (!unsorted_string_list_has_string(refs, path))
-		string_list_append(refs, path);
+	if (!unsorted_string_list_has_string(refs, refname))
+		string_list_append(refs, refname);
 	return 0;
 }
 
