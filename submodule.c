467b8fe1bb900832f51281fe954d9134b545d6dd
René Scharfe <rene.scharfe@lsrfire.ath.cx>
2013-06-09 18:33:45 +0200

    submodule: remove redundant check for the_index.initialized
    read_cache already performs the same check and returns immediately if
    the cache has already been loaded.
    
    Signed-off-by: René Scharfe <rene.scharfe@lsrfire.ath.cx>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- submodule.c
+++ submodule.c
@@ -603,9 +603,8 @@ int fetch_populated_submodules(const struct argv_array *options,
 	if (!work_tree)
 		goto out;
 
-	if (!the_index.initialized)
-		if (read_cache() < 0)
-			die("index file corrupt");
+	if (read_cache() < 0)
+		die("index file corrupt");
 
 	argv_array_push(&argv, "fetch");
 	for (i = 0; i < options->argc; i++)
