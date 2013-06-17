b95e66f507967fdfbf7275a94729fe09aa4b20ca
Ralf Thielow <ralf.thielow@gmail.com>
2013-06-17 06:28:26 +0200

    wt-status: give better advice when cherry-pick is in progress
    When cherry-pick is in progress, 'git status' gives the advice to
    run "git commit" to finish the cherry-pick.
    
    However, this won't continue the sequencer, when picking a range of
    commits.
    
    Advise users to run "git cherry-pick --continue/--abort"; they work
    when picking a single commit as well.
    
    Signed-off-by: Ralf Thielow <ralf.thielow@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.c
+++ wt-status.c
@@ -955,10 +955,12 @@ static void show_cherry_pick_in_progress(struct wt_status *s,
 	if (advice_status_hints) {
 		if (has_unmerged(s))
 			status_printf_ln(s, color,
-				_("  (fix conflicts and run \"git commit\")"));
+				_("  (fix conflicts and run \"git cherry-pick --continue\")"));
 		else
 			status_printf_ln(s, color,
-				_("  (all conflicts fixed: run \"git commit\")"));
+				_("  (all conflicts fixed: run \"git cherry-pick --continue\")"));
+		status_printf_ln(s, color,
+			_("  (use \"git cherry-pick --abort\" to cancel the cherry-pick operation)"));
 	}
 	wt_status_print_trailer(s);
 }
