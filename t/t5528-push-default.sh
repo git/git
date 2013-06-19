396243fa479cd27bef1ced2ea5a9e4cf20f85a4b
Ramkumar Ramachandra <artagnon@gmail.com>
2013-06-19 16:41:43 +0530

    t/t5528-push-default: generalize test_push_*
    The setup creates two bare repositories: repo1 and repo2, but
    test_push_commit() hard-codes checking in repo1 for the actual output.
    Generalize it and its caller, test_push_success(), to optionally accept
    a third argument to specify the name of the repository to check for
    actual output.  We will use this in the next patch.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5528-push-default.sh
+++ t/t5528-push-default.sh
@@ -15,17 +15,19 @@ test_expect_success 'setup bare remotes' '
 
 # $1 = local revision
 # $2 = remote revision (tested to be equal to the local one)
+# $3 = [optional] repo to check for actual output (repo1 by default)
 check_pushed_commit () {
 	git log -1 --format='%h %s' "$1" >expect &&
-	git --git-dir=repo1 log -1 --format='%h %s' "$2" >actual &&
+	git --git-dir="${3:-repo1}" log -1 --format='%h %s' "$2" >actual &&
 	test_cmp expect actual
 }
 
 # $1 = push.default value
 # $2 = expected target branch for the push
+# $3 = [optional] repo to check for actual output (repo1 by default)
 test_push_success () {
 	git -c push.default="$1" push &&
-	check_pushed_commit HEAD "$2"
+	check_pushed_commit HEAD "$2" "$3"
 }
 
 # $1 = push.default value
