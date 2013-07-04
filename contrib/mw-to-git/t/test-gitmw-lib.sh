c7956f90844280d477fefb978ebe0682fab006fb
Benoit Person <benoit.person@ensimag.fr>
2013-07-04 22:38:57 +0200

    git-remote-mediawiki: update tests to run with the new bin-wrapper
    Until now, if git-remote-mediawiki was not installed, the test suite
    copied it to the toplevel directory. This solution pollutes the
    directory with untracked files. Plus, we would need to copy the new
    git-mw.perl file to test it too.
    
    Signed-off-by: Benoit Person <benoit.person@ensimag.fr>
    Signed-off-by: Matthieu Moy <matthieu.moy@grenoble-inp.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/mw-to-git/t/test-gitmw-lib.sh
+++ contrib/mw-to-git/t/test-gitmw-lib.sh
@@ -62,12 +62,8 @@ test_check_precond () {
 		test_done
 	fi
 
-	if [ ! -f "$GIT_BUILD_DIR"/git-remote-mediawiki ];
-	then
-		echo "No remote mediawiki for git found. Copying it in git"
-		echo "cp $GIT_BUILD_DIR/contrib/mw-to-git/git-remote-mediawiki $GIT_BUILD_DIR/"
-		ln -s "$GIT_BUILD_DIR"/contrib/mw-to-git/git-remote-mediawiki "$GIT_BUILD_DIR"
-	fi
+	GIT_EXEC_PATH=$(cd "$(dirname "$0")" && cd "../.." && pwd)
+	PATH="$GIT_EXEC_PATH"'/bin-wrapper:'"$PATH"
 
 	if [ ! -d "$WIKI_DIR_INST/$WIKI_DIR_NAME" ];
 	then
