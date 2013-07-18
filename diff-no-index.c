95a7c546b02a962b9e458f7fd2d0ed9b657c0ca6
Junio C Hamano <gitster@pobox.com>
2013-07-17 17:18:32 -0700

    diff: deprecate -q option to diff-files
    This reimplements the ancient "-q" option to "git diff-files" that
    was inherited from "show-diff -q" in terms of "--diff-filter=d".  We
    will be deprecating the "-q" option, so let's issue a warning when
    we do so.
    
    Incidentally this also tentatively fixes "git diff --no-index" to
    honor "-q" and hide deletions; the use will get the same warning.
    
    We should remove the support for "-q" in a future version but it is
    not that urgent.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff-no-index.c
+++ diff-no-index.c
@@ -187,7 +187,7 @@ void diff_no_index(struct rev_info *revs,
 {
 	int i, prefixlen;
 	int no_index = 0;
-	unsigned options = 0;
+	unsigned deprecated_show_diff_q_option_used = 0;
 	const char *paths[2];
 
 	/* Were we asked to do --no-index explicitly? */
@@ -225,7 +225,7 @@ void diff_no_index(struct rev_info *revs,
 		if (!strcmp(argv[i], "--no-index"))
 			i++;
 		else if (!strcmp(argv[i], "-q")) {
-			options |= DIFF_SILENT_ON_REMOVED;
+			deprecated_show_diff_q_option_used = 1;
 			i++;
 		}
 		else if (!strcmp(argv[i], "--"))
@@ -259,6 +259,9 @@ void diff_no_index(struct rev_info *revs,
 
 	revs->max_count = -2;
 	diff_setup_done(&revs->diffopt);
+
+	if (deprecated_show_diff_q_option_used)
+		handle_deprecated_show_diff_q(&revs->diffopt);
 
 	setup_diff_pager(&revs->diffopt);
 	DIFF_OPT_SET(&revs->diffopt, EXIT_WITH_STATUS);
