6a3bbb4db409c5417960ffd3f020f75e0cb36f81
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-08-16 16:52:03 +0700

    shallow: only add shallow graft points to new shallow file
    for_each_commit_graft() goes through all graft points, and shallow
    boundaries are just one special kind of grafting.
    
    If $GIT_DIR/shallow and $GIT_DIR/info/grafts are both present,
    write_shallow_commits() may catch both sets, accidentally turning
    some graft points to shallow boundaries.  Don't do that.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- shallow.c
+++ shallow.c
@@ -153,6 +153,8 @@ static int write_one_shallow(const struct commit_graft *graft, void *cb_data)
 {
 	struct write_shallow_data *data = cb_data;
 	const char *hex = sha1_to_hex(graft->sha1);
+	if (graft->nr_parent != -1)
+		return 0;
 	data->count++;
 	if (data->use_pack_protocol)
 		packet_buf_write(data->out, "shallow %s", hex);
