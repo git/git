cdab485853b405d6454d4974bdc3825134d85249
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2013-08-16 16:52:05 +0700

    upload-pack: delegate rev walking in shallow fetch to pack-objects
    upload-pack has a special revision walking code for shallow
    recipients. It works almost like the similar code in pack-objects
    except:
    
    1. in upload-pack, graft points could be added for deepening;
    
    2. also when the repository is deepened, the shallow point will be
       moved further away from the tip, but the old shallow point will be
       marked as edge to produce more efficient packs. See 6523078 (make
       shallow repository deepening more network efficient - 2009-09-03).
    
    Pass the file to pack-objects via --shallow-file. This will override
    $GIT_DIR/shallow and give pack-objects the exact repository shape
    that upload-pack has.
    
    mark edge commits by revision command arguments. Even if old shallow
    points are passed as "--not" revisions as in this patch, they will not
    be picked up by mark_edges_uninteresting() because this function looks
    up to parents for edges, while in this case the edge is the children,
    in the opposite direction. This will be fixed in an later patch when
    all given uninteresting commits are marked as edges.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5530-upload-pack-error.sh
+++ t/t5530-upload-pack-error.sh
@@ -54,9 +54,6 @@ test_expect_success 'upload-pack fails due to error in rev-list' '
 	printf "0032want %s\n0034shallow %s00000009done\n0000" \
 		$(git rev-parse HEAD) $(git rev-parse HEAD^) >input &&
 	test_must_fail git upload-pack . <input >/dev/null 2>output.err &&
-	# pack-objects survived
-	grep "Total.*, reused" output.err &&
-	# but there was an error, which must have been in rev-list
 	grep "bad tree object" output.err
 '
 
