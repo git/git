b3a5fd1ec19ca1c3aa58a7c9da879a8f713cbdb5
Karsten Blees <blees@dcon.de>
2013-09-08 14:23:27 +0200

    add infrastructure for read-only file system level caches
    Add a macro to mark code sections that only read from the file system,
    along with a config option and documentation.
    
    This facilitates implementation of relatively simple file system level
    caches without the need to synchronize with the file system.
    
    Enable read-only sections for 'git status' and preload_index.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -229,6 +229,7 @@ enum hide_dotfiles_type {
 static int core_restrict_inherited_handles = -1;
 static enum hide_dotfiles_type hide_dotfiles = HIDE_DOTFILES_DOTGITONLY;
 static char *unset_environment_variables;
+int core_fscache;
 
 int mingw_core_config(const char *var, const char *value, void *cb)
 {
@@ -237,6 +238,11 @@ int mingw_core_config(const char *var, const char *value, void *cb)
 			hide_dotfiles = HIDE_DOTFILES_DOTGITONLY;
 		else
 			hide_dotfiles = git_config_bool(var, value);
+		return 0;
+	}
+
+	if (!strcmp(var, "core.fscache")) {
+		core_fscache = git_config_bool(var, value);
 		return 0;
 	}
 
