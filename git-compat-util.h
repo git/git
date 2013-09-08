330ce9bb3e70258cdbdf5b755f8aea097a3d0399
Karsten Blees <blees@dcon.de>
2013-09-08 14:23:27 +0200

    add infrastructure for read-only file system level caches
    Add a macro to mark code sections that only read from the file system,
    along with a config option and documentation.
    
    This facilitates implementation of relatively simple file system level
    caches without the need to synchronize with the file system.
    
    Enable read-only sections for 'git status' and preload_index.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- git-compat-util.h
+++ git-compat-util.h
@@ -1049,4 +1049,19 @@ struct tm *git_gmtime_r(const time_t *, struct tm *);
 #define mark_as_git_dir(x) /* noop */
 #endif
 
+/*
+ * Enable/disable a read-only cache for file system data on platforms that
+ * support it.
+ *
+ * Implementing a live-cache is complicated and requires special platform
+ * support (inotify, ReadDirectoryChangesW...). enable_fscache shall be used
+ * to mark sections of git code that extensively read from the file system
+ * without modifying anything. Implementations can use this to cache e.g. stat
+ * data or even file content without the need to synchronize with the file
+ * system.
+ */
+#ifndef enable_fscache
+#define enable_fscache(x) /* noop */
+#endif
+
 #endif
