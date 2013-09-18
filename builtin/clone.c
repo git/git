643f918d13906cbccdc5ad188767fc7895e30fc1
Jeff King <peff@peff.net>
2013-09-18 16:35:13 -0400

    clone: always set transport options
    A clone will always create a transport struct, whether we
    are cloning locally or using an actual protocol. In the
    local case, we only use the transport to get the list of
    refs, and then transfer the objects out-of-band.
    
    However, there are many options that we do not bother
    setting up in the local case. For the most part, these are
    noops, because they only affect the object-fetching stage
    (e.g., the --depth option).  However, some options do have a
    visible impact. For example, giving the path to upload-pack
    via "-u" does not currently work for a local clone, even
    though we need upload-pack to get the ref list.
    
    We can just drop the conditional entirely and set these
    options for both local and non-local clones. Rather than
    keep track of which options impact the object versus the ref
    fetching stage, we can simply let the noops be noops (and
    the cost of setting the options in the first place is not
    high).
    
    The one exception is that we also check that the transport
    provides both a "get_refs_list" and a "fetch" method. We
    will now be checking the former for both cases (which is
    good, since a transport that cannot fetch refs would not
    work for a local clone), and we tweak the conditional to
    check for a "fetch" only when we are non-local.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -885,27 +885,25 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 	remote = remote_get(option_origin);
 	transport = transport_get(remote, remote->url[0]);
 
-	if (!is_local) {
-		if (!transport->get_refs_list || !transport->fetch)
-			die(_("Don't know how to clone %s"), transport->url);
+	if (!transport->get_refs_list || (!is_local && !transport->fetch))
+		die(_("Don't know how to clone %s"), transport->url);
 
-		transport_set_option(transport, TRANS_OPT_KEEP, "yes");
+	transport_set_option(transport, TRANS_OPT_KEEP, "yes");
 
-		if (option_depth)
-			transport_set_option(transport, TRANS_OPT_DEPTH,
-					     option_depth);
-		if (option_single_branch)
-			transport_set_option(transport, TRANS_OPT_FOLLOWTAGS, "1");
+	if (option_depth)
+		transport_set_option(transport, TRANS_OPT_DEPTH,
+				     option_depth);
+	if (option_single_branch)
+		transport_set_option(transport, TRANS_OPT_FOLLOWTAGS, "1");
 
-		transport_set_verbosity(transport, option_verbosity, option_progress);
+	transport_set_verbosity(transport, option_verbosity, option_progress);
 
-		if (option_upload_pack)
-			transport_set_option(transport, TRANS_OPT_UPLOADPACK,
-					     option_upload_pack);
+	if (option_upload_pack)
+		transport_set_option(transport, TRANS_OPT_UPLOADPACK,
+				     option_upload_pack);
 
-		if (transport->smart_options && !option_depth)
-			transport->smart_options->check_self_contained_and_connected = 1;
-	}
+	if (transport->smart_options && !option_depth)
+		transport->smart_options->check_self_contained_and_connected = 1;
 
 	refs = transport_get_remote_refs(transport);
 
