b1cdfb54f10ca64dc57be0d9184dcb4c9c069eac
Junio C Hamano <gitster@pobox.com>
2013-10-15 15:27:17 -0700

    archive.c: have SP around arithmetic operators
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- archive.c
+++ archive.c
@@ -440,7 +440,7 @@ static int match_extension(const char *filename, const char *ext)
 	 * prefix is non-empty (k.e., we don't match .tar.gz with no actual
 	 * filename).
 	 */
-	if (prefixlen < 2 || filename[prefixlen-1] != '.')
+	if (prefixlen < 2 || filename[prefixlen - 1] != '.')
 		return 0;
 	return !strcmp(filename + prefixlen, ext);
 }
