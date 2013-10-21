360a3261a471ee59760b0743bbb27d3a60849ae2
Brian Gernhardt <brian@gernhardtsoftware.com>
2013-10-21 13:54:12 -0400

    t5570: Update for clone-progress-to-stderr branch
    git clone now reports its progress to standard error, which throws off
    t5570.  Using test_i18ngrep instead of test_cmp allows the test to be
    more flexible by only looking for the expected error and ignoring any
    other output from the program.
    
    Signed-off-by: Brian Gernhardt <brian@gernhardtsoftware.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5570-git-daemon.sh
+++ t/t5570-git-daemon.sh
@@ -122,8 +122,7 @@ test_remote_error()
 	fi
 
 	test_must_fail git "$cmd" "$GIT_DAEMON_URL/$repo" "$@" 2>output &&
-	echo "fatal: remote error: $msg: /$repo" >expect &&
-	test_cmp expect output
+	test_i18ngrep "fatal: remote error: $msg: /$repo" output &&
 	ret=$?
 	chmod +x "$GIT_DAEMON_DOCUMENT_ROOT_PATH/repo.git"
 	(exit $ret)
