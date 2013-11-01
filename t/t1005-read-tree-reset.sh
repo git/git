77b43cac9feac1079f3643988cfadee63e4365f7
Junio C Hamano <gitster@pobox.com>
2013-11-01 15:04:08 -0700

    t1005: add test for "read-tree --reset -u A B"
    With a conflicted index, this used to give us an error.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1005-read-tree-reset.sh
+++ t/t1005-read-tree-reset.sh
@@ -43,6 +43,21 @@ test_expect_success 'reset should remove remnants from a failed merge' '
 	! test -f old
 '
 
+test_expect_success 'two-way reset should remove remnants too' '
+	read_tree_u_must_succeed --reset -u HEAD &&
+	git ls-files -s >expect &&
+	sha1=$(git rev-parse :new) &&
+	(
+		echo "100644 $sha1 1	old"
+		echo "100644 $sha1 3	old"
+	) | git update-index --index-info &&
+	>old &&
+	git ls-files -s &&
+	read_tree_u_must_succeed --reset -u HEAD HEAD &&
+	git ls-files -s >actual &&
+	! test -f old
+'
+
 test_expect_success 'Porcelain reset should remove remnants too' '
 	read_tree_u_must_succeed --reset -u HEAD &&
 	git ls-files -s >expect &&
