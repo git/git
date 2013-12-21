b77fcd1edca58621627d46af0321a9737b1310f0
Jeff King <peff@peff.net>
2013-12-21 09:00:27 -0500

    repack: handle optional files created by pack-objects
    We ask pack-objects to pack to a set of temporary files, and
    then rename them into place. Some files that pack-objects
    creates may be optional (like a .bitmap file), in which case
    we would not want to call rename(). We already call stat()
    and make the chmod optional if the file cannot be accessed.
    We could simply skip the rename step in this case, but that
    would be a minor regression in noticing problems with
    non-optional files (like the .pack and .idx files).
    
    Instead, we can now annotate extensions as optional, and
    skip them if they don't exist (and otherwise rely on
    rename() to barf).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/repack.c
+++ builtin/repack.c
@@ -117,6 +117,7 @@ int cmd_repack(int argc, const char **argv, const char *prefix)
 {
 	struct {
 		const char *name;
+		unsigned optional:1;
 	} exts[] = {
 		{".pack"},
 		{".idx"},
@@ -323,6 +324,7 @@ int cmd_repack(int argc, const char **argv, const char *prefix)
 		for (ext = 0; ext < ARRAY_SIZE(exts); ext++) {
 			char *fname, *fname_old;
 			struct stat statbuffer;
+			int exists = 0;
 			fname = mkpathdup("%s/pack-%s%s",
 					packdir, item->string, exts[ext].name);
 			fname_old = mkpathdup("%s-%s%s",
@@ -330,9 +332,12 @@ int cmd_repack(int argc, const char **argv, const char *prefix)
 			if (!stat(fname_old, &statbuffer)) {
 				statbuffer.st_mode &= ~(S_IWUSR | S_IWGRP | S_IWOTH);
 				chmod(fname_old, statbuffer.st_mode);
+				exists = 1;
 			}
-			if (rename(fname_old, fname))
-				die_errno(_("renaming '%s' failed"), fname_old);
+			if (exists || !exts[ext].optional) {
+				if (rename(fname_old, fname))
+					die_errno(_("renaming '%s' failed"), fname_old);
+			}
 			free(fname);
 			free(fname_old);
 		}
