2000544330d5b047074d8043042f7cc66157f8f5
Pete Wyckoff <pw@padd.com>
2014-01-21 18:16:46 -0500

    git p4: fix an error message when "p4 where" fails
    When "p4 where" fails, for whatever reason, the error message tries to
    show an undefined variable.  This minor bug applies only when using a
    client spec, and was introduced recently in 9d57c4a (git p4: implement
    view spec wildcards with "p4 where", 2013-08-30).
    
    Signed-off-by: Pete Wyckoff <pw@padd.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-p4.py
+++ git-p4.py
@@ -1871,7 +1871,7 @@ class View(object):
                 # assume error is "... file(s) not in client view"
                 continue
             if "clientFile" not in res:
-                die("No clientFile from 'p4 where %s'" % depot_path)
+                die("No clientFile in 'p4 where' output")
             if "unmap" in res:
                 # it will list all of them, but only one not unmap-ped
                 continue
