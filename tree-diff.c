0b707c3319f37f2ec3700638d62f3199af40c138
Kirill Smelkov <kirr@mns.spb.ru>
2014-02-05 20:57:10 +0400

    tree-diff: convert diff_root_tree_sha1() to just call diff_tree_sha1 with old=NULL
    Now since diff_tree_sha1 understands NULL for both old and new, we could
    indicate an empty tree for root commit by providing just NULL for old
    sha1.
    
    Signed-off-by: Kirill Smelkov <kirr@mns.spb.ru>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- tree-diff.c
+++ tree-diff.c
@@ -311,18 +311,5 @@ int diff_tree_sha1(const unsigned char *old, const unsigned char *new, const cha
 
 int diff_root_tree_sha1(const unsigned char *new, const char *base, struct diff_options *opt)
 {
-	int retval;
-	void *tree;
-	unsigned long size;
-	struct tree_desc empty, real;
-
-	tree = read_object_with_reference(new, tree_type, &size, NULL);
-	if (!tree)
-		die("unable to read root tree (%s)", sha1_to_hex(new));
-	init_tree_desc(&real, tree, size);
-
-	init_tree_desc(&empty, "", 0);
-	retval = diff_tree(&empty, &real, base, opt);
-	free(tree);
-	return retval;
+	return diff_tree_sha1(NULL, new, base, opt);
 }
