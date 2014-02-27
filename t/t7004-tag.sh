9ef176b55c373ba087b2d84e77b5713578891927
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-02-27 19:56:52 +0700

    tag: support --sort=<spec>
    --sort=version:refname (or --sort=v:refname for short) sorts tags as
    if they are versions. --sort=-refname reverses the order (with or
    without ":version").
    
    versioncmp() is copied from string/strverscmp.c in glibc commit
    ee9247c38a8def24a59eb5cfb7196a98bef8cfdc, reformatted to Git coding
    style. The implementation is under LGPL-2.1 and according to [1] I can
    relicense it to GPLv2.
    
    [1] http://www.gnu.org/licenses/gpl-faq.html#AllCompatibility
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7004-tag.sh
+++ t/t7004-tag.sh
@@ -1380,4 +1380,47 @@ test_expect_success 'multiple --points-at are OR-ed together' '
 	test_cmp expect actual
 '
 
+test_expect_success 'lexical sort' '
+	git tag foo1.3 &&
+	git tag foo1.6 &&
+	git tag foo1.10 &&
+	git tag -l --sort=refname "foo*" >actual &&
+	cat >expect <<EOF &&
+foo1.10
+foo1.3
+foo1.6
+EOF
+	test_cmp expect actual
+'
+
+test_expect_success 'version sort' '
+	git tag -l --sort=version:refname "foo*" >actual &&
+	cat >expect <<EOF &&
+foo1.3
+foo1.6
+foo1.10
+EOF
+	test_cmp expect actual
+'
+
+test_expect_success 'reverse version sort' '
+	git tag -l --sort=-version:refname "foo*" >actual &&
+	cat >expect <<EOF &&
+foo1.10
+foo1.6
+foo1.3
+EOF
+	test_cmp expect actual
+'
+
+test_expect_success 'reverse lexical sort' '
+	git tag -l --sort=-refname "foo*" >actual &&
+	cat >expect <<EOF &&
+foo1.6
+foo1.3
+foo1.10
+EOF
+	test_cmp expect actual
+'
+
 test_done
