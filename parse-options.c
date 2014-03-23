b6c2a0d45d4165dfd326bd7a28e66d9cedb8ae84
Junio C Hamano <gitster@pobox.com>
2014-03-23 16:04:36 -0700

    parse-options: make sure argh string does not have SP or _
    We encourage to spell an argument hint that consists of multiple
    words as a single-token separated with dashes.  In order to help
    catching violations added by new callers of parse-options, make sure
    argh does not contain SP or _ when the code validates the option
    definitions.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.c
+++ parse-options.c
@@ -375,6 +375,9 @@ static void parse_options_check(const struct option *opts)
 		default:
 			; /* ok. (usually accepts an argument) */
 		}
+		if (opts->argh &&
+		    strcspn(opts->argh, " _") != strlen(opts->argh))
+			err |= optbug(opts, "multi-word argh should use dash to separate words");
 	}
 	if (err)
 		exit(128);
