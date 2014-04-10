9aa91af0361e3c32fde5f8388dee963838308cd6
Yiannis Marangos <yiannis.marangos@gmail.com>
2014-04-10 21:54:12 +0300

    wrapper.c: add xpread() similar to xread()
    It is a common mistake to call read(2)/pread(2) and forget to
    anticipate that they may return error with EAGAIN/EINTR when the
    system call is interrupted.
    
    We have xread() helper to relieve callers of read(2) from having to
    worry about it; add xpread() helper to do the same for pread(2).
    
    Update the caller in the builtin/index-pack.c and the mmap emulation
    in compat/.
    
    Signed-off-by: Yiannis Marangos <yiannis.marangos@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -174,6 +174,24 @@ ssize_t xwrite(int fd, const void *buf, size_t len)
 	}
 }
 
+/*
+ * xpread() is the same as pread(), but it automatically restarts pread()
+ * operations with a recoverable error (EAGAIN and EINTR). xpread() DOES
+ * NOT GUARANTEE that "len" bytes is read even if the data is available.
+ */
+ssize_t xpread(int fd, void *buf, size_t len, off_t offset)
+{
+	ssize_t nr;
+	if (len > MAX_IO_SIZE)
+		len = MAX_IO_SIZE;
+	while (1) {
+		nr = pread(fd, buf, len, offset);
+		if ((nr < 0) && (errno == EAGAIN || errno == EINTR))
+			continue;
+		return nr;
+	}
+}
+
 ssize_t read_in_full(int fd, void *buf, size_t count)
 {
 	char *p = buf;
