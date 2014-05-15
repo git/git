d6c8a05bd58be82165be54f01c861c0fae28b8c4
Jeff King <peff@peff.net>
2014-05-15 04:54:06 -0400

    open_sha1_file: report "most interesting" errno
    When we try to open a loose object file, we first attempt to
    open in the local object database, and then try any
    alternates. This means that the errno value when we return
    will be from the last place we looked (and due to the way
    the code is structured, simply ENOENT if we do not have have
    any alternates).
    
    This can cause confusing error messages, as read_sha1_file
    checks for ENOENT when reporting a missing object. If errno
    is something else, we report that. If it is ENOENT, but
    has_loose_object reports that we have it, then we claim the
    object is corrupted. For example:
    
        $ chmod 0 .git/objects/??/*
        $ git rev-list --all
        fatal: loose object b2d6fab18b92d49eac46dc3c5a0bcafabda20131 (stored in .git/objects/b2/d6fab18b92d49eac46dc3c5a0bcafabda20131) is corrupt
    
    This patch instead keeps track of the "most interesting"
    errno we receive during our search. We consider ENOENT to be
    the least interesting of all, and otherwise report the first
    error found (so problems in the object database take
    precedence over ones in alternates). Here it is with this
    patch:
    
        $ git rev-list --all
        fatal: failed to read object b2d6fab18b92d49eac46dc3c5a0bcafabda20131: Permission denied
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -1424,20 +1424,24 @@ static int open_sha1_file(const unsigned char *sha1)
 	int fd;
 	char *name = sha1_file_name(sha1);
 	struct alternate_object_database *alt;
+	int most_interesting_errno;
 
 	fd = git_open_noatime(name);
 	if (fd >= 0)
 		return fd;
+	most_interesting_errno = errno;
 
 	prepare_alt_odb();
-	errno = ENOENT;
 	for (alt = alt_odb_list; alt; alt = alt->next) {
 		name = alt->name;
 		fill_sha1_path(name, sha1);
 		fd = git_open_noatime(alt->base);
 		if (fd >= 0)
 			return fd;
+		if (most_interesting_errno == ENOENT)
+			most_interesting_errno = errno;
 	}
+	errno = most_interesting_errno;
 	return -1;
 }
 
