86954eaa0c9bb78309d77048ef5c86098d0a1d69
Sup Yut Sum <ch3cooli@gmail.com>
2014-06-02 12:13:24 +0800

    userdiff: support C# async methods and correct C# keywords
     - async is in C# 5.0
     - foreach is in C# 1.0
     - instanceof is not a C# keyword (it is in Java). Similar keywords
       are typeof, is, as in C# 1.0
     - default, try are in C# 1.0
    
    Signed-off-by: Sup Yut Sum <ch3cooli@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- userdiff.c
+++ userdiff.c
@@ -134,9 +134,9 @@ PATTERNS("cpp",
 	 "|[-+*/<>%&^|=!]=|--|\\+\\+|<<=?|>>=?|&&|\\|\\||::|->\\*?|\\.\\*"),
 PATTERNS("csharp",
 	 /* Keywords */
-	 "!^[ \t]*(do|while|for|if|else|instanceof|new|return|switch|case|throw|catch|using)\n"
+	 "!^[ \t]*(do|while|for|foreach|if|else|typeof|is|as|new|return|switch|case|default|throw|try|catch|using)\n"
 	 /* Methods and constructors */
-	 "^[ \t]*(((static|public|internal|private|protected|new|virtual|sealed|override|unsafe)[ \t]+)*[][<>@.~_[:alnum:]]+[ \t]+[<>@._[:alnum:]]+[ \t]*\\(.*\\))[ \t]*$\n"
+	 "^[ \t]*(((static|public|internal|private|protected|new|virtual|sealed|override|unsafe|async)[ \t]+)*[][<>@.~_[:alnum:]]+[ \t]+[<>@._[:alnum:]]+[ \t]*\\(.*\\))[ \t]*$\n"
 	 /* Properties */
 	 "^[ \t]*(((static|public|internal|private|protected|new|virtual|sealed|override|unsafe)[ \t]+)*[][<>@.~_[:alnum:]]+[ \t]+[@._[:alnum:]]+)[ \t]*$\n"
 	 /* Type definitions */
