c057b2424a479a2219666f3806c0dd93867ca5c9
Jeff King <peff@peff.net>
2014-06-03 03:17:17 -0400

    error_resolve_conflict: rewrap advice message
    If you try to commit with unresolved conflicts in the index,
    you get this message:
    
    	$ git commit
    	U       foo
    	error: 'commit' is not possible because you have unmerged files.
    	hint: Fix them up in the work tree,
    	hint: and then use 'git add/rm <file>' as
    	hint: appropriate to mark resolution and make a commit,
    	hint: or use 'git commit -a'.
    	fatal: Exiting because of an unresolved conflict.
    
    The irregular line-wrapping makes this awkward to read, and
    it takes up more lines than necessary. Instead, let's rewrap
    it to about 60 characters per line:
    
    	$ git commit
    	U       foo
    	error: 'commit' is not possible because you have unmerged files.
    	hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    	hint: as appropriate to mark resolution and make a commit, or use
    	hint: 'git commit -a'.
    	fatal: Exiting because of an unresolved conflict.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- advice.c
+++ advice.c
@@ -84,10 +84,9 @@ int error_resolve_conflict(const char *me)
 		 * Message used both when 'git commit' fails and when
 		 * other commands doing a merge do.
 		 */
-		advise(_("Fix them up in the work tree,\n"
-			 "and then use 'git add/rm <file>' as\n"
-			 "appropriate to mark resolution and make a commit,\n"
-			 "or use 'git commit -a'."));
+		advise(_("Fix them up in the work tree, and then use 'git add/rm <file>'\n"
+			 "as appropriate to mark resolution and make a commit, or use\n"
+			 "'git commit -a'."));
 	return -1;
 }
 
