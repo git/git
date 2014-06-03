d795216ac35f08e1ae2a706fca4c06a887c7d797
Jeff King <peff@peff.net>
2014-06-03 03:23:49 -0400

    error_resolve_conflict: drop quotations around operation
    When you try to commit with unmerged entries, you get an
    error like:
    
      $ git commit
      error: 'commit' is not possible because you have unmerged files.
    
    The quotes around "commit" are clunky; the user doesn't care
    that this message is a template with the command-name filled
    in.  Saying:
    
      error: commit is not possible because you have unmerged files
    
    is easier to read. As this code is called from other places,
    we may also end up with:
    
      $ git merge
      error: merge is not possible because you have unmerged files
    
      $ git cherry-pick foo
      error: cherry-pick is not possible because you have unmerged files
    
      $ git revert foo
      error: revert is not possible because you have unmerged files
    
    All of which look better without the quotes. This also
    happens to match the behavior of "git pull", which generates
    a similar message (but does not share code, as it is a shell
    script).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- advice.c
+++ advice.c
@@ -78,7 +78,7 @@ int git_default_advice_config(const char *var, const char *value)
 
 int error_resolve_conflict(const char *me)
 {
-	error("'%s' is not possible because you have unmerged files.", me);
+	error("%s is not possible because you have unmerged files.", me);
 	if (advice_resolve_conflict)
 		/*
 		 * Message used both when 'git commit' fails and when
