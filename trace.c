124647c4b007eff04144d75042d9d95f63e303f7
Karsten Blees <karsten.blees@gmail.com>
2014-07-12 02:03:01 +0200

    trace: disable additional trace output for unit tests
    Some unit-tests use trace output to verify internal state, and unstable
    output such as timestamps and line numbers are not useful there.
    
    Disable additional trace output if GIT_TRACE_BARE is set.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trace.c
+++ trace.c
@@ -87,10 +87,16 @@ static const char err_msg[] = "Could not trace into fd given by "
 
 static int prepare_trace_line(struct trace_key *key, struct strbuf *buf)
 {
+	static struct trace_key trace_bare = TRACE_KEY_INIT(BARE);
+
 	if (!trace_want(key))
 		return 0;
 
 	set_try_to_free_routine(NULL);	/* is never reset */
+
+	/* unit tests may want to disable additional trace output */
+	if (trace_want(&trace_bare))
+		return 1;
 
 	/* add line prefix here */
 
