41ca19b6a6ca5ad6fea246bd400717c16b80b32c
Johannes Sixt <j6t@kdbg.org>
2014-08-13 21:30:11 +0200

    tests: fix negated test_i18ngrep calls
    The helper function test_i18ngrep pretends that it found the expected
    results when it is running under GETTEXT_POISON. For this reason, it must
    not be used negated like so
    
       ! test_i18ngrep foo bar
    
    because the test case would fail under GETTEXT_POISON. The function offers
    a special syntax to test that a pattern is *not* found:
    
       test_i18ngrep ! foo bar
    
    Convert incorrect uses to this syntax.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t9807-git-p4-submit.sh
+++ t/t9807-git-p4-submit.sh
@@ -404,7 +404,7 @@ test_expect_success 'submit --prepare-p4-only' '
 		git p4 submit --prepare-p4-only >out &&
 		test_i18ngrep "prepared for submission" out &&
 		test_i18ngrep "must be deleted" out &&
-		! test_i18ngrep "everything below this line is just the diff" out
+		test_i18ngrep ! "everything below this line is just the diff" out
 	) &&
 	(
 		cd "$cli" &&
