a50e7ca3215492b16d96221cf762dbe71ed99722
Junio C Hamano <gitster@pobox.com>
2014-08-14 15:31:13 -0700

    gpg-interface: move parse_gpg_output() to where it should be
    Earlier, ffb6d7d5 (Move commit GPG signature verification to
    commit.c, 2013-03-31) moved this helper that used to be in pretty.c
    (i.e. the output code path) to commit.c for better reusability.
    
    It was a good first step in the right direction, but still suffers
    from a myopic view that commits will be the only thing we would ever
    want to sign---we would actually want to be able to reuse it even
    wider.
    
    The function interprets what GPG said; gpg-interface is obviously a
    better place.  Move it there.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- gpg-interface.c
+++ gpg-interface.c
@@ -21,6 +21,42 @@ void signature_check_clear(struct signature_check *sigc)
 	sigc->key = NULL;
 }
 
+static struct {
+	char result;
+	const char *check;
+} sigcheck_gpg_status[] = {
+	{ 'G', "\n[GNUPG:] GOODSIG " },
+	{ 'B', "\n[GNUPG:] BADSIG " },
+	{ 'U', "\n[GNUPG:] TRUST_NEVER" },
+	{ 'U', "\n[GNUPG:] TRUST_UNDEFINED" },
+};
+
+void parse_gpg_output(struct signature_check *sigc)
+{
+	const char *buf = sigc->gpg_status;
+	int i;
+
+	/* Iterate over all search strings */
+	for (i = 0; i < ARRAY_SIZE(sigcheck_gpg_status); i++) {
+		const char *found, *next;
+
+		if (!skip_prefix(buf, sigcheck_gpg_status[i].check + 1, &found)) {
+			found = strstr(buf, sigcheck_gpg_status[i].check);
+			if (!found)
+				continue;
+			found += strlen(sigcheck_gpg_status[i].check);
+		}
+		sigc->result = sigcheck_gpg_status[i].result;
+		/* The trust messages are not followed by key/signer information */
+		if (sigc->result != 'U') {
+			sigc->key = xmemdupz(found, 16);
+			found += 17;
+			next = strchrnul(found, '\n');
+			sigc->signer = xmemdupz(found, next - found);
+		}
+	}
+}
+
 void set_signing_key(const char *key)
 {
 	free(configured_signing_key);
