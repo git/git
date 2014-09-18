7559a1be8a0afb10df41d25e4cf4c5285a5faef1
Patrick Reynolds <patrick.reynolds@github.com>
2014-09-18 11:57:09 -0500

    unblock and unignore SIGPIPE
    Blocked and ignored signals -- but not caught signals -- are inherited
    across exec.  Some callers with sloppy signal-handling behavior can call
    git with SIGPIPE blocked or ignored, even non-deterministically.  When
    SIGPIPE is blocked or ignored, several git commands can run indefinitely,
    ignoring EPIPE returns from write() calls, even when the process that
    called them has gone away.  Our specific case involved a pipe of git
    diff-tree output to a script that reads a limited amount of diff data.
    
    In an ideal world, git would never be called with SIGPIPE blocked or
    ignored.  But in the real world, several real potential callers, including
    Perl, Apache, and Unicorn, sometimes spawn subprocesses with SIGPIPE
    ignored.  It is easier and more productive to harden git against this
    mistake than to clean it up in every potential parent process.
    
    Signed-off-by: Patrick Reynolds <patrick.reynolds@github.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git.c
+++ git.c
@@ -592,6 +592,26 @@ static int run_argv(int *argcp, const char ***argv)
 	return done_alias;
 }
 
+/*
+ * Many parts of Git have subprograms communicate via pipe, expect the
+ * upstream of a pipe to die with SIGPIPE when the downstream of a
+ * pipe does not need to read all that is written.  Some third-party
+ * programs that ignore or block SIGPIPE for their own reason forget
+ * to restore SIGPIPE handling to the default before spawning Git and
+ * break this carefully orchestrated machinery.
+ *
+ * Restore the way SIGPIPE is handled to default, which is what we
+ * expect.
+ */
+static void restore_sigpipe_to_default(void)
+{
+	sigset_t unblock;
+
+	sigemptyset(&unblock);
+	sigaddset(&unblock, SIGPIPE);
+	sigprocmask(SIG_UNBLOCK, &unblock, NULL);
+	signal(SIGPIPE, SIG_DFL);
+}
 
 int main(int argc, char **av)
 {
@@ -610,6 +630,8 @@ int main(int argc, char **av)
 	 * onto stdin/stdout/stderr in the child processes we spawn.
 	 */
 	sanitize_stdfds();
+
+	restore_sigpipe_to_default();
 
 	git_setup_gettext();
 
