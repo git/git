e831855ecc6783bfe4b681017349c623fc2fe8c8
Michael Haggerty <mhagger@alum.mit.edu>
2014-10-01 12:28:26 +0200

    git_config_set_multivar_in_file(): avoid call to rollback_lock_file()
    After commit_lock_file() is called, then the lock_file object is
    necessarily either committed or rolled back.  So there is no need to
    call rollback_lock_file() again in either of these cases.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -2083,6 +2083,7 @@ int git_config_set_multivar_in_file(const char *config_filename,
 	if (commit_lock_file(lock) < 0) {
 		error("could not commit config file %s", config_filename);
 		ret = CONFIG_NO_WRITE;
+		lock = NULL;
 		goto out_free;
 	}
 
