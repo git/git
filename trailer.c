85039fb6e4e7123958a40a22ef40e25bcb29f9c0
Christian Couder <chriscool@tuxfamily.org>
2014-10-13 20:16:31 +0200

    trailer: execute command from 'trailer.<name>.command'
    Let the user specify a command that will give on its standard output
    the value to use for the specified trailer.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trailer.c
+++ trailer.c
@@ -1,5 +1,7 @@
 #include "cache.h"
 #include "string-list.h"
+#include "run-command.h"
+#include "string-list.h"
 #include "trailer.h"
 /*
  * Copyright (c) 2013, 2014 Christian Couder <chriscool@tuxfamily.org>
@@ -32,6 +34,8 @@ struct trailer_item {
 static struct trailer_item *first_conf_item;
 
 static char *separators = ":";
+
+#define TRAILER_ARG_STRING "$ARG"
 
 static int after_or_end(enum action_where where)
 {
@@ -76,6 +80,13 @@ static inline int contains_only_spaces(const char *str)
 	while (*s && isspace(*s))
 		s++;
 	return !*s;
+}
+
+static inline void strbuf_replace(struct strbuf *sb, const char *a, const char *b)
+{
+	const char *ptr = strstr(sb->buf, a);
+	if (ptr)
+		strbuf_splice(sb, ptr - sb->buf, strlen(a), b, strlen(b));
 }
 
 static void free_trailer_item(struct trailer_item *item)
@@ -203,6 +214,63 @@ static struct trailer_item *remove_first(struct trailer_item **first)
 	return item;
 }
 
+static int read_from_command(struct child_process *cp, struct strbuf *buf)
+{
+	if (run_command(cp))
+		return error("running trailer command '%s' failed", cp->argv[0]);
+	if (strbuf_read(buf, cp->out, 1024) < 1)
+		return error("reading from trailer command '%s' failed", cp->argv[0]);
+	strbuf_trim(buf);
+	return 0;
+}
+
+static const char *apply_command(const char *command, const char *arg)
+{
+	struct strbuf cmd = STRBUF_INIT;
+	struct strbuf buf = STRBUF_INIT;
+	struct child_process cp;
+	const char *argv[] = {NULL, NULL};
+	const char *result;
+
+	strbuf_addstr(&cmd, command);
+	if (arg)
+		strbuf_replace(&cmd, TRAILER_ARG_STRING, arg);
+
+	argv[0] = cmd.buf;
+	memset(&cp, 0, sizeof(cp));
+	cp.argv = argv;
+	cp.env = local_repo_env;
+	cp.no_stdin = 1;
+	cp.out = -1;
+	cp.use_shell = 1;
+
+	if (read_from_command(&cp, &buf)) {
+		strbuf_release(&buf);
+		result = xstrdup("");
+	} else
+		result = strbuf_detach(&buf, NULL);
+
+	strbuf_release(&cmd);
+	return result;
+}
+
+static void apply_item_command(struct trailer_item *in_tok, struct trailer_item *arg_tok)
+{
+	if (arg_tok->conf.command) {
+		const char *arg;
+		if (arg_tok->value && arg_tok->value[0]) {
+			arg = arg_tok->value;
+		} else {
+			if (in_tok && in_tok->value)
+				arg = xstrdup(in_tok->value);
+			else
+				arg = xstrdup("");
+		}
+		arg_tok->value = apply_command(arg_tok->conf.command, arg);
+		free((char *)arg);
+	}
+}
+
 static void apply_arg_if_exists(struct trailer_item *in_tok,
 				struct trailer_item *arg_tok,
 				struct trailer_item *on_tok,
@@ -214,16 +282,19 @@ static void apply_arg_if_exists(struct trailer_item *in_tok,
 		free_trailer_item(arg_tok);
 		break;
 	case EXISTS_REPLACE:
+		apply_item_command(in_tok, arg_tok);
 		add_arg_to_input_list(on_tok, arg_tok,
 				      in_tok_first, in_tok_last);
 		remove_from_list(in_tok, in_tok_first, in_tok_last);
 		free_trailer_item(in_tok);
 		break;
 	case EXISTS_ADD:
+		apply_item_command(in_tok, arg_tok);
 		add_arg_to_input_list(on_tok, arg_tok,
 				      in_tok_first, in_tok_last);
 		break;
 	case EXISTS_ADD_IF_DIFFERENT:
+		apply_item_command(in_tok, arg_tok);
 		if (check_if_different(in_tok, arg_tok, 1))
 			add_arg_to_input_list(on_tok, arg_tok,
 					      in_tok_first, in_tok_last);
@@ -231,6 +302,7 @@ static void apply_arg_if_exists(struct trailer_item *in_tok,
 			free_trailer_item(arg_tok);
 		break;
 	case EXISTS_ADD_IF_DIFFERENT_NEIGHBOR:
+		apply_item_command(in_tok, arg_tok);
 		if (check_if_different(on_tok, arg_tok, 0))
 			add_arg_to_input_list(on_tok, arg_tok,
 					      in_tok_first, in_tok_last);
@@ -254,6 +326,7 @@ static void apply_arg_if_missing(struct trailer_item **in_tok_first,
 	case MISSING_ADD:
 		where = arg_tok->conf.where;
 		in_tok = after_or_end(where) ? in_tok_last : in_tok_first;
+		apply_item_command(NULL, arg_tok);
 		if (*in_tok) {
 			add_arg_to_input_list(*in_tok, arg_tok,
 					      in_tok_first, in_tok_last);
@@ -537,7 +610,7 @@ static struct trailer_item *new_trailer_item(struct trailer_item *conf_item,
 					     char *tok, char *val)
 {
 	struct trailer_item *new = xcalloc(sizeof(*new), 1);
-	new->value = val;
+	new->value = val ? val : xstrdup("");
 
 	if (conf_item) {
 		duplicate_conf(&new->conf, &conf_item->conf);
@@ -604,7 +677,17 @@ static struct trailer_item *process_command_line_args(struct string_list *traile
 	struct trailer_item *arg_tok_first = NULL;
 	struct trailer_item *arg_tok_last = NULL;
 	struct string_list_item *tr;
+	struct trailer_item *item;
 
+	/* Add a trailer item for each configured trailer with a command */
+	for (item = first_conf_item; item; item = item->next) {
+		if (item->conf.command) {
+			struct trailer_item *new = new_trailer_item(item, NULL, NULL);
+			add_trailer_item(&arg_tok_first, &arg_tok_last, new);
+		}
+	}
+
+	/* Add a trailer item for each trailer on the command line */
 	for_each_string_list_item(tr, trailers) {
 		struct trailer_item *new = create_trailer_item(tr->string);
 		add_trailer_item(&arg_tok_first, &arg_tok_last, new);
