d0d46abc167d18fdbdbf2ece8ca6df704517c62f
Jeff King <peff@peff.net>
2014-10-15 18:41:53 -0400

    pack-objects: refactor unpack-unreachable expiration check
    When we are loosening unreachable packed objects, we do not
    bother to process objects that would simply be pruned
    immediately anyway. The "would be pruned" check is a simple
    comparison, but is about to get more complicated. Let's pull
    it out into a separate function.
    
    Note that this is slightly less efficient than the original,
    which avoided even opening old packs, since no object in
    them could pass the current check, which cares only about
    the pack mtime.  But the new rules will depend on the exact
    object, so we need to perform the check even for old packs.
    
    Note also that we fix a minor buglet when the pack mtime is
    exactly the same as the expiration time. The prune code
    considers that worth pruning, whereas our check here
    considered it worth keeping. This wasn't a big deal. Besides
    being unlikely to happen, the result was simply that the
    object was loosened and then pruned, missing the
    optimization. Still, we can easily fix it while we are here.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/pack-objects.c
+++ builtin/pack-objects.c
@@ -2407,6 +2407,16 @@ static int has_sha1_pack_kept_or_nonlocal(const unsigned char *sha1)
 	return 0;
 }
 
+static int loosened_object_can_be_discarded(const unsigned char *sha1,
+					    unsigned long mtime)
+{
+	if (!unpack_unreachable_expiration)
+		return 0;
+	if (mtime > unpack_unreachable_expiration)
+		return 0;
+	return 1;
+}
+
 static void loosen_unused_packed_objects(struct rev_info *revs)
 {
 	struct packed_git *p;
@@ -2417,17 +2427,14 @@ static void loosen_unused_packed_objects(struct rev_info *revs)
 		if (!p->pack_local || p->pack_keep)
 			continue;
 
-		if (unpack_unreachable_expiration &&
-		    p->mtime < unpack_unreachable_expiration)
-			continue;
-
 		if (open_pack_index(p))
 			die("cannot open pack index");
 
 		for (i = 0; i < p->num_objects; i++) {
 			sha1 = nth_packed_object_sha1(p, i);
 			if (!packlist_find(&to_pack, sha1, NULL) &&
-				!has_sha1_pack_kept_or_nonlocal(sha1))
+			    !has_sha1_pack_kept_or_nonlocal(sha1) &&
+			    !loosened_object_can_be_discarded(sha1, p->mtime))
 				if (force_object_loose(sha1, p->mtime))
 					die("unable to force loose object");
 		}
