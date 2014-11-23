3a0a3a8972369060a1eff8353b64bf7377714b22
Ramsay Jones <ramsay@ramsay1.demon.co.uk>
2014-11-23 17:14:55 +0000

    git-compat-util.h: don't define _XOPEN_SOURCE on cygwin
    A recent update to the gcc compiler (v4.8.3-5 x86_64) on 64-bit
    cygwin leads to several new warnings about the implicit declaration
    of the memmem(), strlcpy() and strcasestr() functions. For example:
    
      CC archive.o
      archive.c: In function 'format_subst':
      archive.c:44:3: warning: implicit declaration of function 'memmem' \
        [-Wimplicit-function-declaration]
         b = memmem(src, len, "$Format:", 8);
           ^
      archive.c:44:5: warning: assignment makes pointer from integer \
        without a cast [enabled by default]
         b = memmem(src, len, "$Format:", 8);
           ^
    
    This is because <string.h> on Cygwin used to always declare the
    above functions, but a recent version of it no longer make them
    visible when _XOPEN_SOURCE is set (even if _GNU_SOURCE and
    _BSD_SOURCE is set).
    
    In order to suppress the warnings, don't define the _XOPEN_SOURCE
    macro on cygwin.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsay1.demon.co.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-compat-util.h
+++ git-compat-util.h
@@ -75,7 +75,8 @@
 # endif
 #elif !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__USLC__) && \
       !defined(_M_UNIX) && !defined(__sgi) && !defined(__DragonFly__) && \
-      !defined(__TANDEM) && !defined(__QNX__) && !defined(__MirBSD__)
+      !defined(__TANDEM) && !defined(__QNX__) && !defined(__MirBSD__) && \
+      !defined(__CYGWIN__)
 #define _XOPEN_SOURCE 600 /* glibc2 and AIX 5.3L need 500, OpenBSD needs 600 for S_ISLNK() */
 #define _XOPEN_SOURCE_EXTENDED 1 /* AIX 5.3L needs this */
 #endif
