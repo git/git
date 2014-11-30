aaa26805add2a8dedf00dda94c816b60479381c6
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-11-30 15:24:34 +0700

    fast-import: use git_path() for accessing .git dir instead of get_git_dir()
    This allows git_path() to redirect info/fast-import to another place
    if needed
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fast-import.c
+++ fast-import.c
@@ -3112,12 +3112,9 @@ static void parse_progress(void)
 
 static char* make_fast_import_path(const char *path)
 {
-	struct strbuf abs_path = STRBUF_INIT;
-
 	if (!relative_marks_paths || is_absolute_path(path))
 		return xstrdup(path);
-	strbuf_addf(&abs_path, "%s/info/fast-import/%s", get_git_dir(), path);
-	return strbuf_detach(&abs_path, NULL);
+	return xstrdup(git_path("info/fast-import/%s", path));
 }
 
 static void option_import_marks(const char *marks,
