e652c0eb5d772076f92245c7e076bf6aaf6af223
Jeff King <peff@peff.net>
2014-12-03 22:52:29 -0500

    prompt: respect GIT_TERMINAL_PROMPT to disable terminal prompts
    If you run git as part of an automated system, you might
    prefer git to die rather than try to issue a prompt on the
    terminal (because there would be nobody to see it and
    respond, and the process would hang forever).
    
    This usually works out of the box because getpass() (and our
    more featureful replacements) will fail when there is no
    tty, but this does not cover all cases. For example, a batch
    system run via ssh might have a tty, even when the user does
    not expect it.
    
    Let's provide an environment variable the user can set to
    avoid even trying to touch the tty at all.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- prompt.c
+++ prompt.c
@@ -58,11 +58,19 @@ char *git_prompt(const char *prompt, int flags)
 			r = do_askpass(askpass, prompt);
 	}
 
-	if (!r)
-		r = git_terminal_prompt(prompt, flags & PROMPT_ECHO);
 	if (!r) {
-		/* prompts already contain ": " at the end */
-		die("could not read %s%s", prompt, strerror(errno));
+		const char *err;
+
+		if (git_env_bool("GIT_TERMINAL_PROMPT", 1)) {
+			r = git_terminal_prompt(prompt, flags & PROMPT_ECHO);
+			err = strerror(errno);
+		} else {
+			err = "terminal prompts disabled";
+		}
+		if (!r) {
+			/* prompts already contain ": " at the end */
+			die("could not read %s%s", prompt, err);
+		}
 	}
 	return r;
 }
