e5e73ff20b5a750b9622438fd877f4e4a4c3519b
Jeff King <peff@peff.net>
2014-12-04 20:28:54 -0500

    for_each_reflog_ent_reverse: fix newlines on block boundaries
    When we read a reflog file in reverse, we read whole chunks
    of BUFSIZ bytes, then loop over the buffer, parsing any
    lines we find. We find the beginning of each line by looking
    for the newline from the previous line. If we don't find
    one, we know that we are either at the beginning of
    the file, or that we have to read another block.
    
    In the latter case, we stuff away what we have into a
    strbuf, read another block, and continue our parse. But we
    missed one case here. If we did find a newline, and it is at
    the beginning of the block, we must also stuff that newline
    into the strbuf, as it belongs to the block we are about to
    read.
    
    The minimal fix here would be to add this special case to
    the conditional that checks whether we found a newline.
    But we can make the flow a little clearer by rearranging a
    bit: we first handle lines that we are going to show, and
    then at the end of each loop, stuff away any leftovers if
    necessary. That lets us fold this special-case in with the
    more common "we ended in the middle of a line" case.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -3089,24 +3089,49 @@ int for_each_reflog_ent_reverse(const char *refname, each_reflog_ent_fn fn, void
 
 			bp = find_beginning_of_line(buf, scanp);
 
-			if (*bp != '\n') {
-				strbuf_splice(&sb, 0, 0, buf, endp - buf);
-				if (pos)
-					break; /* need to fill another block */
-				scanp = buf - 1; /* leave loop */
-			} else {
+			if (*bp == '\n') {
 				/*
-				 * (bp + 1) thru endp is the beginning of the
-				 * current line we have in sb
+				 * The newline is the end of the previous line,
+				 * so we know we have complete line starting
+				 * at (bp + 1). Prefix it onto any prior data
+				 * we collected for the line and process it.
 				 */
 				strbuf_splice(&sb, 0, 0, bp + 1, endp - (bp + 1));
 				scanp = bp;
 				endp = bp + 1;
-			}
-			ret = show_one_reflog_ent(&sb, fn, cb_data);
-			strbuf_reset(&sb);
-			if (ret)
+				ret = show_one_reflog_ent(&sb, fn, cb_data);
+				strbuf_reset(&sb);
+				if (ret)
+					break;
+			} else if (!pos) {
+				/*
+				 * We are at the start of the buffer, and the
+				 * start of the file; there is no previous
+				 * line, and we have everything for this one.
+				 * Process it, and we can end the loop.
+				 */
+				strbuf_splice(&sb, 0, 0, buf, endp - buf);
+				ret = show_one_reflog_ent(&sb, fn, cb_data);
+				strbuf_reset(&sb);
 				break;
+			}
+
+			if (bp == buf) {
+				/*
+				 * We are at the start of the buffer, and there
+				 * is more file to read backwards. Which means
+				 * we are in the middle of a line. Note that we
+				 * may get here even if *bp was a newline; that
+				 * just means we are at the exact end of the
+				 * previous line, rather than some spot in the
+				 * middle.
+				 *
+				 * Save away what we have to be combined with
+				 * the data from the next read.
+				 */
+				strbuf_splice(&sb, 0, 0, buf, endp - buf);
+				break;
+			}
 		}
 
 	}
