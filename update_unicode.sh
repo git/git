1679acdbff6443e07cd880048598d8de45663370
Beat Bolli <dev+git@drbeat.li>
2014-12-19 17:24:23 +0100

    update_unicode.sh: make the output structure visible
    By using a here document instead of the echo/uniset sequence, the
    final structure of the generated file becomes obvious.
    
    Signed-off-by: Beat Bolli <dev+git@drbeat.li>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- update_unicode.sh
+++ update_unicode.sh
@@ -28,12 +28,14 @@ fi &&
 		make
 	) && {
 		UNICODE_DIR=. && export UNICODE_DIR &&
-		echo "static const struct interval zero_width[] = {" &&
-		uniset/uniset --32 cat:Me,Mn,Cf + U+1160..U+11FF - U+00AD |
-		grep -v plane &&
-		echo "};" &&
-		echo "static const struct interval double_width[] = {" &&
-		uniset/uniset --32 eaw:F,W &&
-		echo "};"
+		cat <<-EOF
+		static const struct interval zero_width[] = {
+			$(uniset/uniset --32 cat:Me,Mn,Cf \
+			  + U+1160..U+11FF - U+00AD | grep -v plane)
+		};
+		static const struct interval double_width[] = {
+			$(uniset/uniset --32 eaw:F,W)
+		};
+		EOF
 	} >$UNICODEWIDTH_H
 )
