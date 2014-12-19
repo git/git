3a77c2096d102e43afb7a145c938b68c83f065ab
Beat Bolli <dev+git@drbeat.li>
2014-12-19 17:24:22 +0100

    update_unicode.sh: shorten uniset invocation path
    "uniset/uniset" is a relative path; there's no need to prefix it
    with "./".
    
    Signed-off-by: Beat Bolli <dev+git@drbeat.li>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- update_unicode.sh
+++ update_unicode.sh
@@ -29,11 +29,11 @@ fi &&
 	) && {
 		UNICODE_DIR=. && export UNICODE_DIR &&
 		echo "static const struct interval zero_width[] = {" &&
-		./uniset/uniset --32 cat:Me,Mn,Cf + U+1160..U+11FF - U+00AD |
+		uniset/uniset --32 cat:Me,Mn,Cf + U+1160..U+11FF - U+00AD |
 		grep -v plane &&
 		echo "};" &&
 		echo "static const struct interval double_width[] = {" &&
-		./uniset/uniset --32 eaw:F,W &&
+		uniset/uniset --32 eaw:F,W &&
 		echo "};"
 	} >$UNICODEWIDTH_H
 )
