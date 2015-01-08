99902739174be82851143d4be2a0f85727a9efe0
Alexander Kuleshov <kuleshovmail@gmail.com>
2015-01-09 00:08:36 +0600

    show-branch: line-wrap show-branch usage
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/show-branch.c
+++ builtin/show-branch.c
@@ -6,7 +6,10 @@
 #include "parse-options.h"
 
 static const char* show_branch_usage[] = {
-    N_("git show-branch [-a|--all] [-r|--remotes] [--topo-order | --date-order] [--current] [--color[=<when>] | --no-color] [--sparse] [--more=<n> | --list | --independent | --merge-base] [--no-name | --sha1-name] [--topics] [(<rev> | <glob>)...]"),
+    N_("git show-branch [-a|--all] [-r|--remotes] [--topo-order | --date-order]\n"
+       "		       [--current] [--color[=<when>] | --no-color] [--sparse]\n"
+       "		       [--more=<n> | --list | --independent | --merge-base]\n"
+       "	       [--no-name | --sha1-name] [--topics] [(<rev> | <glob>)...]"),
     N_("git show-branch (-g|--reflog)[=<n>[,<base>]] [--list] [<ref>]"),
     NULL
 };
