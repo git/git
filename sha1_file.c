b0a4264277b7968741580093e7ea1e366943d297
Jonathon Mah <me@jonathonmah.com>
2015-02-08 20:15:39 -0500

    sha1_file: fix iterating loose alternate objects
    The string in 'base' contains a path suffix to a specific object;
    when its value is used, the suffix must either be filled (as in
    stat_sha1_file, open_sha1_file, check_and_freshen_nonlocal) or
    cleared (as in prepare_packed_git) to avoid junk at the end.
    
    660c889e (sha1_file: add for_each iterators for loose and packed
    objects, 2014-10-15) introduced loose_from_alt_odb(), but this did
    neither and treated 'base' as a complete path to the "base" object
    directory, instead of a pointer to the "base" of the full path
    string.
    
    The trailing path after 'base' is still initialized to NUL, hiding
    the bug in some common cases.  Additionally the descendent
    for_each_file_in_obj_subdir() function swallows ENOENT, so an error
    only shows if the alternate's path was last filled with a valid
    object (where statting /path/to/existing/00/0bjectfile/00 fails).
    
    Signed-off-by: Jonathon Mah <me@JonathonMah.com>
    Helped-by: Kyle J. McKay <mackyle@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -3406,9 +3406,16 @@ static int loose_from_alt_odb(struct alternate_object_database *alt,
 			      void *vdata)
 {
 	struct loose_alt_odb_data *data = vdata;
-	return for_each_loose_file_in_objdir(alt->base,
-					     data->cb, NULL, NULL,
-					     data->data);
+	struct strbuf buf = STRBUF_INIT;
+	int r;
+
+	/* copy base not including trailing '/' */
+	strbuf_add(&buf, alt->base, alt->name - alt->base - 1);
+	r = for_each_loose_file_in_objdir_buf(&buf,
+					      data->cb, NULL, NULL,
+					      data->data);
+	strbuf_release(&buf);
+	return r;
 }
 
 int for_each_loose_object(each_loose_object_fn cb, void *data)
