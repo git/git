7b43ee4ae53e6bfedba50a71b5106fc0ef5cd54e
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-02-24 13:07:10 -0600

    Only use CURLOPT_LOGIN_OPTIONS if it is actually available
    This fixes the compilation on an older Linux that was used to debug
    test failures when upgrading Git for Windows to Git v2.3.0.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- imap-send.c
+++ imap-send.c
@@ -1422,11 +1422,15 @@ static CURL *setup_curl(struct imap_server_conf *srvc)
 	curl_easy_setopt(curl, CURLOPT_PORT, server.port);
 
 	if (server.auth_method) {
+#if LIBCURL_VERSION_NUM < 0x072200
+		warning("No LOGIN_OPTIONS support in this cURL version");
+#else
 		struct strbuf auth = STRBUF_INIT;
 		strbuf_addstr(&auth, "AUTH=");
 		strbuf_addstr(&auth, server.auth_method);
 		curl_easy_setopt(curl, CURLOPT_LOGIN_OPTIONS, auth.buf);
 		strbuf_release(&auth);
+#endif
 	}
 
 	if (!server.use_ssl)
