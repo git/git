b680a86a86f44444643b9be5bf4822feda128cd0
Kyle J. McKay <mackyle@gmail.com>
2015-03-07 21:08:00 -0800

    help.c: use SHELL_PATH instead of hard-coded "/bin/sh"
    If the user has set SHELL_PATH in the Makefile then we
    should respect that value and use it.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/help.c
+++ builtin/help.c
@@ -171,7 +171,7 @@ static void exec_man_cmd(const char *cmd, const char *page)
 {
 	struct strbuf shell_cmd = STRBUF_INIT;
 	strbuf_addf(&shell_cmd, "%s %s", cmd, page);
-	execl("/bin/sh", "sh", "-c", shell_cmd.buf, (char *)NULL);
+	execl(SHELL_PATH, SHELL_PATH, "-c", shell_cmd.buf, (char *)NULL);
 	warning(_("failed to exec '%s': %s"), cmd, strerror(errno));
 }
 
