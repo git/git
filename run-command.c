c29b3962af3df80a43fab4ead4875bd2ca275e4c
Jeff King <peff@peff.net>
2015-03-22 23:54:05 -0400

    run-command: forbid using run_command with piped output
    Because run_command both spawns and wait()s for the command
    before returning control to the caller, any reads from the
    pipes we open must necessarily happen after wait() returns.
    This can lead to deadlock, as the child process may block
    on writing to us while we are blocked waiting for it to
    exit.
    
    Worse, it only happens when the child fills the pipe
    buffer, which means that the problem may come and go
    depending on the platform and the size of the output
    produced by the child.
    
    Let's detect and flag this dangerous construct so that we
    can catch potential bugs early in the test suite rather than
    having them happen in the field.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- run-command.c
+++ run-command.c
@@ -561,7 +561,12 @@ int finish_command(struct child_process *cmd)
 
 int run_command(struct child_process *cmd)
 {
-	int code = start_command(cmd);
+	int code;
+
+	if (cmd->out < 0 || cmd->err < 0)
+		die("BUG: run_command with a pipe can cause deadlock");
+
+	code = start_command(cmd);
 	if (code)
 		return code;
 	return finish_command(cmd);
