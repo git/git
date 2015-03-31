01d1c23ca1b6cac3aa34761953fff220fdc9fa81
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-31 17:51:57 +0100

    mingw: avoid warnings when casting HANDLEs to int
    HANDLE is defined internally as a void *, but in many cases it is
    actually guaranteed to be a 32-bit integer. In these cases, GCC should
    not warn about a cast of a pointer to an integer of a different type
    because we know exactly what we are doing.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/winansi.c
+++ compat/winansi.c
@@ -454,7 +454,8 @@ static HANDLE duplicate_handle(HANDLE hnd)
 	HANDLE hresult, hproc = GetCurrentProcess();
 	if (!DuplicateHandle(hproc, hnd, hproc, &hresult, 0, TRUE,
 			DUPLICATE_SAME_ACCESS))
-		die_lasterr("DuplicateHandle(%li) failed", (long) hnd);
+		die_lasterr("DuplicateHandle(%li) failed",
+			(long) (intptr_t) hnd);
 	return hresult;
 }
 
