5621f24dce4b1ee4c58a39027a08bb653ae29238
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-31 18:07:05 +0100

    Silence GCC's "cast of pointer to integer of a different size" warning
    When calculating hashes from pointers, it actually makes sense to cut
    off the most significant bits. In that case, said warning does not make
    a whole lot of sense.
    
    So let's just work around it.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/regex/regcomp.c
+++ compat/regex/regcomp.c
@@ -2577,7 +2577,7 @@ parse_dup_op (bin_tree_t *elem, re_string_t *regexp, re_dfa_t *dfa,
     old_tree = NULL;
 
   if (elem->token.type == SUBEXP)
-    postorder (elem, mark_opt_subexp, (void *) (long) elem->token.opr.idx);
+    postorder (elem, mark_opt_subexp, (void *) (intptr_t) elem->token.opr.idx);
 
   tree = create_tree (dfa, elem, NULL, (end == -1 ? OP_DUP_ASTERISK : OP_ALT));
   if (BE (tree == NULL, 0))
@@ -3806,7 +3806,7 @@ create_token_tree (re_dfa_t *dfa, bin_tree_t *left, bin_tree_t *right,
 static reg_errcode_t
 mark_opt_subexp (void *extra, bin_tree_t *node)
 {
-  int idx = (int) (long) extra;
+  int idx = (int) (intptr_t) extra;
   if (node->token.type == SUBEXP && node->token.opr.idx == idx)
     node->token.opt_subexp = 1;
 
