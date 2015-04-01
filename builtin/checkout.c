fc792ca86049a85f9cf916d6bcdbbc1799ba91ec
Thomas Schneider <thosch97@gmail.com>
2015-04-01 19:38:00 +0200

    checkout: call a single commit "it" intead of "them"
    When detached and checking out a branch again, git checkout warns
    about commit(s) that might get lost.  It says "If you want to keep
    them ..." even for only one commit.
    
    Use Q_() to allow differentiating singular vs plural.
    
    Signed-off-by: Thomas Schneider <thosch97@gmail.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -743,10 +743,17 @@ static void suggest_reattach(struct commit *commit, struct rev_info *revs)
 
 	if (advice_detached_head)
 		fprintf(stderr,
-			_(
+			Q_(
+			/* The singular version */
+			"If you want to keep it by creating a new branch, "
+			"this may be a good time\nto do so with:\n\n"
+			" git branch <new-branch-name> %s\n\n",
+			/* The plural version */
 			"If you want to keep them by creating a new branch, "
 			"this may be a good time\nto do so with:\n\n"
-			" git branch <new-branch-name> %s\n\n"),
+			" git branch <new-branch-name> %s\n\n",
+			/* Give ngettext() the count */
+			lost),
 			find_unique_abbrev(commit->object.sha1, DEFAULT_ABBREV));
 }
 
