1154aa42159383c3339567f6385c21a615c8b93a
Junio C Hamano <gitster@pobox.com>
2015-04-15 14:18:37 -0700

    fmt-merge-msg: plug small leak of commit buffer
    A broken or badly formatted commit might not record author or
    committer lines or we may not find a valid name on them.  The
    function record_person() returned after calling get_commit_buffer()
    without calling unuse_commit_buffer() on the memory it obtained in
    such cases, potentially leaking it.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fmt-merge-msg.c
+++ builtin/fmt-merge-msg.c
@@ -227,16 +227,14 @@ static void add_branch_desc(struct strbuf *out, const char *name)
 
 #define util_as_integral(elem) ((intptr_t)((elem)->util))
 
-static void record_person(int which, struct string_list *people,
-			  struct commit *commit)
+static void record_person_from_buf(int which, struct string_list *people,
+				   const char *buffer)
 {
-	const char *buffer;
 	char *name_buf, *name, *name_end;
 	struct string_list_item *elem;
 	const char *field;
 
 	field = (which == 'a') ? "\nauthor " : "\ncommitter ";
-	buffer = get_commit_buffer(commit);
 	name = strstr(buffer, field);
 	if (!name)
 		return;
@@ -249,7 +247,6 @@ static void record_person(int which, struct string_list *people,
 	if (name_end < name)
 		return;
 	name_buf = xmemdupz(name, name_end - name + 1);
-	unuse_commit_buffer(commit, buffer);
 
 	elem = string_list_lookup(people, name_buf);
 	if (!elem) {
@@ -258,6 +255,15 @@ static void record_person(int which, struct string_list *people,
 	}
 	elem->util = (void*)(util_as_integral(elem) + 1);
 	free(name_buf);
+}
+
+
+static void record_person(int which, struct string_list *people,
+			  struct commit *commit)
+{
+	const char *buffer = get_commit_buffer(commit);
+	record_person_from_buf(which, people, buffer);
+	unuse_commit_buffer(commit, buffer);
 }
 
 static int cmp_string_list_util_as_integral(const void *a_, const void *b_)
