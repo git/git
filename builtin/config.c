7e1105244202d01ddf1de990f215418b01281bcf
Ossi Herrala <oherrala@gmail.com>
2015-04-17 17:50:10 +0300

    config: fix settings in default_user_config template
    The name (not user) and email setting should be in config section
    "user" and not in "core" as documented in Documentation/config.txt.
    
    Signed-off-by: Ossi Herrala <oherrala@gmail.com>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/config.c
+++ builtin/config.c
@@ -463,9 +463,9 @@ static char *default_user_config(void)
 	struct strbuf buf = STRBUF_INIT;
 	strbuf_addf(&buf,
 		    _("# This is Git's per-user configuration file.\n"
-		      "[core]\n"
+		      "[user]\n"
 		      "# Please adapt and uncomment the following lines:\n"
-		      "#	user = %s\n"
+		      "#	name = %s\n"
 		      "#	email = %s\n"),
 		    ident_default_name(),
 		    ident_default_email());
