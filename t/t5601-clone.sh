d1018c2494c05e6a27ec5945902306cfb9e45b0c
brian m. carlson <sandals@crustytoothpaste.net>
2015-04-26 20:30:11 +0000

    t5601: fix quotation error leading to skipped tests
    One of the tests in t5601 used single quotes to delimit an argument
    containing spaces.  However, this caused test_expect_success to be
    passed three arguments instead of two, which in turn caused the test
    name to be treated as a prerequisite instead of a test name.  As there
    was no prerequisite called "bracketed hostnames are still ssh", the test
    was always skipped.
    
    Because this test was always skipped, the fact that it passed the
    arguments in the wrong order was obscured.  Use double quotes inside the
    test and reorder the arguments so that the test runs and properly
    reflects the arguments that are passed to ssh.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5601-clone.sh
+++ t/t5601-clone.sh
@@ -332,7 +332,7 @@ test_expect_success !MINGW,!CYGWIN 'clone local path foo:bar' '
 
 test_expect_success 'bracketed hostnames are still ssh' '
 	git clone "[myhost:123]:src" ssh-bracket-clone &&
-	expect_ssh myhost '-p 123' src
+	expect_ssh "-p 123" myhost src
 '
 
 counter=0
