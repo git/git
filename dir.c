2845ce7ff1398e851d46b62154293378b74c466d
Paul Tan <pyokagan@gmail.com>
2015-05-06 16:01:00 +0800

    dir.c: replace home_config_paths() with xdg_config_home()
    Since only the xdg excludes file path is required, simplify the code by
    replacing use of home_config_paths() with xdg_config_home().
    
    Signed-off-by: Paul Tan <pyokagan@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- dir.c
+++ dir.c
@@ -1622,14 +1622,11 @@ int remove_dir_recursively(struct strbuf *path, int flag)
 void setup_standard_excludes(struct dir_struct *dir)
 {
 	const char *path;
-	char *xdg_path;
 
 	dir->exclude_per_dir = ".gitignore";
 	path = git_path("info/exclude");
-	if (!excludes_file) {
-		home_config_paths(NULL, &xdg_path, "ignore");
-		excludes_file = xdg_path;
-	}
+	if (!excludes_file)
+		excludes_file = xdg_config_home("ignore");
 	if (!access_or_warn(path, R_OK, 0))
 		add_excludes_from_file(dir, path);
 	if (excludes_file && !access_or_warn(excludes_file, R_OK, 0))
