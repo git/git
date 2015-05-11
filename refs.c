c628edfddbf37eefab810a3107a2c32b45abaedc
Michael Haggerty <mhagger@alum.mit.edu>
2015-05-11 17:25:20 +0200

    reflog_expire(): integrate lock_ref_sha1_basic() errors into ours
    Now that lock_ref_sha1_basic() gives us back its error messages via a
    strbuf, incorporate its error message into our error message rather
    than emitting two separate error messages.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- refs.c
+++ refs.c
@@ -4118,9 +4118,9 @@ int reflog_expire(const char *refname, const unsigned char *sha1,
 	 */
 	lock = lock_ref_sha1_basic(refname, sha1, NULL, NULL, 0, &type, &err);
 	if (!lock) {
-		error("%s", err.buf);
+		error("cannot lock ref '%s': %s", refname, err.buf);
 		strbuf_release(&err);
-		return error("cannot lock ref '%s'", refname);
+		return -1;
 	}
 	if (!reflog_exists(refname)) {
 		unlock_ref(lock);
