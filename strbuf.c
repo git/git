4c9d5946b3a911f8870c2e92fcd82b5f29e04fe5
Karsten Blees <blees@dcon.de>
2015-05-11 22:15:40 +0200

    strbuf_readlink: support link targets that exceed PATH_MAX
    strbuf_readlink() refuses to read link targets that exceed PATH_MAX (even
    if a sufficient size was specified by the caller).
    
    As some platforms support longer paths, remove this restriction (similar
    to strbuf_getcwd()).
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- strbuf.c
+++ strbuf.c
@@ -459,8 +459,6 @@ ssize_t strbuf_write(struct strbuf *sb, FILE *f)
 }
 
 
-#define STRBUF_MAXLINK (2*PATH_MAX)
-
 int strbuf_readlink(struct strbuf *sb, const char *path, size_t hint)
 {
 	size_t oldalloc = sb->alloc;
@@ -468,7 +466,7 @@ int strbuf_readlink(struct strbuf *sb, const char *path, size_t hint)
 	if (hint < 32)
 		hint = 32;
 
-	while (hint < STRBUF_MAXLINK) {
+	for (;;) {
 		ssize_t len;
 
 		strbuf_grow(sb, hint + 1);
