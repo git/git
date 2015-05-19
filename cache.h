00b7cbfcb36b6679c64ad0566c0fc251f7e979fe
Junio C Hamano <gitster@pobox.com>
2015-05-19 10:55:16 -0700

    copy.c: make copy_fd() report its status silently
    When copy_fd() function encounters errors, it emits error messages
    itself, which makes it impossible for callers to take responsibility
    for reporting errors, especially when they want to ignore certain
    errors.
    
    Move the error reporting to its callers in preparation.
    
     - copy_file() and copy_file_with_time() by indirection get their
       own calls to error().
    
     - hold_lock_file_for_append(), when told to die on error, used to
       exit(128) relying on the error message from copy_fd(), but now it
       does its own die() instead.  Note that the callers that do not
       pass LOCK_DIE_ON_ERROR need to be adjusted for this change, but
       fortunately there is none ;-)
    
     - filter_buffer_or_fd() has its own error() already, in addition to
       the message from copy_fd(), so this will change the output but
       arguably in a better way.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1482,9 +1482,13 @@ extern const char *git_mailmap_blob;
 extern void maybe_flush_or_die(FILE *, const char *);
 __attribute__((format (printf, 2, 3)))
 extern void fprintf_or_die(FILE *, const char *fmt, ...);
+
+#define COPY_READ_ERROR (-2)
+#define COPY_WRITE_ERROR (-3)
 extern int copy_fd(int ifd, int ofd);
 extern int copy_file(const char *dst, const char *src, int mode);
 extern int copy_file_with_time(const char *dst, const char *src, int mode);
+
 extern void write_or_die(int fd, const void *buf, size_t count);
 extern int write_or_whine(int fd, const void *buf, size_t count, const char *msg);
 extern int write_or_whine_pipe(int fd, const void *buf, size_t count, const char *msg);
