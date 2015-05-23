a29df1b32dc2a76071f40ebd76e50a90a5188c0c
Karsten Blees <blees@dcon.de>
2015-05-24 01:32:03 +0200

    Win32: implement basic symlink() functionality (file symlinks only)
    Implement symlink() that always creates file symlinks. Fails with ENOSYS
    if symlinks are disabled or unsupported.
    
    Note: CreateSymbolicLinkW() was introduced with symlink support in Windows
    Vista. For compatibility with Windows XP, we need to load it dynamically
    and fail gracefully if it isnt's available.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- compat/mingw.h
+++ compat/mingw.h
@@ -93,8 +93,6 @@ struct itimerval {
  * trivial stubs
  */
 
-static inline int symlink(const char *oldpath, const char *newpath)
-{ errno = ENOSYS; return -1; }
 static inline int fchmod(int fildes, mode_t mode)
 { errno = ENOSYS; return -1; }
 #ifndef __MINGW64_VERSION_MAJOR
@@ -180,6 +178,7 @@ struct passwd *getpwuid(uid_t uid);
 int setitimer(int type, struct itimerval *in, struct itimerval *out);
 int sigaction(int sig, struct sigaction *in, struct sigaction *out);
 int link(const char *oldpath, const char *newpath);
+int symlink(const char *target, const char *link);
 int readlink(const char *path, char *buf, size_t bufsiz);
 
 /*
