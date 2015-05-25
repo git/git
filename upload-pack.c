21758affae79a23e402c46cb375a0fd0407050c9
Michael Haggerty <mhagger@alum.mit.edu>
2015-05-25 18:39:13 +0000

    send_ref(): convert local variable "peeled" to object_id
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- upload-pack.c
+++ upload-pack.c
@@ -717,7 +717,7 @@ static int send_ref(const char *refname, const struct object_id *oid,
 		" side-band-64k ofs-delta shallow no-progress"
 		" include-tag multi_ack_detailed";
 	const char *refname_nons = strip_namespace(refname);
-	unsigned char peeled[20];
+	struct object_id peeled;
 
 	if (mark_our_ref(refname, oid))
 		return 0;
@@ -738,8 +738,8 @@ static int send_ref(const char *refname, const struct object_id *oid,
 		packet_write(1, "%s %s\n", oid_to_hex(oid), refname_nons);
 	}
 	capabilities = NULL;
-	if (!peel_ref(refname, peeled))
-		packet_write(1, "%s %s^{}\n", sha1_to_hex(peeled), refname_nons);
+	if (!peel_ref(refname, peeled.hash))
+		packet_write(1, "%s %s^{}\n", oid_to_hex(&peeled), refname_nons);
 	return 0;
 }
 
