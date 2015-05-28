1570856b510e3722a3620063e7ba209106b75857
Jeff King <peff@peff.net>
2015-05-28 03:56:15 -0400

    config.c: avoid xmmap error messages
    The config-writing code uses xmmap to map the existing
    config file, which will die if the map fails. This has two
    downsides:
    
      1. The error message is not very helpful, as it lacks any
         context about the file we are mapping:
    
           $ mkdir foo
           $ git config --file=foo some.key value
           fatal: Out of memory? mmap failed: No such device
    
      2. We normally do not die in this code path; instead, we'd
         rather report the error and return an appropriate exit
         status (which is part of the public interface
         documented in git-config.1).
    
    This patch introduces a "gentle" form of xmmap which lets us
    produce our own error message. We do not want to use mmap
    directly, because we would like to use the other
    compatibility elements of xmmap (e.g., handling 0-length
    maps portably).
    
    The end result is:
    
        $ git.compile config --file=foo some.key value
        error: unable to mmap 'foo': No such device
        $ echo $?
        3
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -2048,8 +2048,15 @@ int git_config_set_multivar_in_file(const char *config_filename,
 
 		fstat(in_fd, &st);
 		contents_sz = xsize_t(st.st_size);
-		contents = xmmap(NULL, contents_sz, PROT_READ,
-			MAP_PRIVATE, in_fd, 0);
+		contents = xmmap_gently(NULL, contents_sz, PROT_READ,
+					MAP_PRIVATE, in_fd, 0);
+		if (contents == MAP_FAILED) {
+			error("unable to mmap '%s': %s",
+			      config_filename, strerror(errno));
+			ret = CONFIG_INVALID_FILE;
+			contents = NULL;
+			goto out_free;
+		}
 		close(in_fd);
 
 		if (chmod(lock->filename.buf, st.st_mode & 07777) < 0) {
