ce4e7b2ac38cfe5e4c165411d2e37b22e445ab88
Jeff King <peff@peff.net>
2015-06-01 05:56:40 -0400

    suppress errors on missing UNINTERESTING links
    When we are traversing commit parents along the
    UNINTERESTING side of a revision walk, we do not care if
    the parent turns out to be missing. That lets us limit
    traversals using unreachable and possibly incomplete
    sections of history. However, we do still print error
    messages about the missing commits; this patch suppresses
    the error, as well.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -807,7 +807,7 @@ static int add_parents_to_list(struct rev_info *revs, struct commit *commit,
 			parent = parent->next;
 			if (p)
 				p->object.flags |= UNINTERESTING;
-			if (parse_commit(p) < 0)
+			if (parse_commit_gently(p, 1) < 0)
 				continue;
 			if (p->parents)
 				mark_parents_uninteresting(p);
