19139a3ac7566f753933c342a001400e064c3ce9
Junio C Hamano <gitster@pobox.com>
2015-06-09 11:15:54 -0700

    SQUASH??? fix misindent

--- wt-status.c
+++ wt-status.c
@@ -1092,14 +1092,14 @@ static void print_rebase_state(struct wt_status *s,
 				struct wt_status_state *state,
 				const char *color)
 {
-		if (state->branch)
-			status_printf_ln(s, color,
-					 _("You are currently rebasing branch '%s' on '%s'."),
-					 state->branch,
-					 state->onto);
-		else
-			status_printf_ln(s, color,
-					 _("You are currently rebasing."));
+	if (state->branch)
+		status_printf_ln(s, color,
+				 _("You are currently rebasing branch '%s' on '%s'."),
+				 state->branch,
+				 state->onto);
+	else
+		status_printf_ln(s, color,
+				 _("You are currently rebasing."));
 }
 
 static void show_rebase_in_progress(struct wt_status *s,
