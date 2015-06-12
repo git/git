d6d1a75e51cf90650ac6e2cdcd8d108324b6fdb9
Jeff King <peff@peff.net>
2015-06-12 17:28:14 -0400

    pkt-line: tighten sideband PACK check when tracing
    To find the start of the pack data, we accept the word PACK
    at the beginning of any sideband channel, even though what
    we really want is to find the pack data on channel 1. In
    practice this doesn't matter, as sideband-2 messages tend to
    start with "error:" or similar, but it is a good idea to be
    explicit (especially as we add more code in this area, we
    will rely on this assumption).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pkt-line.c
+++ pkt-line.c
@@ -24,7 +24,7 @@ static void packet_trace(const char *buf, unsigned int len, int write)
 	strbuf_addf(&out, "packet: %12s%c ",
 		    packet_trace_prefix, write ? '>' : '<');
 
-	if (starts_with(buf, "PACK") || starts_with(buf + 1, "PACK")) {
+	if (starts_with(buf, "PACK") || starts_with(buf, "\1PACK")) {
 		strbuf_addstr(&out, "PACK ...");
 		trace_disable(&trace_packet);
 	}
