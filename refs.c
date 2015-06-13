1383ec339500edd0807836832e8eb2ba846a2e2e
Michael Haggerty <mhagger@alum.mit.edu>
2015-06-13 16:42:10 +0200

    repack_without_refs(): make function private
    It is no longer called from outside of the refs module. Also move its
    docstring and change it to imperative voice.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -2736,7 +2736,14 @@ int pack_refs(unsigned int flags)
 	return 0;
 }
 
-int repack_without_refs(struct string_list *refnames, struct strbuf *err)
+/*
+ * Rewrite the packed-refs file, omitting any refs listed in
+ * 'refnames'. On error, leave packed-refs unchanged, write an error
+ * message to 'err', and return a nonzero value.
+ *
+ * The refs in 'refnames' needn't be sorted. `err` must not be NULL.
+ */
+static int repack_without_refs(struct string_list *refnames, struct strbuf *err)
 {
 	struct ref_dir *packed;
 	struct string_list_item *refname;
