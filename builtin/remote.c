25cb9aaccc1dd0245634da38e24da7a9634758c6
Michael Haggerty <mhagger@alum.mit.edu>
2015-06-13 16:42:09 +0200

    prune_remote(): use delete_refs()
    This will result in errors being emitted for references that can't be
    deleted, but that is a good thing.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/remote.c
+++ builtin/remote.c
@@ -1314,18 +1314,11 @@ static int prune_remote(const char *remote, int dry_run)
 		string_list_append(&refs_to_prune, item->util);
 	string_list_sort(&refs_to_prune);
 
-	if (!dry_run) {
-		struct strbuf err = STRBUF_INIT;
-		if (repack_without_refs(&refs_to_prune, &err))
-			result |= error("%s", err.buf);
-		strbuf_release(&err);
-	}
+	if (!dry_run)
+		result |= delete_refs(&refs_to_prune);
 
 	for_each_string_list_item(item, &states.stale) {
 		const char *refname = item->util;
-
-		if (!dry_run)
-			result |= delete_ref(refname, NULL, 0);
 
 		if (dry_run)
 			printf_ln(_(" * [would prune] %s"),
