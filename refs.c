300f8137c21fad7e0c0e999bd4ddd4fd3355427f
Michael Haggerty <mhagger@alum.mit.edu>
2015-06-13 16:42:08 +0200

    delete_refs(): improve error message
    Change the error message from
    
        Could not remove branch %s
    
    to
    
        could not remove reference %s
    
    * s/branch/reference/. This change makes sense even for the existing
      caller, which uses the function to delete remote-tracking
      branches.
    
    * Convert it to lower case, as per our usual convention.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -2843,7 +2843,7 @@ int delete_refs(struct string_list *refnames)
 		const char *refname = refnames->items[i].string;
 
 		if (delete_ref(refname, NULL, 0))
-			result |= error(_("Could not remove branch %s"), refname);
+			result |= error(_("could not remove reference %s"), refname);
 	}
 
 	return result;
