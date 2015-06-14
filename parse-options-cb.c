6b3ee18dc5c620d7cb4324e009339b5ca9ac488c
Paul Tan <pyokagan@gmail.com>
2015-06-14 16:41:48 +0800

    parse-options-cb: implement parse_opt_passthru()
    Certain git commands, such as git-pull, are simply wrappers around other
    git commands like git-fetch, git-merge and git-rebase. As such, these
    wrapper commands will typically need to "pass through" command-line
    options of the commands they wrap.
    
    Implement the parse_opt_passthru() parse-options callback, which will
    reconstruct the command-line option into an char* string, such that it
    can be passed to another git command.
    
    Helped-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Helped-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Paul Tan <pyokagan@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options-cb.c
+++ parse-options-cb.c
@@ -134,3 +134,52 @@ int parse_opt_noop_cb(const struct option *opt, const char *arg, int unset)
 {
 	return 0;
 }
+
+/**
+ * Recreates the command-line option in the strbuf.
+ */
+static int recreate_opt(struct strbuf *sb, const struct option *opt,
+		const char *arg, int unset)
+{
+	strbuf_reset(sb);
+
+	if (opt->long_name) {
+		strbuf_addstr(sb, unset ? "--no-" : "--");
+		strbuf_addstr(sb, opt->long_name);
+		if (arg) {
+			strbuf_addch(sb, '=');
+			strbuf_addstr(sb, arg);
+		}
+	} else if (opt->short_name && !unset) {
+		strbuf_addch(sb, '-');
+		strbuf_addch(sb, opt->short_name);
+		if (arg)
+			strbuf_addstr(sb, arg);
+	} else
+		return -1;
+
+	return 0;
+}
+
+/**
+ * For an option opt, recreates the command-line option in opt->value which
+ * must be an char* initialized to NULL. This is useful when we need to pass
+ * the command-line option to another command. Since any previous value will be
+ * overwritten, this callback should only be used for options where the last
+ * one wins.
+ */
+int parse_opt_passthru(const struct option *opt, const char *arg, int unset)
+{
+	static struct strbuf sb = STRBUF_INIT;
+	char **opt_value = opt->value;
+
+	if (recreate_opt(&sb, opt, arg, unset) < 0)
+		return -1;
+
+	if (*opt_value)
+		free(*opt_value);
+
+	*opt_value = strbuf_detach(&sb, NULL);
+
+	return 0;
+}
