5930d1e9c86b9578adb975cdcb07c848b41a8ba1
Junio C Hamano <gitster@pobox.com>
2015-06-15 11:55:24 -0700

    SQUASH avoid die(variable)

--- builtin/clone.c
+++ builtin/clone.c
@@ -496,18 +496,18 @@ static void write_remote_refs(const struct ref *local_refs)
 
 	t = ref_transaction_begin(&err);
 	if (!t)
-		die(err.buf);
+		die("%s", err.buf);
 
 	for (r = local_refs; r; r = r->next) {
 		if (!r->peer_ref)
 			continue;
 		if (ref_transaction_create(t, r->peer_ref->name, r->old_sha1,
 					   0, NULL, &err))
-			die(err.buf);
+			die("%s", err.buf);
 	}
 
 	if (initial_ref_transaction_commit(t, &err))
-		die(err.buf);
+		die("%s", err.buf);
 
 	strbuf_release(&err);
 	ref_transaction_free(t);
