b1456605c26eb6bd991b70b0ca0a3ce0f02473e9
Paul Tan <pyokagan@gmail.com>
2015-06-18 18:54:12 +0800

    pull: remove redirection to git-pull.sh
    At the beginning of the rewrite of git-pull.sh to C, we introduced a
    redirection to git-pull.sh if the environment variable
    _GIT_USE_BUILTIN_PULL was not defined in order to not break test scripts
    that relied on a functional git-pull.
    
    Now that all of git-pull's functionality has been re-implemented in
    builtin/pull.c, remove this redirection, and retire the old git-pull.sh
    into contrib/examples/.
    
    Signed-off-by: Paul Tan <pyokagan@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- Makefile
+++ Makefile
@@ -474,7 +474,6 @@ SCRIPT_SH += git-merge-octopus.sh
 SCRIPT_SH += git-merge-one-file.sh
 SCRIPT_SH += git-merge-resolve.sh
 SCRIPT_SH += git-mergetool.sh
-SCRIPT_SH += git-pull.sh
 SCRIPT_SH += git-quiltimport.sh
 SCRIPT_SH += git-rebase.sh
 SCRIPT_SH += git-remote-testgit.sh
--- builtin/pull.c
+++ builtin/pull.c
@@ -798,13 +798,6 @@ int cmd_pull(int argc, const char **argv, const char *prefix)
 	unsigned char orig_head[GIT_SHA1_RAWSZ], curr_head[GIT_SHA1_RAWSZ];
 	unsigned char rebase_fork_point[GIT_SHA1_RAWSZ];
 
-	if (!getenv("_GIT_USE_BUILTIN_PULL")) {
-		const char *path = mkpath("%s/git-pull", git_exec_path());
-
-		if (sane_execvp(path, (char **)argv) < 0)
-			die_errno("could not exec %s", path);
-	}
-
 	if (!getenv("GIT_REFLOG_ACTION"))
 		set_reflog_message(argc, argv);
 
