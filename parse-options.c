2a514ed8058e35841d3d7b05a898991b83e5eaf0
Charles Bailey <cbailey32@bloomberg.net>
2015-06-21 19:25:44 +0100

    parse-options: move unsigned long option parsing out of pack-objects.c
    The unsigned long option parsing (including 'k'/'m'/'g' suffix
    parsing) is more widely applicable.  Add support for OPT_MAGNITUDE
    to parse-options.h and change pack-objects.c use this support.
    
    The error behavior on parse errors follows that of OPT_INTEGER.  The
    name of the option that failed to parse is reported with a brief
    message describing the expect format for the option argument and
    then the full usage message for the command invoked.
    
    This differs from the previous behavior for OPT_ULONG used in
    pack-objects for --max-pack-size and --window-memory which used to
    display the value supplied in the error message and did not display
    the full usage message.
    
    Signed-off-by: Charles Bailey <cbailey32@bloomberg.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.c
+++ parse-options.c
@@ -180,6 +180,23 @@ static int get_value(struct parse_opt_ctx_t *p,
 			return opterror(opt, "expects a numerical value", flags);
 		return 0;
 
+	case OPTION_MAGNITUDE:
+		if (unset) {
+			*(unsigned long *)opt->value = 0;
+			return 0;
+		}
+		if (opt->flags & PARSE_OPT_OPTARG && !p->opt) {
+			*(unsigned long *)opt->value = opt->defval;
+			return 0;
+		}
+		if (get_arg(p, opt, flags, &arg))
+			return -1;
+		if (!git_parse_ulong(arg, opt->value))
+			return opterror(opt,
+				"expects a non-negative integer value with an optional k/m/g suffix",
+				flags);
+		return 0;
+
 	default:
 		die("should not happen, someone must be hit on the forehead");
 	}
