925d73c4217388838e36bfed85553132c458c7d0
Junio C Hamano <gitster@pobox.com>
2015-07-06 14:18:09 -0700

    rerere: further clarify do_rerere_one_path()
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- rerere.c
+++ rerere.c
@@ -653,16 +653,13 @@ static void do_rerere_one_path(struct string_list_item *rr_item,
 			fprintf(stderr,
 				"Resolved '%s' using previous resolution.\n",
 				path);
-		goto mark_resolved;
+	} else if (!handle_file(path, NULL, NULL)) {
+		/* The user has resolved it. */
+		copy_file(rerere_path(name, "postimage"), path, 0666);
+		fprintf(stderr, "Recorded resolution for '%s'.\n", path);
+	} else {
+		return;
 	}
-
-	/* Let's see if the user has resolved it. */
-	if (handle_file(path, NULL, NULL))
-		return; /* not yet resolved */
-
-	copy_file(rerere_path(name, "postimage"), path, 0666);
-	fprintf(stderr, "Recorded resolution for '%s'.\n", path);
-mark_resolved:
 	free(rr_item->util);
 	rr_item->util = NULL;
 }
