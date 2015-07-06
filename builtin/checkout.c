338dfd0da4b7fd3ccf0772788d64cbcdf089f486
Eric Sunshine <sunshine@sunshineco.com>
2015-07-06 13:30:48 -0400

    checkout: make --to unconditionally verbose
    prepare_linked_checkout() respects git-checkout's --quiet flag, however,
    the plan is to relocate "git checkout --to" functionality to "git
    worktree add", and git-worktree does not (yet) have a --quiet flag.
    Consequently, make prepare_linked_checkout() unconditionally verbose to
    ease eventual code movement to worktree.c.
    
    (A --quiet flag can be added to git-worktree later if there is demand
    for it.)
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -936,8 +936,7 @@ static int prepare_linked_checkout(const struct checkout_opts *opts)
 	strbuf_addf(&sb, "%s/commondir", sb_repo.buf);
 	write_file(sb.buf, 1, "../..\n");
 
-	if (!opts->quiet)
-		fprintf_ln(stderr, _("Enter %s (identifier %s)"), path, name);
+	fprintf_ln(stderr, _("Enter %s (identifier %s)"), path, name);
 
 	setenv("GIT_CHECKOUT_NEW_WORKTREE", "1", 1);
 	setenv(GIT_DIR_ENVIRONMENT, sb_git.buf, 1);
