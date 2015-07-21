69f9c87d4654aad8011444727194c3053827ae07
Zoë Blade <zoe@bytenoise.co.uk>
2015-07-21 14:22:46 +0100

    userdiff: add support for Fountain documents
    Add support for Fountain, a plain text screenplay format.  Git
    facilitates not just programming specifically, but creative writing
    in general, so it makes sense to also support other plain text
    documents besides source code.
    
    In the structure of a screenplay specifically, scenes are roughly
    analogous to functions, in the sense that it makes your job easier
    if you can see which ones were changed in a given range of patches.
    
    More information about the Fountain format can be found on its
    official website, at http://fountain.io .
    
    Signed-off-by: Zoë Blade <zoe@bytenoise.co.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- userdiff.c
+++ userdiff.c
@@ -35,6 +35,8 @@ IPATTERN("fortran",
 	  * they would have been matched above as a variable anyway. */
 	 "|[-+]?[0-9.]+([AaIiDdEeFfLlTtXx][Ss]?[-+]?[0-9.]*)?(_[a-zA-Z0-9][a-zA-Z0-9_]*)?"
 	 "|//|\\*\\*|::|[/<>=]="),
+IPATTERN("fountain", "^((\\.[^.]|(int|ext|est|int\\.?/ext|i/e)[. ]).*)$",
+	 "[^ \t-]+"),
 PATTERNS("html", "^[ \t]*(<[Hh][1-6][ \t].*>.*)$",
 	 "[^<>= \t]+"),
 PATTERNS("java",
