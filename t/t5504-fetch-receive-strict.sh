af1d954b2d3115dc0cf6e647df94ff2b7082d4ea
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-07-28 21:21:26 +0200

    fixup! Work around a problem identified by BuildHive

--- t/t5504-fetch-receive-strict.sh
+++ t/t5504-fetch-receive-strict.sh
@@ -101,7 +101,10 @@ test_expect_success 'push with receive.fsckobjects' '
 		git config transfer.fsckobjects false
 	) &&
 	test_must_fail git push --porcelain dst master:refs/heads/test >act &&
-	test_cmp exp act || test ! -s act
+	if ! test_cmp exp act
+	then
+		test ! -s act
+	fi
 '
 
 test_expect_success 'push with transfer.fsckobjects' '
@@ -112,7 +115,10 @@ test_expect_success 'push with transfer.fsckobjects' '
 		git config transfer.fsckobjects true
 	) &&
 	test_must_fail git push --porcelain dst master:refs/heads/test >act &&
-	test_cmp exp act || test ! -s act
+	if ! test_cmp exp act
+	then
+		test ! -s act
+	fi
 '
 
 test_done
