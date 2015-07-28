bc598c32ae41cd355169ca7b207c2e35d9449232
Michael Haggerty <mhagger@alum.mit.edu>
2015-07-28 23:08:21 +0200

    get_remote_group(): use skip_prefix()
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -973,8 +973,7 @@ static int get_remote_group(const char *key, const char *value, void *priv)
 {
 	struct remote_group_data *g = priv;
 
-	if (starts_with(key, "remotes.") &&
-			!strcmp(key + 8, g->name)) {
+	if (skip_prefix(key, "remotes.", &key) && !strcmp(key, g->name)) {
 		/* split list by white space */
 		while (*value) {
 			size_t wordlen = strcspn(value, " \t\n");
