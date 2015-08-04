52afa7a08d09e74f65f9ce1f76d8b0bad5a57abe
kweij <klaas.werkt@gmail.com>
2015-08-04 11:16:13 +0200

    Update branch.c cmd_branch instructions
    Textual change in instruction to hint the correct git command: track upstream instead of delete.

--- builtin/branch.c
+++ builtin/branch.c
@@ -1033,7 +1033,7 @@ int cmd_branch(int argc, const char **argv, const char *prefix)
 		if (argc == 1 && track == BRANCH_TRACK_OVERRIDE &&
 		    !branch_existed && remote_tracking) {
 			fprintf(stderr, _("\nIf you wanted to make '%s' track '%s', do this:\n\n"), head, branch->name);
-			fprintf(stderr, _("    git branch -d %s\n"), branch->name);
+			fprintf(stderr, _("    git branch -u %s\n"), branch->name);
 			fprintf(stderr, _("    git branch --set-upstream-to %s\n"), branch->name);
 		}
 
