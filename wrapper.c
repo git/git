3ff53df7b4cbc331d302181d2d6644f4cb860a52
Paul Tan <pyokagan@gmail.com>
2015-08-04 21:51:22 +0800

    wrapper: implement xopen()
    A common usage pattern of open() is to check if it was successful, and
    die() if it was not:
    
    	int fd = open(path, O_WRONLY | O_CREAT, 0777);
    	if (fd < 0)
    		die_errno(_("Could not open '%s' for writing."), path);
    
    Implement a wrapper function xopen() that does the above so that we can
    save a few lines of code, and make the die() messages consistent.
    
    Helped-by: Torsten Bögershausen <tboegi@web.de>
    Helped-by: Jeff King <peff@peff.net>
    Helped-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Paul Tan <pyokagan@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -189,6 +189,41 @@ void *xcalloc(size_t nmemb, size_t size)
 # endif
 #endif
 
+/**
+ * xopen() is the same as open(), but it die()s if the open() fails.
+ */
+int xopen(const char *path, int oflag, ...)
+{
+	mode_t mode = 0;
+	va_list ap;
+
+	/*
+	 * va_arg() will have undefined behavior if the specified type is not
+	 * compatible with the argument type. Since integers are promoted to
+	 * ints, we fetch the next argument as an int, and then cast it to a
+	 * mode_t to avoid undefined behavior.
+	 */
+	va_start(ap, oflag);
+	if (oflag & O_CREAT)
+		mode = va_arg(ap, int);
+	va_end(ap);
+
+	for (;;) {
+		int fd = open(path, oflag, mode);
+		if (fd >= 0)
+			return fd;
+		if (errno == EINTR)
+			continue;
+
+		if ((oflag & O_RDWR) == O_RDWR)
+			die_errno(_("could not open '%s' for reading and writing"), path);
+		else if ((oflag & O_WRONLY) == O_WRONLY)
+			die_errno(_("could not open '%s' for writing"), path);
+		else
+			die_errno(_("could not open '%s' for reading"), path);
+	}
+}
+
 /*
  * xread() is the same a read(), but it automatically restarts read()
  * operations with a recoverable error (EAGAIN and EINTR). xread()
