9e9de18f1ad39901a8f0c67f0af70d66d427e326
Jeff King <peff@peff.net>
2015-08-24 02:11:33 -0400

    config: silence warnings for command names with invalid keys
    When we are running the git command "foo", we may have to
    look up the config keys "pager.foo" and "alias.foo". These
    config schemes are mis-designed, as the command names can be
    anything, but the config syntax has some restrictions. For
    example:
    
      $ git foo_bar
      error: invalid key: pager.foo_bar
      error: invalid key: alias.foo_bar
      git: 'foo_bar' is not a git command. See 'git --help'.
    
    You cannot name an alias with an underscore. And if you have
    an external command with one, you cannot configure its
    pager.
    
    In the long run, we may develop a different config scheme
    for these features. But in the near term (and because we'll
    need to support the existing scheme indefinitely), we should
    at least squelch the error messages shown above.
    
    These errors come from git_config_parse_key. Ideally we
    would pass a "quiet" flag to the config machinery, but there
    are many layers between the pager code and the key parsing.
    Passing a flag through all of those would be an invasive
    change.
    
    Instead, let's provide a config function to report on
    whether a key is syntactically valid, and have the pager and
    alias code skip lookup for bogus keys. We can build this
    easily around the existing git_config_parse_key, with two
    minor modifications:
    
      1. We now handle a NULL store_key, to validate but not
         write out the normalized key.
    
      2. We accept a "quiet" flag to avoid writing to stderr.
         This doesn't need to be a full-blown public "flags"
         field, because we can make the existing implementation
         a static helper function, keeping the mess contained
         inside config.c.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -1843,7 +1843,7 @@ int git_config_set(const char *key, const char *value)
  * baselen - pointer to int which will hold the length of the
  *           section + subsection part, can be NULL
  */
-int git_config_parse_key(const char *key, char **store_key, int *baselen_)
+static int git_config_parse_key_1(const char *key, char **store_key, int *baselen_, int quiet)
 {
 	int i, dot, baselen;
 	const char *last_dot = strrchr(key, '.');
@@ -1854,12 +1854,14 @@ int git_config_parse_key(const char *key, char **store_key, int *baselen_)
 	 */
 
 	if (last_dot == NULL || last_dot == key) {
-		error("key does not contain a section: %s", key);
+		if (!quiet)
+			error("key does not contain a section: %s", key);
 		return -CONFIG_NO_SECTION_OR_NAME;
 	}
 
 	if (!last_dot[1]) {
-		error("key does not contain variable name: %s", key);
+		if (!quiet)
+			error("key does not contain variable name: %s", key);
 		return -CONFIG_NO_SECTION_OR_NAME;
 	}
 
@@ -1870,7 +1872,8 @@ int git_config_parse_key(const char *key, char **store_key, int *baselen_)
 	/*
 	 * Validate the key and while at it, lower case it for matching.
 	 */
-	*store_key = xmalloc(strlen(key) + 1);
+	if (store_key)
+		*store_key = xmalloc(strlen(key) + 1);
 
 	dot = 0;
 	for (i = 0; key[i]; i++) {
@@ -1881,24 +1884,40 @@ int git_config_parse_key(const char *key, char **store_key, int *baselen_)
 		if (!dot || i > baselen) {
 			if (!iskeychar(c) ||
 			    (i == baselen + 1 && !isalpha(c))) {
-				error("invalid key: %s", key);
+				if (!quiet)
+					error("invalid key: %s", key);
 				goto out_free_ret_1;
 			}
 			c = tolower(c);
 		} else if (c == '\n') {
-			error("invalid key (newline): %s", key);
+			if (!quiet)
+				error("invalid key (newline): %s", key);
 			goto out_free_ret_1;
 		}
-		(*store_key)[i] = c;
+		if (store_key)
+			(*store_key)[i] = c;
 	}
-	(*store_key)[i] = 0;
+	if (store_key)
+		(*store_key)[i] = 0;
 
 	return 0;
 
 out_free_ret_1:
-	free(*store_key);
-	*store_key = NULL;
+	if (store_key) {
+		free(*store_key);
+		*store_key = NULL;
+	}
 	return -CONFIG_INVALID_KEY;
+}
+
+int git_config_parse_key(const char *key, char **store_key, int *baselen)
+{
+	return git_config_parse_key_1(key, store_key, baselen, 0);
+}
+
+int git_config_key_is_valid(const char *key)
+{
+	return !git_config_parse_key_1(key, NULL, NULL, 1);
 }
 
 /*
--- pager.c
+++ pager.c
@@ -149,7 +149,8 @@ int check_pager_config(const char *cmd)
 	struct strbuf key = STRBUF_INIT;
 	const char *value = NULL;
 	strbuf_addf(&key, "pager.%s", cmd);
-	if (!git_config_get_value(key.buf, &value)) {
+	if (git_config_key_is_valid(key.buf) &&
+	    !git_config_get_value(key.buf, &value)) {
 		int b = git_config_maybe_bool(key.buf, value);
 		if (b >= 0)
 			want = b;
