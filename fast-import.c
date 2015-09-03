547ed71636dbfab2dc303ba8cabb29c2cab22f1e
Jeff King <peff@peff.net>
2015-09-03 22:48:55 +0100

    fast-import: switch crash-report date to iso8601
    When fast-import emits a crash report, it does so in the
    user's local timezone. But because we omit the timezone
    completely for DATE_LOCAL, a reader of the report does not
    immediately know which time zone was used. Let's switch this
    to ISO8601 instead, which includes the time zone.
    
    This does mean we will show the time in UTC, but that's not
    a big deal. A crash report like this will either be looked
    at immediately (in which case nobody even looks at the
    timestamp), or it will be passed along to a developer to
    debug, in which case the original timezone is less likely to
    be of interest.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: John Keeping <john@keeping.me.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fast-import.c
+++ fast-import.c
@@ -421,7 +421,7 @@ static void write_crash_report(const char *err)
 	fprintf(rpt, "fast-import crash report:\n");
 	fprintf(rpt, "    fast-import process: %"PRIuMAX"\n", (uintmax_t) getpid());
 	fprintf(rpt, "    parent process     : %"PRIuMAX"\n", (uintmax_t) getppid());
-	fprintf(rpt, "    at %s\n", show_date(time(NULL), 0, DATE_MODE(LOCAL)));
+	fprintf(rpt, "    at %s\n", show_date(time(NULL), 0, DATE_MODE(ISO8601)));
 	fputc('\n', rpt);
 
 	fputs("fatal: ", rpt);
