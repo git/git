b7301fb4cf7d6075e1ce2c6cd9226b15d855e3db
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-09-18 17:32:26 +0200

    t0302: add forgotten quotes
    This was probably missed because nobody had a left-over `trash/`
    directory and the `-f` flag made sure that no error message was
    produced when the file was not found that *actually* wanted to
    be removed.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t0302-credential-store.sh
+++ t/t0302-credential-store.sh
@@ -37,7 +37,7 @@ helper_test store
 unset XDG_CONFIG_HOME
 
 test_expect_success 'if custom xdg file exists, home and xdg files not created' '
-	test_when_finished "rm -f $HOME/xdg/git/credentials" &&
+	test_when_finished "rm -f \"$HOME/xdg/git/credentials\"" &&
 	test -s "$HOME/xdg/git/credentials" &&
 	test_path_is_missing "$HOME/.git-credentials" &&
 	test_path_is_missing "$HOME/.config/git/credentials"
