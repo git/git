7cd17e80579c2bffd6245837175a6e1b12a78045
Jeff King <peff@peff.net>
2015-09-24 17:02:54 -0400

    show-branch: avoid segfault with --reflog of unborn branch
    When no branch is given to the "--reflog" option, we resolve
    HEAD to get the default branch. However, if HEAD points to
    an unborn branch, resolve_ref returns NULL, and we later
    segfault trying to access it.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/show-branch.c
+++ builtin/show-branch.c
@@ -743,6 +743,8 @@ int cmd_show_branch(int ac, const char **av, const char *prefix)
 			fake_av[1] = NULL;
 			av = fake_av;
 			ac = 1;
+			if (!*av)
+				die("no branches given, and HEAD is not valid");
 		}
 		if (ac != 1)
 			die("--reflog option needs one branch name");
