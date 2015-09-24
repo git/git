f28e3ab231d36c454445fe778ffb931920606109
Jeff King <peff@peff.net>
2015-09-24 17:07:18 -0400

    read_branches_file: simplify string handling
    This function does a lot of manual string handling, and has
    some unnecessary limits. This patch cleans up a number of
    things:
    
      1. Drop the arbitrary 1000-byte limit on the size of the
         remote name (we do not have such a limit in any of the
         other remote-reading mechanisms).
    
      2. Replace fgets into a fixed-size buffer with a strbuf,
         eliminating any limits on the length of the URL.
    
      3. Replace manual whitespace handling with strbuf_trim
         (since we now have a strbuf). This also gets rid
         of a call to strcpy, and the confusing reuse of the "p"
         pointer for multiple purposes.
    
      4. We currently build up the refspecs over multiple strbuf
         calls. We do this to handle the fact that the URL "frag"
         may not be present. But rather than have multiple
         conditionals, let's just default "frag" to "master".
         This lets us format the refspecs with a single xstrfmt.
         It's shorter, and easier to see what the final string
         looks like.
    
         We also update the misleading comment in this area (the
         local branch is named after the remote name, not after
         the branch name on the remote side).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -293,56 +293,42 @@ static void read_remotes_file(struct remote *remote)
 static void read_branches_file(struct remote *remote)
 {
 	char *frag;
-	struct strbuf branch = STRBUF_INIT;
-	int n = 1000;
-	FILE *f = fopen(git_path("branches/%.*s", n, remote->name), "r");
-	char *s, *p;
-	int len;
+	struct strbuf buf = STRBUF_INIT;
+	FILE *f = fopen(git_path("branches/%s", remote->name), "r");
 
 	if (!f)
 		return;
-	s = fgets(buffer, BUF_SIZE, f);
-	fclose(f);
-	if (!s)
+
+	strbuf_getline(&buf, f, '\n');
+	strbuf_trim(&buf);
+	if (!buf.len) {
+		strbuf_release(&buf);
 		return;
-	while (isspace(*s))
-		s++;
-	if (!*s)
-		return;
+	}
+
 	remote->origin = REMOTE_BRANCHES;
-	p = s + strlen(s);
-	while (isspace(p[-1]))
-		*--p = 0;
-	len = p - s;
-	p = xmalloc(len + 1);
-	strcpy(p, s);
 
 	/*
 	 * The branches file would have URL and optionally
 	 * #branch specified.  The "master" (or specified) branch is
-	 * fetched and stored in the local branch of the same name.
+	 * fetched and stored in the local branch matching the
+	 * remote name.
 	 */
-	frag = strchr(p, '#');
-	if (frag) {
+	frag = strchr(buf.buf, '#');
+	if (frag)
 		*(frag++) = '\0';
-		strbuf_addf(&branch, "refs/heads/%s", frag);
-	} else
-		strbuf_addstr(&branch, "refs/heads/master");
+	else
+		frag = "master";
 
-	strbuf_addf(&branch, ":refs/heads/%s", remote->name);
-	add_url_alias(remote, p);
-	add_fetch_refspec(remote, strbuf_detach(&branch, NULL));
+	add_url_alias(remote, strbuf_detach(&buf, NULL));
+	add_fetch_refspec(remote, xstrfmt("refs/heads/%s:refs/heads/%s",
+					  frag, remote->name));
+
 	/*
 	 * Cogito compatible push: push current HEAD to remote #branch
 	 * (master if missing)
 	 */
-	strbuf_init(&branch, 0);
-	strbuf_addstr(&branch, "HEAD");
-	if (frag)
-		strbuf_addf(&branch, ":refs/heads/%s", frag);
-	else
-		strbuf_addstr(&branch, ":refs/heads/master");
-	add_push_refspec(remote, strbuf_detach(&branch, NULL));
+	add_push_refspec(remote, xstrfmt("HEAD:refs/heads/%s", frag));
 	remote->fetch_tags = 1; /* always auto-follow */
 }
 
