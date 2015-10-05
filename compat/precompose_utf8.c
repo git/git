e2b021eb5b5ce2f4be3c10f1f8063981b1a53053
Jeff King <peff@peff.net>
2015-10-04 23:43:14 -0400

    precompose_utf8: drop unused variable
    The result of iconv is assigned to a variable, but we never
    use it (instead, we check errno and whether the function
    consumed all bytes). Let's drop the assignment, as it
    triggers gcc's -Wunused-but-set-variable.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/precompose_utf8.c
+++ compat/precompose_utf8.c
@@ -139,9 +139,8 @@ struct dirent_prec_psx *precompose_utf8_readdir(PREC_DIR *prec_dir)
 				size_t inleft = namelenz;
 				char *outpos = &prec_dir->dirent_nfc->d_name[0];
 				size_t outsz = prec_dir->dirent_nfc->max_name_len;
-				size_t cnt;
 				errno = 0;
-				cnt = iconv(prec_dir->ic_precompose, &cp, &inleft, &outpos, &outsz);
+				iconv(prec_dir->ic_precompose, &cp, &inleft, &outpos, &outsz);
 				if (errno || inleft) {
 					/*
 					 * iconv() failed and errno could be E2BIG, EILSEQ, EINVAL, EBADF
