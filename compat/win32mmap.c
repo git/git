1fda6249f7c4610103ea26c8429fb3127d110852
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-10-13 12:29:58 +0200

    mmap(win32): avoid expensive fstat() call
    On Windows, we have to emulate the fstat() call to fill out information
    that takes extra effort to obtain, such as the file permissions/type.
    
    If all we want is the file size, we can use the much cheaper
    GetFileSizeEx() function (available since Windows XP).
    
    Suggested by Philip Kelley.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/win32mmap.c
+++ compat/win32mmap.c
@@ -2,26 +2,24 @@
 
 void *git_mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	HANDLE hmap;
+	HANDLE osfhandle, hmap;
 	void *temp;
-	off_t len;
-	struct stat st;
+	LARGE_INTEGER len;
 	uint64_t o = offset;
 	uint32_t l = o & 0xFFFFFFFF;
 	uint32_t h = (o >> 32) & 0xFFFFFFFF;
 
-	if (!fstat(fd, &st))
-		len = st.st_size;
-	else
+	osfhandle = (HANDLE)_get_osfhandle(fd);
+	if (!GetFileSizeEx(osfhandle, &len))
 		die("mmap: could not determine filesize");
 
-	if ((length + offset) > len)
-		length = xsize_t(len - offset);
+	if ((length + offset) > len.QuadPart)
+		length = xsize_t(len.QuadPart - offset);
 
 	if (!(flags & MAP_PRIVATE))
 		die("Invalid usage of mmap when built with USE_WIN32_MMAP");
 
-	hmap = CreateFileMapping((HANDLE)_get_osfhandle(fd), NULL,
+	hmap = CreateFileMapping(osfhandle, NULL,
 		prot == PROT_READ ? PAGE_READONLY : PAGE_WRITECOPY, 0, 0, NULL);
 
 	if (!hmap) {
