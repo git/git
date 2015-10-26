95261974bbc8cc1a32ed260f06a694cde86f732d
Max Kirillov <max@max630.net>
2015-10-26 07:29:00 +0200

    blame: fix option name in error message
    The option name used in blame's UI is `--reverse`.
    
    Signed-off-by: Max Kirillov <max@max630.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/blame.c
+++ builtin/blame.c
@@ -2677,7 +2677,7 @@ parse_done:
 		sb.commits.compare = compare_commits_by_commit_date;
 	}
 	else if (contents_from)
-		die("--contents and --children do not blend well.");
+		die("--contents and --reverse do not blend well.");
 	else {
 		final_commit_name = prepare_initial(&sb);
 		sb.commits.compare = compare_commits_by_reverse_commit_date;
