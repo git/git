08a3651fe7f3163acbac461e0daf370329a1d332
SZEDER Gábor <szeder@ira.uka.de>
2015-11-30 12:40:53 +0100

    Make error message after failing commit_lock_file() less confusing
    The error message after a failing commit_lock_file() call sometimes
    looks like this, causing confusion:
    
      $ git remote add remote git@server.com/repo.git
      error: could not commit config file .git/config
      # Huh?!
      # I didn't want to commit anything, especially not my config file!
    
    While in the narrow context of the lockfile module using the verb
    'commit' in the error message makes perfect sense, in the broader
    context of git the word 'commit' already has a very specific meaning,
    hence the confusion.
    
    Reword these error messages to say "could not write" instead of "could
    not commit".
    
    While at it, include strerror in the error messages after writing the
    config file or the credential store fails to provide some information
    about the cause of the failure, and update the style of the error
    message after writing the reflog fails to match surrounding error
    messages (i.e. no '' around the pathname and no () around the error
    description).
    
    Signed-off-by: SZEDER Gábor <szeder@ira.uka.de>
    Signed-off-by: Jeff King <peff@peff.net>

--- credential-store.c
+++ credential-store.c
@@ -64,7 +64,8 @@ static void rewrite_credential_file(const char *fn, struct credential *c,
 		print_line(extra);
 	parse_credential_file(fn, c, NULL, print_line);
 	if (commit_lock_file(&credential_lock) < 0)
-		die_errno("unable to commit credential store");
+		die_errno("unable to write credential store: %s",
+			  strerror(errno));
 }
 
 static void store_credential_file(const char *fn, struct credential *c)
