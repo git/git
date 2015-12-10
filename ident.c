e850194c83985396435b01b70d2db00b6d3af02e
Jeff King <peff@peff.net>
2015-12-10 16:33:05 -0500

    ident: make xgetpwuid_self() a static local helper
    This function is defined in wrapper.c, but nobody besides
    ident.c uses it. And nobody is likely to in the future,
    either, as anything that cares about the user's name should
    be going through the ident code.
    
    Moving it here is a cleanup of the global namespace, but it
    will also enable further cleanups inside ident.c.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ident.c
+++ ident.c
@@ -23,6 +23,18 @@ static int author_ident_explicitly_given;
 #define get_gecos(struct_passwd) ((struct_passwd)->pw_gecos)
 #endif
 
+static struct passwd *xgetpwuid_self(void)
+{
+	struct passwd *pw;
+
+	errno = 0;
+	pw = getpwuid(getuid());
+	if (!pw)
+		die(_("unable to look up current user in the passwd file: %s"),
+		    errno ? strerror(errno) : _("no such user"));
+	return pw;
+}
+
 static void copy_gecos(const struct passwd *w, struct strbuf *name)
 {
 	char *src;
