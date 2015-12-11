b20c46edda46ecf23bdb946f6a38fefd9a551066
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-12-11 07:13:28 +0100

    fixup! mingw: handle absolute paths in expand_user_path()

--- path.c
+++ path.c
@@ -5,6 +5,7 @@
 #include "strbuf.h"
 #include "string-list.h"
 #include "dir.h"
+#include "exec_cmd.h"
 
 static int get_st_mode_bits(const char *path, int *mode)
 {
@@ -352,7 +353,7 @@ char *expand_user_path(const char *path)
 		goto return_null;
 #ifdef __MINGW32__
 	if (path[0] == '/')
-		return system_path((*result) + 1);
+		return system_path(path + 1);
 #endif
 	if (path[0] == '~') {
 		const char *first_slash = strchrnul(path, '/');
