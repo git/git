c30d697901a422c9486d2ef2dc5ce400551b9702
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-12-11 06:59:13 +0100

    mingw: handle absolute paths in expand_user_path()
    On Windows, an absolute POSIX path needs to be turned into a Windows
    one.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- path.c
+++ path.c
@@ -12,6 +12,7 @@
 #include "packfile.h"
 #include "object-store.h"
 #include "lockfile.h"
+#include "exec-cmd.h"
 
 static int get_st_mode_bits(const char *path, int *mode)
 {
@@ -732,6 +733,10 @@ char *expand_user_path(const char *path, int real_home)
 
 	if (path == NULL)
 		goto return_null;
+#ifdef __MINGW32__
+	if (path[0] == '/')
+		return system_path(path + 1);
+#endif
 	if (path[0] == '~') {
 		const char *first_slash = strchrnul(path, '/');
 		const char *username = path + 1;
