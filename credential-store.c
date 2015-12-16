87d01c854bf9860c8c21814c991f58652f6b4332
SZEDER Gábor <szeder@ira.uka.de>
2015-12-16 12:22:55 +0100

    credential-store: don't pass strerror to die_errno()
    Signed-off-by: SZEDER Gábor <szeder@ira.uka.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- credential-store.c
+++ credential-store.c
@@ -64,8 +64,7 @@ static void rewrite_credential_file(const char *fn, struct credential *c,
 		print_line(extra);
 	parse_credential_file(fn, c, NULL, print_line);
 	if (commit_lock_file(&credential_lock) < 0)
-		die_errno("unable to write credential store: %s",
-			  strerror(errno));
+		die_errno("unable to write credential store");
 }
 
 static void store_credential_file(const char *fn, struct credential *c)
