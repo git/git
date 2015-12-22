697b90d7e6de608e616e705c17b8e7395aa5bd70
Elia Pinto <gitter.spiros@gmail.com>
2015-12-22 16:05:52 +0100

    t/t2025-worktree-add.sh: use the $( ... ) construct for command substitution
    The Git CodingGuidelines prefer the $(...) construct for command
    substitution instead of using the backquotes `...`.
    
    The backquoted form is the traditional method for command
    substitution, and is supported by POSIX.  However, all but the
    simplest uses become complicated quickly.  In particular, embedded
    command substitutions and/or the use of double quotes require
    careful escaping with the backslash character.
    
    The patch was generated by:
    
    for _f in $(find . -name "*.sh")
    do
    	perl -i -pe 'BEGIN{undef $/;} s/`(.+?)`/\$(\1)/smg'  "${_f}"
    done
    
    and then carefully proof-read.
    
    Signed-off-by: Elia Pinto <gitter.spiros@gmail.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2025-worktree-add.sh
+++ t/t2025-worktree-add.sh
@@ -123,10 +123,10 @@ test_expect_success 'checkout from a bare repo without "add"' '
 test_expect_success 'checkout with grafts' '
 	test_when_finished rm .git/info/grafts &&
 	test_commit abc &&
-	SHA1=`git rev-parse HEAD` &&
+	SHA1=$(git rev-parse HEAD) &&
 	test_commit def &&
 	test_commit xyz &&
-	echo "`git rev-parse HEAD` $SHA1" >.git/info/grafts &&
+	echo "$(git rev-parse HEAD) $SHA1" >.git/info/grafts &&
 	cat >expected <<-\EOF &&
 	xyz
 	abc
