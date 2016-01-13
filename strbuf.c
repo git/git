dce80bd18cae17b2d5645e3a9c40e76f905fd643
Junio C Hamano <gitster@pobox.com>
2016-01-13 15:10:45 -0800

    strbuf: miniscule style fix
    We write one SP on each side of an operator, even inside an [] pair
    that computes the array index.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- strbuf.c
+++ strbuf.c
@@ -505,8 +505,8 @@ int strbuf_getline(struct strbuf *sb, FILE *fp, int term)
 {
 	if (strbuf_getwholeline(sb, fp, term))
 		return EOF;
-	if (sb->buf[sb->len-1] == term)
-		strbuf_setlen(sb, sb->len-1);
+	if (sb->buf[sb->len - 1] == term)
+		strbuf_setlen(sb, sb->len - 1);
 	return 0;
 }
 
