dca90031fb777b80fab5084d48cba2e163e2596d
Junio C Hamano <gitster@pobox.com>
2016-01-14 13:31:17 -0800

    check-ignore: there are only two possible line terminations
    The program by default reads LF terminated lines, with an option to
    use NUL terminated records.  Instead of pretending that there can be
    other useful values for line_termination, use a boolean variable,
    nul_term_line, to tell if NUL terminated records are used, and
    switch between strbuf_getline_{lf,nul} based on it.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/check-ignore.c
+++ builtin/check-ignore.c
@@ -117,13 +117,14 @@ static int check_ignore_stdin_paths(struct dir_struct *dir, const char *prefix)
 {
 	struct strbuf buf, nbuf;
 	char *pathspec[2] = { NULL, NULL };
-	int line_termination = nul_term_line ? 0 : '\n';
+	strbuf_getline_fn getline_fn;
 	int num_ignored = 0;
 
+	getline_fn = nul_term_line ? strbuf_getline_nul : strbuf_getline_lf;
 	strbuf_init(&buf, 0);
 	strbuf_init(&nbuf, 0);
-	while (strbuf_getline(&buf, stdin, line_termination) != EOF) {
-		if (line_termination && buf.buf[0] == '"') {
+	while (getline_fn(&buf, stdin) != EOF) {
+		if (!nul_term_line && buf.buf[0] == '"') {
 			strbuf_reset(&nbuf);
 			if (unquote_c_style(&nbuf, buf.buf, NULL))
 				die("line is badly quoted");
