b4c8aba659cb3264bcce3110d54bfcaab485408b
Patrick Steinhardt <ps@pks.im>
2016-02-16 13:56:28 +0100

    config: introduce set_or_die wrappers
    A lot of call-sites for the existing family of `git_config_set`
    functions do not check for errors that may occur, e.g. when the
    configuration file is locked. In many cases we simply want to die
    when such a situation arises.
    
    Introduce wrappers that will cause the program to die in those
    cases. These wrappers are temporary only to ease the transition
    to let `git_config_set` die by default. They will be removed
    later on when `git_config_set` itself has been replaced by
    `git_config_set_gently`.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -1831,9 +1831,20 @@ int git_config_set_in_file(const char *config_filename,
 	return git_config_set_multivar_in_file(config_filename, key, value, NULL, 0);
 }
 
+void git_config_set_in_file_or_die(const char *config_filename,
+			const char *key, const char *value)
+{
+	git_config_set_multivar_in_file_or_die(config_filename, key, value, NULL, 0);
+}
+
 int git_config_set(const char *key, const char *value)
 {
 	return git_config_set_multivar(key, value, NULL, 0);
+}
+
+void git_config_set_or_die(const char *key, const char *value)
+{
+	git_config_set_multivar_or_die(key, value, NULL, 0);
 }
 
 /*
@@ -2179,10 +2190,26 @@ write_err_out:
 
 }
 
+void git_config_set_multivar_in_file_or_die(const char *config_filename,
+				const char *key, const char *value,
+				const char *value_regex, int multi_replace)
+{
+	if (git_config_set_multivar_in_file(config_filename, key, value,
+					    value_regex, multi_replace) < 0)
+		die(_("Could not set '%s' to '%s'"), key, value);
+}
+
 int git_config_set_multivar(const char *key, const char *value,
 			const char *value_regex, int multi_replace)
 {
 	return git_config_set_multivar_in_file(NULL, key, value, value_regex,
+					       multi_replace);
+}
+
+void git_config_set_multivar_or_die(const char *key, const char *value,
+			const char *value_regex, int multi_replace)
+{
+	git_config_set_multivar_in_file_or_die(NULL, key, value, value_regex,
 					       multi_replace);
 }
 
