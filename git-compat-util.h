935de81289cd04b4736c538747c53df123c30d1c
Jeff King <peff@peff.net>
2016-02-19 06:21:19 -0500

    add helpers for detecting size_t overflow
    Performing computations on size_t variables that we feed to
    xmalloc and friends can be dangerous, as an integer overflow
    can cause us to allocate a much smaller chunk than we
    realized.
    
    We already have unsigned_add_overflows(), but let's add
    unsigned_mult_overflows() to that. Furthermore, rather than
    have each site manually check and die on overflow, we can
    provide some helpers that will:
    
      - promote the arguments to size_t, so that we know we are
        doing our computation in the same size of integer that
        will ultimately be fed to xmalloc
    
      - check and die on overflow
    
      - return the result so that computations can be done in
        the parameter list of xmalloc.
    
    These functions are a lot uglier to use than normal
    arithmetic operators (you have to do "st_add(foo, bar)"
    instead of "foo + bar"). To at least limit the damage, we
    also provide multi-valued versions. So rather than:
    
      st_add(st_add(a, b), st_add(c, d));
    
    you can write:
    
      st_add4(a, b, c, d);
    
    This isn't nearly as elegant as a varargs function, but it's
    a lot harder to get it wrong. You don't have to remember to
    add a sentinel value at the end, and the compiler will
    complain if you get the number of arguments wrong. This
    patch adds only the numbered variants required to convert
    the current code base; we can easily add more later if
    needed.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-compat-util.h
+++ git-compat-util.h
@@ -96,6 +96,14 @@
 #define unsigned_add_overflows(a, b) \
     ((b) > maximum_unsigned_value_of_type(a) - (a))
 
+/*
+ * Returns true if the multiplication of "a" and "b" will
+ * overflow. The types of "a" and "b" must match and must be unsigned.
+ * Note that this macro evaluates "a" twice!
+ */
+#define unsigned_mult_overflows(a, b) \
+    ((a) && (b) > maximum_unsigned_value_of_type(a) / (a))
+
 #ifdef __GNUC__
 #define TYPEOF(x) (__typeof__(x))
 #else
@@ -697,6 +705,32 @@ extern void release_pack_memory(size_t);
 
 typedef void (*try_to_free_t)(size_t);
 extern try_to_free_t set_try_to_free_routine(try_to_free_t);
+
+static inline size_t st_add(size_t a, size_t b)
+{
+	if (unsigned_add_overflows(a, b))
+		die("size_t overflow: %"PRIuMAX" + %"PRIuMAX,
+		    (uintmax_t)a, (uintmax_t)b);
+	return a + b;
+}
+#define st_add3(a,b,c)   st_add((a),st_add((b),(c)))
+#define st_add4(a,b,c,d) st_add((a),st_add3((b),(c),(d)))
+
+static inline size_t st_mult(size_t a, size_t b)
+{
+	if (unsigned_mult_overflows(a, b))
+		die("size_t overflow: %"PRIuMAX" * %"PRIuMAX,
+		    (uintmax_t)a, (uintmax_t)b);
+	return a * b;
+}
+
+static inline size_t st_sub(size_t a, size_t b)
+{
+	if (a < b)
+		die("size_t underflow: %"PRIuMAX" - %"PRIuMAX,
+		    (uintmax_t)a, (uintmax_t)b);
+	return a - b;
+}
 
 #ifdef HAVE_ALLOCA_H
 # include <alloca.h>
