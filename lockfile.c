aed7480ca4c284089a3b332fae770b4056eb08fc
Matthieu Moy <Matthieu.Moy@imag.fr>
2016-03-01 18:04:10 +0100

    lockfile: improve error message when lockfile exists
    A common mistake leading a user to see this message is to launch "git
    commit", let the editor open (and forget about it), and try again to
    commit.
    
    The previous message was going too quickly to "a git process crashed"
    and to the advice "remove the file manually".
    
    This patch modifies the message in two ways: first, it considers that
    "another process is running" is the norm, not the exception, and it
    explicitly hints the user to look at text editors.
    
    The message is 2 lines longer, but this is not a problem since
    experienced users do not see the message often.
    
    Helped-by: Moritz Neeb <lists@moritzneeb.de>
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- lockfile.c
+++ lockfile.c
@@ -150,9 +150,11 @@ void unable_to_lock_message(const char *path, int err, struct strbuf *buf)
 {
 	if (err == EEXIST) {
 		strbuf_addf(buf, _("Unable to create '%s.lock': %s.\n\n"
-		    "If no other git process is currently running, this probably means a\n"
-		    "git process crashed in this repository earlier. Make sure no other git\n"
-		    "process is running and remove the file manually to continue."),
+		    "Another git process seems to be running in this repository, e.g.\n"
+		    "an editor opened by 'git commit'. Please make sure all processes\n"
+		    "are terminated then try again. If it still fails, a git process\n"
+		    "may have crashed in this repository earlier:\n"
+		    "remove the file manually to continue."),
 			    absolute_path(path), strerror(err));
 	} else
 		strbuf_addf(buf, _("Unable to create '%s.lock': %s"),
