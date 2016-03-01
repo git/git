636ad602846587cb11af34e32681c1e10bed30ab
yaras <yaras6@gmail.com>
2016-03-01 16:12:23 +0100

    Do not mask the username when reading credentials
    When user is asked for credentials there is no need to mask username,
    so PROMPT_ASKPASS flag on calling credential_ask_one for login is
    unnecessary.
    
    credential_ask_one internally uses git_prompt which in case of given
    flag PROMPT_ASKPASS uses masked input method instead of
    git_terminal_prompt, which does not mask user input.
    
    This fixes #675
    
    Signed-off-by: yaras <yaras6@gmail.com>

--- credential.c
+++ credential.c
@@ -132,7 +132,7 @@ static void credential_getpass(struct credential *c)
 {
 	if (!c->username)
 		c->username = credential_ask_one("Username", c,
-						 PROMPT_ASKPASS|PROMPT_ECHO);
+						 PROMPT_ECHO);
 	if (!c->password)
 		c->password = credential_ask_one("Password", c,
 						 PROMPT_ASKPASS);
