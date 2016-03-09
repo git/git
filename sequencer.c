6b7fbc814b7415e6f79a3221645220f43ac3fffb
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-03-09 15:32:18 +0100

    sequencer: lib'ify do_recursive_merge()
    Instead of dying there, let the caller high up in the callchain
    notice the error and handle it (by dying, still).
    
    The only caller of do_recursive_merge(), do_pick_commit() already
    checks the return value and passes it on to its callers, so its caller
    must be already prepared to handle error returns, and with this step,
    we make it notice an error return from this function.
    
    So this is a safe conversion to make do_recursive_merge() callable
    from new callers that want it not to die, without changing the
    external behaviour of anything existing.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -303,7 +303,8 @@ static int do_recursive_merge(struct commit *base, struct commit *next,
 	if (active_cache_changed &&
 	    write_locked_index(&the_index, &index_lock, COMMIT_LOCK))
 		/* TRANSLATORS: %s will be "revert" or "cherry-pick" */
-		die(_("%s: Unable to write new index file"), action_name(opts));
+		return error(_("%s: Unable to write new index file"),
+			action_name(opts));
 	rollback_lock_file(&index_lock);
 
 	if (opts->signoff)
