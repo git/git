3b5f6403c941f054647ef22d1ff7ebeb4ebb1747
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-03-09 15:32:18 +0100

    sequencer: lib'ify prepare_revs()
    Instead of dying there, let the caller high up in the callchain notice
    the error and handle it (by dying, still).
    
    The only caller of prepare_revs(), walk_revs_populate_todo() was just
    taught to return errors, after verifying that its callers are prepared
    to handle error returns, and with this step, we make it notice an
    error return from this function.
    
    So this is a safe conversion to make prepare_revs() callable from new
    callers that want it not to die, without changing the external
    behaviour of anything existing.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -623,7 +623,7 @@ leave:
 	return res;
 }
 
-static void prepare_revs(struct replay_opts *opts)
+static int prepare_revs(struct replay_opts *opts)
 {
 	/*
 	 * picking (but not reverting) ranges (but not individual revisions)
@@ -633,10 +633,11 @@ static void prepare_revs(struct replay_opts *opts)
 		opts->revs->reverse ^= 1;
 
 	if (prepare_revision_walk(opts->revs))
-		die(_("revision walk setup failed"));
+		return error(_("revision walk setup failed"));
 
 	if (!opts->revs->commits)
-		die(_("empty commit set passed"));
+		return error(_("empty commit set passed"));
+	return 0;
 }
 
 static void read_and_refresh_cache(struct replay_opts *opts)
