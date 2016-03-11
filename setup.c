274db840b48d144a8f0f8d8bd324365670c67275
Jeff King <peff@peff.net>
2016-03-11 17:37:22 -0500

    verify_repository_format: mark messages for translation
    These messages are human-readable and should be translated.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -462,7 +462,7 @@ int verify_repository_format(const struct repository_format *format,
 			     struct strbuf *err)
 {
 	if (GIT_REPO_VERSION_READ < format->version) {
-		strbuf_addf(err, "Expected git repo version <= %d, found %d",
+		strbuf_addf(err, _("Expected git repo version <= %d, found %d"),
 			    GIT_REPO_VERSION_READ, format->version);
 		return -1;
 	}
@@ -470,7 +470,7 @@ int verify_repository_format(const struct repository_format *format,
 	if (format->version >= 1 && format->unknown_extensions.nr) {
 		int i;
 
-		strbuf_addstr(err, "unknown repository extensions found:");
+		strbuf_addstr(err, _("unknown repository extensions found:"));
 
 		for (i = 0; i < format->unknown_extensions.nr; i++)
 			strbuf_addf(err, "\n\t%s",
