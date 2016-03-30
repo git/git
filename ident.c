d3c06c196964c02f7343b53301e0e85679fad51f
Marios Titas <redneb@gmx.com>
2016-03-30 22:29:43 +0300

    ident: give "please tell me" message upon useConfigOnly error
    The env_hint message applies perfectly to the case when
    user.useConfigOnly is set and at least one of the user.name and the
    user.email are not provided.
    
    Additionally, use a less descriptive error message to discourage
    users from disabling user.useConfigOnly configuration variable to
    work around this error condition.  We want to encourage them to set
    user.name or user.email instead.
    
    Signed-off-by: Marios Titas <redneb@gmx.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ident.c
+++ ident.c
@@ -352,8 +352,10 @@ const char *fmt_ident(const char *name, const char *email,
 		int using_default = 0;
 		if (!name) {
 			if (strict && ident_use_config_only
-			    && !(ident_config_given & IDENT_NAME_GIVEN))
-				die("user.useConfigOnly set but no name given");
+			    && !(ident_config_given & IDENT_NAME_GIVEN)) {
+				fputs(env_hint, stderr);
+				die("no name was given and auto-detection is disabled");
+			}
 			name = ident_default_name();
 			using_default = 1;
 			if (strict && default_name_is_bogus) {
@@ -375,8 +377,10 @@ const char *fmt_ident(const char *name, const char *email,
 
 	if (!email) {
 		if (strict && ident_use_config_only
-		    && !(ident_config_given & IDENT_MAIL_GIVEN))
-			die("user.useConfigOnly set but no mail given");
+		    && !(ident_config_given & IDENT_MAIL_GIVEN)) {
+			fputs(env_hint, stderr);
+			die("no email was given and auto-detection is disabled");
+		}
 		email = ident_default_email();
 		if (strict && default_email_is_bogus) {
 			fputs(env_hint, stderr);
