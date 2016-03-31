47d5d6487902753f9483dc340de12551f90d4bdb
Stefan Beller <sbeller@google.com>
2016-03-31 14:04:38 -0700

    submodule--helper clone: create the submodule path just once
    We make sure that the parent directory of path exists (or create it
    otherwise) and then do the same for path + "/.git".
    
    That is equivalent to just making sure that the parent directory of
    path + "/.git" exists (or create it otherwise).
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -215,11 +215,7 @@ static int module_clone(int argc, const char **argv, const char *prefix)
 	}
 
 	/* Write a .git file in the submodule to redirect to the superproject. */
-	if (safe_create_leading_directories_const(path) < 0)
-		die(_("could not create directory '%s'"), path);
-
 	strbuf_addf(&sb, "%s/.git", path);
-
 	if (safe_create_leading_directories_const(sb.buf) < 0)
 		die(_("could not create leading directories of '%s'"), sb.buf);
 	submodule_dot_git = fopen(sb.buf, "w");
