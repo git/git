41d796ed5cd78d159f8a8bfa3b54bdbe5d9383a3
David Turner <dturner@twopensource.com>
2016-04-07 15:03:11 -0400

    refs: on symref reflog expire, lock symref not referrent
    When locking a symbolic ref to expire a reflog, lock the symbolic
    ref (using REF_NODEREF) instead of its referent.
    
    Add a test for this.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Reviewed-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -3314,7 +3314,8 @@ int reflog_expire(const char *refname, const unsigned char *sha1,
 	 * reference itself, plus we might need to update the
 	 * reference if --updateref was specified:
 	 */
-	lock = lock_ref_sha1_basic(refname, sha1, NULL, NULL, 0, &type, &err);
+	lock = lock_ref_sha1_basic(refname, sha1, NULL, NULL, REF_NODEREF,
+				   &type, &err);
 	if (!lock) {
 		error("cannot lock ref '%s': %s", refname, err.buf);
 		strbuf_release(&err);
