4e1aa6b09549359e3ee86c3ce817cb8468c56b84
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-08 17:28:04 +0200

    sequencer (rebase -i): write out the final message
    The shell script version of the interactive rebase has a very specific
    final message. Teach the sequencer to print the same.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -2091,6 +2091,9 @@ static int pick_commits(struct todo_list *todo_list, struct replay_opts *opts)
 		}
 		apply_autostash(opts);
 
+		fprintf(stderr, "Successfully rebased and updated %s.\n",
+			head_ref.buf);
+
 		strbuf_release(&buf);
 		strbuf_release(&head_ref);
 	}
