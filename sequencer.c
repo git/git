556caa4188ba74731c84947b26b91939eb0a98a7
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-08 17:30:25 +0200

    sequencer (rebase -i): record interrupted commits in rewritten, too
    When continuing after a `pick` command failed, we want that commit
    to show up in the rewritten-list (and its notes to be rewritten), too.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -2023,6 +2023,17 @@ int sequencer_continue(struct replay_opts *opts)
 			return error_dirty_index(opts);
 		todo_list.current++;
 	}
+	else if (file_exists(rebase_path_stopped_sha())) {
+		struct strbuf buf = STRBUF_INIT;
+		struct object_id oid;
+
+		if (read_oneliner(&buf, rebase_path_stopped_sha(), 1)) {
+			if (!get_sha1_committish(buf.buf, oid.hash))
+				record_in_rewritten(&oid,
+						peek_command(&todo_list, 0));
+		}
+		strbuf_release(&buf);
+	}
 
 	res = pick_commits(&todo_list, opts);
 	todo_list_release(&todo_list);
