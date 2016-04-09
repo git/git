6d38b2938d3f88c1c1baa4ad2234e4a38d97d25f
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-09 15:04:01 +0200

    wt-status: teach has_{unstaged,uncommitted}_changes() about submodules
    Sometimes we are *actually* interested in those changes... For
    example when an interactive rebase wants to continue with a staged
    submodule update.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wt-status.h
+++ wt-status.h
@@ -129,8 +129,9 @@ __attribute__((format (printf, 3, 4)))
 void status_printf(struct wt_status *s, const char *color, const char *fmt, ...);
 
 /* The following functions expect that the caller took care of reading the index. */
-int has_unstaged_changes(void);
-int has_uncommitted_changes(void);
-int require_clean_work_tree(const char *action, const char *hint, int gently);
+int has_unstaged_changes(int ignore_submodules);
+int has_uncommitted_changes(int ignore_submodules);
+int require_clean_work_tree(const char *action, const char *hint,
+	int ignore_submodules, int gently);
 
 #endif /* STATUS_H */
