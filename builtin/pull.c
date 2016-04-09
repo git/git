b999cd3ea3774da25c81cab3f19ce50285500851
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-09 15:04:01 +0200

    wt-status: teach has_{unstaged,uncommitted}_changes() about submodules
    Sometimes we are *actually* interested in those changes... For
    example when an interactive rebase wants to continue with a staged
    submodule update.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/pull.c
+++ builtin/pull.c
@@ -810,7 +810,7 @@ int cmd_pull(int argc, const char **argv, const char *prefix)
 
 		if (!autostash)
 			require_clean_work_tree("pull with rebase",
-				"Please commit or stash them.", 0);
+				"Please commit or stash them.", 1, 0);
 
 		if (get_rebase_fork_point(rebase_fork_point, repo, *refspecs))
 			hashclr(rebase_fork_point);
