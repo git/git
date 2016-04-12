36e6a5baf1d45e125fcdb99781a0c6448109b44d
Pranit Bauva <pranit.bauva@gmail.com>
2016-04-12 23:02:17 +0000

    test-parse-options: print quiet as integer
    We would want to see how multiple --quiet options affect the value of
    the underlying variable (we may want "--quiet --quiet" to still be 1, or
    we may want to see the value incremented to 2). Show the value as
    integer to allow us to inspect it.
    
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- test-parse-options.c
+++ test-parse-options.c
@@ -90,7 +90,7 @@ int main(int argc, char **argv)
 	printf("string: %s\n", string ? string : "(not set)");
 	printf("abbrev: %d\n", abbrev);
 	printf("verbose: %d\n", verbose);
-	printf("quiet: %s\n", quiet ? "yes" : "no");
+	printf("quiet: %d\n", quiet);
 	printf("dry run: %s\n", dry_run ? "yes" : "no");
 	printf("file: %s\n", file ? file : "(not set)");
 
