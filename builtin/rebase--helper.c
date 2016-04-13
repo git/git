fed60c12d1f61bd995ffb65638c2ab95a1086a27
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-13 17:43:05 +0200

    rebase -i: generate the script via rebase--helper
    The first step of an interactive rebase is to generate the so-called "todo
    script", to be stored in the state directory as "git-rebase-todo" and to
    be edited by the user.
    
    Originally, we adjusted the output of `git log <options>` using a simple
    sed script. Over the course of the years, the code became more
    complicated. We now use shell scripting to edit the output of `git log`
    conditionally, depending whether to keep "empty" commits (i.e. commits
    that do not change any files).
    
    On platforms where shell scripting is not native, this can be a serious
    drag. And it opens the door for incompatibilities between platforms when
    it comes to shell scripting or to Unix-y commands.
    
    Let's just re-implement the todo script generation in plain C, using the
    revision machinery directly.
    
    This is substantially faster, improving the speed relative to the
    shell script version of the interactive rebase from 2x to 3x on Windows.
    
    Note that the rearrange_squash() function in git-rebase--interactive
    relied on the fact that we set the "format" variable to the config setting
    rebase.instructionFormat. Relying on a side effect like this is no good,
    hence we explicitly perform that assignment (possibly again) in
    rearrange_squash().
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/rebase--helper.c
+++ builtin/rebase--helper.c
@@ -12,15 +12,19 @@ static const char * const builtin_rebase_helper_usage[] = {
 int cmd_rebase__helper(int argc, const char **argv, const char *prefix)
 {
 	struct replay_opts opts = REPLAY_OPTS_INIT;
+	int keep_empty = 0;
 	enum {
-		CONTINUE = 1, ABORT
+		CONTINUE = 1, ABORT, MAKE_SCRIPT
 	} command = 0;
 	struct option options[] = {
 		OPT_BOOL(0, "ff", &opts.allow_ff, N_("allow fast-forward")),
+		OPT_BOOL(0, "keep-empty", &keep_empty, N_("keep empty commits")),
 		OPT_CMDMODE(0, "continue", &command, N_("continue rebase"),
 				CONTINUE),
 		OPT_CMDMODE(0, "abort", &command, N_("abort rebase"),
 				ABORT),
+		OPT_CMDMODE(0, "make-script", &command,
+			N_("make rebase script"), MAKE_SCRIPT),
 		OPT_END()
 	};
 
@@ -37,5 +41,7 @@ int cmd_rebase__helper(int argc, const char **argv, const char *prefix)
 		return !!sequencer_continue(&opts);
 	if (command == ABORT && argc == 1)
 		return !!sequencer_remove_state(&opts);
+	if (command == MAKE_SCRIPT && argc > 1)
+		return !!sequencer_make_script(keep_empty, stdout, argc, argv);
 	usage_with_options(builtin_rebase_helper_usage, options);
 }
