8ef6244c7e93ad9027696c366bc262539c5cedfa
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-04-13 18:17:19 +0200

    rebase -i: also expand/collapse the SHA-1s via the rebase--helper
    This is crucial to improve performance on Windows, as the speed is now
    mostly dominated by the SHA-1 transformation (because it spawns a new
    rev-parse process for *every* line, and spawning processes is pretty
    slow from Git for Windows' MSYS2 Bash).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.h
+++ sequencer.h
@@ -48,6 +48,8 @@ int sequencer_remove_state(struct replay_opts *opts);
 int sequencer_make_script(int keep_empty, FILE *out,
 		int argc, const char **argv);
 
+int transform_todo_ids(int shorten_sha1s);
+
 extern const char sign_off_header[];
 
 void append_signoff(struct strbuf *msgbuf, int ignore_footer, unsigned flag);
