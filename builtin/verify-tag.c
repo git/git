20972f54d3f17007904cacb829c1feefb84a68dd
Santiago Torres <santiago@nyu.edu>
2016-04-19 13:47:18 -0400

    verify-tag: update variable name and type
    The run_gpg_verify() function has two variables, size and len.
    
    This may come off as confusing when reading the code. Clarify which one
    pertains to the length of the tag headers by renaming len to
    payload_size. Additionally, change the type of payload_size to size_t to
    match the return type of parse_signature.
    
    Signed-off-by: Santiago Torres <santiago@nyu.edu>
    Reviewed-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/verify-tag.c
+++ builtin/verify-tag.c
@@ -21,20 +21,21 @@ static const char * const verify_tag_usage[] = {
 static int run_gpg_verify(const char *buf, unsigned long size, unsigned flags)
 {
 	struct signature_check sigc;
-	int len;
+	size_t payload_size;
 	int ret;
 
 	memset(&sigc, 0, sizeof(sigc));
 
-	len = parse_signature(buf, size);
+	payload_size = parse_signature(buf, size);
 
-	if (size == len) {
+	if (size == payload_size) {
 		if (flags & GPG_VERIFY_VERBOSE)
-			write_in_full(1, buf, len);
+			write_in_full(1, buf, payload_size);
 		return error("no signature found");
 	}
 
-	ret = check_signature(buf, len, buf + len, size - len, &sigc);
+	ret = check_signature(buf, payload_size, buf + payload_size,
+				size - payload_size, &sigc);
 	print_signature_buffer(&sigc, flags);
 
 	signature_check_clear(&sigc);
