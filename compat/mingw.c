3718880c07e8d786bee926460e813100952247d4
Philip Oakley <philipoakley@iee.org>
2016-04-22 10:48:13 +0100

    msvc: fix the declaration of the _REPARSE_DATA_BUFFER structure
    GCC and MSVC disagree about using the GCC extension _ANONYMOUS_UNION.
    Simply skip that offending keyword when compiling with MSVC.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -2283,7 +2283,10 @@ typedef struct _REPARSE_DATA_BUFFER {
 	DWORD  ReparseTag;
 	WORD   ReparseDataLength;
 	WORD   Reserved;
-	_ANONYMOUS_UNION union {
+#ifndef _MSC_VER
+	_ANONYMOUS_UNION
+#endif
+	union {
 		struct {
 			WORD   SubstituteNameOffset;
 			WORD   SubstituteNameLength;
