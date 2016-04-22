bef234b09e5c3b2f63429fb49aff983f6f617f22
Santiago Torres <santiago@nyu.edu>
2016-04-22 10:52:05 -0400

    tag -v: verify directly rather than exec-ing verify-tag
    Instead of having tag -v fork to run verify-tag, use the
    gpg_verify_tag() function directly.
    
    Helped-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Santiago Torres <santiago@nyu.edu>
    Reviewed-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/tag.c
+++ builtin/tag.c
@@ -104,13 +104,7 @@ static int delete_tag(const char *name, const char *ref,
 static int verify_tag(const char *name, const char *ref,
 				const unsigned char *sha1)
 {
-	const char *argv_verify_tag[] = {"verify-tag",
-					"-v", "SHA1_HEX", NULL};
-	argv_verify_tag[2] = sha1_to_hex(sha1);
-
-	if (run_command_v_opt(argv_verify_tag, RUN_GIT_CMD))
-		return error(_("could not verify the tag '%s'"), name);
-	return 0;
+	return gpg_verify_tag(sha1, name, GPG_VERIFY_VERBOSE);
 }
 
 static int do_sign(struct strbuf *buffer)
