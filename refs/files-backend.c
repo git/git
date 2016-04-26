76fc394d50efef8f1308a0f0d56087f502dac689
Michael Haggerty <mhagger@alum.mit.edu>
2016-04-26 09:09:51 +0200

    commit_ref_update(): write error message to *err, not stderr
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -2719,7 +2719,7 @@ static int commit_ref_update(struct ref_lock *lock,
 		}
 	}
 	if (commit_ref(lock)) {
-		error("Couldn't set %s", lock->ref_name);
+		strbuf_addf(err, "Couldn't set %s", lock->ref_name);
 		unlock_ref(lock);
 		return -1;
 	}
