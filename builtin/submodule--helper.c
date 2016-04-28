d92028a575dde9c325e23f89c3d2b24f13868c57
Stefan Beller <sbeller@google.com>
2016-04-28 13:02:45 -0700

    submodule init: fail gracefully with a missing .gitmodules file
    When there is no .gitmodules file availabe to initialize a submodule
    from, `submodule_from_path` just returns NULL. We need to check for
    that and abort gracefully. When `submodule init` was implemented in shell,
    a missing .gitmodules file would result in an error message
    
        No url found for submodule path '%s' in .gitmodules
    
    Replicate that error message for now.
    
    When the .gitmodules file is missing we can probably fail even earlier
    for all of the submodules with an improved error message.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -314,13 +314,17 @@ static void init_submodule(const char *path, const char *prefix, int quiet)
 	/* Only loads from .gitmodules, no overlay with .git/config */
 	gitmodules_config();
 
-	sub = submodule_from_path(null_sha1, path);
-
 	if (prefix) {
 		strbuf_addf(&sb, "%s%s", prefix, path);
 		displaypath = strbuf_detach(&sb, NULL);
 	} else
-		displaypath = xstrdup(sub->path);
+		displaypath = xstrdup(path);
+
+	sub = submodule_from_path(null_sha1, path);
+
+	if (!sub)
+		die(_("No url found for submodule path '%s' in .gitmodules"),
+			displaypath);
 
 	/*
 	 * Copy url setting when it is not set yet.
