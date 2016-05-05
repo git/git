accac4199c1d28dfd6c860b32d7111c3de8df7a6
Junio C Hamano <gitster@pobox.com>
2016-05-05 13:30:10 -0700

    test-parse-options: fix output when callback option fails
    When test-parse-options detects an error on the command line, it
    gives the usage string just like any parse-options API users do,
    without showing any "variable dump".  An exception is the callback
    test, where a "variable dump" for the option is done before the
    command line options are fully parsed.
    
    Do not expose this implementation detail by separating the handling
    of callback test into two phases, one to capture the fact that an
    option was given during the option parsing phase, and the other to
    show that fact as a part of normal "variable dump".
    
    The effect of this fix is seen in the patch to t/t0040 where it
    tried "test-parse-options --no-length" where "--length" is a callback
    that does not take a negative form.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0040-parse-options.sh
+++ t/t0040-parse-options.sh
@@ -356,9 +356,7 @@ test_expect_success 'OPT_CALLBACK() and OPT_BIT() work' '
 	test_cmp expect output
 '
 
-cat >expect <<\EOF
-Callback: "not set", 1
-EOF
+>expect
 
 test_expect_success 'OPT_CALLBACK() and callback errors work' '
 	test_must_fail test-parse-options --no-length >output 2>output.err &&
--- test-parse-options.c
+++ test-parse-options.c
@@ -14,10 +14,18 @@ static char *file = NULL;
 static int ambiguous;
 static struct string_list list;
 
+static struct {
+	int called;
+	const char *arg;
+	int unset;
+} length_cb;
+
 static int length_callback(const struct option *opt, const char *arg, int unset)
 {
-	printf("Callback: \"%s\", %d\n",
-		(arg ? arg : "not set"), unset);
+	length_cb.called = 1;
+	length_cb.arg = arg;
+	length_cb.unset = unset;
+
 	if (unset)
 		return 1; /* do not support unset */
 
@@ -84,6 +92,12 @@ int main(int argc, char **argv)
 
 	argc = parse_options(argc, (const char **)argv, prefix, options, usage, 0);
 
+	if (length_cb.called) {
+		const char *arg = length_cb.arg;
+		int unset = length_cb.unset;
+		printf("Callback: \"%s\", %d\n",
+		       (arg ? arg : "not set"), unset);
+	}
 	printf("boolean: %d\n", boolean);
 	printf("integer: %d\n", integer);
 	printf("magnitude: %lu\n", magnitude);
