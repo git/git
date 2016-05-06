6917a215c628f747a9bfdd273fd59a07ffced333
Philip Oakley <philipoakley@iee.org>
2016-05-06 11:46:05 +0100

    Avoid multiple PREFIX definitions
    The short and sweet PREFIX can be confused when used in many places.
    
    Rename both usages to better describe their purpose. EXEC_CMD_PREFIX is
    used in full to disambiguate it from the nearby GIT_EXEC_PATH.
    
    The PREFIX in sideband.c, while nominally independant of the exec_cmd
    PREFIX, does reside within libgit[1], so the definitions would clash
    when taken together with a PREFIX given on the command line for use by
    exec_cmd.c.
    
    Noticed when compiling Git for Windows using MSVC/Visual Studio [1] which
    reports the conflict beteeen the command line definition and the
    definition in sideband.c within the libgit project.
    
    [1] the libgit functions are brought into a single sub-project
    within the Visual Studio construction script provided in contrib,
    and hence uses a single command for both exec_cmd.c and sideband.c.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- exec_cmd.c
+++ exec_cmd.c
@@ -20,7 +20,7 @@ static const char *system_prefix(void)
 	    !(prefix = strip_path_suffix(argv0_path, GIT_EXEC_PATH)) &&
 	    !(prefix = strip_path_suffix(argv0_path, BINDIR)) &&
 	    !(prefix = strip_path_suffix(argv0_path, "git"))) {
-		prefix = PREFIX;
+		prefix = FALLBACK_RUNTIME_PREFIX;
 		trace_printf("RUNTIME_PREFIX requested, "
 				"but prefix computation failed.  "
 				"Using static fallback '%s'.\n", prefix);
@@ -45,7 +45,7 @@ void git_extract_argv0_path(const char *argv0)
 
 static const char *system_prefix(void)
 {
-	return PREFIX;
+	return FALLBACK_RUNTIME_PREFIX;
 }
 
 void git_extract_argv0_path(const char *argv0)
