8ca65aebad55790d675f1eaead2dfc15fae60847
Junio C Hamano <gitster@pobox.com>
2016-05-06 11:17:05 -0700

    t0040: convert a few tests to use test-parse-options --expect
    As a small example of using "test-parse-options --expect",
    rewrite the "check" helper using it, instead of comparing
    the whole variable dump.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0040-parse-options.sh
+++ t/t0040-parse-options.sh
@@ -57,28 +57,12 @@ test_expect_success 'test help' '
 
 mv expect expect.err
 
-cat >expect.template <<\EOF
-boolean: 0
-integer: 0
-magnitude: 0
-timestamp: 0
-string: (not set)
-abbrev: 7
-verbose: -1
-quiet: 0
-dry run: no
-file: (not set)
-EOF
-
-check() {
+check () {
 	what="$1" &&
 	shift &&
 	expect="$1" &&
 	shift &&
-	sed "s/^$what .*/$what $expect/" <expect.template >expect &&
-	test-parse-options $* >output 2>output.err &&
-	test_must_be_empty output.err &&
-	test_cmp expect output
+	test-parse-options --expect="$what $expect" "$@"
 }
 
 check_unknown_i18n() {
