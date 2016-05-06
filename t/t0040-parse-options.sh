32d51d473f7a0a9f86b4d082fb4f9de78314a0bd
Junio C Hamano <gitster@pobox.com>
2016-05-06 10:52:34 -0700

    t0040: remove unused test helpers
    9a001381 (Fix tests under GETTEXT_POISON on parseopt, 2012-08-27)
    introduced check_i18n, but the helper was never used from the
    beginning.
    
    The same commit also introduced check_unknown_i18n to replace the
    helper check_unknown and changed all users of the latter to use the
    former, but failed to remove check_unknown itself.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0040-parse-options.sh
+++ t/t0040-parse-options.sh
@@ -81,30 +81,6 @@ check() {
 	test_cmp expect output
 }
 
-check_i18n() {
-	what="$1" &&
-	shift &&
-	expect="$1" &&
-	shift &&
-	sed "s/^$what .*/$what $expect/" <expect.template >expect &&
-	test-parse-options $* >output 2>output.err &&
-	test_must_be_empty output.err &&
-	test_i18ncmp expect output
-}
-
-check_unknown() {
-	case "$1" in
-	--*)
-		echo error: unknown option \`${1#--}\' >expect ;;
-	-*)
-		echo error: unknown switch \`${1#-}\' >expect ;;
-	esac &&
-	cat expect.err >>expect &&
-	test_must_fail test-parse-options $* >output 2>output.err &&
-	test_must_be_empty output &&
-	test_cmp expect output.err
-}
-
 check_unknown_i18n() {
 	case "$1" in
 	--*)
