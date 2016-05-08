d2b6afa2cb2e56d506442dfba96aa08b54d71f6b
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-05-08 16:47:59 +0700

    upload-pack.c: use error_errno()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- upload-pack.c
+++ upload-pack.c
@@ -177,8 +177,7 @@ static void create_pack_file(void)
 
 		if (ret < 0) {
 			if (errno != EINTR) {
-				error("poll failed, resuming: %s",
-				      strerror(errno));
+				error_errno("poll failed, resuming");
 				sleep(1);
 			}
 			continue;
