26604f9f621b7b5710309671168d98722b8f5f33
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-05-08 16:47:41 +0700

    credential-cache--daemon.c: use warning_errno()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- credential-cache--daemon.c
+++ credential-cache--daemon.c
@@ -170,12 +170,12 @@ static int serve_cache_loop(int fd)
 
 		client = accept(fd, NULL, NULL);
 		if (client < 0) {
-			warning("accept failed: %s", strerror(errno));
+			warning_errno("accept failed");
 			return 1;
 		}
 		client2 = dup(client);
 		if (client2 < 0) {
-			warning("dup failed: %s", strerror(errno));
+			warning_errno("dup failed");
 			close(client);
 			return 1;
 		}
