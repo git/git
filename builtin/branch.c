896ba1d11269e6e1cbf5c2795f908da5e3e1ce29
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-05-08 16:47:25 +0700

    builtin/branch.c: use error_errno()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/branch.c
+++ builtin/branch.c
@@ -583,8 +583,7 @@ static int edit_branch_description(const char *branch_name)
 		    branch_name, comment_line_char);
 	if (write_file_gently(git_path(edit_description), "%s", buf.buf)) {
 		strbuf_release(&buf);
-		return error(_("could not write branch description template: %s"),
-			     strerror(errno));
+		return error_errno(_("could not write branch description template"));
 	}
 	strbuf_reset(&buf);
 	if (launch_editor(git_path(edit_description), &buf, NULL)) {
