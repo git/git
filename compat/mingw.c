f30afdabbfb9feeec402d351935879caf8b298ad
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-05-11 10:43:37 +0200

    mingw: introduce the 'core.hideDotFiles' setting
    On Unix (and Linux), files and directories whose names start with a dot
    are usually not shown by default. This convention is used by Git: the
    .git/ directory should be left alone by regular users, and only accessed
    through Git itself.
    
    On Windows, no such convention exists. Instead, there is an explicit flag
    to mark files or directories as hidden.
    
    In the early days, Git for Windows did not mark the .git/ directory (or
    for that matter, any file or directory whose name starts with a dot)
    hidden. This lead to quite a bit of confusion, and even loss of data.
    
    Consequently, Git for Windows introduced the core.hideDotFiles setting,
    with three possible values: true, false, and dotGitOnly, defaulting to
    marking only the .git/ directory as hidden.
    
    The rationale: users do not need to access .git/ directly, and indeed (as
    was demonstrated) should not really see that directory, either. However,
    not all dot files should be hidden by default, as e.g. Eclipse does not
    show them (and the user would therefore be unable to see, say, a
    .gitattributes file).
    
    In over five years since the last attempt to bring this patch into core
    Git, a slightly buggy version of this patch has served Git for Windows'
    users well: no single report indicated problems with the hidden .git/
    directory, and the stream of problems caused by the previously non-hidden
    .git/ directory simply stopped. The bugs have been fixed during the
    process of getting this patch upstream.
    
    Note that there is a funny quirk we have to pay attention to when
    creating hidden files: we use Win32's _wopen() function which
    transmogrifies its arguments and hands off to Win32's CreateFile()
    function. That latter function errors out with ERROR_ACCESS_DENIED (the
    equivalent of EACCES) when the equivalent of the O_CREAT flag was passed
    and the file attributes (including the hidden flag) do not match an
    existing file's. And _wopen() accepts no parameter that would be
    transmogrified into said hidden flag. Therefore, we simply try again
    without O_CREAT.
    
    A slightly different method is required for our fopen()/freopen()
    function as we cannot even *remove* the implicit O_CREAT flag.
    Therefore, we briefly mark existing files as unhidden when opening them
    via fopen()/freopen().
    
    The ERROR_ACCESS_DENIED error can also be triggered by opening a file
    that is marked as a system file (which is unlikely to be tracked in
    Git), and by trying to create a file that has *just* been deleted and is
    awaiting the last open handles to be released (which would be handled
    better by the "Try again?" logic, a story for a different patch series,
    though). In both cases, it does not matter much if we try again without
    the O_CREAT flag, read: it does not hurt, either.
    
    For details how ERROR_ACCESS_DENIED can be triggered, see
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858
    
    Original-patch-by: Erik Faye-Lund <kusmabite@gmail.com>
    Initial-Test-By: Pat Thoyts <patthoyts@users.sourceforge.net>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -286,6 +286,49 @@ int mingw_rmdir(const char *pathname)
 	return ret;
 }
 
+static inline int needs_hiding(const char *path)
+{
+	const char *basename;
+
+	if (hide_dotfiles == HIDE_DOTFILES_FALSE)
+		return 0;
+
+	/* We cannot use basename(), as it would remove trailing slashes */
+	mingw_skip_dos_drive_prefix((char **)&path);
+	if (!*path)
+		return 0;
+
+	for (basename = path; *path; path++)
+		if (is_dir_sep(*path)) {
+			do {
+				path++;
+			} while (is_dir_sep(*path));
+			/* ignore trailing slashes */
+			if (*path)
+				basename = path;
+		}
+
+	if (hide_dotfiles == HIDE_DOTFILES_TRUE)
+		return *basename == '.';
+
+	assert(hide_dotfiles == HIDE_DOTFILES_DOTGITONLY);
+	return !strncasecmp(".git", basename, 4) &&
+		(!basename[4] || is_dir_sep(basename[4]));
+}
+
+static int set_hidden_flag(const wchar_t *path, int set)
+{
+	DWORD original = GetFileAttributesW(path), modified;
+	if (set)
+		modified = original | FILE_ATTRIBUTE_HIDDEN;
+	else
+		modified = original & ~FILE_ATTRIBUTE_HIDDEN;
+	if (original == modified || SetFileAttributesW(path, modified))
+		return 0;
+	errno = err_win_to_posix(GetLastError());
+	return -1;
+}
+
 int mingw_mkdir(const char *path, int mode)
 {
 	int ret;
@@ -293,6 +336,8 @@ int mingw_mkdir(const char *path, int mode)
 	if (xutftowcs_path(wpath, path) < 0)
 		return -1;
 	ret = _wmkdir(wpath);
+	if (!ret && needs_hiding(path))
+		return set_hidden_flag(wpath, 1);
 	return ret;
 }
 
@@ -318,6 +363,21 @@ int mingw_open (const char *filename, int oflags, ...)
 		DWORD attrs = GetFileAttributesW(wfilename);
 		if (attrs != INVALID_FILE_ATTRIBUTES && (attrs & FILE_ATTRIBUTE_DIRECTORY))
 			errno = EISDIR;
+	}
+	if ((oflags & O_CREAT) && needs_hiding(filename)) {
+		/*
+		 * Internally, _wopen() uses the CreateFile() API which errors
+		 * out with an ERROR_ACCESS_DENIED if CREATE_ALWAYS was
+		 * specified and an already existing file's attributes do not
+		 * match *exactly*. As there is no mode or flag we can set that
+		 * would correspond to FILE_ATTRIBUTE_HIDDEN, let's just try
+		 * again *without* the O_CREAT flag (that corresponds to the
+		 * CREATE_ALWAYS flag of CreateFile()).
+		 */
+		if (fd < 0 && errno == EACCES)
+			fd = _wopen(wfilename, oflags & ~O_CREAT, mode);
+		if (fd >= 0 && set_hidden_flag(wfilename, 1))
+			warning("could not mark '%s' as hidden.", filename);
 	}
 	return fd;
 }
@@ -350,6 +410,7 @@ int mingw_fgetc(FILE *stream)
 #undef fopen
 FILE *mingw_fopen (const char *filename, const char *otype)
 {
+	int hide = needs_hiding(filename);
 	FILE *file;
 	wchar_t wfilename[MAX_PATH], wotype[4];
 	if (filename && !strcmp(filename, "/dev/null"))
@@ -357,12 +418,19 @@ FILE *mingw_fopen (const char *filename, const char *otype)
 	if (xutftowcs_path(wfilename, filename) < 0 ||
 		xutftowcs(wotype, otype, ARRAY_SIZE(wotype)) < 0)
 		return NULL;
+	if (hide && !access(filename, F_OK) && set_hidden_flag(wfilename, 0)) {
+		error("could not unhide %s", filename);
+		return NULL;
+	}
 	file = _wfopen(wfilename, wotype);
+	if (file && hide && set_hidden_flag(wfilename, 1))
+		warning("could not mark '%s' as hidden.", filename);
 	return file;
 }
 
 FILE *mingw_freopen (const char *filename, const char *otype, FILE *stream)
 {
+	int hide = needs_hiding(filename);
 	FILE *file;
 	wchar_t wfilename[MAX_PATH], wotype[4];
 	if (filename && !strcmp(filename, "/dev/null"))
@@ -370,7 +438,13 @@ FILE *mingw_freopen (const char *filename, const char *otype, FILE *stream)
 	if (xutftowcs_path(wfilename, filename) < 0 ||
 		xutftowcs(wotype, otype, ARRAY_SIZE(wotype)) < 0)
 		return NULL;
+	if (hide && !access(filename, F_OK) && set_hidden_flag(wfilename, 0)) {
+		error("could not unhide %s", filename);
+		return NULL;
+	}
 	file = _wfreopen(wfilename, wotype, stream);
+	if (file && hide && set_hidden_flag(wfilename, 1))
+		warning("could not mark '%s' as hidden.", filename);
 	return file;
 }
 
