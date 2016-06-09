b333d0d6f450d4f9c4535fd9fd6e0f4ef367507c
Ramsay Jones <ramsay@ramsayjones.plus.com>
2016-06-09 23:52:22 +0100

    write_or_die: remove the unused write_or_whine() function
    Now the last caller of this function is gone, and new ones are
    unlikely to appear, because this function is doing very little that
    a regular if() does not besides obfuscating the error message (and
    if we ever did want something like it, we would probably prefer the
    function to come back with more "normal" return value semantics).
    
    Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1715,7 +1715,6 @@ extern int copy_file(const char *dst, const char *src, int mode);
 extern int copy_file_with_time(const char *dst, const char *src, int mode);
 
 extern void write_or_die(int fd, const void *buf, size_t count);
-extern int write_or_whine(int fd, const void *buf, size_t count, const char *msg);
 extern int write_or_whine_pipe(int fd, const void *buf, size_t count, const char *msg);
 extern void fsync_or_die(int fd, const char *);
 
