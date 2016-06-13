750664f5173646e7ef6092c03589c5eec8d78fab
Pranit Bauva <pranit.bauva@gmail.com>
2016-06-14 01:28:19 +0530

    wrapper: move is_empty_file() and rename it as is_empty_or_missing_file()
    is_empty_file() can help to refactor a lot of code. This will be very
    helpful in porting "git bisect" to C.
    
    Suggested-by: Torsten Bögershausen <tboegi@web.de>
    Mentored-by: Lars Schneider <larsxschneider@gmail.com>
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- wrapper.c
+++ wrapper.c
@@ -679,3 +679,16 @@ void sleep_millisec(int millisec)
 {
 	poll(NULL, 0, millisec);
 }
+
+int is_empty_or_missing_file(const char *filename)
+{
+	struct stat st;
+
+	if (stat(filename, &st) < 0) {
+		if (errno == ENOENT)
+			return 1;
+		die_errno(_("could not stat %s"), filename);
+	}
+
+	return !st.st_size;
+}
