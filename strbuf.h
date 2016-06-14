ed008d7bb957ccfbe32c95aad74ee78e0e31de16
Pranit Bauva <pranit.bauva@gmail.com>
2016-06-14 11:44:11 +0530

    strbuf: describe the return value of strbuf_read_file
    Mentored-by: Lars Schneider <larsxschneider@gmail.com>
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- strbuf.h
+++ strbuf.h
@@ -363,6 +363,8 @@ extern ssize_t strbuf_read(struct strbuf *, int fd, size_t hint);
 /**
  * Read the contents of a file, specified by its path. The third argument
  * can be used to give a hint about the file size, to avoid reallocs.
+ * Return the number of bytes read or a negative value if some error
+ * occurred while opening or reading the file.
  */
 extern ssize_t strbuf_read_file(struct strbuf *sb, const char *path, size_t hint);
 
