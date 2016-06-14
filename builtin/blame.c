d993ce1ed2f025708b0f78bed241466e35f1e8a0
Junio C Hamano <gitster@pobox.com>
2016-06-14 10:46:06 -0700

    blame: improve diagnosis for "--reverse NEW"
    "git blame --reverse OLD..NEW -- PATH" tells us to start from the
    contents in PATH at OLD and observe how each line is changed while
    the history develops up to NEW, and report for each line the latest
    commit up to which the line survives in the original form.
    
    If you say "git blame --reverse NEW -- PATH" by mistake, we complain
    about the missing OLD, but we phrased it as "No commit to dig down
    to?"  In this case, however, we are digging up from OLD, so say so.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/blame.c
+++ builtin/blame.c
@@ -2466,14 +2466,14 @@ static char *prepare_initial(struct scoreboard *sb)
 		if (obj->type != OBJ_COMMIT)
 			die("Non commit %s?", revs->pending.objects[i].name);
 		if (sb->final)
-			die("More than one commit to dig down to %s and %s?",
+			die("More than one commit to dig up from, %s and %s?",
 			    revs->pending.objects[i].name,
 			    final_commit_name);
 		sb->final = (struct commit *) obj;
 		final_commit_name = revs->pending.objects[i].name;
 	}
 	if (!final_commit_name)
-		die("No commit to dig down to?");
+		die("No commit to dig up from?");
 	return xstrdup(final_commit_name);
 }
 
