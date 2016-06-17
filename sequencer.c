9b0df093f6e9959bf8e788012d724a74f5f15a21
Vasco Almeida <vascomalmeida@sapo.pt>
2016-06-17 20:20:54 +0000

    i18n: sequencer: mark entire sentences for translation
    Mark entire sentences of error message rather than assembling one using
    placeholders (e.g. "Cannot %s during a %s").
    
    That would facilitate translation work because it is easier to translate
    a entire sentence than translating pieces. We would have better
    translations at the expense of source code verbosity.
    
    Moreover, translators can now 1) translate the terms "revert" and
    "cherry-pick" if they please 2) have more leeway to adapt their
    translations.
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -697,9 +697,14 @@ static struct commit *parse_insn_line(char *bol, char *eol, struct replay_opts *
 	 * opts; we don't support arbitrary instructions
 	 */
 	if (action != opts->action) {
-		const char *action_str;
-		action_str = action == REPLAY_REVERT ? "revert" : "cherry-pick";
-		error(_("Cannot %s during a %s"), action_str, action_name(opts));
+		if (action == REPLAY_REVERT)
+		      error((opts->action == REPLAY_REVERT)
+			    ? _("Cannot revert during a another revert.")
+			    : _("Cannot revert during a cherry-pick."));
+		else
+		      error((opts->action == REPLAY_REVERT)
+			    ? _("Cannot cherry-pick during a revert.")
+			    : _("Cannot cherry-pick during another cherry-pick."));
 		return NULL;
 	}
 
