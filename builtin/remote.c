070b7e441649b9c2543fda7bfbef0ad58761f869
Vasco Almeida <vascomalmeida@sapo.pt>
2016-06-17 20:20:50 +0000

    i18n: builtin/remote.c: fix mark for translation
    The second string inside _() was not being extracted for translation by
    xgettext, meaning that, although the string was passed to gettext, there
    was no translation available.
    
    Mark each individual string instead of marking the result of ternary if.
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/remote.c
+++ builtin/remote.c
@@ -963,9 +963,9 @@ static int show_local_info_item(struct string_list_item *item, void *cb_data)
 
 	printf("    %-*s ", show_info->width, item->string);
 	if (branch_info->rebase) {
-		printf_ln(_(branch_info->rebase == INTERACTIVE_REBASE ?
-			"rebases interactively onto remote %s" :
-			"rebases onto remote %s"), merge->items[0].string);
+		printf_ln(branch_info->rebase == INTERACTIVE_REBASE
+			  ? _("rebases interactively onto remote %s")
+			  : _("rebases onto remote %s"), merge->items[0].string);
 		return 0;
 	} else if (show_info->any_rebase) {
 		printf_ln(_(" merges with remote %s"), merge->items[0].string);
