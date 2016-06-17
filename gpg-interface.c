0581b546419627d4e82f7df8b195fa207ef42f6a
Jeff King <peff@peff.net>
2016-06-17 19:38:55 -0400

    sign_buffer: use pipe_command
    Similar to the prior commit for verify_signed_buffer, the
    motivation here is both to make the code simpler, and to
    avoid any possible deadlocks with gpg.
    
    In this case we have the same "write to stdin, then read
    from stdout" that the verify case had. This is unlikely to
    be a problem in practice, since stdout has the detached
    signature, which it cannot compute until it has read all of
    stdin (if it were a non-detached signature, that would be a
    problem, though).
    
    We don't read from stderr at all currently. However, we will
    want to in a future patch, so this also prepares us there
    (and in that case gpg _does_ write before reading all of the
    input, though again, it is unlikely that a key uid will fill
    up a pipe buffer).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- gpg-interface.c
+++ gpg-interface.c
@@ -151,40 +151,26 @@ const char *get_signing_key(void)
 int sign_buffer(struct strbuf *buffer, struct strbuf *signature, const char *signing_key)
 {
 	struct child_process gpg = CHILD_PROCESS_INIT;
-	ssize_t len;
+	int ret;
 	size_t i, j, bottom;
 
-	gpg.in = -1;
-	gpg.out = -1;
 	argv_array_pushl(&gpg.args,
 			 gpg_program,
 			 "-bsau", signing_key,
 			 NULL);
 
-	if (start_command(&gpg))
-		return error(_("could not run gpg."));
+	bottom = signature->len;
 
 	/*
 	 * When the username signingkey is bad, program could be terminated
 	 * because gpg exits without reading and then write gets SIGPIPE.
 	 */
 	sigchain_push(SIGPIPE, SIG_IGN);
-
-	if (write_in_full(gpg.in, buffer->buf, buffer->len) != buffer->len) {
-		close(gpg.in);
-		close(gpg.out);
-		finish_command(&gpg);
-		return error(_("gpg did not accept the data"));
-	}
-	close(gpg.in);
-
-	bottom = signature->len;
-	len = strbuf_read(signature, gpg.out, 1024);
-	close(gpg.out);
-
+	ret = pipe_command(&gpg, buffer->buf, buffer->len,
+			   signature, 1024, NULL, 0);
 	sigchain_pop(SIGPIPE);
 
-	if (finish_command(&gpg) || !len || len < 0)
+	if (ret || signature->len == bottom)
 		return error(_("gpg failed to sign the data"));
 
 	/* Strip CR from the line endings, in case we are on Windows. */
