8785c425320e9288d13f0218e591a8aa5c57479c
Vasco Almeida <vascomalmeida@sapo.pt>
2016-06-17 20:20:52 +0000

    i18n: advice: internationalize message for conflicts
    Mark message for translation telling the user she has conflicts to
    resolve. Expose each particular use case, in order to enable translating
    entire sentences which would facilitate translating into other
    languages.
    
    Change "Pull" to lowercase to match other instances. Update test
    t5520-pull.sh, that relied on the old error message, to use the new one.
    
    Although we loose in source code conciseness, we would gain better
    translations because translators can 1) translate the entire sentence,
    including those terms concerning Git (committing, merging, etc) 2) have
    leeway to adapt to their languages.
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- advice.c
+++ advice.c
@@ -79,7 +79,20 @@ int git_default_advice_config(const char *var, const char *value)
 
 int error_resolve_conflict(const char *me)
 {
-	error("%s is not possible because you have unmerged files.", me);
+	if (!strcmp(me, "cherry-pick"))
+		error(_("Cherry-picking is not possible because you have unmerged files."));
+	else if (!strcmp(me, "commit"))
+		error(_("Committing is not possible because you have unmerged files."));
+	else if (!strcmp(me, "merge"))
+		error(_("Merging is not possible because you have unmerged files."));
+	else if (!strcmp(me, "pull"))
+		error(_("Pulling is not possible because you have unmerged files."));
+	else if (!strcmp(me, "revert"))
+		error(_("Reverting is not possible because you have unmerged files."));
+	else
+		error(_("It is not possible to %s because you have unmerged files."),
+			me);
+
 	if (advice_resolve_conflict)
 		/*
 		 * Message used both when 'git commit' fails and when
@@ -93,7 +106,7 @@ int error_resolve_conflict(const char *me)
 void NORETURN die_resolve_conflict(const char *me)
 {
 	error_resolve_conflict(me);
-	die("Exiting because of an unresolved conflict.");
+	die(_("Exiting because of an unresolved conflict."));
 }
 
 void NORETURN die_conclude_merge(void)
