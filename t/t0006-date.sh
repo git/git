bab748371a104c58058c0eff9f4073b710ce0355
Jeff King <peff@peff.net>
2016-06-20 17:14:14 -0400

    local_tzoffset: detect errors from tm_to_time_t
    When we want to know the local timezone offset at a given
    timestamp, we compute it by asking for localtime() at the
    given time, and comparing the offset to GMT at that time.
    However, there's some juggling between time_t and "struct
    tm" which happens, which involves calling our own
    tm_to_time_t().
    
    If that function returns an error (e.g., because it only
    handles dates up to the year 2099), it returns "-1", which
    we treat as a time_t, and is clearly bogus, leading to
    bizarre timestamps (that seem to always adjust the time back
    to (time_t)(uint32_t)-1, in the year 2106).
    
    It's not a good idea for local_tzoffset() to simply die
    here; it would make it hard to run "git log" on a repository
    with funny timestamps. Instead, let's just treat such cases
    as "zero offset".
    
    Reported-by: Norbert Kiesel <nkiesel@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0006-date.sh
+++ t/t0006-date.sh
@@ -48,6 +48,11 @@ check_show default "$TIME" 'Wed Jun 15 16:13:20 2016 +0200'
 check_show raw "$TIME" '1466000000 +0200'
 check_show iso-local "$TIME" '2016-06-15 14:13:20 +0000'
 
+# arbitrary time absurdly far in the future
+FUTURE="5758122296 -0400"
+check_show iso       "$FUTURE" "2152-06-19 18:24:56 -0400"
+check_show iso-local "$FUTURE" "2152-06-19 22:24:56 +0000"
+
 check_parse() {
 	echo "$1 -> $2" >expect
 	test_expect_${4:-success} "parse date ($1${3:+ TZ=$3})" "
