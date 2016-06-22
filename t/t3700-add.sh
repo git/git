d426430e6ec2a05bf0a4ee88c319dd6072908504
Emily Xie <emilyxxie@gmail.com>
2016-06-22 19:00:24 -0400

    pathspec: warn on empty strings as pathspec
    An empty string as a pathspec element matches all paths.  A buggy
    script, however, could accidentally assign an empty string to a
    variable that then gets passed to a Git command invocation, e.g.:
    
    	path=... compute a path to be removed in $path ...
            git rm -r "$paht"
    
    which would unintentionally remove all paths in the current
    directory.
    
    The fix for this issue requires a two-step approach. As there may be
    existing scripts that knowingly use empty strings in this manner,
    the first step simply gives a warning that (1) tells that an empty
    string will become an invalid pathspec element and (2) asks the user
    to use "." if they mean to match all.
    
    For step two, a follow-up patch several release cycles later will
    remove the warning and throw an error instead.
    
    This patch is the first step.
    
    Signed-off-by: Emily Xie <emilyxxie@gmail.com>
    Reported-by: David Turner <novalis@novalis.org>
    Mentored-by: Michail Denchev <mdenchev@gmail.com>
    Thanks-to: Sarah Sharp <sarah@thesharps.us> and James Sharp <jamey@minilop.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3700-add.sh
+++ t/t3700-add.sh
@@ -332,4 +332,9 @@ test_expect_success 'git add --dry-run --ignore-missing of non-existing file out
 	test_i18ncmp expect.err actual.err
 '
 
+test_expect_success 'git add empty string should invoke warning' '
+	git add "" 2>output &&
+	test_i18ngrep "warning: empty strings" output
+'
+
 test_done
