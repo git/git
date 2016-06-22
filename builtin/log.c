11f4eb19843c28b1c50d3c1bef1183e6313b87b8
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-06-22 17:01:54 +0200

    format-patch: explicitly switch off color when writing to files
    The --color=auto handling is done by seeing if file descriptor 1
    (the standard output) is connected to a terminal.  format-patch
    used freopen() to reuse the standard output stream even when sending
    its output to an on-disk file, and this check is appropriate.
    
    In the next step, however, we will stop reusing "FILE *stdout", and
    instead start using arbitrary file descriptor obtained by doing an
    fopen(3) ourselves.  The check --color=auto does will become useless,
    as we no longer are writing to the standard output stream.
    
    But then, we do not need to guess to begin with. As argued in the commit
    message of 7787570c (format-patch: ignore ui.color, 2011-09-13), we do not
    allow the ui.color setting to affect format-patch's output. The only time,
    therefore, that we allow color sequences to be written to the output files
    is when the user specified the --color=always command-line option explicitly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/log.c
+++ builtin/log.c
@@ -1578,6 +1578,8 @@ int cmd_format_patch(int argc, const char **argv, const char *prefix)
 		setup_pager();
 
 	if (output_directory) {
+		if (rev.diffopt.use_color != GIT_COLOR_ALWAYS)
+			rev.diffopt.use_color = GIT_COLOR_NEVER;
 		if (use_stdout)
 			die(_("standard output, or directory, which one?"));
 		if (mkdir(output_directory, 0777) < 0 && errno != EEXIST)
