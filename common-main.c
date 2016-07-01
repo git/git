865812a757707c56fe450cf215573605692ba06e
Jeff King <peff@peff.net>
2016-07-01 02:04:04 -0400

    common-main: call git_extract_argv0_path()
    Every program which links against libgit.a must call this
    function, or risk hitting an assert() in system_path() that
    checks whether we have configured argv0_path (though only
    when RUNTIME_PREFIX is defined, so essentially only on
    Windows).
    
    Looking at the diff, you can see that putting it into the
    common main() saves us having to do it individually in each
    of the external commands. But what you can't see are the
    cases where we _should_ have been doing so, but weren't
    (e.g., git-credential-store, and all of the t/helper test
    programs).
    
    This has been an accident-waiting-to-happen for a long time,
    but wasn't triggered until recently because it involves one
    of those programs actually calling system_path(). That
    happened with git-credential-store in v2.8.0 with ae5f677
    (lazily load core.sharedrepository, 2016-03-11). The
    program:
    
      - takes a lock file, which...
    
      - opens a tempfile, which...
    
      - calls adjust_shared_perm to fix permissions, which...
    
      - lazy-loads the config (as of ae5f677), which...
    
      - calls system_path() to find the location of
        /etc/gitconfig
    
    On systems with RUNTIME_PREFIX, this means credential-store
    reliably hits that assert() and cannot be used.
    
    We never noticed in the test suite, because we set
    GIT_CONFIG_NOSYSTEM there, which skips the system_path()
    lookup entirely.  But if we were to tweak git_config() to
    find /etc/gitconfig even when we aren't going to open it,
    then the test suite shows multiple failures (for
    credential-store, and for some other test helpers). I didn't
    include that tweak here because it's way too specific to
    this particular call to be worth carrying around what is
    essentially dead code.
    
    The implementation is fairly straightforward, with one
    exception: there is exactly one caller (git.c) that actually
    cares about the result of the function, and not the
    side-effect of setting up argv0_path. We can accommodate
    that by simply replacing the value of argv[0] in the array
    we hand down to cmd_main().
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- common-main.c
+++ common-main.c
@@ -1,4 +1,5 @@
 #include "git-compat-util.h"
+#include "exec_cmd.h"
 
 int main(int argc, char **av)
 {
@@ -7,6 +8,8 @@ int main(int argc, char **av)
 	 * 84d32bf7678259c08406571cd6ce4b7a6724dcba
 	 */
 	const char **argv = (const char **)av;
+
+	argv[0] = git_extract_argv0_path(argv[0]);
 
 	return cmd_main(argc, argv);
 }
