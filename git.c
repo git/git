12e0437f237ad72df3a2f3f8b067cf8097d792f1
Jeff King <peff@peff.net>
2016-07-01 02:06:35 -0400

    common-main: call restore_sigpipe_to_default()
    This is another safety/sanity setup that should be in force
    everywhere, but which we only applied in git.c. This did
    catch most cases, since even external commands are typically
    run via "git ..." (and the restoration applies to
    sub-processes, too). But there were cases we missed, such as
    somebody calling git-upload-pack directly via ssh, or
    scripts which use dashed external commands directly.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git.c
+++ git.c
@@ -609,27 +609,6 @@ static int run_argv(int *argcp, const char ***argv)
 	return done_alias;
 }
 
-/*
- * Many parts of Git have subprograms communicate via pipe, expect the
- * upstream of a pipe to die with SIGPIPE when the downstream of a
- * pipe does not need to read all that is written.  Some third-party
- * programs that ignore or block SIGPIPE for their own reason forget
- * to restore SIGPIPE handling to the default before spawning Git and
- * break this carefully orchestrated machinery.
- *
- * Restore the way SIGPIPE is handled to default, which is what we
- * expect.
- */
-static void restore_sigpipe_to_default(void)
-{
-	sigset_t unblock;
-
-	sigemptyset(&unblock);
-	sigaddset(&unblock, SIGPIPE);
-	sigprocmask(SIG_UNBLOCK, &unblock, NULL);
-	signal(SIGPIPE, SIG_DFL);
-}
-
 int cmd_main(int argc, const char **argv)
 {
 	const char *cmd;
@@ -638,8 +617,6 @@ int cmd_main(int argc, const char **argv)
 	cmd = argv[0];
 	if (!cmd)
 		cmd = "git-help";
-
-	restore_sigpipe_to_default();
 
 	git_setup_gettext();
 
