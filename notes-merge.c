deb9c1575c456b9d12ff05fdd2bec516dfb34ae4
René Scharfe <l.s.r@web.de>
2016-07-07 22:08:30 +0200

    notes-merge: use O_EXCL to avoid overwriting existing files
    Use the open(2) flag O_EXCL to ensure the file doesn't already exist
    instead of (racily) calling stat(2) through file_exists().  While at it
    switch to xopen() to reduce code duplication and get more consistent
    error messages.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- notes-merge.c
+++ notes-merge.c
@@ -298,12 +298,8 @@ static void write_buf_to_worktree(const unsigned char *obj,
 	char *path = git_pathdup(NOTES_MERGE_WORKTREE "/%s", sha1_to_hex(obj));
 	if (safe_create_leading_directories_const(path))
 		die_errno("unable to create directory for '%s'", path);
-	if (file_exists(path))
-		die("found existing file at '%s'", path);
 
-	fd = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0666);
-	if (fd < 0)
-		die_errno("failed to open '%s'", path);
+	fd = xopen(path, O_WRONLY | O_EXCL | O_CREAT, 0666);
 
 	while (size > 0) {
 		long ret = write_in_full(fd, buf, size);
