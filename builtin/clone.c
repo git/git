38e590ea12336bcfae0310092885743e6ba79781
Jeff King <peff@peff.net>
2016-07-15 06:33:18 -0400

    clone: use a real progress meter for connectivity check
    Because the initial connectivity check for a cloned
    repository can be slow, 0781aa4 (clone: let the user know
    when check_everything_connected is run, 2013-05-03) added a
    "fake" progress meter; we simply say "Checking connectivity"
    when it starts, and "done" at the end, with nothing between.
    
    Since check_connected() now knows how to do a real progress
    meter, we can drop our fake one and use that one instead.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -627,13 +627,10 @@ static void update_remote_refs(const struct ref *refs,
 		struct check_connected_options opt = CHECK_CONNECTED_INIT;
 
 		opt.transport = transport;
+		opt.progress = transport->progress;
 
-		if (transport->progress)
-			fprintf(stderr, _("Checking connectivity... "));
 		if (check_connected(iterate_ref_map, &rm, &opt))
 			die(_("remote did not send all necessary objects"));
-		if (transport->progress)
-			fprintf(stderr, _("done.\n"));
 	}
 
 	if (refs) {
