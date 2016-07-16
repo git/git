378932d3c3214cf658805af20c4bd101922d8fcd
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2016-07-16 07:06:25 +0200

    test-lib.sh: introduce and use $EMPTY_BLOB
    Similar to $EMPTY_TREE this makes it easier to recognize this special
    SHA-1 and change hash later.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7508-status.sh
+++ t/t7508-status.sh
@@ -803,7 +803,7 @@ EOF
 '
 
 cat >expect <<EOF
-:100644 100644 e69de29bb2d1d6434b8b29ae775ad8c2e48c5391 0000000000000000000000000000000000000000 M	dir1/modified
+:100644 100644 $EMPTY_BLOB 0000000000000000000000000000000000000000 M	dir1/modified
 EOF
 test_expect_success 'status refreshes the index' '
 	touch dir2/added &&
