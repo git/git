b0a61ab23c0fd51a1b641b481e6a63f8661a8f9f
Matthieu Moy <Matthieu.Moy@imag.fr>
2016-07-21 14:58:37 +0200

    status: suggest 'git merge --abort' when appropriate
    We already suggest 'git rebase --abort' during a conflicted rebase.
    Similarly, suggest 'git merge --abort' during conflict resolution on
    'git merge'.
    
    Signed-off-by: Matthieu Moy <Matthieu.Moy@imag.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.c
+++ wt-status.c
@@ -946,9 +946,12 @@ static void show_merge_in_progress(struct wt_status *s,
 {
 	if (has_unmerged(s)) {
 		status_printf_ln(s, color, _("You have unmerged paths."));
-		if (s->hints)
+		if (s->hints) {
 			status_printf_ln(s, color,
-				_("  (fix conflicts and run \"git commit\")"));
+					 _("  (fix conflicts and run \"git commit\")"));
+			status_printf_ln(s, color,
+					 _("  (use \"git merge --abort\" to abort the merge)"));
+		}
 	} else {
 		status_printf_ln(s, color,
 			_("All conflicts fixed but you are still merging."));
