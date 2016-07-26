eee98e74f928a49c310038c77026ebc04e6cf4b2
John Keeping <john@keeping.me.uk>
2016-07-26 21:44:44 +0100

    push: add shorthand for --force-with-lease branch creation
    Allow the empty string to stand in for the null SHA-1 when pushing a new
    branch, like we do when deleting branches.
    
    This means that the following command ensures that `new-branch` is
    created on the remote (that is, is must not already exist):
    
    	git push --force-with-lease=new-branch: origin new-branch
    
    Signed-off-by: John Keeping <john@keeping.me.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -2304,6 +2304,8 @@ int parse_push_cas_option(struct push_cas_option *cas, const char *arg, int unse
 	entry = add_cas_entry(cas, arg, colon - arg);
 	if (!*colon)
 		entry->use_tracking = 1;
+	else if (!colon[1])
+		hashclr(entry->expect);
 	else if (get_sha1(colon + 1, entry->expect))
 		return error("cannot parse expected object name '%s'", colon + 1);
 	return 0;
