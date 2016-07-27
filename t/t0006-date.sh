1a2a1e8eb93de2b89a32a52980ec7614fe9d154c
Jeff King <peff@peff.net>
2016-07-27 09:44:41 -0400

    date: document and test "raw-local" mode
    The "raw" format shows a Unix epoch timestamp, but with a
    timezone tacked on. The timestamp is not _in_ that zone, but
    it is extra information about the time (by default, the zone
    the author was in).
    
    The documentation claims that "raw-local" does not work. It
    does, but the end result is rather subtle. Let's describe it
    in better detail, and test to make sure it works (namely,
    the epoch time doesn't change, but the zone does).
    
    While we are rewording the documentation in this area, let's
    not use the phrase "does not work" for the remaining option,
    "--date=relative". It's vague; do we accept it or not? We do
    accept it, but it has no effect (which is a reasonable
    outcome). We should also refer to the option not as
    "--relative" (which is the historical synonym, and does not
    take "-local" at all), but as "--date=relative".
    
    Helped-by: Jakub Narębski <jnareb@gmail.com>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0006-date.sh
+++ t/t0006-date.sh
@@ -47,6 +47,7 @@ check_show short "$TIME" '2016-06-15'
 check_show default "$TIME" 'Wed Jun 15 16:13:20 2016 +0200'
 check_show raw "$TIME" '1466000000 +0200'
 check_show iso-local "$TIME" '2016-06-15 14:13:20 +0000'
+check_show raw-local "$TIME" '1466000000 +0000'
 
 # arbitrary time absurdly far in the future
 FUTURE="5758122296 -0400"
