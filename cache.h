3157c880f6afab26af4f3e4eaceee68fc1b482a8
Jeff King <peff@peff.net>
2016-07-29 00:06:48 -0400

    sha1_file: drop free_pack_by_name
    The point of this function is to drop an entry from the
    "packed_git" cache that points to a file we might be
    overwriting, because our contents may not be the same (and
    hence the only caller was pack-objects as it moved a
    temporary packfile into place).
    
    In older versions of git, this could happen because the
    names of packfiles were derived from the set of objects they
    contained, not the actual bits on disk. But since 1190a1a
    (pack-objects: name pack files after trailer hash,
    2013-12-05), the name reflects the actual bits on disk, and
    any two packfiles with the same name can be used
    interchangeably.
    
    Dropping this function not only saves a few lines of code,
    it makes the lifetime of "struct packed_git" much easier to
    reason about: namely, we now do not ever free these structs.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1410,7 +1410,6 @@ extern unsigned char *use_pack(struct packed_git *, struct pack_window **, off_t
 extern void close_pack_windows(struct packed_git *);
 extern void close_all_packs(void);
 extern void unuse_pack(struct pack_window **);
-extern void free_pack_by_name(const char *);
 extern void clear_delta_base_cache(void);
 extern struct packed_git *add_packed_git(const char *path, size_t path_len, int local);
 
--- sha1_file.c
+++ sha1_file.c
@@ -891,36 +891,6 @@ void close_pack_index(struct packed_git *p)
 	}
 }
 
-/*
- * This is used by git-repack in case a newly created pack happens to
- * contain the same set of objects as an existing one.  In that case
- * the resulting file might be different even if its name would be the
- * same.  It is best to close any reference to the old pack before it is
- * replaced on disk.  Of course no index pointers or windows for given pack
- * must subsist at this point.  If ever objects from this pack are requested
- * again, the new version of the pack will be reinitialized through
- * reprepare_packed_git().
- */
-void free_pack_by_name(const char *pack_name)
-{
-	struct packed_git *p, **pp = &packed_git;
-
-	while (*pp) {
-		p = *pp;
-		if (strcmp(pack_name, p->pack_name) == 0) {
-			clear_delta_base_cache();
-			close_pack(p);
-			free(p->bad_object_sha1);
-			*pp = p->next;
-			if (last_found_pack == p)
-				last_found_pack = NULL;
-			free(p);
-			return;
-		}
-		pp = &p->next;
-	}
-}
-
 static unsigned int get_max_fd_limit(void)
 {
 #ifdef RLIMIT_NOFILE
