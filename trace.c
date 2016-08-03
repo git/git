c0222e762ef57c59fa793862618cfcdfef1cf3f8
Jeff King <peff@peff.net>
2016-08-03 18:58:00 -0400

    trace: stop using write_or_whine_pipe()
    The write_or_whine_pipe function does two things:
    
      1. it checks for EPIPE and converts it into a signal death
    
      2. it prints a message to stderr on error
    
    The first thing does not help us, and actively hurts.
    Generally we would simply die from SIGPIPE in this case,
    unless somebody has taken the time to ignore SIGPIPE for the
    whole process.  And if they _did_ do that, it seems rather
    silly for the trace code, which otherwise takes pains to
    continue even in the face of errors (e.g., by not using
    write_or_die!), to take down the whole process for one
    specific type of error.
    
    Nor does the second thing help us; it just makes it harder
    to write our error message, because we have to feed bits of
    it as an argument to write_or_whine_pipe(). Translators
    never get to see the full message, and it's hard for us to
    customize it.
    
    Let's switch to just using write_in_full() and writing our
    own error string. For now, the error is identical to what
    write_or_whine_pipe() would say, but now that it's more
    under our control, we can improve it in future patches.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- trace.c
+++ trace.c
@@ -132,18 +132,23 @@ static int prepare_trace_line(const char *file, int line,
 	return 1;
 }
 
+static void trace_write(struct trace_key *key, const void *buf, unsigned len)
+{
+	if (write_in_full(get_trace_fd(key), buf, len) < 0)
+		fprintf(stderr, "%s: write error (%s)\n", err_msg, strerror(errno));
+}
+
 void trace_verbatim(struct trace_key *key, const void *buf, unsigned len)
 {
 	if (!trace_want(key))
 		return;
-	write_or_whine_pipe(get_trace_fd(key), buf, len, err_msg);
+	trace_write(key, buf, len);
 }
 
 static void print_trace_line(struct trace_key *key, struct strbuf *buf)
 {
 	strbuf_complete_line(buf);
-
-	write_or_whine_pipe(get_trace_fd(key), buf->buf, buf->len, err_msg);
+	trace_write(key, buf->buf, buf->len);
 	strbuf_release(buf);
 }
 
