ca5c701ca50c24807793294c5ed22230858ac5e6
Jeff King <peff@peff.net>
2016-08-03 19:01:42 -0400

    write_or_die: drop write_or_whine_pipe()
    This function has no callers, and is not likely to gain any
    because it's confusing to use.
    
    It unconditionally complains to stderr, but _doesn't_ die.
    Yet any caller which wants a "gentle" write would generally
    want to suppress the error message, because presumably
    they're going to write a better one, and/or try the
    operation again.
    
    And the check_pipe() call leads to confusing behaviors. It
    means we die for EPIPE, but not for other errors, which is
    confusing and pointless.
    
    On top of all that, it has unusual error return semantics,
    which makes it easy for callers to get it wrong.
    
    Let's drop the function, and if somebody ever needs to
    resurrect something like it, they can fix these warts.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1740,7 +1740,6 @@ extern int copy_file(const char *dst, const char *src, int mode);
 extern int copy_file_with_time(const char *dst, const char *src, int mode);
 
 extern void write_or_die(int fd, const void *buf, size_t count);
-extern int write_or_whine_pipe(int fd, const void *buf, size_t count, const char *msg);
 extern void fsync_or_die(int fd, const char *);
 
 extern ssize_t read_in_full(int fd, void *buf, size_t count);
