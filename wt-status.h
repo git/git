2bb856af92617645ad9bb09a2c7c8189f8e6f9cb
Jeff Hostetler <jeffhost@microsoft.com>
2016-08-05 18:00:26 -0400

    status: rename long-format print routines
    Rename the various wt_status_print*() routines to be
    wt_longstatus_print*() to make it clear that these
    routines are only concerned with the normal/long
    status output and reduce developer confusion as other
    status formats are added in the future.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.h
+++ wt-status.h
@@ -99,7 +99,6 @@ struct wt_status_state {
 void wt_status_truncate_message_at_cut_line(struct strbuf *);
 void wt_status_add_cut_line(FILE *fp);
 void wt_status_prepare(struct wt_status *s);
-void wt_status_print(struct wt_status *s);
 void wt_status_collect(struct wt_status *s);
 void wt_status_get_state(struct wt_status_state *state, int get_detached_from);
 int wt_status_check_rebase(const struct worktree *wt,
@@ -107,6 +106,7 @@ int wt_status_check_rebase(const struct worktree *wt,
 int wt_status_check_bisect(const struct worktree *wt,
 			   struct wt_status_state *state);
 
+void wt_longstatus_print(struct wt_status *s);
 void wt_shortstatus_print(struct wt_status *s);
 void wt_porcelain_print(struct wt_status *s);
 
