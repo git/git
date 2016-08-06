1eb47f167d65d1d305b9c196a1bb40eb96117cb1
René Scharfe <l.s.r@web.de>
2016-08-06 17:41:01 +0200

    use strbuf_add_unique_abbrev() for adding short hashes
    Call strbuf_add_unique_abbrev() to add abbreviated hashes to strbufs
    instead of taking detours through find_unique_abbrev() and its static
    buffer.  This is shorter and a bit more efficient.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -703,8 +703,7 @@ static int add_pending_uninteresting_ref(const char *refname,
 static void describe_one_orphan(struct strbuf *sb, struct commit *commit)
 {
 	strbuf_addstr(sb, "  ");
-	strbuf_addstr(sb,
-		find_unique_abbrev(commit->object.oid.hash, DEFAULT_ABBREV));
+	strbuf_add_unique_abbrev(sb, commit->object.oid.hash, DEFAULT_ABBREV);
 	strbuf_addch(sb, ' ');
 	if (!parse_commit(commit))
 		pp_commit_easy(CMIT_FMT_ONELINE, commit, sb);
