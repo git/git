2703c22fc290993ade5135f85f0963776398a4df
Ville Skyttä <ville.skytta@iki.fi>
2016-08-09 12:34:44 +0300

    completion: complete --delete, --move, and --remotes for git branch
    Signed-off-by: Ville Skyttä <ville.skytta@iki.fi>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -964,8 +964,8 @@ _git_branch ()
 	while [ $c -lt $cword ]; do
 		i="${words[c]}"
 		case "$i" in
-		-d|-m)	only_local_ref="y" ;;
-		-r)	has_r="y" ;;
+		-d|--delete|-m|--move)	only_local_ref="y" ;;
+		-r|--remotes)		has_r="y" ;;
 		esac
 		((c++))
 	done
@@ -979,7 +979,7 @@ _git_branch ()
 			--color --no-color --verbose --abbrev= --no-abbrev
 			--track --no-track --contains --merged --no-merged
 			--set-upstream-to= --edit-description --list
-			--unset-upstream
+			--unset-upstream --delete --move --remotes
 			"
 		;;
 	*)
