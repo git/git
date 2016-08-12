213349abe76511264ffc89a5bbb0de4fe4250261
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-12 15:22:55 +0200

    cat-file --textconv/--smudge: allow specifying the path separately
    There are circumstances when it is relatively easy to figure out the
    object name for a given path, but not the revision. For example, when
    looking at a diff generated by Git, the object names are recorded, but
    not the revision. As a matter of fact, the revisions from which the diff
    was generated may not even exist locally.
    
    In such a case, the user would have to generate a fake revision just to
    be able to use --textconv or --smudge.
    
    Let's simplify this dramatically, because we do not really need that
    revision at all: all we care about is that we know the path. In the
    scenario described above, we do know the path, and we just want to
    specify it separately from the object name.
    
    Example usage:
    
    	git cat-file --textconv --use-path=main.c 0f1937fd
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t8010-cat-file-filters.sh
+++ t/t8010-cat-file-filters.sh
@@ -4,7 +4,7 @@ test_description='git cat-file filters support'
 . ./test-lib.sh
 
 test_expect_success 'setup ' '
-	echo "*.txt eol=crlf" >.gitattributes &&
+	echo "*.txt eol=crlf diff=txt" >.gitattributes &&
 	echo "hello" | append_cr >world.txt &&
 	git add .gitattributes world.txt &&
 	test_tick &&
@@ -29,6 +29,19 @@ test_expect_success 'no filters with cat-file' '
 test_expect_success 'cat-file --smudge converts to worktree version' '
 	git cat-file --smudge HEAD:world.txt >actual &&
 	has_cr actual
+'
+
+test_expect_success 'cat-file --smudge --use-path=<path> works' '
+	sha1=$(git rev-parse -q --verify HEAD:world.txt) &&
+	git cat-file --smudge --use-path=world.txt $sha1 >actual &&
+	has_cr actual
+'
+
+test_expect_success 'cat-file --textconv --use-path=<path> works' '
+	sha1=$(git rev-parse -q --verify HEAD:world.txt) &&
+	test_config diff.txt.textconv "tr A-Za-z N-ZA-Mn-za-m <" &&
+	git cat-file --textconv --use-path=hello.txt $sha1 >rot13 &&
+	test uryyb = "$(cat rot13)"
 '
 
 test_done
