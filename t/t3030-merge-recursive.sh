a25716535ba7ba85322a1e9f20168f31c61dae81
René Scharfe <l.s.r@web.de>
2016-08-13 14:16:04 +0200

    merge-recursive: fix verbose output for multiple base trees
    One of the indirect callers of make_virtual_commit() passes the result of
    oid_to_hex() as the name, i.e. a pointer to a static buffer.  Since the
    function uses that string pointer directly in building a struct
    merge_remote_desc, multiple entries can end up sharing the same name
    inadvertently.
    
    Fix that by calling set_merge_remote_desc(), which creates a copy of the
    string, instead of building the struct by hand.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3030-merge-recursive.sh
+++ t/t3030-merge-recursive.sh
@@ -660,4 +660,22 @@ test_expect_success 'merging with triple rename across D/F conflict' '
 	git merge other
 '
 
+test_expect_success 'merge-recursive remembers the names of all base trees' '
+	git reset --hard HEAD &&
+
+	# more trees than static slots used by oid_to_hex()
+	for commit in $c0 $c2 $c4 $c5 $c6 $c7
+	do
+		git rev-parse "$commit^{tree}"
+	done >trees &&
+
+	# ignore the return code -- it only fails because the input is weird
+	test_must_fail git -c merge.verbosity=5 merge-recursive $(cat trees) -- $c1 $c3 >out &&
+
+	# merge-recursive prints in reverse order, but we do not care
+	sort <trees >expect &&
+	sed -n "s/^virtual //p" out | sort >actual &&
+	test_cmp expect actual
+'
+
 test_done
