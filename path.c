9445b4921e3e996b2d38d58c594f95d63a72dcea
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-16 15:14:27 +0200

    rev-parse: respect core.hooksPath in --git-path
    The idea of the --git-path option is not only to avoid having to
    prefix paths with the output of --git-dir all the time, but also to
    respect overrides for specific common paths inside the .git directory
    (e.g. `git rev-parse --git-path objects` will report the value of the
    environment variable GIT_OBJECT_DIRECTORY, if set).
    
    When introducing the core.hooksPath setting, we forgot to adjust
    git_path() accordingly. This patch fixes that.
    
    While at it, revert the special-casing of core.hooksPath in
    run-command.c, as it is now no longer needed.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- path.c
+++ path.c
@@ -379,6 +379,8 @@ static void adjust_git_path(struct strbuf *buf, int git_dir_len)
 			      get_index_file(), strlen(get_index_file()));
 	else if (git_db_env && dir_prefix(base, "objects"))
 		replace_dir(buf, git_dir_len + 7, get_object_directory());
+	else if (git_hooks_path && dir_prefix(base, "hooks"))
+		replace_dir(buf, git_dir_len + 5, git_hooks_path);
 	else if (git_common_dir_env)
 		update_common_dir(buf, git_dir_len, NULL);
 }
