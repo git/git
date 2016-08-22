f92dd60f95c3e92bc1eac7a0810efae167df9b51
Jeff King <peff@peff.net>
2016-08-22 17:57:58 -0400

    release_delta_base_cache: reuse existing detach function
    This function drops an entry entirely from the cache,
    meaning that aside from the freeing of the buffer, it is
    exactly equivalent to detach_delta_base_cache_entry(). Let's
    build on top of the detach function, which shortens the code
    and will make it simpler when we change out the underlying
    storage in future patches.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1_file.c
+++ sha1_file.c
@@ -2152,10 +2152,7 @@ static inline void release_delta_base_cache(struct delta_base_cache_entry *ent)
 {
 	if (ent->data) {
 		free(ent->data);
-		ent->data = NULL;
-		ent->lru.next->prev = ent->lru.prev;
-		ent->lru.prev->next = ent->lru.next;
-		delta_base_cached -= ent->size;
+		detach_delta_base_cache_entry(ent);
 	}
 }
 
