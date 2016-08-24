7720efbbeeeb63860e8b462d51bb33f8a4cac784
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-08-24 14:07:14 +0200

    fixup! Merge 'cat-file-smudge'
    This reverts commit 7576a30b53892b23c0f9cce903d35ab660638741, reversing
    changes made to 6de9d8ad2728cc067ed23978e4fea3ca2b9eb4f8.
    
    The patch series has been sent upstream and modifications were necessary
    as a consequence. We will merge the new version of the patch series in
    one moment.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -20,34 +20,6 @@ struct batch_options {
 	const char *format;
 };
 
-static const char *force_path;
-
-static int smudge_object(const char *path, unsigned mode, unsigned char *sha1,
-	char **buf, unsigned long *size)
-{
-	enum object_type type;
-
-	*buf = read_sha1_file(sha1, &type, size);
-	if (!*buf)
-		return error(_("cannot read object %s '%s'"),
-			sha1_to_hex(sha1), path);
-	if (type != OBJ_BLOB) {
-		free(*buf);
-		return error(_("blob expected for %s '%s'"),
-			sha1_to_hex(sha1), path);
-	}
-	if (S_ISREG(mode)) {
-		struct strbuf strbuf = STRBUF_INIT;
-		if (convert_to_working_tree(path, *buf, *size, &strbuf)) {
-			free(*buf);
-			*size = strbuf.len;
-			*buf = strbuf_detach(&strbuf, NULL);
-		}
-	}
-
-	return 0;
-}
-
 static int cat_one_file(int opt, const char *exp_type, const char *obj_name,
 			int unknown_type)
 {
@@ -89,25 +61,12 @@ static int cat_one_file(int opt, const char *exp_type, const char *obj_name,
 	case 'e':
 		return !has_sha1_file(sha1);
 
-	case 'w':
-		if (!force_path && !obj_context.path[0])
-			die("git cat-file --smudge %s: <object> must be <sha1:path>",
-			    obj_name);
-
-		if (smudge_object(force_path ? force_path : obj_context.path,
-				  force_path ? 0100644 : obj_context.mode,
-				  sha1, &buf, &size))
-			return -1;
-		break;
-
 	case 'c':
-		if (!force_path && !obj_context.path[0])
+		if (!obj_context.path[0])
 			die("git cat-file --textconv %s: <object> must be <sha1:path>",
 			    obj_name);
 
-		if (textconv_object(force_path ? force_path : obj_context.path,
-				    force_path ? 0100644 : obj_context.mode,
-				    sha1, 1, &buf, &size))
+		if (textconv_object(obj_context.path, obj_context.mode, sha1, 1, &buf, &size))
 			break;
 
 	case 'p':
@@ -481,7 +440,7 @@ static int batch_objects(struct batch_options *opt)
 }
 
 static const char * const cat_file_usage[] = {
-	N_("git cat-file (-t [--allow-unknown-type]|-s [--allow-unknown-type]|-e|-p|<type>|--textconv|--smudge) [--use-path=<path>] <object>"),
+	N_("git cat-file (-t [--allow-unknown-type]|-s [--allow-unknown-type]|-e|-p|<type>|--textconv) <object>"),
 	N_("git cat-file (--batch | --batch-check) [--follow-symlinks]"),
 	NULL
 };
@@ -527,10 +486,6 @@ int cmd_cat_file(int argc, const char **argv, const char *prefix)
 		OPT_CMDMODE('p', NULL, &opt, N_("pretty-print object's content"), 'p'),
 		OPT_CMDMODE(0, "textconv", &opt,
 			    N_("for blob objects, run textconv on object's content"), 'c'),
-		OPT_CMDMODE(0, "smudge", &opt,
-			    N_("for blob objects, run smudge filters on object's content"), 'w'),
-		OPT_STRING(0, "use-path", &force_path, N_("blob"),
-			   N_("use a specific path for --textconv/--smudge")),
 		OPT_BOOL(0, "allow-unknown-type", &unknown_type,
 			  N_("allow -s and -t to work with broken/corrupt objects")),
 		OPT_BOOL(0, "buffer", &batch.buffer_output, N_("buffer --batch output")),
@@ -570,11 +525,6 @@ int cmd_cat_file(int argc, const char **argv, const char *prefix)
 	}
 
 	if ((batch.follow_symlinks || batch.all_objects) && !batch.enabled) {
-		usage_with_options(cat_file_usage, options);
-	}
-
-	if (force_path && opt != 'c' && opt != 'w') {
-		error("--use-path=<path> needs --textconv or --smudge");
 		usage_with_options(cat_file_usage, options);
 	}
 
