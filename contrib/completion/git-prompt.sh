4f58111cb7d20ef9a3ae8aad702904c52921e5e9
Gustavo Henrique Conci <gustavo.conci@gmail.com>
2016-08-31 20:27:22 -0300

    Viewing Unpushed Git Commits

--- contrib/completion/git-prompt.sh
+++ contrib/completion/git-prompt.sh
@@ -478,6 +478,7 @@ __git_ps1 ()
 		then
 			git diff --no-ext-diff --quiet || w="*"
 			git diff --no-ext-diff --cached --quiet || i="+"
+			git diff --stat --quiet origin/"${b##refs/heads/}" || i="^"
 			if [ -z "$short_sha" ] && [ -z "$i" ]; then
 				i="#"
 			fi
