85c30e54750c41d6bea08ea9b4dbbd4b9d0e2223
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-09-06 09:50:33 +0200

    Unbreak interactive GPG prompt upon signing
    With the recent update in efee955 (gpg-interface: check gpg signature
    creation status, 2016-06-17), we ask GPG to send all status updates to
    stderr, and then catch the stderr in an strbuf.
    
    But GPG might fail, and send error messages to stderr. And we simply
    do not show them to the user.
    
    Even worse: this swallows any interactive prompt for a passphrase. And
    detaches stderr from the tty so that the passphrase cannot be read.
    
    So while the first problem could be fixed (by printing the captured
    stderr upon error), the second problem cannot be easily fixed, and
    presents a major regression.
    
    So let's just revert commit efee9553a4f97b2ecd8f49be19606dd4cf7d9c28.
    
    This fixes https://github.com/git-for-windows/git/issues/871
    
    Cc: Michael J Gruber <git@drmicha.warpmail.net>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- gpg-interface.c
+++ gpg-interface.c
@@ -156,11 +156,9 @@ int sign_buffer(struct strbuf *buffer, struct strbuf *signature, const char *sig
 	struct child_process gpg = CHILD_PROCESS_INIT;
 	int ret;
 	size_t i, j, bottom;
-	struct strbuf gpg_status = STRBUF_INIT;
 
 	argv_array_pushl(&gpg.args,
 			 gpg_program,
-			 "--status-fd=2",
 			 "-bsau", signing_key,
 			 NULL);
 
@@ -172,12 +170,10 @@ int sign_buffer(struct strbuf *buffer, struct strbuf *signature, const char *sig
 	 */
 	sigchain_push(SIGPIPE, SIG_IGN);
 	ret = pipe_command(&gpg, buffer->buf, buffer->len,
-			   signature, 1024, &gpg_status, 0);
+			   signature, 1024, NULL, 0);
 	sigchain_pop(SIGPIPE);
 
-	ret |= !strstr(gpg_status.buf, "\n[GNUPG:] SIG_CREATED ");
-	strbuf_release(&gpg_status);
-	if (ret)
+	if (ret || signature->len == bottom)
 		return error(_("gpg failed to sign the data"));
 
 	/* Strip CR from the line endings, in case we are on Windows. */
