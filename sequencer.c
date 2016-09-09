18a4b8d9f83ef1448363b55673624da170e4d3bf
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-09-09 16:47:40 +0200

    sequencer: remove bogus hint for translators
    When translating error messages, we need to be careful *not* to translate
    the todo commands such as "pick", "reword", etc because they are commands,
    and Git would not understand translated versions of those commands.
    
    Therefore, translating the commands in the error messages would simply be
    misleading.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -697,8 +697,6 @@ static int do_pick_commit(enum todo_command command, struct commit *commit,
 		return fast_forward_to(commit->object.oid.hash, head, unborn, opts);
 
 	if (parent && parse_commit(parent) < 0)
-		/* TRANSLATORS: The first %s will be "revert" or
-		   "cherry-pick", the second %s a SHA1 */
 		return error(_("%s: cannot parse parent commit %s"),
 			command_to_string(command),
 			oid_to_hex(&parent->object.oid));
