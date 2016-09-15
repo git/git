ccf638015418d59aafa475b181334f12c1b916df
Vasco Almeida <vascomalmeida@sapo.pt>
2016-09-15 14:59:00 +0000

    i18n: config: mark error message for translation
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/config.c
+++ builtin/config.c
@@ -622,8 +622,8 @@ int cmd_config(int argc, const char **argv, const char *prefix)
 		value = normalize_value(argv[0], argv[1]);
 		ret = git_config_set_in_file_gently(given_config_source.file, argv[0], value);
 		if (ret == CONFIG_NOTHING_SET)
-			error("cannot overwrite multiple values with a single value\n"
-			"       Use a regexp, --add or --replace-all to change %s.", argv[0]);
+			error(_("cannot overwrite multiple values with a single value\n"
+			"       Use a regexp, --add or --replace-all to change %s."), argv[0]);
 		return ret;
 	}
 	else if (actions == ACTION_SET_ALL) {
