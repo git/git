210f4d3ae54aa462b2f0a5d23c174060c684efb3
Lars Schneider <larsxschneider@gmail.com>
2016-09-20 21:02:39 +0200

    run-command: move check_pipe() from write_or_die to run_command
    Move check_pipe() to run_command and make it public. This is necessary
    to call the function from pkt-line in a subsequent patch.
    
    While at it, make async_exit() static to run_command.c as it is no
    longer used from outside.
    
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
    Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- run-command.h
+++ run-command.h
@@ -139,7 +139,7 @@ struct async {
 int start_async(struct async *async);
 int finish_async(struct async *async);
 int in_async(void);
-void NORETURN async_exit(int code);
+void check_pipe(int err);
 
 /**
  * This callback should initialize the child process and preload the
