e48d26dc7889eae2fb0aef270c67c9d434bce6fb
Lars Schneider <larsxschneider@gmail.com>
2016-09-20 21:02:40 +0200

    pkt-line: add packet_write_fmt_gently()
    packet_write_fmt() would die in case of a write error even though for
    some callers an error would be acceptable. Add packet_write_fmt_gently()
    which writes a formatted pkt-line like packet_write_fmt() but does not
    die in case of an error. The function is used in a subsequent patch.
    
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pkt-line.h
+++ pkt-line.h
@@ -23,6 +23,7 @@ void packet_flush(int fd);
 void packet_write_fmt(int fd, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
 void packet_buf_flush(struct strbuf *buf);
 void packet_buf_write(struct strbuf *buf, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
+int packet_write_fmt_gently(int fd, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
 
 /*
  * Read a packetized line into the buffer, which must be at least size bytes
