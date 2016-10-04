82fbd8aedd0030b2ecbb16267ef1ce0179b4d601
Pat Thoyts <patthoyts@users.sourceforge.net>
2016-10-04 12:49:05 +0100

    git-gui: maintain backwards compatibility for merge syntax
    Commit b5f325c updated to use the newer merge syntax but continue to
    support older versions of git.
    
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- lib/merge.tcl
+++ lib/merge.tcl
@@ -112,7 +112,16 @@ method _start {} {
 	close $fh
 	set _last_merged_branch $branch
 
-	set cmd [list git merge --strategy=recursive FETCH_HEAD]
+	if {[git-version >= "2.5.0"]} {
+		set cmd [list git merge --strategy=recursive FETCH_HEAD]
+	} else {
+		set cmd [list git]
+		lappend cmd merge
+		lappend cmd --strategy=recursive
+		lappend cmd [git fmt-merge-msg <[gitdir FETCH_HEAD]]
+		lappend cmd HEAD
+		lappend cmd $name
+	}
 
 	ui_status [mc "Merging %s and %s..." $current_branch $stitle]
 	set cons [console::new [mc "Merge"] "merge $stitle"]
