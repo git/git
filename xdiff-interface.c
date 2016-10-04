7dcad2b834d61826a9e1db3c0582509a0ad45eae
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-10-04 10:49:17 +0200

    fixup! Merge branch 'mmap-regexec'

--- xdiff-interface.c
+++ xdiff-interface.c
@@ -214,10 +214,11 @@ struct ff_regs {
 static long ff_regexp(const char *line, long len,
 		char *buffer, long buffer_size, void *priv)
 {
+	char *line_buffer;
 	struct ff_regs *regs = priv;
 	regmatch_t pmatch[2];
 	int i;
-	int result;
+	int result = -1;
 
 	/* Exclude terminating newline (and cr) from matching */
 	if (len > 0 && line[len-1] == '\n') {
@@ -227,16 +228,18 @@ static long ff_regexp(const char *line, long len,
 			len--;
 	}
 
+	line_buffer = xstrndup(line, len); /* make NUL terminated */
+
 	for (i = 0; i < regs->nr; i++) {
 		struct ff_reg *reg = regs->array + i;
-		if (!regexec_buf(&reg->re, line, len, 2, pmatch, 0)) {
+		if (!regexec(&reg->re, line_buffer, 2, pmatch, 0)) {
 			if (reg->negate)
-				return -1;
+				goto fail;
 			break;
 		}
 	}
 	if (regs->nr <= i)
-		return -1;
+		goto fail;
 	i = pmatch[1].rm_so >= 0 ? 1 : 0;
 	line += pmatch[i].rm_so;
 	result = pmatch[i].rm_eo - pmatch[i].rm_so;
@@ -245,6 +248,8 @@ static long ff_regexp(const char *line, long len,
 	while (result > 0 && (isspace(line[result - 1])))
 		result--;
 	memcpy(buffer, line, result);
+ fail:
+	free(line_buffer);
 	return result;
 }
 
