f6a74fda37e65a4db6a8e3b2563458675b69181c
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-10-04 14:34:21 +0200

    wt-status: begin error messages with lower-case
    The previous code still followed the old git-pull.sh code which did not
    adhere to our new convention.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wt-status.c
+++ wt-status.c
@@ -2265,15 +2265,15 @@ int require_clean_work_tree(const char *action, const char *hint, int ignore_sub
 
 	if (has_unstaged_changes(ignore_submodules)) {
 		/* TRANSLATORS: the action is e.g. "pull with rebase" */
-		error(_("Cannot %s: You have unstaged changes."), _(action));
+		error(_("cannot %s: You have unstaged changes."), _(action));
 		err = 1;
 	}
 
 	if (has_uncommitted_changes(ignore_submodules)) {
 		if (err)
-			error(_("Additionally, your index contains uncommitted changes."));
+			error(_("additionally, your index contains uncommitted changes."));
 		else
-			error(_("Cannot %s: Your index contains uncommitted changes."),
+			error(_("cannot %s: Your index contains uncommitted changes."),
 			      _(action));
 		err = 1;
 	}
