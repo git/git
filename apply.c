965d5c851ac8cea3521e805a3370c587fed78143
Vasco Almeida <vascomalmeida@sapo.pt>
2016-10-14 11:43:35 +0000

    i18n: apply: mark plural string for translation
    Mark plural string for translation using Q_().
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- apply.c
+++ apply.c
@@ -4869,10 +4869,12 @@ int apply_all_patches(struct apply_state *state,
 			goto end;
 		}
 		if (state->applied_after_fixing_ws && state->apply)
-			warning("%d line%s applied after"
-				" fixing whitespace errors.",
-				state->applied_after_fixing_ws,
-				state->applied_after_fixing_ws == 1 ? "" : "s");
+			warning(Q_("%d line applied after"
+				   " fixing whitespace errors.",
+				   "%d lines applied after"
+				   " fixing whitespace errors.",
+				   state->applied_after_fixing_ws),
+				state->applied_after_fixing_ws);
 		else if (state->whitespace_error)
 			warning(Q_("%d line adds whitespace errors.",
 				   "%d lines add whitespace errors.",
