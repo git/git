038ce90f2ff91587f2d61993f5c4c23d6fdca144
Lars Schneider <larsxschneider@gmail.com>
2016-10-16 16:20:32 -0700

    pkt-line: add packet_flush_gently()
    packet_flush() would die in case of a write error even though for some
    callers an error would be acceptable. Add packet_flush_gently() which
    writes a pkt-line flush packet like packet_flush() but does not die in
    case of an error. The function is used in a subsequent patch.
    
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pkt-line.c
+++ pkt-line.c
@@ -91,6 +91,14 @@ void packet_flush(int fd)
 	write_or_die(fd, "0000", 4);
 }
 
+int packet_flush_gently(int fd)
+{
+	packet_trace("0000", 4, 1);
+	if (write_in_full(fd, "0000", 4) == 4)
+		return 0;
+	return error("flush packet write failed");
+}
+
 void packet_buf_flush(struct strbuf *buf)
 {
 	packet_trace("0000", 4, 1);
