f25dfb5e8d26204e58d60a6fc209e4ae65827d83
Vasco Almeida <vascomalmeida@sapo.pt>
2016-10-17 13:15:26 +0000

    i18n: apply: mark error message for translation
    Update test to reflect changes.
    
    Signed-off-by: Vasco Almeida <vascomalmeida@sapo.pt>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- apply.c
+++ apply.c
@@ -1586,8 +1586,8 @@ static int find_header(struct apply_state *state,
 				patch->new_name = xstrdup(patch->def_name);
 			}
 			if (!patch->is_delete && !patch->new_name) {
-				error("git diff header lacks filename information "
-					     "(line %d)", state->linenr);
+				error(_("git diff header lacks filename information "
+					     "(line %d)"), state->linenr);
 				return -128;
 			}
 			patch->is_toplevel_relative = 1;
