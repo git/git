dfbfb9f3771e9070f239458dd2e3b535b64c4012
Ralf Thielow <ralf.thielow@gmail.com>
2016-11-11 18:21:00 +0100

    fetch-pack.c: correct command at the beginning of an error message
    One error message in fetch-pack.c uses 'git fetch_pack' at the beginning
    which is not a git command.  Use 'git fetch-pack' instead.
    
    Signed-off-by: Ralf Thielow <ralf.thielow@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fetch-pack.c
+++ fetch-pack.c
@@ -240,7 +240,7 @@ static enum ack_type get_ack(int fd, unsigned char *result_sha1)
 			return ACK;
 		}
 	}
-	die(_("git fetch_pack: expected ACK/NAK, got '%s'"), line);
+	die(_("git fetch-pack: expected ACK/NAK, got '%s'"), line);
 }
 
 static void send_request(struct fetch_pack_args *args,
