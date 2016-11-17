e87a03609c3f823cb07673e1a531941558bd50da
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-17 17:24:10 +0100

    Allow for platform-specific core.* config settings
    In the Git for Windows project, we have ample precendent for config
    settings that apply to Windows, and to Windows only.
    
    Let's formalize this concept by introducing a platform_core_config()
    function that can be #define'd in a platform-specific manner.
    
    This will allow us to contain platform-specific code better, as the
    corresponding variables no longer need to be exported so that they can
    be defined in environment.c and be set in config.c
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-compat-util.h
+++ git-compat-util.h
@@ -330,6 +330,14 @@ extern char *gitdirname(char *);
 #define _PATH_DEFPATH "/usr/local/bin:/usr/bin:/bin"
 #endif
 
+#ifndef platform_core_config
+static inline int noop_core_config(const char *var, const char *value)
+{
+	return 0;
+}
+#define platform_core_config noop_core_config
+#endif
+
 #ifndef has_dos_drive_prefix
 static inline int git_has_dos_drive_prefix(const char *path)
 {
