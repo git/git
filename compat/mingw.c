b7ddd98aa0b0b48895923c0a8f63c70d80b3c784
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-17 17:24:10 +0100

    Allow for platform-specific core.* config settings
    In the Git for Windows project, we have ample precendent for config
    settings that apply to Windows, and to Windows only.
    
    Let's formalize this concept by introducing a platform_core_config()
    function that can be #define'd in a platform-specific manner.
    
    This will allow us to contain platform-specific code better, as the
    corresponding variables no longer need to be exported so that they can
    be defined in environment.c and be set in config.c
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -202,6 +202,11 @@ static int ask_yes_no_if_possible(const char *format, ...)
 	}
 }
 
+int mingw_core_config(const char *var, const char *value)
+{
+	return 0;
+}
+
 int mingw_unlink(const char *pathname)
 {
 	int ret, tries = 0;
