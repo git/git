f2cbea605cb6c82cf5a0efe5cad38d97bb00a148
Jeff Hostetler <jeffhost@microsoft.com>
2016-11-17 09:23:55 -0500

    hashmap: allow memihash computation to be continued
    Add variant of memihash() to allow the hash computation to
    be continued.  There are times when we compute the hash on
    a full path and then the hash on just the path to the parent
    directory.  This can be expensive on large repositories.
    
    With this, we can hash the parent directory first. And then
    continue the computation to include the "/filename".
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- hashmap.h
+++ hashmap.h
@@ -12,6 +12,7 @@ extern unsigned int strhash(const char *buf);
 extern unsigned int strihash(const char *buf);
 extern unsigned int memhash(const void *buf, size_t len);
 extern unsigned int memihash(const void *buf, size_t len);
+extern unsigned int memihash_cont(unsigned int hash_seed, const void *buf, size_t len);
 
 static inline unsigned int sha1hash(const unsigned char *sha1)
 {
