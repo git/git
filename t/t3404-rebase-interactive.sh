882cd23777f8984e433e0c0addae5e9d70de75e8
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-21 15:18:29 +0100

    rebase -i: handle core.commentChar=auto
    When 84c9dc2 (commit: allow core.commentChar=auto for character auto
    selection, 2014-05-17) extended the core.commentChar functionality to
    allow for the value 'auto', it forgot that rebase -i was already taught to
    handle core.commentChar, and in turn forgot to let rebase -i handle that
    new value gracefully.
    
    Reported by Taufiq Hoven.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -983,7 +983,7 @@ test_expect_success 'rebase -i respects core.commentchar' '
 	test B = $(git cat-file commit HEAD^ | sed -ne \$p)
 '
 
-test_expect_failure 'rebase -i respects core.commentchar=auto' '
+test_expect_success 'rebase -i respects core.commentchar=auto' '
 	test_config core.commentchar auto &&
 	write_script copy-edit-script.sh <<-\EOF &&
 	cp "$1" edit-script
