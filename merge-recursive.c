55e9f0e5c9a918c246b7eae1fe2a2e954f6426af
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-26 13:48:06 +0100

    merge-recursive: handle NULL in add_cacheinfo() correctly
    1335d76e45 ("merge: avoid "safer crlf" during recording of merge
    results", 2016-07-08) tried to split make_cache_entry() call made
    with CE_MATCH_REFRESH into a call to make_cache_entry() without one,
    followed by a call to add_cache_entry(), refresh_cache() and another
    add_cache_entry() as needed.  However the conversion was botched in
    that it forgot that refresh_cache() can return NULL, which was
    handled correctly in make_cache_entry() but in the updated code.
    
    This fixes https://github.com/git-for-windows/git/issues/952
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -213,6 +213,8 @@ static int add_cacheinfo(unsigned int mode, const unsigned char *sha1,
 		struct cache_entry *nce;
 
 		nce = refresh_cache_entry(ce, CE_MATCH_REFRESH | CE_MATCH_IGNORE_MISSING);
+		if (!nce)
+			return error(_("addinfo_cache failed for path '%s'"), path);
 		if (nce != ce)
 			ret = add_cache_entry(nce, options);
 	}
