2ba512cbc68c84132461c2f9736793054f8c0e18
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-11-30 12:00:59 +0100

    t5505,t5516: create .git/branches/ when needed
    It is a real old anachronism from the Cogito days to have a
    .git/branches/ directory. And to have tests that ensure that Cogito
    users can migrate away from using that directory.
    
    But so be it, let's continue testing it.
    
    Let's make sure, however, that git init does not need to create that
    directory.
    
    This bug was noticed when testing with templates that had been
    pre-committed, skipping the empty branches/ directory of course because
    Git does not track empty directories.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5505-remote.sh
+++ t/t5505-remote.sh
@@ -830,6 +830,7 @@ test_expect_success 'migrate a remote from named file in $GIT_DIR/branches' '
 	(
 		cd six &&
 		git remote rm origin &&
+		mkdir -p .git/branches &&
 		echo "$origin_url" >.git/branches/origin &&
 		git remote rename origin origin &&
 		test_path_is_missing .git/branches/origin &&
@@ -844,6 +845,7 @@ test_expect_success 'migrate a remote from named file in $GIT_DIR/branches (2)' 
 	(
 		cd seven &&
 		git remote rm origin &&
+		mkdir -p .git/branches &&
 		echo "quux#foom" > .git/branches/origin &&
 		git remote rename origin origin &&
 		test_path_is_missing .git/branches/origin &&
