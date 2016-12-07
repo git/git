3f061bf514667497a948804828064b9b9c3b249b
Junio C Hamano <gitster@pobox.com>
2016-12-07 10:56:26 -0800

    lockfile: LOCK_REPORT_ON_ERROR
    The "libify sequencer" topic stopped passing the die_on_error option
    to hold_locked_index(), and this lost an error message from "git
    merge --ff-only $commit" when there are competing updates in
    progress.
    
    The command still exits with a non-zero status, but that is not of
    much help for an interactive user.  The last thing the command says
    is "Updating $from..$to".  We used to follow it with a big error
    message that makes it clear that "merge --ff-only" did not succeed.
    
    What is sad is that we should have noticed this regression while
    reviewing the change.  It was clear that the update to the
    checkout_fast_forward() function made a failing hold_locked_index()
    silent, but the only caller of the checkout_fast_forward() function
    had this comment:
    
    	    if (checkout_fast_forward(from, to, 1))
        -               exit(128); /* the callee should have complained already */
        +               return -1; /* the callee should have complained already */
    
    which clearly contradicted the assumption X-<.
    
    Add a new option LOCK_REPORT_ON_ERROR that can be passed instead of
    LOCK_DIE_ON_ERROR to the hold_lock*() family of functions and teach
    checkout_fast_forward() to use it to fix this regression.
    
    After going thourgh all calls to hold_lock*() family of functions
    that used to pass LOCK_DIE_ON_ERROR but were modified to pass 0 in
    the "libify sequencer" topic "git show --first-parent 2a4062a4a8",
    it appears that this is the only one that has become silent.  Many
    others used to give detailed report that talked about "there may be
    competing Git process running" but with the series merged they now
    only give a single liner "Unable to lock ...", some of which may
    have to be tweaked further, but at least they say something, unlike
    the one this patch fixes.
    
    Reported-by: Robbie Iannucci <iannucci@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- lockfile.c
+++ lockfile.c
@@ -174,8 +174,16 @@ int hold_lock_file_for_update_timeout(struct lock_file *lk, const char *path,
 				      int flags, long timeout_ms)
 {
 	int fd = lock_file_timeout(lk, path, flags, timeout_ms);
-	if (fd < 0 && (flags & LOCK_DIE_ON_ERROR))
-		unable_to_lock_die(path, errno);
+	if (fd < 0) {
+		if (flags & LOCK_DIE_ON_ERROR)
+			unable_to_lock_die(path, errno);
+		if (flags & LOCK_REPORT_ON_ERROR) {
+			struct strbuf buf = STRBUF_INIT;
+			unable_to_lock_message(path, errno, &buf);
+			error("%s", buf.buf);
+			strbuf_release(&buf);
+		}
+	}
 	return fd;
 }
 
--- lockfile.h
+++ lockfile.h
@@ -129,9 +129,15 @@ struct lock_file {
 /*
  * If a lock is already taken for the file, `die()` with an error
  * message. If this flag is not specified, trying to lock a file that
- * is already locked returns -1 to the caller.
+ * is already locked silently returns -1 to the caller, or ...
  */
 #define LOCK_DIE_ON_ERROR 1
+
+/*
+ * ... this flag can be passed instead to return -1 and give the usual
+ * error message upon an error.
+ */
+#define LOCK_REPORT_ON_ERROR 2
 
 /*
  * Usually symbolic links in the destination path are resolved. This
