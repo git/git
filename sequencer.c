c01da86827519798f0016186737a0a17047dc98c
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-12-19 14:52:04 +0100

    sequencer: move "else" keyword onto the same line as preceding brace
    It is the current coding style of the Git project to write
    
    	if (...) {
    		...
    	} else {
    		...
    	}
    
    instead of putting the closing brace and the "else" keyword on separate
    lines.
    
    Pointed out by Junio Hamano.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -1117,8 +1117,7 @@ static int create_seq_dir(void)
 		error(_("a cherry-pick or revert is already in progress"));
 		advise(_("try \"git cherry-pick (--continue | --quit | --abort)\""));
 		return -1;
-	}
-	else if (mkdir(git_path_seq_dir(), 0777) < 0)
+	} else if (mkdir(git_path_seq_dir(), 0777) < 0)
 		return error_errno(_("could not create sequencer directory '%s'"),
 				   git_path_seq_dir());
 	return 0;
