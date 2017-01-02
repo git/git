52865279ee1187a0f630779d2015587ae702b1f4
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-02 16:27:34 +0100

    sequencer (rebase -i): the todo can be empty when continuing
    When the last command of an interactive rebase fails, the user needs to
    resolve the problem and then continue the interactive rebase. Naturally,
    the todo script is empty by then. So let's not complain about that!
    
    To that end, let's move that test out of the function that parses the
    todo script, and into the more high-level function read_populate_todo().
    This is also necessary by now because the lower-level parse_insn_buffer()
    has no idea whether we are performing an interactive rebase or not.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -1215,8 +1215,7 @@ static int parse_insn_buffer(char *buf, struct todo_list *todo_list)
 		else if (!is_noop(item->command))
 			fixup_okay = 1;
 	}
-	if (!todo_list->nr)
-		return error(_("no commits parsed."));
+
 	return res;
 }
 
@@ -1239,6 +1238,10 @@ static int read_populate_todo(struct todo_list *todo_list,
 	res = parse_insn_buffer(todo_list->buf.buf, todo_list);
 	if (res)
 		return error(_("unusable instruction sheet: '%s'"), todo_file);
+
+	if (!todo_list->nr &&
+	    (!is_rebase_i(opts) || !file_exists(rebase_path_done())))
+		return error(_("no commits parsed."));
 
 	if (!is_rebase_i(opts)) {
 		enum todo_command valid =
