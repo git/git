e96ff3cd7413d8406cd8527126bd2762ea868701
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-10 11:27:40 +0100

    fixup! Merge branch 'builtin-difftool'
    This branch will be replaced with the fourth iteration of the
    builtin-difftool patch series sent to the Git mailing list.

--- exec_cmd.c
+++ exec_cmd.c
@@ -65,7 +65,6 @@ void git_set_argv_exec_path(const char *exec_path)
 const char *git_exec_path(void)
 {
 	const char *env;
-	static char *system_exec_path;
 
 	if (argv_exec_path)
 		return argv_exec_path;
@@ -75,9 +74,7 @@ const char *git_exec_path(void)
 		return env;
 	}
 
-	if (!system_exec_path)
-		system_exec_path = system_path(GIT_EXEC_PATH);
-	return system_exec_path;
+	return system_path(GIT_EXEC_PATH);
 }
 
 static void add_path(struct strbuf *out, const char *path)
