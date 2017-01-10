39143f9a0786c537830de72b8b8f68cf3899fbfb
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-10 23:30:13 +0100

    mingw: respect core.hidedotfiles = false in git-init again
    This is a brown paper bag. When adding the tests, we actually failed
    to verify that the config variable is heeded in git-init at all. And
    when changing the original patch that marked the .git/ directory as
    hidden after reading the config, it was lost on this developer that
    the new code would use the hide_dotfiles variable before the config
    was read.
    
    The fix is obvious: read the (limited, pre-init) config *before*
    creating the .git/ directory.
    
    This fixes https://github.com/git-for-windows/git/issues/789
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t0001-init.sh
+++ t/t0001-init.sh
@@ -427,6 +427,18 @@ test_expect_success 're-init from a linked worktree' '
 	)
 '
 
+test_expect_success MINGW 'core.hidedotfiles = false' '
+	git config --global core.hidedotfiles false &&
+	rm -rf newdir &&
+	(
+		unset GIT_DIR GIT_WORK_TREE GIT_CONFIG
+		mkdir newdir &&
+		cd newdir &&
+		git init
+	) &&
+	! is_hidden newdir/.git
+'
+
 test_expect_success MINGW 'redirect std handles' '
 	GIT_REDIRECT_STDOUT=output.txt git rev-parse --git-dir &&
 	test .git = "$(cat output.txt)" &&
