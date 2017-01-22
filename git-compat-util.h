3ca869940994866796edf9e53e52d80f82c3c04c
René Scharfe <l.s.r@web.de>
2017-01-22 18:52:13 +0100

    add QSORT_S
    Add the macro QSORT_S, a convenient wrapper for qsort_s() that infers
    the size of the array elements and dies on error.
    
    Basically all possible errors are programming mistakes (passing NULL as
    base of a non-empty array, passing NULL as comparison function,
    out-of-bounds accesses), so terminating the program should be acceptable
    for most callers.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-compat-util.h
+++ git-compat-util.h
@@ -994,6 +994,11 @@ int git_qsort_s(void *base, size_t nmemb, size_t size,
 #define qsort_s git_qsort_s
 #endif
 
+#define QSORT_S(base, n, compar, ctx) do {			\
+	if (qsort_s((base), (n), sizeof(*(base)), compar, ctx))	\
+		die("BUG: qsort_s() failed");			\
+} while (0)
+
 #ifndef REG_STARTEND
 #error "Git requires REG_STARTEND support. Compile with NO_REGEX=NeedsStartEnd"
 #endif
