55d2d812e49aece049b73682ad5980ea84e23839
brian m. carlson <sandals@crustytoothpaste.net>
2017-01-26 00:13:44 +0000

    Documentation: implement linkgit macro for Asciidoctor
    AsciiDoc uses a configuration file to implement macros like linkgit,
    while Asciidoctor uses Ruby extensions.  Implement a Ruby extension that
    implements the linkgit macro for Asciidoctor in the same way that
    asciidoc.conf does for AsciiDoc.  Adjust the Makefile to use it by
    default.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ Documentation/asciidoctor-extensions.rb
@@ -0,0 +1,28 @@
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+module Git
+  module Documentation
+    class LinkGitProcessor < Asciidoctor::Extensions::InlineMacroProcessor
+      use_dsl
+
+      named :chrome
+
+      def process(parent, target, attrs)
+        if parent.document.basebackend? 'html'
+          prefix = parent.document.attr('git-relative-html-prefix')
+          %(<a href="#{prefix}#{target}.html">#{target}(#{attrs[1]})</a>\n)
+        elsif parent.document.basebackend? 'docbook'
+          "<citerefentry>\n" \
+            "<refentrytitle>#{target}</refentrytitle>" \
+            "<manvolnum>#{attrs[1]}</manvolnum>\n" \
+          "</citerefentry>\n"
+        end
+      end
+    end
+  end
+end
+
+Asciidoctor::Extensions.register do
+  inline_macro Git::Documentation::LinkGitProcessor, :linkgit
+end
