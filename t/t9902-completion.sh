e5edbef48db005ef00d65389eecf50c1321c18e8
SZEDER Gábor <szeder.dev@gmail.com>
2017-02-03 03:48:10 +0100

    completion tests: don't add test cruft to the test repository
    While preparing commits, three tests added newly created files to the
    index using 'git add .', which added not only the files in question
    but leftover test cruft from previous tests like the files 'expected'
    and 'actual' as well.  Luckily, this had no effect on the tests'
    correctness.
    
    Add only the files we are actually interested in.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t9902-completion.sh
+++ t/t9902-completion.sh
@@ -486,7 +486,7 @@ test_expect_success 'git --help completion' '
 test_expect_success 'setup for ref completion' '
 	echo content >file1 &&
 	echo more >file2 &&
-	git add . &&
+	git add file1 file2 &&
 	git commit -m one &&
 	git branch mybranch &&
 	git tag mytag
@@ -517,7 +517,7 @@ test_expect_success '<ref>: completes paths' '
 
 test_expect_success 'complete tree filename with spaces' '
 	echo content >"name with spaces" &&
-	git add . &&
+	git add "name with spaces" &&
 	git commit -m spaces &&
 	test_completion "git show HEAD:nam" <<-\EOF
 	name with spaces Z
@@ -526,7 +526,7 @@ test_expect_success 'complete tree filename with spaces' '
 
 test_expect_success 'complete tree filename with metacharacters' '
 	echo content >"name with \${meta}" &&
-	git add . &&
+	git add "name with \${meta}" &&
 	git commit -m meta &&
 	test_completion "git show HEAD:nam" <<-\EOF
 	name with ${meta} Z
