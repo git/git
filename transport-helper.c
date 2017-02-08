438fc68462d5839ef6ca231cdcb411105eaf0ba2
Stefan Beller <sbeller@google.com>
2017-02-08 14:04:00 -0800

    push options: pass push options to the transport helper
    When using non-builtin protocols relying on a transport helper
    (such as http), push options are not propagated to the helper.
    
    The user could ask for push options and a push would seemingly succeed,
    but the push options would never be transported to the server,
    misleading the users expectation.
    
    Fix this by propagating the push options to the transport helper.
    
    This is only addressing the first issue of
       (1) the helper protocol does not propagate push-option
       (2) the http helper is not prepared to handle push-option
    
    Once we fix (2), the http transport helper can make use of push options
    as well, but that happens as a follow up. (1) is a bug fix, whereas (2)
    is a feature, which is why we only do (1) here.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport-helper.c
+++ transport-helper.c
@@ -826,6 +826,13 @@ static void set_common_push_options(struct transport *transport,
 		if (set_helper_option(transport, TRANS_OPT_PUSH_CERT, "if-asked") != 0)
 			die("helper %s does not support --signed=if-asked", name);
 	}
+
+	if (flags & TRANSPORT_PUSH_OPTIONS) {
+		struct string_list_item *item;
+		for_each_string_list_item(item, transport->push_options)
+			if (set_helper_option(transport, "push-option", item->string) != 0)
+				die("helper %s does not support 'push-option'", name);
+	}
 }
 
 static int push_refs_with_push(struct transport *transport,
