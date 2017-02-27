6cc1053375d8e138b8f05dde4cd0d4ab8f92254a
Christian Couder <christian.couder@gmail.com>
2017-02-27 19:00:03 +0100

    update-index: warn in case of split-index incoherency
    When users are using `git update-index --(no-)split-index`, they
    may expect the split-index feature to be used or not according to
    the option they just used, but this might not be the case if the
    new "core.splitIndex" config variable has been set. In this case
    let's warn about what will happen and why.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -1099,12 +1099,21 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 	}
 
 	if (split_index > 0) {
+		if (git_config_get_split_index() == 0)
+			warning(_("core.splitIndex is set to false; "
+				  "remove or change it, if you really want to "
+				  "enable split index"));
 		if (the_index.split_index)
 			the_index.cache_changed |= SPLIT_INDEX_ORDERED;
 		else
 			add_split_index(&the_index);
-	} else if (!split_index)
+	} else if (!split_index) {
+		if (git_config_get_split_index() == 1)
+			warning(_("core.splitIndex is set to true; "
+				  "remove or change it, if you really want to "
+				  "disable split index"));
 		remove_split_index(&the_index);
+	}
 
 	switch (untracked_cache) {
 	case UC_UNSPECIFIED:
