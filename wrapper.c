34de5e4bb0dfe1c05b57c733c2196f1ad3881a98
Ramsay Jones <ramsay@ramsayjones.plus.com>
2017-02-28 01:24:10 +0000

    wrapper.c: remove unused git_mkstemp() function
    The last caller of git_mkstemp() was removed in commit 6fec0a89
    ("verify_signed_buffer: use tempfile object", 16-06-2016). Since
    the introduction of the 'tempfile' APIs, along with git_mkstemp_mode,
    it is unlikely that new callers will materialize. Remove the dead
    code.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wrapper.c
+++ wrapper.c
@@ -440,23 +440,6 @@ int xmkstemp(char *template)
 	return fd;
 }
 
-/* git_mkstemp() - create tmp file honoring TMPDIR variable */
-int git_mkstemp(char *path, size_t len, const char *template)
-{
-	const char *tmp;
-	size_t n;
-
-	tmp = getenv("TMPDIR");
-	if (!tmp)
-		tmp = "/tmp";
-	n = snprintf(path, len, "%s/%s", tmp, template);
-	if (len <= n) {
-		errno = ENAMETOOLONG;
-		return -1;
-	}
-	return mkstemp(path);
-}
-
 /* Adapted from libiberty's mkstemp.c. */
 
 #undef TMP_MAX
