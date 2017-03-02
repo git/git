769dea28724b4156aaf77ccd6245af75e01078b2
Prathamesh <pc44800@gmail.com>
2017-03-02 09:27:17 +0530

    t6004: avoid using pipes
    The exit code of the upstream of a pipe is ignored thus we should avoid
    using it. By writing out the output of the git command to a file, we
    can test the exit codes of both the commands.
    
    Signed-off-by: Prathamesh <pc44800@gmail.com>

--- t/t6004-rev-list-path-optim.sh
+++ t/t6004-rev-list-path-optim.sh
@@ -30,8 +30,8 @@ test_expect_success setup '
 test_expect_success path-optimization '
 	test_tick &&
 	commit=$(echo "Unchanged tree" | git commit-tree "HEAD^{tree}" -p HEAD) &&
-	test $(git rev-list $commit | wc -l) = 2 &&
-	test $(git rev-list $commit -- . | wc -l) = 1
+	test $(git rev-list $commit >out && wc -l <out) = 2 &&
+	test $(git rev-list $commit -- . >out && wc -l <out) = 1
 '
 
 test_expect_success 'further setup' '
