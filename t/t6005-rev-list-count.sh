1e1f07bfe7db46fad84fc45ea59a334ee1f8a7f1
Prathamesh <pc44800@gmail.com>
2017-03-02 09:32:51 +0530

    t6005: avoid using pipes
    The exit code of the upstream of a pipe is ignored thus we should avoid
    using it. By writing out the output of the git command to a file, we
    can test the exit codes of both the commands.
    
    Signed-off-by: Prathamesh <pc44800@gmail.com>

--- t/t6005-rev-list-count.sh
+++ t/t6005-rev-list-count.sh
@@ -13,39 +13,39 @@ test_expect_success 'setup' '
 '
 
 test_expect_success 'no options' '
-    test $(git rev-list HEAD | wc -l) = 5
+    test $(git rev-list HEAD >out && wc -l <out) = 5
 '
 
 test_expect_success '--max-count' '
-    test $(git rev-list HEAD --max-count=0 | wc -l) = 0 &&
-    test $(git rev-list HEAD --max-count=3 | wc -l) = 3 &&
-    test $(git rev-list HEAD --max-count=5 | wc -l) = 5 &&
-    test $(git rev-list HEAD --max-count=10 | wc -l) = 5
+    test $(git rev-list HEAD --max-count=0 >out && wc -l <out) = 0 &&
+    test $(git rev-list HEAD --max-count=3 >out && wc -l <out) = 3 &&
+    test $(git rev-list HEAD --max-count=5 >out && wc -l <out) = 5 &&
+    test $(git rev-list HEAD --max-count=10 >out && wc -l <out) = 5
 '
 
 test_expect_success '--max-count all forms' '
-    test $(git rev-list HEAD --max-count=1 | wc -l) = 1 &&
-    test $(git rev-list HEAD -1 | wc -l) = 1 &&
-    test $(git rev-list HEAD -n1 | wc -l) = 1 &&
-    test $(git rev-list HEAD -n 1 | wc -l) = 1
+    test $(git rev-list HEAD --max-count=1 >out && wc -l <out) = 1 &&
+    test $(git rev-list HEAD -1 >out && wc -l <out) = 1 &&
+    test $(git rev-list HEAD -n1 >out && wc -l <out) = 1 &&
+    test $(git rev-list HEAD -n 1 >out && wc -l <out) = 1
 '
 
 test_expect_success '--skip' '
-    test $(git rev-list HEAD --skip=0 | wc -l) = 5 &&
-    test $(git rev-list HEAD --skip=3 | wc -l) = 2 &&
-    test $(git rev-list HEAD --skip=5 | wc -l) = 0 &&
-    test $(git rev-list HEAD --skip=10 | wc -l) = 0
+    test $(git rev-list HEAD --skip=0 >out && wc -l <out) = 5 &&
+    test $(git rev-list HEAD --skip=3 >out && wc -l <out) = 2 &&
+    test $(git rev-list HEAD --skip=5 >out && wc -l <out) = 0 &&
+    test $(git rev-list HEAD --skip=10 >out && wc -l <out) = 0
 '
 
 test_expect_success '--skip --max-count' '
-    test $(git rev-list HEAD --skip=0 --max-count=0 | wc -l) = 0 &&
-    test $(git rev-list HEAD --skip=0 --max-count=10 | wc -l) = 5 &&
-    test $(git rev-list HEAD --skip=3 --max-count=0 | wc -l) = 0 &&
-    test $(git rev-list HEAD --skip=3 --max-count=1 | wc -l) = 1 &&
-    test $(git rev-list HEAD --skip=3 --max-count=2 | wc -l) = 2 &&
-    test $(git rev-list HEAD --skip=3 --max-count=10 | wc -l) = 2 &&
-    test $(git rev-list HEAD --skip=5 --max-count=10 | wc -l) = 0 &&
-    test $(git rev-list HEAD --skip=10 --max-count=10 | wc -l) = 0
+    test $(git rev-list HEAD --skip=0 --max-count=0 >out && wc -l <out) = 0 &&
+    test $(git rev-list HEAD --skip=0 --max-count=10 >out && wc -l <out) = 5 &&
+    test $(git rev-list HEAD --skip=3 --max-count=0 >out && wc -l <out) = 0 &&
+    test $(git rev-list HEAD --skip=3 --max-count=1 >out && wc -l <out) = 1 &&
+    test $(git rev-list HEAD --skip=3 --max-count=2 >out && wc -l <out) = 2 &&
+    test $(git rev-list HEAD --skip=3 --max-count=10 >out && wc -l <out) = 2 &&
+    test $(git rev-list HEAD --skip=5 --max-count=10 >out && wc -l <out) = 0 &&
+    test $(git rev-list HEAD --skip=10 --max-count=10 >out && wc -l <out) = 0
 '
 
 test_done
