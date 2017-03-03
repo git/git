dc2ef65efb00646bed8356731ff18bcbbc54b085
Prathamesh <pc44800@gmail.com>
2017-03-03 11:36:00 +0530

    t0027: avoid using pipes
    The exit code of the upstream of a pipe is ignored thus we should avoid
    using it. By writing out the output of the git command to a file, we
    can test the exit codes of both the commands.
    
    Signed-off-by: Prathamesh <pc44800@gmail.com>

--- t/t0027-auto-crlf.sh
+++ t/t0027-auto-crlf.sh
@@ -260,8 +260,8 @@ checkout_files () {
 		i/-text w/$(stats_ascii $crlfnul) attr/$(attr_ascii $attr $aeol) crlf_false_attr__CRLF_nul.txt
 		i/-text w/$(stats_ascii $crlfnul) attr/$(attr_ascii $attr $aeol) crlf_false_attr__LF_nul.txt
 		EOF
-		git ls-files --eol crlf_false_attr__* |
-		sed -e "s/	/ /g" -e "s/  */ /g" |
+		git ls-files --eol crlf_false_attr__* >out &&
+		sed -e "s/	/ /g" -e "s/  */ /g" <out |
 		sort >actual &&
 		test_cmp expect actual
 	'
@@ -308,12 +308,12 @@ test_expect_success 'ls-files --eol -o Text/Binary' '
 	i/ w/crlf TeBi_126_CL
 	i/ w/-text TeBi_126_CLC
 	EOF
-	git ls-files --eol -o |
+	git ls-files --eol -o >out &&
 	sed -n -e "/TeBi_/{s!attr/[	]*!!g
 	s!	! !g
 	s!  *! !g
 	p
-	}" | sort >actual &&
+	}" <out | sort >actual &&
 	test_cmp expect actual
 '
 
@@ -543,8 +543,8 @@ test_expect_success 'ls-files --eol -d -z' '
 	i/lf w/ crlf_false_attr__LF.txt
 	i/mixed w/ crlf_false_attr__CRLF_mix_LF.txt
 	EOF
-	git ls-files --eol -d |
-	sed -e "s!attr/[^	]*!!g" -e "s/	/ /g" -e "s/  */ /g" |
+	git ls-files --eol -d >out &&
+	sed -e "s!attr/[^	]*!!g" -e "s/	/ /g" -e "s/  */ /g" <out |
 	sort >actual &&
 	test_cmp expect actual
 '
