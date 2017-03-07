40d05d04dd338f80b5392e8dea3a5c854798351e
Jeff King <peff@peff.net>
2017-03-07 08:37:36 -0500

    send-pack: improve unpack-status error messages
    When the remote tells us that the "unpack" step failed, we
    show an error message. However, unless you are familiar with
    the internals of send-pack and receive-pack, it was not
    clear that this represented an error on the remote side.
    Let's re-word to make that more obvious.
    
    Likewise, when we got an unexpected packet from the other
    end, we complained with a vague message but did not actually
    show the packet.  Let's fix that.
    
    And finally, neither message was marked for translation. The
    message from the remote probably won't be translated, but
    there's no reason we can't do better for the local half.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- send-pack.c
+++ send-pack.c
@@ -134,9 +134,9 @@ static int receive_unpack_status(int in)
 {
 	const char *line = packet_read_line(in, NULL);
 	if (!skip_prefix(line, "unpack ", &line))
-		return error("did not receive remote status");
+		return error(_("unable to parse remote unpack status: %s"), line);
 	if (strcmp(line, "ok"))
-		return error("unpack failed: %s", line);
+		return error(_("remote unpack failed: %s"), line);
 	return 0;
 }
 
