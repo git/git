267b4538c0e13f98870bf808ee6c71fca9cf2ef7
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-13 21:11:08 +0100

    read_early_config(): avoid .git/config hack when unneeded
    So far, we only look whether the startup_info claims to have seen a
    git_dir.
    
    However, do_git_config_sequence() (and consequently the
    git_config_with_options() call used by read_early_config() asks the
    have_git_dir() function whether we have a .git/ directory, which in turn
    also looks at git_dir and at the environment variable GIT_DIR. And when
    this is the case, the repository config is handled already, so we do not
    have to do that again explicitly.
    
    Let's just use the same function, have_git_dir(), to determine whether we
    have to handle .git/config explicitly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.c
+++ config.c
@@ -1427,14 +1427,15 @@ void read_early_config(config_fn_t cb, void *data)
 	 * core.repositoryformatversion), we'll read whatever is in .git/config
 	 * blindly. Similarly, if we _are_ in a repository, but not at the
 	 * root, we'll fail to find .git/config (because it's really
-	 * ../.git/config, etc). See t7006 for a complete set of failures.
+	 * ../.git/config, etc), unless setup_git_directory() was already called.
+	 * See t7006 for a complete set of failures.
 	 *
 	 * However, we have historically provided this hack because it does
 	 * work some of the time (namely when you are at the top-level of a
 	 * valid repository), and would rarely make things worse (i.e., you do
 	 * not generally have a .git/config file sitting around).
 	 */
-	if (!startup_info->have_repository) {
+	if (!have_git_dir()) {
 		struct git_config_source repo_config;
 
 		memset(&repo_config, 0, sizeof(repo_config));
