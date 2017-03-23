b42ca35e5c4f792810c08e95676ba63976f4cd02
Santiago Torres <santiago@nyu.edu>
2017-03-23 18:28:47 -0400

    t7004, t7030: fix here-doc syntax errors
    Jan Palus noticed that some here-doc are spelled incorrectly,
    resulting the entire remainder of the test snippet being slurped
    into the "expect" file as if it were data, e.g. in this sequence
    
    	cat >expect <<EOF &&
    	... expectation ...
    	EOF
    	git $cmd_being_tested >actual &&
    	test_cmp expect actual
    
    the last command of the test is "cat" that sends everything to
    'expect' and succeeds.
    
    Fixing these issues in t7004 and t7030 reveals that "git tag -v"
    and "git verify-tag" with their --format option do not work as the
    test was expecting originally.  Instead of showing both valid tags
    and tags with incorrect signatures on their output, tags that do not
    pass verification are omitted from the output.  Another breakage that
    is uncovered is that these tests must be restricted to environment
    where gpg is available.
    
    Arguably, that is a safer behaviour, and because the format
    specifiers like %(tag) do not have a way to show if the signature
    verifies correctly, the command with the --format option cannot be
    used to get a list of tags annotated with their signature validity
    anyway.
    
    For now, let's fix the here-doc syntax, update the expectation to
    match the reality, and update the test prerequisite.
    
    Maybe later when we extend the --format language available to "git
    tag -v" and "git verify-tag" to include things like "%(gpg:status)",
    we may want to change the behaviour so that piping a list of tag
    names into
    
        xargs git verify-tag --format='%(gpg:status) %(tag)'
    
    becomes a good way to produce such a list, but that is a separate
    topic.
    
    Noticed-by: Jan Palus <jan.palus@gmail.com>
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Santiago Torres <santiago@nyu.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7030-verify-tag.sh
+++ t/t7030-verify-tag.sh
@@ -125,18 +125,16 @@ test_expect_success GPG 'verify multiple tags' '
 	test_cmp expect.stderr actual.stderr
 '
 
-test_expect_success 'verifying tag with --format' '
-	cat >expect <<-\EOF
+test_expect_success GPG 'verifying tag with --format' '
+	cat >expect <<-\EOF &&
 	tagname : fourth-signed
-	EOF &&
+	EOF
 	git verify-tag --format="tagname : %(tag)" "fourth-signed" >actual &&
 	test_cmp expect actual
 '
 
-test_expect_success 'verifying a forged tag with --format fail and format accordingly' '
-	cat >expect <<-\EOF
-	tagname : 7th forged-signed
-	EOF &&
+test_expect_success GPG 'verifying a forged tag with --format should fail silently' '
+	>expect &&
 	test_must_fail git verify-tag --format="tagname : %(tag)" $(cat forged1.tag) >actual-forged &&
 	test_cmp expect actual-forged
 '
