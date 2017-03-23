a42e1b4194b02638ea4f590ac1e7d231274886d1
Brandon Williams <bmwill@google.com>
2017-03-23 10:02:33 -0700

    sequencer: fix missing newline
    When using rebase --interactive where one of the lines is marked as
    'edit' this is the resulting output:
    
        Stopped at ec3b9c4...  stuffYou can amend the commit now, with
    
          git commit --amend
    
        Once you are satisfied with your changes, run
    
          git rebase --continue
    
    A newline character is missing at the end of the "Stopped at ..." line and
    before the "You can amend ..." line.  This patch fixes the malformed output by
    adding the missing newline character to the end of the "Stopped at ..." line.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -1998,7 +1998,7 @@ static int pick_commits(struct todo_list *todo_list, struct replay_opts *opts)
 				struct commit *commit = item->commit;
 				if (!res)
 					fprintf(stderr,
-						_("Stopped at %s...  %.*s"),
+						_("Stopped at %s...  %.*s\n"),
 						short_commit_name(commit),
 						item->arg_len, item->arg);
 				return error_with_patch(commit,
