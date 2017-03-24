eab98ee5ab84def96012f115a8e10571c6e24e4e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-03-24 18:40:53 +0000

    parse-options: add OPT_NONEG to the "contains" option
    Add the OPT_NONEG flag to the "contains" option and its hidden synonym
    "with". Since this was added in commit 694a577519 ("git-branch
    --contains=commit", 2007-11-07) giving --no-{contains,with} hasn't
    been an error, but has emitted the help output since
    filter.with_commit wouldn't get set.
    
    Now git will emit "error: unknown option `no-{contains,with}'" at the
    top of the help output.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.h
+++ parse-options.h
@@ -258,7 +258,7 @@ extern int parse_opt_passthru_argv(const struct option *, const char *, int);
 	  PARSE_OPT_LASTARG_DEFAULT | flag, \
 	  parse_opt_commits, (intptr_t) "HEAD" \
 	}
-#define OPT_CONTAINS(v, h) _OPT_CONTAINS_OR_WITH("contains", v, h, 0)
-#define OPT_WITH(v, h) _OPT_CONTAINS_OR_WITH("with", v, h, PARSE_OPT_HIDDEN)
+#define OPT_CONTAINS(v, h) _OPT_CONTAINS_OR_WITH("contains", v, h, PARSE_OPT_NONEG)
+#define OPT_WITH(v, h) _OPT_CONTAINS_OR_WITH("with", v, h, PARSE_OPT_HIDDEN | PARSE_OPT_NONEG)
 
 #endif
