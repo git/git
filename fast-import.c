98718242cfd18f106dfcd7663282fb9906fd38a5
Jeff King <peff@peff.net>
2017-03-24 13:26:24 -0400

    fast-import: use xsnprintf for formatting headers
    The stream_blob() function checks the return value of
    snprintf and dies. This is more simply done with
    xsnprintf (and matches the similar call in store_object).
    
    The message the user would get is less specific, but since
    the point is that this _shouldn't_ ever happen, that's OK.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fast-import.c
+++ fast-import.c
@@ -1237,9 +1237,7 @@ static void stream_blob(uintmax_t len, unsigned char *sha1out, uintmax_t mark)
 	sha1file_checkpoint(pack_file, &checkpoint);
 	offset = checkpoint.offset;
 
-	hdrlen = snprintf((char *)out_buf, out_sz, "blob %" PRIuMAX, len) + 1;
-	if (out_sz <= hdrlen)
-		die("impossibly large object header");
+	hdrlen = xsnprintf((char *)out_buf, out_sz, "blob %" PRIuMAX, len) + 1;
 
 	git_SHA1_Init(&c);
 	git_SHA1_Update(&c, out_buf, hdrlen);
