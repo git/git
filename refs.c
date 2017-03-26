24c8407e0aab839a2de57573fd6dd6320f172af3
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2017-03-26 09:42:25 +0700

    refs.c: introduce get_main_ref_store()
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -1456,15 +1456,20 @@ static struct ref_store *ref_store_init(const char *submodule)
 	return refs;
 }
 
+static struct ref_store *get_main_ref_store(void)
+{
+	if (main_ref_store)
+		return main_ref_store;
+
+	return ref_store_init(NULL);
+}
+
 struct ref_store *get_ref_store(const char *submodule)
 {
 	struct ref_store *refs;
 
 	if (!submodule || !*submodule) {
-		refs = lookup_ref_store(NULL);
-
-		if (!refs)
-			refs = ref_store_init(NULL);
+		return get_main_ref_store();
 	} else {
 		refs = lookup_ref_store(submodule);
 
