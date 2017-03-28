b0ceab98d72a4861a8cea061487169fb5468b736
Jeff King <peff@peff.net>
2017-03-28 15:46:23 -0400

    tag: use strbuf to format tag header
    We format the tag header into a fixed 1024-byte buffer. But
    since the tag-name and tagger ident can be arbitrarily
    large, we may unceremoniously die with "tag header too big".
    Let's just use a strbuf instead.
    
    Note that it looks at first glance like we can just format
    this directly into the "buf" strbuf where it will ultimately
    go. But that buffer may already contain the tag message, and
    we have no easy way to prepend formatted data to a strbuf
    (we can only splice in an already-generated buffer). This
    isn't a performance-critical path, so going through an extra
    buffer isn't a big deal.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- builtin/tag.c
+++ builtin/tag.c
@@ -231,26 +231,22 @@ static void create_tag(const unsigned char *object, const char *tag,
 		       unsigned char *prev, unsigned char *result)
 {
 	enum object_type type;
-	char header_buf[1024];
-	int header_len;
+	struct strbuf header = STRBUF_INIT;
 	char *path = NULL;
 
 	type = sha1_object_info(object, NULL);
 	if (type <= OBJ_NONE)
 	    die(_("bad object type."));
 
-	header_len = snprintf(header_buf, sizeof(header_buf),
-			  "object %s\n"
-			  "type %s\n"
-			  "tag %s\n"
-			  "tagger %s\n\n",
-			  sha1_to_hex(object),
-			  typename(type),
-			  tag,
-			  git_committer_info(IDENT_STRICT));
-
-	if (header_len > sizeof(header_buf) - 1)
-		die(_("tag header too big."));
+	strbuf_addf(&header,
+		    "object %s\n"
+		    "type %s\n"
+		    "tag %s\n"
+		    "tagger %s\n\n",
+		    sha1_to_hex(object),
+		    typename(type),
+		    tag,
+		    git_committer_info(IDENT_STRICT));
 
 	if (!opt->message_given) {
 		int fd;
@@ -288,7 +284,8 @@ static void create_tag(const unsigned char *object, const char *tag,
 	if (!opt->message_given && !buf->len)
 		die(_("no tag message?"));
 
-	strbuf_insert(buf, 0, header_buf, header_len);
+	strbuf_insert(buf, 0, header.buf, header.len);
+	strbuf_release(&header);
 
 	if (build_tag_object(buf, opt->sign, result) < 0) {
 		if (path)
