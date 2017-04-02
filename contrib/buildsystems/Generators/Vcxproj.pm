e07d7179b00fc1d868daf6c5f522e286f991b999
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-02 22:11:42 +0200

    fixup! contrib/buildsystems: support modern Visual Studio project definitions
    Turns out that the <AdditionalLibraryDirectories> directive is
    unnecessary, as we specify our dependencies with full path anyway.
    
    It is even detrimental, as the flags we get from the current `make
    MSVC=1` dry-run include architecture-specific libraries, which make
    32-bit builds fail when the .vcxproj files were generated in a 64-bit
    setup. These 32-bit builds can be triggered e.g. by calling
    
    	msbuild /p:Configuration=Release;Platform=x86 git.sln
    
    in an MSBuild Command Prompt.
    
    This fixes https://github.com/git-for-windows/git/issues/1104.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/buildsystems/Generators/Vcxproj.pm
+++ contrib/buildsystems/Generators/Vcxproj.pm
@@ -131,7 +131,6 @@ sub createProject {
     if (!$static_library) {
       $libs = join(";", sort(grep /^(?!libgit\.lib|xdiff\/lib\.lib|vcs-svn\/lib\.lib|libcurl\.lib|libeay32\.lib|libiconv\.lib|ssleay32\.lib|zlib\.lib)/, @{$$build_structure{"$prefix${name}_LIBS"}}));
     }
-    my $ldirs = join(";", map { /^-L(?!TCG)(.*)/ ? (File::Spec->file_name_is_absolute($1) ? $1 : "$rel_dir\\$1") : () } @{$$build_structure{"$prefix${name}_LFLAGS"}});
 
     $defines =~ s/-D//g;
     $defines =~ s/\"/&quot;/g;
@@ -245,7 +244,6 @@ sub createProject {
     </Lib>
     <Link>
       <AdditionalDependencies>$libs;\$(AdditionalDependencies)</AdditionalDependencies>
-      <AdditionalLibraryDirectories>$ldirs</AdditionalLibraryDirectories>
       <AdditionalOptions>invalidcontinue.obj %(AdditionalOptions)</AdditionalOptions>
       <ManifestFile>$cdup\\compat\\win32\\git.manifest</ManifestFile>
       <SubSystem>Console</SubSystem>
