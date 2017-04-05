c19ae47a7940428f8f3f1c49ecdb8906f03c43fa
Brandon Williams <bmwill@google.com>
2017-04-05 10:47:17 -0700

    remote: expose parse_push_refspec function
    A future patch needs access to the 'parse_push_refspec()' function so
    let's export the function so other modules can use it.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -630,7 +630,7 @@ struct refspec *parse_fetch_refspec(int nr_refspec, const char **refspec)
 	return parse_refspec_internal(nr_refspec, refspec, 1, 0);
 }
 
-static struct refspec *parse_push_refspec(int nr_refspec, const char **refspec)
+struct refspec *parse_push_refspec(int nr_refspec, const char **refspec)
 {
 	return parse_refspec_internal(nr_refspec, refspec, 0, 0);
 }
