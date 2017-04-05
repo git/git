fc02713df45095df967025d8c6830f9f0fa7d614
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-06 00:56:44 +0200

    abspath_part_inside_repo: respect core.fileMode
    If the file system is case-insensitive, we really must be careful to
    ignore differences in case only.
    
    This fixes https://github.com/git-for-windows/git/issues/735
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- setup.c
+++ setup.c
@@ -36,7 +36,7 @@ static int abspath_part_inside_repo(char *path)
 	off = offset_1st_component(path);
 
 	/* check if work tree is already the prefix */
-	if (wtlen <= len && !strncmp(path, work_tree, wtlen)) {
+	if (wtlen <= len && !fspathncmp(path, work_tree, wtlen)) {
 		if (path[wtlen] == '/') {
 			memmove(path, path + wtlen + 1, len - wtlen);
 			return 0;
@@ -56,7 +56,7 @@ static int abspath_part_inside_repo(char *path)
 		path++;
 		if (*path == '/') {
 			*path = '\0';
-			if (strcmp(real_path(path0), work_tree) == 0) {
+			if (fspathcmp(real_path(path0), work_tree) == 0) {
 				memmove(path0, path + 1, len - (path - path0));
 				return 0;
 			}
@@ -65,7 +65,7 @@ static int abspath_part_inside_repo(char *path)
 	}
 
 	/* check whole path */
-	if (strcmp(real_path(path0), work_tree) == 0) {
+	if (fspathcmp(real_path(path0), work_tree) == 0) {
 		*path0 = '\0';
 		return 0;
 	}
