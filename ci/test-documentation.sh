159e6010c2d586acea9704993971365a804ea1ce
Lars Schneider <larsxschneider@gmail.com>
2017-04-11 10:33:07 +0200

    travis-ci: build documentation with AsciiDoc and Asciidoctor
    ec3366e introduced a knob to enable the use of Asciidoctor in addition
    to AsciiDoc. Build the documentation on TravisCI with this knob to
    reduce the likeliness of breaking Asciidoctor support in the future.
    
    Signed-off-by: Lars Schneider <larsxschneider@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/test-documentation.sh
+++ ci/test-documentation.sh
@@ -7,8 +7,16 @@ set -e
 
 make check-builtins
 make check-docs
-make doc
 
+# Build docs with AsciiDoc
+make doc
 test -s Documentation/git.html
 test -s Documentation/git.xml
 test -s Documentation/git.1
+grep '<meta name="generator" content="AsciiDoc ' Documentation/git.html
+
+# Build docs with AsciiDoctor
+make clean
+make USE_ASCIIDOCTOR=1 doc
+test -s Documentation/git.html
+grep '<meta name="generator" content="Asciidoctor ' Documentation/git.html
