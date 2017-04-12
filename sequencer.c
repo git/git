2f21baa6ed5d2ad41ff380019a125b7d14842af6
Stephen Hicks <sdh@google.com>
2017-04-11 19:22:15 -0700

    Reread the git-rebase-todo file after every successful "exec" command, since it may have been changed.
    Signed-off-by: Stephen Hicks <sdh@google.com>

--- sequencer.c
+++ sequencer.c
@@ -1200,6 +1200,7 @@ struct todo_list {
 	struct todo_item *items;
 	int nr, alloc, current;
 	int done_nr, total_nr;
+        struct stat_data stat;
 };
 
 #define TODO_LIST_INIT { STRBUF_INIT }
@@ -1330,6 +1331,7 @@ static int count_commands(struct todo_list *todo_list)
 static int read_populate_todo(struct todo_list *todo_list,
 			struct replay_opts *opts)
 {
+        struct stat st;
 	const char *todo_file = get_todo_path(opts);
 	int fd, res;
 
@@ -1342,6 +1344,11 @@ static int read_populate_todo(struct todo_list *todo_list,
 		return error(_("could not read '%s'."), todo_file);
 	}
 	close(fd);
+
+        res = stat(todo_file, &st);
+        if (res)
+                return error(_("could not stat '%s'"), todo_file);
+        fill_stat_data(&todo_list->stat, &st);
 
 	res = parse_insn_buffer(todo_list->buf.buf, todo_list);
 	if (res) {
@@ -2032,6 +2039,20 @@ static int pick_commits(struct todo_list *todo_list, struct replay_opts *opts)
 			*end_of_arg = '\0';
 			res = do_exec(item->arg);
 			*end_of_arg = saved;
+
+                        /* Reread the todo file if it has changed. */
+                        if (res == 0) {
+                                struct stat st;
+                                if ((res = stat(get_todo_path(opts), &st)))
+                                        return res;
+                                if (match_stat_data(&todo_list->stat, &st)) {
+                                        todo_list_release(todo_list);
+                                        if ((res = read_populate_todo(todo_list, opts)))
+                                              return res;
+                                        todo_list->current = 0;
+                                        continue;
+                                }
+                        }
 		} else if (!is_noop(item->command))
 			return error(_("unknown command %d"), item->command);
 
