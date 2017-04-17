6f33b7db51c8b148a10865469842b455afe31514
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-17 13:04:57 +0200

    add_commit_patch_id(): avoid allocating memory unnecessarily
    It would appear that we allocate (and forget to release) memory if the
    patch ID is not even defined.
    
    Reported by the Coverity tool.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- patch-ids.c
+++ patch-ids.c
@@ -99,11 +99,12 @@ struct patch_id *has_commit_patch_id(struct commit *commit,
 struct patch_id *add_commit_patch_id(struct commit *commit,
 				     struct patch_ids *ids)
 {
-	struct patch_id *key = xcalloc(1, sizeof(*key));
+	struct patch_id *key;
 
 	if (!patch_id_defined(commit))
 		return NULL;
 
+	key = xcalloc(1, sizeof(*key));
 	if (init_patch_id_entry(key, commit, ids)) {
 		free(key);
 		return NULL;
