bd71388d8666a3451af7a83b09f07b2f1466fd85
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-17 12:17:17 +0200

    winansi: avoid use of uninitialized value
    To initialize the foreground color attributes of "plain text", our ANSI
    emulation tries to infer them from the currently attached console while
    running the is_console() function. This function first tries to detect any
    console attached to stdout, then it is called with stderr.
    
    If neither stdout nor stderr has any console attached, it does not
    actually matter what we use for "plain text" attributes, as we never need
    to output any text to any console in that case.
    
    However, after working on stdout and stderr, is_console() is called with
    stdin, and it still tries to initialize the "plain text" attributes if
    they had not been initialized earlier. In this case, we cannot detect any
    attributes, and we used an uninitialized value for them.
    
    Naturally, Coverity complained about this use case because it could not
    reason about the code deeply enough to figure out that we do not even use
    those attributes in that case.
    
    Let's just initialize the value to 0 in that case, both to avoid future
    Coverity reports, and to help catch future regressions in case anybody
    changes the order of the is_console() calls (which would make the text
    black on black).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/winansi.c
+++ compat/winansi.c
@@ -100,6 +100,13 @@ static int is_console(int fd)
 	if (!fd) {
 		if (!GetConsoleMode(hcon, &mode))
 			return 0;
+		/*
+		 * This code path is only reached if there is no console
+		 * attached to stdout/stderr, i.e. we will not need to output
+		 * any text to any console, therefore we might just as well
+		 * use black as foreground color.
+		 */
+		sbi.wAttributes = 0;
 	} else if (!GetConsoleScreenBufferInfo(hcon, &sbi))
 		return 0;
 
