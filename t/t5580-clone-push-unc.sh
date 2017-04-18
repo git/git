995f11ea90562edc318babd4a9b73d913b8abb27
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-18 12:09:08 +0200

    mingw: demonstrate a problem with certain absolute paths
    On Windows, there are several categories of absolute paths. One such
    category starts with a backslash and is implicitly relative to the
    drive associated with the current working directory. Example:
    
    	c:
    	git clone https://github.com/git-for-windows/git \G4W
    
    should clone into C:\G4W.
    
    There is currently a problem with that, in that mingw_mktemp() does not
    expect the _wmktemp() function to prefix the absolute path with the
    drive prefix, and as a consequence, the resulting path does not fit into
    the originally-passed string buffer. The symptom is a "Result too large"
    error.
    
    Reported by Juan Carlos Arevalo Baeza.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5580-clone-push-unc.sh
+++ t/t5580-clone-push-unc.sh
@@ -17,20 +17,29 @@ fi
 UNCPATH="$(winpwd)"
 case "$UNCPATH" in
 [A-Z]:*)
+	WITHOUTDRIVE="${UNCPATH#?:}"
 	# Use administrative share e.g. \\localhost\C$\git-sdk-64\usr\src\git
 	# (we use forward slashes here because MSYS2 and Git accept them, and
 	# they are easier on the eyes)
-	UNCPATH="//localhost/${UNCPATH%%:*}\$/${UNCPATH#?:}"
-	test -d "$UNCPATH" || {
-		skip_all='could not access administrative share; skipping'
-		test_done
-	}
+	UNCPATH="//localhost/${UNCPATH%%:*}\$$WITHOUTDRIVE"
 	;;
 *)
 	skip_all='skipping UNC path tests, cannot determine current path as UNC'
 	test_done
 	;;
 esac
+
+test_expect_failure 'clone into absolute path lacking a drive prefix' '
+	USINGBACKSLASHES="$(echo "$WITHOUTDRIVE"/without-drive-prefix |
+		tr / \\\\)" &&
+	git clone . "$USINGBACKSLASHES" &&
+	test -f without-drive-prefix/.git/HEAD
+'
+
+test -d "$UNCPATH" || {
+	skip_all='could not access administrative share; skipping'
+	test_done
+}
 
 test_expect_success setup '
 	test_commit initial
