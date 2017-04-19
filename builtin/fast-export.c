1d24c82c57b478c1fdb7ff59009264119bd87b26
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-04-19 16:45:03 +0200

    fast-export: avoid leaking memory in handle_tag()
    Reported by, you guessed it, Coverity.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/fast-export.c
+++ builtin/fast-export.c
@@ -765,6 +765,7 @@ static void handle_tag(const char *name, struct tag *tag)
 	       (int)(tagger_end - tagger), tagger,
 	       tagger == tagger_end ? "" : "\n",
 	       (int)message_size, (int)message_size, message ? message : "");
+	free(buf);
 }
 
 static struct commit *get_commit(struct rev_cmdline_entry *e, char *full_name)
