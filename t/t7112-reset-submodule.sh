35b96d1de8e795aeac8ec281e88e59f2778335e7
Stefan Beller <sbeller@google.com>
2017-04-21 10:39:53 -0700

    builtin/reset: add --recurse-submodules switch
    git-reset is yet another working tree manipulator, which should
    be taught about submodules.
    
    When a user uses git-reset and requests to recurse into submodules,
    this will reset the submodules to the object name as recorded in the
    superproject, detaching the HEADs.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7112-reset-submodule.sh
+++ t/t7112-reset-submodule.sh
@@ -5,6 +5,14 @@ test_description='reset can handle submodules'
 . ./test-lib.sh
 . "$TEST_DIRECTORY"/lib-submodule-update.sh
 
+KNOWN_FAILURE_SUBMODULE_RECURSIVE_NESTED=1
+KNOWN_FAILURE_DIRECTORY_SUBMODULE_CONFLICTS=1
+KNOWN_FAILURE_SUBMODULE_OVERWRITE_IGNORED_UNTRACKED=1
+
+test_submodule_switch_recursing "git reset --recurse-submodules --keep"
+
+test_submodule_forced_switch_recursing "git reset --hard --recurse-submodules"
+
 test_submodule_switch "git reset --keep"
 
 test_submodule_switch "git reset --merge"
