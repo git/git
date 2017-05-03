e2d90fd1c33ae57e4a6da5729ae53876107b3463
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2017-05-03 17:16:48 +0700

    config.mak.uname: set FREAD_READS_DIRECTORIES for Linux and FreeBSD
    This variable is added [1] with the assumption that on a sane system,
    fopen(<dir>, "r") should return NULL. Linux and FreeBSD do not meet this
    expectation while at least Windows and AIX do. Let's make sure they
    behave the same way.
    
    I only tested one version on Linux (4.7.0 with glibc 2.22) and
    FreeBSD (11.0) but since GNU/kFreeBSD is fbsd kernel with gnu userspace,
    I'm pretty sure it shares the same problem.
    
    [1] cba22528fa (Add compat/fopen.c which returns NULL on attempt to open
        directory - 2008-02-08)
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1308-config-set.sh
+++ t/t1308-config-set.sh
@@ -183,6 +183,14 @@ test_expect_success 'proper error on non-existent files' '
 	test_cmp expect actual
 '
 
+test_expect_success 'proper error on directory "files"' '
+	echo "Error (-1) reading configuration file a-directory." >expect &&
+	mkdir a-directory &&
+	test_expect_code 2 test-config configset_get_value foo.bar a-directory 2>output &&
+	grep "^Error" output >actual &&
+	test_cmp expect actual
+'
+
 test_expect_success POSIXPERM,SANITY 'proper error on non-accessible files' '
 	chmod -r .git/config &&
 	test_when_finished "chmod +r .git/config" &&
