7d7acef2587b0b5dcdd7965651269dd1837e1872
Junio C Hamano <gitster@pobox.com>
2017-05-08 13:21:06 +0900

    fixup! checkout: fix memory leak

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -247,6 +247,7 @@ static int checkout_merged(int pos, const struct checkout *state)
 	if (write_sha1_file(result_buf.ptr, result_buf.size,
 			    blob_type, oid.hash))
 		die(_("Unable to add merge result for '%s'"), path);
+	free(result_buf.ptr);
 	ce = make_cache_entry(mode, oid.hash, path, 2, 0);
 	if (!ce)
 		die(_("make_cache_entry failed for path '%s'"), path);
