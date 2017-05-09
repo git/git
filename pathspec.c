2c3b40799fcdb9d2dedc90134e4f3841af08bbe7
Brandon Williams <bmwill@google.com>
2017-05-09 12:17:58 -0700

    pathspec: provide a more descriptive die message
    The current message displayed upon an internal error in
    'init_pathspec_item()' isn't very descriptive and doesn't provide much
    context to where the error occurred.  Update the error message to
    provide more context to where the error occured.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pathspec.c
+++ pathspec.c
@@ -555,7 +555,7 @@ static void init_pathspec_item(struct pathspec_item *item, unsigned flags,
 		 * would trigger that.
 		 */
 		die_inside_submodule_path(item);
-		die ("BUG: item->nowildcard_len > item->len || item->prefix > item->len)");
+		die ("BUG: error initializing pathspec_item");
 	}
 }
 
