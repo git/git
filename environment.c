588a538ae554f61a37d43c972da75d0f7c3ed484
Jeff King <peff@peff.net>
2017-05-12 23:29:18 -0400

    setup_git_env: convert die("BUG") to BUG()
    Converting to BUG() makes it easier to detect and debug
    cases where we hit this assertion. Coupled with a new test
    in t1300, this shows that the test suite can detect such
    corner cases.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- environment.c
+++ environment.c
@@ -169,7 +169,7 @@ static void setup_git_env(void)
 	git_dir = getenv(GIT_DIR_ENVIRONMENT);
 	if (!git_dir) {
 		if (!startup_info->have_repository)
-			die("BUG: setup_git_env called without repository");
+			BUG("setup_git_env called without repository");
 		git_dir = DEFAULT_GIT_DIR_ENVIRONMENT;
 	}
 	gitfile = read_gitfile(git_dir);
