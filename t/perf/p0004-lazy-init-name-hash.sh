1c002d0a9eb52f8e9cbecb8dce4edd86062df3b5
René Scharfe <l.s.r@web.de>
2017-05-13 17:59:46 +0200

    p0004: simplify calls of test-lazy-init-name-hash
    The test library puts helpers into $PATH, so we can simply call them
    without specifying their location.
    
    The suffix $X is also not necessary because .exe files on Windows can be
    started without specifying their extension, and on other platforms it's
    empty anyway.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Acked-by: Jeff Hostetler <git@jeffhostetler.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/perf/p0004-lazy-init-name-hash.sh
+++ t/perf/p0004-lazy-init-name-hash.sh
@@ -7,13 +7,13 @@ test_perf_large_repo
 test_checkout_worktree
 
 test_expect_success 'verify both methods build the same hashmaps' '
-	$GIT_BUILD_DIR/t/helper/test-lazy-init-name-hash$X --dump --single | sort >out.single &&
-	$GIT_BUILD_DIR/t/helper/test-lazy-init-name-hash$X --dump --multi  | sort >out.multi  &&
+	test-lazy-init-name-hash --dump --single | sort >out.single &&
+	test-lazy-init-name-hash --dump --multi  | sort >out.multi  &&
 	test_cmp out.single out.multi
 '
 
 test_expect_success 'multithreaded should be faster' '
-	$GIT_BUILD_DIR/t/helper/test-lazy-init-name-hash$X --perf >out.perf
+	test-lazy-init-name-hash --perf >out.perf
 '
 
 test_done
