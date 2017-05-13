e1ebb569c61d17e2f721f32084fcc7b7394b1692
René Scharfe <l.s.r@web.de>
2017-05-13 18:00:15 +0200

    p0004: avoid using pipes
    The return code of commands on the producing end of a pipe is ignored.
    Evaluate the outcome of test-lazy-init-name-hash by calling sort
    separately.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Acked-by: Jeff Hostetler <git@jeffhostetler.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/perf/p0004-lazy-init-name-hash.sh
+++ t/perf/p0004-lazy-init-name-hash.sh
@@ -7,9 +7,11 @@ test_perf_large_repo
 test_checkout_worktree
 
 test_expect_success 'verify both methods build the same hashmaps' '
-	test-lazy-init-name-hash --dump --single | sort >out.single &&
-	test-lazy-init-name-hash --dump --multi  | sort >out.multi  &&
-	test_cmp out.single out.multi
+	test-lazy-init-name-hash --dump --single >out.single &&
+	test-lazy-init-name-hash --dump --multi >out.multi &&
+	sort <out.single >sorted.single &&
+	sort <out.multi >sorted.multi &&
+	test_cmp sorted.single sorted.multi
 '
 
 test_expect_success 'multithreaded should be faster' '
