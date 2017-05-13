48a6ace8f5a504cea47d4058744982f2bfb81f2f
René Scharfe <l.s.r@web.de>
2017-05-13 18:02:32 +0200

    p0004: use test_perf
    The perf test suite (more specifically: t/perf/aggregate.perl) requires
    each test script to write test results into a file, otherwise it aborts
    when aggregating.  Add actual performance tests with test_perf to allow
    p0004 to be run together with other perf scripts.
    
    Calibrate the value for the parameter --count based on the size of the
    test repository, in order to get meaningful results with smaller repos
    yet still be able to finish the script against huge ones without having
    to wait for hours.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Acked-by: Jeff Hostetler <git@jeffhostetler.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/perf/p0004-lazy-init-name-hash.sh
+++ t/perf/p0004-lazy-init-name-hash.sh
@@ -18,4 +18,40 @@ test_expect_success 'multithreaded should be faster' '
 	test-lazy-init-name-hash --perf >out.perf
 '
 
+test_expect_success 'calibrate' '
+	entries=$(wc -l <out.single) &&
+
+	case $entries in
+	?) count=1000000 ;;
+	??) count=100000 ;;
+	???) count=10000 ;;
+	????) count=1000 ;;
+	?????) count=100 ;;
+	??????) count=10 ;;
+	*) count=1 ;;
+	esac &&
+	export count &&
+
+	case $entries in
+	1) entries_desc="1 entry" ;;
+	*) entries_desc="$entries entries" ;;
+	esac &&
+
+	case $count in
+	1) count_desc="1 round" ;;
+	*) count_desc="$count rounds" ;;
+	esac &&
+
+	desc="$entries_desc, $count_desc" &&
+	export desc
+'
+
+test_perf "single-threaded, $desc" "
+	test-lazy-init-name-hash --single --count=$count
+"
+
+test_perf "multi-threaded, $desc" "
+	test-lazy-init-name-hash --multi --count=$count
+"
+
 test_done
