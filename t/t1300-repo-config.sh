25cd291963e4b0fae0eabe7fe02be693702d79bb
Jeff King <peff@peff.net>
2017-05-12 23:29:31 -0400

    config: complain about --local outside of a git repo
    The "--local" option instructs git-config to read or modify
    the repository-level config. This doesn't make any sense if
    you're not actually in a repository.
    
    Older versions of Git would blindly try to read or write
    ".git/config". For reading, this would result in a quiet
    failure, since there was no config to read (and thus no
    matching config value). Writing would generally fail
    noisily, since ".git" was unlikely to exist. But since
    b1ef400ee (setup_git_env: avoid blind fall-back to ".git",
    2016-10-20), we catch this in the call to git_pathdup() and
    die with an assertion.
    
    Dying is the right thing to do, but we should catch the
    problem early and give a more human-friendly error message.
    
    Note that even without --local, git-config will sometimes
    default to using local repository config (e.g., when
    writing). These cases are already protected by similar
    checks, and covered by a test in t1308.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1300-repo-config.sh
+++ t/t1300-repo-config.sh
@@ -1539,7 +1539,7 @@ test_expect_success !MINGW '--show-origin blob ref' '
 	test_cmp expect output
 '
 
-test_expect_failure '--local requires a repo' '
+test_expect_success '--local requires a repo' '
 	# we expect 128 to ensure that we do not simply
 	# fail to find anything and return code "1"
 	test_expect_code 128 nongit git config --local foo.bar
