3851e4483f0d62e677703297218c9bb47325806f
Thomas Gummerer <t.gummerer@gmail.com>
2017-05-16 20:59:45 +0100

    completion: add git stash push
    When introducing git stash push in f5727e26e4 ("stash: introduce push
    verb", 2017-02-19), I forgot to add it to the completion code. Add it
    now.
    
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2507,7 +2507,7 @@ _git_show_branch ()
 _git_stash ()
 {
 	local save_opts='--all --keep-index --no-keep-index --quiet --patch --include-untracked'
-	local subcommands='save list show apply clear drop pop create branch'
+	local subcommands='push save list show apply clear drop pop create branch'
 	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		case "$cur" in
@@ -2522,6 +2522,9 @@ _git_stash ()
 		esac
 	else
 		case "$subcommand,$cur" in
+		push,--*)
+			__gitcomp "$save_opts --message"
+			;;
 		save,--*)
 			__gitcomp "$save_opts"
 			;;
