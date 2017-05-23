d10411665a219a0c08d60a80f2040f7d9658bed1
Junio C Hamano <gitster@pobox.com>
2017-05-23 21:41:15 +0900

    Revert "Merge branch 'js/bs-is-a-dir-sep-on-windows' into next"
    This reverts commit e27040ceb048bb26693ba9b2d16037586deb12b3, reversing
    changes made to 977491e52a268fe77ad8ba98d10874117d62b282, in order
    to pick up a rerolled version of the two patches.

--- remote.c
+++ remote.c
@@ -649,10 +649,7 @@ static int valid_remote_nick(const char *name)
 {
 	if (!name[0] || is_dot_or_dotdot(name))
 		return 0;
-	while (*name)
-		if (is_dir_sep(*name++))	/* no slash */
-			return 0;
-	return 1;
+	return !strchr(name, '/'); /* no slash */
 }
 
 const char *remote_for_branch(struct branch *branch, int *explicit)
