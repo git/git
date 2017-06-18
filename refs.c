03df567fbf6afeca32f6a27d04656c1a3a162453
Michael Haggerty <mhagger@alum.mit.edu>
2017-06-18 15:39:41 +0200

    for_each_bisect_ref(): don't trim refnames
    `for_each_bisect_ref()` is called by `for_each_bad_bisect_ref()` with
    a term "bad". This used to make it call `for_each_ref_in_submodule()`
    with a prefix "refs/bisect/bad". But the latter is the name of the
    reference that is being sought, so the empty string was being passed
    to the callback as the trimmed refname. Moreover, this questionable
    practice was turned into an error by
    
        b9c8e7f2fb prefix_ref_iterator: don't trim too much, 2017-05-22
    
    It makes more sense (and agrees better with the documentation of
    `--bisect`) for the callers to receive the full reference names. So
    
    * Add a new function, `for_each_fullref_in_submodule()`, to the refs
      API. This plugs a gap in the existing functionality, analogous to
      `for_each_fullref_in()` but accepting a `submodule` argument.
    
    * Change `for_each_bad_bisect_ref()` to call the new function rather
      than `for_each_ref_in_submodule()`.
    
    * Add a test.
    
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -1341,6 +1341,18 @@ int for_each_ref_in_submodule(const char *submodule, const char *prefix,
 				    prefix, fn, cb_data);
 }
 
+int for_each_fullref_in_submodule(const char *submodule, const char *prefix,
+				  each_ref_fn fn, void *cb_data,
+				  unsigned int broken)
+{
+	unsigned int flag = 0;
+
+	if (broken)
+		flag = DO_FOR_EACH_INCLUDE_BROKEN;
+	return do_for_each_ref(get_submodule_ref_store(submodule),
+			       prefix, fn, 0, flag, cb_data);
+}
+
 int for_each_replace_ref(each_ref_fn fn, void *cb_data)
 {
 	return do_for_each_ref(get_main_ref_store(),
