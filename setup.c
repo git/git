7aee274fb4f3eb8d47ec9edce7042f809ce0e579
Brandon Williams <bmwill@google.com>
2017-06-20 12:19:33 -0700

    setup: add comment indicating a hack
    'GIT_TOPLEVEL_PREFIX_ENVIRONMENT' was added in (b58a68c1c setup: allow
    for prefix to be passed to git commands) to aid in fixing a bug where
    'ls-files' and 'grep' were not able to properly recurse when called from
    within a subdirectory.  Add a 'NEEDSWORK' comment indicating that this
    envvar should be removed once 'ls-files' and 'grep' can recurse
    in-process.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -1079,6 +1079,12 @@ const char *setup_git_directory_gently(int *nongit_ok)
 		die("BUG: unhandled setup_git_directory_1() result");
 	}
 
+	/*
+	 * NEEDSWORK: This was a hack in order to get ls-files and grep to have
+	 * properly formated output when recursing submodules.  Once ls-files
+	 * and grep have been changed to perform this recursing in-process this
+	 * needs to be removed.
+	 */
 	env_prefix = getenv(GIT_TOPLEVEL_PREFIX_ENVIRONMENT);
 	if (env_prefix)
 		prefix = env_prefix;
