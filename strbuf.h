4904cbc9e195f1de2cb6ff3c5b3d5b756343ef44
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-06-24 12:14:51 +0000

    strbuf.h comment: discuss strbuf_addftime() arguments in order
    Change the comment documenting the strbuf_addftime() function to
    discuss the parameters in the order in which they appear, which makes
    this easier to read than discussing them out of order.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- strbuf.h
+++ strbuf.h
@@ -340,10 +340,10 @@ extern void strbuf_vaddf(struct strbuf *sb, const char *fmt, va_list ap);
 
 /**
  * Add the time specified by `tm`, as formatted by `strftime`.
- * `tz_name` is used to expand %Z internally unless it's NULL.
  * `tz_offset` is in decimal hhmm format, e.g. -600 means six hours west
  * of Greenwich, and it's used to expand %z internally.  However, tokens
  * with modifiers (e.g. %Ez) are passed to `strftime`.
+ * `tz_name` is used to expand %Z internally unless it's NULL.
  */
 extern void strbuf_addftime(struct strbuf *sb, const char *fmt,
 			    const struct tm *tm, int tz_offset,
