b3cf1b77894861920671def129a981b54ec9618e
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-06-30 17:42:21 +0530

    commit-template: distinguish status information unconditionally
    The commit template adds the status information without
    adding a new line to distinguish them in the absence
    of optional parts. This results in difficulty in interpreting
    it's content, specifically for inexperienced users.
    
    Unconditionally, add new lines to separate the status message
    from the other parts of the commit-template to make it more
    readable.
    
    Signed-off-by: Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -873,8 +873,7 @@ static int prepare_to_commit(const char *index_file, const char *prefix,
 				(int)(ci.name_end - ci.name_begin), ci.name_begin,
 				(int)(ci.mail_end - ci.mail_begin), ci.mail_begin);
 
-		if (ident_shown)
-			status_printf_ln(s, GIT_COLOR_NORMAL, "%s", "");
+		status_printf_ln(s, GIT_COLOR_NORMAL, "%s", ""); /* Add new line for clarity */
 
 		saved_color_setting = s->use_color;
 		s->use_color = 0;
