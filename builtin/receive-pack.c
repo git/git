f730944a49b2a210bb10520700c0a3f6c49bc020
René Scharfe <l.s.r@web.de>
2017-07-15 20:33:54 +0200

    receive-pack: don't access hash of NULL object_id pointer
    We set old_oid to NULL if we found out that it's a corrupt reference.
    In that case don't try to access the hash member and pass NULL to
    ref_transaction_delete() instead.
    
    Found with Clang's UBSan.
    
    Signed-off-by: Rene Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/receive-pack.c
+++ builtin/receive-pack.c
@@ -1137,7 +1137,7 @@ static const char *update(struct command *cmd, struct shallow_info *si)
 		}
 		if (ref_transaction_delete(transaction,
 					   namespaced_name,
-					   old_oid->hash,
+					   old_oid ? old_oid->hash : NULL,
 					   0, "push", &err)) {
 			rp_error("%s", err.buf);
 			strbuf_release(&err);
