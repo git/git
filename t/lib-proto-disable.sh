c757368ad461dca8d13a179373c430722022f402
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-07-18 01:15:40 +0200

    tests: use the correct path separator with BusyBox
    BusyBox-w32 is a true Win32 application, i.e. it does not come with a
    POSIX emulation layer.
    
    That also means that it does *not* use the Unix convention of separating
    the entries in the PATH variable using colons, but semicolons.
    
    However, there are also BusyBox ports to Windows which use a POSIX
    emulation layer such as Cygwin's or MSYS2's runtime, i.e. using colons
    as PATH separators.
    
    As a tell-tale, let's use the presence of semicolons in the PATH
    variable: on Unix, it is highly unlikely that it contains semicolons,
    and on Windows (without POSIX emulation), it is virtually guaranteed, as
    everybody should have both $SYSTEMROOT and $SYSTEMROOT/system32 in their
    PATH.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/lib-proto-disable.sh
+++ t/lib-proto-disable.sh
@@ -210,7 +210,7 @@ setup_ext_wrapper () {
 		cd "$TRASH_DIRECTORY/remote" &&
 		eval "$*"
 		EOF
-		PATH=$TRASH_DIRECTORY:$PATH &&
+		PATH=$TRASH_DIRECTORY$PATH_SEP$PATH &&
 		export TRASH_DIRECTORY
 	'
 }
