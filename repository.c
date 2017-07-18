3f13877595500bd54ee370167bc868f3c1eee5b7
Brandon Williams <bmwill@google.com>
2017-07-18 12:05:18 -0700

    repo_read_index: don't discard the index
    Have 'repo_read_index()' behave more like the other read_index family of
    functions and don't discard the index if it has already been populated
    and instead rely on the quick return of read_index_from which has:
    
      /* istate->initialized covers both .git/index and .git/sharedindex.xxx */
      if (istate->initialized)
        return istate->cache_nr;
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- repository.c
+++ repository.c
@@ -235,8 +235,6 @@ int repo_read_index(struct repository *repo)
 {
 	if (!repo->index)
 		repo->index = xcalloc(1, sizeof(*repo->index));
-	else
-		discard_index(repo->index);
 
 	return read_index_from(repo->index, repo->index_file);
 }
