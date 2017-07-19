c04b6e9d8ac4780a49e28623d7aa7e952c223116
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-07-19 17:07:56 +0200

    test-lib: add BUSYBOX prerequisite
    When running with BusyBox, we will want to avoid calling executables on
    the PATH that are implemented in BusyBox itself.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -1624,6 +1624,10 @@ test_lazy_prereq UNZIP '
 	test $? -ne 127
 '
 
+test_lazy_prereq BUSYBOX '
+	case "$($SHELL --help 2>&1)" in *BusyBox*) true;; *) false;; esac
+'
+
 run_with_limited_cmdline () {
 	(ulimit -s 128 && "$@")
 }
