ef9c4dc3b6a4feeabd28e9576260e077c607159e
Kevin Willford <kewillf@microsoft.com>
2017-08-28 14:28:28 -0600

    merge-recursive: remove return value from get_files_dirs
    The return value of the get_files_dirs call is never being used.
    Looking at the history of the file and it was originally only
    being used for debug output statements.  Also when
    read_tree_recursive return value is non zero it is changed to
    zero.  This leads me to believe that it doesn't matter if
    read_tree_recursive gets an error.
    
    Since the debug output has been removed and the caller isn't
    checking the return value there is no reason to keep calculating
    and returning a value.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -328,15 +328,11 @@ static int save_files_dirs(const unsigned char *sha1,
 	return (S_ISDIR(mode) ? READ_TREE_RECURSIVE : 0);
 }
 
-static int get_files_dirs(struct merge_options *o, struct tree *tree)
+static void get_files_dirs(struct merge_options *o, struct tree *tree)
 {
-	int n;
 	struct pathspec match_all;
 	memset(&match_all, 0, sizeof(match_all));
-	if (read_tree_recursive(tree, "", 0, 0, &match_all, save_files_dirs, o))
-		return 0;
-	n = o->current_file_set.nr + o->current_directory_set.nr;
-	return n;
+	read_tree_recursive(tree, "", 0, 0, &match_all, save_files_dirs, o);
 }
 
 /*
