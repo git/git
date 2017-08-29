4993659ca44a89d094c200e665cd43e3e8a729ab
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-08-30 01:28:22 +0200

    mingw: ensure that core.longPaths is handled *always*
    A ton of Git commands simply do not read (or at least parse) the core.*
    settings. This is not good, as Git for Windows relies on the
    core.longPaths setting to be read quite early on.
    
    So let's just make sure that all commands read the config and give
    platform_core_config() a chance.
    
    This patch teaches tons of Git commands to respect the config setting
    `core.longPaths = true`, including `pack-refs`, thereby fixing
    https://github.com/git-for-windows/git/issues/1218
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/log.c
+++ builtin/log.c
@@ -2314,6 +2314,7 @@ int cmd_cherry(int argc, const char **argv, const char *prefix)
 		OPT_END()
 	};
 
+	git_config(git_default_config, NULL);
 	argc = parse_options(argc, argv, prefix, options, cherry_usage, 0);
 
 	switch (argc) {
