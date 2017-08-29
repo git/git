6a6f01c0bddaf6966d2acf1c430d7dc69472bd03
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-08-30 01:28:22 +0200

    mingw: ensure that core.longPaths is handled *always*
    A ton of Git commands simply do not read (or at least parse) the core.*
    settings. This is not good, as Git for Windows relies on the
    core.longPaths setting to be read quite early on.
    
    So let's just make sure that all commands read the config and give
    platform_core_config() a chance.
    
    This patch teaches tons of Git commands to respect the config setting
    `core.longPaths = true`, including `pack-refs`, thereby fixing
    https://github.com/git-for-windows/git/issues/1218
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/clone.c
+++ builtin/clone.c
@@ -957,6 +957,8 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 
 	struct argv_array ref_prefixes = ARGV_ARRAY_INIT;
 
+	git_config(platform_core_config, NULL);
+
 	packet_trace_identity("clone");
 	argc = parse_options(argc, argv, prefix, builtin_clone_options,
 			     builtin_clone_usage, 0);
