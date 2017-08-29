a28d9045b3089ddb798dcd7eb0cb9eb93f8ea4e9
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-08-30 01:28:22 +0200

    mingw: ensure that core.longPaths is handled *always*
    A ton of Git commands simply do not read (or at least parse) the core.*
    settings. This is not good, as Git for Windows relies on the
    core.longPaths setting to be read quite early on.
    
    So let's just make sure that all commands read the config and give
    platform_core_config() a chance.
    
    This patch teaches tons of Git commands to respect the config setting
    `core.longPaths = true`, including `pack-refs`, thereby fixing
    https://github.com/git-for-windows/git/issues/1218
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/merge-tree.c
+++ builtin/merge-tree.c
@@ -12,6 +12,7 @@
 #include "exec-cmd.h"
 #include "merge-blobs.h"
 #include "quote.h"
+#include "config.h"
 
 static int line_termination = '\n';
 
@@ -507,6 +508,8 @@ int cmd_merge_tree(int argc, const char **argv, const char *prefix)
 			   PARSE_OPT_NONEG),
 		OPT_END()
 	};
+
+	git_config(git_default_config, NULL);
 
 	/* Parse arguments */
 	original_argc = argc - 1; /* ignoring argv[0] */
