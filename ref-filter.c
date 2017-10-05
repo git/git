a230e60785b4043d73b9b2e67801a75419ee9cac
J Wyman <jwyman@microsoft.com>
2017-10-05 13:00:41 +0200

    for-each-ref: let upstream/push optionally remote ref name
    There are times when scripts want to know not only the name of the
    push branch on the remote, but also the name of the branch as known
    by the remote repository.
    
    An example of this is when a tool wants to push to the very same branch
    from which it would pull automatically, i.e. the `<remote>` and the `<to>`
    in `git push <remote> <from>:<to>` would be provided by
    `%(upstream:remotename)` and `%(upstream:remoteref)`, respectively.
    
    This patch offers the new suffix :remoteref for the `upstream` and `push`
    atoms, allowing to show exactly that. Example:
    
    	$ cat .git/config
    	...
    	[remote "origin"]
    		url = https://where.do.we.come/from
    		fetch = refs/heads/*:refs/remote/origin/*
    	[branch "master"]
    		remote = origin
    		merge = refs/heads/master
    	[branch "develop/with/topics"]
    		remote = origin
    		merge = refs/heads/develop/with/topics
    	...
    
    	$ git for-each-ref \
    		--format='%(push) %(push:remoteref)' \
    		refs/heads
    	refs/remotes/origin/master refs/heads/master
    	refs/remotes/origin/develop/with/topics refs/heads/develop/with/topics
    
    Signed-off-by: J Wyman <jwyman@microsoft.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ref-filter.c
+++ ref-filter.c
@@ -77,7 +77,7 @@ static struct used_atom {
 		struct align align;
 		struct {
 			enum {
-				RR_REF, RR_TRACK, RR_TRACKSHORT, RR_REMOTE_NAME
+				RR_REF, RR_TRACK, RR_TRACKSHORT, RR_REMOTE_NAME, RR_REMOTE_REF
 			} option;
 			struct refname_atom refname;
 			unsigned int nobracket : 1, push : 1, push_remote : 1;
@@ -163,6 +163,9 @@ static void remote_ref_atom_parser(const struct ref_format *format, struct used_
 			atom->u.remote_ref.nobracket = 1;
 		else if (!strcmp(s, "remotename")) {
 			atom->u.remote_ref.option = RR_REMOTE_NAME;
+			atom->u.remote_ref.push_remote = 1;
+		} else if (!strcmp(s, "remoteref")) {
+			atom->u.remote_ref.option = RR_REMOTE_REF;
 			atom->u.remote_ref.push_remote = 1;
 		} else {
 			atom->u.remote_ref.option = RR_REF;
@@ -1262,6 +1265,14 @@ static void fill_remote_ref_details(struct used_atom *atom, const char *refname,
 			remote_for_branch(branch, &explicit);
 		if (explicit)
 			*s = xstrdup(remote);
+		else
+			*s = "";
+	} else if (atom->u.remote_ref.option == RR_REMOTE_REF) {
+		int explicit, for_push = starts_with(atom->name, "push");
+		const char *merge = remote_ref_for_branch(branch, for_push,
+							  &explicit);
+		if (explicit)
+			*s = xstrdup(merge);
 		else
 			*s = "";
 	} else
