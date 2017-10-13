8280c4c1ea59bc6d101c5616490627b63934318e
Junio C Hamano <gitster@pobox.com>
2017-10-13 12:57:02 +0900

    branch: streamline "attr_only" handling in validate_new_branchname()
    The function takes a parameter "attr_only" (which is a name that is
    hard to reason about, which will be corrected soon) and skips some
    checks when it is set.  Reorganize the conditionals to make it more
    obvious that some checks are never made when this parameter is set.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- branch.c
+++ branch.c
@@ -181,21 +181,25 @@ int read_branch_desc(struct strbuf *buf, const char *branch_name)
 int validate_new_branchname(const char *name, struct strbuf *ref,
 			    int force, int attr_only)
 {
+	const char *head;
+
 	if (strbuf_check_branch_ref(ref, name))
 		die(_("'%s' is not a valid branch name."), name);
 
 	if (!ref_exists(ref->buf))
 		return 0;
-	else if (!force && !attr_only)
-		die(_("A branch named '%s' already exists."), ref->buf + strlen("refs/heads/"));
 
-	if (!attr_only) {
-		const char *head;
+	if (attr_only)
+		return 1;
 
-		head = resolve_ref_unsafe("HEAD", 0, NULL, NULL);
-		if (!is_bare_repository() && head && !strcmp(head, ref->buf))
-			die(_("Cannot force update the current branch."));
-	}
+	if (!force)
+		die(_("A branch named '%s' already exists."),
+		    ref->buf + strlen("refs/heads/"));
+
+	head = resolve_ref_unsafe("HEAD", 0, NULL, NULL);
+	if (!is_bare_repository() && head && !strcmp(head, ref->buf))
+		die(_("Cannot force update the current branch."));
+
 	return 1;
 }
 
