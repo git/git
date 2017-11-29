91564af617ced11c11c52e64f79c87f0581aa4ad
Elijah Newren <newren@gmail.com>
2017-11-28 17:42:37 -0800

    merge-recursive: ensure we write updates for directory-renamed file
    When a file is present in HEAD before the merge and the other side of the
    merge does not modify that file, we try to avoid re-writing the file and
    making it stat-dirty.  However, when a file is present in HEAD before the
    merge and was in a directory that was renamed by the other side of the
    merge, we have to move the file to a new location and re-write it.
    Update the code that checks whether we can skip the update to also work in
    the presence of directory renames.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6043-merge-rename-directories.sh
+++ t/t6043-merge-rename-directories.sh
@@ -3713,7 +3713,7 @@ test_expect_success '12b-setup: Moving one directory hierarchy into another' '
 	)
 '
 
-test_expect_failure '12b-check: Moving one directory hierarchy into another' '
+test_expect_success '12b-check: Moving one directory hierarchy into another' '
 	(
 		cd 12b &&
 
