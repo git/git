6ffcf2c26a3d9bb9be4da8977dcc4cc8abf4417c
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-12-10 23:02:59 +0100

    sequencer: allow introducing new root commits
    In the context of the new --rebase-merges mode, which was designed
    specifically to allow for changing the existing branch topology
    liberally, a user may want to extract commits into a completely fresh
    branch that starts with a newly-created root commit.
    
    This is now possible by inserting the command `reset [new root]` before
    `pick`ing the commit that wants to become a root commit. Example:
    
    	reset [new root]
    	pick 012345 a commit that is about to become a root commit
    	pick 234567 this commit will have the previous one as parent
    
    This does not conflict with other uses of the `reset` command because
    `[new root]` is not (part of) a valid ref name: both the opening bracket
    as well as the space are illegal in ref names.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t3430-rebase-merges.sh
+++ t/t3430-rebase-merges.sh
@@ -241,4 +241,38 @@ test_expect_success 'refuse to merge ancestors of HEAD' '
 	test_cmp_rev HEAD $before
 '
 
+test_expect_success 'root commits' '
+	git checkout --orphan unrelated &&
+	(GIT_AUTHOR_NAME="Parsnip" GIT_AUTHOR_EMAIL="root@example.com" \
+	 test_commit second-root) &&
+	test_commit third-root &&
+	cat >script-from-scratch <<-\EOF &&
+	pick third-root
+	label first-branch
+	reset [new root]
+	pick second-root
+	merge first-branch # Merge the 3rd root
+	EOF
+	test_config sequence.editor \""$PWD"/replace-editor.sh\" &&
+	test_tick &&
+	git rebase -i --force --root -r &&
+	test "Parsnip" = "$(git show -s --format=%an HEAD^)" &&
+	test $(git rev-parse second-root^0) != $(git rev-parse HEAD^) &&
+	test $(git rev-parse second-root:second-root.t) = \
+		$(git rev-parse HEAD^:second-root.t) &&
+	test_cmp_graph HEAD <<-\EOF &&
+	*   Merge the 3rd root
+	|\
+	| * third-root
+	* second-root
+	EOF
+
+	: fast forward if possible &&
+	before="$(git rev-parse --verify HEAD)" &&
+	test_might_fail git config --unset sequence.editor &&
+	test_tick &&
+	git rebase -i --root -r &&
+	test_cmp_rev HEAD $before
+'
+
 test_done
