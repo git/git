ae59a4e44f359f74d0fede109c29bb53f726209b
Thomas Gummerer <t.gummerer@gmail.com>
2018-01-07 22:30:15 +0000

    travis: run tests with GIT_TEST_SPLIT_INDEX
    Split index mode only has a few dedicated tests, but as the index is
    involved in nearly every git operation, this doesn't quite cover all the
    ways repositories with split index can break.  To use split index mode
    throughout the test suite a GIT_TEST_SPLIT_INDEX environment variable
    can be set, which makes git split the index at random and thus
    excercises the functionality much more thoroughly.
    
    As this is not turned on by default, it is not executed nearly as often
    as the test suite is run, so occationally breakages slip through.  Try
    to counteract that by running the test suite with GIT_TEST_SPLIT_INDEX
    mode turned on on travis.
    
    To avoid using too many cycles on travis only run split index mode in
    the linux-gcc target only.  The Linux build was chosen over the Mac OS
    builds because it tends to be much faster to complete.
    
    The linux gcc build was chosen over the linux clang build because the
    linux clang build is the fastest build, so it can serve as an early
    indicator if something is broken and we want to avoid spending the extra
    cycles of running the test suite twice for that.
    
    Helped-by: Lars Schneider <larsxschneider@gmail.com>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -8,3 +8,7 @@
 mkdir -p $HOME/travis-cache
 ln -s $HOME/travis-cache/.prove t/.prove
 make --quiet test
+if test "$jobname" = "linux-gcc"
+then
+	GIT_TEST_SPLIT_INDEX=YesPlease make --quiet test
+fi
