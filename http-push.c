a2b9820ceccf2065a3d3d347e9b1cf7834f5e146
Patryk Obara <patryk.obara@gmail.com>
2018-01-24 12:11:53 +0100

    http-push: improve error log
    When git push fails due to server-side WebDAV error, it's not easy to
    point to the main culprit.  Additional information about exact cURL
    error and HTTP server response is helpful for debugging purpose.
    
    New error log helped me pinpoint failing test t5540-http-push-webdav
    to a missing Apache dependency in Fedora 27:
    https://bugzilla.redhat.com/show_bug.cgi?id=1491151
    
    Signed-off-by: Patryk Obara <patryk.obara@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http-push.c
+++ http-push.c
@@ -915,6 +915,10 @@ static struct remote_lock *lock_remote(const char *path, long timeout)
 				lock->timeout = -1;
 			}
 			XML_ParserFree(parser);
+		} else {
+			fprintf(stderr,
+				"error: curl result=%d, HTTP code=%ld\n",
+				results.curl_result, results.http_code);
 		}
 	} else {
 		fprintf(stderr, "Unable to start LOCK request\n");
