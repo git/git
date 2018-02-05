822c8a45d049f86ea5c59c0b434303964e4e6f3d
Stephen Guglielmo <srg@guglielmo.us>
2018-02-04 20:46:13 -0500

    Ensure GPG signatures are not shown
    Fixes 'subtree add' and 'subtree pull' for GPG signed commits

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -475,7 +475,7 @@ squash_msg () {
 
 toptree_for_commit () {
 	commit="$1"
-	git log -1 --pretty=format:'%T' "$commit" -- || exit $?
+	git log --no-show-signature -1 --pretty=format:'%T' "$commit" -- || exit $?
 }
 
 subtree_for_commit () {
