a4ca4553e08ab5626f744171c1d2d7fbfed11e8b
SZEDER Gábor <szeder.dev@gmail.com>
2018-02-08 16:56:49 +0100

    t5812: add 'test_i18ngrep's missing filename parameter
    The second 'test_i18ngrep' invocation in the test 'curl redirects
    respect whitelist' is missing its filename parameter.  This has
    remained unnoticed since its introduction in f4113cac0 (http: limit
    redirection to protocol-whitelist, 2015-09-22), because it would only
    cause the test to fail if Git was built with a sufficiently old
    libcurl version.  The test's two ||-chained 'test_i18ngrep'
    invocations are supposed to check that either one of the two patterns
    is present in 'git clone's error message.  As it happens, the first
    invocation covers the error message from any reasonably up-to-date
    libcurl, thus the second invocation, the one without the filename
    parameter, isn't executed at all.  Apparently no one has run the test
    suite's httpd tests with such an old libcurl in the last 2+ years, or
    at least they haven't bothered to notify us about the failed test.
    
    Fix this by consolidating the two patterns into a single extended
    regexp, eliminating the need for an ||-chained second 'test_i18ngrep'
    invocation.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5812-proto-disable-http.sh
+++ t/t5812-proto-disable-http.sh
@@ -20,10 +20,7 @@ test_expect_success 'curl redirects respect whitelist' '
 	test_must_fail env GIT_ALLOW_PROTOCOL=http:https \
 			   GIT_SMART_HTTP=0 \
 		git clone "$HTTPD_URL/ftp-redir/repo.git" 2>stderr &&
-	{
-		test_i18ngrep "ftp.*disabled" stderr ||
-		test_i18ngrep "your curl version is too old"
-	}
+	test_i18ngrep -E "(ftp.*disabled|your curl version is too old)" stderr
 '
 
 test_expect_success 'curl limits redirects' '
