aedffe9525084e3fcea251c8dd453c14deab6f4d
SZEDER Gábor <szeder.dev@gmail.com>
2018-02-24 00:39:51 +0100

    travis-ci: run tests with '-x' tracing
    Now that the test suite runs successfully with '-x' tracing even with
    /bin/sh, enable it on Travis CI in order to
    
      - get more information about test failures, and
    
      - catch constructs breaking '-x' with /bin/sh sneaking into our test
        suite.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/lib-travisci.sh
+++ ci/lib-travisci.sh
@@ -97,7 +97,7 @@ fi
 export DEVELOPER=1
 export DEFAULT_TEST_TARGET=prove
 export GIT_PROVE_OPTS="--timer --jobs 3 --state=failed,slow,save"
-export GIT_TEST_OPTS="--verbose-log"
+export GIT_TEST_OPTS="--verbose-log -x"
 export GIT_TEST_CLONE_2GB=YesPlease
 
 case "$jobname" in
