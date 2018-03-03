10cf3b076da31320108ad16bc3695a5e6a4600b0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-03-03 15:38:12 +0000

    perl: update our copy of Mail::Address
    Update our copy of Mail::Address from 2.19 (Aug 22, 2017) to 2.20 (Jan
    23, 2018). Like the preceding Error.pm update this is done simply to
    keep up-to-date with upstream, and as can be shown from the diff
    there's no functional changes.
    
    The updated source was retrieved from
    https://fastapi.metacpan.org/source/MARKOV/MailTools-2.20/lib/Mail/Address.pm
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- perl/Git/FromCPAN/Mail/Address.pm
+++ perl/Git/FromCPAN/Mail/Address.pm
@@ -1,10 +1,14 @@
-# Copyrights 1995-2017 by [Mark Overmeer <perl@overmeer.net>].
+# Copyrights 1995-2018 by [Mark Overmeer].
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
 # Pod stripped from pm file by OODoc 2.02.
+# This code is part of the bundle MailTools.  Meta-POD processed with
+# OODoc into POD and HTML manual-pages.  See README.md for Copyright.
+# Licensed under the same terms as Perl itself.
+
 package Mail::Address;
 use vars '$VERSION';
-$VERSION = '2.19';
+$VERSION = '2.20';
 
 use strict;
 
