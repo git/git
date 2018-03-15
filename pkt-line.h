f1f4d8acf400912a9e2a7b97080afced56719c29
Brandon Williams <bmwill@google.com>
2018-03-15 10:31:35 -0700

    pkt-line: add packet_buf_write_len function
    Add the 'packet_buf_write_len()' function which allows for writing an
    arbitrary length buffer into a 'struct strbuf' and formatting it in
    packet-line format.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pkt-line.h
+++ pkt-line.h
@@ -26,6 +26,7 @@ void packet_buf_flush(struct strbuf *buf);
 void packet_buf_delim(struct strbuf *buf);
 void packet_write(int fd_out, const char *buf, size_t size);
 void packet_buf_write(struct strbuf *buf, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
+void packet_buf_write_len(struct strbuf *buf, const char *data, size_t len);
 int packet_flush_gently(int fd);
 int packet_write_fmt_gently(int fd, const char *fmt, ...) __attribute__((format (printf, 2, 3)));
 int write_packetized_from_fd(int fd_in, int fd_out);
