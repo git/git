1775e990f793965dcfc2cf62f4efccf0f2c21e28
Todd Zullinger <tmz@pobox.com>
2018-03-18 00:01:35 -0400

    completion: complete tags with git tag --delete/--verify
    Completion of tag names has worked for the short -d/-v options since
    88e21dc746 ("Teach bash about completing arguments for git-tag",
    2007-08-31).  The long options were not added to "git tag" until many
    years later, in c97eff5a95 ("git-tag: introduce long forms for the
    options", 2011-08-28).
    
    Extend tag name completion to --delete/--verify.
    
    Signed-off-by: Todd Zullinger <tmz@pobox.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -3045,7 +3045,7 @@ _git_tag ()
 	while [ $c -lt $cword ]; do
 		i="${words[c]}"
 		case "$i" in
-		-d|-v)
+		-d|--delete|-v|--verify)
 			__gitcomp_direct "$(__git_tags "" "$cur" " ")"
 			return
 			;;
