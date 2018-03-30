48988c4d0c35b5c569a2645b61c9346f6062021d
Jeff King <peff@peff.net>
2018-03-30 14:34:46 -0400

    set_git_dir: die when setenv() fails
    The set_git_dir() function returns an error if setenv()
    fails, but there are zero callers who pay attention to this
    return value. If this ever were to happen, it could cause
    confusing results, as sub-processes would see a potentially
    stale GIT_DIR (e.g., if it is relative and we chdir()-ed to
    the root of the working tree).
    
    We _could_ try to fix each caller, but there's really
    nothing useful to do after this failure except die. Let's
    just lump setenv() failure into the same category as malloc
    failure: things that should never happen and cause us to
    abort catastrophically.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- environment.c
+++ environment.c
@@ -296,13 +296,12 @@ char *get_graft_file(void)
 	return the_repository->graft_file;
 }
 
-int set_git_dir(const char *path)
+void set_git_dir(const char *path)
 {
 	if (setenv(GIT_DIR_ENVIRONMENT, path, 1))
-		return error("Could not set GIT_DIR to '%s'", path);
+		die("could not set GIT_DIR to '%s'", path);
 	repo_set_gitdir(the_repository, path);
 	setup_git_env();
-	return 0;
 }
 
 const char *get_log_output_encoding(void)
