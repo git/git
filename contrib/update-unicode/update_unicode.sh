e233bef43e4a73ec183eb927ab4803aefc77bab9
Stefan Beller <sbeller@google.com>
2018-04-10 14:26:17 -0700

    unicode_width.h: rename to use dash in file name
    This is more consistent with the project style. The majority of Git's
    source files use dashes in preference to underscores in their file names.
    
    Also adjust contrib/update-unicode as well.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>

--- contrib/update-unicode/update_unicode.sh
+++ contrib/update-unicode/update_unicode.sh
@@ -6,7 +6,7 @@
 #Cf Format          a format control character
 #
 cd "$(dirname "$0")"
-UNICODEWIDTH_H=$(git rev-parse --show-toplevel)/unicode_width.h
+UNICODEWIDTH_H=$(git rev-parse --show-toplevel)/unicode-width.h
 
 wget -N http://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt \
 	http://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt &&
