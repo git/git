1131ec98189e993dcc48043c4f8e8b0128f52055
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-04-25 14:29:38 +0200

    pull: accept --rebase=merges to recreate the branch topology
    Similar to the `preserve` mode simply passing the `--preserve-merges`
    option to the `rebase` command, the `merges` mode simply passes the
    `--rebase-merges` option.
    
    This will allow users to conveniently rebase non-trivial commit
    topologies when pulling new commits, without flattening them.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2115,7 +2115,7 @@ _git_config ()
 		return
 		;;
 	branch.*.rebase)
-		__gitcomp "false true preserve interactive"
+		__gitcomp "false true merges preserve interactive"
 		return
 		;;
 	remote.pushdefault)
