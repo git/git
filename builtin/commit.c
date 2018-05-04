dc6b1d92ca9c0c538daa244e3910bb8b2a50d959
Eckhard S. Maaß <eckhard.s.maass@googlemail.com>
2018-05-04 13:12:15 +0200

    wt-status: use settings from git_diff_ui_config
    If you do something like
    
        - git add .
        - git status
        - git commit
        - git show (or git diff HEAD)
    
    one would expect to have analogous output from git status and git show
    (or similar diff-related programs). This is generally not the case, as
    git status has hard coded values for diff related options.
    
    With this commit the hard coded settings are dropped from the status
    command in favour for values provided by git_diff_ui_config.
    
    What follows are some remarks on the concrete options which were hard
    coded in git status:
    
    diffopt.detect_rename
    
    Since the very beginning of git status in a3e870f2e2 ("Add "commit"
    helper script", 2005-05-30), git status always used rename detection,
    whereas with commands like show and log one had to activate it with a
    command line option. After 5404c116aa ("diff: activate diff.renames by
    default", 2016-02-25) the default behaves the same by coincidence, but
    changing diff.renames to other values can break the consistency between
    git status and other commands again. With this commit one control the
    same default behaviour with diff.renames.
    
    diffopt.rename_limit
    
    Similarly one has the option diff.renamelimit to adjust this limit for
    all commands but git status. With this commit git status will also honor
    those.
    
    diffopt.break_opt
    
    Unlike the other two options this cannot be configured by a
    configuration option yet. This commit will also change the default
    behaviour to not use break rewrites. But as rename detection is most
    likely on, this is dangerous to be activated anyway as one can see
    here:
    
        https://public-inbox.org/git/xmqqegqaahnh.fsf@gitster.dls.corp.google.com/
    
    Signed-off-by: Eckhard S. Maaß <eckhard.s.maass@gmail.com>
    Reviewed-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -161,9 +161,9 @@ static void determine_whence(struct wt_status *s)
 static void status_init_config(struct wt_status *s, config_fn_t fn)
 {
 	wt_status_prepare(s);
+	init_diff_ui_defaults();
 	git_config(fn, s);
 	determine_whence(s);
-	init_diff_ui_defaults();
 	s->hints = advice_status_hints; /* must come after git_config() */
 }
 
