92c4a7a129ec5ee5a524c72d8cfe1a305dd496a0
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2018-05-05 08:51:44 +0200

    completion: fix misspelled config key aliasesfiletype
    The correct name in git-send-email.perl is aliasfiletype [1]. There are
    actually two instances of this misspelling. The other was found and
    fixed in 6068ac8848 (completion: add missing configuration variables -
    2010-12-20)
    
    [1] 994d6c66d3 (send-email: address expansion for common mailers - 2006-05-14)
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2177,7 +2177,7 @@ _git_config ()
 		__gitcomp "$__git_log_date_formats"
 		return
 		;;
-	sendemail.aliasesfiletype)
+	sendemail.aliasfiletype)
 		__gitcomp "mutt mailrc pine elm gnus"
 		return
 		;;
