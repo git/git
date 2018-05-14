4362da078e0b507fbb322ac3b33476a863f32493
SZEDER Gábor <szeder.dev@gmail.com>
2018-05-14 12:28:12 +0200

    t7005-editor: get rid of the SPACES_IN_FILENAMES prereq
    The last two tests 'editor with a space' and 'core.editor with a
    space' in 't7005-editor.sh' need the SPACES_IN_FILENAMES prereq to
    ensure that they are only run on filesystems that allow, well, spaces
    in filenames.  However, we have been putting a space in the name of
    the trash directory for just over a decade now, so we wouldn't be able
    to run any of our tests on such a filesystem in the first place.
    
    This prereq is therefore unnecessary, remove it.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7005-editor.sh
+++ t/t7005-editor.sh
@@ -111,14 +111,8 @@ do
 	'
 done
 
-if echo 'echo space > "$1"' > "e space.sh"
-then
-	# FS supports spaces in filenames
-	test_set_prereq SPACES_IN_FILENAMES
-fi
-
-test_expect_success SPACES_IN_FILENAMES 'editor with a space' '
-
+test_expect_success 'editor with a space' '
+	echo "echo space >\$1" >"e space.sh" &&
 	chmod a+x "e space.sh" &&
 	GIT_EDITOR="./e\ space.sh" git commit --amend &&
 	test space = "$(git show -s --pretty=format:%s)"
@@ -126,7 +120,7 @@ test_expect_success SPACES_IN_FILENAMES 'editor with a space' '
 '
 
 unset GIT_EDITOR
-test_expect_success SPACES_IN_FILENAMES 'core.editor with a space' '
+test_expect_success 'core.editor with a space' '
 
 	git config core.editor \"./e\ space.sh\" &&
 	git commit --amend &&
