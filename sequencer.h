cd327be464b71ddf1fc3dc06f728fbbadb94f367
Alban Gruin <alban.gruin@gmail.com>
2018-05-29 20:16:22 +0200

    rebase--interactive: rewrite append_todo_help() in C
    This rewrites append_todo_help() from shell to C. It also incorporates
    some parts of initiate_action() and complete_action() that also write
    help texts to the todo file.
    
    Two flags are added to rebase--helper.c: one to call
    append_todo_help() (`--append-todo-help`), and another one to tell
    append_todo_help() to write the help text suited for the edit-todo
    mode (`--write-edit-todo`).
    
    Finally, append_todo_help() is removed from git-rebase--interactive.sh
    to use `rebase--helper --append-todo-help` instead.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>

--- sequencer.h
+++ sequencer.h
@@ -80,6 +80,7 @@ int sequencer_make_script(FILE *out, int argc, const char **argv,
 int sequencer_add_exec_commands(const char *command);
 int transform_todos(unsigned flags);
 int check_todo_list(void);
+int append_todo_help(unsigned edit_todo, unsigned keep_empty);
 int skip_unnecessary_picks(void);
 int rearrange_squash(void);
 
