9dfd7bfece7fbed24602a710796f2c35a1c35373
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-06-07 10:47:25 +0200

    tests: replace mingw_test_cmp with a helper in C
    This helper is slightly more performant than the script with MSYS2's
    Bash. And a lot more readable.
    
    To accommodate t1050, which wants to compare files weighing in with 3MB
    (falling outside of t1050's malloc limit of 1.5MB), we simply lift the
    allocation limit by setting the environment variable GIT_ALLOC_LIMIT to
    zero when calling the helper.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/helper/test-tool.h
+++ t/helper/test-tool.h
@@ -7,6 +7,7 @@
 int cmd__advise_if_enabled(int argc, const char **argv);
 int cmd__bloom(int argc, const char **argv);
 int cmd__chmtime(int argc, const char **argv);
+int cmd__cmp(int argc, const char **argv);
 int cmd__config(int argc, const char **argv);
 int cmd__ctype(int argc, const char **argv);
 int cmd__date(int argc, const char **argv);
