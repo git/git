8c1f06af91bc10e447ba35a3d7e15ce39539aeae
Alban Gruin <alban.gruin@gmail.com>
2018-06-12 21:38:56 +0200

    editor: add a function to launch the sequence editor
    As part of the rewrite of interactive rebase, the sequencer will need to
    open the sequence editor to allow the user to edit the todo list.
    Instead of duplicating the existing launch_editor() function, this
    refactors it to a new function, launch_specified_editor(), which takes
    the editor as a parameter, in addition to the path, the buffer and the
    environment variables.  launch_sequence_editor() is then added to launch
    the sequence editor.
    
    Signed-off-by: Alban Gruin <alban.gruin@gmail.com>

--- cache.h
+++ cache.h
@@ -1472,6 +1472,7 @@ extern const char *fmt_name(const char *name, const char *email);
 extern const char *ident_default_name(void);
 extern const char *ident_default_email(void);
 extern const char *git_editor(void);
+extern const char *git_sequence_editor(void);
 extern const char *git_pager(int stdout_is_tty);
 extern int is_terminal_dumb(void);
 extern int git_ident_config(const char *, const char *, void *);
