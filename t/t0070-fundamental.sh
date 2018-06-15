037714252f74ae77e816018e43b2504a1b057246
Junio C Hamano <gitster@pobox.com>
2018-06-15 11:13:39 -0700

    tests: clean after SANITY tests
    Some of our tests try to make sure Git behaves sensibly in a
    read-only directory, by dropping 'w' permission bit before doing a
    test and then restoring it after it is done.  The latter is needed
    for the test framework to clean after itself without leaving a
    leftover directory that cannot be removed.
    
    Ancient parts of tests however arrange the above with
    
    	chmod a-w . &&
    	... do the test ...
    	status=$?
    	chmod 775 .
    	(exit $status)
    
    which obviously would not work if the test somehow dies before it
    has the chance to do "chmod 775".  Rewrite them by following a more
    robust pattern recently written tests use, which is
    
    	test_when_finished "chmod 775 ." &&
    	chmod a-w . &&
    	... do the test ...
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0070-fundamental.sh
+++ t/t0070-fundamental.sh
@@ -19,8 +19,8 @@ test_expect_success 'mktemp to nonexistent directory prints filename' '
 
 test_expect_success POSIXPERM,SANITY 'mktemp to unwritable directory prints filename' '
 	mkdir cannotwrite &&
-	chmod -w cannotwrite &&
 	test_when_finished "chmod +w cannotwrite" &&
+	chmod -w cannotwrite &&
 	test_must_fail test-mktemp cannotwrite/testXXXXXX 2>err &&
 	grep "cannotwrite/test" err
 '
