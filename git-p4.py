efdcc99263871818cfe297a5a981d5841c77ebac
Luke Diamand <luke@diamand.org>
2018-06-19 09:04:09 +0100

    git-p4: python3: basestring workaround
    In Python3, basestring no longer exists, so use this workaround.
    
    Signed-off-by: Luke Diamand <luke@diamand.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-p4.py
+++ git-p4.py
@@ -27,6 +27,22 @@ import zlib
 import ctypes
 import errno
 
+# support basestring in python3
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 try:
     from subprocess import CalledProcessError
 except ImportError:
