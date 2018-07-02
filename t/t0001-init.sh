ed6c994af407dd2bea110460657222e39a7d83ad
Eric Sunshine <sunshine@sunshineco.com>
2018-07-01 20:23:43 -0400

    t: use sane_unset() rather than 'unset' with broken &&-chain
    These tests intentionally break the &&-chain after using 'unset' since
    they don't know if 'unset' will succeed or fail and don't want a local
    'unset' failure to fail the test overall. We can do better by using
    sane_unset(), which can be linked into the &&-chain as usual.
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0001-init.sh
+++ t/t0001-init.sh
@@ -408,7 +408,7 @@ is_hidden () {
 test_expect_success MINGW '.git hidden' '
 	rm -rf newdir &&
 	(
-		unset GIT_DIR GIT_WORK_TREE
+		sane_unset GIT_DIR GIT_WORK_TREE &&
 		mkdir newdir &&
 		cd newdir &&
 		git init &&
@@ -420,7 +420,7 @@ test_expect_success MINGW '.git hidden' '
 test_expect_success MINGW 'bare git dir not hidden' '
 	rm -rf newdir &&
 	(
-		unset GIT_DIR GIT_WORK_TREE GIT_CONFIG
+		sane_unset GIT_DIR GIT_WORK_TREE GIT_CONFIG &&
 		mkdir newdir &&
 		cd newdir &&
 		git --bare init
