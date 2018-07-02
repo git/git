627c87f84c157885d587125cf0c73591a0e3ee7a
Martin Schön <Martin.Schoen@loewensteinmedical.de>
2018-07-02 15:28:09 +0200

    git-gui: use commit message template
    Use the file described by commit.template (if set) to show the commit message
    template, just like other GUIs.
    
    Signed-off-by: Martin Schön <Martin.Schoen@loewensteinmedical.de>
    Signed-off-by: Pratyush Yadav <me@yadavpratyush.com>

--- git-gui.sh
+++ git-gui.sh
@@ -1482,6 +1482,7 @@ proc rescan {after {honor_trustmtime 1}} {
 		} elseif {[run_prepare_commit_msg_hook]} {
 		} elseif {[load_message MERGE_MSG]} {
 		} elseif {[load_message SQUASH_MSG]} {
+		} elseif {[load_message [get_config commit.template]]} {
 		}
 		$ui_comm edit reset
 		$ui_comm edit modified false
@@ -1613,6 +1614,12 @@ proc run_prepare_commit_msg_hook {} {
 	} elseif {[file isfile [gitdir SQUASH_MSG]]} {
 		set pcm_source "squash"
 		set fd_sm [open [gitdir SQUASH_MSG] r]
+		fconfigure $fd_sm -encoding utf-8
+		puts -nonewline $fd_pcm [read $fd_sm]
+		close $fd_sm
+	} elseif {[file isfile [get_config commit.template]]} {
+		set pcm_source "template"
+		set fd_sm [open [get_config commit.template] r]
 		fconfigure $fd_sm -encoding utf-8
 		puts -nonewline $fd_pcm [read $fd_sm]
 		close $fd_sm
