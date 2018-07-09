91c2f2040ab83f11a87b271a842905d2e5c5ada4
Beat Bolli <dev+git@drbeat.li>
2018-07-09 21:25:33 +0200

    refs/refs-internal.h: avoid forward declaration of an enum
    Include iterator.h to define enum iterator_selection.
    
    Signed-off-by: Beat Bolli <dev+git@drbeat.li>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs/refs-internal.h
+++ refs/refs-internal.h
@@ -1,6 +1,8 @@
 #ifndef REFS_REFS_INTERNAL_H
 #define REFS_REFS_INTERNAL_H
 
+#include "iterator.h"   /* for enum iterator_selection */
+
 /*
  * Data structures and functions for the internal use of the refs
  * module. Code outside of the refs module should use only the public
