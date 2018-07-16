21c770b63ea7ddcb6e52527d82089068b33eb9f3
Stefan Beller <sbeller@google.com>
2018-07-16 16:05:35 -0700

    xdiff/xdiffi.c: remove unneeded function declarations
    There is no need to forward-declare these functions, as they are used
    after their implementation only.
    
    Signed-off-by: Stefan Beller <sbeller@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- xdiff/xdiffi.c
+++ xdiff/xdiffi.c
@@ -22,33 +22,16 @@
 
 #include "xinclude.h"
 
-
-
 #define XDL_MAX_COST_MIN 256
 #define XDL_HEUR_MIN_COST 256
 #define XDL_LINE_MAX (long)((1UL << (CHAR_BIT * sizeof(long) - 1)) - 1)
 #define XDL_SNAKE_CNT 20
 #define XDL_K_HEUR 4
 
-
-
 typedef struct s_xdpsplit {
 	long i1, i2;
 	int min_lo, min_hi;
 } xdpsplit_t;
-
-
-
-
-static long xdl_split(unsigned long const *ha1, long off1, long lim1,
-		      unsigned long const *ha2, long off2, long lim2,
-		      long *kvdf, long *kvdb, int need_min, xdpsplit_t *spl,
-		      xdalgoenv_t *xenv);
-static xdchange_t *xdl_add_change(xdchange_t *xscr, long i1, long i2, long chg1, long chg2);
-
-
-
-
 
 /*
  * See "An O(ND) Difference Algorithm and its Variations", by Eugene Myers.
