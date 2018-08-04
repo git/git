46af44b07dd433c073778f0d5ab27d0167dbcec4
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-04 12:23:09 -0700

    pull --rebase=<type>: allow single-letter abbreviations for the type
    Git for Windows' original 4aa8b8c8283 (Teach 'git pull' to handle
    --rebase=interactive, 2011-10-21) had support for the very convenient
    abbreviation
    
    	git pull --rebase=i
    
    which was later lost when it was ported to the builtin `git pull`, and
    it was not introduced before the patch eventually made it into Git as
    f5eb87b98dd (pull: allow interactive rebase with --rebase=interactive,
    2016-01-13).
    
    However, it is *really* a useful short hand for the occasional rebasing
    pull on branches that do not usually want to be rebased.
    
    So let's reintroduce this convenience, at long last.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/pull.c
+++ builtin/pull.c
@@ -48,11 +48,11 @@ static enum rebase_type parse_config_rebase(const char *key, const char *value,
 		return REBASE_FALSE;
 	else if (v > 0)
 		return REBASE_TRUE;
-	else if (!strcmp(value, "preserve"))
+	else if (!strcmp(value, "preserve") || !strcmp(value, "p"))
 		return REBASE_PRESERVE;
-	else if (!strcmp(value, "merges"))
+	else if (!strcmp(value, "merges") || !strcmp(value, "m"))
 		return REBASE_MERGES;
-	else if (!strcmp(value, "interactive"))
+	else if (!strcmp(value, "interactive") || !strcmp(value, "i"))
 		return REBASE_INTERACTIVE;
 
 	if (fatal)
