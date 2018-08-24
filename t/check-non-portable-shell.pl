b2fa7a2372f42af404a942a4691a5341af8868e9
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-08-24 15:20:12 +0000

    tests: fix and add lint for non-portable seq
    The seq command is not in POSIX, and doesn't exist on
    e.g. OpenBSD. We've had the test_seq wrapper since d17cf5f3a3 ("tests:
    Introduce test_seq", 2012-08-04), but use of it keeps coming back,
    e.g. in the recently added "fetch negotiator" tests being added here.
    
    So let's also add a check to "make test-lint". The regex is aiming to
    capture the likes of $(seq ..) and "seq" as a stand-alone command,
    without capturing some existing cases where we e.g. have files called
    "seq", as \bseq\b would do.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/check-non-portable-shell.pl
+++ t/check-non-portable-shell.pl
@@ -42,6 +42,7 @@ while (<>) {
 	/\btest\s+[^=]*==/ and err '"test a == b" is not portable (use =)';
 	/\bwc -l.*"\s*=/ and err '`"$(wc -l)"` is not portable (use test_line_count)';
 	/\bhead\s+-c\b/ and err 'head -c is not portable (use test_copy_bytes BYTES <file >out)';
+	/(?:\$\(seq|^\s*seq\b)/ and err 'seq is not portable (use test_seq)';
 	/\bexport\s+[A-Za-z0-9_]*=/ and err '"export FOO=bar" is not portable (use FOO=bar && export FOO)';
 	/^\s*([A-Z0-9_]+=(\w+|(["']).*?\3)\s+)+(\w+)/ and exists($func{$4}) and
 		err '"FOO=bar shell_func" assignment extends beyond "shell_func"';
