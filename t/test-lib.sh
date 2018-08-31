099c26f0b52cf0318e0ddfebd4f371de26f1dc78
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-31 15:40:14 +0200

    tests: include detailed trace logs with --write-junit-xml upon failure
    The JUnit XML format lends itself to be presented in a powerful UI,
    where you can drill down to the information you are interested in very
    quickly.
    
    For test failures, this usually means that you want to see the detailed
    trace of the failing tests.
    
    With Travis CI, we passed the `--verbose-log` option to get those
    traces. However, that seems excessive, as we do not need/use the logs in
    almost all of those cases: only when a test fails do we have a way to
    include the trace.
    
    So let's do something different when using Azure DevOps: let's run all
    the tests with `--quiet` first, and only if a failure is encountered,
    try to trace the commands as they are executed.
    
    Of course, we cannot turn on `--verbose-log` after the fact. So let's
    just re-run the test with all the same options, adding `--verbose-log`.
    And then munging the output file into the JUnit XML on the fly.
    
    Note: there is an off chance that re-running the test in verbose mode
    "fixes" the failures (and this does happen from time to time!). That is
    a possibility we should be able to live with. Ideally, we would label
    this as "Passed upon rerun", and Azure Pipelines even know about that
    outcome, but it is not available when using the JUnit XML format for
    now:
    https://github.com/Microsoft/azure-pipelines-agent/blob/master/src/Agent.Worker/TestResults/JunitResultReader.cs
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -95,6 +95,13 @@ done,*)
 	test "$(cat "$BASE.exit")" = 0
 	exit
 	;;
+*' --write-junit-xml '*)
+	# record how to call this script *with* --verbose-log, in case
+	# we encounter a breakage
+	junit_rerun_options_sq="$(printf '%s\n' "$0" --verbose-log -x "$@" |
+		sed -e "s/'/'\\\\''/g" -e "s/^/'/" -e "s/\$/'/" |
+		tr '\012' ' ')"
+	;;
 esac
 
 # For repeatability, reset the environment to known value.
@@ -503,10 +510,31 @@ test_ok_ () {
 test_failure_ () {
 	if test -n "$write_junit_xml"
 	then
+		if test -z "$GIT_TEST_TEE_OUTPUT_FILE"
+		then
+			# clean up
+			test_atexit_handler
+
+			# re-run with --verbose-log
+			echo "# Re-running: $junit_rerun_options_sq" >&2
+
+			cd "$TEST_DIRECTORY" &&
+			eval "${TEST_SHELL_PATH}" "$junit_rerun_options_sq" \
+				>/dev/null 2>&1
+			status=$?
+
+			say_color "" "$(test 0 = $status ||
+				echo "not ")ok $test_count - (re-ran with trace)"
+			say "1..$test_count"
+			GIT_EXIT_OK=t
+			exit $status
+		fi
+
 		junit_insert="<failure message=\"not ok $test_count -"
 		junit_insert="$junit_insert $(xml_attr_encode "$1")\">"
 		junit_insert="$junit_insert $(xml_attr_encode \
-			"$(printf '%s\n' "$@" | sed 1d)")"
+			"$(cat "$GIT_TEST_TEE_OUTPUT_FILE")")"
+		>"$GIT_TEST_TEE_OUTPUT_FILE"
 		junit_insert="$junit_insert</failure>"
 		write_junit_xml_testcase "$1" "      $junit_insert"
 	fi
@@ -791,6 +819,10 @@ test_start_ () {
 	if test -n "$write_junit_xml"
 	then
 		junit_start=$(test-tool date getnanos)
+
+		# truncate output
+		test -z "$GIT_TEST_TEE_OUTPUT_FILE" ||
+		>"$GIT_TEST_TEE_OUTPUT_FILE"
 	fi
 }
 
