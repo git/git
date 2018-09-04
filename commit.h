0d280933e3123e97bbd3a0917e47c291f0ff04e6
Pratik Karki <predatoramigo@gmail.com>
2018-09-04 15:00:08 -0700

    merge-base --fork-point: extract libified function
    We need this functionality in the builtin rebase.
    
    Note: to make this function truly reusable, we have to switch the call
    get_merges_many_dirty() to get_merges_many() because we want the commit
    flags to be reset (otherwise, subsequent get_merge_bases() calls would
    obtain incorrect results). This did not matter when the function was
    called in `git rev-parse --fork-point` because in that command, the
    process definitely did not traverse any commits before exiting.
    
    Signed-off-by: Pratik Karki <predatoramigo@gmail.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- commit.h
+++ commit.h
@@ -212,6 +212,8 @@ extern struct commit_list *get_octopus_merge_bases(struct commit_list *in);
 /* To be used only when object flags after this call no longer matter */
 extern struct commit_list *get_merge_bases_many_dirty(struct commit *one, int n, struct commit **twos);
 
+struct commit *get_fork_point(const char *refname, struct commit *commit);
+
 /* largest positive number a signed 32-bit integer can contain */
 #define INFINITE_DEPTH 0x7fffffff
 
