8e97b89542137df827992c1b686866de15a86aa0
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-10-23 12:57:17 -0700

    rebase --autostash: fix issue with dirty submodules
    Since we cannot stash dirty submodules, there is no use in requiring
    them to be clean (or stash them when they are not).
    
    This brings the built-in rebase in line with the previous, scripted
    version, which also did not care about dirty submodules (but it was
    admittedly not very easy to figure that out).
    
    This fixes https://github.com/git-for-windows/git/issues/1820
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/rebase.c
+++ builtin/rebase.c
@@ -1352,7 +1352,7 @@ int cmd_rebase(int argc, const char **argv, const char *prefix)
 			update_index_if_able(&the_index, &lock_file);
 		rollback_lock_file(&lock_file);
 
-		if (has_unstaged_changes(0) || has_uncommitted_changes(0)) {
+		if (has_unstaged_changes(1) || has_uncommitted_changes(1)) {
 			const char *autostash =
 				state_dir_path("autostash", &options);
 			struct child_process stash = CHILD_PROCESS_INIT;
