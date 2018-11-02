3a00b507d8493a7d7e82288a8b35e708bba5a4c9
Ben Peart <benpeart@microsoft.com>
2018-11-02 11:19:10 -0400

    fscache: fscache takes an initial size
    Update enable_fscache() to take an optional initial size parameter which is
    used to initialize the hashmap so that it can avoid having to rehash as
    additional entries are added.
    
    Add a separate disable_fscache() macro to make the code clearer and easier
    to read.
    
    Signed-off-by: Ben Peart <benpeart@microsoft.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/win32/fscache.h
+++ compat/win32/fscache.h
@@ -1,8 +1,9 @@
 #ifndef FSCACHE_H
 #define FSCACHE_H
 
-int fscache_enable(int enable);
-#define enable_fscache(x) fscache_enable(x)
+int fscache_enable(int enable, size_t initial_size);
+#define enable_fscache(initial_size) fscache_enable(1, initial_size)
+#define disable_fscache() fscache_enable(0, 0)
 
 int fscache_enabled(const char *path);
 #define is_fscache_enabled(path) fscache_enabled(path)
