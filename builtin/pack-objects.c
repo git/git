f53c163bcd7ae9bf83aa87be81dc6a4661924f3e
Jeff King <peff@peff.net>
2018-11-05 01:39:38 -0500

    pack-objects: mark index-version option as NONEG
    Running "git pack-objects --no-index-version" will segfault, since the
    callback is not prepared to handle the "unset" flag.
    
    In theory this might be used to counteract an earlier "--index-version",
    or override a pack.indexversion config setting. But the semantics aren't
    immediately obvious, and it's unlikely anybody wants this. Let's just
    disable the broken option for now.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/pack-objects.c
+++ builtin/pack-objects.c
@@ -3252,7 +3252,7 @@ int cmd_pack_objects(int argc, const char **argv, const char *prefix)
 			 N_("similar to --all-progress when progress meter is shown")),
 		{ OPTION_CALLBACK, 0, "index-version", NULL, N_("<version>[,<offset>]"),
 		  N_("write the pack index file in the specified idx format version"),
-		  0, option_parse_index_version },
+		  PARSE_OPT_NONEG, option_parse_index_version },
 		OPT_MAGNITUDE(0, "max-pack-size", &pack_size_limit,
 			      N_("maximum size of each output pack file")),
 		OPT_BOOL(0, "local", &local,
