d27525e519310b515b3014cef5920f80930a20ce
Sven Strickroth <email@cs-ware.de>
2018-11-19 16:14:42 +0100

    msvc: directly use MS version (_stricmp) of strcasecmp
    This also removes an implicit conversion from size_t (unsigned) to int (signed).
    
    _stricmp as well as _strnicmp are both available since VS2012.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/msvc.h
+++ compat/msvc.h
@@ -10,17 +10,11 @@
 #define inline __inline
 #define __inline__ __inline
 #define __attribute__(x)
+#define strcasecmp   _stricmp
 #define strncasecmp  _strnicmp
 #define ftruncate    _chsize
 #define strtoull     _strtoui64
 #define strtoll      _strtoi64
-
-static __inline int strcasecmp (const char *s1, const char *s2)
-{
-	int size1 = strlen(s1);
-	int sisz2 = strlen(s2);
-	return _strnicmp(s1, s2, sisz2 > size1 ? sisz2 : size1);
-}
 
 #undef ERROR
 
