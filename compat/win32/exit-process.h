0f05a8c013f4ad7bacded53dce14f406017869f1
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-11-27 22:48:07 +0100

    fixup! mingw: kill child processes in a gentler way
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/win32/exit-process.h
+++ compat/win32/exit-process.h
@@ -127,7 +127,7 @@ static int exit_process(HANDLE process, int exit_code)
 		HANDLE thread = NULL;
 
 		if (!initialized) {
-			HINSTANCE kernel32 = GetModuleHandle("kernel32");
+			HINSTANCE kernel32 = GetModuleHandleA("kernel32");
 			if (!kernel32)
 				die("BUG: cannot find kernel32");
 			exit_process_address = (LPTHREAD_START_ROUTINE)
