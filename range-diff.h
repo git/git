d8981c3f885ceaddfec0e545b0f995b96e5ec58f
Junio C Hamano <gitster@pobox.com>
2018-11-30 13:27:11 +0900

    format-patch: do not let its diff-options affect --range-diff
    Stop leaking how the primary output of format-patch is customized to
    the range-diff machinery and instead let the latter use its own
    "reasonable default", in order to correct the breakage introduced by
    a5170794 ("Merge branch 'ab/range-diff-no-patch'", 2018-11-18) on
    the 'master' front.  "git format-patch --range-diff..." without any
    weird diff option started to include the "range-diff --stat" output,
    which is rather useless right now, that made the whole thing
    unusable and this is probably the least disruptive way to whip the
    codebase into a shippable shape.
    
    We may want to later make the range-diff driven by format-patch more
    configurable, but that would have to wait until we have a good
    design.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- range-diff.h
+++ range-diff.h
@@ -5,6 +5,11 @@
 
 #define RANGE_DIFF_CREATION_FACTOR_DEFAULT 60
 
+/*
+ * Compare series of commmits in RANGE1 and RANGE2, and emit to the
+ * standard output.  NULL can be passed to DIFFOPT to use the built-in
+ * default.
+ */
 int show_range_diff(const char *range1, const char *range2,
 		    int creation_factor, int dual_color,
 		    struct diff_options *diffopt);
