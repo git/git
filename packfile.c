3b424f7c2acafb400284089b218d2fb57f3192f2
Derrick Stolee <dstolee@microsoft.com>
2018-12-17 16:42:53 -0500

    repack: refactor pack deletion for future use
    The repack builtin deletes redundant pack-files and their
    associated .idx, .promisor, .bitmap, and .keep files. We will want
    to re-use this logic in the future for other types of repack, so
    pull the logic into 'unlink_pack_path()' in packfile.c.
    
    The 'ignore_keep' parameter is enabled for the use in repack, but
    will be important for a future caller.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- packfile.c
+++ packfile.c
@@ -352,6 +352,34 @@ void close_all_packs(struct raw_object_store *o)
 	}
 }
 
+void unlink_pack_path(const char *pack_name, int force_delete)
+{
+	static const char *exts[] = {".pack", ".idx", ".keep", ".bitmap", ".promisor"};
+	int i;
+	struct strbuf buf = STRBUF_INIT;
+	size_t plen;
+
+	strbuf_addstr(&buf, pack_name);
+	strip_suffix_mem(buf.buf, &buf.len, ".pack");
+	plen = buf.len;
+
+	if (!force_delete) {
+		strbuf_addstr(&buf, ".keep");
+		if (!access(buf.buf, F_OK)) {
+			strbuf_release(&buf);
+			return;
+		}
+	}
+
+	for (i = 0; i < ARRAY_SIZE(exts); i++) {
+		strbuf_setlen(&buf, plen);
+		strbuf_addstr(&buf, exts[i]);
+		unlink(buf.buf);
+	}
+
+	strbuf_release(&buf);
+}
+
 /*
  * The LRU pack is the one with the oldest MRU window, preferring packs
  * with no used windows, or the oldest mtime if it has no windows allocated.
