a3b994b71f0280c8d6993207bc8cc142eab8e344
Denton Liu <liu.denton@gmail.com>
2019-01-22 21:06:23 -0800

    merge: cleanup messages like commit
    This change allows git-merge messages to be cleaned up with the
    commit.cleanup configuration or --cleanup option, just like how
    git-commit does it.
    
    We also give git-pull the passthrough option of --cleanup so that it can
    also take advantage of this change.
    
    Finally, add testing to ensure that messages are properly cleaned up.
    Note that some newlines that were added to the commit message were
    removed so that if a file were read via -F, it would be copied
    faithfully.
    
    Signed-off-by: Denton Liu <liu.denton@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- wt-status.h
+++ wt-status.h
@@ -126,6 +126,7 @@ struct wt_status_state {
 };
 
 size_t wt_status_locate_end(const char *s, size_t len);
+void wt_status_append_cut_line(struct strbuf *buf);
 void wt_status_add_cut_line(FILE *fp);
 void wt_status_prepare(struct wt_status *s);
 void wt_status_print(struct wt_status *s);
