e7ec2303612f462fb60932ca5ec76eaee84cf0f7
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-02-12 12:08:35 +0100

    fixup! status: verify that --show-ignored-directory prints a warning
    We said we would remove --show-ignored-directory in v2.16.0 or v2.17.0,
    but v2.36.1 is now the current version and we still have not removed it.
    Let's do it now.
    
    We had to wait for a while because some users were still on Visual
    Studio 2017 (which used that option until a certain stage). But then,
    users who upgrade Git will also upgrade Visual Studio, or so one would
    hope, therefore they should not be affected by this change.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t7522-status-show-ignored-directory.sh
+++ t/t7522-status-show-ignored-directory.sh
@@ -21,7 +21,6 @@ test_expect_success 'setup initial commit and ignore file' '
 '
 
 cat >expect <<\EOF
-? err
 ? expect
 ? output
 ! dir/ignored/ignored_1.ign
@@ -39,9 +38,8 @@ test_expect_success 'setup folder with ignored files' '
 
 test_expect_success 'Verify behavior of status on folders with ignored files' '
 	test_when_finished "git clean -fdx" &&
-	git status --porcelain=v2 --ignored --untracked-files=all --show-ignored-directory >output 2>err &&
-	test_cmp expect output &&
-	grep "deprecated.*use --ignored=matching instead" err
+	git status --porcelain=v2 --ignored --untracked-files=all --show-ignored-directory >output &&
+	test_cmp expect output
 '
 
 # Test status bahavior on folder with tracked and ignored files
