5de54543b05adf5906dc515bf92a2c4dd86fe122
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-02-25 23:16:30 +0000

    stash: optionally use the scripted version again
    We recently converted the `git stash` command from Unix shell scripts
    to builtins.
    
    Let's end users a way out when they discover a bug in the
    builtin command: `stash.useBuiltin`.
    
    As the file name `git-stash` is already in use, let's rename the
    scripted backend to `git-legacy-stash`.
    
    To make the test suite pass with `stash.useBuiltin=false`, this commit
    also backports rudimentary support for `-q` (but only *just* enough
    to appease the test suite), and adds a super-ugly hack to force exit
    code 129 for `git stash -h`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/stash.c
+++ builtin/stash.c
@@ -14,6 +14,7 @@
 #include "revision.h"
 #include "log-tree.h"
 #include "diffcore.h"
+#include "exec-cmd.h"
 
 #define INCLUDE_ALL_FILES 2
 
@@ -1512,6 +1513,26 @@ static int save_stash(int argc, const char **argv, const char *prefix)
 	return ret;
 }
 
+static int use_builtin_stash(void)
+{
+	struct child_process cp = CHILD_PROCESS_INIT;
+	struct strbuf out = STRBUF_INIT;
+	int ret;
+
+	argv_array_pushl(&cp.args,
+			 "config", "--bool", "stash.usebuiltin", NULL);
+	cp.git_cmd = 1;
+	if (capture_command(&cp, &out, 6)) {
+		strbuf_release(&out);
+		return 1;
+	}
+
+	strbuf_trim(&out);
+	ret = !strcmp("true", out.buf);
+	strbuf_release(&out);
+	return ret;
+}
+
 int cmd_stash(int argc, const char **argv, const char *prefix)
 {
 	int i = -1;
@@ -1522,6 +1543,20 @@ int cmd_stash(int argc, const char **argv, const char *prefix)
 	struct option options[] = {
 		OPT_END()
 	};
+
+	if (!use_builtin_stash()) {
+		const char *path = mkpath("%s/git-legacy-stash",
+					  git_exec_path());
+
+		if (sane_execvp(path, (char **)argv) < 0)
+			die_errno(_("could not exec %s"), path);
+		else
+			BUG("sane_execvp() returned???");
+	}
+
+	prefix = setup_git_directory();
+	trace_repo_setup(prefix);
+	setup_work_tree();
 
 	git_config(git_diff_basic_config, NULL);
 
