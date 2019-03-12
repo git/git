8e868214c727291de2dbc80b52a4f7a2511214b9
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-03-12 16:40:46 +0100

    built-in add -p: color the prompt and the help text
    ... just like the Perl version ;-)
    
    Note that this requires the `get_add_i_color()` function being defined
    globally, which is the entire reason why we gave it such a descriptive
    name in the first place.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- add-interactive.h
+++ add-interactive.h
@@ -16,6 +16,15 @@ struct add_i_state {
 
 int init_add_i_state(struct repository *r, struct add_i_state *s);
 
+enum color_add_i {
+	COLOR_HEADER = 0,
+	COLOR_HELP,
+	COLOR_PROMPT,
+	COLOR_ERROR,
+	COLOR_RESET,
+};
+const char *get_add_i_color(enum color_add_i ix);
+
 struct repository;
 struct pathspec;
 int run_add_i(struct repository *r, const struct pathspec *ps);
