e8ca52059cc0e65d3c0eea7ab6e90764686a1dae
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-03-23 19:36:22 +0100

    t3701: verify that the diff.algorithm config setting is handled
    Without this patch, there is actually no test in Git's test suite that
    covers the diff.algorithm feature. Let's add one.
    
    We do this by passing a bogus value and then expecting `git diff-files`
    to produce the appropriate error message.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t3701-add-interactive.sh
+++ t/t3701-add-interactive.sh
@@ -521,6 +521,16 @@ test_expect_success 'detect bogus diffFilter output' '
 	printf y | test_must_fail force_color git add -p
 '
 
+test_expect_success 'diff.algorithm is passed to `git diff-files`' '
+	git reset --hard &&
+
+	>file &&
+	git add file &&
+	echo changed >file &&
+	git -c diff.algorithm=bogus add -p 2>err &&
+	test_i18ngrep "error: option diff-algorithm accepts " err
+'
+
 test_expect_success 'patch-mode via -i prompts for files' '
 	git reset --hard &&
 
