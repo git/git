ccbbd8bf66ca88385a34b16abcc1d5a800650d3a
Jeff King <peff@peff.net>
2019-03-24 08:09:46 -0400

    http: normalize curl results for dumb loose and alternates fetches
    If the dumb-http walker encounters a 404 when fetching a loose object,
    it then looks at any http-alternates for the object. The 404 check is
    implemented by missing_target(), which checks not only the http code,
    but also that we got an http error from the CURLcode.
    
    That broke when we stopped using CURLOPT_FAILONERROR in 17966c0a63
    (http: avoid disconnecting on 404s for loose objects, 2016-07-11), since
    our CURLcode will now be CURLE_OK. As a result, fetching over dumb-http
    from a repository with alternates could result in Git printing "Unable
    to find abcd1234..." and aborting.
    
    We could probably fix this just by loosening missing_target(). However,
    there's other code which looks at the curl result, and it would have to
    be tweaked as well. Instead, let's just normalize the result the same
    way the smart-http code does.
    
    There's a similar case in processing the alternates (where we failover
    from "info/http-alternates" to "info/alternates"). We'll give it the
    same treatment.
    
    After this patch, we should be hitting all code paths that need this
    normalization (notably absent here is the http_pack_request path, but it
    does not use FAILONERROR, nor missing_target()).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- http-walker.c
+++ http-walker.c
@@ -98,6 +98,11 @@ static void process_object_response(void *callback_data)
 	process_http_object_request(obj_req->req);
 	obj_req->state = COMPLETE;
 
+	normalize_curl_result(&obj_req->req->curl_result,
+			      obj_req->req->http_code,
+			      obj_req->req->errorstr,
+			      sizeof(obj_req->req->errorstr));
+
 	/* Use alternates if necessary */
 	if (missing_target(obj_req->req)) {
 		fetch_alternates(walker, alt->base);
@@ -207,6 +212,9 @@ static void process_alternates_response(void *callback_data)
 	const char null_byte = '\0';
 	char *data;
 	int i = 0;
+
+	normalize_curl_result(&slot->curl_result, slot->http_code,
+			      curl_errorstr, sizeof(curl_errorstr));
 
 	if (alt_req->http_specific) {
 		if (slot->curl_result != CURLE_OK ||
