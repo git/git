323786f1595dfa3e9aed76f207f152cd48b047c2
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-03-26 21:28:10 +0100

    terminal: add a new function to read a single keystroke
    Typically, input on the command-line is line-based. It is actually not
    really easy to get single characters (or better put: keystrokes).
    
    We provide two implementations here:
    
    - One that handles `/dev/tty` based systems as well as native Windows.
      The former uses the `tcsetattr()` function to put the terminal into
      "raw mode", which allows us to read individual keystrokes, one by one.
      The latter uses `stty.exe` to do the same, falling back to direct
      Win32 Console access.
    
      Thanks to the refactoring leading up to this commit, this is a single
      function, with the platform-specific details hidden away in
      conditionally-compiled code blocks.
    
    - A fall-back which simply punts and reads back an entire line.
    
    Note that the function writes the keystroke into an `strbuf` rather than
    a `char`, in preparation for reading Escape sequences (e.g. when the
    user hit an arrow key). This is also required for UTF-8 sequences in
    case the keystroke corresponds to a non-ASCII letter.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/terminal.h
+++ compat/terminal.h
@@ -3,4 +3,7 @@
 
 char *git_terminal_prompt(const char *prompt, int echo);
 
+/* Read a single keystroke, without echoing it to the terminal */
+int read_key_without_echo(struct strbuf *buf);
+
 #endif /* COMPAT_TERMINAL_H */
