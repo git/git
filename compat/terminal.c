cad52c8f876a0154ac98419b393922962f150956
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-05-07 10:49:00 +0200

    fixup! mingw (git_terminal_prompt): turn on echo explictly
    The `add -p` changes conflict with this commit, so let's revert it for
    now.
    
    Besides, it would appear that the rationale of the change is not sound:
    in a quick test in a PowerShell, it seems that echo *is* the default. To
    verify, test, run this in a PowerShell session:
    
    	git -c alias.s="!
    	unset GIT_CONFIG_PARAMETERS;
    	printf `"hostname=123\\n\\n`" | GIT_ASKPASS= ./git \
    	-c credential.helper= --exec-path=`$PWD credential fill
    	" s
    
    This reverts commit b9990a392775401e0600971fefa1529098342dce.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/terminal.c
+++ compat/terminal.c
@@ -77,37 +77,23 @@ static void restore_term(void)
 	hconin = INVALID_HANDLE_VALUE;
 }
 
-static int set_echo(int echo)
+static int disable_echo(void)
 {
-	DWORD new_cmode;
-
-	if (hconin == INVALID_HANDLE_VALUE)
-		hconin = CreateFile("CONIN$", GENERIC_READ | GENERIC_WRITE,
-				    FILE_SHARE_READ, NULL, OPEN_EXISTING,
-				    FILE_ATTRIBUTE_NORMAL, NULL);
+	hconin = CreateFile("CONIN$", GENERIC_READ | GENERIC_WRITE,
+	    FILE_SHARE_READ, NULL, OPEN_EXISTING,
+	    FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hconin == INVALID_HANDLE_VALUE)
 		return -1;
 
 	GetConsoleMode(hconin, &cmode);
-	new_cmode = cmode | ENABLE_LINE_INPUT;
-	if (echo)
-		new_cmode |= ENABLE_ECHO_INPUT;
-	else
-		new_cmode &= ~ENABLE_ECHO_INPUT;
-
 	sigchain_push_common(restore_term_on_signal);
-	if (!SetConsoleMode(hconin, new_cmode)) {
+	if (!SetConsoleMode(hconin, cmode & (~ENABLE_ECHO_INPUT))) {
 		CloseHandle(hconin);
 		hconin = INVALID_HANDLE_VALUE;
 		return -1;
 	}
 
 	return 0;
-}
-
-static int disable_echo(void)
-{
-	return set_echo(0);
 }
 
 static char *shell_prompt(const char *prompt, int echo)
@@ -183,8 +169,6 @@ char *git_terminal_prompt(const char *prompt, int echo)
 	if (result)
 		return result;
 
-	if (echo && set_echo(1))
-		return NULL;
 #endif
 
 	input_fh = fopen(INPUT_PATH, "r" FORCE_TEXT);
