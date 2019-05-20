7931d5456bab1ffe86c2ef4baaeb67a635a855c4
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-05-20 13:51:36 +0200

    fixup! Windows: add support for a Windows-wide configuration
    Let's avoid the triple function call; We can just store the result and
    use it three times instead.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- config.c
+++ config.c
@@ -1675,14 +1675,13 @@ static int do_git_config_sequence(const struct config_options *opts,
 
 	current_parsing_scope = CONFIG_SCOPE_SYSTEM;
 	if (git_config_system()) {
-		if (git_program_data_config() &&
-		    !access_or_die(git_program_data_config(), R_OK, 0))
-			ret += git_config_from_file(fn,
-						    git_program_data_config(),
-						    data);
-		if (!access_or_die(git_etc_gitconfig(), R_OK, 0))
-			ret += git_config_from_file(fn, git_etc_gitconfig(),
-						    data);
+		const char *program_data = git_program_data_config();
+		const char *etc = git_etc_gitconfig();
+
+		if (program_data && !access_or_die(program_data, R_OK, 0))
+			ret += git_config_from_file(fn, program_data, data);
+		if (!access_or_die(etc, R_OK, 0))
+			ret += git_config_from_file(fn, etc, data);
 	}
 
 	current_parsing_scope = CONFIG_SCOPE_GLOBAL;
