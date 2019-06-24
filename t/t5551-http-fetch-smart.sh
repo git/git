e532a90a9fe45a55158d2d1974d41b1f37408fc4
SZEDER Gábor <szeder.dev@gmail.com>
2019-06-24 14:44:46 +0200

    t5551: use 'test_i18ngrep' to check translated output
    The two tests 'invalid Content-Type rejected' and 'server-side error
    detected' in 't5551-http-fetch-smart.sh' use "plain" 'grep' to check
    that 'git clone' failed with the expected error message, but the
    messages they are checking are translated, and, consequently, these
    tests fail when the test script is run with GIT_TEST_GETTEXT_POISON
    enabled.
    
    Use 'test_i18ngrep' instead.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5551-http-fetch-smart.sh
+++ t/t5551-http-fetch-smart.sh
@@ -199,7 +199,7 @@ test_expect_success 'GIT_SMART_HTTP can disable smart http' '
 
 test_expect_success 'invalid Content-Type rejected' '
 	test_must_fail git clone $HTTPD_URL/broken_smart/repo.git 2>actual &&
-	grep "not valid:" actual
+	test_i18ngrep "not valid:" actual
 '
 
 test_expect_success 'create namespaced refs' '
@@ -466,7 +466,7 @@ test_expect_success 'GIT_TRACE_CURL_NO_DATA prevents data from being traced' '
 
 test_expect_success 'server-side error detected' '
 	test_must_fail git clone $HTTPD_URL/error_smart/repo.git 2>actual &&
-	grep "server-side error" actual
+	test_i18ngrep "server-side error" actual
 '
 
 test_done
