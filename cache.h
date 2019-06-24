cd1096b2820d64d5fcb5da20613d5af290e42929
SZEDER Gábor <szeder.dev@gmail.com>
2019-06-24 20:13:16 +0200

    pager: add a helper function to clear the last line in the terminal
    There are a couple of places where we want to clear the last line on
    the terminal, e.g. when a progress bar line is overwritten by a
    shorter line, then the end of that progress line would remain visible,
    unless we cover it up.
    
    In 'progress.c' we did this by always appending a fixed number of
    space characters to the next line (even if it was not shorter than the
    previous), but as it turned out that fixed number was not quite large
    enough, see the fix in 9f1fd84e15 (progress: clear previous progress
    update dynamically, 2019-04-12).  From then on we've been keeping
    track of the length of the last displayed progress line and appending
    the appropriate number of space characters to the next line, if
    necessary, but, alas, this approach turned out to be error prone, see
    the fix in 1aed1a5f25 (progress: avoid empty line when breaking the
    progress line, 2019-05-19).  The next patch in this series is about to
    fix a case where we don't clear the last line, and on occasion do end
    up with such garbage at the end of the line.  It would be great if we
    could do that without the need to deal with that without meticulously
    computing the necessary number of space characters.
    
    So add a helper function to clear the last line on the terminal using
    an ANSI escape sequence, which has the advantage to clear the whole
    line no matter how wide it is, even after the terminal width changed.
    Such an escape sequence is not available on dumb terminals, though, so
    in that case fall back to simply print a whole terminal width (as
    reported by term_columns()) worth of space characters.
    
    In 'editor.c' launch_specified_editor() already used this ANSI escape
    sequence, so replace it with a call to this function.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1759,6 +1759,7 @@ void setup_pager(void);
 int pager_in_use(void);
 extern int pager_use_color;
 int term_columns(void);
+void term_clear_line(void);
 int decimal_width(uintmax_t);
 int check_pager_config(const char *cmd);
 void prepare_pager_args(struct child_process *, const char *pager);
