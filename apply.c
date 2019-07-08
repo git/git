570fe9911b6eb5c64f22a54eb3a7590b7ba5f854
Thomas Gummerer <t.gummerer@gmail.com>
2019-07-08 17:33:05 +0100

    apply: only pass required data to check_header_line
    Currently the 'check_header_line()' function takes 'struct
    apply_state' as parameter, even though it only needs the linenr from
    that struct.
    
    This function is in the callchain of 'parse_git_header()', which we
    want to make more generally useful in a subsequent commit.  To make
    that happen we only want to pass in the required data to
    'parse_git_header()', and not the whole 'struct apply_state', and thus
    we want functions in the callchain of 'parse_git_header()' to only
    take arguments they really need.
    
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- apply.c
+++ apply.c
@@ -1302,15 +1302,15 @@ static char *git_header_name(int p_value,
 	}
 }
 
-static int check_header_line(struct apply_state *state, struct patch *patch)
+static int check_header_line(int linenr, struct patch *patch)
 {
 	int extensions = (patch->is_delete == 1) + (patch->is_new == 1) +
 			 (patch->is_rename == 1) + (patch->is_copy == 1);
 	if (extensions > 1)
 		return error(_("inconsistent header lines %d and %d"),
-			     patch->extension_linenr, state->linenr);
+			     patch->extension_linenr, linenr);
 	if (extensions && !patch->extension_linenr)
-		patch->extension_linenr = state->linenr;
+		patch->extension_linenr = linenr;
 	return 0;
 }
 
@@ -1380,7 +1380,7 @@ static int parse_git_header(struct apply_state *state,
 			res = p->fn(state, line + oplen, patch);
 			if (res < 0)
 				return -1;
-			if (check_header_line(state, patch))
+			if (check_header_line(state->linenr, patch))
 				return -1;
 			if (res > 0)
 				return offset;
