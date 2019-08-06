58ebccb478b34a1babf2719fd64e3adb3974139c
Jeff King <peff@peff.net>
2019-08-06 08:26:45 -0400

    t1309: use short branch name in includeIf.onbranch test
    Commit 85fe0e800c (config: work around bug with includeif:onbranch and
    early config, 2019-07-31) tests that our early config-reader does not
    access the file mentioned by includeIf.onbranch:refs/heads/master.path.
    But it would never do so even if the feature were implemented, since the
    onbranch matching code uses the short refname "master".
    
    The test still serves its purpose, since the bug fixed by 85fe0e800c is
    actually that we hit a BUG() before even deciding whether to match the
    ref. But let's use the correct name to avoid confusion (and which we'll
    eventually want to trigger once we do the "real" fix described in that
    commit).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1309-early-config.sh
+++ t/t1309-early-config.sh
@@ -91,7 +91,7 @@ test_expect_failure 'ignore .git/ with invalid config' '
 
 test_expect_success 'early config and onbranch' '
 	echo "[broken" >broken &&
-	test_with_config "[includeif \"onbranch:refs/heads/master\"]path=../broken"
+	test_with_config "[includeif \"onbranch:master\"]path=../broken"
 '
 
 test_done
