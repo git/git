c9b9c09dae175f75bed4363cc6278c3f0cb3b9dd
René Scharfe <l.s.r@web.de>
2019-08-07 15:08:51 +0200

    nedmalloc: do assignments only after the declaration section
    Avoid the following compiler warning:
    
    In file included from compat/nedmalloc/nedmalloc.c:63:
    compat/nedmalloc/malloc.c.h: In function ‘pthread_release_lock’:
    compat/nedmalloc/malloc.c.h:1759:5: error: ISO C90 forbids mixed declarations and code [-Werror=declaration-after-statement]
     1759 |     volatile unsigned int* lp = &sl->l;
          |     ^~~~~~~~
    
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/nedmalloc/malloc.c.h
+++ compat/nedmalloc/malloc.c.h
@@ -1755,10 +1755,10 @@ static FORCEINLINE void pthread_release_lock (MLOCK_T *sl) {
   assert(sl->l != 0);
   assert(sl->threadid == CURRENT_THREAD);
   if (--sl->c == 0) {
-    sl->threadid = 0;
     volatile unsigned int* lp = &sl->l;
     int prev = 0;
     int ret;
+    sl->threadid = 0;
     __asm__ __volatile__ ("lock; xchgl %0, %1"
 			  : "=r" (ret)
 			  : "m" (*(lp)), "0"(prev)
