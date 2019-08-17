10f751c06bb3dbc2afed0e80ecfbd6242e12d538
Elijah Newren <newren@gmail.com>
2019-08-17 11:41:27 -0700

    merge-recursive: remove another implicit dependency on the_repository
    Commit d7cf3a96e9a0 ("merge-recursive.c: remove implicit dependency on
    the_repository", 2019-01-12) and follow-ups like commit 34e7771bc644
    ("Use the right 'struct repository' instead of the_repository",
    2019-06-27), removed most implicit uses of the_repository.  Convert
    calls to get_commit_tree() to instead use repo_get_commit_tree() to get
    rid of another.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -3587,8 +3587,11 @@ int merge_recursive(struct merge_options *opt,
 		repo_read_index(opt->repo);
 
 	opt->ancestor = ancestor_name;
-	clean = merge_trees(opt, get_commit_tree(h1), get_commit_tree(h2),
-			    get_commit_tree(merged_common_ancestors),
+	clean = merge_trees(opt,
+			    repo_get_commit_tree(opt->repo, h1),
+			    repo_get_commit_tree(opt->repo, h2),
+			    repo_get_commit_tree(opt->repo,
+						 merged_common_ancestors),
 			    &mrtree);
 	strbuf_release(&merge_base_abbrev);
 	if (clean < 0) {
