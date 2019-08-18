365fc561efd215a0b730923f412804eb7743e6ee
pi1024e <doremylover123@gmail.com>
2019-08-18 10:49:07 -0400

    Fix

--- builtin/am.c
+++ builtin/am.c
@@ -540,9 +540,7 @@ static int is_mail(FILE *fp)
 	if (regcomp(&regex, header_regex, REG_NOSUB | REG_EXTENDED))
 		die("invalid pattern: %s", header_regex);
 
-	while (!strbuf_getline(&sb, fp)) {
-		if (!sb.len)
-			break; /* End of header */
+	while (!strbuf_getline(&sb, fp) && sb.len) {
 
 		/* Ignore indented folded lines */
 		if (*sb.buf == '\t' || *sb.buf == ' ')
