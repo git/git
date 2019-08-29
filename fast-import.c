e075dba3723875f478654068609f69b2a5af8566
Jeff King <peff@peff.net>
2019-08-29 13:07:04 -0400

    fast-import: stop creating leading directories for import-marks
    When asked to import marks from "subdir/file.marks", we create the
    leading directory "subdir" if it doesn't exist. This makes no sense for
    importing marks, where we only ever open the path for reading.
    
    Most of the time this would be a noop, since if the marks file exists,
    then the leading directories exist, too. But if it doesn't (e.g.,
    because --import-marks-if-exists was used), then we'd create the useless
    directory.
    
    This dates back to 580d5f83e7 (fast-import: always create marks_file
    directories, 2010-03-29). Even then it was useless, so it seems to have
    been added in error alongside the --export-marks case (which _is_
    helpful).
    
    Signed-off-by: Jeff King <peff@peff.net>

--- fast-import.c
+++ fast-import.c
@@ -3228,7 +3228,6 @@ static void option_import_marks(const char *marks,
 	}
 
 	import_marks_file = make_fast_import_path(marks);
-	safe_create_leading_directories_const(import_marks_file);
 	import_marks_file_from_stream = from_stream;
 	import_marks_file_ignore_missing = ignore_missing;
 }
