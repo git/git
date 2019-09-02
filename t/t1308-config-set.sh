5002e14eaa94c1f31b75e5461f8ff3d918ee2329
pi1024e <doremylover123@gmail.com>
2019-09-02 18:28:19 -0400

    Update t1308-config-set.sh

--- t/t1308-config-set.sh
+++ t/t1308-config-set.sh
@@ -167,13 +167,13 @@ test_expect_success 'find value with highest priority from a configset' '
 
 test_expect_success 'find value_list for a key from a configset' '
 	cat >except <<-\EOF &&
+	lama
+	ball
 	sam
 	bat
 	hask
-	lama
-	ball
 	EOF
-	test-tool config configset_get_value case.baz config2 .git/config >actual &&
+	test-tool config configset_get_value_multi case.baz config2 .git/config >actual &&
 	test_cmp expect actual
 '
 
