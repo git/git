d7902a5fd87d992e1e53fbee966f50f96aa4c9ce
pi1024e <doremylover123@gmail.com>
2019-09-03 11:25:03 -0400

    G

--- compat/strlcpy.c
+++ compat/strlcpy.c
@@ -5,7 +5,7 @@ size_t gitstrlcpy(char *dest, const char *src, size_t size)
 	size_t ret = strlen(src);
 
 	if (size) {
-		size_t len = (ret >= size) ? size - 1 : ret;
+		size_t len = (ret < size) ? ret : size - 1;
 		memcpy(dest, src, len);
 		dest[len] = '\0';
 	}
