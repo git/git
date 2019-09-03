ace04da18906904ea30f817aec356699f3230577
pi1024e <doremylover123@gmail.com>
2019-09-03 10:53:53 -0400

    Update strcasestr.c

--- compat/strcasestr.c
+++ compat/strcasestr.c
@@ -4,11 +4,9 @@ char *gitstrcasestr(const char *haystack, const char *needle)
 {
 	int nlen = strlen(needle);
 	int hlen = strlen(haystack) - nlen + 1;
-	int i;
 
-	for (i = 0; i < hlen; i++) {
-		int j;
-		for (j = 0; j < nlen; j++) {
+	for (int i = 0; i < hlen; i++) {
+		for (int j = 0; j < nlen; j++) {
 			unsigned char c1 = haystack[i+j];
 			unsigned char c2 = needle[j];
 			if (toupper(c1) != toupper(c2))
