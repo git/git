5a520e48366e04c1a8069950569129ff5c5a1ca0
pi1024e <doremylover123@gmail.com>
2019-09-03 09:51:19 -0400

    Revert "Update t1308-config-set.sh"
    This reverts commit 70029aa644322a17d13a733fe01f0bbfed2d5f8a.

--- t/t1308-config-set.sh
+++ t/t1308-config-set.sh
@@ -166,13 +166,14 @@ test_expect_success 'find value with highest priority from a configset' '
 '
 
 test_expect_success 'find value_list for a key from a configset' '
-	cat >expect <<-\EOF &&
-	lama
-	ball
+	cat >except <<-\EOF &&
+	sam
 	bat
 	hask
+	lama
+	ball
 	EOF
-	test-tool config configset_get_value_multi case.baz config2 .git/config >actual &&
+	test-tool config configset_get_value case.baz config2 .git/config >actual &&
 	test_cmp expect actual
 '
 
