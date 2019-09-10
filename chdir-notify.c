a382d272738f330bdf24873b5631985fe0331af9
pi1024e <doremylover123@gmail.com>
2019-09-10 13:43:37 -0400

    Finished

--- chdir-notify.c
+++ chdir-notify.c
@@ -11,8 +11,7 @@ struct chdir_notify_entry {
 };
 static LIST_HEAD(chdir_notify_entries);
 
-void chdir_notify_register(const char *name,
-			   chdir_notify_callback cb,
+void chdir_notify_register(const char *name, chdir_notify_callback cb,
 			   void *data)
 {
 	struct chdir_notify_entry *e = xmalloc(sizeof(*e));
@@ -22,25 +21,21 @@ void chdir_notify_register(const char *name,
 	list_add_tail(&e->list, &chdir_notify_entries);
 }
 
-static void reparent_cb(const char *name,
-			const char *old_cwd,
-			const char *new_cwd,
-			void *data)
+static void reparent_cb(const char *name, const char *old_cwd,
+			const char *new_cwd, void *data)
 {
-
+	char **path = data;
 	char *tmp = *path;
 
-    if (tmp) {
-        char **path = data;
-        *path = reparent_relative_path(old_cwd, new_cwd, tmp);
-        free(tmp);
+	if (!tmp)
+		return;
+	*path = reparent_relative_path(old_cwd, new_cwd, tmp);
+	free(tmp);
 
-        if (name) {
-            trace_printf_key(&trace_setup_key,
-                             "setup: reparent %s to '%s'",
-                             name, *path);
-        }
-    }
+	if (name) {
+		trace_printf_key(&trace_setup_key, "setup: reparent %s to '%s'",
+				 name, *path);
+	}
 }
 
 void chdir_notify_reparent(const char *name, char **path)
@@ -62,11 +57,10 @@ int chdir_notify(const char *new_cwd)
 		return -1;
 	}
 
-	trace_printf_key(&trace_setup_key,
-			 "setup: chdir from '%s' to '%s'",
+	trace_printf_key(&trace_setup_key, "setup: chdir from '%s' to '%s'",
 			 old_cwd.buf, new_cwd);
 
-	list_for_each(pos, &chdir_notify_entries) {
+	list_for_each (pos, &chdir_notify_entries) {
 		struct chdir_notify_entry *e =
 			list_entry(pos, struct chdir_notify_entry, list);
 		e->cb(e->name, old_cwd.buf, new_cwd, e->data);
@@ -76,8 +70,7 @@ int chdir_notify(const char *new_cwd)
 	return 0;
 }
 
-char *reparent_relative_path(const char *old_cwd,
-			     const char *new_cwd,
+char *reparent_relative_path(const char *old_cwd, const char *new_cwd,
 			     const char *path)
 {
 	char *ret, *full;
