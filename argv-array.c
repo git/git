f7d9b070f9dccd5b6442c74c0a97d375aa91ff40
pi1024e <doremylover123@gmail.com>
2019-09-10 14:01:52 -0400

    No miced code

--- argv-array.c
+++ argv-array.c
@@ -102,7 +102,9 @@ void argv_array_clear(struct argv_array *array)
 const char **argv_array_detach(struct argv_array *array) {
     if (array->argv == empty_argv)
         return xcalloc(1, sizeof(const char *));
-    const char **ret = array->argv;
-    argv_array_init(array);
-    return ret;
+    else {
+        const char **ret = array->argv;
+        argv_array_init(array);
+        return ret;
+    }
 }
