a155285cce39c1fa61a28121f1203ed25c0ca053
pi1024e <doremylover123@gmail.com>
2019-09-10 15:27:15 -0400

    Fix

--- notes-utils.c
+++ notes-utils.c
@@ -103,72 +103,71 @@ static int notes_rewrite_config(const char *k, const char *v, void *cb)
 			return 1;
 		}
 	} else if (!c->refs_from_env && !strcmp(k, "notes.rewriteref")) {
-		/* note that a refs/ prefix is implied in the
-		 * underlying for_each_glob_ref */
-		if (starts_with(v, "refs/notes/"))
-			string_list_add_refs_by_glob(c->refs, v);
-		else
-			warning(_("Refusing to rewrite notes in %s"
-				  " (outside of refs/notes/)"),
-				v);
-	}
+        /* note that a refs/ prefix is implied in the
+         * underlying for_each_glob_ref */
+        if (starts_with(v, "refs/notes/"))
+            string_list_add_refs_by_glob(c->refs, v);
+        else
+            warning(_("Refusing to rewrite notes in %s"
+                      " (outside of refs/notes/)"),
+                    v);
+    }
 	return 0;
 }
 
-struct notes_rewrite_cfg *init_copy_notes_for_rewrite(const char *cmd)
-{
-	struct notes_rewrite_cfg *c = xmalloc(sizeof(struct notes_rewrite_cfg));
-	const char *rewrite_mode_env =
-		getenv(GIT_NOTES_REWRITE_MODE_ENVIRONMENT);
-	const char *rewrite_refs_env =
-		getenv(GIT_NOTES_REWRITE_REF_ENVIRONMENT);
-	c->cmd = cmd;
-	c->enabled = 1;
-	c->combine = combine_notes_concatenate;
-	c->refs = xcalloc(1, sizeof(struct string_list));
-	c->refs->strdup_strings = 1;
-	c->refs_from_env = 0;
-	c->mode_from_env = 0;
-	if (rewrite_mode_env) {
-		c->mode_from_env = 1;
-		c->combine = parse_combine_notes_fn(rewrite_mode_env);
-		if (!c->combine)
-			/*
-			 * TRANSLATORS: The first %s is the name of
-			 * the environment variable, the second %s is
-			 * its value.
-			 */
-			error(_("Bad %s value: '%s'"),
-			      GIT_NOTES_REWRITE_MODE_ENVIRONMENT,
-			      rewrite_mode_env);
-	}
-	if (rewrite_refs_env) {
-		c->refs_from_env = 1;
-		string_list_add_refs_from_colon_sep(c->refs, rewrite_refs_env);
-	}
-	git_config(notes_rewrite_config, c);
-	if (!c->enabled || !c->refs->nr) {
-		string_list_clear(c->refs, 0);
-		free(c->refs);
-		free(c);
-		return NULL;
-	}
-	c->trees = load_notes_trees(c->refs, NOTES_INIT_WRITABLE);
-	string_list_clear(c->refs, 0);
-	free(c->refs);
-	return c;
+struct notes_rewrite_cfg *init_copy_notes_for_rewrite(const char *cmd) {
+    struct notes_rewrite_cfg *c = xmalloc(sizeof(struct notes_rewrite_cfg));
+    const char *rewrite_mode_env =
+            getenv(GIT_NOTES_REWRITE_MODE_ENVIRONMENT);
+    const char *rewrite_refs_env =
+            getenv(GIT_NOTES_REWRITE_REF_ENVIRONMENT);
+    c->cmd = cmd;
+    c->enabled = 1;
+    c->combine = combine_notes_concatenate;
+    c->refs = xcalloc(1, sizeof(struct string_list));
+    c->refs->strdup_strings = 1;
+    c->refs_from_env = 0;
+    c->mode_from_env = 0;
+    if (rewrite_mode_env) {
+        c->mode_from_env = 1;
+        c->combine = parse_combine_notes_fn(rewrite_mode_env);
+        if (!c->combine)
+            /*
+             * TRANSLATORS: The first %s is the name of
+             * the environment variable, the second %s is
+             * its value.
+             */
+            error(_("Bad %s value: '%s'"),
+                  GIT_NOTES_REWRITE_MODE_ENVIRONMENT,
+                  rewrite_mode_env);
+    }
+    if (rewrite_refs_env) {
+        c->refs_from_env = 1;
+        string_list_add_refs_from_colon_sep(c->refs, rewrite_refs_env);
+    }
+    git_config(notes_rewrite_config, c);
+    if (c->enabled && c->refs->nr) {
+        c->trees = load_notes_trees(c->refs, NOTES_INIT_WRITABLE);
+        string_list_clear(c->refs, 0);
+        free(c->refs);
+        return c;
+    }
+
+    string_list_clear(c->refs, 0);
+    free(c->refs);
+    free(c);
+    return NULL;
 }
 
 int copy_note_for_rewrite(struct notes_rewrite_cfg *c,
 			  const struct object_id *from_obj,
-			  const struct object_id *to_obj)
-{
-	int ret = 0;
-	int i;
-	for (i = 0; c->trees[i]; i++)
-		ret = copy_note(c->trees[i], from_obj, to_obj, 1, c->combine) ||
-		      ret;
-	return ret;
+			  const struct object_id *to_obj) {
+    int ret = 0;
+    int i;
+    for (i = 0; c->trees[i]; i++)
+        ret = copy_note(c->trees[i], from_obj, to_obj, 1, c->combine) ||
+              ret;
+    return ret;
 }
 
 void finish_copy_notes_for_rewrite(struct repository *r,
