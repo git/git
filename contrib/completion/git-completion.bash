1f9247a3bd809b770bf9acfa0877467621a76d69
James Shubin <james@shubin.ca>
2019-11-13 00:47:52 +0000

    completion: tab-complete "git svn --recursive"
    Signed-off-by: James Shubin <james@shubin.ca>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2730,6 +2730,7 @@ _git_svn ()
 			--log-window-size= --no-checkout --quiet
 			--repack-flags --use-log-author --localtime
 			--add-author-from
+			--recursive
 			--ignore-paths= --include-paths= $remote_opts
 			"
 		local init_opts="
