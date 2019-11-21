df6d3d6802abcb89a30f4808fcca3bfe1485ce34
Denton Liu <liu.denton@gmail.com>
2019-11-20 16:45:48 -0800

    lib-bash.sh: move `then` onto its own line
    The code style for tests is to have statements on their own line if
    possible. Move the `then` onto its own line so that it conforms with the
    test style.
    
    Signed-off-by: Denton Liu <liu.denton@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/lib-bash.sh
+++ t/lib-bash.sh
@@ -2,10 +2,12 @@
 # to run under Bash; primarily intended for tests of the completion
 # script.
 
-if test -n "$BASH" && test -z "$POSIXLY_CORRECT"; then
+if test -n "$BASH" && test -z "$POSIXLY_CORRECT"
+then
 	# we are in full-on bash mode
 	true
-elif type bash >/dev/null 2>&1; then
+elif type bash >/dev/null 2>&1
+then
 	# execute in full-on bash mode
 	unset POSIXLY_CORRECT
 	exec bash "$0" "$@"
