ffa1f28fea000312ac917874db40f7b298458ef5
Mike Hommey <mh@glandium.org>
2019-11-22 17:37:03 +0900

    revision: clear the topo-walk flags in reset_revision_walk
    Not doing so can lead to wrong topo-walks when using the revision walk API
    consecutively.
    
    Signed-off-by: Mike Hommey <mh@glandium.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -3088,7 +3088,7 @@ static void set_children(struct rev_info *revs)
 
 void reset_revision_walk(void)
 {
-	clear_object_flags(SEEN | ADDED | SHOWN);
+	clear_object_flags(SEEN | ADDED | SHOWN | TOPO_WALK_EXPLORED | TOPO_WALK_INDEGREE);
 }
 
 static int mark_uninteresting(const struct object_id *oid,
