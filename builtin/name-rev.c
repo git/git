2059e79c0dac1e1e4e55733618dc6ca9d00b8aa4
René Scharfe <l.s.r@web.de>
2019-11-26 16:23:31 +0100

    name-rev: use skip_prefix() instead of starts_with()
    Let skip_prefix() advance refname to get rid of two magic numbers.
    
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Acked-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/name-rev.c
+++ builtin/name-rev.c
@@ -161,10 +161,10 @@ static const char *name_ref_abbrev(const char *refname, int shorten_unambiguous)
 {
 	if (shorten_unambiguous)
 		refname = shorten_unambiguous_ref(refname, 0);
-	else if (starts_with(refname, "refs/heads/"))
-		refname = refname + 11;
-	else if (starts_with(refname, "refs/"))
-		refname = refname + 5;
+	else if (skip_prefix(refname, "refs/heads/", &refname))
+		; /* refname already advanced */
+	else
+		skip_prefix(refname, "refs/", &refname);
 	return refname;
 }
 
