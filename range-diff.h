f8675343d79058ab5200e93e6c33058e13d7bb43
Denton Liu <liu.denton@gmail.com>
2019-12-06 12:16:26 -0800

    range-diff: mark pointers as const
    The contents pointed to by `diffopt` and `other_arg` should not be
    modified. Mark these as `const` to indicate this.
    
    Signed-off-by: Denton Liu <liu.denton@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- range-diff.h
+++ range-diff.h
@@ -13,7 +13,7 @@
  */
 int show_range_diff(const char *range1, const char *range2,
 		    int creation_factor, int dual_color,
-		    struct diff_options *diffopt,
-		    struct argv_array *other_arg);
+		    const struct diff_options *diffopt,
+		    const struct argv_array *other_arg);
 
 #endif
