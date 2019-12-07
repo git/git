147ee35558a300a7e51af5335ef59b09fd57ab7c
René Scharfe <l.s.r@web.de>
2019-12-07 12:16:04 +0100

    commit: use strbuf_add() to add a length-limited string
    This is shorter and simpler.
    
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -534,7 +534,7 @@ static void export_one(const char *var, const char *s, const char *e, int hack)
 	struct strbuf buf = STRBUF_INIT;
 	if (hack)
 		strbuf_addch(&buf, hack);
-	strbuf_addf(&buf, "%.*s", (int)(e - s), s);
+	strbuf_add(&buf, s, e - s);
 	setenv(var, buf.buf, 1);
 	strbuf_release(&buf);
 }
