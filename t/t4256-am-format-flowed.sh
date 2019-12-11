cb05d6a5ed1ecc687cfb388935381d0b8251f720
René Scharfe <l.s.r@web.de>
2019-12-11 18:50:02 +0100

    t4256: don't create unused file
    The file "stdout" has been created by the test script since its initial
    (and so far only) version added by 3aa4d81f88 (mailinfo: support
    format=flowed, 2018-08-25), but has never been used.  Get rid of it.
    
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4256-am-format-flowed.sh
+++ t/t4256-am-format-flowed.sh
@@ -11,7 +11,7 @@ test_expect_success 'setup' '
 '
 
 test_expect_success 'am with format=flowed' '
-	git am <"$TEST_DIRECTORY/t4256/1/patch" >stdout 2>stderr &&
+	git am <"$TEST_DIRECTORY/t4256/1/patch" 2>stderr &&
 	test_i18ngrep "warning: Patch sent with format=flowed" stderr &&
 	test_cmp "$TEST_DIRECTORY/t4256/1/mailinfo.c" mailinfo.c
 '
