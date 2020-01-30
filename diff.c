eb999b3295b22680a41c0d807a8b442407b1b19f
Matheus Tavares <matheus.bernardino@usp.br>
2020-01-30 17:32:17 -0300

    diff: make diff_populate_filespec() honor its repo argument
    diff_populate_filespec() takes a struct repository argument but it
    doesn't get passed down to read_object_file().
    
    Signed-off-by: Matheus Tavares <matheus.bernardino@usp.br>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- diff.c
+++ diff.c
@@ -4024,7 +4024,7 @@ int diff_populate_filespec(struct repository *r,
 				return 0;
 			}
 		}
-		s->data = read_object_file(&s->oid, &type, &s->size);
+		s->data = repo_read_object_file(r, &s->oid, &type, &s->size);
 		if (!s->data)
 			die("unable to read %s", oid_to_hex(&s->oid));
 		s->should_free = 1;
