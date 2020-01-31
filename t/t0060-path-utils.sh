5aa891387cc7482cfb92248c5e04976beec4a546
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-02-01 00:31:16 +0100

    mingw: allow `git.exe` to be used instead of the "Git wrapper"
    Git for Windows wants to add `git.exe` to the users' `PATH`, without
    cluttering the latter with unnecessary executables such as `wish.exe`.
    To that end, it invented the concept of its "Git wrapper", i.e. a tiny
    executable located in `C:\Program Files\Git\cmd\git.exe` (originally a
    CMD script) whose sole purpose is to set up a couple of environment
    variables and then spawn the _actual_ `git.exe` (which nowadays lives in
    `C:\Program Files\Git\mingw64\bin\git.exe` for 64-bit, and the obvious
    equivalent for 32-bit installations).
    
    Currently, the following environment variables are set unless already
    initialized:
    
    - `MSYSTEM`, to make sure that the MSYS2 Bash and the MSYS2 Perl
      interpreter behave as expected, and
    
    - `PLINK_PROTOCOL`, to force PuTTY's `plink.exe` to use the SSH
      protocol instead of Telnet,
    
    - `PATH`, to make sure that the `bin` folder in the user's home
      directory, as well as the `/mingw64/bin` and the `/usr/bin`
      directories are included. The trick here is that the `/mingw64/bin/`
      and `/usr/bin/` directories are relative to the top-level installation
      directory of Git for Windows (which the included Bash interprets as
      `/`, i.e. as the MSYS pseudo root directory).
    
    Using the absence of `MSYSTEM` as a tell-tale, we can detect in
    `git.exe` whether these environment variables have been initialized
    properly. Therefore we can call `C:\Program Files\Git\mingw64\bin\git`
    in-place after this change, without having to call Git through the Git
    wrapper.
    
    Obviously, above-mentioned directories must be _prepended_ to the `PATH`
    variable, otherwise we risk picking up executables from unrelated Git
    installations. We do that by constructing the new `PATH` value from
    scratch, appending `$HOME/bin` (if `HOME` is set), then the MSYS2 system
    directories, and then appending the original `PATH`.
    
    Side note: this modification of the `PATH` variable is independent of
    the modification necessary to reach the executables and scripts in
    `/mingw64/libexec/git-core/`, i.e. the `GIT_EXEC_PATH`. That
    modification is still performed by Git, elsewhere, long after making the
    changes described above.
    
    While we _still_ cannot simply hard-link `mingw64\bin\git.exe` to `cmd`
    (because the former depends on a couple of `.dll` files that are only in
    `mingw64\bin`, i.e. calling `...\cmd\git.exe` would fail to load due to
    missing dependencies), at least we can now avoid that extra process of
    running the Git wrapper (which then has to wait for the spawned
    `git.exe` to finish) by calling `...\mingw64\bin\git.exe` directly, via
    its absolute path.
    
    Testing this is in Git's test suite tricky: we set up a "new" MSYS
    pseudo-root and copy the `git.exe` file into the appropriate location,
    then verify that `MSYSTEM` is set properly, and also that the `PATH` is
    modified so that scripts can be found in `$HOME/bin`, `/mingw64/bin/`
    and `/usr/bin/`.
    
    This addresses https://github.com/git-for-windows/git/issues/2283
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t0060-path-utils.sh
+++ t/t0060-path-utils.sh
@@ -540,7 +540,8 @@ test_expect_success RUNTIME_PREFIX,CAN_EXEC_IN_PWD 'RUNTIME_PREFIX works' '
 	cp "$GIT_EXEC_PATH"/git$X pretend/bin/ &&
 	GIT_EXEC_PATH= ./pretend/bin/git here >actual &&
 	echo HERE >expect &&
-	test_cmp expect actual'
+	test_cmp expect actual
+'
 
 test_expect_success RUNTIME_PREFIX,CAN_EXEC_IN_PWD '%(prefix)/ works' '
 	mkdir -p pretend/bin &&
@@ -548,6 +549,26 @@ test_expect_success RUNTIME_PREFIX,CAN_EXEC_IN_PWD '%(prefix)/ works' '
 	git config yes.path "%(prefix)/yes" &&
 	GIT_EXEC_PATH= ./pretend/bin/git config --path yes.path >actual &&
 	echo "$(pwd)/pretend/yes" >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success MINGW 'MSYSTEM/PATH is adjusted if necessary' '
+	mkdir -p "$HOME"/bin pretend/mingw64/bin \
+		pretend/mingw64/libexec/git-core pretend/usr/bin &&
+	cp "$GIT_EXEC_PATH"/git.exe pretend/mingw64/bin/ &&
+	cp "$GIT_EXEC_PATH"/git.exe pretend/mingw64/libexec/git-core/ &&
+	echo "env | grep MSYSTEM=" | write_script "$HOME"/bin/git-test-home &&
+	echo "echo mingw64" | write_script pretend/mingw64/bin/git-test-bin &&
+	echo "echo usr" | write_script pretend/usr/bin/git-test-bin2 &&
+
+	(
+		MSYSTEM= &&
+		GIT_EXEC_PATH= &&
+		pretend/mingw64/libexec/git-core/git.exe test-home >actual &&
+		pretend/mingw64/libexec/git-core/git.exe test-bin >>actual &&
+		pretend/mingw64/bin/git.exe test-bin2 >>actual
+	) &&
+	test_write_lines MSYSTEM=$MSYSTEM mingw64 usr >expect &&
 	test_cmp expect actual
 '
 
