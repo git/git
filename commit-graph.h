282c08a9aa064dd220b4d0ea5162446cbb484148
Garima Singh <garima.singh@microsoft.com>
2020-02-05 22:56:26 +0000

    commit-graph: write Bloom filters to commit graph file
    Update the technical documentation for commit-graph-format with the formats for
    the Bloom filter index (BIDX) and Bloom filter data (BDAT) chunks. Write the
    computed Bloom filters information to the commit graph file using this format.
    
    Helped-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Garima Singh <garima.singh@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- commit-graph.h
+++ commit-graph.h
@@ -10,6 +10,7 @@
 #define GIT_TEST_COMMIT_GRAPH_DIE_ON_LOAD "GIT_TEST_COMMIT_GRAPH_DIE_ON_LOAD"
 
 struct commit;
+struct bloom_filter_settings;
 
 char *get_commit_graph_filename(const char *obj_dir);
 int open_commit_graph(const char *graph_file, int *fd, struct stat *st);
@@ -58,6 +59,10 @@ struct commit_graph {
 	const unsigned char *chunk_commit_data;
 	const unsigned char *chunk_extra_edges;
 	const unsigned char *chunk_base_graphs;
+	const unsigned char *chunk_bloom_indexes;
+	const unsigned char *chunk_bloom_data;
+
+	struct bloom_filter_settings *bloom_filter_settings;
 };
 
 struct commit_graph *load_commit_graph_one_fd_st(int fd, struct stat *st);
@@ -77,7 +82,7 @@ enum commit_graph_write_flags {
 	COMMIT_GRAPH_WRITE_SPLIT      = (1 << 2),
 	/* Make sure that each OID in the input is a valid commit OID. */
 	COMMIT_GRAPH_WRITE_CHECK_OIDS = (1 << 3),
-	COMMIT_GRAPH_WRITE_BLOOM_FILTERS = (1 << 4)
+	COMMIT_GRAPH_WRITE_BLOOM_FILTERS = (1 << 4),
 };
 
 struct split_commit_graph_opts {
