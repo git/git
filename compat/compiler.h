688d337975917079e7597c5f07fecf28818f758e
Emily Shaffer <emilyshaffer@google.com>
2020-02-20 15:34:37 -0800

    fixup compat compiler info

--- compat/compiler.h
+++ compat/compiler.h
@@ -6,22 +6,17 @@
 
 #ifdef __GLIBC__
 #include <gnu/libc-version.h>
+#endif
 
 static inline void get_compiler_info(struct strbuf *info)
 {
+#ifdef __GLIBC__
 	strbuf_addf(info, "glibc: %s\n", gnu_get_libc_version());
+#endif
+
 #ifdef __GNUC__
 	strbuf_addf(info, "gnuc: %d.%d\n", __GNUC__, __GNUC_MINOR__);
 #endif
 }
-
-#else
-
-static inline void get_compiler_info(struct strbuf *info)
-{
-	strbuf_addstr(info, "get_compiler_info() not implemented");
-}
-
-#endif
 
 #endif /* COMPILER_H */
