faaafcd3f446874b4f6bf7bf5b4e2ee7101b2a82
Han-Wen Nienhuys <hanwen@google.com>
2020-02-27 19:04:20 +0100

    Codify that for_each_rawref / for_each_ref should iterate over the "refs/" name space.
    This happens implicitly in the files/packed ref backend; making it
    explicit will simplify adding alternate ref storage backends.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- refs.c
+++ refs.c
@@ -1569,7 +1569,7 @@ static int do_for_each_ref(struct ref_store *refs, const char *prefix,
 
 int refs_for_each_ref(struct ref_store *refs, each_ref_fn fn, void *cb_data)
 {
-	return do_for_each_ref(refs, "", fn, 0, 0, cb_data);
+	return do_for_each_ref(refs, "refs/", fn, 0, 0, cb_data);
 }
 
 int for_each_ref(each_ref_fn fn, void *cb_data)
@@ -1629,8 +1629,8 @@ int for_each_namespaced_ref(each_ref_fn fn, void *cb_data)
 
 int refs_for_each_rawref(struct ref_store *refs, each_ref_fn fn, void *cb_data)
 {
-	return do_for_each_ref(refs, "", fn, 0,
-			       DO_FOR_EACH_INCLUDE_BROKEN, cb_data);
+	return do_for_each_ref(refs, "refs/", fn, 0, DO_FOR_EACH_INCLUDE_BROKEN,
+			       cb_data);
 }
 
 int for_each_rawref(each_ref_fn fn, void *cb_data)
