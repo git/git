743c226803c148a8f4e0b497fc5b304d4447a81e
Heba Waly <heba.waly@gmail.com>
2020-03-02 20:02:00 +0000

    tag: use new advice API to check visibility
    change the advise call in tag library from advise() to
    advise_if_enabled() to construct an example of the usage of
    the new API.
    
    Signed-off-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- advice.h
+++ advice.h
@@ -29,7 +29,6 @@ extern int advice_ignored_hook;
 extern int advice_waiting_for_editor;
 extern int advice_graft_file_deprecated;
 extern int advice_checkout_ambiguous_remote_branch_name;
-extern int advice_nested_tag;
 extern int advice_submodule_alternate_error_strategy_die;
 
 /*
