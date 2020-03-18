e9c7e8ec46f284d44c5c7451f1175cbf3d9ce622
Elijah Newren <newren@gmail.com>
2020-03-18 15:31:42 -0700

    sparse-checkout: use improved unpack_trees porcelain messages
    setup_unpack_trees_porcelain() provides much improved error/warning
    messages; instead of a message that assumes that there is only one path
    with a given problem despite being used by code that intentionally is
    grouping and showing errors together, it uses a message designed to be
    used with groups of paths.  For example, this transforms
    
        error: Entry '	folder1/a
    	folder2/a
        ' not uptodate. Cannot update sparse checkout.
    
    into
    
        error: Cannot update sparse checkout: the following entries are not up to date:
    	folder1/a
    	folder2/a
    
    In the past the suboptimal messages were never actually triggered
    because we would error out if the working directory wasn't clean before
    we even called unpack_trees().  The previous commit changed that,
    though, so let's use the better error messages.
    
    Reviewed-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- builtin/sparse-checkout.c
+++ builtin/sparse-checkout.c
@@ -112,7 +112,9 @@ static int update_working_directory(struct pattern_list *pl)
 
 	repo_hold_locked_index(r, &lock_file, LOCK_DIE_ON_ERROR);
 
+	setup_unpack_trees_porcelain(&o, "sparse-checkout");
 	result = update_sparsity(&o);
+	clear_unpack_trees_porcelain(&o);
 
 	if (result == UPDATE_SPARSITY_WARNINGS)
 		/*
