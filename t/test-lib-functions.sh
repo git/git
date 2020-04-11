9814d0a4ad0f9670b5959939f38dd42f9a7d83b6
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-04-11 13:40:21 +0000

    mingw: make test_path_is_hidden more robust
    This function uses Windows' system tool `attrib` to determine the state
    of the hidden flag of a file or directory.
    
    We should not actually expect the first `attrib.exe` in the PATH to
    be the one we are looking for. Or that it is in the PATH, for that
    matter.
    
    Let's use the full path to the tool instead.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib-functions.sh
+++ t/test-lib-functions.sh
@@ -1547,6 +1547,6 @@ test_bitmap_traversal () {
 # Tests for the hidden file attribute on windows
 is_hidden () {
 	# Use the output of `attrib`, ignore the absolute path
-	case "$(attrib "$1")" in *H*?:*) return 0;; esac
+	case "$("$SYSTEMROOT"/system32/attrib "$1")" in *H*?:*) return 0;; esac
 	return 1
 }
