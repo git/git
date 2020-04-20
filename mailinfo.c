2a2ff6039606b93ad08d8d377804ddcd0bc41312
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-04-21 06:54:35 +0700

    mailinfo.c: avoid strlen on strings that can contains NUL
    We're passing buffer from strbuf to reencode_string,
    which will call strlen(3) on that buffer,
    and discard the length of newly created buffer.
    Then, we compute the length of the return buffer to attach to strbuf.
    
    During this process, we introduce a discrimination between mail
    originally written in utf-8 and other encoding.
    
    * if the email was written in utf-8, we leave it as is. If there is
      a NUL character in that line, we complains loudly:
    
      	error: a NUL byte in commit log message not allowed.
    
    * if the email was written in other encoding, we truncate the data as
      the NUL character in that line, then we used the truncated line for
      the metadata.
    
    We can do better by reusing all the available information,
    and call the underlying lower level function that will be called
    indirectly by reencode_string. By doing this, we will also postpone
    the NUL character processing to the commit step, which will
    complains about the faulty metadata.
    
    Signed-off-by: Đoàn Trần Công Danh <congdanhqx@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- mailinfo.c
+++ mailinfo.c
@@ -447,19 +447,21 @@ static int convert_to_utf8(struct mailinfo *mi,
 			   struct strbuf *line, const char *charset)
 {
 	char *out;
+	size_t out_len;
 
 	if (!mi->metainfo_charset || !charset || !*charset)
 		return 0;
 
 	if (same_encoding(mi->metainfo_charset, charset))
 		return 0;
-	out = reencode_string(line->buf, mi->metainfo_charset, charset);
+	out = reencode_string_len(line->buf, line->len,
+				  mi->metainfo_charset, charset, &out_len);
 	if (!out) {
 		mi->input_error = -1;
 		return error("cannot convert from %s to %s",
 			     charset, mi->metainfo_charset);
 	}
-	strbuf_attach(line, out, strlen(out), strlen(out));
+	strbuf_attach(line, out, out_len, out_len);
 	return 0;
 }
 
