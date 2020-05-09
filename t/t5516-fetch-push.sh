e6b46721f56a3b278088f85bb1b6f17d2b2e79d2
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-05-09 16:19:06 +0200

    t5505/t5516: allow running without `.git/branches/` in the templates
    When we commit the template directory as part of `make vcxproj`, the
    `branches/` directory is not actually commited, as it is empty.
    
    Two tests were not prepared for that situation.
    
    This developer tried to get rid of the support for `.git/branches/` a
    long time ago, but that effort did not bear fruit, so the best we can do
    is work around in these here tests.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5516-fetch-push.sh
+++ t/t5516-fetch-push.sh
@@ -911,6 +911,7 @@ test_expect_success 'fetch with branches' '
 	mk_empty testrepo &&
 	git branch second $the_first_commit &&
 	git checkout second &&
+	mkdir -p testrepo/.git/branches &&
 	echo ".." > testrepo/.git/branches/branch1 &&
 	(
 		cd testrepo &&
@@ -924,6 +925,7 @@ test_expect_success 'fetch with branches' '
 
 test_expect_success 'fetch with branches containing #' '
 	mk_empty testrepo &&
+	mkdir -p testrepo/.git/branches &&
 	echo "..#second" > testrepo/.git/branches/branch2 &&
 	(
 		cd testrepo &&
@@ -938,6 +940,7 @@ test_expect_success 'fetch with branches containing #' '
 test_expect_success 'push with branches' '
 	mk_empty testrepo &&
 	git checkout second &&
+	mkdir -p .git/branches &&
 	echo "testrepo" > .git/branches/branch1 &&
 	git push branch1 &&
 	(
@@ -950,6 +953,7 @@ test_expect_success 'push with branches' '
 
 test_expect_success 'push with branches containing #' '
 	mk_empty testrepo &&
+	mkdir -p .git/branches &&
 	echo "testrepo#branch3" > .git/branches/branch2 &&
 	git push branch2 &&
 	(
