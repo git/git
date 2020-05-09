4860eaa35dc97c2259e168a49bb8dfa2405ed071
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-05-09 19:24:23 +0200

    t5505/t5516: fix white-space around redirectors
    The convention in Git project's shell scripts is to have white-space
    _before_, but not _after_ the `>` (or `<`).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5505-remote.sh
+++ t/t5505-remote.sh
@@ -835,8 +835,8 @@ test_expect_success '"remote show" does not show symbolic refs' '
 	(
 		cd three &&
 		git remote show origin >output &&
-		! grep "^ *HEAD$" < output &&
-		! grep -i stale < output
+		! grep "^ *HEAD$" <output &&
+		! grep -i stale <output
 	)
 '
 
@@ -1055,7 +1055,7 @@ test_expect_success 'migrate a remote from named file in $GIT_DIR/branches (2)' 
 		cd seven &&
 		git remote rm origin &&
 		mkdir -p .git/branches &&
-		echo "quux#foom" > .git/branches/origin &&
+		echo "quux#foom" >.git/branches/origin &&
 		git remote rename origin origin &&
 		test_path_is_missing .git/branches/origin &&
 		test "$(git config remote.origin.url)" = "quux" &&
