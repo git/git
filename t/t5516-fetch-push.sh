b62ca41311a0a606f59990f6e06d76a472d69188
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-05-09 19:24:23 +0200

    t5505/t5516: fix white-space around redirectors
    The convention in Git project's shell scripts is to have white-space
    _before_, but not _after_ the `>` (or `<`).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t5516-fetch-push.sh
+++ t/t5516-fetch-push.sh
@@ -912,7 +912,7 @@ test_expect_success 'fetch with branches' '
 	git branch second $the_first_commit &&
 	git checkout second &&
 	mkdir -p testrepo/.git/branches &&
-	echo ".." > testrepo/.git/branches/branch1 &&
+	echo ".." >testrepo/.git/branches/branch1 &&
 	(
 		cd testrepo &&
 		git fetch branch1 &&
@@ -926,7 +926,7 @@ test_expect_success 'fetch with branches' '
 test_expect_success 'fetch with branches containing #' '
 	mk_empty testrepo &&
 	mkdir -p testrepo/.git/branches &&
-	echo "..#second" > testrepo/.git/branches/branch2 &&
+	echo "..#second" >testrepo/.git/branches/branch2 &&
 	(
 		cd testrepo &&
 		git fetch branch2 &&
@@ -941,7 +941,7 @@ test_expect_success 'push with branches' '
 	mk_empty testrepo &&
 	git checkout second &&
 	mkdir -p .git/branches &&
-	echo "testrepo" > .git/branches/branch1 &&
+	echo "testrepo" >.git/branches/branch1 &&
 	git push branch1 &&
 	(
 		cd testrepo &&
@@ -954,7 +954,7 @@ test_expect_success 'push with branches' '
 test_expect_success 'push with branches containing #' '
 	mk_empty testrepo &&
 	mkdir -p .git/branches &&
-	echo "testrepo#branch3" > .git/branches/branch2 &&
+	echo "testrepo#branch3" >.git/branches/branch2 &&
 	git push branch2 &&
 	(
 		cd testrepo &&
@@ -1479,7 +1479,7 @@ EOF
 	git init no-thin &&
 	git --git-dir=no-thin/.git config receive.unpacklimit 0 &&
 	git push no-thin/.git refs/heads/main:refs/heads/foo &&
-	echo modified >> path1 &&
+	echo modified >>path1 &&
 	git commit -am modified &&
 	git repack -adf &&
 	rcvpck="git receive-pack --reject-thin-pack-for-testing" &&
