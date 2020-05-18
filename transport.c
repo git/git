54ebe590753fa74d8f62bac7211b0ef7c480a082
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2020-05-18 05:40:29 -0400

    transport: not report a non-head push as a branch
    When pushing a new reference (not a head or tag), report it as a new
    reference instead of a new branch.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport.c
+++ transport.c
@@ -500,9 +500,12 @@ static void print_ok_ref_status(struct ref *ref, int porcelain, int summary_widt
 				 porcelain, summary_width);
 	else if (is_null_oid(&ref->old_oid))
 		print_ref_status('*',
-			(starts_with(ref->name, "refs/tags/") ? "[new tag]" :
-			"[new branch]"),
-			ref, ref->peer_ref, NULL, porcelain, summary_width);
+				 (starts_with(ref->name, "refs/tags/")
+				  ? "[new tag]"
+				  : (starts_with(ref->name, "refs/heads/")
+				     ? "[new branch]"
+				     : "[new reference]")),
+				 ref, ref->peer_ref, NULL, porcelain, summary_width);
 	else {
 		struct strbuf quickref = STRBUF_INIT;
 		char type;
