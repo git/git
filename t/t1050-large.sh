793731f742ab1df28deb289714e61244e3de3b37
brian m. carlson <sandals@crustytoothpaste.net>
2020-06-19 17:55:53 +0000

    t1050: pass algorithm to index-pack when outside repo
    When outside a repository, git index-pack is unable to guess the hash
    algorithm in use for a pack, since packs don't contain any information
    on the algorithm in use. Pass an option to index-pack to help it out in
    this test.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1050-large.sh
+++ t/t1050-large.sh
@@ -12,6 +12,7 @@ file_size () {
 }
 
 test_expect_success setup '
+	test_oid_init &&
 	# clone does not allow us to pass core.bigfilethreshold to
 	# new repos, so set core.bigfilethreshold globally
 	git config --global core.bigfilethreshold 200k &&
@@ -177,7 +178,8 @@ test_expect_success 'git-show a large file' '
 
 test_expect_success 'index-pack' '
 	git clone file://"$(pwd)"/.git foo &&
-	GIT_DIR=non-existent git index-pack --strict --verify foo/.git/objects/pack/*.pack
+	GIT_DIR=non-existent git index-pack --object-format=$(test_oid algo) \
+		--strict --verify foo/.git/objects/pack/*.pack
 '
 
 test_expect_success 'repack' '
