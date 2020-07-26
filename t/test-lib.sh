b2a926330e91c3925c1597b1349d91d9017c9655
brian m. carlson <sandals@crustytoothpaste.net>
2020-07-26 19:54:21 +0000

    t: make SHA1 prerequisite depend on default hash
    Currently, the SHA1 prerequisite depends on the output of git
    hash-object.  However, in order for that to produce sane behavior, we
    must be in a repository.  If we are not, the default will remain SHA-1,
    and we'll produce wrong results if we're using SHA-256 for the testsuite
    but the test assertion starts when we're not in a repository.
    
    Check the environment variable we use for this purpose, leaving it to
    default to SHA-1 if none is specified.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -1689,7 +1689,8 @@ test_lazy_prereq CURL '
 # which will not work with other hash algorithms and tests that work but don't
 # test anything meaningful (e.g. special values which cause short collisions).
 test_lazy_prereq SHA1 '
-	test $(git hash-object /dev/null) = e69de29bb2d1d6434b8b29ae775ad8c2e48c5391
+	test $(git hash-object /dev/null) = e69de29bb2d1d6434b8b29ae775ad8c2e48c5391 &&
+	{ test -z "$GIT_DEFAULT_HASH" || test "$GIT_DEFAULT_HASH" = "sha1"; }
 '
 
 test_lazy_prereq REBASE_P '
