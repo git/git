a80b300e64e2f44b851e63a5f4f9a6714b3cffcb
Jeff King <peff@peff.net>
2020-07-28 12:38:53 -0400

    revision: add "--ignore-merges" option to counteract "-m"
    The "-m" option sets revs->ignore_merges to "0", but there's no way to
    undo it. This probably isn't something anybody overly cares about, since
    "1" is already the default, but it will serve as an escape hatch when we
    flip the default for ignore_merges to "0" in more situations.
    
    We'll also add a few extra niceties:
    
      - initialize the value to "-1" to indicate "not set", and then resolve
        it to the normal 0/1 bool in setup_revisions(). This lets any tweak
        functions, as well as setup_revisions() itself, avoid clobbering the
        user's preference (which until now they couldn't actually express).
    
      - since we now have --ignore-merges, let's add the matching
        --no-ignore-merges, which is just a synonym for "-m". In fact, it's
        simpler to just document --no-ignore-merges alongside "-m", and
        leave it implied that its opposite countermands it.
    
    The new test shows that this behaves just the same as the current
    behavior without "-m".
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/log.c
+++ builtin/log.c
@@ -599,8 +599,8 @@ static int show_tree_object(const struct object_id *oid,
 static void show_setup_revisions_tweak(struct rev_info *rev,
 				       struct setup_revision_opt *opt)
 {
-	if (rev->ignore_merges) {
-		/* There was no "-m" on the command line */
+	if (rev->ignore_merges < 0) {
+		/* There was no "-m" variant on the command line */
 		rev->ignore_merges = 0;
 		if (!rev->first_parent_only && !rev->combine_merges) {
 			/* No "--first-parent", "-c", or "--cc" */
