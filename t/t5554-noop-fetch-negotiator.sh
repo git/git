cbe566a0710b39691ca8f6cb6a764f6c553cd97b
Jonathan Tan <jonathantanmy@google.com>
2020-08-17 21:01:31 -0700

    negotiator/noop: add noop fetch negotiator
    Add a noop fetch negotiator. This is introduced to allow partial clones
    to skip the unneeded negotiation step when fetching missing objects
    using a "git fetch" subprocess. (The implementation of spawning a "git
    fetch" subprocess will be done in a subsequent patch.) But this can also
    be useful for end users, e.g. as a blunt fix for object corruption.
    
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t5554-noop-fetch-negotiator.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+test_description='test noop fetch negotiator'
+. ./test-lib.sh
+
+test_expect_success 'noop negotiator does not emit any "have"' '
+	rm -f trace &&
+
+	test_create_repo server &&
+	test_commit -C server to_fetch &&
+
+	test_create_repo client &&
+	test_commit -C client we_have &&
+
+	test_config -C client fetch.negotiationalgorithm noop &&
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client fetch "$(pwd)/server" &&
+
+	! grep "fetch> have" trace &&
+	grep "fetch> done" trace
+'
+
+test_done
