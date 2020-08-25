f165a774f6938acdeec136b9fedd42cf2bf83abc
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-08-25 12:13:26 +0200

    mingw: ignore HOMEDRIVE/HOMEPATH if it points to Windows' system directory
    Internally, Git expects the environment variable `HOME` to be set, and
    to point to the current user's home directory.
    
    This environment variable is not set by default on Windows, and
    therefore Git tries its best to construct one if it finds `HOME` unset.
    
    There are actually two different approaches Git tries: first, it looks
    at `HOMEDRIVE`/`HOMEPATH` because this is widely used in corporate
    environments with roaming profiles, and a user generally wants their
    global Git settings to be in a roaming profile.
    
    Only when `HOMEDRIVE`/`HOMEPATH` is either unset or does not point to a
    valid location, Git will fall back to using `USERPROFILE` instead.
    
    However, starting with Windows Vista, for secondary logons and services,
    the environment variables `HOMEDRIVE`/`HOMEPATH` point to Windows'
    system directory (usually `C:\Windows\system32`).
    
    That is undesirable, and that location is usually write-protected anyway.
    
    So let's verify that the `HOMEDRIVE`/`HOMEPATH` combo does not point to
    Windows' system directory before using it, falling back to `USERPROFILE`
    if it does.
    
    This fixes git-for-windows#2709
    
    Initial-Path-by: Ivan Pozdeev <vano@mail.mipt.ru>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -2587,6 +2587,18 @@ static size_t append_system_bin_dirs(char *path, size_t size)
 }
 #endif
 
+static int is_system32_path(const char *path)
+{
+	WCHAR system32[MAX_LONG_PATH], wpath[MAX_LONG_PATH];
+
+	if (xutftowcs_long_path(wpath, path) < 0 ||
+	    !GetSystemDirectoryW(system32, ARRAY_SIZE(system32)) ||
+	    _wcsicmp(system32, wpath))
+		return 0;
+
+	return 1;
+}
+
 static void setup_windows_environment(void)
 {
 	char *tmp = getenv("TMPDIR");
@@ -2627,7 +2639,8 @@ static void setup_windows_environment(void)
 			strbuf_addstr(&buf, tmp);
 			if ((tmp = getenv("HOMEPATH"))) {
 				strbuf_addstr(&buf, tmp);
-				if (is_directory(buf.buf))
+				if (!is_system32_path(buf.buf) &&
+				    is_directory(buf.buf))
 					setenv("HOME", buf.buf, 1);
 				else
 					tmp = NULL; /* use $USERPROFILE */
