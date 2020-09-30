26e28fe7bbdf8b22ed096dfd76a9311e86ffb200
Jeff King <peff@peff.net>
2020-09-30 08:30:27 -0400

    test-advise: check argument count with argc instead of argv
    We complain if "test-tool advise" is not given an argument, but we
    quietly ignore any additional arguments it receives. Let's instead check
    that we got the expected number. As a bonus, this silences
    -Wunused-parameter, which notes that we don't ever look at argc.
    
    While we're here, we can also fix the indentation in the conditional.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/helper/test-advise.c
+++ t/helper/test-advise.c
@@ -5,8 +5,8 @@
 
 int cmd__advise_if_enabled(int argc, const char **argv)
 {
-	if (!argv[1])
-	die("usage: %s <advice>", argv[0]);
+	if (argc != 2)
+		die("usage: %s <advice>", argv[0]);
 
 	setup_git_directory();
 	git_config(git_default_config, NULL);
