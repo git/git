ec7967cfafd5844e33afbc1e6fb8ba1b9e5b111b
Michał Kępień <michal@isc.org>
2020-10-20 08:48:08 +0200

    merge-base, xdiff: zero out xpparam_t structures
    xpparam_t structures are usually zero-initialized before their specific
    fields are assigned to, but there are three locations in the tree where
    that does not happen.  Add the missing memset() calls in order to make
    initialization of xpparam_t structures consistent tree-wide and to
    prevent stack garbage from being used as field values.
    
    Signed-off-by: Michał Kępień <michal@isc.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- xdiff/xpatience.c
+++ xdiff/xpatience.c
@@ -318,6 +318,8 @@ static int fall_back_to_classic_diff(struct hashmap *map,
 		int line1, int count1, int line2, int count2)
 {
 	xpparam_t xpp;
+
+	memset(&xpp, 0, sizeof(xpp));
 	xpp.flags = map->xpp->flags & ~XDF_DIFF_ALGORITHM_MASK;
 
 	return xdl_fall_back_diff(map->env, &xpp,
