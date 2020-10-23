9ce698981839211f4f2e4748ca79be23afa05a40
Sachin Kumar <62640074+ScottBen@users.noreply.github.com>
2020-10-23 12:32:07 +0530

    Update blob.c

--- blob.c
+++ blob.c
@@ -1,11 +1,11 @@
-#include "cache.h"
-#include "blob.h"
-#include "repository.h"
-#include "alloc.h"
+#include "cache.h" // Standard Libraries Included.
+#include "blob.h"    
+#include "repository.h" 
+#include "alloc.h"       
 
-const char *blob_type = "blob";
+const char *blob_type = "blob"; // Constant Defined
 
-struct blob *lookup_blob(struct repository *r, const struct object_id *oid)
+struct blob *lookup_blob(struct repository *r, const struct object_id *oid) //Structure is defined for lookup 
 {
 	struct object *obj = lookup_object(r, oid);
 	if (!obj)
@@ -13,7 +13,7 @@ struct blob *lookup_blob(struct repository *r, const struct object_id *oid)
 	return object_as_type(obj, OBJ_BLOB, 0);
 }
 
-int parse_blob_buffer(struct blob *item, void *buffer, unsigned long size)
+int parse_blob_buffer(struct blob *item, void *buffer, unsigned long size) // Function is Defined
 {
 	item->object.parsed = 1;
 	return 0;
