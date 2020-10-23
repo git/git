704fed9ea22230929639c9e5fca122045f84311d
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-10-23 14:00:00 +0000

    tests: start moving to a different default main branch name
    To allow for an incremental conversion to a new default main branch
    name, let's introduce `GIT_TEST_DEFAULT_MAIN_BRANCH_NAME`. This
    environment variable can be set at the top of each converted test
    script, overriding the default main branch name to use when initializing
    new repositories (or cloning empty repositories).
    
    Note: the `GIT_TEST_DEFAULT_MAIN_BRANCH_NAME` is _not_ intended to be
    used manually; many tests require a specific main branch name and cannot
    simply work with another one. This `GIT_TEST_*` variable is meant purely
    for the transitional period while the entire test suite is converted to
    use `main` as the initial branch name by default.
    
    We also introduce the `PREPARE_FOR_MAIN_BRANCH` prereq that determines
    whether the default main branch name is `main`, and adjust a couple of
    test functions to use it. This prereq will be used to temporarily
    disable a couple test cases to allow for adjusting the test script
    incrementally. Once an entire test is adjusted, we will adjust the test
    so that it is run with `GIT_TEST_DEFAULT_MAIN_BRANCH_NAME=main`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -1702,3 +1702,10 @@ test_lazy_prereq SHA1 '
 test_lazy_prereq REBASE_P '
 	test -z "$GIT_TEST_SKIP_REBASE_P"
 '
+# Special-purpose prereq for transitioning to a new default branch name:
+# Some tests need more than just a mindless (case-preserving) s/master/main/g
+# replacement. The non-trivial adjustments are guarded behind this
+# prerequisite, acting kind of as a feature flag
+test_lazy_prereq PREPARE_FOR_MAIN_BRANCH '
+	test "$GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME" = main
+'
