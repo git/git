bc925ce3f3c338b20b6e95b72bc40cbdc4cd3dbb
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-11-18 23:44:32 +0000

    t551*: adjust the references to the default branch name "main"
    This trick was performed via
    
    	$ (cd t &&
    	   sed -i -e 's/master/main/g' -e 's/MASTER/MAIN/g' \
    		-e 's/Master/Main/g' -- t551*.sh)
    
    This allows us to define `GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main`
    for those tests.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5518-fetch-exit-status.sh
+++ t/t5518-fetch-exit-status.sh
@@ -5,7 +5,7 @@
 
 test_description='fetch exit status test'
 
-GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=master
+GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 
 . ./test-lib.sh
@@ -20,20 +20,20 @@ test_expect_success setup '
 	echo side >file &&
 	git commit -a -m side &&
 
-	git checkout master &&
+	git checkout main &&
 	echo next >file &&
 	git commit -a -m next
 '
 
 test_expect_success 'non-fast-forward fetch' '
 
-	test_must_fail git fetch . master:side
+	test_must_fail git fetch . main:side
 
 '
 
 test_expect_success 'forced update' '
 
-	git fetch . +master:side
+	git fetch . +main:side
 
 '
 
