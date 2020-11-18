a900999d7cd74f878686933a6341e3852726165d
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-11-18 14:49:05 +0000

    t2106: adjust style to the current conventions
    We settled on the style where the test cases' code starts by the opening
    single quote being on the `test_expect_*` line, and the closing quote
    being in its own line after the code.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2106-update-index-assume-unchanged.sh
+++ t/t2106-update-index-assume-unchanged.sh
@@ -5,20 +5,22 @@ test_description='git update-index --assume-unchanged test.
 
 . ./test-lib.sh
 
-test_expect_success 'setup' \
-	': >file &&
-	 git add file &&
-	 git commit -m initial &&
-	 git branch other &&
-	 echo upstream >file &&
-	 git add file &&
-	 git commit -m upstream'
+test_expect_success 'setup' '
+	: >file &&
+	git add file &&
+	git commit -m initial &&
+	git branch other &&
+	echo upstream >file &&
+	git add file &&
+	git commit -m upstream
+'
 
-test_expect_success 'do not switch branches with dirty file' \
-	'git reset --hard &&
-	 git checkout other &&
-	 echo dirt >file &&
-	 git update-index --assume-unchanged file &&
-	 test_must_fail git checkout master'
+test_expect_success 'do not switch branches with dirty file' '
+	git reset --hard &&
+	git checkout other &&
+	echo dirt >file &&
+	git update-index --assume-unchanged file &&
+	test_must_fail git checkout master
+'
 
 test_done
