3ac8f6301ee9d4bf3f9e58b3ef12ba536005cb9d
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-11-18 23:44:33 +0000

    t55[23]*: adjust the references to the default branch name "main"
    Carefully excluding t5526, which sees independent development elsewhere
    at the time of writing, we use `main` as the default branch name in
    t55[23]*. This trick was performed via
    
    	$ (cd t &&
    	   sed -i -e 's/master/main/g' -e 's/MASTER/MAIN/g' \
    		-e 's/Master/Main/g' -e 's/naster/nain/g' -- \
    		t55[23]*.sh &&
    	   git checkout HEAD -- t5526\*)
    
    Note that t5533 contains a variation of the name `master` (`naster`)
    that we rename here, too.
    
    This commit allows us to define
    `GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main` for that range of tests.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5521-pull-options.sh
+++ t/t5521-pull-options.sh
@@ -2,7 +2,7 @@
 
 test_description='pull options'
 
-GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=master
+GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 
 . ./test-lib.sh
@@ -102,13 +102,13 @@ test_expect_success 'git pull --force' '
 	cat >>.git/config <<-\EOF &&
 	[remote "one"]
 		url = ../parent
-		fetch = refs/heads/master:refs/heads/mirror
+		fetch = refs/heads/main:refs/heads/mirror
 	[remote "two"]
 		url = ../parent
-		fetch = refs/heads/master:refs/heads/origin
-	[branch "master"]
+		fetch = refs/heads/main:refs/heads/origin
+	[branch "main"]
 		remote = two
-		merge = refs/heads/master
+		merge = refs/heads/main
 	EOF
 	git pull two &&
 	test_commit A &&
@@ -127,9 +127,9 @@ test_expect_success 'git pull --all' '
 	[remote "two"]
 		url = ../parent
 		fetch = refs/heads/*:refs/remotes/two/*
-	[branch "master"]
+	[branch "main"]
 		remote = one
-		merge = refs/heads/master
+		merge = refs/heads/main
 	EOF
 	git pull --all
 	)
@@ -142,7 +142,7 @@ test_expect_success 'git pull --dry-run' '
 		cd clonedry &&
 		git pull --dry-run ../parent &&
 		test_path_is_missing .git/FETCH_HEAD &&
-		test_path_is_missing .git/refs/heads/master &&
+		test_path_is_missing .git/refs/heads/main &&
 		test_path_is_missing .git/index &&
 		test_path_is_missing file
 	)
@@ -156,7 +156,7 @@ test_expect_success 'git pull --all --dry-run' '
 		git remote add origin ../parent &&
 		git pull --all --dry-run &&
 		test_path_is_missing .git/FETCH_HEAD &&
-		test_path_is_missing .git/refs/remotes/origin/master &&
+		test_path_is_missing .git/refs/remotes/origin/main &&
 		test_path_is_missing .git/index &&
 		test_path_is_missing file
 	)
