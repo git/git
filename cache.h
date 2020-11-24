030998a732ac54f41473b07c7376ca46b817bf4e
Han-Wen Nienhuys <hanwen@google.com>
2020-11-24 19:19:30 +0100

    move sleep_millisec to git-compat-util.h
    The sleep function is defined in wrapper.c, so it makes more sense to be a in
    system compatibility header.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- cache.h
+++ cache.h
@@ -1960,7 +1960,6 @@ int stat_validity_check(struct stat_validity *sv, const char *path);
 void stat_validity_update(struct stat_validity *sv, int fd);
 
 int versioncmp(const char *s1, const char *s2);
-void sleep_millisec(int millisec);
 
 /*
  * Create a directory and (if share is nonzero) adjust its permissions
