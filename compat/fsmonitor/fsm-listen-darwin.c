3da0588a5eb6a5e4104ae80a9a8d9a6df3d29325
Jeff Hostetler <jeffhost@microsoft.com>
2020-12-17 10:41:07 -0500

    compat/fsmonitor/fsm-listen-darwin: stub in backend for Darwin
    Stub in empty implementation of fsmonitor--daemon
    backend for Darwin (aka MacOS).
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ compat/fsmonitor/fsm-listen-darwin.c
@@ -0,0 +1,20 @@
+#include "cache.h"
+#include "fsmonitor.h"
+#include "fsm-listen.h"
+
+int fsm_listen__ctor(struct fsmonitor_daemon_state *state)
+{
+	return -1;
+}
+
+void fsm_listen__dtor(struct fsmonitor_daemon_state *state)
+{
+}
+
+void fsm_listen__stop_async(struct fsmonitor_daemon_state *state)
+{
+}
+
+void fsm_listen__loop(struct fsmonitor_daemon_state *state)
+{
+}
