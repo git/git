086dcb12c12110750ce31a690a1ccca20333b434
Jeff Hostetler <jeffhost@microsoft.com>
2020-12-17 10:29:17 -0500

    compat/fsmonitor/fsm-listen-win32: stub in backend for Windows
    Stub in empty filesystem listener backend for fsmonitor--daemon on Windows.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ compat/fsmonitor/fsm-listen-win32.c
@@ -0,0 +1,21 @@
+#include "cache.h"
+#include "config.h"
+#include "fsmonitor.h"
+#include "fsm-listen.h"
+
+void fsm_listen__stop_async(struct fsmonitor_daemon_state *state)
+{
+}
+
+void fsm_listen__loop(struct fsmonitor_daemon_state *state)
+{
+}
+
+int fsm_listen__ctor(struct fsmonitor_daemon_state *state)
+{
+	return -1;
+}
+
+void fsm_listen__dtor(struct fsmonitor_daemon_state *state)
+{
+}
