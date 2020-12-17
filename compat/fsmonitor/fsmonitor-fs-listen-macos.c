5667e8d7fe6986151744f5a290cba90a2ca2f6da
Jeff Hostetler <jeffhost@microsoft.com>
2020-12-17 10:41:07 -0500

    fsmonitor-fs-listen-macos: stub in backend for MacOS
    Stub in empty implementation of fsmonitor--daemon
    backend for MacOS.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ compat/fsmonitor/fsmonitor-fs-listen-macos.c
@@ -0,0 +1,20 @@
+#include "cache.h"
+#include "fsmonitor.h"
+#include "fsmonitor-fs-listen.h"
+
+int fsmonitor_fs_listen__ctor(struct fsmonitor_daemon_state *state)
+{
+	return -1;
+}
+
+void fsmonitor_fs_listen__dtor(struct fsmonitor_daemon_state *state)
+{
+}
+
+void fsmonitor_fs_listen__stop_async(struct fsmonitor_daemon_state *state)
+{
+}
+
+void fsmonitor_fs_listen__loop(struct fsmonitor_daemon_state *state)
+{
+}
