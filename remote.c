18f9c9884582c743d8ba04ef5cbbe647947d2578
Nipunn Koorapati <nipunn@dropbox.com>
2020-12-22 03:58:16 +0000

    negative-refspec: fix segfault on : refspec
    The logic added to check for negative pathspec match by c0192df630
    (refspec: add support for negative refspecs, 2020-09-30) looks at
    refspec->src assuming it is never NULL, however when
    remote.origin.push is set to ":", then refspec->src is NULL,
    causing a segfault within strcmp.
    
    Tell git to handle matching refspec by adding the needle to the
    set of positively matched refspecs, since matching ":" refspecs
    match anything as src.
    
    Add test for matching refspec pushes fetch-negative-refspec
    both individually and in combination with a negative refspec.
    
    Signed-off-by: Nipunn Koorapati <nipunn@dropbox.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -755,9 +755,13 @@ static int query_matches_negative_refspec(struct refspec *rs, struct refspec_ite
 
 			if (match_name_with_pattern(key, needle, value, &expn_name))
 				string_list_append_nodup(&reversed, expn_name);
-		} else {
-			if (!strcmp(needle, refspec->src))
-				string_list_append(&reversed, refspec->src);
+		} else if (refspec->matching) {
+			/* For the special matching refspec, any query should match */
+			string_list_append(&reversed, needle);
+		} else if (!refspec->src) {
+			BUG("refspec->src should not be null here");
+		} else if (!strcmp(needle, refspec->src)) {
+			string_list_append(&reversed, refspec->src);
 		}
 	}
 
