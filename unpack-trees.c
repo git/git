dd23022acbf7433514e20a14a9f74e39fec9d340
Derrick Stolee <dstolee@microsoft.com>
2021-01-23 19:58:17 +0000

    sparse-checkout: load sparse-checkout patterns
    A future feature will want to load the sparse-checkout patterns into a
    pattern_list, but the current mechanism to do so is a bit complicated.
    This is made difficult due to needing to find the sparse-checkout file
    in different ways throughout the codebase.
    
    The logic implemented in the new get_sparse_checkout_patterns() was
    duplicated in populate_from_existing_patterns() in unpack-trees.c. Use
    the new method instead, keeping the logic around handling the struct
    unpack_trees_options.
    
    The callers to get_sparse_checkout_filename() in
    builtin/sparse-checkout.c manipulate the sparse-checkout file directly,
    so it is not appropriate to replace logic in that file with
    get_sparse_checkout_patterns().
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- unpack-trees.c
+++ unpack-trees.c
@@ -1549,14 +1549,10 @@ static void mark_new_skip_worktree(struct pattern_list *pl,
 static void populate_from_existing_patterns(struct unpack_trees_options *o,
 					    struct pattern_list *pl)
 {
-	char *sparse = git_pathdup("info/sparse-checkout");
-
-	pl->use_cone_patterns = core_sparse_checkout_cone;
-	if (add_patterns_from_file_to_list(sparse, "", 0, pl, NULL) < 0)
+	if (get_sparse_checkout_patterns(pl) < 0)
 		o->skip_sparse_checkout = 1;
 	else
 		o->pl = pl;
-	free(sparse);
 }
 
 
