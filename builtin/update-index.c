cfd56faf58b60c40e930a7c688a5378e06b6da1c
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-03-05 21:20:26 +0100

    config: FSMonitor is repository-specific
    This commit refactors `git_config_get_fsmonitor()` into the `repo_*()`
    form that takes a parameter `struct repository *r`.
    
    That change prepares for the upcoming `core.useFSMonitorDaemon` flag which
    will be stored in the `repo_settings` struct.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -1214,14 +1214,14 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 	}
 
 	if (fsmonitor > 0) {
-		if (git_config_get_fsmonitor() == 0)
+		if (repo_config_get_fsmonitor(r) == 0)
 			warning(_("core.fsmonitor is unset; "
 				"set it if you really want to "
 				"enable fsmonitor"));
 		add_fsmonitor(&the_index);
 		report(_("fsmonitor enabled"));
 	} else if (!fsmonitor) {
-		if (git_config_get_fsmonitor() == 1)
+		if (repo_config_get_fsmonitor(r) == 1)
 			warning(_("core.fsmonitor is set; "
 				"remove it if you really want to "
 				"disable fsmonitor"));
