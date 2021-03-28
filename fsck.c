e35d65a78ab7520b2705b27062758ab20a60e462
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-28 15:15:39 +0200

    fsck.c: refactor fsck_msg_type() to limit scope of "int msg_type"
    Refactor "if options->msg_type" and other code added in
    0282f4dced0 (fsck: offer a function to demote fsck errors to warnings,
    2015-06-22) to reduce the scope of the "int msg_type" variable.
    
    This is in preparation for changing its type in a subsequent commit,
    only using it in the "!options->msg_type" scope makes that change
    
    This also brings the code in line with the fsck_set_msg_type()
    function (also added in 0282f4dced0), which does a similar check for
    "!options->msg_type". Another minor benefit is getting rid of the
    style violation of not having braces for the body of the "if".
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fsck.c
+++ fsck.c
@@ -167,19 +167,17 @@ void list_config_fsck_msg_ids(struct string_list *list, const char *prefix)
 static int fsck_msg_type(enum fsck_msg_id msg_id,
 	struct fsck_options *options)
 {
-	int msg_type;
-
 	assert(msg_id >= 0 && msg_id < FSCK_MSG_MAX);
 
-	if (options->msg_type)
-		msg_type = options->msg_type[msg_id];
-	else {
-		msg_type = msg_id_info[msg_id].msg_type;
+	if (!options->msg_type) {
+		int msg_type = msg_id_info[msg_id].msg_type;
+
 		if (options->strict && msg_type == FSCK_WARN)
 			msg_type = FSCK_ERROR;
+		return msg_type;
 	}
 
-	return msg_type;
+	return options->msg_type[msg_id];
 }
 
 static int parse_msg_type(const char *str)
