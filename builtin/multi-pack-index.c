cd57bc41bbcc8d260040243946d19075eff0bfaf
Taylor Blau <me@ttaylorr.com>
2021-03-30 11:04:04 -0400

    builtin/multi-pack-index.c: display usage on unrecognized command
    When given a sub-command that it doesn't understand, 'git
    multi-pack-index' dies with the following message:
    
        $ git multi-pack-index bogus
        fatal: unrecognized subcommand: bogus
    
    Instead of 'die()'-ing, we can display the usage text, which is much
    more helpful:
    
        $ git.compile multi-pack-index bogus
        error: unrecognized subcommand: bogus
        usage: git multi-pack-index [<options>] write
           or: git multi-pack-index [<options>] verify
           or: git multi-pack-index [<options>] expire
           or: git multi-pack-index [<options>] repack [--batch-size=<size>]
    
            --object-dir <file>   object directory containing set of packfile and pack-index pairs
            --progress            force progress reporting
    
    While we're at it, clean up some duplication between the "no sub-command"
    and "unrecognized sub-command" conditionals.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/multi-pack-index.c
+++ builtin/multi-pack-index.c
@@ -152,8 +152,7 @@ int cmd_multi_pack_index(int argc, const char **argv,
 		opts.object_dir = get_object_directory();
 
 	if (argc == 0)
-		usage_with_options(builtin_multi_pack_index_usage,
-				   builtin_multi_pack_index_options);
+		goto usage;
 
 	if (!strcmp(argv[0], "repack"))
 		return cmd_multi_pack_index_repack(argc, argv);
@@ -163,6 +162,10 @@ int cmd_multi_pack_index(int argc, const char **argv,
 		return cmd_multi_pack_index_verify(argc, argv);
 	else if (!strcmp(argv[0], "expire"))
 		return cmd_multi_pack_index_expire(argc, argv);
-	else
-		die(_("unrecognized subcommand: %s"), argv[0]);
+	else {
+usage:
+		error(_("unrecognized subcommand: %s"), argv[0]);
+		usage_with_options(builtin_multi_pack_index_usage,
+				   builtin_multi_pack_index_options);
+	}
 }
