900ad3b91d76e95eae351b51f666d5f79f334257
Patrick Steinhardt <ps@pks.im>
2021-04-12 16:46:49 +0200

    config: unify code paths to get global config paths
    There's two callsites which assemble global config paths, once in the
    config loading code and once in the git-config(1) builtin. We're about
    to implement a way to override global config paths via an environment
    variable which would require us to adjust both sites.
    
    Unify both code paths into a single `git_global_config()` function which
    returns both paths for `~/.gitconfig` and the XDG config file. This will
    make the subsequent patch which introduces the new envvar easier to
    implement.
    
    No functional changes are expected from this patch.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- config.h
+++ config.h
@@ -327,6 +327,7 @@ int config_error_nonbool(const char *);
 #endif
 
 char *git_system_config(void);
+void git_global_config(char **user, char **xdg);
 
 int git_config_parse_parameter(const char *, config_fn_t fn, void *data);
 
