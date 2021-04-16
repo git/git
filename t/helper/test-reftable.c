eeaf64f607ee29d416a69fb354662ea54e3149c7
Han-Wen Nienhuys <hanwen@google.com>
2021-04-16 13:43:23 +0200

    reftable: implement stack, a mutable database of reftable files.
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- t/helper/test-reftable.c
+++ t/helper/test-reftable.c
@@ -10,6 +10,7 @@ int cmd__reftable(int argc, const char **argv)
 	record_test_main(argc, argv);
 	refname_test_main(argc, argv);
 	readwrite_test_main(argc, argv);
+	stack_test_main(argc, argv);
 	tree_test_main(argc, argv);
 	return 0;
 }
