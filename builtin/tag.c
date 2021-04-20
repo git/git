844c3f0b0b5cc45f8aa8bd65c7ad407df7301c39
ZheNing Hu <adlternative@gmail.com>
2021-04-20 16:52:11 +0000

    ref-filter: reuse output buffer
    When we use `git for-each-ref`, every ref will allocate
    its own output strbuf and error strbuf. But we can reuse
    the final strbuf for each step ref's output. The error
    buffer will also be reused, despite the fact that the git
    will exit when `format_ref_array_item()` return a non-zero
    value and output the contents of the error buffer.
    
    The performance for `git for-each-ref` on the Git repository
    itself with performance testing tool `hyperfine` changes from
    23.7 ms ± 0.9 ms to 22.2 ms ± 1.0 ms. Optimization is relatively
    minor.
    
    At the same time, we apply this optimization to `git tag -l`
    and `git branch -l`.
    
    This approach is similar to the one used by 79ed0a5
    (cat-file: use a single strbuf for all output, 2018-08-14)
    to speed up the cat-file builtin.
    
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Helped-by: Jeff King <peff@peff.net>
    Helped-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/tag.c
+++ builtin/tag.c
@@ -39,6 +39,8 @@ static int list_tags(struct ref_filter *filter, struct ref_sorting *sorting,
 		     struct ref_format *format)
 {
 	struct ref_array array;
+	struct strbuf output = STRBUF_INIT;
+	struct strbuf err = STRBUF_INIT;
 	char *to_free = NULL;
 	int i;
 
@@ -64,17 +66,16 @@ static int list_tags(struct ref_filter *filter, struct ref_sorting *sorting,
 	ref_array_sort(sorting, &array);
 
 	for (i = 0; i < array.nr; i++) {
-		struct strbuf output = STRBUF_INIT;
-		struct strbuf err = STRBUF_INIT;
-
+		strbuf_reset(&output);
+		strbuf_reset(&err);
 		if (format_ref_array_item(array.items[i], format, &output, &err))
 			die("%s", err.buf);
 		fwrite(output.buf, 1, output.len, stdout);
 		putchar('\n');
-
-		strbuf_release(&err);
-		strbuf_release(&output);
 	}
+
+	strbuf_release(&err);
+	strbuf_release(&output);
 	ref_array_clear(&array);
 	free(to_free);
 
