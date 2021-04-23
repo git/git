7e8181e77d409af7595e357ad233b7781e026b78
Han-Wen Nienhuys <hanwen@google.com>
2021-04-23 12:02:03 +0200

    refs: remove EINVAL specification from the errno sideband in read_raw_ref_fn
    A grep for EINVAL */*c reveals that no code inspects EINVAL after reading
    references.
    
    The files ref backend does use EINVAL so parse_loose_ref_contents() can
    communicate to lock_raw_ref() about garbage following the hex SHA1, or a short
    read in files_read_raw_ref(), but the files backend does not call into
    refs_read_raw_ref(), so its EINVAL sideband error is unused.
    
    As the errno sideband is unintuitive and error-prone, remove EINVAL
    value, as a step towards getting rid of the errno sideband altogether.
    
    Spotted by Ævar Arnfjörð Bjarmason <avarab@gmail.com>.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

--- refs/refs-internal.h
+++ refs/refs-internal.h
@@ -617,11 +617,10 @@ typedef int reflog_expire_fn(struct ref_store *ref_store,
  * properly-formatted or even safe reference name. NEITHER INPUT NOR
  * OUTPUT REFERENCE NAMES ARE VALIDATED WITHIN THIS FUNCTION.
  *
- * Return 0 on success. If the ref doesn't exist, set errno to ENOENT
- * and return -1. If the ref exists but is neither a symbolic ref nor
- * an object ID, it is broken; set REF_ISBROKEN in type, set errno to
- * EINVAL, and return -1. If there is another error reading the ref,
- * set errno appropriately and return -1.
+ * Return 0 on success. If the ref doesn't exist, set errno to ENOENT and return
+ * -1. If the ref exists but is neither a symbolic ref nor an object ID, it is
+ * broken; set REF_ISBROKEN in type, and return -1. If there is another error
+ * reading the ref, set errno appropriately and return -1.
  *
  * Backend-specific flags might be set in type as well, regardless of
  * outcome.
