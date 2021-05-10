97c8aac9c5fd54b5b6092d7ae5d3289ce5d9afe8
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-05-10 16:19:09 +0200

    test-lib: do not show advice about init.defaultBranch under --verbose
    Arrange for the advice about naming the initial branch not to be shown
    in the --verbose output of the test suite.
    
    Since 675704c74dd (init: provide useful advice about
    init.defaultBranch, 2020-12-11) some tests have been very chatty with
    repeated occurrences of this multi-line advice. Having it be this
    verbose isn't helpful for anyone in the context of git's own test
    suite, and it makes debugging tests that use their own "git init"
    invocations needlessly distracting.
    
    By setting the GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME variable early in
    test-lib.sh itself we'll squash the warning not only for
    test_create_repo(), as 675704c74dd explicitly intended, but also for
    other "git init" invocations.
    
    And once we'd like to have this configuration set for all "git init"
    invocations in the test suite we can get rid of the init.defaultBranch
    configuration setting in test_create_repo(), as
    repo_default_branch_name() in refs.c will take the GIT_TEST_* variable
    over it being set.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -64,6 +64,11 @@ then
 	export GIT_TEST_DISALLOW_ABBREVIATED_OPTIONS
 fi
 
+# Explicitly set the default branch name for testing, to avoid the
+# transitory "git init" warning under --verbose.
+: ${GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME:=master}
+export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+
 ################################################################
 # It appears that people try to run tests without building...
 "${GIT_TEST_INSTALLED:-$GIT_BUILD_DIR}/git$X" >/dev/null
