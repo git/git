b0144836d53d8d564143e7dbb52f40644ffe00e0
Jeff Hostetler <jeffhost@microsoft.com>
2021-06-23 10:18:17 -0400

    fsmonitor-settings: bare repos are incompatible with FSMonitor
    Bare repos do not have a worktree, so there is nothing for the
    daemon watch.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- fsmonitor-settings.h
+++ fsmonitor-settings.h
@@ -4,9 +4,18 @@
 struct repository;
 
 enum fsmonitor_mode {
+	FSMONITOR_MODE_INCOMPATIBLE = -1, /* see _reason */
 	FSMONITOR_MODE_DISABLED = 0,
 	FSMONITOR_MODE_HOOK = 1, /* core.fsmonitor=<hook_path> */
 	FSMONITOR_MODE_IPC = 2,  /* core.fsmonitor=<true> */
+};
+
+/*
+ * Incompatibility reasons.
+ */
+enum fsmonitor_reason {
+	FSMONITOR_REASON_OK = 0, /* no incompatibility or when disbled */
+	FSMONITOR_REASON_BARE,
 };
 
 void fsm_settings__set_ipc(struct repository *r);
@@ -15,6 +24,9 @@ void fsm_settings__set_disabled(struct repository *r);
 
 enum fsmonitor_mode fsm_settings__get_mode(struct repository *r);
 const char *fsm_settings__get_hook_path(struct repository *r);
+
+enum fsmonitor_reason fsm_settings__get_reason(struct repository *r);
+int fsm_settings__error_if_incompatible(struct repository *r);
 
 struct fsmonitor_settings;
 
