d1c5ae78ce1260c94c9e626b83dc0901e6843178
brian m. carlson <sandals@crustytoothpaste.net>
2021-07-11 21:55:10 +0000

    rev-list: add option for --pretty=format without header
    In general, we encourage users to use plumbing commands, like git
    rev-list, over porcelain commands, like git log, when scripting.
    However, git rev-list has one glaring problem that prevents it from
    being used in certain cases: when --pretty is used with a custom format,
    it always prints out a line containing "commit" and the object ID.  This
    makes it unsuitable for many scripting needs, and forces users to use
    git log instead.
    
    While we can't change this behavior for backwards compatibility, we can
    add an option to suppress this behavior, so let's do so, and call it
    "--no-commit-header".  Additionally, add the corresponding positive
    option to switch it back on.
    
    Note that this option doesn't affect the built-in formats, only custom
    formats.  This is exactly the same behavior as users already have from
    git log and is what most users will be used to.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.h
+++ revision.h
@@ -215,7 +215,8 @@ struct rev_info {
 			missing_newline:1,
 			date_mode_explicit:1,
 			preserve_subject:1,
-			encode_email_headers:1;
+			encode_email_headers:1,
+			include_header:1;
 	unsigned int	disable_stdin:1;
 	/* --show-linear-break */
 	unsigned int	track_linear:1,
