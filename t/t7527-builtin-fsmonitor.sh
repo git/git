647da054e1c9f0f23fb26a9088529e73e865b43c
Jeff Hostetler <jeffhost@microsoft.com>
2021-07-12 12:50:31 -0400

    t7527: test builtin FSMonitor watching repos with unicode paths
    Create some test repos with UTF8 pathnames and verify that
    the builtin FSMonitor can watch them.  This test is mainly
    for Windows where we need to avoid `*A()` routines.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- t/t7527-builtin-fsmonitor.sh
+++ t/t7527-builtin-fsmonitor.sh
@@ -699,4 +699,27 @@ do
 	done
 done
 
+# Test Unicode UTF-8 characters in the pathname of the working
+# directory.  Use of "*A()" routines rather than "*W()" routines
+# on Windows can sometimes lead to odd failures.
+#
+u1=$(printf "u_c3_a6__\xC3\xA6")
+u2=$(printf "u_e2_99_ab__\xE2\x99\xAB")
+u_values="$u1 $u2"
+for u in $u_values
+do
+	test_expect_success "Unicode path: $u" '
+		test_when_finished "stop_daemon_delete_repo $u" &&
+
+		git init "$u" &&
+		echo 1 >"$u"/file1 &&
+		git -C "$u" add file1 &&
+		git -C "$u" config core.useBuiltinFSMonitor true &&
+
+		start_daemon "$u" &&
+		git -C "$u" status >actual &&
+		grep "new file:   file1" actual
+	'
+done
+
 test_done
