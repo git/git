95b87268f6c8fc8c20bcf3940240a6a8ea9a9ace
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-22 14:55:04 +0200

    progress.c: add temporary variable from progress struct
    Add a temporary "progress" variable for the dereferenced p_progress
    pointer to a "struct progress *". Before 98a13647408 (trace2: log
    progress time and throughput, 2020-05-12) we didn't dereference
    "p_progress" in this function, now that we do it's easier to read the
    code if we work with a "progress" struct pointer like everywhere else,
    instead of a pointer to a pointer.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- progress.c
+++ progress.c
@@ -331,15 +331,16 @@ void stop_progress(struct progress **p_progress)
 	finish_if_sparse(*p_progress);
 
 	if (*p_progress) {
+		struct progress *progress = *p_progress;
 		trace2_data_intmax("progress", the_repository, "total_objects",
 				   (*p_progress)->total);
 
 		if ((*p_progress)->throughput)
 			trace2_data_intmax("progress", the_repository,
 					   "total_bytes",
-					   (*p_progress)->throughput->curr_total);
+					   progress->throughput->curr_total);
 
-		trace2_region_leave("progress", (*p_progress)->title, the_repository);
+		trace2_region_leave("progress", progress->title, the_repository);
 	}
 
 	stop_progress_msg(p_progress, _("done"));
