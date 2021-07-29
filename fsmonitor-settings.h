6ca05b84dde55129bbc832dead5777764c641cad
Jeff Hostetler <jeffhost@microsoft.com>
2021-07-29 13:33:21 -0400

    fsmonitor-settings: stub in platform-specific incompatibility checking
    Extend generic incompatibility checkout with platform-specific
    mechanism.  Stub in Win32 version.
    
    In the existing fsmonitor-settings code we have a way to mark
    types of repos as incompatible with fsmonitor (whether via the
    hook and ipc APIs).  For example, we do this for bare repos,
    since there are no files to watch.
    
    Extend this exclusion mechanism for platfor-specific reasons.
    This commit just creates the framework and adds a stub for Win32.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- fsmonitor-settings.h
+++ fsmonitor-settings.h
@@ -29,4 +29,17 @@ enum fsmonitor_reason fsm_settings__get_reason(struct repository *r,
 
 struct fsmonitor_settings;
 
+#ifdef HAVE_FSMONITOR_OS_SETTINGS
+/*
+ * Ask platform-specific code whether the repository is incompatible
+ * with fsmonitor (both hook and ipc modes).  For example, if the working
+ * directory is on a remote volume and mounted via a technology that does
+ * not support notification events.
+ *
+ * fsm_os__* routines should considered private to fsm_settings__
+ * routines.
+ */
+enum fsmonitor_reason fsm_os__incompatible(struct repository *r);
+#endif /* HAVE_FSMONITOR_OS_SETTINGS */
+
 #endif /* FSMONITOR_SETTINGS_H */
