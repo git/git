fe74acc51af072d1d440a8e61062e143d667f69c
Jeff Hostetler <jeffhost@microsoft.com>
2021-08-04 10:20:55 -0400

    fsmonitor-settings: stub in macOS-specific incompatibility checking
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ compat/fsmonitor/fsm-settings-darwin.c
@@ -0,0 +1,9 @@
+#include "cache.h"
+#include "config.h"
+#include "repository.h"
+#include "fsmonitor-settings.h"
+
+enum fsmonitor_reason fsm_os__incompatible(struct repository *r)
+{
+	return FSMONITOR_REASON_OK;
+}
