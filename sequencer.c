7469ede75f45afed717f3a0619d844df4f4e2a2f
ZheNing Hu <adlternative@gmail.com>
2021-08-14 10:27:43 +0000

    cherry-pick: use better advice message
    In the past, git cherry-pick would print such advice when
    there was a conflict:
    
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'
    
    But in fact, when we want to cherry-pick multiple commits,
    we should not use "git commit" after resolving conflicts, which
    will make Git generate some errors. We should recommend users to
    use `git cherry-pick --continue`, `git cherry-pick --abort`, just
    like git rebase does.
    
    This is the improved advice:
    
    hint: Resolve all conflicts manually, mark them as resolved with
    hint: "git add/rm <conflicted_files>", then run
    hint: "git cherry-pick --continue".
    hint: You can instead skip this commit: run "git cherry-pick --skip".
    hint: To abort and get back to the state before "git cherry-pick",
    hint: run "git cherry-pick --abort".
    
    Similarly, this optimization can be applied to git revert:
    
    hint: Resolve all conflicts manually, mark them as resolved with
    hint: "git add/rm <conflicted_files>", then run
    hint: "git revert --continue".
    hint: You can instead skip this commit: run "git revert --skip".
    hint: To abort and get back to the state before "git revert",
    hint: run "git revert --abort".
    
    It is worth mentioning that now we use advice() to print the content
    of GIT_CHERRY_PICK_HELP in print_advice(), each line of output will
    start with "hint: ".
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by Hariom Verma <hariom18599@gmail.com>:
    Helped-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Hepled-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sequencer.c
+++ sequencer.c
@@ -403,7 +403,7 @@ static void print_advice(struct repository *r, int show_hint,
 	char *msg = getenv("GIT_CHERRY_PICK_HELP");
 
 	if (msg) {
-		fprintf(stderr, "%s\n", msg);
+		advise("%s\n", msg);
 		/*
 		 * A conflict has occurred but the porcelain
 		 * (typically rebase --interactive) wants to take care
@@ -418,6 +418,20 @@ static void print_advice(struct repository *r, int show_hint,
 		if (opts->no_commit)
 			advise(_("after resolving the conflicts, mark the corrected paths\n"
 				 "with 'git add <paths>' or 'git rm <paths>'"));
+		else if (opts->action == REPLAY_PICK)
+			advise(_("Resolve all conflicts manually, mark them as resolved with\n"
+				 "\"git add/rm <conflicted_files>\", then run\n"
+				 "\"git cherry-pick --continue\".\n"
+				 "You can instead skip this commit: run \"git cherry-pick --skip\".\n"
+				 "To abort and get back to the state before \"git cherry-pick\",\n"
+				 "run \"git cherry-pick --abort\"."));
+		else if (opts->action == REPLAY_REVERT)
+			advise(_("Resolve all conflicts manually, mark them as resolved with\n"
+				 "\"git add/rm <conflicted_files>\", then run\n"
+				 "\"git revert --continue\".\n"
+				 "You can instead skip this commit: run \"git revert --skip\".\n"
+				 "To abort and get back to the state before \"git revert\",\n"
+				 "run \"git revert --abort\"."));
 		else
 			advise(_("after resolving the conflicts, mark the corrected paths\n"
 				 "with 'git add <paths>' or 'git rm <paths>'\n"
--- t/t3501-revert-cherry-pick.sh
+++ t/t3501-revert-cherry-pick.sh
@@ -158,4 +158,24 @@ test_expect_success 'cherry-pick works with dirty renamed file' '
 	grep -q "^modified$" renamed
 '
 
+test_expect_success 'advice from failed revert' '
+	echo dream >dream &&
+	git add dream &&
+	git commit -m "add dream" &&
+	dream_oid=$(git rev-parse --short HEAD) &&
+	cat <<-EOF >expected &&
+	error: could not revert $dream_oid... add dream
+	hint: Resolve all conflicts manually, mark them as resolved with
+	hint: "git add/rm <conflicted_files>", then run
+	hint: "git revert --continue".
+	hint: You can instead skip this commit: run "git revert --skip".
+	hint: To abort and get back to the state before "git revert",
+	hint: run "git revert --abort".
+	EOF
+	echo dream >>dream &&
+	git add dream &&
+	git commit -m "double-add dream" &&
+	test_must_fail git revert HEAD^ 2>actual &&
+	test_cmp expected actual
+'
 test_done
