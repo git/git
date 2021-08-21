95b4ff3931064ff222e87eecf6af504ba55069d7
René Scharfe <l.s.r@web.de>
2021-08-21 14:52:40 +0200

    compat: let git_mmap use malloc(3) directly
    xmalloc() dies on error, allows zero-sized allocations and enforces
    GIT_ALLOC_LIMIT for testing.  Our mmap replacement doesn't need any of
    that.  Let's cut out the wrapper, reject zero-sized requests as required
    by POSIX and use malloc(3) directly.  Allocation errors were needlessly
    handled by git_mmap() before; this code becomes reachable now.
    
    Signed-off-by: René Scharfe <l.s.r@web.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mmap.c
+++ compat/mmap.c
@@ -7,7 +7,12 @@ void *git_mmap(void *start, size_t length, int prot, int flags, int fd, off_t of
 	if (start != NULL || flags != MAP_PRIVATE || prot != PROT_READ)
 		die("Invalid usage of mmap when built with NO_MMAP");
 
-	start = xmalloc(length);
+	if (length == 0) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+
+	start = malloc(length);
 	if (start == NULL) {
 		errno = ENOMEM;
 		return MAP_FAILED;
