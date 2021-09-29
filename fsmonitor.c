c6e29c27dc749a5db6e20befd2619b7b79858fef
Jeff Hostetler <jeffhost@microsoft.com>
2021-09-29 15:15:40 -0400

    fixup! fsmonitor: use IPC to query the builtin FSMonitor daemon
    This reverts commit abc1c46df9fb45358d8541c7e6a9bc53e176d5bc.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- fsmonitor.c
+++ fsmonitor.c
@@ -254,37 +254,8 @@ void refresh_fsmonitor(struct index_state *istate)
 	trace_printf_key(&trace_fsmonitor, "refresh fsmonitor");
 
 	if (fsm_mode == FSMONITOR_MODE_IPC) {
-		query_success = !fsmonitor_ipc__send_query(
-			istate->fsmonitor_last_update ?
-			istate->fsmonitor_last_update : "builtin:fake",
-			&query_result);
-		if (query_success) {
-			/*
-			 * The response contains a series of nul terminated
-			 * strings.  The first is the new token.
-			 *
-			 * Use `char *buf` as an interlude to trick the CI
-			 * static analysis to let us use `strbuf_addstr()`
-			 * here (and only copy the token) rather than
-			 * `strbuf_addbuf()`.
-			 */
-			buf = query_result.buf;
-			strbuf_addstr(&last_update_token, buf);
-			bol = last_update_token.len + 1;
-		} else {
-			/*
-			 * The builtin daemon is not available on this
-			 * platform -OR- we failed to get a response.
-			 *
-			 * Generate a fake token (rather than a V1
-			 * timestamp) for the index extension.  (If
-			 * they switch back to the hook API, we don't
-			 * want ambiguous state.)
-			 */
-			strbuf_addstr(&last_update_token, "builtin:fake");
-		}
-
-		goto apply_results;
+		/* TODO */
+		return;
 	}
 
 	assert(fsm_mode == FSMONITOR_MODE_HOOK);
@@ -350,7 +321,6 @@ void refresh_fsmonitor(struct index_state *istate)
 				 query_success ? "success" : "failure");
 	}
 
-apply_results:
 	/* a fsmonitor process can return '/' to indicate all entries are invalid */
 	if (query_success && query_result.buf[bol] != '/') {
 		/* Mark all entries returned by the monitor as dirty */
