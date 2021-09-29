cd4411cb3c093e8a057bfc85636507e3049d5c28
Jeff Hostetler <jeffhost@microsoft.com>
2021-09-29 15:14:18 -0400

    fixup! fsmonitor-settings: stub in platform-specific incompatibility checking
    This reverts commit 56b6c9aa9926d5c23719a2de680b825a6eb2f0c0.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- compat/fsmonitor/fsm-settings-win32.c
+++
@@ -1,9 +0,0 @@
-#include "cache.h"
-#include "config.h"
-#include "repository.h"
-#include "fsmonitor-settings.h"
-
-enum fsmonitor_reason fsm_os__incompatible(struct repository *r)
-{
-	return FSMONITOR_REASON_ZERO;
-}
