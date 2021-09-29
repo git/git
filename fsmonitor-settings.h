eee16aa4aaa132893d24b033901a7063e167820e
Jeff Hostetler <jeffhost@microsoft.com>
2021-09-29 15:15:20 -0400

    fixup! fsmonitor--daemon: do not try to operate on bare repos
    This reverts commit eab07e13cc2d91931102e5ba3ffc14f63bf7a5ff.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

--- fsmonitor-settings.h
+++ fsmonitor-settings.h
@@ -4,18 +4,9 @@
 struct repository;
 
 enum fsmonitor_mode {
-	FSMONITOR_MODE_INCOMPATIBLE = -1, /* see _reason */
 	FSMONITOR_MODE_DISABLED = 0,
 	FSMONITOR_MODE_HOOK = 1, /* core.fsmonitor */
 	FSMONITOR_MODE_IPC = 2,  /* core.useBuiltinFSMonitor */
-};
-
-/*
- * Incompatibility reasons.
- */
-enum fsmonitor_reason {
-	FSMONITOR_REASON_ZERO = 0,
-	FSMONITOR_REASON_BARE = 1,
 };
 
 void fsm_settings__set_ipc(struct repository *r);
@@ -24,8 +15,6 @@ void fsm_settings__set_disabled(struct repository *r);
 
 enum fsmonitor_mode fsm_settings__get_mode(struct repository *r);
 const char *fsm_settings__get_hook_path(struct repository *r);
-enum fsmonitor_reason fsm_settings__get_reason(struct repository *r,
-					       struct strbuf *buf_reason);
 
 struct fsmonitor_settings;
 
