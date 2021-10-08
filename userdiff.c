350b87cd658553598a269fdd320ca05ee4789a10
Johannes Sixt <j6t@kdbg.org>
2021-10-08 19:09:55 +0000

    userdiff-cpp: tighten word regex
    Generally, word regex can be written such that they match tokens
    liberally and need not model the actual syntax because it can be assumed
    that the regex will only be applied to syntactically correct text.
    
    The regex for cpp (C/C++) is too liberal, though. It regards these
    sequences as single tokens:
    
       1+2
       1.5-e+2+f
    
    and the following amalgams as one token:
    
       .l      as in str.length
       .f      as in str.find
       .e      as in str.erase
    
    Tighten the regex in the following way:
    
    - Accept + and - only in one position in the exponent. + and - are no
      longer regarded as the sign of a number and are treated by the
      catcher-all that is not visible in the driver's regex.
    
    - Accept a leading decimal point only when it is followed by a digit.
    
    For readability, factor hex- and binary numbers into an own term.
    
    As a drive-by, this fixes that floating point numbers such as 12E5
    (with upper-case E) were split into two tokens.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- userdiff.c
+++ userdiff.c
@@ -64,8 +64,14 @@ PATTERNS("cpp",
 	 /* functions/methods, variables, and compounds at top level */
 	 "^((::[[:space:]]*)?[A-Za-z_].*)$",
 	 /* -- */
+	 /* identifiers and keywords */
 	 "[a-zA-Z_][a-zA-Z0-9_]*"
-	 "|[-+0-9.e]+[fFlL]?|0[xXbB]?[0-9a-fA-F]+[lLuU]*"
+	 /* decimal and octal integers as well as floatingpoint numbers */
+	 "|[0-9][0-9.]*([Ee][-+]?[0-9]+)?[fFlLuU]*"
+	 /* hexadecimal and binary integers */
+	 "|0[xXbB][0-9a-fA-F]+[lLuU]*"
+	 /* floatingpoint numbers that begin with a decimal point */
+	 "|\\.[0-9]+([Ee][-+]?[0-9]+)?[fFlL]?"
 	 "|[-+*/<>%&^|=!]=|--|\\+\\+|<<=?|>>=?|&&|\\|\\||::|->\\*?|\\.\\*"),
 PATTERNS("csharp",
 	 /* Keywords */
