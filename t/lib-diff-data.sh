15b808da74eb239ad8fcdbe6168ed0b5260eadc1
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-10-15 11:30:16 +0200

    "lib-diff" tests: make "README" and "COPYING" test data smaller
    Follow-up the change in 459b8d22e54 (tests: do not borrow from COPYING
    and README from the real source, 2015-02-15) by not shipping a full
    copy of older versions of the top-level "COPYING" and "README" files.
    
    The tests that use them just need the small blurb at the top of
    "COPYING" as test data, or mock data that's dissimilar. Let's provide
    that with a "COPYING_test_data" function instead.
    
    We're not replacing this with some other generic test
    data (e.g. "lorum ipsum") because these tests require test file header
    to be the old "COPYING" file. See e.g. "t4003-diff-rename-1.sh" which
    changes the file, and then does full "test_cmp" comparisons on the
    resulting "git diff" output.
    
    This change only changes tests that used the "lib-diff.sh" library,
    but splits up what they need into a new "lib-diff-data.sh". A
    subsequent commit will change related tests that were missed in
    459b8d22e54.
    
    For the test in "t4008-diff-break-rewrite.sh" the "README" file can go
    away in favor of echoing the line "some dissimilar content" to a file
    in the one test that needed it.
    
    The point of that test is to start with files "A" and "B", and then
    have A be more similar to the state of "B" than to its old version (by
    copying over the content from the "COPYING" file). Just comparing the
    pre-image of "some dissimilar content" and later a munged version of
    the "COPYING" output serves that purpose.
    
    While we're at it get rid of a stray "echo $tree" debugging line added
    in 15d061b435a ([PATCH] Fix the way diffcore-rename records unremoved
    source., 2005-05-27), and stop calling "hash-object" to get the hash
    of an object we've just added to the index. We can instead extract
    that information from the index itself with "rev-parse".
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/lib-diff-data.sh
@@ -0,0 +1,22 @@
+COPYING_test_data () {
+	cat <<\EOF
+
+ Note that the only valid version of the GPL as far as this project
+ is concerned is _this_ particular version of the license (ie v2, not
+ v2.2 or v3.x or whatever), unless explicitly otherwise stated.
+
+ HOWEVER, in order to allow a migration to GPLv3 if that seems like
+ a good idea, I also ask that people involved with the project make
+ their preferences known. In particular, if you trust me to make that
+ decision, you might note so in your copyright message, ie something
+ like
+
+	This file is licensed under the GPL v2, or a later version
+	at the discretion of Linus.
+
+  might avoid issues. But we can also just decide to synchronize and
+  contact all copyright holders on record if/when the occasion arises.
+
+			Linus Torvalds
+EOF
+}
