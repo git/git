d7927d428cdb6ed0d709fea2e5353d56833ae02e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-10-21 21:57:59 +0200

    git-sh-setup: remove unused sane_egrep() function
    The is_zero_oid() function in git-submodule.sh has not been used since
    e83e3333b57 (submodule: port submodule subcommand 'summary' from shell
    to C, 2020-08-13), so we can remove it, and the sane_egrep() function,
    dead is_zero_oid() was the only function which still referenced it.
    
    Unlike some other functions in git-sh-setup.sh, this function has not
    been documented in git-sh-setup(1), so per [1] it should be OK to
    remove it. I'm still unclear about the future of some of the other
    functions[2], but any questions in that area should not apply here.
    
    1. https://lore.kernel.org/git/xmqqr1dtgnn8.fsf@gitster.g/
    1. https://lore.kernel.org/git/87tuiwjfvi.fsf@evledraar.gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- git-sh-setup.sh
+++ git-sh-setup.sh
@@ -177,10 +177,6 @@ sane_grep () {
 	GREP_OPTIONS= LC_ALL=C grep @@SANE_TEXT_GREP@@ "$@"
 }
 
-sane_egrep () {
-	GREP_OPTIONS= LC_ALL=C egrep @@SANE_TEXT_GREP@@ "$@"
-}
-
 is_bare_repository () {
 	git rev-parse --is-bare-repository
 }
