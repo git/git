191eb491ed44b86c7ab4fe279c8ce47f030624b0
Johannes Sixt <j6t@kdbg.org>
2021-11-05 15:08:01 +0100

    generate-cmdlist.sh: spawn fewer processes
    The function get_categories() is invoked in a loop over all commands.
    As it runs several processes, this takes an awful lot of time on
    Windows. To reduce the number of processes, move the process that
    filters empty lines to the other invoker of the function, where it is
    needed. The invocation of get_categories() in the loop does not need
    the empty line filtered away because the result is word-split by the
    shell, which eliminates the empty line automatically.
    
    Furthermore, use sort -u instead of sort | uniq to remove yet another
    process.
    
    [Ævar: on Linux this seems to speed things up a bit, although with
    hyperfine(1) the results are fuzzy enough to land within the
    confidence interval]:
    
    $ git show HEAD~:generate-cmdlist.sh >generate-cmdlist.sh.old
    $ hyperfine --warmup 1 -L s ,.old -p 'make clean' 'sh generate-cmdlist.sh{s} command-list.txt'
    Benchmark #1: sh generate-cmdlist.sh command-list.txt
      Time (mean ± σ):     371.3 ms ±  64.2 ms    [User: 430.4 ms, System: 72.5 ms]
      Range (min … max):   320.5 ms … 517.7 ms    10 runs
    
    Benchmark #2: sh generate-cmdlist.sh.old command-list.txt
      Time (mean ± σ):     489.9 ms ± 185.4 ms    [User: 724.7 ms, System: 141.3 ms]
      Range (min … max):   346.0 ms … 885.3 ms    10 runs
    
    Summary
      'sh generate-cmdlist.sh command-list.txt' ran
        1.32 ± 0.55 times faster than 'sh generate-cmdlist.sh.old command-list.txt'
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- generate-cmdlist.sh
+++ generate-cmdlist.sh
@@ -11,15 +11,14 @@ command_list () {
 
 get_categories () {
 	tr ' ' '\012' |
-	grep -v '^$' |
-	sort |
-	uniq
+	LC_ALL=C sort -u
 }
 
 category_list () {
 	command_list "$1" |
 	cut -c 40- |
-	get_categories
+	get_categories |
+	grep -v '^$'
 }
 
 get_synopsis () {
