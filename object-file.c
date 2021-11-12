bf99aaf19925ccd83b0bd6b3620ae314f5197a94
Philip Oakley <philipoakley@iee.email>
2021-11-12 21:16:51 +0000

    hash algorithms: use size_t for section lengths
    Continue walking the code path for the >4GB `hash-object --literally`
    test to the hash algorithm step for LLP64 systems.
    
    This patch lets the SHA1DC code use `size_t`, making it compatible with
    LLP64 data models (as used e.g. by Windows).
    
    The interested reader of this patch will note that we adjust the
    signature of the `git_SHA1DCUpdate()` function without updating _any_
    call site. This certainly puzzled at least one reviewer already, so here
    is an explanation:
    
    This function is never called directly, but always via the macro
    `platform_SHA1_Update`, which is usually called via the macro
    `git_SHA1_Update`. However, we never call `git_SHA1_Update()` directly
    in `struct git_hash_algo`. Instead, we call `git_hash_sha1_update()`,
    which is defined thusly:
    
        static void git_hash_sha1_update(git_hash_ctx *ctx,
                                         const void *data, size_t len)
        {
            git_SHA1_Update(&ctx->sha1, data, len);
        }
    
    i.e. it contains an implicit downcast from `size_t` to `unsigned long`
    (before this here patch). With this patch, there is no downcast anymore.
    
    With this patch, finally, the t1007-hash-object.sh "files over 4GB hash
    literally" test case is fixed.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.email>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- object-file.c
+++ object-file.c
@@ -534,7 +534,7 @@ out:
 }
 
 static void hash_object_body(const struct git_hash_algo *algo, struct git_hash_ctx *c,
-			     const void *buf, unsigned long len,
+			     const void *buf, size_t len,
 			     struct object_id *oid,
 			     char *hdr, size_t *hdrlen)
 {
@@ -554,7 +554,7 @@ static void write_object_file_prepare(const struct git_hash_algo *algo,
 	/* Generate the header */
 	*hdrlen = format_object_header(hdr, *hdrlen, type, len);
 
-	/* Sha1.. */
+	/* Hash (function pointers) computation */
 	hash_object_body(algo, &c, buf, len, oid, hdr, hdrlen);
 }
 
