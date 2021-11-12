6c951e98c20f6e326e6d6735a321a4e1a668ac1a
Philip Oakley <philipoakley@iee.email>
2021-11-12 21:14:50 +0000

    object-file.c: use size_t for header lengths
    Continue walking the code path for the >4GB `hash-object --literally`
    test. The `hash_object_file_literally()` function internally uses both
    `hash_object_file()` and `write_object_file_prepare()`. Both function
    signatures use `unsigned long` rather than `size_t` for the mem buffer
    sizes. Use `size_t` instead, for LLP64 compatibility.
    
    While at it, convert those function's object's header buffer length to
    `size_t` for consistency. The value is already upcast to `uintmax_t` for
    print format compatibility.
    
    Note: The hash-object test still does not pass. A subsequent commit
    continues to walk the call tree's lower level hash functions to identify
    further fixes.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.email>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- object-file.c
+++ object-file.c
@@ -503,7 +503,7 @@ cleanup:
 static void hash_object_body(const struct git_hash_algo *algo, struct git_hash_ctx *c,
 			     const void *buf, unsigned long len,
 			     struct object_id *oid,
-			     char *hdr, int *hdrlen)
+			     char *hdr, size_t *hdrlen)
 {
 	algo->init_fn(c);
 	git_hash_update(c, hdr, *hdrlen);
@@ -512,9 +512,9 @@ static void hash_object_body(const struct git_hash_algo *algo, struct git_hash_c
 }
 
 static void write_object_file_prepare(const struct git_hash_algo *algo,
-				      const void *buf, unsigned long len,
+				      const void *buf, size_t len,
 				      enum object_type type, struct object_id *oid,
-				      char *hdr, int *hdrlen)
+				      char *hdr, size_t *hdrlen)
 {
 	struct git_hash_ctx c;
 
@@ -657,11 +657,11 @@ out:
 }
 
 void hash_object_file(const struct git_hash_algo *algo, const void *buf,
-		      unsigned long len, enum object_type type,
+		      size_t len, enum object_type type,
 		      struct object_id *oid)
 {
 	char hdr[MAX_HEADER_LEN];
-	int hdrlen = sizeof(hdr);
+	size_t hdrlen = sizeof(hdr);
 
 	write_object_file_prepare(algo, buf, len, type, oid, hdr, &hdrlen);
 }
@@ -1037,7 +1037,7 @@ cleanup:
 }
 
 int write_object_file(struct odb_source *source,
-		      const void *buf, unsigned long len,
+		      const void *buf, size_t len,
 		      enum object_type type, struct object_id *oid,
 		      struct object_id *compat_oid_in, unsigned flags)
 {
@@ -1045,7 +1045,7 @@ int write_object_file(struct odb_source *source,
 	const struct git_hash_algo *compat = source->odb->repo->compat_hash_algo;
 	struct object_id compat_oid;
 	char hdr[MAX_HEADER_LEN];
-	int hdrlen = sizeof(hdr);
+	size_t hdrlen = sizeof(hdr);
 
 	/* Generate compat_oid */
 	if (compat) {
