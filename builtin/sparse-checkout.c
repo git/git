595ba13860352abcbd857c454c0b2d7e83efd6c0
Elijah Newren <newren@gmail.com>
2021-12-01 16:35:33 -0800

    sparse-checkout: enable `set` to initialize sparse-checkout mode
    The previously suggested workflow:
      git sparse-checkout init ...
      git sparse-checkout set ...
    
    Suffered from three problems:
      1) It would delete nearly all files in the first step, then
         restore them in the second.  That was poor performance and
         forced unnecessary rebuilds.
      2) The two-step process resulted in two progress bars, which
         was suboptimal from a UI point of view for wrappers that
         invoked both of these commands but only exposed a single
         command to their end users.
      3) With cone mode, the first step would delete nearly all
         ignored files everywhere, because everything was considered
         to be outside of the specified sparsity paths.  (The user was
         not allowed to specify any sparsity paths in the `init` step.)
    
    Avoid these problems by teaching `set` to understand the extra
    parameters that `init` takes and performing any necessary initialization
    if not already in a sparse checkout.
    
    Reviewed-by: Derrick Stolee <dstolee@microsoft.com>
    Reviewed-by: Victoria Dye <vdye@github.com>
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- builtin/sparse-checkout.c
+++ builtin/sparse-checkout.c
@@ -706,17 +706,26 @@ static int sparse_checkout_add(int argc, const char **argv, const char *prefix)
 }
 
 static char const * const builtin_sparse_checkout_set_usage[] = {
-	N_("git sparse-checkout set (--stdin | <patterns>)"),
+	N_("git sparse-checkout set [--[no-]cone] [--[no-]sparse-index] (--stdin | <patterns>)"),
 	NULL
 };
 
 static struct sparse_checkout_set_opts {
+	int cone_mode;
+	int sparse_index;
 	int use_stdin;
 } set_opts;
 
 static int sparse_checkout_set(int argc, const char **argv, const char *prefix)
 {
+	int default_patterns_nr = 2;
+	const char *default_patterns[] = {"/*", "!/*/", NULL};
+
 	static struct option builtin_sparse_checkout_set_options[] = {
+		OPT_BOOL(0, "cone", &set_opts.cone_mode,
+			 N_("initialize the sparse-checkout in cone mode")),
+		OPT_BOOL(0, "sparse-index", &set_opts.sparse_index,
+			 N_("toggle the use of a sparse index")),
 		OPT_BOOL_F(0, "stdin", &set_opts.use_stdin,
 			   N_("read patterns from standard in"),
 			   PARSE_OPT_NONEG),
@@ -725,10 +734,26 @@ static int sparse_checkout_set(int argc, const char **argv, const char *prefix)
 
 	repo_read_index(the_repository);
 
+	set_opts.cone_mode = -1;
+	set_opts.sparse_index = -1;
+
 	argc = parse_options(argc, argv, prefix,
 			     builtin_sparse_checkout_set_options,
 			     builtin_sparse_checkout_set_usage,
 			     PARSE_OPT_KEEP_UNKNOWN);
+
+	if (update_modes(&set_opts.cone_mode, &set_opts.sparse_index))
+		return 1;
+
+	/*
+	 * Cone mode automatically specifies the toplevel directory.  For
+	 * non-cone mode, if nothing is specified, manually select just the
+	 * top-level directory (much as 'init' would do).
+	 */
+	if (!core_sparse_checkout_cone && argc == 0) {
+		argv = default_patterns;
+		argc = default_patterns_nr;
+	}
 
 	return modify_pattern_list(argc, argv, set_opts.use_stdin, REPLACE);
 }
