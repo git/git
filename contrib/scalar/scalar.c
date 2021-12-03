0a43fb220269f061c53fbb5e6f66d28acdb450ca
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-12-03 13:34:16 +0000

    scalar: create a rudimentary executable
    The idea of Scalar (https://github.com/microsoft/scalar), and before
    that, of VFS for Git, has always been to prove that Git _can_ scale, and
    to upstream whatever strategies have been demonstrated to help.
    
    With this patch, we start the journey from that C# project to move what
    is left to Git's own `contrib/` directory, reimplementing it in pure C,
    with the intention to facilitate integrating the functionality into core
    Git all while maintaining backwards-compatibility for existing Scalar
    users (which will be much easier when both live in the same worktree).
    It has always been the plan to contribute all of the proven strategies
    back to core Git.
    
    For example, while the virtual filesystem provided by VFS for Git helped
    the team developing the Windows operating system to move onto Git, while
    trying to upstream it we realized that it cannot be done: getting the
    virtual filesystem to work (which we only managed to implement fully on
    Windows, but not on, say, macOS or Linux), and the required server-side
    support for the GVFS protocol, made this not quite feasible.
    
    The Scalar project learned from that and tackled the problem with
    different tactics: instead of pretending to Git that the working
    directory is fully populated, it _specifically_ teaches Git about
    partial clone (which is based on VFS for Git's cache server), about
    sparse checkout (which VFS for Git tried to do transparently, in the
    file system layer), and regularly runs maintenance tasks to keep the
    repository in a healthy state.
    
    With partial clone, sparse checkout and `git maintenance` having been
    upstreamed, there is little left that `scalar.exe` does which `git.exe`
    cannot do. One such thing is that `scalar clone <url>` will
    automatically set up a partial, sparse clone, and configure
    known-helpful settings from the start.
    
    So let's bring this convenience into Git's tree.
    
    The idea here is that you can (optionally) build Scalar via
    
    	make -C contrib/scalar/
    
    This will build the `scalar` executable and put it into the
    contrib/scalar/ subdirectory.
    
    The slightly awkward addition of the `contrib/scalar/*` bits to the
    top-level `Makefile` are actually really required: we want to link to
    `libgit.a`, which means that we will need to use the very same `CFLAGS`
    and `LDFLAGS` as the rest of Git.
    
    An early development version of this patch tried to replicate all the
    conditional code in `contrib/scalar/Makefile` (e.g. `NO_POLL`) just like
    `contrib/svn-fe/Makefile` used to do before it was retired. It turned
    out to be quite the whack-a-mole game: the SHA-1-related flags, the
    flags enabling/disabling `compat/poll/`, `compat/regex/`,
    `compat/win32mmap.c` & friends depending on the current platform... To
    put it mildly: it was a major mess.
    
    Instead, this patch makes minimal changes to the top-level `Makefile` so
    that the bits in `contrib/scalar/` can be compiled and linked, and
    adds a `contrib/scalar/Makefile` that uses the top-level `Makefile` in a
    most minimal way to do the actual compiling.
    
    Note: With this commit, we only establish the infrastructure, no
    Scalar functionality is implemented yet; We will do that incrementally
    over the next few commits.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ contrib/scalar/scalar.c
@@ -0,0 +1,36 @@
+/*
+ * The Scalar command-line interface.
+ */
+
+#include "cache.h"
+#include "gettext.h"
+#include "parse-options.h"
+
+static struct {
+	const char *name;
+	int (*fn)(int, const char **);
+} builtins[] = {
+	{ NULL, NULL},
+};
+
+int cmd_main(int argc, const char **argv)
+{
+	struct strbuf scalar_usage = STRBUF_INIT;
+	int i;
+
+	if (argc > 1) {
+		argv++;
+		argc--;
+
+		for (i = 0; builtins[i].name; i++)
+			if (!strcmp(builtins[i].name, argv[0]))
+				return !!builtins[i].fn(argc, argv);
+	}
+
+	strbuf_addstr(&scalar_usage,
+		      N_("scalar <command> [<options>]\n\nCommands:\n"));
+	for (i = 0; builtins[i].name; i++)
+		strbuf_addf(&scalar_usage, "\t%s\n", builtins[i].name);
+
+	usage(scalar_usage.buf);
+}
