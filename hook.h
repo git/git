96e7225b310cb45a9b1198fb7bb1621e638e3329
Emily Shaffer <emilyshaffer@google.com>
2021-12-22 04:59:27 +0100

    hook: add 'run' subcommand
    In order to enable hooks to be run as an external process, by a
    standalone Git command, or by tools which wrap Git, provide an external
    means to run all configured hook commands for a given hook event.
    
    Most of our hooks require more complex functionality than this, but
    let's start with the bare minimum required to support our simplest
    hooks.
    
    In terms of implementation the usage_with_options() and "goto usage"
    pattern here mirrors that of
    builtin/{commit-graph,multi-pack-index}.c.
    
    Some of the implementation here, such as a function being named
    run_hooks_opt() when it's tasked with running one hook, to using the
    run_processes_parallel_tr2() API to run with jobs=1 is somewhere
    between a bit odd and and an overkill for the current features of this
    "hook run" command and the hook.[ch] API.
    
    This code will eventually be able to run multiple hooks declared in
    config in parallel, by starting out with these names and APIs we
    reduce the later churn of renaming functions, switching from the
    run_command() to run_processes_parallel_tr2() API etc.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Acked-by: Emily Shaffer <emilyshaffer@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- hook.h
+++ hook.h
@@ -1,5 +1,31 @@
 #ifndef HOOK_H
 #define HOOK_H
+#include "strvec.h"
+
+struct run_hooks_opt
+{
+	/* Environment vars to be set for each hook */
+	struct strvec env;
+
+	/* Args to be passed to each hook */
+	struct strvec args;
+
+	/* Emit an error if the hook is missing */
+	unsigned int error_if_missing:1;
+};
+
+#define RUN_HOOKS_OPT_INIT { \
+	.env = STRVEC_INIT, \
+	.args = STRVEC_INIT, \
+}
+
+struct hook_cb_data {
+	/* rc reflects the cumulative failure state */
+	int rc;
+	const char *hook_name;
+	const char *hook_path;
+	struct run_hooks_opt *options;
+};
 
 /*
  * Returns the path to the hook file, or NULL if the hook is missing
@@ -13,4 +39,13 @@ const char *find_hook(const char *name);
  */
 int hook_exists(const char *hookname);
 
+/**
+ * Takes a `hook_name`, resolves it to a path with find_hook(), and
+ * runs the hook for you with the options specified in "struct
+ * run_hooks opt". Will free memory associated with the "struct run_hooks_opt".
+ *
+ * Returns the status code of the run hook, or a negative value on
+ * error().
+ */
+int run_hooks_opt(const char *hook_name, struct run_hooks_opt *options);
 #endif
