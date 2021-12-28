fa476be8f0963d4dd512eaf8ee23b3cd3986a500
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-12-28 14:28:43 +0100

    parse-options API: add a usage_msg_optf()
    Add a usage_msg_optf() as a shorthand for the sort of
    usage_msg_opt(xstrfmt(...)) used in builtin/stash.c. I'll make more
    use of this function in builtin/cat-file.c shortly.
    
    The disconnect between the "..." and "fmt" is a bit unusual, but it
    works just fine and this keeps it consistent with usage_msg_opt(),
    i.e. a caller of it can be moved to usage_msg_optf() and not have to
    have its arguments re-arranged.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options.h
+++ parse-options.h
@@ -225,6 +225,16 @@ NORETURN void usage_msg_opt(const char *msg,
 			    const char * const *usagestr,
 			    const struct option *options);
 
+/**
+ * usage_msg_optf() is like usage_msg_opt() except that the first
+ * argument is a format string, and optional format arguments follow
+ * after the 3rd option.
+ */
+__attribute__((format (printf,1,4)))
+void NORETURN usage_msg_optf(const char *fmt,
+			     const char * const *usagestr,
+			     const struct option *options, ...);
+
 /*
  * Use these assertions for callbacks that expect to be called with NONEG and
  * NOARG respectively, and do not otherwise handle the "unset" and "arg"
