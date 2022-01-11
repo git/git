b9ca5e26579ceb820103b49648c01187a4a0dddd
Victoria Dye <vdye@github.com>
2022-01-11 18:05:06 +0000

    update-index: reduce scope of index expansion in do_reupdate
    Replace unconditional index expansion in 'do_reupdate()' with one scoped to
    only where a full index is needed. A full index is only required in
    'do_reupdate()' when a sparse directory in the index differs from HEAD; in
    that case, the index is expanded and the operation restarted.
    
    Because the index should only be expanded if a sparse directory is modified,
    add a test ensuring the index is not expanded when differences only exist
    within the sparse cone.
    
    Signed-off-by: Victoria Dye <vdye@github.com>
    Reviewed-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -1265,7 +1265,10 @@ test_expect_success 'sparse index is not expanded: update-index' '
 
 	ensure_not_expanded update-index --add README.md &&
 	ensure_not_expanded update-index a &&
-	ensure_not_expanded update-index --remove deep/a
+	ensure_not_expanded update-index --remove deep/a &&
+
+	ensure_not_expanded reset --soft update-deep &&
+	ensure_not_expanded update-index --add --remove --again
 '
 
 test_expect_success 'sparse index is not expanded: blame' '
