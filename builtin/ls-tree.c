d5e7af7b03142b417de41fd5a8117420252cb293
Teng Long <dyroneteng@gmail.com>
2022-01-13 11:42:08 +0800

    ls-tree: optimize naming and handling of "return" in show_tree()
    The variable which "show_tree()" return is named "retval", a name that's
    a little hard to understand. This commit tries to make the variable
    and the related codes more clear in the context.
    
    The commit firstly rename "retval" to "recurse" which is a more
    meaningful name than before. Secondly, "get_type()" is introduced
    to setup the "type" by "mode", this will remove some of the nested if.
    After this, The codes here become a little bit clearer, so we do not
    need to take a look at "read_tree_at()" in "tree.c" to make sure the
    context of the return value.
    
    Signed-off-by: Teng Long <dyronetengb@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/ls-tree.c
+++ builtin/ls-tree.c
@@ -61,24 +61,27 @@ static int show_recursive(const char *base, size_t baselen, const char *pathname
 	return 0;
 }
 
+static enum object_type get_type(unsigned int mode)
+{
+	return (S_ISGITLINK(mode)
+		? OBJ_COMMIT
+		: S_ISDIR(mode)
+		? OBJ_TREE
+		: OBJ_BLOB);
+}
+
 static int show_tree(const struct object_id *oid, struct strbuf *base,
 		const char *pathname, unsigned mode, void *context)
 {
-	int retval = 0;
+	int recurse = 0;
 	size_t baselen;
-	enum object_type type = OBJ_BLOB;
+	enum object_type type = get_type(mode);
 
-	if (S_ISGITLINK(mode)) {
-		type = OBJ_COMMIT;
-	} else if (S_ISDIR(mode)) {
-		if (show_recursive(base->buf, base->len, pathname)) {
-			retval = READ_TREE_RECURSIVE;
-			if (!(ls_options & LS_SHOW_TREES))
-				return retval;
-		}
-		type = OBJ_TREE;
-	}
-	else if (ls_options & LS_TREE_ONLY)
+	if (type == OBJ_TREE && show_recursive(base->buf, base->len, pathname))
+		recurse = READ_TREE_RECURSIVE;
+	if (type == OBJ_TREE && recurse && !(ls_options & LS_SHOW_TREES))
+		return recurse;
+	if (type == OBJ_BLOB && (ls_options & LS_TREE_ONLY))
 		return 0;
 
 	if (!(ls_options & LS_NAME_ONLY)) {
@@ -109,7 +112,7 @@ static int show_tree(const struct object_id *oid, struct strbuf *base,
 				   chomp_prefix ? ls_tree_prefix : NULL,
 				   stdout, line_termination);
 	strbuf_setlen(base, baselen);
-	return retval;
+	return recurse;
 }
 
 int cmd_ls_tree(int argc, const char **argv, const char *prefix)
