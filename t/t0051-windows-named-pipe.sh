0a2bfccb9c85458f329fdbf714af699edd86fe32
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-02-04 14:42:14 +0100

    t0051: use "skip_all" under !MINGW in single-test file
    Have this file added in 06ba9d03e34 (t0051: test GIT_TRACE to a
    windows named pipe, 2018-09-11) use the same "skip_all" pattern as an
    existing Windows-only test added in 0e218f91c29 (mingw: unset PERL5LIB
    by default, 2018-10-30) uses.
    
    This way TAP consumers like "prove" will show a nice summary when the
    test is skipped. Instead of:
    
        $ prove t0051-windows-named-pipe.sh
        [...]
        t0051-windows-named-pipe.sh .. ok
        [...]
    
    We will prominently show a "skipped" notice:
    
        $ prove t0051-windows-named-pipe.sh
        [...]
        t0051-windows-named-pipe.sh ... skipped: skipping Windows-specific tests
        [...]
    
    This is because we are now making use of the right TAP-y way to
    communicate this to the consumer. I.e. skipping the whole test file,
    v.s. skipping individual tests (in this case there's only one test).
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0051-windows-named-pipe.sh
+++ t/t0051-windows-named-pipe.sh
@@ -3,8 +3,13 @@
 test_description='Windows named pipes'
 
 . ./test-lib.sh
+if ! test_have_prereq MINGW
+then
+	skip_all='skipping Windows-specific tests'
+	test_done
+fi
 
-test_expect_success MINGW 'o_append write to named pipe' '
+test_expect_success 'o_append write to named pipe' '
 	GIT_TRACE="$(pwd)/expect" git status >/dev/null 2>&1 &&
 	{ test-tool windows-named-pipe t0051 >actual 2>&1 & } &&
 	pid=$! &&
