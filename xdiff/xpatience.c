9df0fc3d578acfdeb7fa1914fcf0507adb021fa5
Phillip Wood <phillip.wood@dunelm.org.uk>
2022-02-16 10:15:06 +0000

    xdiff: fix a memory leak
    Although the patience and histogram algorithms initialize the
    environment they do not free it if there is an error. In contrast for
    the Myers algorithm the environment is initalized in xdl_do_diff() and
    it is freed if there is an error. Fix this by always initializing the
    environment in xdl_do_diff() and freeing it there if there is an
    error. Remove the comment in do_patience_diff() about the environment
    being freed by xdl_diff() as it is not accurate because (a) xdl_diff()
    does not do that if there is an error and (b) xdl_diff() is not the
    only caller.
    
    Reported-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- xdiff/xpatience.c
+++ xdiff/xpatience.c
@@ -373,10 +373,6 @@ static int patience_diff(mmfile_t *file1, mmfile_t *file2,
 int xdl_do_patience_diff(mmfile_t *file1, mmfile_t *file2,
 		xpparam_t const *xpp, xdfenv_t *env)
 {
-	if (xdl_prepare_env(file1, file2, xpp, env) < 0)
-		return -1;
-
-	/* environment is cleaned up in xdl_diff() */
 	return patience_diff(file1, file2, xpp, env,
 			1, env->xdf1.nrec, 1, env->xdf2.nrec);
 }
