09188ed930bcb08e56d1846fbf9f5cb972a2d188
Jaydeep P Das <jaydeepjd.8914@gmail.com>
2022-03-12 10:18:32 +0530

    userdiff: add builtin diff driver for kotlin language.
    The xfuncname pattern finds func/class declarations
    in diffs to display as a hunk header. The word_regex
    pattern finds individual tokens in Kotlin code to generate
    appropriate diffs.
    
    This patch adds xfuncname regex and word_regex for Kotlin
    language.
    
    Signed-off-by: Jaydeep P Das <jaydeepjd.8914@gmail.com>
    Acked-by: Johannes Sixt <j6t@kdbg.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4034-diff-words.sh
+++ t/t4034-diff-words.sh
@@ -324,6 +324,7 @@ test_language_driver dts
 test_language_driver fortran
 test_language_driver html
 test_language_driver java
+test_language_driver kotlin
 test_language_driver matlab
 test_language_driver objc
 test_language_driver pascal
