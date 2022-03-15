eb54a3391bb3bdd6806ebffc21281eae8d9c0dca
John Cai <johncai86@gmail.com>
2022-03-15 02:40:36 +0000

    cat-file: skip expanding default format
    When format is passed into --batch, --batch-check, --batch-command,
    the format gets expanded. When nothing is passed in, the default format
    is set and the expand_format() gets called.
    
    We can save on these cycles by hardcoding how to print the
    information when nothing is passed as the format, or when the default
    format is passed. There is no need for the fully expanded format with
    the default. Since batch_object_write() happens on every object provided
    in batch mode, we get a nice performance improvement.
    
    git rev-list --all > /tmp/all-obj.txt
    
    git cat-file --batch-check </tmp/all-obj.txt
    
    with HEAD^:
    
    Time (mean ± σ): 57.6 ms ± 1.7 ms [User: 51.5 ms, System: 6.2 ms]
    Range (min … max): 54.6 ms … 64.7 ms 50 runs
    
    with HEAD:
    
    Time (mean ± σ): 49.8 ms ± 1.7 ms [User: 42.6 ms, System: 7.3 ms]
    Range (min … max): 46.9 ms … 55.9 ms 56 runs
    
    If nothing is provided as a format argument, or if the default format is
    passed, skip expanding of the format and print the object info with a
    default format.
    
    See https://lore.kernel.org/git/87eecf8ork.fsf@evledraar.gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: John Cai <johncai86@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -351,6 +351,13 @@ static void print_object_or_die(struct batch_options *opt, struct expand_data *d
 	}
 }
 
+static void print_default_format(struct strbuf *scratch, struct expand_data *data)
+{
+	strbuf_addf(scratch, "%s %s %"PRIuMAX"\n", oid_to_hex(&data->oid),
+		    type_name(data->type),
+		    (uintmax_t)data->size);
+}
+
 /*
  * If "pack" is non-NULL, then "offset" is the byte offset within the pack from
  * which the object may be accessed (though note that we may also rely on
@@ -382,8 +389,14 @@ static void batch_object_write(const char *obj_name,
 	}
 
 	strbuf_reset(scratch);
-	strbuf_expand(scratch, opt->format, expand_format, data);
-	strbuf_addch(scratch, '\n');
+
+	if (!opt->format) {
+		print_default_format(scratch, data);
+	} else {
+		strbuf_expand(scratch, opt->format, expand_format, data);
+		strbuf_addch(scratch, '\n');
+	}
+
 	batch_write(opt, scratch->buf, scratch->len);
 
 	if (opt->print_contents) {
@@ -508,6 +521,8 @@ static int batch_unordered_packed(const struct object_id *oid,
 				      data);
 }
 
+#define DEFAULT_FORMAT "%(objectname) %(objecttype) %(objectsize)"
+
 static int batch_objects(struct batch_options *opt)
 {
 	struct strbuf input = STRBUF_INIT;
@@ -516,9 +531,6 @@ static int batch_objects(struct batch_options *opt)
 	int save_warning;
 	int retval = 0;
 
-	if (!opt->format)
-		opt->format = "%(objectname) %(objecttype) %(objectsize)";
-
 	/*
 	 * Expand once with our special mark_query flag, which will prime the
 	 * object_info to be handed to oid_object_info_extended for each
@@ -526,12 +538,17 @@ static int batch_objects(struct batch_options *opt)
 	 */
 	memset(&data, 0, sizeof(data));
 	data.mark_query = 1;
-	strbuf_expand(&output, opt->format, expand_format, &data);
+	strbuf_expand(&output,
+		      opt->format ? opt->format : DEFAULT_FORMAT,
+		      expand_format,
+		      &data);
 	data.mark_query = 0;
 	strbuf_release(&output);
 	if (opt->cmdmode)
 		data.split_on_whitespace = 1;
 
+	if (opt->format && !strcmp(opt->format, DEFAULT_FORMAT))
+		opt->format = NULL;
 	/*
 	 * If we are printing out the object, then always fill in the type,
 	 * since we will want to decide whether or not to stream.
