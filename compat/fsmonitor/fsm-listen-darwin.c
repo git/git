5ff01b1f1e8e42805bdf98cb0bd3277d18543b07
Jeff Hostetler <jeffhost@microsoft.com>
2022-03-25 18:02:59 +0000

    compat/fsmonitor/fsm-listen-darwin: add MacOS header files for FSEvent
    Include MacOS system declarations to allow us to use FSEvent and
    CoreFoundation APIs.  We need different versions of the declarations
    for GCC vs. clang because of compiler and header file conflicts.
    
    While it is quite possible to #include Apple's CoreServices.h when
    compiling C source code with clang, trying to build it with GCC
    currently fails with this error:
    
    In file included
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/Security.framework/Headers/AuthSession.h:32,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/Security.framework/Headers/Security.h:42,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/CoreServices.framework/Frameworks/...
       ...OSServices.framework/Headers/CSIdentity.h:43,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/CoreServices.framework/Frameworks/...
       ...OSServices.framework/Headers/OSServices.h:29,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/CoreServices.framework/Frameworks/...
       ...LaunchServices.framework/Headers/IconsCore.h:23,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/CoreServices.framework/Frameworks/...
       ...LaunchServices.framework/Headers/LaunchServices.h:23,
       from /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
       ...Library/Frameworks/CoreServices.framework/Headers/CoreServices.h:45,
    
         /Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/...
         ...Library/Frameworks/Security.framework/Headers/Authorization.h:193:7:
         error: variably modified 'bytes' at file scope
           193 | char bytes[kAuthorizationExternalFormLength];
               |      ^~~~~
    
    The underlying reason is that GCC (rightfully) objects that an `enum`
    value such as `kAuthorizationExternalFormLength` is not a constant
    (because it is not, the preprocessor has no knowledge of it, only the
    actual C compiler does) and can therefore not be used to define the size
    of a C array.
    
    This is a known problem and tracked in GCC's bug tracker:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=93082
    
    In the meantime, let's not block things and go the slightly ugly route
    of declaring/defining the FSEvents constants, data structures and
    functions that we need, so that we can avoid above-mentioned issue.
    
    Let's do this _only_ for GCC, though, so that the CI/PR builds (which
    build both with clang and with GCC) can guarantee that we _are_ using
    the correct data types.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/fsmonitor/fsm-listen-darwin.c
+++ compat/fsmonitor/fsm-listen-darwin.c
@@ -1,3 +1,27 @@
+#ifndef __clang__
+#include "fsm-darwin-gcc.h"
+#else
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+
+#ifndef AVAILABLE_MAC_OS_X_VERSION_10_13_AND_LATER
+/*
+ * This enum value was added in 10.13 to:
+ *
+ * /Applications/Xcode.app/Contents/Developer/Platforms/ \
+ *    MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/ \
+ *    Library/Frameworks/CoreServices.framework/Frameworks/ \
+ *    FSEvents.framework/Versions/Current/Headers/FSEvents.h
+ *
+ * If we're compiling against an older SDK, this symbol won't be
+ * present.  Silently define it here so that we don't have to ifdef
+ * the logging or masking below.  This should be harmless since older
+ * versions of macOS won't ever emit this FS event anyway.
+ */
+#define kFSEventStreamEventFlagItemCloned         0x00400000
+#endif
+#endif
+
 #include "cache.h"
 #include "fsmonitor.h"
 #include "fsm-listen.h"
