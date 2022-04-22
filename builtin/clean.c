e79d110273a84815b7933c2ae8fb59d41b3852b6
Ben Boeckel <mathstuf@gmail.com>
2022-04-22 09:06:23 -0400

    clean: suggest using `core.longPaths` if paths are too long to remove
    On Windows, git repositories may have extra files which need cleaned
    (e.g., a build directory) that may be arbitrarily deep. Suggest using
    `core.longPaths` if such situations are encountered.
    
    Fixes: #2715
    Signed-off-by: Ben Boeckel <mathstuf@gmail.com>

--- builtin/clean.c
+++ builtin/clean.c
@@ -22,6 +22,7 @@
 #include "pathspec.h"
 #include "help.h"
 #include "prompt.h"
+#include "advice.h"
 
 static int require_force = -1; /* unset */
 static int interactive;
@@ -217,6 +218,9 @@ static int remove_dirs(struct strbuf *path, const char *prefix, int force_flag,
 			quote_path(path->buf, prefix, &quoted, 0);
 			errno = saved_errno;
 			warning_errno(_(msg_warn_remove_failed), quoted.buf);
+			if (saved_errno == ENAMETOOLONG) {
+				advise_if_enabled(ADVICE_NAME_TOO_LONG, _("Setting `core.longPaths` may allow the deletion to succeed."));
+			}
 			*dir_gone = 0;
 		}
 		ret = res;
@@ -252,6 +256,9 @@ static int remove_dirs(struct strbuf *path, const char *prefix, int force_flag,
 				quote_path(path->buf, prefix, &quoted, 0);
 				errno = saved_errno;
 				warning_errno(_(msg_warn_remove_failed), quoted.buf);
+				if (saved_errno == ENAMETOOLONG) {
+					advise_if_enabled(ADVICE_NAME_TOO_LONG, _("Setting `core.longPaths` may allow the deletion to succeed."));
+				}
 				*dir_gone = 0;
 				ret = 1;
 			}
@@ -295,6 +302,9 @@ static int remove_dirs(struct strbuf *path, const char *prefix, int force_flag,
 				quote_path(path->buf, prefix, &quoted, 0);
 				errno = saved_errno;
 				warning_errno(_(msg_warn_remove_failed), quoted.buf);
+				if (saved_errno == ENAMETOOLONG) {
+					advise_if_enabled(ADVICE_NAME_TOO_LONG, _("Setting `core.longPaths` may allow the deletion to succeed."));
+				}
 				*dir_gone = 0;
 				ret = 1;
 			}
@@ -1107,6 +1117,9 @@ int cmd_clean(int argc,
 				qname = quote_path(item->string, NULL, &buf, 0);
 				errno = saved_errno;
 				warning_errno(_(msg_warn_remove_failed), qname);
+				if (saved_errno == ENAMETOOLONG) {
+					advise_if_enabled(ADVICE_NAME_TOO_LONG, _("Setting `core.longPaths` may allow the deletion to succeed."));
+				}
 				errors++;
 			} else if (!quiet) {
 				qname = quote_path(item->string, NULL, &buf, 0);
