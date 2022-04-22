d306f3a1792a0a5cf89cd1d355cd66050079f408
Ben Boeckel <mathstuf@gmail.com>
2022-04-22 09:06:23 -0400

    clean: suggest using `core.longPaths` if paths are too long to remove
    On Windows, git repositories may have extra files which need cleaned
    (e.g., a build directory) that may be arbitrarily deep. Suggest using
    `core.longPaths` if such situations are encountered.
    
    Fixes: #2715
    Signed-off-by: Ben Boeckel <mathstuf@gmail.com>

--- advice.c
+++ advice.c
@@ -59,6 +59,7 @@ static struct {
 	[ADVICE_IGNORED_HOOK]				= { "ignoredHook" },
 	[ADVICE_IMPLICIT_IDENTITY]			= { "implicitIdentity" },
 	[ADVICE_MERGE_CONFLICT]				= { "mergeConflict" },
+	[ADVICE_NAME_TOO_LONG]				= { "nameTooLong" },
 	[ADVICE_NESTED_TAG]				= { "nestedTag" },
 	[ADVICE_OBJECT_NAME_WARNING]			= { "objectNameWarning" },
 	[ADVICE_PUSH_ALREADY_EXISTS]			= { "pushAlreadyExists" },
