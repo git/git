d1c9195116beb18b2e8d9caf37d21289bb5f48b3
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2022-05-12 15:39:38 -0700

    ci: avoid brew for installing perforce
    Perfoce's cask in brew is meant[1] to be used only by humans, so replace
    its use from the CI with a scripted binary download which is less likely
    to fail, as it is done in Linux.
    
    Kept the logic together so it will be less likely to break when moved
    around as on the fly code changes in this area are settled, at which
    point it will also feasable to ammend it to avoid some of the hardcoded
    values by using similar variables to the ones Linux does.
    
    In that same line, a POSIX sh syntax is used instead of the similar one
    used in Linux in preparation for an unrelated future change that might
    change the shell currently configured for it.
    
    This change reintroduces the risk that the installed binaries might not
    work because of being quarantined that was fixed with 5ed9fc3fc86 (ci:
    prevent `perforce` from being quarantined, 2020-02-27) but fixing that
    now was also punted for simplicity and since the affected cloud provider
    is scheduled to be retired with an on the fly change, but should be
    addressed if that other change is not integrated further.
    
    The discussion on the need to keep 2 radically different versions of
    the binaries to be tested with Linux vs macOS or how to upgrade to
    newer versions now that brew won't do that automatically for us has
    been punted for now as well.  On that line the now obsolete comment
    about it in lib.sh was originally being updated by this change but
    created conflicts as it is moved around by other on the fly changes,
    so will be addressed independently as well.
    
    [1] https://github.com/Homebrew/homebrew-cask/pull/122347#discussion_r856026584
    
    Signed-off-by: Carlo Marcelo Arenas Belón <carenas@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -37,13 +37,14 @@ macos-latest)
 	test -z "$BREW_INSTALL_PACKAGES" ||
 	brew install $BREW_INSTALL_PACKAGES
 	brew link --force gettext
-	brew install --cask --no-quarantine perforce || {
-		# Update the definitions and try again
-		cask_repo="$(brew --repository)"/Library/Taps/homebrew/homebrew-cask &&
-		git -C "$cask_repo" pull --no-stat --ff-only &&
-		brew install --cask --no-quarantine perforce
-	} ||
-	brew install homebrew/cask/perforce
+	mkdir -p $HOME/bin
+	(
+		cd $HOME/bin
+		wget -q "https://cdist2.perforce.com/perforce/r21.2/bin.macosx1015x86_64/helix-core-server.tgz" &&
+		tar -xf helix-core-server.tgz
+	)
+	PATH="$PATH:${HOME}/bin"
+	export PATH
 
 	if test -n "$CC_PACKAGE"
 	then
