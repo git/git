f0db573531a86cf9a132a20e77e8efb1fe558996
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-06-21 23:12:07 +0200

    fixup! Allow debugging unsafe directories' ownership
    Let's also hint to the user in the error message that they can debug
    this further.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- setup.c
+++ setup.c
@@ -1385,10 +1385,17 @@ const char *setup_git_directory_gently(int *nongit_ok)
 		if (!nongit_ok) {
 			struct strbuf prequoted = STRBUF_INIT;
 			struct strbuf quoted = STRBUF_INIT;
+			struct strbuf hint = STRBUF_INIT;
 
 #ifdef __MINGW32__
 			if (dir.buf[0] == '/')
 				strbuf_addstr(&prequoted, "%(prefix)/");
+			if (!git_env_bool("GIT_TEST_DEBUG_UNSAFE_DIRECTORIES", 0))
+				strbuf_addstr(&hint,
+					      _("\n\nSet the environment variable "
+						"GIT_TEST_DEBUG_UNSAFE_DIRECTORIES=true "
+						"and run\n"
+						"again for more information."));
 #endif
 
 			strbuf_add(&prequoted, dir.buf, dir.len);
@@ -1397,8 +1404,8 @@ const char *setup_git_directory_gently(int *nongit_ok)
 			die(_("unsafe repository ('%s' is owned by someone else)\n"
 			      "To add an exception for this directory, call:\n"
 			      "\n"
-			      "\tgit config --global --add safe.directory %s"),
-			    dir.buf, quoted.buf);
+			      "\tgit config --global --add safe.directory %s%s"),
+			    dir.buf, quoted.buf, hint.buf);
 		}
 		*nongit_ok = 1;
 		break;
