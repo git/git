386e7a9d307938eb9827c1c65ef75dc38daf0b3c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-06-30 12:18:34 +0200

    tests: add missing double quotes to included library paths
    Fix inclusion errors which would occur if the $TEST_DIRECTORY had $IFS
    whitespace in it.
    
    See d42bab442d7 (core.fsyncmethod: tests for batch mode, 2022-04-04)
    and a242c150ebb (vimdiff: integrate layout tests in the unit tests
    framework ('t' folder), 2022-03-30) for the two relevant commits. Both
    were first released with v2.37.0-rc0 (and were also part of v2.37.0).
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7609-mergetool--lib.sh
+++ t/t7609-mergetool--lib.sh
@@ -7,7 +7,7 @@ Testing basic merge tools options'
 . ./test-lib.sh
 
 test_expect_success 'mergetool --tool=vimdiff creates the expected layout' '
-	. $GIT_BUILD_DIR/mergetools/vimdiff &&
+	. "$GIT_BUILD_DIR"/mergetools/vimdiff &&
 	run_unit_tests
 '
 
