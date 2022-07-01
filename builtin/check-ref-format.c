99b6c45d8f846ed055ba814d985084733d36dabc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-07-01 12:42:50 +0200

    check-ref-format: fix trivial memory leak
    Fix a memory leak in "git check-ref-format" that's been present in the
    code in one form or another since 38eedc634bc (git check-ref-format
    --print, 2009-10-12), the code got substantially refactored in
    cfbe22f03f9 (check-ref-format: handle subcommands in separate
    functions, 2010-08-05).
    
    As a result we can mark a test as passing with SANITIZE=leak using
    "TEST_PASSES_SANITIZE_LEAK=true".
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/check-ref-format.c
+++ builtin/check-ref-format.c
@@ -57,6 +57,8 @@ int cmd_check_ref_format(int argc, const char **argv, const char *prefix)
 	int normalize = 0;
 	int flags = 0;
 	const char *refname;
+	char *to_free = NULL;
+	int ret = 1;
 
 	if (argc == 2 && !strcmp(argv[1], "-h"))
 		usage(builtin_check_ref_format_usage);
@@ -81,11 +83,14 @@ int cmd_check_ref_format(int argc, const char **argv, const char *prefix)
 
 	refname = argv[i];
 	if (normalize)
-		refname = collapse_slashes(refname);
+		refname = to_free = collapse_slashes(refname);
 	if (check_refname_format(refname, flags))
-		return 1;
+		goto cleanup;
 	if (normalize)
 		printf("%s\n", refname);
 
-	return 0;
+	ret = 0;
+cleanup:
+	free(to_free);
+	return ret;
 }
