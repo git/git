dc88e349a297de6b7d0e21d81ac98f7816fcd473
Siddharth Asthana <siddharthasthana31@gmail.com>
2022-07-19 01:21:00 +0530

    ident: move commit_rewrite_person() to ident.c
    commit_rewrite_person() and rewrite_ident_line() are static functions
    defined in revision.c.
    
    Their usages are as follows:
    - commit_rewrite_person() takes a commit buffer and replaces the author
      and committer idents with their canonical versions using the mailmap
      mechanism
    - rewrite_ident_line() takes author/committer header lines from the
      commit buffer and replaces the idents with their canonical versions
      using the mailmap mechanism.
    
    This patch moves commit_rewrite_person() and rewrite_ident_line() to
    ident.c which contains many other functions related to idents like
    split_ident_line(). By moving commit_rewrite_person() to ident.c, we
    also intend to use it in git-cat-file to replace committer and author
    idents from the headers to their canonical versions using the mailmap
    mechanism. The function is moved as is for now to make it clear that
    there are no other changes, but it will be renamed in a following
    commit.
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: John Cai <johncai86@gmail.com>
    Signed-off-by: Siddharth Asthana <siddharthasthana31@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1689,6 +1689,12 @@ struct ident_split {
 int split_ident_line(struct ident_split *, const char *, int);
 
 /*
+ * Given a commit object buffer and the commit headers, replaces the idents
+ * in the headers with their canonical versions using the mailmap mechanism.
+ */
+void commit_rewrite_person(struct strbuf *, const char **, struct string_list *);
+
+/*
  * Compare split idents for equality or strict ordering. Note that we
  * compare only the ident part of the line, ignoring any timestamp.
  *
