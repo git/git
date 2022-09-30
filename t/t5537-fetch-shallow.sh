d9fcaeece2ff946dc3648a22252a92c5400fabe0
Taylor Blau <me@ttaylorr.com>
2022-09-30 16:47:00 -0400

    t5537: prepare for changing protocol.file.allow
    Explicitly cloning over the "file://" protocol in t5537 in preparation
    for merging a security release which will change the default value of
    this configuration to be "user".
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- t/t5537-fetch-shallow.sh
+++ t/t5537-fetch-shallow.sh
@@ -175,7 +175,8 @@ test_expect_success 'fetch --update-shallow into a repo with submodules' '
 test_expect_success 'fetch --update-shallow a commit that is also a shallow point into a repo with submodules' '
 	test_when_finished "rm -rf repo-with-sub" &&
 	git init repo-with-sub &&
-	git -C repo-with-sub submodule add ../a-submodule a-submodule &&
+	git -c protocol.file.allow=always -C repo-with-sub \
+		submodule add ../a-submodule a-submodule &&
 	git -C repo-with-sub commit -m "added submodule" &&
 
 	SHALLOW=$(cat shallow/.git/shallow) &&
