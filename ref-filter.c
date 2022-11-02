8e1c5fcf286322c019e376563781458e6a719231
Jeff King <peff@peff.net>
2022-11-02 03:44:00 -0400

    ref-filter: fix parsing of signatures with CRLF and no body
    This commit fixes a bug when parsing tags that have CRLF line endings, a
    signature, and no body, like this (the "^M" are marking the CRs):
    
      this is the subject^M
      -----BEGIN PGP SIGNATURE-----^M
      ^M
      ...some stuff...^M
      -----END PGP SIGNATURE-----^M
    
    When trying to find the start of the body, we look for a blank line
    separating the subject and body. In this case, there isn't one. But we
    search for it using strstr(), which will find the blank line in the
    signature.
    
    In the non-CRLF code path, we check whether the line we found is past
    the start of the signature, and if so, put the body pointer at the start
    of the signature (effectively making the body empty). But the CRLF code
    path doesn't catch the same case, and we end up with the body pointer in
    the middle of the signature field. This has two visible problems:
    
      - printing %(contents:subject) will show part of the signature, too,
        since the subject length is computed as (body - subject)
    
      - the length of the body is (sig - body), which makes it negative.
        Asking for %(contents:body) causes us to cast this to a very large
        size_t when we feed it to xmemdupz(), which then complains about
        trying to allocate too much memory.
    
    These are essentially the same bugs fixed in the previous commit, except
    that they happen when there is a CRLF blank line in the signature,
    rather than no blank line at all. Both are caused by the refactoring in
    9f75ce3d8f (ref-filter: handle CRLF at end-of-line more gracefully,
    2020-10-29).
    
    We can fix this by doing the same "sigstart" check that we do in the
    non-CRLF case. And rather than repeat ourselves, we can just use
    short-circuiting OR to collapse both cases into a single conditional.
    I.e., rather than:
    
      if (strstr("\n\n"))
        ...found blank, check if it's in signature...
      else if (strstr("\r\n\r\n"))
        ...found blank, check if it's in signature...
      else
        ...no blank line found...
    
    we can collapse this to:
    
      if (strstr("\n\n")) ||
          strstr("\r\n\r\n")))
        ...found blank, check if it's in signature...
      else
        ...no blank line found...
    
    The tests show the problem and the fix. Though it wasn't broken, I
    included contents:signature here to make sure it still behaves as
    expected, but note the shell hackery needed to make it work. A
    less-clever option would be to skip using test_atom and just "append_cr
    >expected" ourselves.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- ref-filter.c
+++ ref-filter.c
@@ -1375,10 +1375,10 @@ static void find_subpos(const char *buf,
 	/* subject is first non-empty line */
 	*sub = buf;
 	/* subject goes to first empty line before signature begins */
-	if ((eol = strstr(*sub, "\n\n"))) {
+	if ((eol = strstr(*sub, "\n\n")) ||
+	    (eol = strstr(*sub, "\r\n\r\n"))) {
 		eol = eol < sigstart ? eol : sigstart;
-	/* check if message uses CRLF */
-	} else if (! (eol = strstr(*sub, "\r\n\r\n"))) {
+	} else {
 		/* treat whole message as subject */
 		eol = sigstart;
 	}
