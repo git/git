cc74a4ac7255510b4a928d4973dcfcc746d74e8f
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-11-08 15:10:32 +0100

    submodule--helper: move "config" to a test-tool
    As with other moves to "test-tool" in f322e9f51b5 (Merge branch
    'ab/submodule-helper-prep', 2022-09-13) the "config" sub-command was
    only used by our own tests.
    
    It was last used by "git submodule" itself in code that went away with
    a6226fd772b (submodule--helper: convert the bulk of cmd_add() to C,
    2021-08-10).
    
    Let's move it over, and while doing so make it easier to reason about
    by splitting up the various uses for it into separate sub-commands, so
    that we don't need to count arguments to see what it does.
    
    This also has the advantage that we stop wasting future translator
    time on this command, currently the usage information for this
    internal-only tool has been translated into several languages. The use
    of the "_" function has also been removed from the "please make
    sure..." message.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -2861,51 +2861,6 @@ cleanup:
 	return ret;
 }
 
-static int module_config(int argc, const char **argv, const char *prefix)
-{
-	enum {
-		CHECK_WRITEABLE = 1,
-		DO_UNSET = 2
-	} command = 0;
-	struct option module_config_options[] = {
-		OPT_CMDMODE(0, "check-writeable", &command,
-			    N_("check if it is safe to write to the .gitmodules file"),
-			    CHECK_WRITEABLE),
-		OPT_CMDMODE(0, "unset", &command,
-			    N_("unset the config in the .gitmodules file"),
-			    DO_UNSET),
-		OPT_END()
-	};
-	const char *const git_submodule_helper_usage[] = {
-		N_("git submodule--helper config <name> [<value>]"),
-		N_("git submodule--helper config --unset <name>"),
-		"git submodule--helper config --check-writeable",
-		NULL
-	};
-
-	argc = parse_options(argc, argv, prefix, module_config_options,
-			     git_submodule_helper_usage, PARSE_OPT_KEEP_ARGV0);
-
-	if (argc == 1 && command == CHECK_WRITEABLE)
-		return is_writing_gitmodules_ok() ? 0 : -1;
-
-	/* Equivalent to ACTION_GET in builtin/config.c */
-	if (argc == 2 && command != DO_UNSET)
-		return print_config_from_gitmodules(the_repository, argv[1]);
-
-	/* Equivalent to ACTION_SET in builtin/config.c */
-	if (argc == 3 || (argc == 2 && command == DO_UNSET)) {
-		const char *value = (argc == 3) ? argv[2] : NULL;
-
-		if (!is_writing_gitmodules_ok())
-			die(_("please make sure that the .gitmodules file is in the working tree"));
-
-		return config_set_in_gitmodules_file_gently(argv[1], value);
-	}
-
-	usage_with_options(git_submodule_helper_usage, module_config_options);
-}
-
 static int module_set_url(int argc, const char **argv, const char *prefix)
 {
 	int quiet = 0;
@@ -3424,7 +3379,6 @@ static struct cmd_struct commands[] = {
 	{"summary", module_summary, 0},
 	{"push-check", push_check, 0},
 	{"absorbgitdirs", absorb_git_dirs, SUPPORT_SUPER_PREFIX},
-	{"config", module_config, 0},
 	{"set-url", module_set_url, 0},
 	{"set-branch", module_set_branch, 0},
 	{"create-branch", module_create_branch, 0},
