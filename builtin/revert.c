37be611d0b6a1e2f33c3a45a332d1214f0eda216
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-11-08 19:17:49 +0100

    cherry-pick: free "struct replay_opts" members
    Call the release_revisions() function added in
    1878b5edc03 (revision.[ch]: provide and start using a
    release_revisions(), 2022-04-13) in cmd_cherry_pick(), as well as
    freeing the xmalloc()'d "revs" member itself.
    
    This is the same change as the one made for cmd_revert() a few lines
    above it in fd74ac95ac3 (revert: free "struct replay_opts" members,
    2022-07-01).
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- builtin/revert.c
+++ builtin/revert.c
@@ -261,6 +261,9 @@ int cmd_cherry_pick(int argc, const char **argv, const char *prefix)
 	opts.action = REPLAY_PICK;
 	sequencer_init_config(&opts);
 	res = run_sequencer(argc, argv, &opts);
+	if (opts.revs)
+		release_revisions(opts.revs);
+	free(opts.revs);
 	if (res < 0)
 		die(_("cherry-pick failed"));
 	return res;
