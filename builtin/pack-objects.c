fe032566c21458befcc0638d03732ce36cacd43d
Christian Couder <chriscool@tuxfamily.org>
2022-11-22 18:51:49 +0100

    pack-objects: allow --filter without --stdout
    9535ce7337 (pack-objects: add list-objects filtering, 2017-11-21)
    taught pack-objects to use --filter, but required the use of
    --stdout since a partial clone mechanism was not yet in place to
    handle missing objects. Since then, changes like 9e27beaa23
    (promisor-remote: implement promisor_remote_get_direct(), 2019-06-25)
    and others added support to dynamically fetch objects that were missing.
    
    Remove the --stdout requirement so that in a follow-up commit, repack
    can pass --filter to pack-objects to omit certain objects from the
    resulting packfile.
    
    Signed-off-by: John Cai <johncai86@gmail.com>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/pack-objects.c
+++ builtin/pack-objects.c
@@ -4385,12 +4385,8 @@ int cmd_pack_objects(int argc, const char **argv, const char *prefix)
 	if (!rev_list_all || !rev_list_reflog || !rev_list_index)
 		unpack_unreachable_expiration = 0;
 
-	if (pfd.have_revs && pfd.revs.filter.choice) {
-		if (!pack_to_stdout)
-			die(_("cannot use --filter without --stdout"));
-		if (stdin_packs)
+	if (pfd.have_revs && pfd.revs.filter.choice && stdin_packs)
 			die(_("cannot use --filter with --stdin-packs"));
-	}
 
 	if (stdin_packs && use_internal_rev_list)
 		die(_("cannot use internal rev list with --stdin-packs"));
