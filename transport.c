7cce9074a728fb32501054577963d8ae31d007be
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-12-22 15:14:10 +0000

    bundle-uri client: add boolean transfer.bundleURI setting
    The yet-to-be introduced client support for bundle-uri will always
    fall back on a full clone, but we'd still like to be able to ignore a
    server's bundle-uri advertisement entirely.
    
    The new transfer.bundleURI config option defaults to 'false', but a user
    can set it to 'true' to enable checking for bundle URIs from the origin
    Git server using protocol v2.
    
    Co-authored-by: Derrick Stolee <derrickstolee@github.com>
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport.c
+++ transport.c
@@ -1516,12 +1516,20 @@ int transport_fetch_refs(struct transport *transport, struct ref *refs)
 
 int transport_get_remote_bundle_uri(struct transport *transport)
 {
+	int value = 0;
 	const struct transport_vtable *vtable = transport->vtable;
 
 	/* Check config only once. */
 	if (transport->got_remote_bundle_uri)
 		return 0;
 	transport->got_remote_bundle_uri = 1;
+
+	/*
+	 * Don't request bundle-uri from the server unless configured to
+	 * do so by the transfer.bundleURI=true config option.
+	 */
+	if (git_config_get_bool("transfer.bundleuri", &value) || !value)
+		return 0;
 
 	if (!vtable->get_bundle_uri)
 		return error(_("bundle-uri operation not supported by protocol"));
