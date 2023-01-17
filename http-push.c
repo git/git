4fab049258a294e375431e07f343d1752994fba3
Jeff King <peff@peff.net>
2023-01-16 22:04:38 -0500

    http-push: prefer CURLOPT_UPLOAD to CURLOPT_PUT
    The two options do exactly the same thing, but the latter has been
    deprecated and in recent versions of curl may produce a compiler
    warning. Since the UPLOAD form is available everywhere (it was
    introduced in the year 2000 by curl 7.1), we can just switch to it.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- http-push.c
+++ http-push.c
@@ -198,7 +198,7 @@ static void curl_setup_http(CURL *curl, const char *url,
 		const char *custom_req, struct buffer *buffer,
 		curl_write_callback write_fn)
 {
-	curl_easy_setopt(curl, CURLOPT_PUT, 1);
+	curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
 	curl_easy_setopt(curl, CURLOPT_URL, url);
 	curl_easy_setopt(curl, CURLOPT_INFILE, buffer);
 	curl_easy_setopt(curl, CURLOPT_INFILESIZE, buffer->buf.len);
