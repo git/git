7bfffb454c5d837077fe08087e43abb194efd1f4
Glen Choo <chooglen@google.com>
2023-04-07 20:11:42 -0700

    config: inline git_color_default_config
    git_color_default_config() is a shorthand for calling two other config
    callbacks. There are no other non-static functions that do this and it
    will complicate our refactoring of config_fn_t so inline it instead.
    
    Signed-off-by: Glen Choo <chooglen@google.com>

--- builtin/grep.c
+++ builtin/grep.c
@@ -293,7 +293,10 @@ static int wait_all(void)
 static int grep_cmd_config(const char *var, const char *value, void *cb)
 {
 	int st = grep_config(var, value, cb);
-	if (git_color_default_config(var, value, NULL) < 0)
+
+	if (git_color_config(var, value, cb) < 0)
+		st = -1;
+	else if (git_default_config(var, value, cb) < 0)
 		st = -1;
 
 	if (!strcmp(var, "grep.threads")) {
