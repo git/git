9a5bb4efa0c8c76a0438f5a6d6fdc4df2667e366
Calvin Wan <calvinwan@google.com>
2023-05-12 17:15:14 +0000

    path: move related function to path
    Move path-related function from strbuf.[ch] to path.[ch] so that strbuf
    is focused on string manipulation routines with minimal dependencies.
    
    Signed-off-by: Calvin Wan <calvinwan@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- path.c
+++ path.c
@@ -1213,6 +1213,26 @@ int normalize_path_copy(char *dst, const char *src)
 	return normalize_path_copy_len(dst, src, NULL);
 }
 
+int strbuf_normalize_path(struct strbuf *src)
+{
+	struct strbuf dst = STRBUF_INIT;
+
+	strbuf_grow(&dst, src->len);
+	if (normalize_path_copy(dst.buf, src->buf) < 0) {
+		strbuf_release(&dst);
+		return -1;
+	}
+
+	/*
+	 * normalize_path does not tell us the new length, so we have to
+	 * compute it by looking for the new NUL it placed
+	 */
+	strbuf_setlen(&dst, strlen(dst.buf));
+	strbuf_swap(src, &dst);
+	strbuf_release(&dst);
+	return 0;
+}
+
 /*
  * path = Canonical absolute path
  * prefixes = string_list containing normalized, absolute paths without
