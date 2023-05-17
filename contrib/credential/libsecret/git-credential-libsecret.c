aaa68ad022d3100ff0342a515f672c7811644820
Junio C Hamano <gitster@pobox.com>
2023-05-17 08:40:07 -0700

    Revert "Merge branch 'mh/credential-password-expiry-libsecret' into next"
    This reverts commit fd898e37564343333ed91e3846cbe78f8e0cb548, reversing
    changes made to c2148408a2ae76fb39d0ecdf1f991ee8dc540936.
    
    With the previous round, because libsecret overwrites items if and
    only if the attributes match exactly, you end up with many items in
    the secret store that differ only by expiry date. This is inelegant,
    and confusing to users.  It was caught before merged to 'master' and
    being replaced.

--- contrib/credential/libsecret/git-credential-libsecret.c
+++ contrib/credential/libsecret/git-credential-libsecret.c
@@ -39,7 +39,6 @@ struct credential {
 	char *path;
 	char *username;
 	char *password;
-	char *password_expiry_utc;
 };
 
 #define CREDENTIAL_INIT { 0 }
@@ -54,21 +53,6 @@ struct credential_operation {
 #define CREDENTIAL_OP_END { NULL, NULL }
 
 /* ----------------- Secret Service functions ----------------- */
-
-static const SecretSchema schema = {
-	"org.git.Password",
-	/* Ignore schema name for backwards compatibility with previous versions */
-	SECRET_SCHEMA_DONT_MATCH_NAME,
-	{
-		{  "user", SECRET_SCHEMA_ATTRIBUTE_STRING },
-		{  "object", SECRET_SCHEMA_ATTRIBUTE_STRING },
-		{  "protocol", SECRET_SCHEMA_ATTRIBUTE_STRING },
-		{  "port", SECRET_SCHEMA_ATTRIBUTE_INTEGER },
-		{  "server", SECRET_SCHEMA_ATTRIBUTE_STRING },
-		{  "password_expiry_utc", SECRET_SCHEMA_ATTRIBUTE_INTEGER },
-		{  NULL, 0 },
-	}
-};
 
 static char *make_label(struct credential *c)
 {
@@ -94,9 +78,6 @@ static GHashTable *make_attr_list(struct credential *c)
 		g_hash_table_insert(al, "port", g_strdup_printf("%hu", c->port));
 	if (c->path)
 		g_hash_table_insert(al, "object", g_strdup(c->path));
-	if (c->password_expiry_utc)
-		g_hash_table_insert(al, "password_expiry_utc",
-			g_strdup(c->password_expiry_utc));
 
 	return al;
 }
@@ -120,7 +101,7 @@ static int keyring_get(struct credential *c)
 
 	attributes = make_attr_list(c);
 	items = secret_service_search_sync(service,
-					   &schema,
+					   SECRET_SCHEMA_COMPAT_NETWORK,
 					   attributes,
 					   SECRET_SEARCH_LOAD_SECRETS | SECRET_SEARCH_UNLOCK,
 					   NULL,
@@ -145,12 +126,6 @@ static int keyring_get(struct credential *c)
 		if (s) {
 			g_free(c->username);
 			c->username = g_strdup(s);
-		}
-
-		s = g_hash_table_lookup(attributes, "password_expiry_utc");
-		if (s) {
-			g_free(c->password_expiry_utc);
-			c->password_expiry_utc = g_strdup(s);
 		}
 
 		s = secret_value_get_text(secret);
@@ -187,7 +162,7 @@ static int keyring_store(struct credential *c)
 
 	label = make_label(c);
 	attributes = make_attr_list(c);
-	secret_password_storev_sync(&schema,
+	secret_password_storev_sync(SECRET_SCHEMA_COMPAT_NETWORK,
 				    attributes,
 				    NULL,
 				    label,
@@ -223,7 +198,7 @@ static int keyring_erase(struct credential *c)
 		return EXIT_FAILURE;
 
 	attributes = make_attr_list(c);
-	secret_password_clearv_sync(&schema,
+	secret_password_clearv_sync(SECRET_SCHEMA_COMPAT_NETWORK,
 				    attributes,
 				    NULL,
 				    &error);
@@ -263,7 +238,6 @@ static void credential_clear(struct credential *c)
 	g_free(c->path);
 	g_free(c->username);
 	g_free(c->password);
-	g_free(c->password_expiry_utc);
 
 	credential_init(c);
 }
@@ -310,9 +284,6 @@ static int credential_read(struct credential *c)
 		} else if (!strcmp(key, "username")) {
 			g_free(c->username);
 			c->username = g_strdup(value);
-		} else if (!strcmp(key, "password_expiry_utc")) {
-			g_free(c->password_expiry_utc);
-			c->password_expiry_utc = g_strdup(value);
 		} else if (!strcmp(key, "password")) {
 			g_free(c->password);
 			c->password = g_strdup(value);
@@ -340,11 +311,9 @@ static void credential_write_item(FILE *fp, const char *key, const char *value)
 
 static void credential_write(const struct credential *c)
 {
-	/* only write username/password/expiry, if set */
+	/* only write username/password, if set */
 	credential_write_item(stdout, "username", c->username);
 	credential_write_item(stdout, "password", c->password);
-	credential_write_item(stdout, "password_expiry_utc",
-		c->password_expiry_utc);
 }
 
 static void usage(const char *name)
