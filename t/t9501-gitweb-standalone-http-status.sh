024ceebd6385d85a10d885fa741a39069b475749
John Cai <johncai86@gmail.com>
2023-05-17 22:10:09 -0400

    t9501-gitweb-standalone-http-status: modernize test format
    Some tests still use the old format with four spaces indentation.
    Standardize the tests to the new format with tab indentation.
    
    Signed-off-by: John Cai <johncai86@gmail.com>

--- t/t9501-gitweb-standalone-http-status.sh
+++ t/t9501-gitweb-standalone-http-status.sh
@@ -35,42 +35,42 @@ cat >>gitweb_config.perl <<\EOF
 $feature{'snapshot'}{'override'} = 0;
 EOF
 
-test_expect_success \
-    'snapshots: tgz only default format enabled' \
-    'gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
-    grep "Status: 200 OK" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tbz2" &&
-    grep "403 - Unsupported snapshot format" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=txz" &&
-    grep "403 - Snapshot format not allowed" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
-    grep "403 - Unsupported snapshot format" gitweb.output'
+test_expect_success 'snapshots: tgz only default format enabled' '
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
+	grep "Status: 200 OK" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tbz2" &&
+	grep "403 - Unsupported snapshot format" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=txz" &&
+	grep "403 - Snapshot format not allowed" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
+	grep "403 - Unsupported snapshot format" gitweb.output
+'
 
 
 cat >>gitweb_config.perl <<\EOF
 $feature{'snapshot'}{'default'} = ['tgz','tbz2','txz','zip'];
 EOF
 
-test_expect_success \
-    'snapshots: all enabled in default, use default disabled value' \
-    'gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
-    grep "Status: 200 OK" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tbz2" &&
-    grep "Status: 200 OK" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=txz" &&
-    grep "403 - Snapshot format not allowed" gitweb.output &&
-    gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
-    grep "Status: 200 OK" gitweb.output'
+test_expect_success 'snapshots: all enabled in default, use default disabled value' '
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
+	grep "Status: 200 OK" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tbz2" &&
+	grep "Status: 200 OK" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=txz" &&
+	grep "403 - Snapshot format not allowed" gitweb.output &&
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
+	grep "Status: 200 OK" gitweb.output
+'
 
 
 cat >>gitweb_config.perl <<\EOF
 $known_snapshot_formats{'zip'}{'disabled'} = 1;
 EOF
 
-test_expect_success \
-    'snapshots: zip explicitly disabled' \
-    'gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
-    grep "403 - Snapshot format not allowed" gitweb.output'
+test_expect_success 'snapshots: zip explicitly disabled' '
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=zip" &&
+	grep "403 - Snapshot format not allowed" gitweb.output
+'
 test_debug 'cat gitweb.output'
 
 
@@ -78,10 +78,11 @@ cat >>gitweb_config.perl <<\EOF
 $known_snapshot_formats{'tgz'}{'disabled'} = 0;
 EOF
 
-test_expect_success \
-    'snapshots: tgz explicitly enabled' \
-    'gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
-    grep "Status: 200 OK" gitweb.output'
+test_expect_success 'snapshots: tgz explicitly enabled' '
+	gitweb_run "p=.git;a=snapshot;h=HEAD;sf=tgz" &&
+	grep "Status: 200 OK" gitweb.output
+'
+
 test_debug 'cat gitweb.headers'
 
 
