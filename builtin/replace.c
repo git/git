80d4e5f3a5c6d2e07b8e8f82801d8e8c4445fefd
Jeff King <peff@peff.net>
2023-07-03 02:44:25 -0400

    replace: mark unused parameter in ref callback
    We don't look at the "flags" parameter, which is natural for something
    that is just printing the contents of the replace refs. But let's mark
    it to appease -Wunused-parameter.
    
    This probably should have been part of 63e14ee2d6 (refs: mark unused
    each_ref_fn parameters, 2022-08-19), but I missed it as this one is a
    repo_each_ref_fn, which takes an extra repository argument.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/replace.c
+++ builtin/replace.c
@@ -49,7 +49,7 @@ struct show_data {
 
 static int show_reference(struct repository *r, const char *refname,
 			  const struct object_id *oid,
-			  int flag, void *cb_data)
+			  int flag UNUSED, void *cb_data)
 {
 	struct show_data *data = cb_data;
 
