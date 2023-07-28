6d04de38f2605e317fb53f0fe721de4e1ffcbc3c
David Lomas <dl3@pale-eds.co.uk>
2023-07-28 15:31:25 +0100

    mingw: suggest `windows.appendAtomically` in more cases
    When running Git for Windows on a remote APFS filesystem, it would
    appear that the `mingw_open_append()`/`write()` combination would fail
    almost exactly like on some CIFS-mounted shares as had been reported in
    https://github.com/git-for-windows/git/issues/2753, albeit with a
    different `errno` value.
    
    Let's handle that `errno` value just the same, by suggesting to set
    `windows.appendAtomically=false`.
    
    Signed-off-by: David Lomas <dl3@pale-eds.co.uk>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -705,7 +705,7 @@ ssize_t mingw_write(int fd, const void *buf, size_t len)
 {
 	ssize_t result = write(fd, buf, len);
 
-	if (result < 0 && errno == EINVAL && buf) {
+	if (result < 0 && (errno == EINVAL || errno == EBADF) && buf) {
 		/* check if fd is a pipe */
 		HANDLE h = (HANDLE) _get_osfhandle(fd);
 		if (GetFileType(h) == FILE_TYPE_PIPE)
