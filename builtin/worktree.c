bbfc4f53b9622ba996c1cd083eaa6cb10d84f6d0
Jeff King <peff@peff.net>
2023-08-29 19:45:15 -0400

    worktree: mark unused parameters in each_ref_fn callback
    This is similar to the cases in 63e14ee2d6 (refs: mark unused
    each_ref_fn parameters, 2022-08-19), but it was added after that commit.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/worktree.c
+++ builtin/worktree.c
@@ -628,10 +628,10 @@ static void print_preparing_worktree_line(int detach,
  *
  * Returns 0 on failure and non-zero on success.
  */
-static int first_valid_ref(const char *refname,
-			   const struct object_id *oid,
-			   int flags,
-			   void *cb_data)
+static int first_valid_ref(const char *refname UNUSED,
+			   const struct object_id *oid UNUSED,
+			   int flags UNUSED,
+			   void *cb_data UNUSED)
 {
 	return 1;
 }
