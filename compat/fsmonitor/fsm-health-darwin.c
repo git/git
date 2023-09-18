4cb5e0b3b98c70912756551238b91fccf5f09c1e
Jeff King <peff@peff.net>
2023-09-18 18:32:38 -0400

    fsmonitor: mark unused parameters in stub functions
    The fsmonitor code has some platform-specific functions for which one or
    more platforms implement noop or stub functions. We can't get rid of
    these functions nor change their interface, since the point is to match
    their equivalents in other platforms. But let's annotate their
    parameters to quiet the compiler's -Wunused-parameter warning.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/fsmonitor/fsm-health-darwin.c
+++ compat/fsmonitor/fsm-health-darwin.c
@@ -4,21 +4,21 @@
 #include "fsm-health.h"
 #include "fsmonitor--daemon.h"
 
-int fsm_health__ctor(struct fsmonitor_daemon_state *state)
+int fsm_health__ctor(struct fsmonitor_daemon_state *state UNUSED)
 {
 	return 0;
 }
 
-void fsm_health__dtor(struct fsmonitor_daemon_state *state)
+void fsm_health__dtor(struct fsmonitor_daemon_state *state UNUSED)
 {
 	return;
 }
 
-void fsm_health__loop(struct fsmonitor_daemon_state *state)
+void fsm_health__loop(struct fsmonitor_daemon_state *state UNUSED)
 {
 	return;
 }
 
-void fsm_health__stop_async(struct fsmonitor_daemon_state *state)
+void fsm_health__stop_async(struct fsmonitor_daemon_state *state UNUSED)
 {
 }
