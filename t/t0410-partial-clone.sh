e98839843b1169b92d863eb56f1d3299c6fad8b9
Patrick Steinhardt <ps@pks.im>
2024-02-15 09:25:34 +0100

    t0410: convert tests to use DEFAULT_REPO_FORMAT prereq
    In t0410 we have two tests which exercise how partial clones behave in
    the context of a repository with extensions. These tests are marked to
    require a repository using SHA1 and the "files" backend because we
    explicitly set the repository format version to 0, and setting up either
    the "objectFormat" or "refStorage" extensions requires a repository
    format version of 1.
    
    We have recently introduced a new DEFAULT_REPO_FORMAT prerequisite.
    Despite capturing the intent more directly, it also has the added
    benefit that it can easily be extended in the future in case we add new
    repository extensions. Adapt the tests to use it.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t0410-partial-clone.sh
+++ t/t0410-partial-clone.sh
@@ -49,7 +49,7 @@ test_expect_success 'convert shallow clone to partial clone' '
 	test_cmp_config -C client 1 core.repositoryformatversion
 '
 
-test_expect_success SHA1,REFFILES 'convert to partial clone with noop extension' '
+test_expect_success DEFAULT_REPO_FORMAT 'convert to partial clone with noop extension' '
 	rm -fr server client &&
 	test_create_repo server &&
 	test_commit -C server my_commit 1 &&
@@ -60,7 +60,7 @@ test_expect_success SHA1,REFFILES 'convert to partial clone with noop extension'
 	git -C client fetch --unshallow --filter="blob:none"
 '
 
-test_expect_success SHA1,REFFILES 'converting to partial clone fails with unrecognized extension' '
+test_expect_success DEFAULT_REPO_FORMAT 'converting to partial clone fails with unrecognized extension' '
 	rm -fr server client &&
 	test_create_repo server &&
 	test_commit -C server my_commit 1 &&
