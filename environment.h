103d563f3716385080594f54ae312cc32bf7ec4d
Jeff King <peff@peff.net>
2024-03-12 05:17:47 -0400

    environment: drop comment_line_char compatibility macro
    There is no longer any code which references the single-byte
    comment_line_char. Let's drop it, clearing the way for true multi-byte
    entries in comment_line_str.
    
    It's possible there are topics in flight that have added new references
    to comment_line_char. But we would prefer to fail compilation (and then
    fix it) upon merging with this, rather than have them quietly ignore the
    bytes after the first.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- environment.h
+++ environment.h
@@ -8,7 +8,6 @@ struct strvec;
  * The character that begins a commented line in user-editable file
  * that is subject to stripspace.
  */
-#define comment_line_char (comment_line_str[0])
 extern const char *comment_line_str;
 extern int auto_comment_line_char;
 
