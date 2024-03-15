37ea7c4875f4b581a75a6a2ac6c4904040a75580
Beat Bolli <bb@drbeat.li>
2024-03-15 20:46:15 +0100

    t/t5*: merge a "grep | sed" pipeline
    Signed-off-by: Beat Bolli <dev+git@drbeat.li>
    Acked-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5401-update-hooks.sh
+++ t/t5401-update-hooks.sh
@@ -123,7 +123,7 @@ remote: STDOUT post-update
 remote: STDERR post-update
 EOF
 test_expect_success 'send-pack stderr contains hook messages' '
-	grep ^remote: send.err | sed "s/ *\$//" >actual &&
+	sed -n "/^remote:/s/ *\$//p" send.err >actual &&
 	test_cmp expect actual
 '
 
