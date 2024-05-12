fc0202b0e9679d44ebc5806c07439442d0a94a7a
Dr. David Alan Gilbert <dave@treblig.org>
2024-05-12 01:59:13 +0100

    fetch-pack: remove unused 'struct loose_object_iter'
    'struct loose_object_iter' in fetch-pack.c is unused since commit
    97b2fa08 (fetch-pack: drop custom loose object cache, 2018-11-12).
    
    Remove it.
    
    Signed-off-by: Dr. David Alan Gilbert <dave@treblig.org>
    Acked-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fetch-pack.c
+++ fetch-pack.c
@@ -728,11 +728,6 @@ static void mark_alternate_complete(struct fetch_negotiator *unused,
 	mark_complete(&obj->oid);
 }
 
-struct loose_object_iter {
-	struct oidset *loose_object_set;
-	struct ref *refs;
-};
-
 /*
  * Mark recent commits available locally and reachable from a local ref as
  * COMPLETE.
