35dfccb2b430ee67fddb7b1a1f8cd7a1a8fb7cbe
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-05-20 20:22:05 +0000

    Revert "Add a helper function to compare file contents"
    Now that during a `git clone`, the hooks' contents are no longer
    compared to the templates' files', the caller for which the
    `do_files_match()` function was introduced is gone, and therefore this
    function can be retired, too.
    
    This reverts commit 584de0b4c23 (Add a helper function to compare file
    contents, 2024-03-30).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache.h
+++ cache.h
@@ -1785,20 +1785,6 @@ int copy_fd(int ifd, int ofd);
 int copy_file(const char *dst, const char *src, int mode);
 int copy_file_with_time(const char *dst, const char *src, int mode);
 
-/*
- * Compare the file mode and contents of two given files.
- *
- * If both files are actually symbolic links, the function returns 1 if the link
- * targets are identical or 0 if they are not.
- *
- * If any of the two files cannot be accessed or in case of read failures, this
- * function returns 0.
- *
- * If the file modes and contents are identical, the function returns 1,
- * otherwise it returns 0.
- */
-int do_files_match(const char *path1, const char *path2);
-
 void write_or_die(int fd, const void *buf, size_t count);
 void fsync_or_die(int fd, const char *);
 int fsync_component(enum fsync_component component, int fd);
