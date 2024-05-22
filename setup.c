407997c1ddbbedfda2eb7cdc5848781c96b37dc3
Patrick Steinhardt <ps@pks.im>
2024-05-22 12:38:46 +0200

    setup: fix bug with "includeIf.onbranch" when initializing dir
    It was reported that git-init(1) can fail when initializing an existing
    directory in case the config contains an "includeIf.onbranch:"
    condition:
    
        $ mkdir repo
        $ git -c includeIf.onbranch:main.path=nonexistent init repo
        BUG: refs.c:2056: reference backend is unknown
    
    The same error can also be triggered when re-initializing an already
    existing repository.
    
    The bug has been introduced in 173761e21b (setup: start tracking ref
    storage format, 2023-12-29), which wired up the ref storage format. The
    root cause is in `init_db()`, which tries to read the config before we
    have initialized `the_repository` and most importantly its ref storage
    format. We eventually end up calling `include_by_branch()` and execute
    `refs_resolve_ref_unsafe()`, but because we have not initialized the ref
    storage format yet this will trigger the above bug.
    
    Interestingly, `include_by_branch()` has a mechanism that will only
    cause us to resolve the ref when `the_repository->gitdir` is set. This
    is also the reason why this only happens when we initialize an already
    existing directory or repository: `gitdir` is set in those cases, but
    not when creating a new directory.
    
    Now there are two ways to address the issue:
    
      - We can adapt `include_by_branch()` to also make the code conditional
        on whether `the_repository->ref_storage_format` is set.
    
      - We can shift around code such that we initialize the repository
        format before we read the config.
    
    While the first approach would be safe, it may also cause us to paper
    over issues where a ref store should have been set up. In our case for
    example, it may be reasonable to expect that re-initializing the repo
    will cause the "onbranch:" condition to trigger, but we would not do
    that if the ref storage format was not set up yet. This also used to
    work before the above commit that introduced this bug.
    
    Rearrange the code such that we set up the repository format before
    reading the config. This fixes the bug and ensures that "onbranch:"
    conditions can trigger.
    
    Reported-by: Heghedus Razvan <heghedus.razvan@protonmail.com>
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Tested-by: Heghedus Razvan <heghedus.razvan@protonmail.com>
    [jc: fixed a test and backported to v2.44.0 codebase]
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- setup.c
+++ setup.c
@@ -2196,13 +2196,6 @@ int init_db(const char *git_dir, const char *real_git_dir,
 	}
 	startup_info->have_repository = 1;
 
-	/* Ensure `core.hidedotfiles` is processed */
-	git_config(platform_core_config, NULL);
-
-	safe_create_dir(git_dir, 0);
-
-	prev_bare_repository = is_bare_repository();
-
 	/* Check to see if the repository version is right.
 	 * Note that a newly created repository does not have
 	 * config file, so this will not fail.  What we are catching
@@ -2213,16 +2206,27 @@ int init_db(const char *git_dir, const char *real_git_dir,
 	validate_hash_algorithm(&repo_fmt, hash);
 	validate_ref_storage_format(&repo_fmt, ref_storage_format);
 
-	reinit = create_default_files(template_dir, original_git_dir,
-				      &repo_fmt, prev_bare_repository,
-				      init_shared_repository);
-
 	/*
 	 * Now that we have set up both the hash algorithm and the ref storage
 	 * format we can update the repository's settings accordingly.
 	 */
 	repo_set_hash_algo(the_repository, repo_fmt.hash_algo);
 	repo_set_ref_storage_format(the_repository, repo_fmt.ref_storage_format);
+
+	/*
+	 * Ensure `core.hidedotfiles` is processed. This must happen after we
+	 * have set up the repository format such that we can evaluate
+	 * includeIf conditions correctly in the case of re-initialization.
+	 */
+	git_config(platform_core_config, NULL);
+
+	safe_create_dir(git_dir, 0);
+
+	prev_bare_repository = is_bare_repository();
+
+	reinit = create_default_files(template_dir, original_git_dir,
+				      &repo_fmt, prev_bare_repository,
+				      init_shared_repository);
 
 	if (!(flags & INIT_DB_SKIP_REFDB))
 		create_reference_database(repo_fmt.ref_storage_format,
