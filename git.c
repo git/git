1dedc040f0b6be3a50df53fbfcd29feb66f9d8df
Derrick Stolee <stolee@gmail.com>
2024-06-07 12:18:50 -0400

    backfill: add builtin boilerplate
    In anticipation of implementing 'git backfill', populate the necessary files
    with the boilerplate of a new builtin.
    
    RFC TODO: When preparing this for a full implementation, make sure it is
    based on the newest standards introduced by [1].
    
    [1] https://lore.kernel.org/git/xmqqjzfq2f0f.fsf@gitster.g/T/#m606036ea2e75a6d6819d6b5c90e729643b0ff7f7
        [PATCH 1/3] builtin: add a repository parameter for builtin functions
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- git.c
+++ git.c
@@ -509,6 +509,7 @@ static struct cmd_struct commands[] = {
 	{ "annotate", cmd_annotate, RUN_SETUP },
 	{ "apply", cmd_apply, RUN_SETUP_GENTLY },
 	{ "archive", cmd_archive, RUN_SETUP_GENTLY },
+	{ "backfill", cmd_backfill, RUN_SETUP },
 	{ "bisect", cmd_bisect, RUN_SETUP },
 	{ "blame", cmd_blame, RUN_SETUP },
 	{ "branch", cmd_branch, RUN_SETUP | DELAY_PAGER_CONFIG },
