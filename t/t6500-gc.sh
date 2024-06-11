f1160393c172da29d0d0874c7d69ce76dbdc8c0c
Derrick Stolee <derrickstolee@github.com>
2024-06-11 15:09:15 +0000

    commit-graph: increment progress indicator
    This fixes a bug that was introduced by 368d19b0b7 (commit-graph:
    refactor compute_topological_levels(), 2023-03-20): Previously, the
    progress indicator was updated from `i + 1` where `i` is the loop
    variable of the enclosing `for` loop. After this patch, the update used
    `info->progress_cnt + 1` instead, however, unlike `i`, the
    `progress_cnt` attribute was not incremented. Let's increment it.
    
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    [jc: squashed in a test update from Patrick Steinhardt]
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t6500-gc.sh
+++ t/t6500-gc.sh
@@ -174,7 +174,7 @@ test_expect_success TTY 'with TTY: gc --no-quiet' '
 		git -c gc.writeCommitGraph=true gc --no-quiet >stdout 2>stderr &&
 	test_must_be_empty stdout &&
 	test_i18ngrep "Enumerating objects" stderr &&
-	test_i18ngrep "Computing commit graph generation numbers" stderr
+	test_i18ngrep "Computing commit graph generation numbers: 100% (4/4), done." stderr
 '
 
 test_expect_success 'gc --quiet' '
