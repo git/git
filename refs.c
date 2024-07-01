4db87ecf1c5aafb8aa3dc4366679810f9c65406a
shejialuo <shejialuo@gmail.com>
2024-07-01 23:20:26 +0800

    refs: set up ref consistency check infrastructure
    The interfaces defined in the `ref_storage_be` are carefully structured
    in semantic. It's organized as the five parts:
    
    1. The name and the initialization interfaces.
    2. The ref transaction interfaces.
    3. The ref internal interfaces (pack, rename and copy).
    4. The ref filesystem interfaces.
    5. The reflog related interfaces.
    
    To keep consistent with the git-fsck(1), add a new interface named
    "fsck_refs_fn" to the end of "ref_storage_be". This semantic cannot be
    grouped into any above five categories. Explicitly add blank line to
    make it different from others.
    
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: shejialuo <shejialuo@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- refs.c
+++ refs.c
@@ -316,6 +316,11 @@ int check_refname_format(const char *refname, int flags)
 	return check_or_sanitize_refname(refname, flags, NULL);
 }
 
+int refs_fsck(struct ref_store *refs, struct fsck_options *o)
+{
+	return refs->be->fsck(refs, o);
+}
+
 void sanitize_refname_component(const char *refname, struct strbuf *out)
 {
 	if (check_or_sanitize_refname(refname, REFNAME_ALLOW_ONELEVEL, out))
