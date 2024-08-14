9ddd5f755d78dd2a3119b63d5f78ca93e4fd3eba
Patrick Steinhardt <ps@pks.im>
2024-08-14 08:52:00 +0200

    object-name: fix leaking symlink paths in object context
    The object context may be populated with symlink contents when reading a
    symlink, but the associated strbuf doesn't ever get released when
    releasing the object context, causing a memory leak. Plug it.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- object-name.c
+++ object-name.c
@@ -1765,6 +1765,7 @@ int strbuf_check_branch_ref(struct strbuf *sb, const char *name)
 void object_context_release(struct object_context *ctx)
 {
 	free(ctx->path);
+	strbuf_release(&ctx->symlink_path);
 }
 
 /*
