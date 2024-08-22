13b23d2da552b9476a0025562bc4ab8954d5be45
Patrick Steinhardt <ps@pks.im>
2024-08-22 11:18:11 +0200

    transport: fix leaking negotiation tips
    We do not free negotiation tips in the transport's smart options. Fix
    this by freeing them on disconnect.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- transport.c
+++ transport.c
@@ -947,6 +947,10 @@ static int disconnect_git(struct transport *transport)
 		finish_connect(data->conn);
 	}
 
+	if (data->options.negotiation_tips) {
+		oid_array_clear(data->options.negotiation_tips);
+		free(data->options.negotiation_tips);
+	}
 	list_objects_filter_release(&data->options.filter_options);
 	oid_array_clear(&data->extra_have);
 	oid_array_clear(&data->shallow);
