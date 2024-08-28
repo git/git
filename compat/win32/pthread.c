b652382d761607c76258e2e91fa753dffe5c21dc
Jeff King <peff@peff.net>
2024-08-28 00:00:16 -0400

    compat: mark unused parameters in win32/mingw functions
    The compat/ directory contains many stub functions, wrappers, and so on
    that have to conform to a specific interface, but don't necessarily need
    to use all of their parameters. Let's mark them to avoid complaints from
    -Wunused-parameter.
    
    This was done mostly via guess-and-check with the Windows build in
    GitHub CI. I also confirmed that the win+VS build is similarly happy.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/win32/pthread.c
+++ compat/win32/pthread.c
@@ -21,7 +21,7 @@ static unsigned __stdcall win32_start_routine(void *arg)
 	return 0;
 }
 
-int pthread_create(pthread_t *thread, const void *unused,
+int pthread_create(pthread_t *thread, const void *attr UNUSED,
 		   void *(*start_routine)(void *), void *arg)
 {
 	thread->arg = arg;
