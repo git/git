bb27ee4887f865c97c28ba7bdfbaceafb79d4de0
Derrick Stolee <stolee@gmail.com>
2024-09-18 14:38:05 -0400

    pack-objects: thread the path-based compression
    Adapting the implementation of ll_find_deltas(), create a threaded
    version of the --path-walk compression step in 'git pack-objects'.
    
    This involves adding a 'regions' member to the thread_params struct,
    allowing each thread to own a section of paths. We can simplify the way
    jobs are split because there is no value in extending the batch based on
    name-hash the way sections of the object entry array are attempted to be
    grouped. We re-use the 'list_size' and 'remaining' items for the purpose
    of borrowing work in progress from other "victim" threads when a thread
    has finished its batch of work more quickly.
    
    Using the Git repository as a test repo, the p5313 performance test
    shows that the resulting size of the repo is the same, but the threaded
    implementation gives gains of varying degrees depending on the number of
    objects being packed. (This was tested on a 16-core machine.)
    
    Test                                    HEAD~1    HEAD
    -------------------------------------------------------------
    5313.6: thin pack with --path-walk        0.01    0.01  +0.0%
    5313.7: thin pack size with --path-walk    475     475  +0.0%
    5313.12: big pack with --path-walk        1.99    1.87  -6.0%
    5313.13: big pack size with --path-walk  14.4M   14.3M  -0.4%
    5313.18: repack with --path-walk         98.14   41.46 -57.8%
    5313.19: repack size with --path-walk   197.2M  197.3M  +0.0%
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/perf/p5313-pack-objects.sh
+++ t/perf/p5313-pack-objects.sh
@@ -69,7 +69,7 @@ test_perf 'thin pack with --path-walk' '
 '
 
 test_size 'thin pack size with --path-walk' '
-	wc -c <out
+	test_file_size out
 '
 
 test_perf 'big pack with --path-walk' '
@@ -77,7 +77,7 @@ test_perf 'big pack with --path-walk' '
 '
 
 test_size 'big pack size with --path-walk' '
-	wc -c <out
+	test_file_size out
 '
 
 test_perf 'repack with --path-walk' '
@@ -85,7 +85,8 @@ test_perf 'repack with --path-walk' '
 '
 
 test_size 'repack size with --path-walk' '
-	wc -c <.git/objects/pack/pack-*.pack
+	pack=$(ls .git/objects/pack/pack-*.pack) &&
+	test_file_size "$pack"
 '
 
 test_done
