b1b8dfde6929ec9463eca0a858c4adb9786d7c93
Taylor Blau <me@ttaylorr.com>
2024-09-26 11:22:38 -0400

    finalize_object_file(): implement collision check
    We've had "FIXME!!! Collision check here ?" in finalize_object_file()
    since aac1794132 (Improve sha1 object file writing., 2005-05-03). That
    is, when we try to write a file with the same name, we assume the
    on-disk contents are the same and blindly throw away the new copy.
    
    One of the reasons we never implemented this is because the files it
    moves are all named after the cryptographic hash of their contents
    (either loose objects, or packs which have their hash in the name these
    days). So we are unlikely to see such a collision by accident. And even
    though there are weaknesses in sha1, we assume they are mitigated by our
    use of sha1dc.
    
    So while it's a theoretical concern now, it hasn't been a priority.
    However, if we start using weaker hashes for pack checksums and names,
    this will become a practical concern. So in preparation, let's actually
    implement a byte-for-byte collision check.
    
    The new check will cause the write of new differing content to be a
    failure, rather than a silent noop, and we'll retain the temporary file
    on disk. If there's no collision present, we'll clean up the temporary
    file as usual after either rename()-ing or link()-ing it into place.
    
    Note that this may cause some extra computation when the files are in
    fact identical, but this should happen rarely.
    
    Loose objects are exempt from this check, and the collision check may be
    skipped by calling the _flags variant of this function with the
    FOF_SKIP_COLLISION_CHECK bit set. This is done for a couple of reasons:
    
      - We don't treat the hash of the loose object file's contents as a
        checksum, since the same loose object can be stored using different
        bytes on disk (e.g., when adjusting core.compression, using a
        different version of zlib, etc.).
    
        This is fundamentally different from cases where
        finalize_object_file() is operating over a file which uses the hash
        value as a checksum of the contents. In other words, a pair of
        identical loose objects can be stored using different bytes on disk,
        and that should not be treated as a collision.
    
      - We already use the path of the loose object as its hash value /
        object name, so checking for collisions at the content level doesn't
        add anything.
    
        Adding a content-level collision check would have to happen at a
        higher level than in finalize_object_file(), since (avoiding race
        conditions) writing an object loose which already exists in the
        repository will prevent us from even reaching finalize_object_file()
        via the object freshening code.
    
        There is a collision check in index-pack via its `check_collision()`
        function, but there isn't an analogous function in unpack-objects,
        which just feeds the result to write_object_file().
    
        So skipping the collision check here does not change for better or
        worse the hardness of loose object writes.
    
    As a small note related to the latter bullet point above, we must teach
    the tmp-objdir routines to similarly skip the content-level collision
    checks when calling migrate_one() on a loose object file, which we do by
    setting the FOF_SKIP_COLLISION_CHECK bit when we are inside of a loose
    object shard.
    
    Co-authored-by: Jeff King <peff@peff.net>
    Signed-off-by: Jeff King <peff@peff.net>
    Helped-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- object-file.c
+++ object-file.c
@@ -1899,10 +1899,67 @@ static void write_object_file_prepare_literally(const struct git_hash_algo *algo
 	hash_object_body(algo, &c, buf, len, oid, hdr, hdrlen);
 }
 
+static int check_collision(const char *filename_a, const char *filename_b)
+{
+	char buf_a[4096], buf_b[4096];
+	int fd_a = -1, fd_b = -1;
+	int ret = 0;
+
+	fd_a = open(filename_a, O_RDONLY);
+	if (fd_a < 0) {
+		ret = error_errno(_("unable to open %s"), filename_a);
+		goto out;
+	}
+
+	fd_b = open(filename_b, O_RDONLY);
+	if (fd_b < 0) {
+		ret = error_errno(_("unable to open %s"), filename_b);
+		goto out;
+	}
+
+	while (1) {
+		ssize_t sz_a, sz_b;
+
+		sz_a = read_in_full(fd_a, buf_a, sizeof(buf_a));
+		if (sz_a < 0) {
+			ret = error_errno(_("unable to read %s"), filename_a);
+			goto out;
+		}
+
+		sz_b = read_in_full(fd_b, buf_b, sizeof(buf_b));
+		if (sz_b < 0) {
+			ret = error_errno(_("unable to read %s"), filename_b);
+			goto out;
+		}
+
+		if (sz_a != sz_b || memcmp(buf_a, buf_b, sz_a)) {
+			ret = error(_("files '%s' and '%s' differ in contents"),
+				    filename_a, filename_b);
+			goto out;
+		}
+
+		if (sz_a < sizeof(buf_a))
+			break;
+	}
+
+out:
+	if (fd_a > -1)
+		close(fd_a);
+	if (fd_b > -1)
+		close(fd_b);
+	return ret;
+}
+
 /*
  * Move the just written object into its final resting place.
  */
 int finalize_object_file(const char *tmpfile, const char *filename)
+{
+	return finalize_object_file_flags(tmpfile, filename, 0);
+}
+
+int finalize_object_file_flags(const char *tmpfile, const char *filename,
+			       enum finalize_object_file_flags flags)
 {
 	struct stat st;
 	int ret = 0;
@@ -1941,7 +1998,9 @@ int finalize_object_file(const char *tmpfile, const char *filename)
 			errno = saved_errno;
 			return error_errno(_("unable to write file %s"), filename);
 		}
-		/* FIXME!!! Collision check here ? */
+		if (!(flags & FOF_SKIP_COLLISION_CHECK) &&
+		    check_collision(tmpfile, filename))
+				return -1;
 		unlink_or_warn(tmpfile);
 	}
 
@@ -2195,7 +2254,8 @@ static int write_loose_object(const struct object_id *oid, char *hdr,
 			warning_errno(_("failed utime() on %s"), tmp_file.buf);
 	}
 
-	return finalize_object_file(tmp_file.buf, filename.buf);
+	return finalize_object_file_flags(tmp_file.buf, filename.buf,
+					  FOF_SKIP_COLLISION_CHECK);
 }
 
 static int freshen_loose_object(const struct object_id *oid)
@@ -2317,7 +2377,8 @@ int stream_loose_object(struct input_stream *in_stream, size_t len,
 		strbuf_release(&dir);
 	}
 
-	err = finalize_object_file(tmp_file.buf, filename.buf);
+	err = finalize_object_file_flags(tmp_file.buf, filename.buf,
+					 FOF_SKIP_COLLISION_CHECK);
 	if (!err && compat)
 		err = repo_add_loose_object_map(the_repository, oid, &compat_oid);
 cleanup:
