3f5e8d23cceb83eee92702c2139c9e7c7943b9eb
Patrick Steinhardt <ps@pks.im>
2024-10-23 11:56:07 +0200

    reftable/system: provide thin wrapper for tempfile subsystem
    We use the tempfile subsystem to write temporary tables, but given that
    we're in the process of converting the reftable library to become
    standalone we cannot use this subsystem directly anymore. While we could
    in theory convert the code to use mkstemp(3p) instead, we'd lose access
    to our infrastructure that automatically prunes tempfiles via atexit(3p)
    or signal handlers.
    
    Provide a thin wrapper for the tempfile subsystem instead. Like this,
    the compatibility shim is fully self-contained in "reftable/system.c".
    Downstream users of the reftable library would have to implement their
    own tempfile shims by replacing "system.c" with a custom version.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- reftable/system.h
+++ reftable/system.h
@@ -13,6 +13,45 @@ https://developers.google.com/open-source/licenses/bsd
 
 #include "git-compat-util.h"
 #include "lockfile.h"
-#include "tempfile.h"
+
+/*
+ * An implementation-specific temporary file. By making this specific to the
+ * implementation it becomes possible to tie temporary files into any kind of
+ * signal or atexit handlers for cleanup on abnormal situations.
+ */
+struct reftable_tmpfile {
+	const char *path;
+	int fd;
+	void *priv;
+};
+#define REFTABLE_TMPFILE_INIT ((struct reftable_tmpfile) { .fd = -1, })
+
+/*
+ * Create a temporary file from a pattern similar to how mkstemp(3p) would.
+ * The `pattern` shall not be modified. On success, the structure at `out` has
+ * been initialized such that it is ready for use. Returns 0 on success, a
+ * reftable error code on error.
+ */
+int tmpfile_from_pattern(struct reftable_tmpfile *out, const char *pattern);
+
+/*
+ * Close the temporary file's file descriptor without removing the file itself.
+ * This is a no-op in case the file has already been closed beforehand. Returns
+ * 0 on success, a reftable error code on error.
+ */
+int tmpfile_close(struct reftable_tmpfile *t);
+
+/*
+ * Close the temporary file and delete it. This is a no-op in case the file has
+ * already been deleted or renamed beforehand. Returns 0 on success, a reftable
+ * error code on error.
+ */
+int tmpfile_delete(struct reftable_tmpfile *t);
+
+/*
+ * Rename the temporary file to the provided path. The temporary file must be
+ * active. Return 0 on success, a reftable error code on error.
+ */
+int tmpfile_rename(struct reftable_tmpfile *t, const char *path);
 
 #endif
