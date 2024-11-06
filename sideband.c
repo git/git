2a0d918c2f95374df96e80c49cb5ff7197567637
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-11-06 21:07:51 +0100

    sideband: introduce an "escape hatch" to allow control characters
    The preceding commit fixed the vulnerability whereas sideband messages
    (that are under the control of the remote server) could contain ANSI
    escape sequences that would be sent to the terminal verbatim.
    
    However, this fix may not be desirable under all circumstances, e.g.
    when remote servers deliberately add coloring to their messages to
    increase their urgency.
    
    To help with those use cases, give users a way to opt-out of the
    protections: `sideband.allowControlCharacters`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sideband.c
+++ sideband.c
@@ -26,6 +26,8 @@ static struct keyword_entry keywords[] = {
 	{ "error",	GIT_COLOR_BOLD_RED },
 };
 
+static int allow_control_characters;
+
 /* Returns a color setting (GIT_COLOR_NEVER, etc). */
 static int use_sideband_colors(void)
 {
@@ -38,6 +40,9 @@ static int use_sideband_colors(void)
 
 	if (use_sideband_colors_cached >= 0)
 		return use_sideband_colors_cached;
+
+	git_config_get_bool("sideband.allowcontrolcharacters",
+			    &allow_control_characters);
 
 	if (!git_config_get_string_tmp(key, &value))
 		use_sideband_colors_cached = git_config_colorbool(key, value);
@@ -68,6 +73,11 @@ void list_config_color_sideband_slots(struct string_list *list, const char *pref
 
 static void strbuf_add_sanitized(struct strbuf *dest, const char *src, int n)
 {
+	if (allow_control_characters) {
+		strbuf_add(dest, src, n);
+		return;
+	}
+
 	strbuf_grow(dest, n);
 	for (; n && *src; src++, n--) {
 		if (!iscntrl(*src) || *src == '\t' || *src == '\n')
