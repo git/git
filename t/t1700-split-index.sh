a5408d1820e0ea75612bbd6f6a6c495066e5ffcb
Patrick Steinhardt <ps@pks.im>
2024-11-20 14:39:41 +0100

    split-index: fix memory leak in `move_cache_to_base_index()`
    In `move_cache_to_base_index()` we move the index cache of the main
    index into the split index, which is used when writing a shared index.
    But we don't release the old split index base in case we already had a
    split index before this operation, which can thus leak memory.
    
    Plug the leak by releasing the previous base.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1700-split-index.sh
+++ t/t1700-split-index.sh
@@ -5,6 +5,7 @@ test_description='split index mode tests'
 GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 
+TEST_PASSES_SANITIZE_LEAK=true
 . ./test-lib.sh
 
 # We need total control of index splitting here
