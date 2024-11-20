141766d1bb801ae2a9c7358920ce8dc9697f53c4
Patrick Steinhardt <ps@pks.im>
2024-11-20 14:39:37 +0100

    line-log: fix leak when rewriting commit parents
    In `process_ranges_merge_commit()` we try to figure out which of the
    parents can be blamed for the given line changes. When we figure out
    that none of the files in the line-log have changed we assign the
    complete blame to that commit and rewrite the parents of the current
    commit to only use that single parent.
    
    This is done via `commit_list_append()`, which is misleadingly _not_
    appending to the list of parents. Instead, we overwrite the parents with
    the blamed parent. This makes us lose track of the old pointers,
    creating a memory leak.
    
    Fix this issue by freeing the parents before we overwrite them.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4211-line-log.sh
+++ t/t4211-line-log.sh
@@ -4,6 +4,7 @@ test_description='test log -L'
 GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
 
+TEST_PASSES_SANITIZE_LEAK=true
 . ./test-lib.sh
 
 test_expect_success 'setup (import history)' '
