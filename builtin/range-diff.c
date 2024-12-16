f8043236c6c9cb9e943a87ab2e55e8e394796727
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-12-16 14:11:21 +0000

    range-diff: optionally include merge commits' diffs in the analysis
    The `git log` command already offers support for including diffs for
    merges, via the `--diff-merges=<format>` option.
    
    Let's add corresponding support for `git range-diff`, too. This makes it
    more convenient to spot differences between commit ranges that contain
    merges.
    
    This is especially true in scenarios with non-trivial merges, i.e.
    merges introducing changes other than, or in addition to, what merge ORT
    would have produced. Merging a topic branch that changes a function
    signature into a branch that added a caller of that function, for
    example, would require the merge commit itself to adjust that caller to
    the modified signature.
    
    In my code reviews, I found the `--diff-merges=remerge` option
    particularly useful.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/range-diff.c
+++ builtin/range-diff.c
@@ -21,6 +21,7 @@ int cmd_range_diff(int argc,
 {
 	struct diff_options diffopt = { NULL };
 	struct strvec other_arg = STRVEC_INIT;
+	struct strvec diff_merges_arg = STRVEC_INIT;
 	struct range_diff_options range_diff_opts = {
 		.creation_factor = RANGE_DIFF_CREATION_FACTOR_DEFAULT,
 		.diffopt = &diffopt,
@@ -36,6 +37,8 @@ int cmd_range_diff(int argc,
 		OPT_PASSTHRU_ARGV(0, "notes", &other_arg,
 				  N_("notes"), N_("passed to 'git log'"),
 				  PARSE_OPT_OPTARG),
+		OPT_PASSTHRU_ARGV(0, "diff-merges", &diff_merges_arg,
+				  N_("style"), N_("passed to 'git log'"), 0),
 		OPT_BOOL(0, "left-only", &left_only,
 			 N_("only emit output related to the first range")),
 		OPT_BOOL(0, "right-only", &right_only,
@@ -61,6 +64,12 @@ int cmd_range_diff(int argc,
 	/* force color when --dual-color was used */
 	if (!simple_color)
 		diffopt.use_color = 1;
+
+	/* If `--diff-merges` was specified, imply `--merges` */
+	if (diff_merges_arg.nr) {
+		range_diff_opts.include_merges = 1;
+		strvec_pushv(&other_arg, diff_merges_arg.v);
+	}
 
 	for (i = 0; i < argc; i++)
 		if (!strcmp(argv[i], "--")) {
@@ -155,6 +164,7 @@ int cmd_range_diff(int argc,
 	res = show_range_diff(range1.buf, range2.buf, &range_diff_opts);
 
 	strvec_clear(&other_arg);
+	strvec_clear(&diff_merges_arg);
 	strbuf_release(&range1);
 	strbuf_release(&range2);
 
