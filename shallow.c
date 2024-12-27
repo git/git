24027256aa9614a445563707a72af7ce5ff49b5b
Junio C Hamano <gitster@pobox.com>
2024-12-27 12:25:30 -0800

    sign-compare: avoid comparing ptrdiff with an int/unsigned
    Instead, offset the base pointer with integer and compare it with
    the other pointer.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- shallow.c
+++ shallow.c
@@ -534,7 +534,7 @@ static uint32_t *paint_alloc(struct paint_info *info)
 	unsigned nr = DIV_ROUND_UP(info->nr_bits, 32);
 	unsigned size = nr * sizeof(uint32_t);
 	void *p;
-	if (!info->pool_count || size > info->end - info->free) {
+	if (!info->pool_count || info->end < info->free + size) {
 		if (size > POOL_SIZE)
 			BUG("pool size too small for %d in paint_alloc()",
 			    size);
