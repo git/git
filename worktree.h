5eaf54bc281ad0a7d3197fe3da6d9c9daf620365
shejialuo <shejialuo@gmail.com>
2024-12-29 22:21:26 +0800

    builtin/refs: get worktrees without reading head information
    In "packed-backend.c", there are some functions such as "create_snapshot"
    and "next_record" which would check the correctness of the content of
    the "packed-ref" file. When anything is bad, the program will die.
    
    It may seem that we have nothing relevant to above feature, because we
    are going to read and parse the raw "packed-ref" file without creating
    the snapshot and using the ref iterator to check the consistency.
    
    However, when using "get_worktrees" in "builtin/refs", we would parse
    the "HEAD" information. If the referent of the "HEAD" is inside the
    "packed-ref", we will call "create_snapshot" function to parse the
    "packed-ref" to get the information. No matter whether the entry of
    "HEAD" in "packed-ref" is correct, "create_snapshot" would call
    "verify_buffer_safe" to check whether there is a newline in the last
    line of the file. If not, the program will die.
    
    Although this behavior has no harm for the program, it will
    short-circuit the program. When the users execute "git refs verify" or
    "git fsck", we should avoid reading the head information, which may
    execute the read operation in packed backend with stricter checks to die
    the program. Instead, we should continue to check other parts of the
    "packed-refs" file completely.
    
    Fortunately, in 465a22b338 (worktree: skip reading HEAD when repairing
    worktrees, 2023-12-29), we have introduced a function
    "get_worktrees_internal" which allows us to get worktrees without
    reading head information.
    
    Create a new exposed function "get_worktrees_without_reading_head", then
    replace the "get_worktrees" in "builtin/refs" with the new created
    function.
    
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: shejialuo <shejialuo@gmail.com>

--- worktree.h
+++ worktree.h
@@ -31,6 +31,13 @@ struct worktree {
 struct worktree **get_worktrees(void);
 
 /*
+ * Like `get_worktrees`, but does not read HEAD. Skip reading HEAD allows to
+ * get the worktree without worrying about failures pertaining to parsing
+ * the HEAD ref. This is useful when we want to check the ref db consistency.
+ */
+struct worktree **get_worktrees_without_reading_head(void);
+
+/*
  * Returns 1 if linked worktrees exist, 0 otherwise.
  */
 int submodule_uses_worktrees(const char *path);
