8c53354658462aa6783022def86750ab0b79eb6f
Jeff King <peff@peff.net>
2025-01-09 03:49:07 -0500

    tree-diff: pass whole path string to path_appendnew()
    When diffing trees, we'll have a strbuf "base" containing the
    slash-separted names of our parent trees, and a "path" string
    representing an entry name from the current tree. We pass these
    separately to path_appendnew(), which combines them to form a single
    path string in the combine_diff_path struct.
    
    Instead, let's append the path string to our base strbuf ourselves, pass
    in the result, and then roll it back with strbuf_setlen(). This lets us
    simplify path_appendnew() a bit, enabling further refactoring.
    
    And while it might seem like this causes extra wasted allocations, it
    does not in practice. We reuse the same strbuf for each tree entry, so
    we only have to allocate it to match the largest name. Plus, in a
    recursive diff we'll end up doing this same operation to extend the base
    for the next level of recursion. So we're really just incurring a small
    memcpy().
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- tree-diff.c
+++ tree-diff.c
@@ -129,11 +129,10 @@ static int emit_diff_first_parent_only(struct diff_options *opt, struct combine_
  * and append it to paths list tail.
  */
 static struct combine_diff_path *path_appendnew(struct combine_diff_path *last,
-	int nparent, const struct strbuf *base, const char *path, int pathlen,
+	int nparent, const char *path, size_t len,
 	unsigned mode, const struct object_id *oid)
 {
 	struct combine_diff_path *p;
-	size_t len = st_add(base->len, pathlen);
 	size_t alloclen = combine_diff_path_size(nparent, len);
 
 	p = xmalloc(alloclen);
@@ -141,8 +140,7 @@ static struct combine_diff_path *path_appendnew(struct combine_diff_path *last,
 	last->next = p;
 
 	p->path = (char *)&(p->parent[nparent]);
-	memcpy(p->path, base->buf, base->len);
-	memcpy(p->path + base->len, path, pathlen);
+	memcpy(p->path, path, len);
 	p->path[len] = 0;
 	p->mode = mode;
 	oidcpy(&p->oid, oid ? oid : null_oid());
@@ -206,7 +204,10 @@ static struct combine_diff_path *emit_path(struct combine_diff_path *p,
 	if (emitthis) {
 		int keep;
 		struct combine_diff_path *pprev = p;
-		p = path_appendnew(p, nparent, base, path, pathlen, mode, oid);
+
+		strbuf_add(base, path, pathlen);
+		p = path_appendnew(p, nparent, base->buf, base->len, mode, oid);
+		strbuf_setlen(base, old_baselen);
 
 		for (i = 0; i < nparent; ++i) {
 			/*
