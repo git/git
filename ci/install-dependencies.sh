2a21098b98ae2f9581a91e2e474c397e5cbede12
Patrick Steinhardt <ps@pks.im>
2025-01-10 12:31:59 +0100

    github: adapt containerized jobs to be rootless
    The containerized jobs in GitHub Actions run as root, giving them
    special permissions to for example delete files even when the user
    shouldn't be able to due to file permissions. This limitation keeps us
    from using containerized jobs for most of our Ubuntu-based jobs as it
    causes a number of tests to fail.
    
    Adapt the jobs to create a separate user that executes the test suite.
    This follows similar infrastructure that we already have in GitLab CI.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -31,7 +31,7 @@ alpine-*)
 	;;
 fedora-*|almalinux-*)
 	dnf -yq update >/dev/null &&
-	dnf -yq install make gcc findutils diffutils perl python3 gettext zlib-devel expat-devel openssl-devel curl-devel pcre2-devel >/dev/null
+	dnf -yq install shadow-utils sudo make gcc findutils diffutils perl python3 gettext zlib-devel expat-devel openssl-devel curl-devel pcre2-devel >/dev/null
 	;;
 ubuntu-*|ubuntu32-*|debian-*)
 	# Required so that apt doesn't wait for user input on certain packages.
