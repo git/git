69094f6ab84c42ba97b70a208aee473bcc0b4100
Patrick Steinhardt <ps@pks.im>
2025-01-16 10:17:18 +0100

    compat/zlib: provide `deflateBound()` shim centrally
    The `deflateBound()` function has only been introduced with zlib 1.2.0.
    When linking against a zlib version older than that we thus provide our
    own compatibility shim. Move this shim into "compat/zlib.h" so that we
    can adapt it based on whether or not we use zlib-ng in a subsequent
    commit.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/zlib-compat.h
+++ compat/zlib-compat.h
@@ -3,4 +3,8 @@
 
 #include <zlib.h>
 
+#if defined(NO_DEFLATE_BOUND) || ZLIB_VERNUM < 0x1200
+# define deflateBound(c,s)  ((s) + (((s) + 7) >> 3) + (((s) + 63) >> 6) + 11)
+#endif
+
 #endif /* COMPAT_ZLIB_H */
