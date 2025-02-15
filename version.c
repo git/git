50418d55acc6cfeba4a47a5ddeb322726b85be38
Usman Akinyemi <usmanakinyemi202@gmail.com>
2025-02-15 21:20:52 +0530

    agent: advertise OS name via agent capability
    As some issues that can happen with a Git client can be operating system
    specific, it can be useful for a server to know which OS a client is
    using. In the same way it can be useful for a client to know which OS
    a server is using.
    
    Our current agent capability is in the form of "package/version" (e.g.,
    "git/1.8.3.1"). Let's extend it to include the operating system name (os)
    i.e in the form "package/version-os" (e.g., "git/1.8.3.1-Linux").
    
    Including OS details in the agent capability simplifies implementation,
    maintains backward compatibility, avoids introducing a new capability,
    encourages adoption across Git-compatible software, and enhances
    debugging by providing complete environment information without affecting
    functionality. The operating system name is retrieved using the 'sysname'
    field of the `uname(2)` system call or its equivalent.
    
    However, there are differences between `uname(1)` (command-line utility)
    and `uname(2)` (system call) outputs on Windows. These discrepancies
    complicate testing on Windows platforms. For example:
      - `uname(1)` output: MINGW64_NT-10.0-20348.3.4.10-87d57229.x86_64\
      .2024-02-14.20:17.UTC.x86_64
      - `uname(2)` output: Windows.10.0.20348
    
    On Windows, uname(2) is not actually system-supplied but is instead
    already faked up by Git itself. We could have overcome the test issue
    on Windows by implementing a new `uname` subcommand in `test-tool`
    using uname(2), but except uname(2), which would be tested against
    itself, there would be nothing platform specific, so it's just simpler
    to disable the tests on Windows.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Usman Akinyemi <usmanakinyemi202@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- version.c
+++ version.c
@@ -1,8 +1,9 @@
+#define USE_THE_REPOSITORY_VARIABLE
+
 #include "git-compat-util.h"
 #include "version.h"
 #include "version-def.h"
 #include "strbuf.h"
-#include "sane-ctype.h"
 #include "gettext.h"
 
 const char git_version_string[] = GIT_VERSION;
@@ -34,6 +35,27 @@ const char *git_user_agent(void)
 	return agent;
 }
 
+/*
+  Retrieve, sanitize and cache operating system info for subsequent
+  calls. Return a pointer to the sanitized operating system info
+  string.
+*/
+static const char *os_info(void)
+{
+	static const char *os = NULL;
+
+	if (!os) {
+		struct strbuf buf = STRBUF_INIT;
+
+		get_uname_info(&buf, 0);
+		/* Sanitize the os information immediately */
+		redact_non_printables(&buf);
+		os = strbuf_detach(&buf, NULL);
+	}
+
+	return os;
+}
+
 const char *git_user_agent_sanitized(void)
 {
 	static const char *agent = NULL;
@@ -42,6 +64,11 @@ const char *git_user_agent_sanitized(void)
 		struct strbuf buf = STRBUF_INIT;
 
 		strbuf_addstr(&buf, git_user_agent());
+
+		if (!getenv("GIT_USER_AGENT")) {
+			strbuf_addch(&buf, '-');
+			strbuf_addstr(&buf, os_info());
+		}
 		redact_non_printables(&buf);
 		agent = strbuf_detach(&buf, NULL);
 	}
