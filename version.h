4871e67f0423dffeffcbacb9c2b2d48968b18db3
Usman Akinyemi <usmanakinyemi202@gmail.com>
2025-02-15 21:20:50 +0530

    version: extend get_uname_info() to hide system details
    Currently, get_uname_info() function provides the full OS information.
    In a following commit, we will need it to provide only the OS name.
    
    Let's extend it to accept a "full" flag that makes it switch between
    providing full OS information and providing only the OS name.
    
    We may need to refactor this function in the future if an
    `osVersion.format` is added.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Usman Akinyemi <usmanakinyemi202@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- version.h
+++ version.h
@@ -12,6 +12,6 @@ const char *git_user_agent_sanitized(void);
   Return -1 and put an error message into 'buf' in case of uname()
   error. Return 0 and put uname info into 'buf' otherwise.
 */
-int get_uname_info(struct strbuf *buf);
+int get_uname_info(struct strbuf *buf, unsigned int full);
 
 #endif /* VERSION_H */
