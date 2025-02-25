dbed91c4ec4991d7a677ba7816b77693300db11b
Seyi Kuforiji <kuforiji98@gmail.com>
2025-02-25 11:10:41 +0100

    t/unit-tests: implement clar specific oid helper functions
    `get_oid_arbitrary_hex()` and `init_hash_algo()` are both required for
    oid-related tests to run without errors. In the current implementation,
    both functions are defined and declared in the
    `t/unit-tests/lib-oid.{c,h}` which is utilized by oid-related tests in
    the homegrown unit tests structure.
    
    Adapt functions in lib-oid.{c,h} to use clar. Both these functions
    become available for oid-related test files implemented using the clar
    testing framework, which requires them. This will be used by subsequent
    commits.
    
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Seyi Kuforiji <kuforiji98@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/unit-tests/lib-oid.h
+++ t/unit-tests/lib-oid.h
@@ -5,6 +5,7 @@
 
 /*
  * Convert arbitrary hex string to object_id.
+ *
  * For example, passing "abc12" will generate
  * "abc1200000000000000000000000000000000000" hex of length 40 for SHA-1 and
  * create object_id with that.
@@ -12,14 +13,16 @@
  * algo is not allowed. The hash algo is decided based on GIT_TEST_DEFAULT_HASH
  * environment variable.
  */
-int get_oid_arbitrary_hex(const char *s, struct object_id *oid);
+
+void cl_parse_any_oid (const char *s, struct object_id *oid);
 /*
  * Returns one of GIT_HASH_{SHA1, SHA256, UNKNOWN} based on the value of
  * GIT_TEST_DEFAULT_HASH environment variable. The fallback value in the
  * absence of GIT_TEST_DEFAULT_HASH is GIT_HASH_SHA1. It also uses
- * check(algo != GIT_HASH_UNKNOWN) before returning to verify if the
+ * cl_assert(algo != GIT_HASH_UNKNOWN) before returning to verify if the
  * GIT_TEST_DEFAULT_HASH's value is valid or not.
  */
-int init_hash_algo(void);
+
+int cl_setup_hash_algo(void);
 
 #endif /* LIB_OID_H */
