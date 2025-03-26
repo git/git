79aa0145aa089cdb2f85f1811ff9f62a37a6bd66
Seyi Kuforiji <kuforiji98@gmail.com>
2025-03-26 11:32:18 +0100

    t/unit-tests: adapt lib-reftable{c,h} helper functions to clar
    Helper functions defined in `t/unit-tests/lib-reftable.{c,h}` are
    required for the reftable-related test files to run efficeintly. In the
    current implementation these functions are designed to conform with our
    homegrown unit-testing structure. So in other to convert the reftable
    test files, there is need for a clar specific implementation of these
    helper functions.
    
    type cast `for (size_t i = 0; i < (size_t)stats->ref_stats.blocks; i++)`
    Adapt functions in lib-reftable.{c,h} to use clar. These functions
    conform with the clar testing framework and become available for all
    reftable-related test files implemented using the clar testing
    framework, which requires them. This change migrates the helper
    functions back into `lib-reftable.{c,h}`.
    
    Signed-off-by: Seyi Kuforiji <kuforiji98@gmail.com>

--- t/unit-tests/lib-reftable.h
+++ t/unit-tests/lib-reftable.h
@@ -6,12 +6,12 @@
 
 struct reftable_buf;
 
-void t_reftable_set_hash(uint8_t *p, int i, enum reftable_hash id);
+void cl_reftable_set_hash(uint8_t *p, int i, enum reftable_hash id);
 
-struct reftable_writer *t_reftable_strbuf_writer(struct reftable_buf *buf,
+struct reftable_writer *cl_reftable_strbuf_writer(struct reftable_buf *buf,
 						 struct reftable_write_options *opts);
 
-void t_reftable_write_to_buf(struct reftable_buf *buf,
+void cl_reftable_write_to_buf(struct reftable_buf *buf,
 			     struct reftable_ref_record *refs,
 			     size_t nrecords,
 			     struct reftable_log_record *logs,
