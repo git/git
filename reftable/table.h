0a502c532838acba11319c474d7e82a10e64421f
Patrick Steinhardt <ps@pks.im>
2025-03-31 10:41:31 +0200

    reftable/table: add `reftable_table` to the public interface
    The `reftable_table` interface is an internal implementation detail that
    callers have no access to. Having direct access to this structure is
    important though for a subsequent patch series that will implement
    consistency checks for the reftable backend.
    
    Move the structure into "reftable-table.h" so that it part of the public
    interface.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reftable/table.h
+++ reftable/table.h
@@ -14,39 +14,6 @@
 #include "reftable-iterator.h"
 #include "reftable-table.h"
 
-/* metadata for a block type */
-struct reftable_table_offsets {
-	int is_present;
-	uint64_t offset;
-	uint64_t index_offset;
-};
-
-/* The state for reading a reftable file. */
-struct reftable_table {
-	/* for convenience, associate a name with the instance. */
-	char *name;
-	struct reftable_block_source source;
-
-	/* Size of the file, excluding the footer. */
-	uint64_t size;
-
-	/* The hash function used for ref records. */
-	enum reftable_hash hash_id;
-
-	uint32_t block_size;
-	uint64_t min_update_index;
-	uint64_t max_update_index;
-	/* Length of the OID keys in the 'o' section */
-	int object_id_len;
-	int version;
-
-	struct reftable_table_offsets ref_offsets;
-	struct reftable_table_offsets obj_offsets;
-	struct reftable_table_offsets log_offsets;
-
-	uint64_t refcount;
-};
-
 const char *reftable_table_name(struct reftable_table *t);
 
 int table_init_iter(struct reftable_table *t,
