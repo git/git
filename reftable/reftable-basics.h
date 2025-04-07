6dcc05ffc3ead0745d19decd0e8ecd65edc9d414
Patrick Steinhardt <ps@pks.im>
2025-04-07 15:16:13 +0200

    reftable: fix formatting of the license header
    The license headers used across the reftable library doesn't follow our
    typical coding style for multi-line comments. Fix it.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reftable/reftable-basics.h
+++ reftable/reftable-basics.h
@@ -4,7 +4,7 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file or at
  * https://developers.google.com/open-source/licenses/bsd
-*/
+ */
 
 #ifndef REFTABLE_BASICS_H
 #define REFTABLE_BASICS_H
