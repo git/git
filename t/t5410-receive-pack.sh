95262afe788e0b6b4ae5ff37e8d28fb5883c1a82
Justin Tobler <jltobler@gmail.com>
2025-05-20 11:32:17 -0500

    t5410: test receive-pack connectivity check
    As part of git-recieve-pack(1), the connectivity of objects is checked.
    Add a test validating that git-receive-pack(1) fails due to an incoming
    packfile that would leave the repository with missing objects. Instead
    of creating a new test file, "t5410" is generalized for receive-pack
    testing.
    
    Signed-off-by: Justin Tobler <jltobler@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5410-receive-pack.sh
+++ t/t5410-receive-pack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test_description='git receive-pack with alternate ref filtering'
+test_description='git receive-pack'
 
 GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
 export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
@@ -39,6 +39,27 @@ test_expect_success 'with core.alternateRefsPrefixes' '
 	printf "0000" | git receive-pack fork >actual &&
 	extract_haves <actual >actual.haves &&
 	test_cmp expect actual.haves
+'
+
+test_expect_success 'receive-pack missing objects fails connectivity check' '
+	test_when_finished rm -rf repo remote.git setup.git &&
+
+	git init repo &&
+	git -C repo commit --allow-empty -m 1 &&
+	git clone --bare repo setup.git &&
+	git -C repo commit --allow-empty -m 2 &&
+
+	# Capture git-send-pack(1) output sent to git-receive-pack(1).
+	git -C repo send-pack ../setup.git --all \
+		--receive-pack="tee ${SQ}$(pwd)/out${SQ} | git-receive-pack" &&
+
+	# Replay captured git-send-pack(1) output on new empty repository.
+	git init --bare remote.git &&
+	git receive-pack remote.git <out >actual 2>err &&
+
+	test_grep "missing necessary objects" actual &&
+	test_grep "fatal: Failed to traverse parents" err &&
+	test_must_fail git -C remote.git cat-file -e $(git -C repo rev-parse HEAD)
 '
 
 test_done
