176f9b16b2f2742dd28cfc88ac1e85e11bd1a0f0
jinyaoguo <guo846@purdue.edu>
2025-05-23 15:18:34 -0400

    Fix buffer underflow in xdl_build_script
    The loop in xdl_build_script used `i1 >= 0 || i2 >= 0`, causing
    `i1` (or `i2`) to reach 0 and then access `rchg1[i1-1]` (or
    `rchg2[i2-1]`), which underflows the buffer.
    This commit adds explicit `i1 > 0` and `i2 > 0` checks around
    those array accesses to prevent invalid negative indexing.
    
    Signed-off-by: Alex Guo <alexguo1023@gmail.com>

--- xdiff/xdiffi.c
+++ xdiff/xdiffi.c
@@ -951,9 +951,10 @@ int xdl_build_script(xdfenv_t *xe, xdchange_t **xscr) {
 	 * Trivial. Collects "groups" of changes and creates an edit script.
 	 */
 	for (i1 = xe->xdf1.nrec, i2 = xe->xdf2.nrec; i1 >= 0 || i2 >= 0; i1--, i2--)
-		if (rchg1[i1 - 1] || rchg2[i2 - 1]) {
-			for (l1 = i1; rchg1[i1 - 1]; i1--);
-			for (l2 = i2; rchg2[i2 - 1]; i2--);
+		if ((i1 > 0 && rchg1[i1 - 1]) ||
+			(i2 > 0 && rchg2[i2 - 1])) {
+			for (l1 = i1; i1 > 0 && rchg1[i1 - 1]; i1--);
+            for (l2 = i2; i2 > 0 && rchg2[i2 - 1]; i2--);
 
 			if (!(xch = xdl_add_change(cscr, i1, i2, l1 - i1, l2 - i2))) {
 				xdl_free_script(cscr);
