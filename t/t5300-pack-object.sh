b3cf5e2e11f36765e2ef3fe6ef41acb6ff0ae056
Derrick Stolee <dstolee@microsoft.com>
2025-06-20 09:38:02 +0200

    fixup! pack-objects: refactor path-walk delta phase
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t5300-pack-object.sh
+++ t/t5300-pack-object.sh
@@ -726,9 +726,7 @@ test_expect_success '--name-hash-version=2 and --write-bitmap-index are incompat
 # Basic "repack everything" test
 test_expect_success '--path-walk pack everything' '
 	git -C server rev-parse HEAD >in &&
-	GIT_PROGRESS_DELAY=0 git -C server pack-objects \
-		--stdout --revs --path-walk --progress <in >out.pack 2>err &&
-	grep "Compressing objects by path" err &&
+	git -C server pack-objects --stdout --revs --path-walk <in >out.pack &&
 	git -C server index-pack --stdin <out.pack
 '
 
@@ -738,9 +736,7 @@ test_expect_success '--path-walk thin pack' '
 	$(git -C server rev-parse HEAD)
 	^$(git -C server rev-parse HEAD~2)
 	EOF
-	GIT_PROGRESS_DELAY=0 git -C server pack-objects \
-		--thin --stdout --revs --path-walk --progress <in >out.pack 2>err &&
-	grep "Compressing objects by path" err &&
+	git -C server pack-objects --thin --stdout --revs --path-walk <in >out.pack &&
 	git -C server index-pack --fix-thin --stdin <out.pack
 '
 
