c4e9775c60b880f06054c22f98520bdb4f1ba38e
Kristoffer Haugsbakk <code@khaugsbakk.name>
2025-07-01 17:14:32 +0200

    config: mention --url in the synopsis
    4e513890008 (builtin/config: introduce "get" subcommand, 2024-05-06)
    introduced `get` and `--url` but didn’t add `--url` to the synopsis.
    
    Acked-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Kristoffer Haugsbakk <code@khaugsbakk.name>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/config.c
+++ builtin/config.c
@@ -17,7 +17,7 @@
 
 static const char *const builtin_config_usage[] = {
 	N_("git config list [<file-option>] [<display-option>] [--includes]"),
-	N_("git config get [<file-option>] [<display-option>] [--includes] [--all] [--regexp] [--value=<pattern>] [--fixed-value] [--default=<default>] <name>"),
+	N_("git config get [<file-option>] [<display-option>] [--includes] [--all] [--regexp] [--value=<pattern>] [--fixed-value] [--default=<default>] [--url=<url>] <name>"),
 	N_("git config set [<file-option>] [--type=<type>] [--all] [--value=<pattern>] [--fixed-value] <name> <value>"),
 	N_("git config unset [<file-option>] [--all] [--value=<pattern>] [--fixed-value] <name>"),
 	N_("git config rename-section [<file-option>] <old-name> <new-name>"),
