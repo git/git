82edffe3cb9a27e6278745cc660102a08db603a8
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2025-08-15 10:55:18 -0300

    repo: add the field references.format
    This commit is part of the series that introduces the new subcommand
    git-repo-info.
    
    The flag `--show-ref-format` from git-rev-parse is used for retrieving
    the reference format (i.e. `files` or `reftable`). This way, it is
    used for querying repository metadata, fitting in the purpose of
    git-repo-info.
    
    Add a new field `references.format` to the repo-info subcommand
    containing that information.
    
    Helped-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Helped-by: Junio C Hamano <gitster@pobox.com>
    Helped-by: Justin Tobler <jltobler@gmail.com>
    Helped-by: Eric Sunshine <sunshine@sunshineco.com>
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Mentored-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t1900-repo.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+test_description='test git repo-info'
+
+. ./test-lib.sh
+
+# Test whether a key-value pair is correctly returned
+#
+# Usage: test_repo_info <label> <init command> <repo_name> <key> <expected value>
+#
+# Arguments:
+#   label: the label of the test
+#   init_command: a command which creates a repository
+#   repo_name: the name of the repository that will be created in init_command
+#   key: the key of the field that is being tested
+#   expected_value: the value that the field should contain
+test_repo_info () {
+	label=$1
+	init_command=$2
+	repo_name=$3
+	key=$4
+	expected_value=$5
+
+	test_expect_success "$label" '
+		eval "$init_command $repo_name" &&
+		echo "$key=$expected_value" >expected &&
+		git -C $repo_name repo info "$key" >actual &&
+		test_cmp expected actual
+	'
+}
+
+test_repo_info 'ref format files is retrieved correctly' \
+	'git init --ref-format=files' 'format-files' 'references.format' 'files'
+
+test_repo_info 'ref format reftable is retrieved correctly' \
+	'git init --ref-format=reftable' 'format-reftable' 'references.format' 'reftable'
+
+test_expect_success 'git-repo-info fails if an invalid key is requested' '
+	echo "error: key ${SQ}foo${SQ} not found" >expected_err &&
+	test_must_fail git repo info foo 2>actual_err &&
+	test_cmp expected_err actual_err
+'
+
+test_expect_success 'git-repo-info outputs data even if there is an invalid field' '
+	echo "references.format=$(test_detect_ref_format)" >expected &&
+	test_must_fail git repo info foo references.format bar >actual &&
+	test_cmp expected actual
+'
+
+test_done
