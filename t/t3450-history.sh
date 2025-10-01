0fd62be8e9fb2860b202b4ee31fdd536c5d31b06
Patrick Steinhardt <ps@pks.im>
2025-10-01 17:57:32 +0200

    builtin/history: implement "reword" subcommand
    Implement a new "reword" subcommand for git-history(1). This subcommand
    is essentially the same as if a user performed an interactive rebase
    with a single commit changed to use the "reword" verb.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3450-history.sh
+++ t/t3450-history.sh
@@ -5,13 +5,13 @@ test_description='tests for git-history command'
 . ./test-lib.sh
 
 test_expect_success 'does nothing without any arguments' '
-	git history >out 2>&1 &&
-	test_must_be_empty out
+	test_must_fail git history 2>err &&
+	test_grep "need a subcommand" err
 '
 
 test_expect_success 'raises an error with unknown argument' '
 	test_must_fail git history garbage 2>err &&
-	test_grep "unrecognized argument: garbage" err
+	test_grep "unknown subcommand: .garbage." err
 '
 
 test_done
