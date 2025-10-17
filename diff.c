e4745b504fb48b236e8b916a17299cd01546197c
Lidong Yan <yldhome2d2@gmail.com>
2025-10-17 10:38:36 +0800

    diff: stop output garbled message in dry run mode
    In dry run mode, diff_flush_patch() should not produce any output.
    However, in commit b55e6d36eb (diff: ensure consistent diff behavior
    with ignore options, 2025-08-08), only the output during the
    comparison of two file contents was suppressed. For file deletions
    or mode changes, diff_flush_patch() still produces output. In
    run_extern_diff(), set quiet to true if in dry run mode. In
    emit_diff_symbol_from_struct(), directly return if in dry run mode.
    
    Signed-off-by: Lidong Yan <yldhome2d2@gmail.com>

--- diff.c
+++ diff.c
@@ -1351,6 +1351,9 @@ static void emit_diff_symbol_from_struct(struct diff_options *o,
 	int len = eds->len;
 	unsigned flags = eds->flags;
 
+	if (o->dry_run)
+		return;
+
 	switch (s) {
 	case DIFF_SYMBOL_NO_LF_EOF:
 		context = diff_get_color_opt(o, DIFF_CONTEXT);
@@ -4420,7 +4423,7 @@ static void run_external_diff(const struct external_diff *pgm,
 {
 	struct child_process cmd = CHILD_PROCESS_INIT;
 	struct diff_queue_struct *q = &diff_queued_diff;
-	int quiet = !(o->output_format & DIFF_FORMAT_PATCH);
+	int quiet = !(o->output_format & DIFF_FORMAT_PATCH) || o->dry_run;
 	int rc;
 
 	/*
