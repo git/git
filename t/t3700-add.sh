c59e51c079f226f56d1298b7f07bc4d71c489279
brian m. carlson <sandals@crustytoothpaste.net>
2025-11-12 23:54:34 +0000

    object-file: disallow adding submodules of different hash algo
    The design of the hash algorithm transition plan is that objects stored
    must be entirely in one algorithm since we lack any way to indicate a
    mix of algorithms.  This also includes submodules, but we have
    traditionally not enforced this, which leads to various problems when
    trying to clone or check out the the submodule from the remote.
    
    Since this cannot work in the general case, restrict adding a submodule
    of a different algorithm to the index.  Add tests for git add and git
    submodule add that these are rejected.
    
    Note that we cannot check this in git fsck because the malformed
    submodule is stored in the tree as an object ID which is either
    truncated (when a SHA-256 submodule is added to a SHA-1 repository) or
    padded with zeros (when a SHA-1 submodule is added to a SHA-256
    repository).  We cannot detect even the latter case because someone
    could have an actual submodule that actually ends in 24 zeros, which
    would be a false positive.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3700-add.sh
+++ t/t3700-add.sh
@@ -541,6 +541,33 @@ test_expect_success 'all statuses changed in folder if . is given' '
 	)
 '
 
+test_expect_success 'cannot add a submodule of a different algorithm' '
+	git init --object-format=sha256 sha256 &&
+	(
+		cd sha256 &&
+		test_commit abc &&
+		git init --object-format=sha1 submodule &&
+		(
+			cd submodule &&
+			test_commit def
+		) &&
+		test_must_fail git add submodule &&
+		test $(git ls-files --stage | grep ^160000 | wc -l) -eq 0
+	) &&
+	git init --object-format=sha1 sha1 &&
+	(
+		cd sha1 &&
+		test_commit abc &&
+		git init --object-format=sha256 submodule &&
+		(
+			cd submodule &&
+			test_commit def
+		) &&
+		test_must_fail git add submodule &&
+		test $(git ls-files --stage | grep ^160000 | wc -l) -eq 0
+	)
+'
+
 test_expect_success CASE_INSENSITIVE_FS 'path is case-insensitive' '
 	path="$(pwd)/BLUB" &&
 	touch "$path" &&
