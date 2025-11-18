17bd1108eac98d8977a24233a498143ffd577c31
Jeff King <peff@peff.net>
2025-11-18 04:35:19 -0500

    ci(windows-meson-test): handle options and output like other test jobs
    The GitHub windows-meson-test jobs directly run "meson test" with the
    --slice option. This means they skip all of the ci/lib.sh
    infrastructure, and in particular:
    
      1. They do not actually set any GIT_TEST_OPTS like --verbose-log or
         -x.
    
      2. They do not do the usual handle_failed_tests() magic to print test
         failures or tar up failed directories.
    
    As a result, you get almost no feedback at all when a test fails in this
    job, making debugging rather tricky.
    
    Let's try to make this behave more like the other CI jobs. Because we're
    on Windows, we can't just use the normal run-build-and-tests.sh script.
    Our build runs as a separate job (like the non-meson Windows job), and
    then we parallelize the tests across several job slices. So we need
    something like the run-test-slice.sh script that the "windows-test" job
    uses.
    
    In theory we could just swap out the "make" invocation there for
    "meson". But it doesn't quite work, because "make" knows how to pull
    GIT_TEST_OPTS out of GIT-BUILD-OPTIONS automatically. But for meson, we
    have to extract them into the --test-args option ourselves. I tried
    making the logic in run-test-slice.sh conditional, but there ended up
    being hardly any common code at all (and there are some tricky ordering
    constraints). So I added up with a new meson-specific test-slice runner.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ ci/run-test-slice-meson.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# We must load the build options so we know where to find
+# things like TEST_OUTPUT_DIRECTORY. This has to come before
+# loading lib.sh, though, because it may clobber some CI lib
+# variables like our custom GIT_TEST_OPTS.
+. "$1"/GIT-BUILD-OPTIONS
+. ${0%/*}/lib.sh
+
+group "Run tests" \
+	meson test -C "$1" --no-rebuild --print-errorlogs \
+		--test-args="$GIT_TEST_OPTS" --slice "$((1+$2))/$3" ||
+handle_failed_tests
