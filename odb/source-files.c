7e0aa0ab803405076b82ca66d328d314d17870ac
Patrick Steinhardt <ps@pks.im>
2026-03-05 15:19:44 +0100

    odb: move reparenting logic into respective subsystems
    The primary object database source may be initialized with a relative
    path. When the process changes its current working directory we thus
    have to update this path and have it point to the same path, but
    relative to the new working directory.
    
    This logic is handled in the object database layer. It consists of three
    steps:
    
      1. We undo any potential temporary object directory, which are used
         for transactions. This is done so that we don't end up modifying
         the temporary object database source that got applied for the
         transaction.
    
      2. We then iterate through the non-transactional sources and reparent
         their respective paths.
    
      3. We reapply the temporary object directory, but update its path.
    
    All of this logic is heavily tied to how the object database source
    handles paths in the first place. It's an internal implementation
    detail, and as sources may not even use an on-disk path at all it is not
    a mechanism that applies to all potential sources.
    
    Refactor the code so that the logic to reparent the sources is hosted by
    the "files" source and the temporary object directory subsystems,
    respectively. This logic is easier to reason about, but it also ensures
    that this logic is handled at the correct level.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- odb/source-files.c
+++ odb/source-files.c
@@ -1,13 +1,28 @@
 #include "git-compat-util.h"
+#include "abspath.h"
+#include "chdir-notify.h"
 #include "object-file.h"
 #include "odb/source.h"
 #include "odb/source-files.h"
 #include "packfile.h"
 
+static void odb_source_files_reparent(const char *name UNUSED,
+				      const char *old_cwd,
+				      const char *new_cwd,
+				      void *cb_data)
+{
+	struct odb_source_files *files = cb_data;
+	char *path = reparent_relative_path(old_cwd, new_cwd,
+					    files->base.path);
+	free(files->base.path);
+	files->base.path = path;
+}
+
 void odb_source_files_free(struct odb_source_files *files)
 {
 	if (!files)
 		return;
+	chdir_notify_unregister(NULL, odb_source_files_reparent, files);
 	odb_source_loose_free(files->loose);
 	packfile_store_free(files->packed);
 	odb_source_release(&files->base);
@@ -24,6 +39,14 @@ struct odb_source_files *odb_source_files_new(struct object_database *odb,
 	odb_source_init(&files->base, odb, path, local);
 	files->loose = odb_source_loose_new(&files->base);
 	files->packed = packfile_store_new(&files->base);
+
+	/*
+	 * Ideally, we would only ever store absolute paths in the source. This
+	 * is not (yet) possible though because we access and assume relative
+	 * paths in the primary ODB source in some user-facing functionality.
+	 */
+	if (!is_absolute_path(path))
+		chdir_notify_register(NULL, odb_source_files_reparent, files);
 
 	return files;
 }
