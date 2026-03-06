6005932d95ff05541f9dbe8c49a45b7abaf7432e
Mirko Faina <mroik@delayed.space>
2026-03-07 00:34:42 +0100

    format-patch: add ability to use alt cover format
    Often when sending patch series there's a need to clarify to the
    reviewer what's the purpose of said series, since it might be difficult
    to understand it from reading the commits messages one by one.
    
    "git format-patch" provides the useful "--cover-letter" flag to declare
    if we want it to generate a template for us to use. By default it will
    generate a "git shortlog" of the changes, which developers find less
    useful than they'd like, mainly because the shortlog groups commits by
    author, and gives no obvious chronological order.
    
    Give format-patch the ability to specify an alternative format spec
    through the "--cover-letter-format" option. This option either takes
    "shortlog", which is the current format, or a format spec prefixed with
    "log:".
    
    Example:
        git format-patch --cover-letter \
            --cover-letter-format="log:[%(count)/%(total)] %s (%an)" HEAD~3
    
        [1/3] this is a commit summary (Mirko Faina)
        [2/3] this is another commit summary (Mirko Faina)
        ...
    
    Signed-off-by: Mirko Faina <mroik@delayed.space>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t4014-format-patch.sh
+++ t/t4014-format-patch.sh
@@ -380,6 +380,54 @@ test_expect_success 'filename limit applies only to basename' '
 	done
 '
 
+test_expect_success 'cover letter with subject, author and count' '
+	rm -rf patches &&
+	test_when_finished "git reset --hard HEAD~1" &&
+	test_when_finished "rm -rf patches result test_file" &&
+	touch test_file &&
+	git add test_file &&
+	git commit -m "This is a subject" &&
+	git format-patch --cover-letter \
+	--cover-letter-format="log:[%(count)/%(total)] %s (%an)" -o patches HEAD~1 &&
+	grep "^\[1/1\] This is a subject (A U Thor)$" patches/0000-cover-letter.patch >result &&
+	test_line_count = 1 result
+'
+
+test_expected_success 'cover letter with author and count' '
+	test_when_finished "git reset --hard HEAD~1" &&
+	test_when_finished "rm -rf patches result test_file" &&
+	touch test_file &&
+	git add test_file &&
+	git commit -m "This is a subject" &&
+	git format-patch --cover-letter \
+	--cover-letter-format="log:[%(count)/%(total)] %an" -o patches HEAD~1 &&
+	grep "^\[1/1\] A U Thor$" patches/0000-cover-letter.patch >result &&
+	test_line_count = 1 result
+'
+
+test_expect_success 'cover letter shortlog' '
+	test_when_finished "git reset --hard HEAD~1" &&
+	test_when_finished "rm -rf patches result test_file" &&
+	touch test_file &&
+	git add test_file &&
+	git commit -m "This is a subject" &&
+	git format-patch --cover-letter --cover-letter-format=shortlog \
+	-o patches HEAD~1 &&
+	sed -n -e "/^A U Thor/p;" patches/0000-cover-letter.patch >result &&
+	test_line_count = 1 result
+'
+
+test_expect_success 'cover letter no format' '
+	test_when_finished "git reset --hard HEAD~1" &&
+	test_when_finished "rm -rf patches result test_file" &&
+	touch test_file &&
+	git add test_file &&
+	git commit -m "This is a subject" &&
+	git format-patch --cover-letter -o patches HEAD~1 &&
+	sed -n -e "/^A U Thor/p;" patches/0000-cover-letter.patch >result &&
+	test_line_count = 1 result
+'
+
 test_expect_success 'reroll count' '
 	rm -fr patches &&
 	git format-patch -o patches --cover-letter --reroll-count 4 main..side >list &&
