916b45080534e9603094dda39c4599a2f2a466d1
Jiamu Sun <39@barroit.sh>
2026-03-17 00:36:23 +0900

    doc: document autocorrect API
    Explain behaviors for autocorrect_resolve(), autocorrect_confirm(), and
    struct autocorrect.
    
    Signed-off-by: Jiamu Sun <39@barroit.sh>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- autocorrect.h
+++ autocorrect.h
@@ -9,13 +9,24 @@ enum autocorrect_mode {
 	AUTOCORRECT_DELAY,
 };
 
+/**
+ * `mode` indicates which action will be performed by autocorrect_confirm().
+ * `delay` is the timeout before autocorrect_confirm() returns, in tenths of a
+ * second. Use it only with AUTOCORRECT_DELAY.
+ */
 struct autocorrect {
 	enum autocorrect_mode mode;
 	int delay;
 };
 
+/**
+ * Resolve the autocorrect configuration into `conf`.
+ */
 void autocorrect_resolve(struct autocorrect *conf);
 
+/**
+ * Interact with the user in different ways depending on `conf->mode`.
+ */
 void autocorrect_confirm(struct autocorrect *conf, const char *assumed);
 
 #endif /* AUTOCORRECT_H */
