9168faad08cc594645db1adf4359a66a65764fff
Patrick Steinhardt <ps@pks.im>
2026-03-31 14:13:03 +0200

    t: prepare `test_match_signal ()` calls for `set -e`
    We have a couple of calls to `test_match_signal ()` where we execute a
    Git command and expect it to die with a specific signal. These calls
    will essentially execute the process in a subshell via `foo; echo $?`,
    but as we expect `foo` to fail this will cause the overall subshell to
    fail once we `set -e`.
    
    Fix this issue by using `foo && echo 0 || echo $?` instead.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>

--- t/t0005-signals.sh
+++ t/t0005-signals.sh
@@ -42,12 +42,12 @@ test_expect_success 'create blob' '
 '
 
 test_expect_success !MINGW 'a constipated git dies with SIGPIPE' '
-	OUT=$( ((large_git; echo $? 1>&3) | :) 3>&1 ) &&
+	OUT=$( ((large_git && echo 0 1>&3 || echo $? 1>&3) | :) 3>&1 ) &&
 	test_match_signal 13 "$OUT"
 '
 
 test_expect_success !MINGW 'a constipated git dies with SIGPIPE even if parent ignores it' '
-	OUT=$( ((trap "" PIPE && large_git; echo $? 1>&3) | :) 3>&1 ) &&
+	OUT=$( ((trap "" PIPE && large_git && echo 0 1>&3 || echo $? 1>&3) | :) 3>&1 ) &&
 	test_match_signal 13 "$OUT"
 '
 
