8a121f230095cd57851fbedec0f0fee189209229
Tian Yuchen <cat@malon.dev>
2026-05-28 20:03:04 +0800

    environment: move 'trust_executable_bit' into repo_config_values
    Move the global 'trust_executable_bit' configuration into the
    repository-specific 'repo_config_values' struct.
    
    For now, associated functions in read-cache.c access this configuration
    by explicitly falling back to 'the_repository'.
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Ayush Chandekar <ayu.chandekar@gmail.com>
    Mentored-by: Olamide Caleb Bello <belkid98@gmail.com>
    Signed-off-by: Tian Yuchen <cat@malon.dev>

--- apply.c
+++ apply.c
@@ -3890,10 +3890,12 @@ static int check_preimage(struct apply_state *state,
 	}
 
 	if (!state->cached && !previous) {
+		struct repo_config_values *cfg = repo_config_values(the_repository);
+
 		if (*ce && !(*ce)->ce_mode)
 			BUG("ce_mode == 0 for path '%s'", old_name);
 
-		if (trust_executable_bit || !S_ISREG(st->st_mode))
+		if (cfg->trust_executable_bit || !S_ISREG(st->st_mode))
 			st_mode = ce_mode_from_stat(*ce, st->st_mode);
 		else if (*ce)
 			st_mode = (*ce)->ce_mode;
--- environment.h
+++ environment.h
@@ -90,6 +90,7 @@ struct repository;
 struct repo_config_values {
 	/* section "core" config values */
 	char *attributes_file;
+	int trust_executable_bit;
 	int apply_sparse_checkout;
 
 	/* section "branch" config values */
@@ -160,7 +161,6 @@ int is_bare_repository(void);
 extern char *git_work_tree_cfg;
 
 /* Environment bits from configuration mechanism */
-extern int trust_executable_bit;
 extern int trust_ctime;
 extern int check_stat;
 extern int has_symlinks;
