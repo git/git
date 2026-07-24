1b63735c5f6d9682be5739d28d95d714a2ee47de
Pablo Sabater <pabloosabaterr@gmail.com>
2026-07-24 23:24:00 +0200

    cat-file: unify default format
    %(objecttype) is supported both by the client and by the server.
    Change the temporary default format to the unified version that the
    other commands use.
    
    Update documentation to remove %(objecttype) from the caveats of
    remote-object-info and show %(objecttype) support.
    
    Now that type is supported and the default format unified, update the
    tests to expect the new default format.
    
    Mentored-by: Karthik Nayak <karthik.188@gmail.com>
    Mentored-by: Chandra Pratap <chandrapratap3519@gmail.com>
    Signed-off-by: Pablo Sabater <pabloosabaterr@gmail.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -821,15 +821,9 @@ static void parse_cmd_remote_object_info(struct batch_options *opt,
 	char *line_to_split;
 	struct fetch_object_info_results results = FETCH_OBJECT_INFO_RESULTS_INIT;
 	struct oid_array object_info_oids = OID_ARRAY_INIT;
-	const char *saved_format = opt->format;
 
 	if (strlen(line) >= MAX_REMOTE_OBJ_INFO_LINE)
 		die(_("remote-object-info command too long"));
-	/*
-	 * TODO: Use the default format once %(objecttype) is supported.
-	 */
-	if (!opt->format)
-		opt->format = "%(objectname) %(objectsize)";
 
 	line_to_split = xstrdup(line);
 	count = split_cmdline(line_to_split, &argv);
@@ -881,7 +875,6 @@ static void parse_cmd_remote_object_info(struct batch_options *opt,
 		data->is_remote = 0;
 	}
 	data->skip_object_info = 0;
-	opt->format = saved_format;
 
 	free_fetch_object_info_results(&results);
 	free(line_to_split);
