2b27cabfb9fbe09559aaf7ba7d35d112e0156c70
K Jayatheerth <jayatheerthkulkarni2005@gmail.com>
2026-09-11 20:15:19 +0530

    repo: add path.cdup
    Scripts sometimes need the relative path from the current working
    directory to the repository's working tree root (cdup). While this
    information can be retrieved through `git rev-parse --show-cdup`,
    `git repo info` does not currently expose it as a scriptable key.
    
    Introduce the `path.cdup` key to `git repo info`. The key returns the
    path from the current working directory to the root of the working tree,
    returning the empty string when invoked from the working tree root.
    
    Mentored-by: Justin Tobler <jltobler@gmail.com>
    Mentored-by: Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
    Signed-off-by: K Jayatheerth <jayatheerthkulkarni2005@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1900-repo-info.sh
+++ t/t1900-repo-info.sh
@@ -215,6 +215,29 @@ test_repo_info_path 'commondir with only GIT_DIR' 'commondir' \
 	'.git' \
 	'GIT_DIR="../.git" && export GIT_DIR'
 
+test_expect_success 'path.cdup at repository root' '
+	test_when_finished "rm -rf repo" &&
+	git init repo &&
+	(
+		cd repo &&
+		echo "path.cdup=" >expect &&
+		git repo info path.cdup >actual &&
+		test_cmp expect actual
+	)
+'
+
+test_expect_success 'path.cdup in subdirectory' '
+	test_when_finished "rm -rf repo" &&
+	git init repo &&
+	mkdir -p repo/sub/dir &&
+	(
+		cd repo/sub/dir &&
+		echo "path.cdup=../../" >expect &&
+		git repo info path.cdup >actual &&
+		test_cmp expect actual
+	)
+'
+
 test_expect_success 'path.git-prefix at repository root' '
 	test_when_finished "rm -rf repo" &&
 	git init repo &&
