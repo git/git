5c86262d4d3c09996df93c458dc7129fce7187a5
Jeff King <peff@peff.net>
2026-09-11 13:11:39 -0400

    merge-ll: catch close() errors when writing external tempfiles
    When writing out tempfiles for an external merge driver, we catch the
    case that write() fails, but not the follow-up close(). This close()
    would usually succeed, but the system could report a delayed write error
    (e.g., on a network file system).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-ll.c
+++ merge-ll.c
@@ -180,9 +180,9 @@ static void create_temp(mmfile_t *src, char *path, size_t len)
 
 	xsnprintf(path, len, ".merge_file_XXXXXX");
 	fd = xmkstemp(path);
-	if (write_in_full(fd, src->ptr, src->size) < 0)
+	if (write_in_full(fd, src->ptr, src->size) < 0 ||
+	    close(fd) < 0)
 		die_errno("unable to write temp-file");
-	close(fd);
 }
 
 /*
