ce389b95560842bd8970a7f49b4a42bf6d6185bb
Tyler Cipriani <tyler@tylercipriani.com>
2026-09-15 17:33:04 -0600

    push: fix --force-if-includes non-branch advice
    When a --force-if-includes push is rejected due lacking reflog to
    consult, the advice is misleading:
    
         ! [rejected] HEAD -> main (remote ref updated since checkout)
        error: failed to push some refs to '<remote>'
        hint: Updates were rejected because the tip of the remote-tracking
        hint: branch has been updated since the last checkout. If you want
        hint: to integrate the remote changes, use 'git pull' before
        hint: pushing again. See the 'Note about fast-forwards' in 'git
        hint: push --help' for details.
    
    But a `git pull` will not fix this rejection. What is required is either
    
    - Specify the expected remote tip with --force-with-lease=<ref>:<expect>
    - Ignore the error with --no-force-if-includes
    
    Add ref->unverifiable to differentiate pushing something without a
    reflog to consult vs. a remote update rejection.
    
    Ensure tests check the rejection message.
    
    Reported-by: D. Ben Knoble <ben.knoble@gmail.com>
    Signed-off-by: Tyler Cipriani <tyler@tylercipriani.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- remote.c
+++ remote.c
@@ -1701,6 +1701,9 @@ void set_ref_status_for_push(struct ref *remote_refs, int send_mirror,
 			else if (ref->check_reachable && ref->unreachable)
 				reject_reason =
 					REF_STATUS_REJECT_REMOTE_UPDATED;
+			else if (ref->check_reachable && ref->unverifiable)
+				reject_reason =
+					REF_STATUS_REJECT_UNVERIFIABLE;
 			else
 				/*
 				 * If the ref isn't stale, and is reachable
@@ -2826,7 +2829,7 @@ static void check_if_includes_upstream(struct ref *remote)
 	 * then there is no reliable reflog to check
 	 */
 	if (!name || !starts_with(name, "refs/heads/")) {
-		remote->unreachable = 1;
+		remote->unverifiable = 1;
 		return;
 	}
 
